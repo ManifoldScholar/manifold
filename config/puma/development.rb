316351ea2de0066e77a96b253dae60d048ef9a3b
Zach Davis <zach@castironcoding.com>
2015-10-25 08:23:11 -0700

    [CHORE] Simplify running in prod env on developer machine
    Add environment config to Idea development server run configuration
    and adjusts puma config to be aware of boxen socket env vars.

--- config/puma/development.rb
+++ config/puma/development.rb
@@ -5,14 +5,20 @@ pidfile 'tmp/pids/puma.pid'
 state_path 'tmp/pids/puma.state'
 threads 0, 16
 tag 'manifold-api'
+environment 'development'
+
+name = 'manifold-api'
 
 if ENV['BOXEN_SOCKET_DIR']
-  socket_path = "unix://#{ENV['BOXEN_SOCKET_DIR']}"
-  the_environment = 'development'
+  socket_dir = "unix://#{ENV['BOXEN_SOCKET_DIR']}"
+  socket_path = "#{socket_dir}/#{name}"
+elsif ENV['APP_SOCKET_DIR']
+  socket_dir  = "unix://#{ENV['APP_SOCKET_PATH']}"
+  socket_path = "#{socket_dir}/#{name}"
 else
-  socket_path = "unix://#{ENV['APP_SOCKET_PATH']}"
-  the_environment = 'production'
+  socket_dir = "unix://#{ENV['RAILS_SERVER_SOCKET_DIR']}"
+  socket_path = "unix://#{ENV['RAILS_SERVER_SOCKET_PATH']}"
 end
 
-bind "#{socket_path}/manifold-api"
-activate_control_app "#{socket_path}/manifold-api-control"
+bind socket_path
+activate_control_app "#{socket_dir}/#{name}-control"
