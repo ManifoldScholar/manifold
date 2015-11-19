969bbea74fc5cec5abdc8b9b7de0d5514f9c1968
Zach Davis <zach@castironcoding.com>
2015-11-19 13:57:26 -0800

    [BUGIFX] Restore font files to webpack conf

--- client/webpack/dev.config.js
+++ client/webpack/dev.config.js
@@ -81,10 +81,10 @@ module.exports = {
         '!css?importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]' +
         '!autoprefixer?browsers=last 2 version' +
         '!sass?outputStyle=expanded&sourceMap'},
-      //{ test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      //{ test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      //{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-      //{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
       { test: webpackIsomorphicToolsPlugin.regular_expression('images'), loader: 'url-loader?limit=10240' }
     ]
