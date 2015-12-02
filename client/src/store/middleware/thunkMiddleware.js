cb8e80ea079743efbe59797b3ce1d20c99e62dac
Zach Davis <zach@castironcoding.com>
2015-12-02 15:59:58 -0800

    [F] Add thunk middleware to client store
    Thunk middleware allows actions to return a function. The middleware
    will call the function, passing it dispatch and getStore. The function
    should, in turn, return an action object. Manifold needs this middleware
    for the upcoming authenticated API fetch actions, which will need access
    to state in order to grab the authToken and pass it in the request.

---
+++ client/src/store/middleware/thunkMiddleware.js
@@ -0,0 +1,11 @@
+export default function thunkMiddleware({ dispatch, getState }) {
+  return (next) => (action) => {
+    if (typeof action === 'function') {
+      action(dispatch, getState);
+    } else {
+      return next(action);
+
+    }
+  };
+}
+
