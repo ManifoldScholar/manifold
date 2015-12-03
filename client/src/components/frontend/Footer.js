7c47625c27a2d7ea614fff7ec6f652e395b32520
Naomi Rubin <naomi@castironcoding.com>
2015-12-03 15:32:00 -0800

    [F] Add footer with stub navigation/colophon
    [FEATURE #108400688] Stub markup for footer seen in browse views and
    basic styling for wide-screen displays. This commit also includes
    styling for inputs with styles featured in the footer comps as well as
    inputs with inline icon/buttons.

--- client/src/components/frontend/Footer.js
+++ client/src/components/frontend/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import {Link} from 'react-router';
+import {Link} from 'react-router';
 
 export default class Footer extends Component {
 
@@ -7,8 +7,95 @@ export default class Footer extends Component {
 
   render = () => {
     return (
-        <section className="footer-browse">
-        </section>
+        <footer className="footer-browse">
+          <div className="container">
+            <div className="rel">
+              <Link to={'/browse'} className="logo">
+                <i className="manicon manicon-manifold-logo"></i>
+                {'Manifold'}
+              </Link>
+            </div>
+            <nav className="text-nav">
+              <ul>
+                <li>
+                  <ul>
+                    <li>
+                      <a href="#">
+                        {'Log In'}
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        {'Projects'}
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        {'About'}
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <ul>
+                    <li>
+                      <a href="#">
+                        {'Publishers'}
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        {'Terms'}
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        {'Contact'}
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <ul>
+                    <li>
+                      <a href="#">
+                        {'Twitter'}
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        {'Facebook'}
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        {'Email'}
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+
+            <section className="footer-secondary">
+              <form>
+                <div className="search-button-inline">
+                  <input type="text" placeholder="Search"/>
+                  <button className="manicon manicon-magnify">
+                    <span className="screen-reader-text">Click to submit search query</span>
+                  </button>
+                </div>
+              </form>
+              <p className="colophon">
+                {'Search © 2016 Manifold. Vestibulum id ligula porta felis '}
+                <Link to="{#}">
+                  {'euismod semper'}
+                </Link>
+                {'. Vivamus sagittis lacus vel.Twitter'}
+              </p>
+            </section>
+          </div>
+        </footer>
     );
   };
 }
