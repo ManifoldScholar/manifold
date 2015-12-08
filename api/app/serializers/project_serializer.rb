cea347d0813c72bca77b03992fabf60d920df24c
Zach Davis <zach@castironcoding.com>
2015-12-08 15:09:03 -0800

    [F] Add alpha project serializer

---
+++ api/app/serializers/project_serializer.rb
@@ -0,0 +1,9 @@
+# Provides a partial serialization of a project model.
+class ProjectSerializer < ActiveModel::Serializer
+  attributes :id, :title, :subtitle, :published, :published_datetime, :description,
+             :cover_url, :created_at, :updated_at, :featured
+
+  has_many :creators, serializer: MakerSerializer
+  has_many :contributors, serializer: MakerSerializer
+  has_many :texts, serializer: TextPartialSerializer
+end
