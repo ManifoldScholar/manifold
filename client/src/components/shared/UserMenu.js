d8c02e4f689bd792b986fac2b8df6c3817450169
Naomi Rubin <naomi@castironcoding.com>
2015-12-18 17:06:41 -0800

    [WIP] Initial component refactoring of dropdowns

--- client/src/components/shared/UserMenu.js
+++ client/src/components/shared/UserMenu.js
@@ -1,9 +1,9 @@
 import React, { Component, PropTypes } from 'react';
+import { Dropdown } from '../../components/shared';
 import { UserMenuButton } from './';
-import { Link } from 'react-router';
-import classNames from 'classnames';
+import { UserMenuBody } from './';
 
-export default class UserButton extends Component {
+export default class UserMenu extends Component {
 
   static propTypes = {
     toggleUserMenu: PropTypes.func,
@@ -14,43 +14,13 @@ export default class UserButton extends Component {
     visible: PropTypes.bool
   };
 
-  logout = () => {
-    this.props.startLogout();
-    this.props.hideUserMenu();
-  };
-
   render = () => {
-    const menuClass = classNames({
-      'user-menu': true,
-      'menu-hidden': !this.props.visible,
-      'menu-visible': this.props.visible
-    });
+
     return (
-        <div className="container-user-menu">
-          <UserMenuButton
-              toggleUserMenu={this.props.toggleUserMenu}
-              showLoginOverlay={this.props.showLoginOverlay}
-              authenticated={this.props.authenticated}
-              menuVisible={this.props.visible}
-          />
-          <nav className={menuClass}>
-            <i className="tail"></i>
-            <ul>
-              <li>
-                <Link to="#">
-                  <i className="manicon manicon-person-pencil"></i>
-                  {'Edit Profile'}
-                </Link>
-              </li>
-              <li>
-                <button onClick={this.logout}>
-                  <i className="manicon manicon-circle-arrow-out-right"></i>
-                  {'Logout'}
-                </button>
-              </li>
-            </ul>
-          </nav>
-        </div>
+      <Dropdown
+        triggerComponent={UserMenuButton}
+        bodyComponent={UserMenuBody}
+      />
     );
   };
 }
