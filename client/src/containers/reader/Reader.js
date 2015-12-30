c9a5ecc9d6c1e7d296b0b1a37d6b31087bdb09ec
Naomi Rubin <naomi@castironcoding.com>
2015-12-30 15:02:35 -0800

    [C] Fix linter warning for history PropTypes

--- client/src/containers/reader/Reader.js
+++ client/src/containers/reader/Reader.js
@@ -45,7 +45,8 @@ class Reader extends Component {
     text: PropTypes.object,
     visibility: PropTypes.object,
     authentication: PropTypes.object,
-    dispatch: PropTypes.func
+    dispatch: PropTypes.func,
+    history: PropTypes.object
   };
 
   static contextTypes = {
