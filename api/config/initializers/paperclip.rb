a931f8b0fc9819983403561e6bf80bbe3e839a29
Zach Davis <zach@castironcoding.com>
2016-02-28 08:21:37 -0800

    [B] Address Paperclip spoofing false positives
    On the staging host, Paperclip repeatedly reports non-spoofed
    attachments as spoofed, which breaks ingestion. We're quick-fixing it
    with a monkey patch for now, and in doing so will incur some technical
    debt. We'll circle back later and attempt to come up with a more robust
    fix, perhaps by avioding paperclip alltogether.
    
    See https://github.com/thoughtbot/paperclip/issues/1429 for more on
    this issue.

--- api/config/initializers/paperclip.rb
+++ api/config/initializers/paperclip.rb
@@ -5,3 +5,12 @@ Paperclip.options[:content_type_mappings] = {
   ttf: ["application/x-font-ttf", "application/x-font-truetype"],
   webm: ["video/webm", "audio/webm"]
 }
+
+require 'paperclip/media_type_spoof_detector'
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+      false
+    end
+  end
+end
