6c7006b397ab91fb39d75bcf0054fd35a1aaafa8
Naomi Rubin <naomi@castironcoding.com>
2016-03-07 10:53:41 -0800

    [B] Project's published text is optional association

--- api/app/models/project.rb
+++ api/app/models/project.rb
@@ -1,7 +1,7 @@
 # The project model is the primary unit of Manifold.
 class Project < ActiveRecord::Base
   has_many :texts
-  belongs_to :published_text, class_name: "Text"
+  belongs_to :published_text, class_name: "Text", optional: true
   has_many :text_categories, -> { for_text }, class_name: "Category"
   has_many :resource_categories, -> { for_resource }, class_name: "Category"
 
