523a1f5e96e5c1e471d7bed0530534dcbe81fb4c
Zach Davis <zach@castironcoding.com>
2016-03-08 11:49:58 -0800

    [C] Further lint API

--- api/spec/models/resource_spec.rb
+++ api/spec/models/resource_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Resource, type: :model do
   end
 
   it { is_expected.to have_attached_file(:attachment) }
-  #it { is_expected.to validate_attachment_presence(:attachment) }
+  # it { is_expected.to validate_attachment_presence(:attachment) }
   # it do
   #   is_expected.to validate_attachment_content_type(:attachment).allowing("image/gif")
   # end
