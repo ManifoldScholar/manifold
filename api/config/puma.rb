0918cd48c92548dde0554f5fcf422b0e6041318e
Zach Davis <zach@castironcoding.com>
2016-04-02 17:40:11 -0700

    [C] Remove puma config redundancy

--- api/config/puma.rb
+++ api/config/puma.rb
@@ -5,8 +5,7 @@ pidfile "tmp/pids/puma.pid"
 state_path "tmp/pids/puma.state"
 threads 0, 16
 tag "manifold-api"
-environment "production"
-
+environment ENV["RAILS_ENV"] || "development"
 name = "manifold-api"
 
 if ENV["BOXEN_SOCKET_DIR"]
@@ -19,6 +18,5 @@ else
   socket_dir = "unix://#{ENV['RAILS_SERVER_SOCKET_DIR']}"
   socket_path = "unix://#{ENV['RAILS_SERVER_SOCKET_PATH']}"
 end
-
 bind socket_path
 activate_control_app "#{socket_dir}/#{name}-control"
