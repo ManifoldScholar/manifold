e6531e755b400b2ea395f3c7b2ed604d8aa1fb57
Zach Davis <zach@castironcoding.com>
2016-05-05 17:41:31 -0700

    [F] API returns camelized keys

--- api/config/initializers/active_model_serializers.rb
+++ api/config/initializers/active_model_serializers.rb
@@ -1,3 +1,3 @@
 ActiveModelSerializers.config.adapter = ActiveModelSerializers::Adapter::JsonApi
-ActiveModelSerializers.config.key_transform = :unaltered
+ActiveModelSerializers.config.key_transform = :camel_lower
 
