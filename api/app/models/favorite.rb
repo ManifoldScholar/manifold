fa8682ca7deea414cf7bc2c2e923ba1b5ec5988e
Zach Davis <zach@castironcoding.com>
2016-09-08 09:37:15 -0700

    [B] Fix failing tests

--- api/app/models/favorite.rb
+++ api/app/models/favorite.rb
@@ -3,6 +3,7 @@ class Favorite < ActiveRecord::Base
   belongs_to :user
   belongs_to :favoritable, polymorphic: true
   validates :favoritable_id, uniqueness: { scope: [:user_id, :favoritable_type] }
+  validates :user, presence: true
 
   scope :only_projects, -> { where(favoritable_type: "Project") }
   scope :only_texts, -> { where(favoritable_type: "Text") }
--- api/spec/models/page_spec.rb
+++ api/spec/models/page_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Page, type: :model do
   it "creates a slug from the nav title" do
     FactoryGirl.create(:page, nav_title: "My Page", slug: nil)
     page = Page.last
-    expect(page.slug).to be("my-page")
+    expect(page.slug).to eq("my-page")
   end
 
 end
