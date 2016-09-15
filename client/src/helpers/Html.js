dbb1602f581c110d4d834e2b4f05b4e460a394dc
Naomi Rubin <naomi@castironcoding.com>
2016-09-15 16:03:35 -0700

    [C] Add viewport meta for mobile devices

--- client/src/helpers/Html.js
+++ client/src/helpers/Html.js
@@ -27,6 +27,7 @@ export default class Html extends Component {
       <html lang="en-us">
         <head>
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
           {DocumentMeta.renderAsReact()}
 
           <link rel="shortcut icon" href="/favicon.ico" />
