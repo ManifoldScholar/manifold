b235a07d0142835c0b37fdf5947d28abda67294a
Naomi Rubin <naomi@castironcoding.com>
2016-11-04 11:51:59 -0700

    [F] Add backend icons, secondary header style

--- client/src/containers/backend/BackendProjectDetail/Wrapper.js
+++ client/src/containers/backend/BackendProjectDetail/Wrapper.js
@@ -2,16 +2,16 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 export default class BackendWrapper extends Component {
-  //static mapStateToProps(state) {
-  //  return {
-  //    project: select(requests.showProjectDetail, state.entityStore)
-  //  };
-  //}
+  // static mapStateToProps(state) {
+  //   return {
+  //     project: select(requests.showProjectDetail, state.entityStore)
+  //   };
+  // }
 
   static propTypes = {};
 
   render() {
-    return(
+    return (
       <div>
         {'hi!'}
       </div>
