11a5db46d19b76ff08976de8bd6e9c79056b2782
Zach Davis <zach@castironcoding.com>
2016-11-04 12:08:42 -0700

    [C] Clean up initializers

--- api/config/initializers/01_json_param_key_transform.rb
+++ api/config/initializers/01_json_param_key_transform.rb
@@ -1,7 +1,6 @@
 # Transform JSON request param keys from JSON-conventional camelCase to
 # Rails-conventional snake_case:
 ActionDispatch::Request.parameter_parsers[Mime[:json].symbol] = lambda do |raw_post|
-  Rails.logger.info("RAMBO CALLED THIS LAMBDA")
   data = ActiveSupport::JSON.decode(raw_post)
   data = { _json: data } unless data.is_a?(Hash)
   data.deep_transform_keys!(&:underscore)
