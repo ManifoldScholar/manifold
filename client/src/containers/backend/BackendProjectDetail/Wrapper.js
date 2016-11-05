61e972c66e880e2a29248a30ebfa46f69e91587b
Naomi Rubin <naomi@castironcoding.com>
2016-11-04 17:14:06 -0700

    [F] Add static header to backend project detail

--- client/src/containers/backend/BackendProjectDetail/Wrapper.js
+++ client/src/containers/backend/BackendProjectDetail/Wrapper.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 export default class BackendWrapper extends Component {
   // static mapStateToProps(state) {
@@ -12,9 +13,32 @@ export default class BackendWrapper extends Component {
 
   render() {
     return (
-      <div>
-        {'hi!'}
-      </div>
+      <section className="bg-neutral95">
+        <div className="container flush">
+          <Link to={'/backend/dashboard'} className="back-link-primary">
+             <i className="manicon manicon-arrow-left"></i>
+             Back to: <span>ALL PROJECTS</span>
+          </Link>
+          <header className="project-header">
+            <figure>
+              <i className="manicon manicon-project-placeholder"></i>
+            </figure>
+            <div className="project-title">
+              <h1>
+                Japanese Documentary Film
+                <span className="subtitle">
+                  The Meiji Era through Hiroshima
+                </span>
+              </h1>
+              <div className="project-utility">
+                <button className="button-bare-primary">Preview <i className="manicon manicon-eye-outline"></i></button>
+                <button className="button-bare-primary">Duplicate <i className="manicon manicon-check-double"></i></button>
+                <button className="button-bare-primary">Delete <i className="manicon manicon-trashcan"></i></button>
+              </div>
+            </div>
+          </header>
+        </div>
+      </section>
     );
   }
 }
