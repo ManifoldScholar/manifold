0174a68b7e3306b6232b53c0c6909eb7387ef27f
Naomi Rubin <naomi@castironcoding.com>
2016-11-08 11:58:19 -0800

    [F] Add static upload form to backend
    FEATURE #133878071

--- client/src/components/backend/Form/index.js
+++ client/src/components/backend/Form/index.js
@@ -2,10 +2,12 @@ import Makers from './Makers.js';
 import Radios from './Radios.js';
 import TextArea from './TextArea.js';
 import TextInput from './TextInput.js';
+import Upload from './Upload.js';
 
 export default {
   Makers,
   Radios,
   TextArea,
-  TextInput
+  TextInput,
+  Upload
 };
