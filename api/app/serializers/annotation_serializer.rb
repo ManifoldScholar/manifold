03661e0219b5b49997e8a508ef6731b2be33fce8
Zach Davis <zach@castironcoding.com>
2016-11-11 12:21:22 -0800

    [B] Correct annotation create

--- api/app/serializers/annotation_serializer.rb
+++ api/app/serializers/annotation_serializer.rb
@@ -2,5 +2,5 @@
 class AnnotationSerializer < ActiveModel::Serializer
   cache key: "annotation", expires_in: 3.hours
   attributes :created_at, :end_char, :end_node, :id, :start_char, :start_node,
-             :text_section_id, :updated_at, :user_id, :format
+             :text_section_id, :updated_at, :creator_id, :format
 end
