cdfe3391dae3e85a8e165a8893467aa24ce03539
Naomi Rubin <naomi@castironcoding.com>
2016-11-18 14:16:52 -0800

    [WIP] Add open animation to slide read-more
    FEATURE #133900955

--- client/src/components/frontend/ResourceList/Slide/Caption.js
+++ client/src/components/frontend/ResourceList/Slide/Caption.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
+import classNames from 'classnames';
 
 export default class ResourceSlideCaption extends Component {
   static propTypes = {
@@ -17,24 +18,58 @@ export default class ResourceSlideCaption extends Component {
   handleReadMore(event) {
     const transitionClass = 'transition-height';
 
-    if (this._description) {
-      this._description.style.height = 'auto';
-      const natHeight = this._description.offsetHeight;
-      this._description.style.height = '5em';
-      if (this._description.classList)
-        this._description.classList.add(transitionClass);
-      else
-        this._description.className += ' ' + transitionClass;
-
-      this._description.style.height = `calc(${natHeight}px + 2.5em)`;
-      this._description.style.height = `0`;
-      this._description.style.height = `calc(${natHeight}px + 2.5em)`;
+    if (this._description && !this.state.expanded) {
+      // Open description to full size
+      this.openDescription();
+    } else if (this._description && this.state.expanded) {
+      // Close description if it is open
+      this.closeDescription();
     }
+  }
+
+  // Can happen even if it's already open, will shrink if need be.
+  openDescription() {
+    const origHeight = this._description.offsetHeight;
+    this._description.style.height = 'auto';
+    const natHeight = this._description.offsetHeight;
+    this._description.style.height = origHeight + 'px';
+    this.setState({expanded: true});
+
+    // Ensure height transition doesn't run until class with transition
+    // has been added
+    const transitionInterval = setInterval(() => {
+      if (this.state.expanded) {
+        this._description.style.height = `${natHeight}px`;
+        clearInterval(transitionInterval);
+        setTimeout(() => {
+          this._description.style.height = 'auto';
+        }, 250)
+      }
+    }, 20);
+  }
+
+  closeDescription() {
+    // Manually set the height to transition back
+    this._description.style.height = this._description.offsetHeight + 'px';
+    this._description.style.height = '5em';
+    // Give a transition amount of time before changing the class state
+    setTimeout(() => {
+        this.setState({expanded: false});
+    }, 250);
   }
 
   render() {
     const resource = this.props.resource;
     const attr = resource.attributes;
+    const descriptionClass = classNames({
+      'resource-description': true,
+      'transition-height': this.state.expanded
+    });
+
+    const moreLinkClass = classNames({
+      'more-link': true,
+      'open': this.state.expanded
+    });
 
     return (
       <div className="slide-caption">
@@ -44,7 +79,7 @@ export default class ResourceSlideCaption extends Component {
           </h2>
         </header>
         <div
-          className="resource-description"
+          className={descriptionClass}
           ref={ (c) => {
             this._description = c;
           } }
@@ -55,8 +90,13 @@ export default class ResourceSlideCaption extends Component {
         </div>
         <div className="resource-utility">
           <div className="bg-neutral90">
-            <button className="more-link" onClick={this.handleReadMore}>
-              {'Read More'}
+            <button className={moreLinkClass} onClick={this.handleReadMore}>
+              <span className="open-text">
+                {'Read More'}
+              </span>
+              <span className="close-text">
+                {'Hide Description'}
+              </span>
             </button>
             <a href={attr.attachmentUrl} className="download-link">
               {'Download'} <i className="manicon manicon-arrow-down"></i>
