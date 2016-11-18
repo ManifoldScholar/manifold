8d1e67a28954181d39051c10e1a4395f2c8d8e36
Naomi Rubin <naomi@castironcoding.com>
2016-11-18 08:59:31 -0800

    [WIP] Add readmore to slideshow

--- client/src/components/frontend/ResourceList/Slide/Caption.js
+++ client/src/components/frontend/ResourceList/Slide/Caption.js
@@ -6,6 +6,32 @@ export default class ResourceSlideCaption extends Component {
     resource: PropTypes.object
   };
 
+  constructor() {
+    super();
+    this.state = {
+      expanded: false
+    };
+    this.handleReadMore = this.handleReadMore.bind(this);
+  }
+
+  handleReadMore(event) {
+    const transitionClass = 'transition-height';
+
+    if (this._description) {
+      this._description.style.height = 'auto';
+      const natHeight = this._description.offsetHeight;
+      this._description.style.height = '5em';
+      if (this._description.classList)
+        this._description.classList.add(transitionClass);
+      else
+        this._description.className += ' ' + transitionClass;
+
+      this._description.style.height = `calc(${natHeight}px + 2.5em)`;
+      this._description.style.height = `0`;
+      this._description.style.height = `calc(${natHeight}px + 2.5em)`;
+    }
+  }
+
   render() {
     const resource = this.props.resource;
     const attr = resource.attributes;
@@ -17,14 +43,19 @@ export default class ResourceSlideCaption extends Component {
             {attr.title}
           </h2>
         </header>
-        <div className="resource-description">
+        <div
+          className="resource-description"
+          ref={ (c) => {
+            this._description = c;
+          } }
+        >
           <p>
             {attr.description}
           </p>
         </div>
         <div className="resource-utility">
           <div className="bg-neutral90">
-            <button className="more-link">
+            <button className="more-link" onClick={this.handleReadMore}>
               {'Read More'}
             </button>
             <a href={attr.attachmentUrl} className="download-link">
