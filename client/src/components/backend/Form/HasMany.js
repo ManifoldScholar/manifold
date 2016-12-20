f3d66131c93d385c2cb1cfd0222ec76a7c5a7187
Naomi Rubin <naomi@castironcoding.com>
2016-12-20 12:21:49 -0800

    [B] Refactor makers list to has-many-list
    FIXES #135692067

--- client/src/components/backend/Form/HasMany.js
+++ client/src/components/backend/Form/HasMany.js
@@ -106,12 +106,12 @@ export default class Makers extends PureComponent {
         label={this.props.label}
       >
         <label>{this.props.label}</label>
-        <nav className="maker-utility-list">
+        <nav className="has-many-list">
           <ul>
             {entities.map((entity, index) => {
               return (
                 <li key={index} >
-                  <div className="maker">
+                  <div className="association">
                     { this.props.entityAvatarAttribute ?
                       <figure>
                         { entity.attributes[this.props.entityAvatarAttribute] ?
@@ -124,7 +124,7 @@ export default class Makers extends PureComponent {
                         }
                       </figure>
                     : null}
-                    <h4 className="maker-name">
+                    <h4 className="association-name">
                       {this.label(entity, this.props)}
                     </h4>
                   </div>
