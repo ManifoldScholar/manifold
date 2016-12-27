967648cd61f6fed8f21af20bfb4a6359c31dbe54
Zach Davis <zach@castironcoding.com>
2016-12-27 12:29:18 -0800

    [F] Expose project on /text/:id endpoint

--- api/app/controllers/api/v1/texts_controller.rb
+++ api/app/controllers/api/v1/texts_controller.rb
@@ -22,7 +22,7 @@ module Api
       # GET /texts/1
       def show
         @text = load_text
-        includes = %w(category creators contributors stylesheets)
+        includes = INCLUDES + %w(category creators contributors stylesheets)
         render_single_resource(@text, include: includes)
       end
 
