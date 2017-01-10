c36bcfcd97bc49340abd46e64c598d1c680a3981
Zach Davis <zach@castironcoding.com>
2017-01-10 08:20:35 -0800

    [B] Do not retry event jobs when subject is missing

--- api/app/jobs/create_event_job.rb
+++ api/app/jobs/create_event_job.rb
@@ -2,6 +2,14 @@
 class CreateEventJob < ApplicationJob
   queue_as :default
 
+  rescue_from(::Factory::Errors::NoEventProject) do
+    # do nothing, don't retry
+  end
+
+  rescue_from(::Factory::Errors::NoEventSubject) do
+    # do nothing, don't retry
+  end
+
   def perform(event_type, **args)
     Factory::Event.new.create(event_type, args)
   end
---
+++ api/app/services/factory/errors/no_event_subject.rb
@@ -0,0 +1,6 @@
+module Factory
+  module Errors
+    class NoEventSubject < StandardError
+    end
+  end
+end
