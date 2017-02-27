650465060421bf8891ac8992caba0a08de2a074b
Zach Davis <zach@castironcoding.com>
2017-02-27 08:26:30 -0800

    [F] Add simple RequireRole HOC for authorization
    This is an initial implementation of this component. If we want to restrict
    access based on multiple roles, it will need to be expanded a bit.

--- client/src/containers/frontend/Following.js
+++ client/src/containers/frontend/Following.js
@@ -1,11 +1,12 @@
 import React, { Component, PropTypes } from 'react';
-import { Link, browserHistory } from 'react-router';
+import { Link } from 'react-router';
 import { ProjectList, Layout } from 'components/frontend';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { uiFilterActions, entityStoreActions } from 'actions';
 import { entityUtils } from 'utils';
 import { projectsAPI, favoriteProjectsAPI } from 'api';
+import HigherOrder from 'containers/global/HigherOrder';
 import get from 'lodash/get';
 
 const { select } = entityUtils;
@@ -16,13 +17,16 @@ class FollowingContainer extends Component {
 
   static fetchData(getState, dispatch) {
     const state = getState();
-    const followedProjectsRequest =
-      request(favoriteProjectsAPI.index(state.ui.projectFilters), requests.followedProjects);
-    const featuredRequest =
-      request(projectsAPI.featured(), requests.browseFeaturedProjects);
-    const { promise: one } = dispatch(followedProjectsRequest);
-    const { promise: two } = dispatch(featuredRequest);
-    return Promise.all([one, two]);
+    if (state.authentication.authenticated) {
+      const followedProjectsRequest =
+        request(favoriteProjectsAPI.index(state.ui.projectFilters), requests.followedProjects);
+      const featuredRequest =
+        request(projectsAPI.featured(), requests.browseFeaturedProjects);
+      const { promise: one } = dispatch(followedProjectsRequest);
+      const { promise: two } = dispatch(featuredRequest);
+      return Promise.all([one, two]);
+    }
+    return Promise.all([]);
   }
 
   static propTypes = {
@@ -39,7 +43,6 @@ class FollowingContainer extends Component {
     store: PropTypes.object.isRequired
   };
 
-
   static mapStateToProps(state) {
     return {
       projectFilters: state.ui.filters.project,
@@ -55,19 +58,12 @@ class FollowingContainer extends Component {
     this.renderFollowedProjects = this.renderFollowedProjects.bind(this);
   }
 
-  componentWillMount() {
-    this.maybeRedirect();
-  }
-
   componentDidMount() {
     window.scrollTo(0, 0);
   }
 
   componentDidUpdate(prevProps) {
-    // Did the user log out?
-    if (this.props.authentication.authenticated === false) {
-      return this.maybeRedirect();
-    }
+    if (!this.props.authentication.authenticated) return;
     // Favorite projects filters changed?
     if (prevProps.projectFilters !== this.props.projectFilters) {
       this.updateFavorites();
@@ -77,11 +73,6 @@ class FollowingContainer extends Component {
       this.props.authentication.currentUser.favorites) {
       this.updateFavorites();
     }
-  }
-
-  maybeRedirect() {
-    if (this.props.authentication.authenticated === true) return null;
-    browserHistory.push('/browse');
   }
 
   updateFavorites() {
@@ -123,35 +114,37 @@ class FollowingContainer extends Component {
 
   render() {
     return (
-      <div>
-        {this.props.followedProjects && this.props.followedProjects.length > 0 ?
-            this.renderFollowedProjects() : <Layout.NoFollow />
-        }
-        <section>
-          <div className="container">
-            <header className="section-heading utility-right">
-              <h4 className="title">
-                <i className="manicon manicon-lamp"></i>
-                {'Featured Projects'}
-              </h4>
-              <div className="section-heading-utility-right">
-                <Link to={`/browse/`} className="button-primary">
-                  See all Featured
-                </Link>
-              </div>
-            </header>
-            { this.props.featuredProjects ?
-              <ProjectList.Grid
-                authenticated={this.props.authentication.authenticated}
-                favorites={get(this.props.authentication, 'currentUser.favorites')}
-                dispatch={this.props.dispatch}
-                projects={this.props.featuredProjects}
-              /> : null
-            }
-          </div>
-        </section>
-        <Layout.ButtonNavigation grayBg showFollowing={false} />
-      </div>
+      <HigherOrder.RequireRole requiredRole="any" redirect="/browse">
+        <div>
+          {this.props.followedProjects && this.props.followedProjects.length > 0 ?
+              this.renderFollowedProjects() : <Layout.NoFollow />
+          }
+          <section>
+            <div className="container">
+              <header className="section-heading utility-right">
+                <h4 className="title">
+                  <i className="manicon manicon-lamp"></i>
+                  {'Featured Projects'}
+                </h4>
+                <div className="section-heading-utility-right">
+                  <Link to={`/browse/`} className="button-primary">
+                    See all Featured
+                  </Link>
+                </div>
+              </header>
+              { this.props.featuredProjects ?
+                <ProjectList.Grid
+                  authenticated={this.props.authentication.authenticated}
+                  favorites={get(this.props.authentication, 'currentUser.favorites')}
+                  dispatch={this.props.dispatch}
+                  projects={this.props.featuredProjects}
+                /> : null
+              }
+            </div>
+          </section>
+          <Layout.ButtonNavigation grayBg showFollowing={false} />
+        </div>
+      </HigherOrder.RequireRole>
     );
   }
 }
