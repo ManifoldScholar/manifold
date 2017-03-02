17d1338995439d6b4f351021fc22c840edb51c8e
Zach Davis <zach@castironcoding.com>
2017-03-02 08:45:56 -0800

    [F] Add debug tool to FormContainer.Form

--- client/src/containers/backend/Form/Form.js
+++ client/src/containers/backend/Form/Form.js
@@ -5,6 +5,7 @@ import { entityEditorActions, entityStoreActions } from 'actions';
 import { bindActionCreators } from 'redux';
 import { entityUtils } from 'utils';
 import get from 'lodash/get';
+import JSONTree from 'react-json-tree'
 
 const { select } = entityUtils;
 const { request, flush } = entityStoreActions;
@@ -23,13 +24,15 @@ class FormContainer extends PureComponent {
     update: PropTypes.func.isRequired,
     create: PropTypes.func.isRequired,
     name: PropTypes.string.isRequired,
-    onSuccess: PropTypes.func
+    onSuccess: PropTypes.func,
+    debug: PropTypes.bool
   };
 
   static defaultProps = {
     model: {
       attributes: {}
-    }
+    },
+    debug: false
   }
 
   static mapStateToProps(state, ownProps) {
@@ -156,10 +159,58 @@ class FormContainer extends PureComponent {
     };
   }
 
+  renderDebugger() {
+    if (!__DEVELOPMENT__) return null;
+    if (!this.props.debug) return null;
+    const theme = {
+      scheme: 'green screen',
+      author: 'chris kempson (http://chriskempson.com)',
+      base00: '#001100',
+      base01: '#003300',
+      base02: '#005500',
+      base03: '#007700',
+      base04: '#009900',
+      base05: '#00bb00',
+      base06: '#00dd00',
+      base07: '#00ff00',
+      base08: '#007700',
+      base09: '#009900',
+      base0A: '#007700',
+      base0B: '#00bb00',
+      base0C: '#005500',
+      base0D: '#009900',
+      base0E: '#00bb00',
+      base0F: '#005500'
+    }
+    const advanced = {
+      extend: theme,
+      tree: {
+        padding: 20,
+        margin: 0
+      }
+    }
+    return (
+      <div style={{
+        marginBottom: 15
+      }}>
+        <div style={{
+          backgroundColor: "rgb(0, 17, 0)",
+          color: "rgb(0, 153, 0)",
+          margin: 0,
+          padding: "10px 20px",
+          display: "inline-block"
+        }}>Form Debugger</div>
+        <JSONTree hideRoot={true} theme={advanced} invertTheme={false} data={this.props.session} />
+      </div>
+    )
+  }
+
+
   render() {
     if (!this.props.session) return null;
     return (
       <div>
+        {this.renderDebugger()}
         <form onSubmit={this.handleSubmit} className={this.props.className} >
           {this.renderChildren(this.props)}
         </form>
