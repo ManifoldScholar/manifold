7884154f070782cac6e84428a88ca17de3d9a3d7
Zach Davis <zach@castironcoding.com>
2017-03-03 15:00:24 -0800

    [F] Allow users to destroy their own annotations

--- api/app/authorizers/annotation_authorizer.rb
+++ api/app/authorizers/annotation_authorizer.rb
@@ -8,8 +8,15 @@ class AnnotationAuthorizer < ApplicationAuthorizer
     true
   end
 
+  def self.deletable_by?(_user)
+    true
+  end
+
   def creatable_by?(user)
     resource.format == Annotation::TYPE_RESOURCE ? user.admin? : true
   end
 
+  def deletable_by?(user)
+    resource.creator == user || user.admin?
+  end
 end
