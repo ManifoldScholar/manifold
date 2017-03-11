022ff771a1044a6499596a3824c546a0c3ef014b
Zach Davis <zach@castironcoding.com>
2017-03-10 16:11:09 -0800

    [F] Generate thumbnails for PDFs
    [Delivers #141530937, #141530925, #141530919]

--- api/app/models/concerns/attachments.rb
+++ api/app/models/concerns/attachments.rb
@@ -1,27 +1,60 @@
 # Model concern that tracks who created a record
+# rubocop:disable Metrics/LineLength, Style/TrailingWhitespace
 module Attachments
   extend ActiveSupport::Concern
 
-  MANIFOLD_ATTACHMENT_IMAGE_STYLES = {
-    small: ["320x320"],
-    small_square: "",
-    small_landscape: "",
-    small_portrait: "",
-    medium: ["640x640"],
-    medium_square: "",
-    medium_landscape: "",
-    medium_portrait: "",
-    large_landscape: ""
-  }.freeze
+  RATIO = 1.6
+  SMALL = 320
+  SMALL_REL = (SMALL / RATIO).to_i
+  MED = 640
+  MED_REL = (MED / RATIO).to_i
+  LRG = 1280
+  LRG_REL = (LRG / RATIO).to_i
 
-  MANIFOLD_ATTACHMENT_IMAGE_CONVERT_OPTIONS = {
-    small_square: "-gravity north -thumbnail 320x320^ -extent 320x320",
-    small_landscape: "-gravity north -thumbnail 320x200^ -extent 320x200",
-    small_portrait: "-gravity north -thumbnail 200x320^ -extent 200x320",
-    medium_square: "-gravity north -thumbnail 640x640^ -extent 640x640",
-    medium_landscape: "-gravity north -thumbnail 640x400^ -extent 640x400",
-    medium_portrait: "-gravity north -thumbnail 400x640^ -extent 400x640",
-    large_landscape: "-gravity north -thumbnail 1280x800^ -extent 1280x800"
+  BASE_STYLES = {
+    small: {
+      geometry: "#{SMALL}x#{SMALL}",
+      format: :jpg
+    },
+    small_square: {
+      geometry: "",
+      format: :jpg,
+      convert_options: "-gravity north -thumbnail #{SMALL}x#{SMALL}^ -extent #{SMALL}x#{SMALL}"
+    },
+    small_landscape: {
+      geometry: "",
+      format: :jpg,
+      convert_options: "-gravity north -thumbnail #{SMALL}x#{SMALL_REL}^ -extent #{SMALL}x#{SMALL_REL}"
+    },
+    small_portrait: {
+      geometry: "",
+      format: :jpg,
+      convert_options: "-gravity north -thumbnail #{SMALL_REL}x#{SMALL}^ -extent #{SMALL_REL}x#{SMALL}"
+    },
+    medium: {
+      geometry: "#{MED}x#{MED}",
+      format: :jpg
+    },
+    medium_square: {
+      geometry: "",
+      format: :jpg,
+      convert_options: "-gravity north -thumbnail #{MED}x#{MED}^ -extent #{MED}x#{MED}"
+    },
+    medium_landscape: {
+      geometry: "",
+      format: :jpg,
+      convert_options: "-gravity north -thumbnail #{MED}x#{MED_REL}^ -extent #{MED}x#{MED_REL}"
+    },
+    medium_portrait: {
+      geometry: "",
+      format: :jpg,
+      convert_options: "-gravity north -thumbnail #{MED / 1.6}x#{MED}^ -extent #{MED_REL}"
+    },
+    large_landscape: {
+      geometry: "",
+      format: :jpg,
+      convert_options: "-gravity north -thumbnail #{LRG}x#{LRG / 1.6}^ -extent #{LRG}x#{LRG / 1.6}"
+    }
   }.freeze
 
   # rubocop:disable Metrics/BlockLength
@@ -39,14 +72,12 @@ module Attachments
     # can_process_avatar?
     # It also adds a before processing callback for Paperclip to process the variants if,
     # and only if the attachment is processable.
-    # rubocop:disable Metrics/LineLength, Style/TrailingWhitespace
-    def manifold_has_attached_file(field, type, styles: {}, convert_options: {}, no_styles: false)
+    def manifold_has_attached_file(field, type, styles: {}, no_styles: false)
       # Create the style
-      final_styles = no_styles ? {} : MANIFOLD_ATTACHMENT_IMAGE_STYLES.merge(styles)
+
       has_attached_file(
         field,
-        styles: MANIFOLD_ATTACHMENT_IMAGE_STYLES.merge(final_styles),
-        convert_options: MANIFOLD_ATTACHMENT_IMAGE_CONVERT_OPTIONS.merge(convert_options)
+        styles: ->(a) { a.instance.send("#{field}_style_configuration", styles, no_styles) }
       )
 
       class_eval <<-RUBY, __FILE__, __LINE__ + 1
@@ -63,6 +94,27 @@ module Attachments
   
         before_#{field}_post_process :can_process_#{field}_styles?
         
+        def manifold_attachment_image_styles
+          return BASE_STYLES
+        end
+
+        def manifold_attachment_pdf_styles
+          BASE_STYLES.map do |style_name, base_style|
+            out = base_style.clone
+            out[:format] = :png
+            [style_name, out]
+          end.to_h
+        end
+
+        def #{field}_style_configuration(additional_styles = {}, no_styles = false)
+          out = {} if no_styles
+          out = manifold_attachment_image_styles if #{field}_is_image? 
+          out = manifold_attachment_pdf_styles if #{field}_is_pdf? 
+          out = manifold_attachment_image_styles
+          # Debugger.add_breakpoint(__FILE__, __LINE__ + 1)
+          out
+        end
+
         def #{field}_url
           return nil unless #{field}.present?
           Rails.configuration.manifold.api_url + #{field}.url
@@ -73,11 +125,11 @@ module Attachments
         end
   
         def #{field}_styles
-          is_image = #{field}_is_image?
           styles = #{field}.styles.keys.map do |style|
-            value = nil
-            if is_image
+            if #{field}.url(style).present? && can_process_#{field}_styles?
               value = Rails.configuration.manifold.api_url + #{field}.url(style)
+            else 
+              value = nil
             end
             [style, value]
           end
@@ -90,13 +142,50 @@ module Attachments
           config = Rails.configuration.manifold.attachments.validations
           !#{field}_content_type.match(Regexp.union(config[:image][:allowed_mime])).nil?          
         end
+
+        def #{field}_is_video?
+          config = Rails.configuration.manifold.attachments.validations
+          !#{field}_content_type.match(Regexp.union(config[:video][:allowed_mime])).nil?  
+        end
+      
+        def #{field}_is_audio?
+          config = Rails.configuration.manifold.attachments.validations
+          !#{field}_content_type.match(Regexp.union(config[:audio][:allowed_mime])).nil?  
+        end
+      
+        def #{field}_is_plain_text?
+          types = Rails.configuration.manifold.attachments.validations.plain_text.allowed_ext
+          types.include? #{field}_extension
+        end
+      
+        def #{field}_is_excel?
+          types = Rails.configuration.manifold.attachments.validations.excel.allowed_ext
+          types.include? #{field}_extension
+        end
+      
+        def #{field}_is_word_document?
+          types = Rails.configuration.manifold.attachments.validations.word.allowed_ext
+          types.include? #{field}_extension
+        end
+      
+        def #{field}_is_powerpoint?
+          types = Rails.configuration.manifold.attachments.validations.powerpoint.allowed_ext
+          types.include? #{field}_extension
+        end
+      
+        def #{field}_is_pdf?
+          types = Rails.configuration.manifold.attachments.validations.pdf.allowed_ext
+          types.include? #{field}_extension
+        end
   
         def can_process_#{field}_styles?
-          #{field}_is_image?
+          return false if #{field}.size > 200000000
+          (#{field}_is_image? || #{field}_is_pdf?)
         end
+
       RUBY
     end
-    # rubocop:enable Metrics/LineLength, Style/TrailingWhitespace
   end
   # rubocop:enable Metrics/BlockLength
 end
+# rubocop:enable Metrics/LineLength, Style/TrailingWhitespace
