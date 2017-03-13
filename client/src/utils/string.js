be7d04a2a0673e3913be73dd7d54165c1ea55de9
Zach Davis <zach@castironcoding.com>
2017-03-13 16:39:08 -0700

    [C] Add simple, fast, insecure string hash function

--- client/src/utils/string.js
+++ client/src/utils/string.js
@@ -21,3 +21,14 @@ export function possessivize(str) {
   }
   return out;
 }
+
+// Thanks, darkskyapp
+// https://github.com/darkskyapp/string-hash/blob/master/index.js
+export function hash(str) {
+  let hash = 5381;
+  let i = str.length;
+  while(i) {
+    hash = (hash * 33) ^ str.charCodeAt(--i);
+  }
+  return hash >>> 0;
+}
