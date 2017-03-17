3666305ea9b50e7daf71883ce1ae321d06e1b04e
Naomi Rubin <naomi@castironcoding.com>
2017-03-17 12:31:59 -0700

    [WIP] Fix kindpicker alignment, add mobile version

--- client/src/components/backend/Resource/Form/KindPicker.js
+++ client/src/components/backend/Resource/Form/KindPicker.js
@@ -26,32 +26,64 @@ export default class KindPicker extends PureComponent {
     ];
 
     return (
-      <div className="resource-kind-picker">
-        <ul>
-          {kindList.map((kind) => {
-            const safeKind = kind.toLowerCase();
-            const buttonClass = classNames({
-              active: safeKind === this.props.kind
-            });
-            return (
-              <li key={kind}>
-                <button
-                  onClick={() => { this.props.setKind(safeKind); }}
-                  className={buttonClass}
+      <div className="resource-kind-picker form-secondary">
+        <div className="form-input">
+        <label>
+          Kind
+        </label>
+          <div className="picker-select">
+            <figure>
+              <div className={`resource-icon ${this.props.kind}`}>
+                <Resource.Icon.Composer kind={this.props.kind}/>
+              </div>
+            </figure>
+            <div className="form-select">
+              <i className="manicon manicon-caret-down"></i>
+                <select
+                  onChange={(event) => {
+                    console.log(event.target.value); this.props.setKind(event.target.value);
+                  }}
+                  value={this.props.kind.toLowerCase()}
                 >
-                  <figure>
-                    <figcaption>
-                      {kind}
-                    </figcaption>
-                    <div className={`resource-icon ${safeKind}`}>
-                      <Resource.Icon.Composer kind={safeKind}/>
-                    </div>
-                  </figure>
-                </button>
-              </li>
-            );
-          })}
-        </ul>
+                  {kindList.map((kind) => {
+                    const safeKind = kind.toLowerCase();
+
+                    return (
+                      <option key={safeKind} value={safeKind} id={safeKind}>
+                        {kind}
+                      </option>
+                    );
+                  })}
+                </select>
+            </div>
+          </div>
+          <ul>
+            {kindList.map((kind) => {
+              const safeKind = kind.toLowerCase();
+              const buttonClass = classNames({
+                button: true,
+                active: safeKind === this.props.kind
+              });
+              return (
+                <li key={safeKind}>
+                  <div
+                    onClick={() => { this.props.setKind(safeKind); }}
+                    className={buttonClass}
+                  >
+                    <figure>
+                      <figcaption>
+                        {kind}
+                      </figcaption>
+                      <div className={`resource-icon ${safeKind}`}>
+                        <Resource.Icon.Composer kind={safeKind}/>
+                      </div>
+                    </figure>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     );
   }
