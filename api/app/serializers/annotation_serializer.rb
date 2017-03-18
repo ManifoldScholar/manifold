29c6d3192a4154293867a2672ae631e86e0749c7
Zach Davis <zach@castironcoding.com>
2017-03-17 18:21:47 -0700

    [F] Allow body and private annotation params

--- api/app/serializers/annotation_serializer.rb
+++ api/app/serializers/annotation_serializer.rb
@@ -2,6 +2,13 @@
 class AnnotationSerializer < ActiveModel::Serializer
   meta(partial: false)
 
+  has_one :creator
   attributes :created_at, :end_char, :end_node, :id, :start_char, :start_node,
-             :text_section_id, :updated_at, :creator_id, :format, :resource_id
+             :text_section_id, :updated_at, :format, :resource_id, :creator_id,
+             :body, :private, :subject, :current_user_is_creator
+
+  def current_user_is_creator
+    scope.authenticated_as.id == object.creator_id
+  end
+
 end
