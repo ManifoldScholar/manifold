4ff5f2bbc796860e41673d6a9f7fca7622d885fa
Zach Davis <zach@castironcoding.com>
2017-03-20 12:32:13 -0700

    [B] Authorize annotation reply + delete in client

--- api/app/serializers/annotation_serializer.rb
+++ api/app/serializers/annotation_serializer.rb
@@ -1,11 +1,13 @@
 # Serializes an Annotation model
 class AnnotationSerializer < ActiveModel::Serializer
+  include Authorization
   meta(partial: false)
 
   has_one :creator
   attributes :created_at, :end_char, :end_node, :id, :start_char, :start_node,
              :text_section_id, :updated_at, :format, :resource_id, :creator_id,
-             :body, :private, :subject, :current_user_is_creator
+             :body, :private, :subject, :current_user_is_creator, :can_update_object,
+             :can_delete_object
 
   def current_user_is_creator
     user_id = scope.try(:authenticated_as).try(:id)
