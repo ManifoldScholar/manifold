6acc2adc24c56a8237ebb89fbbc72248df485693
Zach Davis <zach@castironcoding.com>
2017-04-20 13:11:39 -0700

    [F] Setter HOC passes setOther prop to child
    With this commit, the Setter HOC that sits between the Form container
    and child form input components will now pass as `setOther` prop to
    the wrapped component in addition to the `set`, `onChange`, `value`, &
    `initialValue` props that were already passed. The `setOther` prop
    accepts value and name (in the form of `attributes[name]`), and will
    trigger updates to the editing session dirty model. The intended use
    is for complex form components that need to manage more than one
    attribute on the model being edited, such as the project avatar
    builder component.

---
+++ client/src/components/backend/Form/__tests__/setter-test.js
@@ -0,0 +1,116 @@
+import React, { Component, PropTypes } from 'react';
+import renderer from 'react-test-renderer';
+import { Form } from 'components/backend';
+import { shallow, mount, render } from 'enzyme';
+
+import setter from '../setter';
+
+class WithPropTypes extends Component {
+  static propTypes = {
+    foo: PropTypes.string
+  };
+
+  render() {
+    return <div />
+  }
+}
+
+const Settable = setter(WithPropTypes );
+
+describe("setter higher order component", () => {
+
+  const mockedSetAction = jest.fn();
+
+  const mockedFormProps = {
+    sessionKey: "123",
+    dirtyModel: { attributes: {}},
+    sourceModel: { attributes: {property: "foo" }},
+    actions: { "set": mockedSetAction }
+  };
+
+  const settable = (
+    <Settable
+      {...mockedFormProps}
+      foo="bar"
+      bar="foo"
+      label="A form label"
+      name="attributes[property]"
+    />
+  );
+
+  describe("applied to a component with a name prop", () => {
+
+    const component = renderer.create(settable);
+    const tree = component.toJSON();
+    const wrapper = shallow(settable).first().shallow();
+    const instance = wrapper.instance();
+    const props = instance.props;
+
+    it('renders correctly', () => {
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('transfers props defined in propTypes', () => {
+      expect(props.foo).toBe("bar");
+    });
+
+    it('transfers props not defined in propTypes', () => {
+      expect(props.bar).toBe("foo");
+    });
+
+    it('transfers the name prop to settable components', () => {
+      expect(props.name).toBeDefined();
+    });
+
+    it('passes a set prop', () => {
+      expect(props.set).toBeDefined();
+    });
+
+    it('passes an onChange prop', () => {
+      expect(props.onChange).toBeDefined();
+    });
+
+    it('passes a value prop with the current value', () => {
+      expect(props.value).toBe("foo");
+    });
+
+    it('passes an initial value prop with the initial value', () => {
+      expect(props.initialValue).toBe("foo");
+    });
+
+    it('passes a setOther prop', () => {
+      expect(props.setOther).toBeDefined();
+    });
+
+    it('does not transfer the readFrom prop to settable components', () => {
+      expect(props.readFrom).not.toBeDefined();
+    });
+
+    it('does not transfer the actions prop to settable components', () => {
+      expect(props.actions).not.toBeDefined();
+    });
+
+    it('does not transfer the dirtyModel prop to settable components', () => {
+      expect(props.dirtyModel).not.toBeDefined();
+    });
+
+    it('does not transfer the sourceModel prop to settable components', () => {
+      expect(props.sourceModel).not.toBeDefined();
+    });
+
+    it('triggers the set action when the child set prop is executed', () => {
+      mockedSetAction.mockClear();
+      props.set('bar');
+      expect(mockedSetAction).toHaveBeenCalled();
+    })
+
+    it('triggers the set action when the child onChange prop is executed', () => {
+      mockedSetAction.mockClear();
+      const mockEvent = { target: { value: "bar" }};
+      props.onChange(mockEvent);
+      expect(mockedSetAction).toHaveBeenCalled();
+    })
+
+  });
+
+});
