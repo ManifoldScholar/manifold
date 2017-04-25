07c11fe974372e938641ee3d27b800a9913be13b
Max Ono <max@castironcoding.com>
2017-04-25 10:47:45 -0700

    [F] Add resource detail snapshot text and fixtures

---
+++ client/src/test/fixtures/store.js
@@ -0,0 +1,18 @@
+import createStore from 'store/createStore';
+import entity from './entity';
+
+export default function makeStoreFixture() {
+
+  const settings = entity.settings();
+  const store = createStore();
+  store.dispatch({
+    type: 'API_RESPONSE/SETTINGS',
+    error: false,
+    payload: {
+      data: settings
+    },
+    meta: "settings"
+  });
+  return store;
+
+}
