f774e91dca927be2a5bc4692d93013ec1b339c8d
Max Ono <max@castironcoding.com>
2017-05-19 14:37:15 -0700

    [B] Enable download link and fix SSR mismatch
    [Fixes #354]

--- api/app/serializers/resource_partial_serializer.rb
+++ api/app/serializers/resource_partial_serializer.rb
@@ -4,8 +4,12 @@ class ResourcePartialSerializer < ActiveModel::Serializer
 
   attributes :title, :kind, :sub_kind, :caption, :alt_text, :title_formatted, :project_id,
              :caption_formatted, :attachment_styles, :variant_thumbnail_styles,
-             :credit_formatted, :credit, :external_type, :external_id, :slug
+             :credit_formatted, :credit, :external_type, :external_id, :slug,
+             :downloadable
 
   has_many :collection_resources
 
+  def downloadable
+    object.downloadable?
+  end
 end
