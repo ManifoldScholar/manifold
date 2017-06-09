99269d6634f3de3ae5c1a9fdcb5e48d66ca67966
Max Ono <max@castironcoding.com>
2017-06-09 16:46:21 -0700

    [B] Make followed projects into a component
    [Fixes #391]

---
+++ client/src/components/frontend/ProjectList/__tests__/Following-test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Following from '../Following';
+import build from 'test/fixtures/build';
+import { wrapWithRouter, renderWithRouter } from 'test/helpers/routing';
+
+describe("Frontend.ProjectList.Following component", () => {
+
+  const projectA = build.entity.project("1");
+  const projectB = build.entity.project("2");
+  const projects = [projectA, projectB];
+  const favorites = {
+    1: projectA,
+    2: projectB
+  };
+  const user = build.entity.user("3");
+  const dispatchMock = jest.fn();
+  const updateMock = jest.fn();
+
+  it('renders correctly', () => {
+    user.favorites = favorites;
+    const component = renderer.create(
+      wrapWithRouter(
+        <Following
+          dispatch={dispatchMock}
+          followedProjects={projects}
+          authentication={{
+            currentUser: user
+          }}
+          subjects={projects}
+          handleUpdate={updateMock}
+        />
+      )
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly with no projects', () => {
+    user.favorites = {};
+    const component = renderer.create(
+      wrapWithRouter(
+        <Following
+          dispatch={dispatchMock}
+          followedProjects={[]}
+          authentication={{
+            currentUser: user
+          }}
+          subjects={[]}
+          handleUpdate={updateMock}
+        />
+      )
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
