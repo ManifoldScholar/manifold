3386410917bd68dcae883719841d00d9d105949a
Zach Davis <zach@castironcoding.com>
2017-06-13 10:13:23 -0700

    [C] Improve mock API client handling of collections
    This revision adds a mechanism for the mocked API client to return
    an array rather than an object in the case of requests that expect
    a collection response. Collection routes need to be manually set in
    the mocked client.

--- client/src/api/__mocks__/client.js
+++ client/src/api/__mocks__/client.js
@@ -1,8 +1,25 @@
+import pagination from 'test/fixtures/pagination';
+
 export default class ApiClient {
 
   call = (endpoint, method, options) => {
+
+    const collectionEndpoints = [
+      "/api/v1/projects/1/relationships/resources"
+    ];
+
+    let response;
+    if (collectionEndpoints.includes(endpoint)) {
+      response = {
+        data: [],
+        meta: { pagination: pagination() }
+      };
+    } else {
+      response = { data: {} };
+    }
+
     return new Promise((resolve, reject) => {
-      resolve({ data: {} });
+      resolve(response);
     });
   }
 
