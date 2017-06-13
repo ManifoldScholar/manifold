119f8f93afe37ea8689ba8123045cb05fd37256a
Zach Davis <zach@castironcoding.com>
2017-06-13 09:58:40 -0700

    [C] Exclude generated chunks from node app servers
    As we start to incorporate more third party libraries for specific
    features, we need a mechanism for spliting library code out into
    separate chunks. A subsequent commit will use webpack 1's
    require.ensure feature to split the code for the Ace editor into a
    separate chunk. All code that is split into a separate chunk will be
    loaded asynchronously via Webpack's jsonp approach. This code will
    not be necessary in the server-side app, however, and should not
    be included in the server-side build. Prior to this commit, the
    universal-webpack module we're using was collapsing all chunks into
    a single file on the serve. The change in this commit monkey patches
    our webpack config so that the chunks are output to separate files,
    which are ignored by the server-side application.
    
    This whole approach will likey change, of course, when we upgrade to
    Webpack 2. Yay Javascript churn.

--- client/webpack/webpack.config.react-server.js
+++ client/webpack/webpack.config.react-server.js
@@ -1,8 +1,19 @@
 import { server_configuration } from 'universal-webpack'
 import settings from './react-server-webpack-settings'
 import configuration from './webpack.config'
+import webpack from 'webpack';
 
-const config = server_configuration(configuration, settings)
+const config = server_configuration(configuration, settings);
+
+// The universal-webpack plugin that we're using automatically collapses chunks into a
+// single file. In Manifold, we're currently only splitting code into chunks if it can be
+// loaded asynchronously, which means that it's only client-side code. For this reason,
+// chunks should not be included in the server-side bundle. This line removes the
+// LimitChunkCountPlugin, which is set by universal-webpack to have 1 maxChunk.
+// We'll likely want to revisit this hack when we upgrade Webpack to v2.
+config.plugins = config.plugins.filter((plugin) => {
+  return plugin.constructor !== webpack.optimize.LimitChunkCountPlugin;
+});
 
 config.node = {
   __dirname: false,
