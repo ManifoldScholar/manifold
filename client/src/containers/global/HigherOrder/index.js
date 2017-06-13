8d2df5a13ccca8e1a8d2bc13166a60ccdcaadc96
Zach Davis <zach@castironcoding.com>
2017-06-13 10:00:09 -0700

    [F] Add withDispatch HOC
    This higher order component can be used to inject redux's dispatch
    function into components that are otherwise unconnected to the store.
    While we understand the value of keeping containers and components
    separate, we've come to think that turning a component into a container
    just so it can dispatch an action is often overkill.

--- client/src/containers/global/HigherOrder/index.js
+++ client/src/containers/global/HigherOrder/index.js
@@ -1,9 +1,11 @@
 import RequireRole from './RequireRole';
 import withSettings from './withSettings';
 import withCurrentUser from './withCurrentUser';
+import withDispatch from './withDispatch';
 
 export default {
   RequireRole,
   withSettings,
-  withCurrentUser
+  withCurrentUser,
+  withDispatch
 };
