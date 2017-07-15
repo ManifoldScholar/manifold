f11861894c7f2435d41a22e69b33371baabb11dd
Zach Davis <zach@castironcoding.com>
2017-07-15 05:15:49 -0700

    [C] Add withFormSession HOC
    Wrap a component with this HOC and pass it the name of a form. Doing
    so will pass a form prop to the wrapped component with access to the
    editing session with the dirty model, a changed flag, and a
    getModelValue method. One use for this is for handling conditional
    logic in a form based on the currentn state of the form.

--- client/src/containers/global/HigherOrder/index.js
+++ client/src/containers/global/HigherOrder/index.js
@@ -2,10 +2,12 @@ import RequireRole from "./RequireRole";
 import withSettings from "./withSettings";
 import withCurrentUser from "./withCurrentUser";
 import withDispatch from "./withDispatch";
+import withFormSession from "./withFormSession";
 
 export default {
   RequireRole,
   withSettings,
   withCurrentUser,
-  withDispatch
+  withDispatch,
+  withFormSession
 };
---
+++ client/src/containers/global/HigherOrder/withFormSession.js
@@ -0,0 +1,72 @@
+import React from "react";
+import PropTypes from "prop-types";
+import hoistStatics from "hoist-non-react-statics";
+import { connect } from "react-redux";
+import get from "lodash/get";
+import has from "lodash/has";
+import brackets2dots from "brackets2dots";
+
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || "Component";
+}
+
+export default function withFormSession(WrappedComponent, sessionKey) {
+  const displayName = `HigherOrder.WithFormSession('${getDisplayName(
+    WrappedComponent
+  )})`;
+
+  class WithFormSession extends React.PureComponent {
+    static displayName = displayName;
+
+    static WrappedComponent = WrappedComponent;
+
+    static propTypes = {
+      session: PropTypes.object,
+      response: PropTypes.object,
+      errors: PropTypes.object
+    };
+
+    static mapStateToProps = state => {
+      return {
+        session: get(state.entityEditor.sessions, sessionKey),
+        response: get(state.entityStore.responses, sessionKey),
+        errors: get(state.entityStore.responses, `${sessionKey}.errors`)
+      };
+    };
+
+    nameToPath(name) {
+      return brackets2dots(name);
+    }
+
+    lookupValue = (name, props) => {
+      if (!props.session) return null;
+      const path = this.nameToPath(name);
+      if (has(props.session.dirty, path)) {
+        return get(props.session.dirty, path);
+      }
+      if (has(props.session.source, path)) {
+        return get(props.session.source, path);
+      }
+      return null;
+    };
+
+    render() {
+      const childProps = {
+        form: {
+          session: this.props.session,
+          response: this.props.response,
+          errors: this.props.errors,
+          getModelValue: name => this.lookupValue(name, this.props)
+        }
+      };
+      const props = Object.assign({}, childProps);
+      return React.createElement(WrappedComponent, props);
+    }
+  }
+
+  const ConnectedWithFormSession = connect(WithFormSession.mapStateToProps)(
+    WithFormSession
+  );
+
+  return hoistStatics(ConnectedWithFormSession, WrappedComponent);
+}
