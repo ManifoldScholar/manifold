09bfc072bfdb9820d10549b3dffdfdf6e3bbe2d0
Naomi Rubin <naomi@castironcoding.com>
2017-08-03 11:30:27 -0700

    [B] Refactor section label to use only text
    Related to #489.

--- client/src/components/reader/Section/__tests__/Label-test.js
+++ client/src/components/reader/Section/__tests__/Label-test.js
@@ -4,11 +4,7 @@ import renderer from "react-test-renderer";
 import build from "test/fixtures/build";
 
 describe("Reader.Section.Label component", () => {
-  const text = build.entity.text("1");
-  text.attributes.published = false;
-  text.relationships.category = build.entity.category("2");
-
-  const root = <Label text={text} />;
+  const root = <Label label={"Hip Hop Classics"} />;
 
   it("renders correctly", () => {
     const component = renderer.create(root);
