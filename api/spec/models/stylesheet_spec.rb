d8cd65c34148e12c6a20c8bdaa08499455d5c6a6
Max Ono <max@castironcoding.com>
2017-08-04 12:27:35 -0700

    [B] Return errors for stylesheets
    Fixes #502

--- api/spec/models/stylesheet_spec.rb
+++ api/spec/models/stylesheet_spec.rb
@@ -8,6 +8,10 @@ RSpec.describe Stylesheet, type: :model do
     expect(FactoryGirl.build(:stylesheet)).to be_valid
   end
 
+  it "is invalid without a name" do
+    expect(FactoryGirl.build(:stylesheet, name: nil)).to_not be_valid
+  end
+
   it "changes the computed styles when its raw styles change" do
     stylesheet.raw_styles = ".some-class { color: red }"
     expect{ stylesheet.save }.to change(stylesheet, :styles)
