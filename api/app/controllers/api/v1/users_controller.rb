b7d9b7e5e0a7b7304a30d1d459c736364ee0cbc4
Max Ono <max@castironcoding.com>
2017-08-07 12:31:41 -0700

    [B] Fix error on user sign up
    Fixes #509

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -30,7 +30,7 @@ module Api
 
       def create
         @user = ::Updaters::User.new(user_params).update(User.new)
-        include_password = user_params[:data][:meta][:created_by_admin] == true
+        include_password = user_params.dig("data", "meta", "created_by_admin") == true
         AccountMailer.welcome(@user, include_password).deliver
         render_single_resource @user
       end
