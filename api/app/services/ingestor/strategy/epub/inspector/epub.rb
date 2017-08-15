b185a7408817032d353da946d8aa105911073ba1
Zach Davis <zach@castironcoding.com>
2017-08-15 15:15:32 -0700

    [B] Improve ingestion performance with memoization

--- api/app/services/ingestor/strategy/epub/inspector/epub.rb
+++ api/app/services/ingestor/strategy/epub/inspector/epub.rb
@@ -1,3 +1,5 @@
+require "memoist"
+
 module Ingestor
   module Strategy
     module EPUB
@@ -9,6 +11,7 @@ module Ingestor
         # rubocop: disable Metrics/ClassLength
         class EPUB
 
+          extend Memoist
           include Ingestor::Loggable
 
           attr_reader :logger, :ingestion
@@ -21,97 +24,120 @@ module Ingestor
           def unique_id
             rendition_unique_id_node&.content
           end
+          memoize :unique_id
 
           def epub_version
             rendition_package_node_attribute_value("version")
           end
+          memoize :epub_version
 
           def title_nodes
             metadata_node.search("//dc:title", "dc" => dc)
           end
+          memoize :title_nodes
 
           def metadata_node
             rendition_parsed.at("//xmlns:package/xmlns:metadata")
           end
+          memoize :metadata_node
 
           def creator_nodes
             metadata_node.search("//dc:creator", "dc" => dc)
           end
+          memoize :creator_nodes
 
           def contributor_nodes
             metadata_node.search("//dc:contributor", "dc" => dc)
           end
+          memoize :contributor_nodes
 
           def language_node
             metadata_node.at("//dc:language", "dc" => dc)
           end
+          memoize :language_node
 
           def date_node
             metadata_node.xpath("//dc:date", "dc" => dc)
           end
+          memoize :date_node
 
           def rights_node
             metadata_node.xpath("//dc:rights", "dc" => dc)
           end
+          memoize :rights_node
 
           def description_node
             metadata_node.xpath("//dc:description", "dc" => dc)
           end
+          memoize :description_node
 
           def manifest_item_nodes
             manifest_node.search("//xmlns:item")
           end
+          memoize :manifest_item_nodes
 
           def stylesheet_nodes
             manifest_item_nodes.search("//*[@media-type=\"text/css\"]")
           end
+          memoize :stylesheet_nodes
 
           def spine_item_nodes
             spine_node.xpath("//xmlns:itemref")
           end
+          memoize :spine_item_nodes
 
           def open_rendition_source_by_href(href)
             # Rendition HREFs could be relative to the rendition (OPF) file
             open_rendition_source_by_path(rendition_href_to_path(href))
           end
+          memoize :open_rendition_source_by_href
 
           def read_rendition_source_by_href(href)
             read_rendition_source_by_path(rendition_href_to_path(href))
           end
+          memoize :read_rendition_source_by_href
 
           def toc_inspector
             return TOC.new(self, ingestion) if nav_parsed
             Naught.build
           end
+          memoize :toc_inspector
 
           def nav_path
             href = manifest_nav_node&.attribute("href")&.value
             ingestion.href_to_ingestion_path(rendition_path, href)
           end
+          memoize :nav_path
 
           def nav_parsed
             xml_parse(nav_path)
           end
+          memoize :nav_parsed
 
           def v3?
             epub_version.starts_with?("3")
           end
+          memoize :v3?
 
           def v2?
             epub_version.starts_with?("2")
           end
+          memoize :v2?
 
           def rendition_source_node_by_id(id)
             manifest_item_nodes.at("//*[@id='#{id}']")
           end
+          memoize :rendition_source_node_by_id
 
           def rendition_source_node_by_property(property)
             manifest_node.at("//*[contains(@properties, \"#{property}\")]")
           end
+          memoize :rendition_source_node_by_property
 
           def spine_item_parsed(id)
             xml_parse(rendition_source_path_by_id(id))
           end
+          memoize :spine_item_parsed
 
           def manifest_cover_node
             if v2?
@@ -122,10 +148,12 @@ module Ingestor
             # V3
             rendition_source_node_by_property("cover-image")
           end
+          memoize :manifest_cover_node
 
           def manifest_cover_node_id
             manifest_cover_node&.attribute("id")&.value
           end
+          memoize :manifest_cover_node_id
 
           def spine_source_ids
             spine_item_nodes.map do |node|
@@ -133,20 +161,24 @@ module Ingestor
                                                                 .source_identifier
             end
           end
+          memoize :spine_source_ids
 
           def start_section_identifier
             return v2_start_section_identifier if v2?
             return v3_start_section_identifier if v3?
           end
+          memoize :start_section_identifier
 
           # V2 only
           def guide_node
             rendition_parsed.at("//xmlns:package/xmlns:guide")
           end
+          memoize :guide_node
 
           def rendition_href_to_path(href)
             ingestion.href_to_ingestion_path(rendition_path, href)
           end
+          memoize :rendition_href_to_path
 
           protected
 
@@ -243,9 +275,12 @@ module Ingestor
             xml_parse(rendition_path)
           end
 
-          def xml_parse(rel_path)
-            Nokogiri::XML(ingestion.open(rel_path), nil, "utf-8")
+          def xml_parse(rel_path, remove_namespaces: false)
+            doc = Nokogiri::XML(ingestion.open(rel_path), nil, "utf-8")
+            doc.remove_namespaces! if remove_namespaces
+            doc
           end
+          # memoize :xml_parse
 
           def v2_guide_node_by_type(type)
             guide_node&.css("[type=\"#{type}\"]")&.first
