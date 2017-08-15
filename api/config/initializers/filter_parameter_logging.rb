863502fc0bb1100a63907443d77ab0fc0278b5e3
Zach Davis <zach@castironcoding.com>
2017-08-15 16:23:51 -0700

    [C] Filter uploads and secrets from logs
    Fixes #528

--- api/config/initializers/filter_parameter_logging.rb
+++ api/config/initializers/filter_parameter_logging.rb
@@ -2,3 +2,22 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [:password]
+
+# Let's not log secrets
+Rails.application.config.filter_parameters += [:secrets]
+
+# Let's not log uploaded attachments
+Rails.application.config.filter_parameters += [
+  "source.data",
+  "avatar.data",
+  "hero.data",
+  "cover.data",
+  "thumbnail.data",
+  "high_res.data",
+  "attachment.data",
+  "variant_format_one.data",
+  "variant_format_two.data",
+  "variant_thumbnail.data",
+  "variant_poster.data",
+  "press_logo.data"
+]
