a82571810987c7a41dc1aa58e9dfb5b18f6f9c6a
Zach Davis <zach@castironcoding.com>
2017-08-18 14:08:22 -0700

    [F] Add utility for passive scroll feature detection

--- client/src/utils/domUtils.js
+++ client/src/utils/domUtils.js
@@ -13,3 +13,23 @@ export function closest(el, selector) {
   }
   return output;
 }
+
+/* eslint-disable no-empty */
+export function detectPassiveEventOptionsSupport() {
+  if (!window && window.addEventListener) return false;
+  let passiveSupported = false;
+  try {
+    const options = Object.defineProperty({}, "passive", {
+      get() {
+        passiveSupported = true;
+      }
+    });
+    window.addEventListener("test", null, options);
+  } catch (err) {}
+  return passiveSupported;
+}
+/* eslint-enable no-empty */
+
+export function scrollOptions(passive = true) {
+  return detectPassiveEventOptionsSupport() ? { passive } : false;
+}
