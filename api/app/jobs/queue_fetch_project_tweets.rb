303dfdf137dee66e8e2896674afd58154e5cc034
Zach Davis <zach@castironcoding.com>
2017-09-12 07:36:01 -0700

    [B] Prevent unnecessary tweet fetching jobs

--- api/app/jobs/queue_fetch_project_tweets.rb
+++ api/app/jobs/queue_fetch_project_tweets.rb
@@ -3,7 +3,7 @@ class QueueFetchProjectTweets < ApplicationJob
   queue_as :default
 
   def perform
-    Project.all.each do |project|
+    Project.where.not(tweet_fetch_config: "{}").each do |project|
       FetchProjectTweets.perform_later(project.id)
     end
   end
