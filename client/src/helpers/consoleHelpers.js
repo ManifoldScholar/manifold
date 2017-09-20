752b7392e4bc5c7d9ef5d175856065d5da04fb62
Zach Davis <zach@castironcoding.com>
2017-09-20 14:24:37 -0700

    [C] Make console helpers universal; remove chalk

--- client/src/helpers/consoleHelpers.js
+++ client/src/helpers/consoleHelpers.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-console */
 const emoji = require("node-emoji");
-const chalk = require("chalk");
 
 function baseString(emojiKey) {
   let out = "";
@@ -10,32 +9,55 @@ function baseString(emojiKey) {
   return out;
 }
 
+function log(string, color, bold) {
+  if (process.title === "node") {
+    let colorCode = "";
+    if (color === "blue") colorCode = "\x1b[34m";
+    if (color === "green") colorCode = "\x1b[32m";
+    if (color === "gray") colorCode = "\x1b[2m";
+    if (color === "red") colorCode = "\x1b[31m";
+    const resetCode = "\x1b[0m";
+    const boldCode = bold ? "\x1b[1m" : "";
+    console.log(colorCode + boldCode + string + resetCode);
+  } else {
+    let colorCode = color;
+    if (color === "blue") colorCode = "#01B6E1";
+    if (color === "green") colorCode = "#34a178";
+    if (color === "gray") colorCode = "#555555";
+    if (color === "red") colorCode = "#f75884";
+    const style = `color: ${colorCode}`;
+    console.log(`%c${string}`, style);
+  }
+}
+
 function header(string, emojiKey = "fist") {
-  let out = baseString(emojiKey) + string;
-  out = chalk.bold.green(out);
-  console.log(out);
+  const out = baseString(emojiKey) + string;
+  log(out, "green", true);
 }
 
 function info(string, emojiKey = "bell") {
-  let out = baseString(emojiKey) + string;
-  out = chalk.bold.cyan(out);
-  console.log(out);
+  const out = baseString(emojiKey) + string;
+  log(out, "blue", true);
 }
 
 function error(string, emojiKey = "fire") {
-  let out = baseString(emojiKey) + string;
-  out = chalk.bold.red(out);
-  console.log(out);
+  const out = baseString(emojiKey) + string;
+  log(out, "red", true);
+}
+
+function notice(string, emojiKey = "floppy_disk") {
+  const out = baseString(emojiKey) + string;
+  log(out, "gray", true);
 }
 
 function background(string) {
-  const out = chalk.bold.gray(string);
-  console.log(out);
+  log(string, "gray", true);
 }
 
 module.exports = {
   header,
   info,
+  notice,
   error,
   background
 };
