ManifoldScholar/manifold

commit 208690ff29f45e5a9610473bde0750aa223560ca

[F] EPUBs can be ingested through a URL
Closes #420

client/src/components/backend/Ingestion/Form/Upload.js
@@ -1,15 +1,33 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import { Form } from "components/backend";
+import setter from "components/backend/Form/setter";
 
-export default class IngestionFormUpload extends PureComponent {
+class IngestionFormUpload extends PureComponent {
   static displayName = "ProjectDetail.Text.Ingestion.Form.Upload";
 
   static propTypes = {
     getModelValue: PropTypes.func,
     location: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired
+    history: PropTypes.object.isRequired,
+    setOther: PropTypes.func
   };
+
+  constructor() {
+    super();
+    this.onSourceChange = this.onSourceChange.bind(this);
+    this.onUrlChange = this.onUrlChange.bind(this);
+  }
+
+  onSourceChange(source) {
+    this.props.setOther(source, "attributes[source]");
+    this.props.setOther(null, "attributes[externalSourceUrl]");
+  }
+
+  onUrlChange(event) {
+    this.props.setOther(event.target.value, "attributes[externalSourceUrl]");
+    this.props.setOther(null, "attributes[source]");
+  }
 
   get valid() {
     return (
@@ -42,46 +60,66 @@ export default class IngestionFormUpload extends PureComponent {
 
     /* eslint-disable max-len */
     return (
-      <Form.FieldGroup {...this.props}>
+      <div>
         {ingestionType === "googledoc"
-          ? <Form.TextInput
-              label="URL"
-              name="attributes[externalSourceUrl]"
-              instructions="Manifold can ingest any publicly available Google doc by entering its URL."
-            />
+          ? <Form.FieldGroup {...this.props}>
+              <Form.TextInput
+                label="URL"
+                name="attributes[externalSourceUrl]"
+                instructions="Manifold can ingest any publicly available Google doc by entering its URL."
+              />
+            </Form.FieldGroup>
           : null}
         {ingestionType === "epub"
-          ? <Form.Upload
-              inlineStyle={{ width: "100%" }}
-              layout="landscape"
-              name="attributes[source]"
-              readFrom="attributes[sourceFileName]"
-              instructions="Manifold supports both v2 and v3 epub files."
-              label="Upload a file ending in .epub"
-              accepts="epubs"
-            />
+          ? <Form.FieldGroup {...this.props}>
+              <Form.Upload
+                inlineStyle={{ width: "100%" }}
+                layout="landscape"
+                // name="attributes[source]"
+                readFrom="attributes[source]"
+                instructions="Manifold supports both v2 and v3 epub files."
+                label="Upload a file ending in .epub"
+                value={this.props.getModelValue("attributes[source]")}
+                set={this.onSourceChange}
+                accepts="epubs"
+              />
+              <div className="form-divider">or</div>
+              <Form.TextInput
+                label="URL"
+                // name="attributes[externalSourceUrl]"
+                instructions="Manifold can also ingest epub files by entering a URL"
+                value={this.props.getModelValue(
+                  "attributes[externalSourceUrl]"
+                )}
+                onChange={event => this.onUrlChange(event)}
+              />
+            </Form.FieldGroup>
           : null}
         {ingestionType === "html"
-          ? <Form.Upload
-              inlineStyle={{ width: "100%" }}
-              layout="landscape"
-              name="attributes[source]"
-              readFrom="attributes[sourceFileName]"
-              instructions="Create a zip archive with a .htm or .html file in the root."
-              label="Zip source file"
-              accepts="zips"
-            />
+          ? <Form.FieldGroup {...this.props}>
+              <Form.Upload
+                inlineStyle={{ width: "100%" }}
+                layout="landscape"
+                name="attributes[source]"
+                readFrom="attributes[sourceFileName]"
+                instructions="Create a zip archive with a .htm or .html file in the root."
+                label="Zip source file"
+                accepts="zips"
+              />
+            </Form.FieldGroup>
           : null}
         {ingestionType === "markdown"
-          ? <Form.Upload
-              inlineStyle={{ width: "100%" }}
-              layout="landscape"
-              name="attributes[source]"
-              readFrom="attributes[sourceFileName]"
-              instructions="Upload a single markdown file, or a zipped collection of markdown files with a book.json file in root directory"
-              label="Markdown or .zip source file"
-              accepts="zips"
-            />
+          ? <Form.FieldGroup {...this.props}>
+              <Form.Upload
+                inlineStyle={{ width: "100%" }}
+                layout="landscape"
+                name="attributes[source]"
+                readFrom="attributes[sourceFileName]"
+                instructions="Upload a single markdown file, or a zipped collection of markdown files with a book.json file in root directory"
+                label="Markdown or .zip source file"
+                accepts="zips"
+              />
+            </Form.FieldGroup>
           : null}
         <div style={{ marginTop: 30 }} className="buttons-icon-horizontal">
           <button
@@ -100,7 +138,9 @@ export default class IngestionFormUpload extends PureComponent {
             Continue
           </button>
         </div>
-      </Form.FieldGroup>
+      </div>
     );
   }
 }
+
+export default setter(IngestionFormUpload);

api/app/models/ingestion.rb
@@ -81,6 +81,7 @@ class Ingestion < ApplicationRecord
   end
 
   def file_based_ingestion?
+    return external_source_url.blank? if ingestion_type == "epub"
     ingestion_type != "googledoc"
   end
 

api/app/services/ingestor/ingestion.rb
@@ -30,7 +30,7 @@ module Ingestor
     end
 
     def teardown
-      # FileUtils.rm_rf(root)
+      FileUtils.rm_rf(root)
     end
 
     def open(rel_path, options = "r")
@@ -57,13 +57,21 @@ module Ingestor
       file_operation(:write, rel_path, [contents])
     end
 
-    def write_tmp(name, ext, contents)
+    def write_tmp(name, ext, contents = nil, url: nil)
       tmp = Tempfile.new([name, ".#{ext}"])
       tmp.close
-      File.open(tmp.path, "wb") do |f|
-        f.write(contents)
+      if url.present?
+        fetch_and_write(url, tmp.path)
+      else
+        File.open(tmp.path, "wb") do |f|
+          f.write(contents)
+        end
       end
       [tmp, tmp.path]
+    end
+
+    def fetch_and_write(uri, path)
+      IO.copy_stream(URI(uri).open, path)
     end
 
     def dir?(rel_path)

api/app/services/ingestor/strategy/epub/inspector/epub.rb
@@ -22,6 +22,7 @@ module Ingestor
           end
 
           def unique_id
+            return Digest::MD5.hexdigest(ingestion.source_url) if ingestion.source_url
             rendition_unique_id_node&.content
           end
           memoize :unique_id

api/app/services/ingestor/strategy/epub/strategy.rb
@@ -10,10 +10,13 @@ module Ingestor
       class Strategy < Ingestor::Strategy::Base
         include Ingestor::Loggable
 
+        # Return true if epub file or is a url to an EPUB file
         def self.can_ingest?(ingestion)
-          ingestion.dir?("META-INF")
+          ingestion.dir?("META-INF") || epub_url?(ingestion)
         end
 
+        # Returns an MD5 string of file contents if ingestion is a url OR
+        # returns the EPUB's unique identifier
         def self.unique_id(ingestion)
           inspector(ingestion).unique_id
         end
@@ -23,6 +26,7 @@ module Ingestor
         end
 
         def self.ingest(ingestion)
+          return new(ingestion).fetch(ingestion).ingest if epub_url?(ingestion)
           new(ingestion).ingest
         end
 
@@ -40,6 +44,26 @@ module Ingestor
           b = ::Ingestor::Strategy::EPUB::Builder.new(i, @ingestion.logger)
           b.build(text)
           text
+        end
+
+        # Return true if ingestion has a url to a file ending with an EPUB extension
+        def self.epub_url?(ingestion)
+          return false unless ingestion.url?
+          File.extname(ingestion.source_url).include? "epub"
+        end
+
+        # Fetches and writes the contents of the file at the end of the ingestion URL to
+        # a temporary file for ingestion
+        def fetch(ingestion)
+          tmp_file, tmp_path = @ingestion.write_tmp(
+            "epub_ingestion",
+            "epub",
+            url: ingestion.source_url
+          )
+          @ingestion.update_working_dir(tmp_path)
+          self
+        ensure
+          tmp_file.unlink
         end
       end
     end

api/spec/services/ingestor/strategy/epub/strategy_spec.rb
@@ -163,4 +163,111 @@ RSpec.describe Ingestor::Strategy::EPUB::Strategy do
 
   end
 
+  context "when ingesting from url", :slow, :integration do
+
+    before(:all) {
+      logger = NullLogger.new
+      Ingestor.logger = logger
+      Settings.potentially_update_from_environment!
+      @creator = FactoryGirl.create(:user)
+      WebMock.allow_net_connect!
+      @epub_source = "https://standardebooks.org/ebooks/e-t-a-hoffmann/master-flea/george-soane/dist/e-t-a-hoffmann_master-flea.epub3"
+      @text = Ingestor.ingest(@epub_source, @creator, Ingestor::Strategy::EPUB::Strategy)
+      WebMock.disable_net_connect!
+    }
+
+    describe "the text generated by the ingestion" do
+
+      it "has a title that matches the dublin core language metadata" do
+        expect(@text.language).to eq "en-GB"
+      end
+
+      it "has a title that matches the dublin core title metadata" do
+        expect(@text.title).to eq "Master Flea"
+      end
+
+      it "has a title that matches the dublin core rights metadata" do
+        expect(@text.rights).to start_with("The source text and artwork in this ebook edition") &&
+          end_with("Uncopyright file included at the end of this ebook.")
+      end
+
+      it "has a title that matches the dublin core description metadata" do
+        description = "A gentleman bachelor gains an epic advantage of tiny proportions in this allegorical, Romantic, fairy-tale novel."
+        expect(@text.description).to eq description
+      end
+
+      it "has the correct number of text sections" do
+        expect(@text.text_sections.length).to be 12
+      end
+
+      it "has an empty page list" do
+        expect(@text.page_list).to eq []
+      end
+
+      it "has the correct number of stylesheets" do
+        expect(@text.stylesheets.length).to eq 2
+      end
+
+      it "has the correct number of spine entries" do
+        expect(@text.spine.length).to eq 12
+      end
+
+      it "has a text_section for each spine entry" do
+        expect(@text.text_sections.length).to eq 12
+      end
+
+      it "has an ingestion_source for each manifest item" do
+        expect(@text.ingestion_sources.length).to eq 18
+      end
+
+      it "sets the creator correctly" do
+        author = @text.creators.first
+        expect(author.first_name).to eq "E. T. A."
+        expect(author.last_name).to eq "Hoffmann"
+      end
+
+      it "has a cover image" do
+        expect(@text.cover.present?).to be true
+      end
+
+      describe "the generated ingestion sources" do
+
+        it "has a source_path property equal to the relative path within the package" do
+          section = @text.text_sections.first
+          expect(section.source_path).to eq "epub/text/titlepage.xhtml"
+        end
+
+      end
+
+      describe "its table of contents" do
+
+        it "has the correct number of entries" do
+          toc = @text.toc
+          expect(toc.length).to eq 12
+        end
+
+        it "has the correct labels" do
+          target_toc = ["Title Page", "Imprint", "I: First Adventure", "II: Second Adventure", "III: Third Adventure", "IV: Fourth Adventure", "V: Fifth Adventure", "VI: Sixth Adventure", "VII: Seventh Adventure", "Endnotes", "Colophon", "Uncopyright"]
+          toc = @text.toc
+          expect(toc.map { |i| i[:label] }).to eq target_toc
+        end
+
+        it "assigns a text_section ID to each item" do
+          def walk(array)
+            ids = []
+            array.each do |hash|
+              ids.push hash[:id]
+              if hash.key? :children
+                ids.concat walk(hash[:children])
+              end
+            end
+            ids
+          end
+          expect(walk(@text.toc).any? { |i| i.nil?}).to be false
+        end
+      end
+    end
+
+  end
+
 end

client/src/components/backend/Ingestion/Form/__tests__/__snapshots__/Upload-test.js.snap
@@ -1,9 +1,7 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
 exports[`Backend.Ingestion.Form.Upload component renders correctly 1`] = `
-<div
-  className="form-section"
->
+<div>
   <div
     className="buttons-icon-horizontal"
     style={

client/src/containers/backend/Form/Form.js
@@ -152,16 +152,17 @@ export class FormContainer extends PureComponent {
   }
 
   childProps(props) {
-    return {
+    const out = {
       actions: {
         set: bindActionCreators(set, props.dispatch)
       },
       dirtyModel: props.session.dirty,
       sourceModel: props.session.source,
       getModelValue: name => this.lookupValue(name, this.props),
-      sessionKey: props.name,
-      errors: props.errors || []
+      sessionKey: props.name
     };
+    if (!this.props.groupErrors) out.errors = props.errors || [];
+    return out;
   }
 
   isBlocking() {

commit b3b18b5a344c830fa551efc8f1185c51ff024b23

[B] Fix regression in google doc ingestion
#208690f introduced a regression wherein the url input for google doc
ingestion could not be set. This commit remedies this mistake.

client/src/components/backend/Ingestion/Form/Upload.js
@@ -65,8 +65,11 @@ class IngestionFormUpload extends PureComponent {
           ? <Form.FieldGroup {...this.props}>
               <Form.TextInput
                 label="URL"
-                name="attributes[externalSourceUrl]"
                 instructions="Manifold can ingest any publicly available Google doc by entering its URL."
+                value={this.props.getModelValue(
+                  "attributes[externalSourceUrl]"
+                )}
+                onChange={event => this.onUrlChange(event)}
               />
             </Form.FieldGroup>
           : null}
@@ -75,7 +78,6 @@ class IngestionFormUpload extends PureComponent {
               <Form.Upload
                 inlineStyle={{ width: "100%" }}
                 layout="landscape"
-                // name="attributes[source]"
                 readFrom="attributes[source]"
                 instructions="Manifold supports both v2 and v3 epub files."
                 label="Upload a file ending in .epub"
@@ -86,7 +88,6 @@ class IngestionFormUpload extends PureComponent {
               <div className="form-divider">or</div>
               <Form.TextInput
                 label="URL"
-                // name="attributes[externalSourceUrl]"
                 instructions="Manifold can also ingest epub files by entering a URL"
                 value={this.props.getModelValue(
                   "attributes[externalSourceUrl]"

client/src/containers/backend/Ingestion/Ingest.js
@@ -305,16 +305,6 @@ export class IngestionIngest extends PureComponent {
                 Back
               </button>
             : null}
-          {this.props.ingestion.attributes.state === "finished"
-            ? <button
-                onClick={this.backToEdit}
-                className="button-icon-secondary dull"
-              >
-                <i className="manicon manicon-check small" />
-                Ingestion Complete.
-              </button>
-            : null}
-
           {this.canProcess
             ? <button onClick={this.ingest} className="button-icon-secondary">
                 <i className="manicon manicon-arrow-right small" />