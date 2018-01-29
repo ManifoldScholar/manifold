f311671af77005acff3cf7ad96b6a1cfeb76c01a
Max Ono <max@castironcoding.com>
2018-01-29 11:39:19 -0800

    [B] Fix sizing of external videos in preview overlay
    Fixes #755

--- client/src/components/frontend/Resource/Preview/Video.js
+++ client/src/components/frontend/Resource/Preview/Video.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Resource } from "components/frontend";
 import PropTypes from "prop-types";
+import classnames from "classnames";
 
 export default class ResourcePreviewVideo extends Component {
   static displayName = "Resource.Preview.Video";
@@ -11,9 +12,14 @@ export default class ResourcePreviewVideo extends Component {
 
   render() {
     const resource = this.props.resource;
+    const videoClasses = classnames({
+      "resource-preview": true,
+      "resource-preview-video": true,
+      "external-video": resource.attributes.subKind === "external_video"
+    });
 
     return (
-      <div className="resource-preview resource-preview-video">
+      <div className={videoClasses}>
         <Resource.Player.Video resource={resource} />
       </div>
     );
