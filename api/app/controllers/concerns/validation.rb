c4002c1cbe48456174bc79d03639ee574b1760c4
Max Ono <max@castironcoding.com>
2018-01-30 15:17:50 -0800

    [B] Fix non-human name parsing
    Fixes #766

--- api/app/controllers/concerns/validation.rb
+++ api/app/controllers/concerns/validation.rb
@@ -237,7 +237,7 @@ module Validation
   def maker_params
     params.require(:data)
     attributes = [:first_name, :last_name, :middle_name, :suffix, attachment(:avatar),
-                  :remove_avatar]
+                  :remove_avatar, :name]
     relationships = [:users]
     param_config = structure_params(attributes: attributes, relationships: relationships)
     params.permit(param_config)
