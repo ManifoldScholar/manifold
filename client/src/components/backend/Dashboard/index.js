c3a78adf6dbd9382a1b6e401896ff570c7d8f29b
Max Ono <max@castironcoding.com>
2018-01-31 09:19:14 -0800

    [WIP] Separate backend dashboards

--- client/src/components/backend/Dashboard/index.js
+++ client/src/components/backend/Dashboard/index.js
@@ -1,7 +1,11 @@
 import Activity from "./Activity";
+import Admin from "./Admin";
+import Author from "./Author";
 import Notifications from "./Notifications";
 
 export default {
   Activity,
+  Admin,
+  Author,
   Notifications
 };
