1c2d5e5503b39043bb663ce048c9c94aa477bfea
Naomi Rubin <naomi@castironcoding.com>
2018-02-15 10:10:26 -0800

    [F] Complete initial style/scripts for column map
    Feature #434

---
+++ client/src/components/backend/Form/ColumnMap/Column.js
@@ -0,0 +1,56 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import { Draggable } from 'react-beautiful-dnd';
+
+export default class FormColumnMapColumn extends PureComponent {
+  static displayName = "Form.ColumnMap.Column"
+
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    mapping: PropTypes.string,
+    unLink: PropTypes.func,
+  }
+
+  handleCancel = (event) => {
+    event.preventDefault();
+    this.props.unLink(this.props.mapping, this.props.name);
+  }
+
+  render() {
+    return(
+      <Draggable draggableId={this.props.name}>
+        {(provided, snapshot) => {
+          return (
+            <div className="form-column-listing"
+              style={{marginRight: snapshot.isDragging ? '-200px' : 0}}
+            >
+              <div
+                ref={provided.innerRef}
+                className="form-column-available"
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+              >
+                <span className="text">
+                  {this.props.name}
+                </span>
+              </div>
+              {provided.placeholder}
+
+              <button
+                className="cancel"
+                onClick={this.handleCancel}
+              >
+                <span className="screen-reader-text">
+                  {'Click to cancel mapping of '}
+                  {`${this.props.name} to ${this.props.match}`}
+                </span>
+                <i className="manicon manicon-x-bold">
+                </i>
+              </button>
+            </div>
+          )
+        }}
+      </Draggable>
+    )
+  }
+}
