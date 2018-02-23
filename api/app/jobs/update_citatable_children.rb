a6a28dba7713dab7d8b216e33a7808b0b19fb519
Max Ono <max@castironcoding.com>
2018-02-22 16:53:22 -0800

    [B] Move citable children update to a job
    Fixes #802

---
+++ api/app/jobs/update_citatable_children.rb
@@ -0,0 +1,8 @@
+class UpdateCitatableChildren < ApplicationJob
+  queue_as :default
+
+  def perform(parent)
+    return unless parent.citable_children.any?
+    parent.citable_children.each { |child| parent.send(child).each(&:save) }
+  end
+end
