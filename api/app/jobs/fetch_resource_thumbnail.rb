546fd35841aa1620d5af89fea106e0d7ed68e3e9
Zach Davis <zach@castironcoding.com>
2018-03-15 17:29:38 -0700

    [B] Lower priority of thumbnail fetching
    Additionally, catch RecordNotFound exceptions.

--- api/app/jobs/fetch_resource_thumbnail.rb
+++ api/app/jobs/fetch_resource_thumbnail.rb
@@ -1,6 +1,11 @@
 # Job to fetch youtube and vimeo thumbnails in the background.
 class FetchResourceThumbnail < ApplicationJob
-  queue_as :default
+  queue_as :low_priority
+
+  rescue_from(ActiveRecord::RecordNotFound) do |exception|
+    Rails.logger.info("Could not fetch thumbnail for resource #{exception.id} because " \
+    "resource was not found.")
+  end
 
   def perform(resource_id)
     resource = Resource.find(resource_id)
