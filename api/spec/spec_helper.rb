cf53eb4794bee658f22f8616f9bcab311a808038
Zach Davis <zach@castironcoding.com>
2018-03-15 17:33:54 -0700

    [C] Add ability to focus spec run

--- api/spec/spec_helper.rb
+++ api/spec/spec_helper.rb
@@ -51,8 +51,8 @@ RSpec.configure do |config|
   #   # to individual examples or groups you care about by tagging them with
   #   # `:focus` metadata. When nothing is tagged with `:focus`, all examples
   #   # get run.
-  #   config.filter_run :focus
-  #   config.run_all_when_everything_filtered = true
+    config.filter_run :focus
+    config.run_all_when_everything_filtered = true
   #
   #   # Limits the available syntax to the non-monkey patched syntax that is recommended.
   #   # For more details, see:
