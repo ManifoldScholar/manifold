fe89803e73692ee12a3ce0fbfc33438c20714cc8
Zach Davis <zach@castironcoding.com>
2018-03-25 15:19:13 -0700

    [B] Reduce unnecessary MetaList rendering

--- client/src/components/global/MetaList.js
+++ client/src/components/global/MetaList.js
@@ -5,34 +5,26 @@ import humps from "humps";
 export default class MetaList extends Component {
   static displayName = "MetaList";
 
-  constructor() {
-    super();
-
-    this.state = {
-      long: {},
-      short: {}
-    }
-  }
-
   static propTypes = {
     metadata: PropTypes.object
   };
 
-  componentDidMount() {
-    this.separateMetaByLength(this.props.metadata);
+  constructor(props) {
+    super(props);
+    this.state = this.regenerateState(props.metadata);
   }
 
-  componentDidUpdate(prev) {
-    if (prev.metadata !== this.props.metadata) {
-      this.separateMetaByLength(this.props.metadata);
+  componentWillReceiveProps(nextProps) {
+    if (this.props.metadata !== nextProps.metadata) {
+      this.setState(this.regenerateState(nextProps.metadata));
     }
   }
 
-  separateMetaByLength(metadata) {
+  regenerateState(metadata) {
     const long = {};
     const short = {};
 
-    Object.keys(metadata).forEach((key) => {
+    Object.keys(metadata).forEach(key => {
       const value = metadata[key];
       if (value.length > 280) {
         long[key] = value;
@@ -41,10 +33,10 @@ export default class MetaList extends Component {
       }
     });
 
-    this.setState({
+    return {
       long: Object.assign({}, long),
       short: Object.assign({}, short)
-    });
+    };
   }
 
   render() {
@@ -54,8 +46,8 @@ export default class MetaList extends Component {
     return (
       <div>
         <ul className="meta-list-primary">
-          {Object.keys(long).map((key) => {
-            return(
+          {Object.keys(long).map(key => {
+            return (
               <li key={key}>
                 <span className="meta-label">
                   {humps.decamelize(key, { separator: " " })}
@@ -63,7 +55,7 @@ export default class MetaList extends Component {
                 <div
                   className="meta-value"
                   dangerouslySetInnerHTML={{ __html: long[key] }}
-                  />
+                />
               </li>
             );
           })}
@@ -78,7 +70,7 @@ export default class MetaList extends Component {
                 <div
                   className="meta-value"
                   dangerouslySetInnerHTML={{ __html: short[key] }}
-                  />
+                />
               </li>
             );
           })}
