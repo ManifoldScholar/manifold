195486720ce4a703c4591b8d5e2d63e603154838
Max Ono <max@castironcoding.com>
2018-04-17 14:58:24 -0700

    [B] Fix encoding on .csv imports
    Fixes #963

--- api/app/services/resource_imports/parse_csv.rb
+++ api/app/services/resource_imports/parse_csv.rb
@@ -1,9 +1,14 @@
+require "charlock_holmes/string"
+
 module ResourceImports
   class ParseCSV < ActiveInteraction::Base
     record :resource_import
 
+    VALID_ENCODING = "UTF-8".freeze
+
     def execute
-      rows = CSV.read resource_import.data.path
+      csv = ensure_encoding(resource_import.data.path)
+      rows = CSV.parse csv
       @line_number = 0
       rows.each do |row|
         @line_number += 1
@@ -21,5 +26,11 @@ module ResourceImports
               line_number: @line_number
     end
 
+    def ensure_encoding(file_path)
+      content = File.read(file_path)
+      detection = CharlockHolmes::EncodingDetector.detect(content)
+      return content if detection[:encoding] == VALID_ENCODING
+      CharlockHolmes::Converter.convert content, detection[:encoding], "UTF-8"
+    end
   end
 end
