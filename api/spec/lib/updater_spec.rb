ManifoldScholar/manifold

commit a3a91d3329b5c3549e6884ee6db4e5a11e962ba9

[B] Fix updater reordering
Fixes #1012

api/app/lib/updaters.rb
@@ -32,6 +32,7 @@ module Updaters
     @model = model
     update_without_save(model, creator: creator)
     save_model(model)
+    post_update(model) if model.persisted?
     model
   end
 

api/app/controllers/api/v1/projects/relationships/resources_controller.rb
@@ -21,8 +21,8 @@ module Api
 
           def create
             @resource = ::Updaters::Resource.new(resource_params)
-                                            .update(@project.resources.new)
-            @resource.creator = @current_user
+                                            .update(@project.resources.new,
+                                                    creator: @current_user)
             @resource.save
             authorize_action_for @resource
             location = api_v1_project_relationships_resources_url(

api/app/controllers/api/v1/stylesheets_controller.rb
@@ -18,7 +18,7 @@ module Api
       end
 
       def create
-        @stylesheet = ::Updaters::Stylesheet
+        @stylesheet = ::Updaters::Default
                       .new(stylesheet_params)
                       .update_without_save(@text.stylesheets.new)
         authorize_action_for @stylesheet
@@ -33,7 +33,7 @@ module Api
 
       def update
         @stylesheet = load_and_authorize_stylesheet
-        ::Updaters::Stylesheet.new(stylesheet_params).update(@stylesheet)
+        ::Updaters::Default.new(stylesheet_params).update(@stylesheet)
         render_single_resource(
           @stylesheet,
           serializer: set_serializer,

api/app/lib/updaters/feature.rb
@@ -6,14 +6,5 @@ module Updaters
     def attachment_fields
       [:foreground, :background]
     end
-
-    def update(model)
-      @model = model
-      update_without_save(model)
-      save_model(model)
-      update_position(model)
-      model
-    end
-
   end
 end

api/app/lib/updaters/stylesheet.rb
@@ -1,15 +1,0 @@
-module Updaters
-  # Updates an Stylesheet model from JSON-API style params
-  class Stylesheet
-    include ::Updaters
-
-    def update(model)
-      @model = model
-      update_without_save(model)
-      save_model(model)
-      update_position(model)
-      model
-    end
-
-  end
-end

api/app/lib/updaters/text.rb
@@ -3,14 +3,5 @@ module Updaters
   class Text
     include ::Updaters
     include ::Updaters::Concerns::HasSortableCollaborators
-
-    def update(model)
-      @model = model
-      update_without_save(model)
-      save_model(model)
-      update_position(model)
-      model
-    end
-
   end
 end

api/spec/requests/categories_spec.rb
@@ -16,4 +16,15 @@ RSpec.describe "Categories API", type: :request do
       end
     end
   end
+
+
+
+  describe "updates a text" do
+
+    it_should_behave_like "orderable api requests" do
+      let(:path) { "api_v1_category_path" }
+      let!(:object_a) { FactoryBot.create(:category, position: 1) }
+      let!(:object_b) { FactoryBot.create(:category, position: 2, project: object_a.project) }
+    end
+  end
 end

api/spec/requests/stylesheets_spec.rb
@@ -43,41 +43,22 @@ RSpec.describe "Stylesheets API", type: :request do
   end
 
   describe "updates a stylesheet" do
-
     let(:stylesheet) { FactoryBot.create(:stylesheet, text: text, creator: admin)}
-    let(:path) { api_v1_stylesheet_path(stylesheet.id) }
     let(:api_response) { JSON.parse(response.body) }
 
     it "updates the name attribute" do
       valid_params = json_payload(attributes: { name: "Rambo Stoolz"})
-      put path, headers: admin_headers, params: valid_params
+      put api_v1_stylesheet_path(stylesheet.id), headers: admin_headers, params: valid_params
       expect(api_response["data"]["attributes"]["name"]).to eq("Rambo Stoolz")
     end
 
-    context "when the position attribute is set to" do
+    describe "updates a text" do
 
-      let(:stylesheet_1) { FactoryBot.create(:stylesheet, text: text, creator: admin, position: 1) }
-      let(:stylesheet_2) { FactoryBot.create(:stylesheet, text: text, creator: admin, position: 2) }
-
-      before(:each) do
-        stylesheet_1
-        stylesheet_2
+      it_should_behave_like "orderable api requests" do
+        let(:path) { "api_v1_stylesheet_path" }
+        let!(:object_a) { FactoryBot.create(:stylesheet, position: 1) }
+        let!(:object_b) { FactoryBot.create(:stylesheet, position: 2, text: object_a.text) }
       end
-
-      it "\"up\", it returns the new position" do
-        valid_params = json_payload(attributes: { position: "up"})
-        path = api_v1_stylesheet_path(stylesheet_2.id)
-        put path, headers: admin_headers, params: valid_params
-        expect(api_response["data"]["attributes"]["position"]).to eq 1
-      end
-
-      it "\"down\", it returns the new position" do
-        valid_params = json_payload(attributes: { position: "down"})
-        path = api_v1_stylesheet_path(stylesheet_1.id)
-        put path, headers: admin_headers, params: valid_params
-        expect(api_response["data"]["attributes"]["position"]).to eq 2
-      end
-
     end
   end
 

commit 8a56e49d4a46c0e31f073abac65394429f21581f

[B] Fix updater reloading model when errors were present
Fixes #1090

This was a regression from a3a91d3329b5c3549e6884ee6db4e5a11e962ba9.
The check `model.persisted?` was always true because at that point a
record has either been created or was already present. This then meant
the `#post_update` method was always called, which included a `model.reload`.
This fix uses `save_model`'s return to determine whether the `post_update`
method should be called.

api/app/lib/updaters.rb
@@ -31,8 +31,7 @@ module Updaters
   def update(model, creator: nil)
     @model = model
     update_without_save(model, creator: creator)
-    save_model(model)
-    post_update(model) if model.persisted?
+    post_update(model) if save_model(model)
     model
   end
 
@@ -44,12 +43,12 @@ module Updaters
   protected
 
   def save_model(model)
-    @saved = false
+    saved = false
     run_callbacks "save" do
-      @saved = model.save
+      saved = model.save
     end
-    model.reload if model.id && @saved
-    @saved = false
+    model.reload if model.id && saved
+    saved
   end
 
   def attachment_fields

api/spec/lib/updater_spec.rb
@@ -1,0 +1,15 @@
+describe Updaters::Default do
+
+  let(:updatable) {
+    FactoryBot.create(:user)
+  }
+
+  let(:invalid_params) {
+    { type: "user", data: { attributes: { email: "" } } }
+  }
+
+  it "returns a model with errors if the request is invalid" do
+    expect(::Updaters::User.new(invalid_params).update(updatable).valid?).to be false
+  end
+
+end