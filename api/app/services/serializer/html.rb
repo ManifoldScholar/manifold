c89506610fbf3e3912e6fcb55c7df842919bf8c5
Max Ono <max@castironcoding.com>
2018-07-26 11:53:41 -0700

    [B] Strip <link> tags from text section bodies
    Fixes #1244

--- api/app/services/serializer/html.rb
+++ api/app/services/serializer/html.rb
@@ -6,7 +6,7 @@ module Serializer
   # which will likely then be transformed into JSON. The Manifold React frontend can then
   # traverse this structure and create React components on the fly.
   class Html
-    ELEMENT_BLACK_LIST = %w(script).freeze
+    ELEMENT_BLACK_LIST = %w(script link).freeze
     INLINE_ELEMENTS = %w(b big i small tt abbr acronym cite code dfn em kbd strong samp
                          time var a bdo br img map object q script span sub sup button
                          input label select textarea).freeze
