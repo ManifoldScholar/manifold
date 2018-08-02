926f12a5b0e444647331abcbb51b017e4c0d99da
blnkt <blake@castironcoding.com>
2018-08-02 11:01:46 -0700

    [B] Fix focus trap return focus for UIPanels
    Resolves #1279
    
    The focus trap expects you to be mounting and unmounting the target element when activating and deactivating focus trap. So, when the focus trap mounts/activates the first time it uses whatever element had focus before it activated as the element to return focus to when it is deactivated.  As such if the element was mounted on page load, in the case of the UIPanels, the "return focus to" element defaulted to the document body, and, when the focus trap is toggled manually, it returns focus to the body instead of the element that opened the panel.  This fix mounts and unmounts the panels instead of hiding with css and uses CSSTransitionGroup, along with some new css animations, to emulate the existing slide and fade transitions used with panels.

--- client/src/components/global/UIPanel.js
+++ client/src/components/global/UIPanel.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
+import { CSSTransitionGroup as ReactCSSTransitionGroup } from "react-transition-group";
 import FocusTrap from "focus-trap-react";
 
 export default class UIPanel extends Component {
@@ -11,6 +12,20 @@ export default class UIPanel extends Component {
     hidePanel: PropTypes.func
   };
 
+  componentDidMount() {
+    document.addEventListener("keyup", this.handleLeaveKey);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keyup", this.handleLeaveKey);
+  }
+
+  handleLeaveKey = event => {
+    if (event.keyCode === 27) {
+      this.props.hidePanel(event);
+    }
+  };
+
   render() {
     const visibility = this.props.visibility[this.props.id];
     const visibilityClass = classNames({
@@ -19,21 +34,33 @@ export default class UIPanel extends Component {
     });
 
     return (
-      <div className={visibilityClass}>
-        <FocusTrap
-          active={visibility}
-          focusTrapOptions={{
-            clickOutsideDeactivates: true,
-            escapeDeactivates: false
-          }}
-        >
-          <button className="screen-reader-text" onClick={this.props.hidePanel}>
-            Close Panel
-          </button>
-          {/* Second argument as props */}
-          {React.createElement(this.props.bodyComponent, { ...this.props })}
-        </FocusTrap>
-      </div>
+      /* eslint-disable react/jsx-boolean-value */
+      <ReactCSSTransitionGroup
+        transitionName="panel"
+        transitionEnter={false}
+        transitionLeaveTimeout={200}
+      >
+        {visibility ? (
+          <div className={visibilityClass} key={this.props.id}>
+            <FocusTrap
+              focusTrapOptions={{
+                clickOutsideDeactivates: true,
+                escapeDeactivate: false
+              }}
+            >
+              <button
+                className="screen-reader-text"
+                onClick={this.props.hidePanel}
+              >
+                Close Panel
+              </button>
+              {/* Second argument as props */}
+              {React.createElement(this.props.bodyComponent, { ...this.props })}
+            </FocusTrap>
+          </div>
+        ) : null}
+      </ReactCSSTransitionGroup>
     );
+    /* eslint-enable react/jsx-boolean-value */
   }
 }
