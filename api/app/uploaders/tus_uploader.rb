a4b829676a94beca0e253a7efb123721d0020cf2
Alexa Grey <alexa@castironcoding.com>
2018-08-08 14:10:39 -0700

    [C] Ensure that all tus files are promoted on save

--- api/app/uploaders/tus_uploader.rb
+++ api/app/uploaders/tus_uploader.rb
@@ -2,4 +2,8 @@
 class TusUploader < Shrine
   # use Shrine::Storage::Tus for temporary storage
   storages[:cache] = storages[:tus]
+
+  # Ensure that all uploads from tus get removed
+  # after promotion
+  plugin :delete_promoted
 end
