ee910af585df92eafb5262c659fd90ae94b42493
Max Ono <max@castironcoding.com>
2018-08-20 20:17:39 -0700

    [C] Fix FollowingContainer snapshot

--- client/src/containers/frontend/__tests__/Following-test.js
+++ client/src/containers/frontend/__tests__/Following-test.js
@@ -1,9 +1,10 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { FeaturedContainer } from "../Featured";
+import { FollowingContainer } from "../Following";
 import { Provider } from "react-redux";
 import build from "test/fixtures/build";
 import { wrapWithRouter } from "test/helpers/routing";
+import auth from "test/helpers/auth";
 
 describe("Frontend Following Container", () => {
   const store = build.store();
@@ -25,25 +26,30 @@ describe("Frontend Following Container", () => {
     currentUser: user
   };
 
-  const component = renderer.create(
+  const component = () => renderer.create(
     wrapWithRouter(
       <Provider store={store}>
-        <FeaturedContainer
+        <FollowingContainer
           authentication={authentication}
           featuredProjects={featuredProjects}
           followedProjects={followedProjects}
+          dispatch={store.dispatch}
         />
       </Provider>
     )
   );
 
   it("renders correctly", () => {
-    let tree = component.toJSON();
+    global.scrollTo = jest.fn();
+    auth.startSession(store.dispatch, user);
+    let tree = component().toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("doesn't render to null", () => {
-    let tree = component.toJSON();
+    global.scrollTo = jest.fn();
+    auth.startSession(store.dispatch, user);
+    let tree = component().toJSON();
     expect(tree).not.toBe(null);
   });
 });
