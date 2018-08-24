603b2d27422990ec576ba16904e79dfd245f6b60
Max Ono <max@castironcoding.com>
2018-08-24 10:34:40 -0700

    [C] Monkeypatch classy_enum to allow type casting
    Among other things, this patch allows passing a reference to an actual enum
    rather than requiring a conversion to string/symbol.

--- api/config/initializers/30_monkeypatches.rb
+++ api/config/initializers/30_monkeypatches.rb
@@ -1,4 +1,5 @@
 require Rails.root.join("lib", "patches", "metadown_custom_renderer").to_s
+require Rails.root.join("lib", "patches", "better_enums").to_s
 
 # See https://github.com/nahi/httpclient/issues/252
 class WebAgent
