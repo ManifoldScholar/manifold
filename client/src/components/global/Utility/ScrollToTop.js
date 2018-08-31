6e2bd760227c9de4f963fe9f5331db5b1f95096b
Zach Davis <zach@castironcoding.com>
2018-08-31 12:31:21 -0700

    [B] Reset scroll when moving from BE to FE

--- client/src/components/global/Utility/ScrollToTop.js
+++ client/src/components/global/Utility/ScrollToTop.js
@@ -1,16 +1,33 @@
 import { PureComponent } from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
+import locationHelper from "helpers/location";
 
 class ScrollToTop extends PureComponent {
   static propTypes = {
     location: PropTypes.object
   };
 
-  componentDidUpdate(prevProps) {
-    if (this.props.location !== prevProps.location) {
-      window.scrollTo(0, 0);
+  componentDidMount() {
+    if (!locationHelper.preventsScroll(this.props.location)) {
+      this.doScroll();
     }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      locationHelper.triggersScrollToTop(
+        this.props.location,
+        prevProps.location
+      )
+    ) {
+      this.doScroll();
+    }
+  }
+
+  doScroll() {
+    if (!__CLIENT__) return;
+    window.scrollTo(0, 0);
   }
 
   render() {
