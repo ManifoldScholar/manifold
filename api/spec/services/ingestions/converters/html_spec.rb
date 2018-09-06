35920781d85f498a7d469e68486df16c47629c55
Zach Davis <zach@castironcoding.com>
2018-09-06 06:32:49 -0700

    [B] Restore support for non-latin ingestion
    Resolves #1364

--- api/spec/services/ingestions/converters/html_spec.rb
+++ api/spec/services/ingestions/converters/html_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Ingestions::Converters::Html do
-  let(:path) { Rails.root.join("spec", "data", "ingestion", "html", "with_inline_styles", "index.html") }
   let(:ingestion) do
     ingestion = FactoryBot.create(:ingestion, text: nil)
     allow(ingestion).to receive(:ingestion_source).and_return(path)
@@ -11,7 +10,10 @@ RSpec.describe Ingestions::Converters::Html do
   let(:output) { Ingestions::Converters::Html.run context: context, source_path: context.rel(context.source_path) }
   let(:parsed) { Nokogiri::HTML(output.result) }
 
-  describe "the resulting html" do
+  describe "when ingesting html with inline styles" do
+
+    let(:path) { Rails.root.join("spec", "data", "ingestion", "html", "with_inline_styles", "index.html") }
+
     it "has a style tag with the extracted inline styles assigned to classes" do
       styles = parsed.at("//style")
       expected = ".extracted-inline-style-1 { font-weight: bold; } .extracted-inline-style-2 { text-decoration: underline; }"
@@ -24,5 +26,20 @@ RSpec.describe Ingestions::Converters::Html do
       expect(spans.map { |span| span.attributes["class"].value }).to eq expected
     end
   end
+
+  describe "when ingesting html with non-latin characters" do
+
+    let(:path) { Rails.root.join("spec", "data", "ingestion", "html", "non_latin", "index.html") }
+
+    it "has the correct content" do
+      paragraphs = parsed.xpath("//p")
+      expected = "Είναι πλέον"
+      paragraphs.each do |paragraph|
+        expect(paragraph.text.strip).to eq expected
+      end
+    end
+
+  end
+
 
 end
