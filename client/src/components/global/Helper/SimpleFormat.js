32e506f6b91eecb04a6abe7929b86eb41983b7b7
Zach Davis <zach@castironcoding.com>
2018-10-26 08:50:09 -0700

    [B] Link URLs in comments and annotations
    Fixes #1543

--- client/src/components/global/Helper/SimpleFormat.js
+++ client/src/components/global/Helper/SimpleFormat.js
@@ -1,5 +1,29 @@
 import React, { PureComponent, createElement } from "react";
 import PropTypes from "prop-types";
+import nl2br from "nl2br";
+import he from "he";
+import Loadable from "react-loadable";
+
+const formattedText = props => {
+  const sanitized = he.encode(props.text);
+  const formatted = nl2br(sanitized);
+
+  const Loaded = Loadable({
+    loader: () => import(/* webpackChunkName: "autolinker" */ "autolinker"),
+    render(autolinker) {
+      return (
+        <p dangerouslySetInnerHTML={{ __html: autolinker.link(formatted) }} />
+      );
+    },
+    loading: () => <p dangerouslySetInnerHTML={{ __html: sanitized }} />
+  });
+
+  return <Loaded {...props} />;
+};
+
+formattedText.propTypes = {
+  text: PropTypes.string.isRequired
+};
 
 export default class SimpleFormat extends PureComponent {
   static propTypes = {
@@ -14,30 +38,12 @@ export default class SimpleFormat extends PureComponent {
     wrapperTagProps: {}
   };
 
-  lines(text) {
-    const lines = text.match(/[^\r\n]+/g);
-    return lines;
-  }
-
   render() {
-    /* eslint-disable no-unused-vars */
-    const { text, wrapperTag, wrapperTagProps, postfix } = this.props;
-    /* eslint-enable no-unused-vars */
-    const lines = this.lines(text);
-    const children = (
-      <p>
-        {lines.map((line, index) => {
-          const last = index === lines.length - 1;
-          const after = last ? null : <br />;
-          return (
-            <span key={line}>
-              {line}
-              {after}
-            </span>
-          );
-        })}
-      </p>
+    const { wrapperTag, wrapperTagProps } = this.props;
+    return createElement(
+      wrapperTag,
+      wrapperTagProps,
+      formattedText(this.props)
     );
-    return createElement(wrapperTag, wrapperTagProps, children);
   }
 }
