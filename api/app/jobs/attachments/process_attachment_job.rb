b82846bd79bb4f5a516980b434d5a06355dadf76
Alexa Grey <alexa@castironcoding.com>
2018-11-02 14:29:08 -0700

    [C] Resolve race conditions with attachments
    * Use an advisory lock in the job to ensure only one
      attachment per model/id tuple is processed at a time
    * Wrap the process in a transaction as well to ensure it
      is isolated from other changes that may be occuring
      outside the advisory lock.

--- api/app/jobs/attachments/process_attachment_job.rb
+++ api/app/jobs/attachments/process_attachment_job.rb
@@ -1,9 +1,19 @@
 module Attachments
   class ProcessAttachmentJob < ApplicationJob
+    include Concerns::ExclusiveJob
+
     queue_as :default
 
     def perform(data)
       AttachmentUploader::Attacher.promote data
+    end
+
+    def exclusive_lock_name
+      klass, id = *arguments.dig(0, 'record')
+
+      return super unless klass.present? && id.present?
+
+      "#{super}:#{klass}:#{id}"
     end
   end
 end
---
+++ api/app/jobs/concerns/exclusive_job.rb
@@ -0,0 +1,33 @@
+module Concerns
+  module ExclusiveJob
+    extend ActiveSupport::Concern
+
+    included do
+      around_perform :lock_and_transact!
+    end
+
+    # @return [void]
+    def lock_and_transact!
+      logger.debug "LOCKING"
+
+      result = ApplicationRecord.with_advisory_lock_result(exclusive_lock_name, timeout_seconds: 30, transaction: true) do
+        logger.debug "LOCK ACQUIRED"
+
+        yield if block_given?
+      end
+
+      logger.debug "LOCK RELEASED"
+
+      return if result.lock_was_acquired?
+
+      logger.warn "LOCK FAILED TO ACQUIRE IN TIME"
+
+      reenqueue 30..60, "lock failed to acquire"
+    end
+
+    # @return [String]
+    def exclusive_lock_name
+      self.class.name
+    end
+  end
+end
