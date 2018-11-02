7cf6284c8ace0f5e6251d12085b878d70214c718
Alexa Grey <alexa@castironcoding.com>
2018-11-02 14:25:15 -0700

    [C] Make migration work sans Paperclip

--- api/db/migrate/20180921160539_add_press_logo_footer_to_settings.rb
+++ api/db/migrate/20180921160539_add_press_logo_footer_to_settings.rb
@@ -1,13 +1,25 @@
 class AddPressLogoFooterToSettings < ActiveRecord::Migration[5.0]
-  def self.up
-    change_table :settings do |t|
-      t.attachment :press_logo_footer
-      t.attachment :press_logo_mobile
-    end
+  def change
+    paperclip_attachment :settings, :press_logo_footer
+    paperclip_attachment :settings, :press_logo_mobile
   end
 
-  def self.down
-    remove_attachment :settings, :press_logo_footer
-    remove_attachment :settings, :press_logo_mobile
+  private
+
+  def paperclip_attachment(table, prefix)
+    reversible do |dir|
+      dir.up do
+        add_column table, :"#{prefix}_file_name", :string
+        add_column table, :"#{prefix}_content_type", :string
+        add_column table, :"#{prefix}_file_size", :integer
+        add_column table, :"#{prefix}_updated_at", :timestamp
+      end
+
+      dir.down do
+        %w(file_name content_type file_size updated_at).each do |suffix|
+          remove_column table, :"#{prefix}_#{suffix}"
+        end
+      end
+    end
   end
 end
