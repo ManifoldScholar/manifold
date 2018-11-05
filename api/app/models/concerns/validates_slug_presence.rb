fa71810afe76affbb43276471466e5c95ff9199f
Max Ono <max@castironcoding.com>
2018-11-05 08:47:54 -0800

    [B] Validate slug uniqueness
    Fixes #1591

--- api/app/models/concerns/validates_slug_presence.rb
+++ api/app/models/concerns/validates_slug_presence.rb
@@ -3,7 +3,7 @@ module Concerns
     extend ActiveSupport::Concern
 
     included do
-      validates :slug, presence: true, allow_nil: true
+      validates :slug, presence: true, uniqueness: true, allow_nil: true
     end
   end
 end
