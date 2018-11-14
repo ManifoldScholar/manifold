9d22673625d8c76e4922e6a2ce7a4a5c0d4149d1
Max Ono <max@castironcoding.com>
2018-11-14 09:18:10 -0800

    [C] Remove sort_name from Maker
    Closes #1642

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181114164450) do
+ActiveRecord::Schema.define(version: 20181114170950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -284,7 +284,6 @@ ActiveRecord::Schema.define(version: 20181114164450) do
   end
 
   create_table "makers", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.string   "sort_name"
     t.datetime "created_at",                                  null: false
     t.datetime "updated_at",                                  null: false
     t.string   "first_name"
