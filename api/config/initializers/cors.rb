0c670a40aa32c176be7ea9ee84577dbd11863e4f
Zach Davis <zach@castironcoding.com>
2018-11-15 11:46:52 -0800

    [C] Allow any CORS requests
    In production, Manifold should be served behind an nginx proxy, and cors should
    be enforced at that level rather than within the application.

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,6 +7,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
-    resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options]
+    resource "*", headers: :any, methods: :any
   end
 end
