4c12ccec8c82b08e85ca18f2c1f0f9235cee2504
Zach Davis <zach@castironcoding.com>
2018-11-29 08:48:57 -0800

    [C] Reorganize client modules

--- client/src/containers/reader/index.js
+++
@@ -1,8 +0,0 @@
-import Reader from "./Reader";
-import Notation from "./Notation";
-import Annotation from "./Annotation";
-import Section from "./Section";
-import ReaderNotes from "./ReaderNotes";
-import Search from "./Search";
-
-export { Reader, Notation, Annotation, Section, ReaderNotes, Search };
