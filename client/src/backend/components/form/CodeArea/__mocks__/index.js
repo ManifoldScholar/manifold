15fceed30323cdb8ee61153b29b60065bfb2f61e
Zach Davis <zach@castironcoding.com>
2018-11-30 08:57:04 -0800

    [C] Adjust mock locations

--- client/src/backend/components/form/CodeArea/__mocks__/index.js
+++ client/src/backend/components/form/CodeArea/__mocks__/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
-import setter from "../setter";
+import setter from "../../setter";
 
 class FormCodeArea extends Component {
   render() {
