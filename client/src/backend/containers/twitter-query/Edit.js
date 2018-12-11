11be132410fbd59bafb88b19c6526654c4e98e22
Max Ono <max@castironcoding.com>
2018-12-11 12:37:29 -0800

    [B] Hide tweet fetching if not configured
    Fixes #1736

--- client/src/backend/containers/twitter-query/Edit.js
+++ client/src/backend/containers/twitter-query/Edit.js
@@ -13,6 +13,7 @@ const { request, flush } = entityStoreActions;
 export class TwitterQueryEditContainer extends PureComponent {
   static mapStateToProps = (state, ownPropsIgnored) => {
     return {
+      settings: select(requests.settings, state.entityStore),
       twitterQuery: select(requests.beTwitterQuery, state.entityStore)
     };
   };
@@ -47,13 +48,24 @@ export class TwitterQueryEditContainer extends PureComponent {
     this.props.dispatch(flush([requests.beTwitterQuery]));
   }
 
+  get twitterEnabled() {
+    const { settings } = this.props;
+    if (!settings) return false;
+
+    const {
+      twitterAppId,
+      twitterAccessToken
+    } = settings.attributes.integrations;
+    return twitterAppId && twitterAccessToken;
+  }
+
   fetchTwitterQuery(id) {
     const call = twitterQueriesAPI.show(id);
     const queryRequest = request(call, requests.beTwitterQuery);
     this.props.dispatch(queryRequest);
   }
 
-  handleQueryDestroy(event, query) {
+  handleQueryDestroy = () => {
     const heading = "Are you sure you want to delete this twitter query?";
     const message = (
       <div>
@@ -74,31 +86,32 @@ export class TwitterQueryEditContainer extends PureComponent {
       });
     }).then(
       () => {
-        this.destroyQuery(query);
+        this.destroyQuery();
         this.closeDialog();
       },
       () => {
         this.closeDialog();
       }
     );
-  }
+  };
 
-  destroyQuery(query) {
-    const call = twitterQueriesAPI.destroy(query.id);
-    const options = { removes: query };
+  destroyQuery() {
+    const { twitterQuery } = this.props;
+    const call = twitterQueriesAPI.destroy(twitterQuery.id);
+    const options = { removes: twitterQuery };
     const queryRequest = request(call, requests.beTwitterQueryDestroy, options);
     this.props.dispatch(queryRequest).promise.then(() => {
       this.redirectToProjectSocial(this.props.match.params.pId);
     });
   }
 
-  handleQueryFetch(query) {
-    const call = twitterQueriesAPI.fetch(query.id);
+  handleQueryFetch = () => {
+    const call = twitterQueriesAPI.fetch(this.props.twitterQuery.id);
     const queryRequest = request(call, requests.beTwitterQueryFetch);
     this.props.dispatch(queryRequest).promise.then(() => {
       this.redirectToProjectSocial(this.props.match.params.pId);
     });
-  }
+  };
 
   redirectToProjectSocial(pId) {
     return this.props.history.push(lh.link("backendProjectSocial", pId));
@@ -106,6 +119,20 @@ export class TwitterQueryEditContainer extends PureComponent {
 
   closeDialog() {
     this.setState({ confirmation: null });
+  }
+
+  renderFetchButton() {
+    if (!this.twitterEnabled) return null;
+
+    return (
+      <React.Fragment>
+        <button className="button-bare-primary" onClick={this.handleQueryFetch}>
+          {"Fetch Tweets"}
+          <i className="manicon manicon-check" aria-hidden="true" />
+        </button>
+        <br />
+      </React.Fragment>
+    );
   }
 
   render() {
@@ -124,19 +151,10 @@ export class TwitterQueryEditContainer extends PureComponent {
             <span>{twitterQuery.attributes.displayName}</span>
           </h2>
           <div className="buttons-bare-vertical">
+            {this.renderFetchButton()}
             <button
               className="button-bare-primary"
-              onClick={() => this.handleQueryFetch(twitterQuery)}
-            >
-              {"Fetch Tweets"}
-              <i className="manicon manicon-check" aria-hidden="true" />
-            </button>
-            <br />
-            <button
-              className="button-bare-primary"
-              onClick={event => {
-                this.handleQueryDestroy(event, twitterQuery);
-              }}
+              onClick={this.handleQueryDestroy}
             >
               {"Delete Query"}
               <i className="manicon manicon-trashcan" aria-hidden="true" />
