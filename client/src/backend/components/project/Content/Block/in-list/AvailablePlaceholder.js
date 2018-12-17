f092dd4ff80125717a602f32f55de703a28cf14e
Max Ono <max@castironcoding.com>
2018-12-17 13:26:47 -0800

    [F] Scaffold backend content block interface

---
+++ client/src/backend/components/project/Content/Block/in-list/AvailablePlaceholder.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import Utility from "global/components/utility";
+import Identity from "../parts/Identity";
+
+export default class ProjectContentBlockInListAvailablePlaceholder extends PureComponent {
+  static displayName = "Project.Content.Block.InList.AvailablePlaceholder";
+
+  static propTypes = {
+    typeComponent: PropTypes.oneOfType([PropTypes.string, PropTypes.func])
+  };
+
+  render() {
+    const TypeComponent = this.props.typeComponent;
+
+    return (
+      <TypeComponent>
+        {block => (
+          <React.Fragment>
+            <Identity icon={block.icon} title={block.title} />
+          </React.Fragment>
+        )}
+      </TypeComponent>
+    );
+  }
+}
