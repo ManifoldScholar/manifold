95389de30453a16f44aaf45bc510a26fc12bb83a
Dana Johnson <dana@castironcoding.com>
2019-01-10 15:18:30 -0800

    [F] Build out markup & styles for backend Project Content Blocks

--- api/app/serializers/content_block_serializer.rb
+++ api/app/serializers/content_block_serializer.rb
@@ -3,11 +3,20 @@ class ContentBlockSerializer < ApplicationSerializer
   type "content_blocks"
   meta(partial: false)
 
-  attributes :id, :type, :position, :visible, :configurable
+  attributes :id, :type, :position, :visible, :configurable, :orderable,
+             :hideable, :abilities
 
   belongs_to :project
 
   def configurable
     object.configurable?
+  end
+
+  def orderable
+    object.orderable?
+  end
+
+  def hideable
+    object.hideable?
   end
 end
