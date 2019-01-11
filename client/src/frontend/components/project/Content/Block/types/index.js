5ce9d8bac868bd5c0263e33067f34722e1f2efb2
Max Ono <max@castironcoding.com>
2019-01-11 11:47:38 -0800

    [C] Add placeholder frontend content blocks; Resolve component from type

---
+++ client/src/frontend/components/project/Content/Block/types/index.js
@@ -0,0 +1,13 @@
+import Markdown from "./Markdown";
+import Metadata from "./Metadata";
+import RecentActivity from "./RecentActivity";
+import Resources from "./Resources";
+import TableOfContents from "./TableOfContents";
+
+export default {
+  Markdown,
+  Metadata,
+  RecentActivity,
+  Resources,
+  TableOfContents
+};
---
+++ client/src/frontend/components/project/Content/index.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import Block from "./Block";
+
+export default class ProjectContent extends PureComponent {
+  static displayName = "Project.Content";
+
+  static propTypes = {
+    project: PropTypes.object
+  };
+
+  get project() {
+    return this.props.project;
+  }
+
+  get contentBlocks() {
+    return this.project.relationships.contentBlocks;
+  }
+
+  render() {
+    return this.contentBlocks.map(block => {
+        return <Block block={block} project={this.project} />
+      }
+    )
+  }
+}
