68738fff9637115487c2413cabb3fc05a820607d
Max Ono <max@castironcoding.com>
2019-01-11 11:47:38 -0800

    [C] Add placeholder frontend content blocks; Resolve component from type

---
+++ client/src/helpers/contentBlockHelpers.js
@@ -0,0 +1,16 @@
+class ContentBlockHelpers {
+  typeToComponentKey(type) {
+    const matches = type.match(/^Content::(.*)Block$/);
+    if (!matches) return null;
+    return matches[1];
+  }
+
+  typeToComponent(type, types) {
+    const key = this.typeToComponentKey(type);
+    if (!key) return null;
+    const component = types[key];
+    return component || null;
+  }
+}
+
+export default new ContentBlockHelpers();
