4f57ceea455487077d65cc2251f2fcbc6c5be9ed
Dana Johnson <dana@castironcoding.com>
2019-01-15 13:31:29 -0800

    [F] Build shared frontend content block elements
    Creates a Wrapper and a Heading component used across frontend project
    content blocks and implements them with each block type.

--- client/src/frontend/components/project/Content/Block/types/Metadata.js
+++ client/src/frontend/components/project/Content/Block/types/Metadata.js
@@ -1,18 +1,33 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
+import Wrapper from "../parts/Wrapper";
+import Heading from "../parts/Heading";
 
 export default class ProjectContentBlockMetadataBlock extends PureComponent {
   static displayName = "Project.Content.Block.Metadata";
 
   static propTypes = {
+    title: PropTypes.string,
+    icon: PropTypes.string,
     block: PropTypes.object.isRequired
   };
+
+  static defaultProps = {
+    title: "Metadata",
+    icon: "tag"
+  }
+
 
   get block() {
     return this.props.block;
   }
 
   render() {
-    return <div>Metadata Block [{this.block.id}]</div>;
+    return (
+      <Wrapper>
+        <Heading title={this.props.title} icon={this.props.icon} />
+        <div>Metadata Block [{this.block.id}]</div>
+      </Wrapper>
+    );
   }
 }
