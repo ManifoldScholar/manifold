e8237fb1bb3210a3fcc028f131a33eadae6ad484
Max Ono <max@castironcoding.com>
2019-01-15 10:13:10 -0800

    [B] Fix video thumbnail attachments not saving
    Also refactors variant form components and adds support for a video
    file poster image.

---
+++ client/src/backend/components/resource/form/kind/Variants/Interactive.js
@@ -0,0 +1,36 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import Form from "backend/components/form";
+
+export default class ResourceFormKindVariantsInteractive extends PureComponent {
+  static displayName = "Resource.Form.Kind.Variants.Interactive";
+
+  static propTypes = {
+    kind: PropTypes.string
+  };
+
+  render() {
+    return (
+      <div className="form-section">
+        <Form.Upload
+          layout="landscape"
+          label="Poster Image"
+          accepts="images"
+          readFrom="attributes[variantPosterStyles][small]"
+          name="attributes[variantPoster]"
+          remove="attributes[removeVariantPoster]"
+          {...this.props}
+        />
+        <Form.Upload
+          layout="square"
+          label="Thumbnail Image"
+          accepts="images"
+          readFrom="attributes[variantThumbnailStyles][small]"
+          name="attributes[variantThumbnail]"
+          remove="attributes[removeVariantThumbnail]"
+          {...this.props}
+        />
+      </div>
+    );
+  }
+}
---
+++ client/src/backend/components/resource/form/kind/Variants/Video.js
@@ -0,0 +1,61 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import Form from "backend/components/form";
+
+export default class ResourceFormKindVariantsVideo extends PureComponent {
+  static displayName = "Resource.Form.Kind.Variants.Video";
+
+  static propTypes = {
+    kind: PropTypes.string,
+    resource: PropTypes.object.isRequired
+  };
+
+  get externalVideo() {
+    return this.props.resource.attributes.subKind === "external_video";
+  }
+
+  render() {
+    return (
+      <Form.FieldGroup>
+        <Form.Upload
+          layout="square"
+          label="Variant #1"
+          accepts={this.props.kind}
+          readFrom="attributes[variantFormatOneFileName]"
+          name="attributes[variantFormatOne]"
+          remove="attributes[removeVariantFormatOne]"
+          {...this.props}
+        />
+        <Form.Upload
+          layout="square"
+          label="Variant #2"
+          accepts={this.props.kind}
+          readFrom="attributes[variantFormatTwoFileName]"
+          name="attributes[variantFormatTwo]"
+          remove="attributes[removeVariantFormatTwo]"
+          {...this.props}
+        />
+        {!this.externalVideo && (
+          <Form.Upload
+            layout="landscape"
+            label="Poster Image"
+            accepts="images"
+            readFrom="attributes[variantPosterStyles][small]"
+            name="attributes[variantPoster]"
+            remove="attributes[removeVariantPoster]"
+            {...this.props}
+          />
+        )}
+        <Form.Upload
+          layout="square"
+          label="Thumbnail Image"
+          accepts="images"
+          readFrom="attributes[variantThumbnailStyles][small]"
+          name="attributes[variantThumbnail]"
+          remove="attributes[removeVariantThumbnail]"
+          {...this.props}
+        />
+      </Form.FieldGroup>
+    );
+  }
+}
