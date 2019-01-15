fa6f686f2435865843fd28f567c9a7f2884eb404
Dana Johnson <dana@castironcoding.com>
2019-01-15 13:31:29 -0800

    [F] Build shared frontend content block elements
    Creates a Wrapper and a Heading component used across frontend project
    content blocks and implements them with each block type.

--- client/src/frontend/components/project/Content/Block/types/Resources.js
+++ client/src/frontend/components/project/Content/Block/types/Resources.js
@@ -1,18 +1,32 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
+import Wrapper from "../parts/Wrapper";
+import Heading from "../parts/Heading";
 
 export default class ProjectContentBlockResourcesBlock extends PureComponent {
   static displayName = "Project.Content.Block.Resources";
 
   static propTypes = {
+    title: PropTypes.string,
+    icon: PropTypes.string,
     block: PropTypes.object.isRequired
   };
+
+  static defaultProps = {
+    title: "Resources",
+    icon: "cubeShine"
+  }
 
   get block() {
     return this.props.block;
   }
 
   render() {
-    return <div>Resources Block [{this.block.id}]</div>;
+    return (
+      <Wrapper>
+        <Heading title={this.props.title} icon={this.props.icon} />
+        <div>Resources Block [{this.block.id}]</div>
+      </Wrapper>
+    );
   }
 }
