befa05b11411509d000b355a2ecb09fe8b6bbace
Max Ono <max@castironcoding.com>
2019-01-16 11:08:38 -0800

    [B] MarkdownBlock form sets default style value

--- client/src/backend/components/project/Content/TypeForm/types/Markdown.js
+++ client/src/backend/components/project/Content/TypeForm/types/Markdown.js
@@ -1,8 +1,17 @@
 import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
 import Form from "backend/components/form";
 
 export default class ProjectContentTypeFormMarkdown extends PureComponent {
   static displayName = "Project.Content.TypeForm.Types.Markdown";
+
+  static propTypes = {
+    setOther: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    this.props.setOther("normal", "attributes[style]");
+  }
 
   render() {
     return (
