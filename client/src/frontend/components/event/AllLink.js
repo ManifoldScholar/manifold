9f10b226a3589970de60bd1fc899461ecbe05913
Dana Johnson <dana@castironcoding.com>
2019-01-16 10:47:24 -0800

    [F] Build out Recent Activity block

--- client/src/frontend/components/event/AllLink.js
+++ client/src/frontend/components/event/AllLink.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import lh from "helpers/linkHandler";
+import classNames from "classnames";
 
 export default class EventAllLink extends Component {
   static displayName = "Event.AllLink";
@@ -9,14 +10,15 @@ export default class EventAllLink extends Component {
   static propTypes = {
     count: PropTypes.number,
     threshold: PropTypes.number,
-    project: PropTypes.object.isRequired
+    project: PropTypes.object.isRequired,
+    wrapperClasses: PropTypes.string
   };
 
   render() {
-    const { project } = this.props;
+    const { project, wrapperClasses } = this.props;
     if (this.props.count <= this.props.threshold) return null;
     return (
-      <div className="utility right">
+      <div className={classNames(wrapperClasses)}>
         <Link
           to={lh.link("frontendProjectEvents", project.attributes.slug)}
           className="button-primary"
