e005e3601d9cf126a35131a33c2fb8daf6f2ac9a
Max Ono <max@castironcoding.com>
2019-01-17 10:29:52 -0800

    [F] Add TextsBlock and backend form

---
+++ api/app/serializers/content/texts_block_serializer.rb
@@ -0,0 +1,10 @@
+module Content
+  class TextsBlockSerializer < ::ContentBlockSerializer
+
+    attributes :show_authors, :show_descriptions, :show_subtitles, :show_covers,
+               :show_dates, :show_category_labels, :title, :description
+
+    has_many :included_categories
+    has_many :texts
+  end
+end
