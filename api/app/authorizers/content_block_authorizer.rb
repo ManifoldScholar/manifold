544593e1532b6f5938a313980d625a974db38bb0
Zach Davis <zach@castironcoding.com>
2019-01-16 17:15:44 -0800

    [C] Improve hero position UI logic

--- api/app/authorizers/content_block_authorizer.rb
+++ api/app/authorizers/content_block_authorizer.rb
@@ -1,8 +1,8 @@
 class ContentBlockAuthorizer < ProjectRestrictedChildAuthorizer
 
-  def deletable_by?(user, _options = {})
-    return false if resource.type == "Content::HeroBlock"
-    super
-  end
+  # def deletable_by?(user, _options = {})
+  #   return false if resource.type == "Content::HeroBlock"
+  #   super
+  # end
 
 end
