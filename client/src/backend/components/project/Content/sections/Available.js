aea63d2a6fa5ddac4c55b2e29f7760fc4acac019
Zach Davis <zach@castironcoding.com>
2019-01-16 17:15:44 -0800

    [C] Improve hero position UI logic

--- client/src/backend/components/project/Content/sections/Available.js
+++ client/src/backend/components/project/Content/sections/Available.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Header from "./parts/Header";
 import Block from "../Block";
 import { Droppable } from "react-beautiful-dnd";
+import resolver from "../helpers/resolver";
 
 export default class ProjectContentSectionsAvailable extends PureComponent {
   static displayName = "Project.Content.Sections.Available";
@@ -16,16 +17,8 @@ export default class ProjectContentSectionsAvailable extends PureComponent {
     currentBlocks: []
   };
 
-  static types = [
-    "Content::RecentActivityBlock",
-    "Content::ResourcesBlock",
-    "Content::MetadataBlock",
-    "Content::TableOfContentsBlock",
-    "Content::MarkdownBlock"
-  ];
-
   get types() {
-    return this.constructor.types;
+    return Object.keys(resolver.blockComponentsByType());
   }
 
   render() {
@@ -35,23 +28,33 @@ export default class ProjectContentSectionsAvailable extends PureComponent {
           Drag content blocks directly into your project page layout, or use the
           plus symbol to add them.
         </Header>
-        <Droppable droppableId="available" isDropDisabled>
-          {provided => (
-            <div ref={provided.innerRef} className="block-grid full-width">
-              {this.types.map((type, index) => (
-                <Block
-                  currentBlocks={this.props.currentBlocks}
-                  key={type}
-                  context="available"
-                  type={type}
-                  index={index}
-                  onClickAdd={this.props.onClickAdd}
-                />
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
+        <div className="block-grid full-width">
+          {this.types.map((type, index) => (
+            <Droppable
+              key={type}
+              type={
+                resolver.blockComponentsByType()[type].top ? "TOP" : "BOTTOM"
+              }
+              droppableId={`available-${type}`}
+              isDropDisabled
+            >
+              {provided => (
+                <React.Fragment>
+                  <div ref={provided.innerRef}>
+                    <Block
+                      currentBlocks={this.props.currentBlocks}
+                      context="available"
+                      type={type}
+                      index={index}
+                      onClickAdd={this.props.onClickAdd}
+                    />
+                  </div>
+                  {provided.placeholder}
+                </React.Fragment>
+              )}
+            </Droppable>
+          ))}
+        </div>
       </div>
     );
   }
