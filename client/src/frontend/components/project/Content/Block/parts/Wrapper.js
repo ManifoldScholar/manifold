5979d29d007af26d7e48c942bcf5218f63844ac4
Dana Johnson <dana@castironcoding.com>
2019-01-18 15:37:39 -0800

    [F] Build out frontend for Markdown content block
    * Builds markup and styles for block wrapper and style attribute
    * Exposes body_formatted attribute for block
    * Updates page-content styles for markdown content

--- client/src/frontend/components/project/Content/Block/parts/Wrapper.js
+++ client/src/frontend/components/project/Content/Block/parts/Wrapper.js
@@ -1,16 +1,18 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 
 export default class ProjectContentBlockWrapper extends PureComponent {
   static displayName = "Project.Content.Block.Wrapper";
 
   static propTypes = {
-    children: PropTypes.node.isRequired
+    children: PropTypes.node.isRequired,
+    additionalClasses: PropTypes.string
   };
 
   render() {
     return (
-      <section>
+      <section className={classNames(this.props.additionalClasses)}>
         <div className="container entity-section-wrapper">
           {this.props.children}
         </div>
