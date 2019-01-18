cdab73bceecd08625045e9587ff265e968a08bd0
Max Ono <max@castironcoding.com>
2019-01-18 14:40:23 -0800

    [C] Remove unused title column from texts

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190122232547) do
+ActiveRecord::Schema.define(version: 20190122232712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -711,7 +711,6 @@ ActiveRecord::Schema.define(version: 20190122232547) do
   end
 
   create_table "texts", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.string   "title"
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
     t.date     "publication_date"
