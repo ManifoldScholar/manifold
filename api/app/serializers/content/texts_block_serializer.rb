c8c34d6d549414ff348873b4efff2f4cfdc042ce
Max Ono <max@castironcoding.com>
2019-01-22 13:43:28 -0800

    [B] TextsBlock uses formatted description

--- api/app/serializers/content/texts_block_serializer.rb
+++ api/app/serializers/content/texts_block_serializer.rb
@@ -2,7 +2,8 @@ module Content
   class TextsBlockSerializer < ::ContentBlockSerializer
 
     attributes :show_authors, :show_descriptions, :show_subtitles, :show_covers,
-               :show_dates, :show_category_labels, :title, :description
+               :show_dates, :show_category_labels, :title, :description,
+               :description_formatted
 
     has_many :included_categories
     has_many :texts
