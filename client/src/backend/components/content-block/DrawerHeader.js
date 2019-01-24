afd81e5658886b1549e1c2e68377f06b07fa7301
Zach Davis <zach@castironcoding.com>
2019-01-24 11:28:22 -0800

    [F] Add drawer headers to block forms

---
+++ client/src/backend/components/content-block/DrawerHeader.js
@@ -0,0 +1,84 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import Navigation from "backend/components/navigation";
+import resolver from "./helpers/resolver";
+
+export default class ContentBlockDrawerHeader extends PureComponent {
+  static displayName = "ContentBlock.DrawerHeader";
+
+  static propTypes = {
+    contentBlock: PropTypes.object.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    onVisibilityToggle: PropTypes.func.isRequired,
+    icon: PropTypes.string
+  };
+
+  get contentBlock() {
+    return this.props.contentBlock;
+  }
+
+  get title() {
+    const BlockComponent = resolver.typeToBlockComponent(
+      this.contentBlock.attributes.type
+    );
+    if (
+      BlockComponent &&
+      BlockComponent.defaultProps &&
+      BlockComponent.defaultProps.title
+    )
+      return BlockComponent.defaultProps.title;
+    return "Content Block";
+  }
+
+  get icon() {
+    const BlockComponent = resolver.typeToBlockComponent(
+      this.contentBlock.attributes.type
+    );
+    if (
+      BlockComponent &&
+      BlockComponent.defaultProps &&
+      BlockComponent.defaultProps.icon
+    )
+      return BlockComponent.defaultProps.icon;
+    return null;
+  }
+
+  get onDelete() {
+    return this.props.onDelete;
+  }
+
+  get onVisibilityToggle() {
+    return this.props.onVisibilityToggle;
+  }
+
+  get hidden() {
+    return this.contentBlock && !this.contentBlock.attributes.visible;
+  }
+
+  get buttons() {
+    const buttons = [];
+    if (this.onDelete)
+      buttons.push({
+        onClick: this.onDelete,
+        label: "delete",
+        icon: "trash"
+      });
+    if (this.onVisibilityToggle)
+      buttons.push({
+        onClick: this.onVisibilityToggle,
+        label: this.hidden ? "show" : "hide",
+        icon: this.hidden ? "eye-closed" : "eye-open"
+      });
+    return buttons;
+  }
+
+  render() {
+    return (
+      <Navigation.DrawerHeader
+        icon={this.icon}
+        title={this.title}
+        buttons={this.buttons}
+      />
+    );
+  }
+}
