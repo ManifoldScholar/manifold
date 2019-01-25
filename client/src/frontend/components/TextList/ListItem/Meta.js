28bd9a80f2ff4165f37ca1fd94c37b0a3cfbcd1f
Dana Johnson <dana@castironcoding.com>
2019-01-25 13:16:28 -0800

    [B] Minor style updates to Texts block frontend
    * Adjusts mobile spacing and font sizes
    * Hides cover element on mobile
    * Adjusts markup & styles to better handle conditional layout of date
    and publish status

--- client/src/frontend/components/TextList/ListItem/Meta.js
+++ client/src/frontend/components/TextList/ListItem/Meta.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import Date from "./Date";
 import Counts from "./Counts";
 
@@ -14,22 +15,37 @@ export default class TextListListItemMeta extends Component {
     publishedVisible: PropTypes.bool
   };
 
+  get showStatus() {
+    return this.props.datesVisible || this.props.publishedVisible;
+  }
+
   render() {
     const text = this.props.text;
     const baseClass = this.props.baseClass;
 
     return (
       <div className={`${baseClass}__meta`}>
-        {this.props.datesVisible && (
-          <Date
-            baseClass={baseClass}
-            date={this.props.text.attributes.createdAt}
-            datePrefix={this.props.datePrefix}
-            inline
-          />
-        )}
-        {this.props.publishedVisible && (
-          <span className={`${this.props.baseClass}__status`}>Published</span>
+        {this.showStatus && (
+          <div
+            className={classNames(
+              `${baseClass}__status`,
+              `${baseClass}__status--inline`
+            )}
+          >
+            {this.props.datesVisible && (
+              <Date
+                baseClass={baseClass}
+                date={this.props.text.attributes.createdAt}
+                datePrefix={this.props.datePrefix}
+                inline
+              />
+            )}
+            {this.props.publishedVisible && (
+              <span className={`${this.props.baseClass}__published`}>
+                Published
+              </span>
+            )}
+          </div>
         )}
         <Counts text={text} baseClass={baseClass} />
       </div>
