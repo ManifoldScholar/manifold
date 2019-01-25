05246ede49eff45e2856403a7656d17c68446a6b
Zach Davis <zach@castironcoding.com>
2019-01-25 11:09:27 -0800

    [C] Improve resource content block rendering

--- client/src/frontend/components/content-block/Block/types/Resources.js
+++ client/src/frontend/components/content-block/Block/types/Resources.js
@@ -24,49 +24,97 @@ export default class ProjectContentBlockResourcesBlock extends PureComponent {
     return this.props.block;
   }
 
+  get blockTitle() {
+    if (!this.block) return null;
+    return this.block.attributes.title || this.props.title;
+  }
+
   get project() {
     return this.props.project;
   }
 
-  get showCollections() {
-    return this.block.attributes.showCollections;
+  get hasVisibleCollections() {
+    return this.countVisibleCollections > 0;
+  }
+
+  get countVisibleCollections() {
+    return this.visibleCollections.length;
+  }
+
+  get projectCollections() {
+    return this.project.relationships.collections;
+  }
+
+  get countProjectCollections() {
+    return this.projectCollections.length;
   }
 
   get featuredCollections() {
     return this.block.relationships.featuredCollections;
   }
 
-  renderCollectionList() {
+  get visibleCollections() {
+    if (this.block.attributes.showAllCollections)
+      return this.projectCollections;
+    return this.featuredCollections;
+  }
+
+  get hasVisibleResources() {
+    return this.visibleResources.length > 0;
+  }
+
+  get visibleResources() {
+    return this.project.relationships.resources;
+  }
+
+  get resourcesTotal() {
+    return this.project.attributes.resourcesCount;
+  }
+
+  get isSparse() {
     return (
-      <React.Fragment>
-        <ResourceCollectionList.Grid
-          project={this.project}
-          collections={this.featuredCollections}
-        />
-        <ResourceCollectionList.Totals
-          project={this.project}
-          count={this.featuredCollections.length}
-        />
-      </React.Fragment>
+      (!this.hasVisibleCollections && !this.hasVisibleResources) ||
+      (this.hasVisibleCollections && this.visibleCollections.length < 2) ||
+      (this.hasVisibleResources && this.visibleResources.length < 5)
     );
   }
 
   render() {
     const baseClass = "entity-section-wrapper";
-    const { relationships, attributes } = this.project;
 
     return (
       <Wrapper>
-        <Heading title={this.props.title} icon={this.props.icon} />
+        <Heading title={this.blockTitle} icon={this.props.icon} />
         <div className={`${baseClass}__body`}>
-          {this.showCollections && this.renderCollectionList()}
+          {this.hasVisibleCollections && (
+            <React.Fragment>
+              <ResourceCollectionList.Grid
+                project={this.project}
+                collections={this.visibleCollections}
+              />
+              <ResourceCollectionList.Totals
+                alignLeft={this.isSparse}
+                project={this.project}
+                count={this.countProjectCollections}
+              />
+            </React.Fragment>
+          )}
           <ResourceList.Thumbnails
             project={this.project}
-            resources={relationships.uncollectedResources}
+            resources={this.visibleResources}
           />
+          {!this.hasVisibleCollections && (
+            <ResourceCollectionList.Totals
+              alignLeft={this.isSparse}
+              tight
+              project={this.project}
+              count={this.countProjectCollections}
+            />
+          )}
           <ResourceList.Totals
+            alignLeft={this.isSparse}
             project={this.project}
-            count={attributes.uncollectedResourcesCount}
+            count={this.resourcesTotal}
           />
         </div>
       </Wrapper>
