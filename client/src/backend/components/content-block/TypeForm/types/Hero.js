a40b3afc6b83c15f827adb1deafa0ec3f315b1e6
Zach Davis <zach@castironcoding.com>
2019-01-25 12:16:34 -0800

    [C] Remove Hero content block

--- client/src/backend/components/content-block/TypeForm/types/Hero.js
+++
@@ -1,9 +0,0 @@
-import React, { PureComponent } from "react";
-
-export default class ProjectContentTypeFormHero extends PureComponent {
-  static displayName = "Project.Content.TypeForm.Types.Hero";
-
-  render() {
-    return <React.Fragment />;
-  }
-}
