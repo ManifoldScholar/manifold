3003b48036ebe75560b7ba03e18e2ad36ae59a6f
Dana Johnson <dana@castironcoding.com>
2019-01-28 14:49:44 -0800

    [F] Build out & style TOC Block components

--- client/src/frontend/components/content-block/Block/types/TableOfContents.js
+++ client/src/frontend/components/content-block/Block/types/TableOfContents.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import Wrapper from "../parts/Wrapper";
 import Heading from "../parts/Heading";
+import Contents from "./TOCBlock/Contents";
 
 export default class ProjectContentBlockTableOfContentsBlock extends PureComponent {
   static displayName = "Project.Content.Block.TableOfContents";
@@ -17,15 +18,40 @@ export default class ProjectContentBlockTableOfContentsBlock extends PureCompone
     icon: "bulletList"
   };
 
-  get block() {
-    return this.props.block;
+  get blockAttributes() {
+    return this.props.block.attributes;
+  }
+
+  get text() {
+    return this.props.block.relationships.text;
+  }
+
+  get depth() {
+    return this.blockAttributes.depth;
+  }
+
+  get showAuthors() {
+    return this.blockAttributes.showAuthors;
+  }
+
+  get showTextTitle() {
+    return this.blockAttributes.showTextTitle;
   }
 
   render() {
+    const blockClass = "entity-section-wrapper";
+
     return (
       <Wrapper>
         <Heading title={this.props.title} icon={this.props.icon} />
-        <div>Table Of Contents Block [{this.block.id}]</div>
+        <div className={`${blockClass}__body`}>
+          <Contents
+            showTextTitle={this.showTextTitle}
+            showAuthors={this.showAuthors}
+            text={this.text}
+            depth={this.depth}
+          />
+        </div>
       </Wrapper>
     );
   }
