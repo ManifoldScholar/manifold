0c2ec6fb5fc70bde2d149ce1b2045bbcde5b4857
Zach Davis <zach@castironcoding.com>
2019-01-30 10:05:36 -0800

    [C] Create action_callouts table

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,13 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190125204011) do
+ActiveRecord::Schema.define(version: 20190125222928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
   enable_extension "pg_trgm"
   enable_extension "citext"
+
+  create_table "action_callouts", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string   "title"
+    t.string   "url"
+    t.integer  "kind",            default: 0,     null: false
+    t.integer  "location",        default: 0,     null: false
+    t.jsonb    "attachment_data", default: {}
+    t.boolean  "button",          default: false
+    t.integer  "position",        default: 1,     null: false
+    t.uuid     "project_id"
+    t.uuid     "text_id"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.index ["project_id"], name: "index_action_callouts_on_project_id", using: :btree
+    t.index ["text_id"], name: "index_action_callouts_on_text_id", using: :btree
+  end
 
   create_table "annotations", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
     t.string   "start_node"
@@ -81,14 +97,14 @@ ActiveRecord::Schema.define(version: 20190125204011) do
     t.string   "title"
     t.text     "description"
     t.uuid     "project_id"
-    t.string   "thumbnail_checksum"
-    t.string   "fingerprint"
-    t.datetime "created_at",                                     null: false
-    t.datetime "updated_at",                                     null: false
     t.string   "thumbnail_file_name_deprecated"
     t.string   "thumbnail_content_type_deprecated"
     t.integer  "thumbnail_file_size_deprecated"
     t.datetime "thumbnail_updated_at_deprecated"
+    t.string   "thumbnail_checksum"
+    t.string   "fingerprint"
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
     t.string   "slug"
     t.integer  "collection_resources_count",        default: 0
     t.integer  "events_count",                      default: 0
@@ -174,12 +190,6 @@ ActiveRecord::Schema.define(version: 20190125204011) do
     t.string   "link_text"
     t.string   "link_url"
     t.string   "link_target"
-    t.integer  "position"
-    t.text     "style",                              default: "dark"
-    t.boolean  "hidden",                             default: false
-    t.uuid     "creator_id"
-    t.datetime "created_at",                                          null: false
-    t.datetime "updated_at",                                          null: false
     t.string   "background_file_name_deprecated"
     t.string   "background_content_type_deprecated"
     t.integer  "background_file_size_deprecated"
@@ -188,6 +198,12 @@ ActiveRecord::Schema.define(version: 20190125204011) do
     t.string   "foreground_content_type_deprecated"
     t.integer  "foreground_file_size_deprecated"
     t.datetime "foreground_updated_at_deprecated"
+    t.integer  "position"
+    t.text     "style",                              default: "dark"
+    t.boolean  "hidden",                             default: false
+    t.uuid     "creator_id"
+    t.datetime "created_at",                                          null: false
+    t.datetime "updated_at",                                          null: false
     t.string   "background_color"
     t.string   "foreground_color"
     t.string   "header_color"
@@ -284,6 +300,10 @@ ActiveRecord::Schema.define(version: 20190125204011) do
   create_table "ingestions", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
     t.string   "state"
     t.string   "log",                                           array: true
+    t.string   "source_file_name"
+    t.string   "source_content_type"
+    t.integer  "source_file_size"
+    t.datetime "source_updated_at"
     t.string   "strategy"
     t.string   "external_source_url"
     t.string   "ingestion_type"
@@ -292,10 +312,6 @@ ActiveRecord::Schema.define(version: 20190125204011) do
     t.uuid     "project_id",                       null: false
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
-    t.string   "source_file_name"
-    t.string   "source_content_type"
-    t.integer  "source_file_size"
-    t.datetime "source_updated_at"
     t.jsonb    "source_data",         default: {}, null: false
     t.index ["creator_id"], name: "index_ingestions_on_creator_id", using: :btree
     t.index ["project_id"], name: "index_ingestions_on_project_id", using: :btree
@@ -478,6 +494,10 @@ ActiveRecord::Schema.define(version: 20190125204011) do
     t.string   "storage_type"
     t.string   "storage_identifier"
     t.string   "source",                                       null: false
+    t.string   "data_file_name_deprecated"
+    t.string   "data_content_type_deprecated"
+    t.integer  "data_file_size_deprecated"
+    t.datetime "data_updated_at_deprecated"
     t.string   "url"
     t.integer  "header_row",                   default: 1
     t.jsonb    "column_map",                   default: {},    null: false
@@ -485,10 +505,6 @@ ActiveRecord::Schema.define(version: 20190125204011) do
     t.boolean  "parse_error",                  default: false, null: false
     t.datetime "created_at",                                   null: false
     t.datetime "updated_at",                                   null: false
-    t.string   "data_file_name_deprecated"
-    t.string   "data_content_type_deprecated"
-    t.integer  "data_file_size_deprecated"
-    t.datetime "data_updated_at_deprecated"
     t.jsonb    "data_data",                    default: {}
     t.index ["creator_id"], name: "index_resource_imports_on_creator_id", using: :btree
     t.index ["project_id"], name: "index_resource_imports_on_project_id", using: :btree
