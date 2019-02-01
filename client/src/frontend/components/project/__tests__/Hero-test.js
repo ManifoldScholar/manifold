10af5e194b3d0b4114715f30feabfccf79a03b79
Dana Johnson <dana@castironcoding.com>
2019-02-01 15:45:04 -0800

    [C] Update tests

--- client/src/frontend/components/project/__tests__/Hero-test.js
+++ client/src/frontend/components/project/__tests__/Hero-test.js
@@ -4,10 +4,21 @@ import Hero from "../Hero";
 import build from "test/fixtures/build";
 
 describe("Frontend.Project.Hero component", () => {
-  const project = build.entity.project("1");
+  const actionCallout = build.entity.actionCallout("1");
+  const project = build.entity.project("1", {}, {actionCallouts: [actionCallout]});
+
+  const refMock = element => {
+    return { style: { objectFit: "" }};
+  }
 
   it("renders correctly", () => {
-    const component = renderer.create(<Hero project={project} />);
+    const component = renderer.create(<Hero project={project} />, { createNodeMock: refMock });
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders correctly when object-fit is unsupported", () => {
+    const component = renderer.create(<Hero project={project} />, { createNodeMock: element => {return { style: {}}} });
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
