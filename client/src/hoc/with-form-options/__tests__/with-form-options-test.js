dce3aec74e135137743d7f78cb55e4dd3aa1c19a
Zach Davis <zach@castironcoding.com>
2019-02-04 12:54:34 -0800

    [B] Correct withFormOptions handling of integers
    Correct wrapped component displayName as well.

---
+++ client/src/hoc/with-form-options/__tests__/with-form-options-test.js
@@ -0,0 +1,114 @@
+import React from "react";
+import withFormOptions from "../";
+import Enzyme from "enzyme/build";
+import Adapter from "enzyme-adapter-react-16/build";
+Enzyme.configure({ adapter: new Adapter() });
+
+jest.mock("backend/components/form/setter", () => {
+  return (wrappedComponent) => wrappedComponent;
+});
+
+describe("withFormOptions HOC", () => {
+
+  class MockOptionsComponent extends React.Component {
+    render() {
+      return (
+        <div>A mocked component that consumes options</div>
+      )
+    }
+  }
+
+  const FormOptionsComponent = withFormOptions(MockOptionsComponent);
+  const setup = (mock, options = []) => {
+    const wrapper = Enzyme.mount(
+      <FormOptionsComponent
+        name="foo"
+        set={mock}
+        options={options}
+      />);
+    const child = wrapper.find(MockOptionsComponent).instance();
+    return { child };
+  };
+
+  it("calls set with the correct value when options are strings and the value is a string", () => {
+    const set = jest.fn();
+    const { child } = setup(set, [
+      { label: "foo", value: "foo" },
+      { label: "bar", value: "bar" }
+    ]);
+    child.props.onChange({ target: { value: "bar" }});
+    const setValue = set.mock.calls.pop()[0];
+    expect(setValue).toEqual("bar");
+  });
+
+  it("calls set with the correct value when options are integers and the value is a string", () => {
+    const set = jest.fn();
+    const { child } = setup(set, [
+      { label: "1", value: 1 },
+      { label: "2", value: 2 }
+    ]);
+    child.props.onChange({ target: { value: "2" }});
+    const setValue = set.mock.calls.pop()[0];
+    expect(setValue).toEqual(2);
+  });
+
+  it("calls set with the correct value when options are integers and the value is an integer", () => {
+    const set = jest.fn();
+    const { child } = setup(set, [
+      { label: "1", value: 1 },
+      { label: "2", value: 2 }
+    ]);
+    child.props.onChange({ target: { value: 2 }});
+    const setValue = set.mock.calls.pop()[0];
+    expect(setValue).toEqual(2);
+  });
+
+  it("calls set with the correct value when options entities and the value is a string ID", () => {
+    const set = jest.fn();
+    const foo = { id: "foo" };
+    const bar = { id: "bar" };
+    const { child } = setup(set, [
+      { label: "foo", value: foo },
+      { label: "bar", value: bar }
+    ]);
+    child.props.onChange({ target: { value: "bar" }});
+    const setValue = set.mock.calls.pop()[0];
+    expect(setValue).toEqual(bar);
+  });
+
+  it("calls set with the correct value when options entities and the value is an integer ID", () => {
+    const set = jest.fn();
+    const foo = { id: 1 };
+    const bar = { id: 2 };
+    const { child } = setup(set, [
+      { label: "foo", value: foo },
+      { label: "bar", value: bar }
+    ]);
+    child.props.onChange({ target: { value: 2 }});
+    const setValue = set.mock.calls.pop()[0];
+    expect(setValue).toEqual(bar);
+  });
+
+  it("calls set with the correct value when options entities and the value is boolean", () => {
+    const set = jest.fn();
+    const { child } = setup(set, [
+      { label: "foo", value: true },
+      { label: "bar", value: false }
+    ]);
+    child.props.onChange({ target: { value: true }});
+    const setValue = set.mock.calls.pop()[0];
+    expect(setValue).toEqual(true);
+  });
+
+  it("calls set with the correct value when options entities and the value is string boolean", () => {
+    const set = jest.fn();
+    const { child } = setup(set, [
+      { label: "foo", value: true },
+      { label: "bar", value: false }
+    ]);
+    child.props.onChange({ target: { value: "true" }});
+    const setValue = set.mock.calls.pop()[0];
+    expect(setValue).toEqual(true);
+  });
+
+});
