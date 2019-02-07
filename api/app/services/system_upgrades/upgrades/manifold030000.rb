1f2fadc223c6e7d91ca24a9b9291fa96d6a799b3
Max Ono <max@castironcoding.com>
2019-02-07 09:00:57 -0800

    [F] Scaffold project content in v3.0.0 upgrade

--- api/app/services/system_upgrades/upgrades/manifold030000.rb
+++ api/app/services/system_upgrades/upgrades/manifold030000.rb
@@ -5,6 +5,7 @@ module SystemUpgrades
 
       def perform!
         reprocess_text_covers!
+        scaffold_project_content!
       end
 
       private
@@ -21,8 +22,26 @@ module SystemUpgrades
 
         Text.find_each do |text|
           next unless text.cover_attacher.stored?
+
           logger.info("Generating cover styles for Text #{text.id}")
           text.update cover: text.cover[:original]
+        end
+      end
+
+      def scaffold_project_content!
+        logger.info("===================================================================")
+        logger.info("Creating Content Blocks for Projects                               ")
+        logger.info("===================================================================")
+        logger.info("Manifold version 3.0.0 introduces ContentBlocks, which are used to ")
+        logger.info("customize the layout of the project detail page. By default a      ")
+        logger.info("project is created with a RecentActivityBlock, TextsBlock,         ")
+        logger.info("ResourcesBlock, MetadataBlock.  These will be created automatically")
+        logger.info("for new projects, but must be added to existing projects manually. ")
+        logger.info("===================================================================")
+
+        Project.find_each do |project|
+          logger.info("Creating content blocks for #{project.title} [#{project.id}]")
+          Content::ScaffoldProjectContent.run project: project
         end
       end
 
