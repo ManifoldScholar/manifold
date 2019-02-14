a02faad1dbe358426c0e9869b5b4e783da1a2a81
Zach Davis <zach@castironcoding.com>
2019-02-14 11:13:53 -0800

    [B] Generate PDF thumbnails
    …and rescue image processing exceptions.
    
    Fixes #1822

--- api/app/services/attachments/processor.rb
+++ api/app/services/attachments/processor.rb
@@ -26,7 +26,8 @@ module Attachments
 
     def process_versions!
       styles.map do |key, value|
-        versions[key] = process_version value
+        version = process_version value
+        versions[key] = version unless version.nil?
       end
     end
 
--- api/app/services/attachments/version_processor.rb
+++ api/app/services/attachments/version_processor.rb
@@ -6,7 +6,9 @@ module Attachments
     hash :config, strip: false
 
     def execute
-      ImageProcessing::MiniMagick.apply(config).call(source.open)
+      ImageProcessing::MiniMagick.apply(config).loader(page: 0).call(source.open)
+    rescue ImageProcessing::Error
+      nil
     end
   end
 end
