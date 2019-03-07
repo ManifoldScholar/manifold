87c097d090d485df6afffeeb3e27fa197cb8f44f
Max Ono <max@castironcoding.com>
2019-03-07 09:06:13 -0800

    [C] Add #merge_errors! to ApplicationRecord
    This commit implements a method to merge another set of errors into
    an ActiveModel::Errors object.  This is included in the Errors class
    in Rails 5.2, so this should only be needed until Manifold is upgraded.
    
    https://github.com/rails/rails/blob/94b5cd3a20edadd6f6b8cf0bdf1a4d4919df86cb/activemodel/lib/active_model/errors.rb#L105

--- api/app/models/application_record.rb
+++ api/app/models/application_record.rb
@@ -5,6 +5,13 @@ class ApplicationRecord < ActiveRecord::Base
   include Concerns::ArelHelpers
   include WithAdvisoryLock::Concern
 
+  # https://api.rubyonrails.org/classes/ActiveModel/Errors.html#method-i-merge-21
+  # This exists in Rails 5.2, so we can remove this whenever we upgrade to 5.2
+  def merge_errors!(other)
+    errors.messages.merge!(other.messages) { |_, ary1, ary2| ary1 + ary2 }
+    errors.details.merge!(other.details) { |_, ary1, ary2| ary1 + ary2 }
+  end
+
   class << self
     def in_the_week_of(date)
       where(created_at: date.to_week_range)
