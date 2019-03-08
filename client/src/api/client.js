68bba4758811a7095151ce8196639cdcbf965022
Zach Davis <zach@castironcoding.com>
2019-03-08 14:48:42 -0800

    [C] Make low level API easier to mock

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,64 +1,8 @@
 /* eslint-disable no-console */
-import qs from "qs";
 import isPlainObject from "lodash/isPlainObject";
-import config from "config";
-import humps from "humps";
+import LowLevelApiClient from "./LowLevelApiClient";
 
 require("isomorphic-fetch");
-
-export class LowLevelApiClient {
-  constructor() {
-    this.defaultOptions = {
-      authToken: null,
-      params: {}
-    };
-  }
-
-  _adjustedOptions(options) {
-    return Object.assign({}, this.defaultOptions, options);
-  }
-
-  _adjustedMethod(method) {
-    return method.toLowerCase();
-  }
-
-  _adjustedEndpoint(endpoint) {
-    return config.services.api + endpoint;
-  }
-
-  _endpointWithParams(endpoint, params) {
-    return endpoint + "?" + qs.stringify(humps.decamelizeKeys(params));
-  }
-
-  call(rawEndpoint, rawMethod, rawOptions) {
-    const method = this._adjustedMethod(rawMethod);
-    const options = this._adjustedOptions(rawOptions);
-    const endpoint = this._endpointWithParams(
-      this._adjustedEndpoint(rawEndpoint),
-      options.params
-    );
-    const fetchConfig = {
-      method,
-      body: options.body,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${options.authToken}`
-      }
-    };
-    const out = fetch(endpoint, fetchConfig).catch(error => {
-      return new Promise((resolve, reject) => {
-        reject({
-          response: {
-            status: 503,
-            statusText: error.name,
-            exception: error.message
-          }
-        });
-      });
-    });
-    return out;
-  }
-}
 
 export default class ApiClient {
   constructor() {
