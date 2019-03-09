6fff576edebe4adba7d3606be39b2242dbe1a594
Zach Davis <zach@castironcoding.com>
2019-03-09 13:57:49 -0800

    [F] Allow CLIENT_URL to be set via environment

--- api/config/initializers/20_configuration.rb
+++ api/config/initializers/20_configuration.rb
@@ -4,7 +4,7 @@ m = Hashie::Mash.new(Rails.application.config_for(:manifold))
 # for accessing configuration that can be spread across a few locations.
 ssl_enabled = %w(1 true).include?(ENV["SSL_ENABLED"].to_s.downcase)
 m.domain = ENV["DOMAIN"]
-m.url ||= "#{ssl_enabled ? 'https' : 'http'}://#{m.domain}"
+m.url ||= ENV["CLIENT_URL"] || "#{ssl_enabled ? 'https' : 'http'}://#{m.domain}"
 m.api_url ||= ENV["CLIENT_BROWSER_API_URL"] || m.url
 m.elastic_search!.url ||= ENV["ELASTICSEARCH_URL"]
 
