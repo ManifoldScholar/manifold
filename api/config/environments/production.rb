cdeb54382469d95b8a803749347665a03e7e3dfd
Max Ono <max@castironcoding.com>
2019-03-25 09:26:03 -0700

    [F] Add Lograge for production logging Closes #1953

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Use Lograge for request logging
+  config.lograge.enabled = true
 end
