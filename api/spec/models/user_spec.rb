a5746d9b71648a14b37c2324987c87710e2028a7
Max Ono <max@castironcoding.com>
2019-04-04 10:18:41 -0700

    [B] Remove isolation in spec

--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -121,7 +121,7 @@ RSpec.describe User, type: :model do
       expect(results.length).to be 1
     end
 
-    fit "by email" do
+    it "by email" do
       results = User.filter({keyword: email, typeahead: true})
       expect(results.length).to be 1
     end
