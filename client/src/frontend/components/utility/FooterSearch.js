24066c9ed5d7f6eb7ecd9e699572dbf89d90876d
Molly Jean Bennett <molly@castironcoding.com>
2019-04-16 15:07:32 -0700

    [C] Move footer search into separate component

---
+++ client/src/frontend/components/utility/FooterSearch.js
@@ -0,0 +1,54 @@
+import React, { PureComponent } from "react";
+import Utility from "global/components/utility";
+import lh from "helpers/linkHandler";
+
+export default class FooterSearch extends PureComponent {
+  static displayName = "Utility.FooterSearch";
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: ""
+    };
+  }
+
+  updateSearchWord = event => {
+    this.setState({ keyword: event.target.value });
+  };
+
+  doSearch = event => {
+    event.preventDefault();
+    const path = lh.link("frontendSearch");
+    this.props.push(path, {
+      searchQueryState: { keyword: this.state.keyword }
+    });
+    this.setState({ keyword: "" });
+  };
+
+  render() {
+    return (
+      <form className="search-form" onSubmit={this.doSearch}>
+        <div className="search-button-inline">
+          <label htmlFor="footer-search" className="screen-reader-text">
+            Site Search
+          </label>
+          <input
+            type="text"
+            id="footer-search"
+            placeholder="Search"
+            value={this.state.keyword}
+            onChange={this.updateSearchWord}
+          />
+          <button className="icon">
+            <Utility.IconComposer
+              className="search-icon"
+              icon="search16"
+              size={20}
+            />
+            <span className="screen-reader-text">Search</span>
+          </button>
+        </div>
+      </form>
+    )
+  }
+}
