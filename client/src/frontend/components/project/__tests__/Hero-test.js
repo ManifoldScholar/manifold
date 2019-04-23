e5785e080afadbe2a7d342a59beb592cedbd8ab8
Max Ono <max@castironcoding.com>
2019-04-23 09:25:29 -0700

    [B] Open relative CTA links in same tab
    Fixes #2145

--- client/src/frontend/components/project/__tests__/Hero-test.js
+++ client/src/frontend/components/project/__tests__/Hero-test.js
@@ -2,23 +2,35 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Hero from "../Hero";
 import build from "test/fixtures/build";
+import { wrapWithRouter } from "test/helpers/routing";
+import { Provider } from "react-redux";
 
 describe("Frontend.Project.Hero component", () => {
   const actionCallout = build.entity.actionCallout("1");
   const project = build.entity.project("1", {}, {actionCallouts: [actionCallout]});
+  const store = build.store();
 
   const refMock = element => {
-    return { style: { objectFit: "" }};
-  }
+    return { style: { objectFit: "" } };
+  };
 
   it("renders correctly", () => {
-    const component = renderer.create(<Hero project={project} />, { createNodeMock: refMock });
+    const component = renderer.create(wrapWithRouter(
+      <Provider store={store}>
+        <Hero project={project} />
+      </Provider>
+    ), { createNodeMock: refMock });
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("renders correctly when object-fit is unsupported", () => {
-    const component = renderer.create(<Hero project={project} />, { createNodeMock: element => {return { style: {}}} });
+    const component = renderer.create(
+      wrapWithRouter(
+        <Provider store={store}>
+          <Hero project={project} />
+        </Provider>
+      ), { createNodeMock: element => { return { style: {} } } });
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
