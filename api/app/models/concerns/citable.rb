ManifoldScholar/manifold

commit 96c9e76dc1994a2bd1ec2d72129b94a3ef00b195

[C] Resolve race conditions with data loader
* Wrap `DataLoader#load` in a transaction
* Use `after_commit` for `update_citable_children`
* Use advisory lock for event creation
* Idiomatic: use GlobalID in CacheCollectionProjects

api/app/models/concerns/citable.rb
@@ -50,7 +50,7 @@ module Citable
 
     def with_citable_children(*children)
       attr_reader :citable_children
-      after_save :update_citable_children if respond_to? :after_save
+      after_commit :update_citable_children if respond_to? :after_commit
 
       @citable_children = children
 

api/app/jobs/create_event_job.rb
@@ -1,5 +1,7 @@
 # Simple job to create a new event in the background.
 class CreateEventJob < ApplicationJob
+  include Concerns::ExclusiveJob
+
   queue_as :default
 
   rescue_from(::Factory::Errors::NoEventProject) do

api/app/jobs/project_collection_jobs/cache_collection_projects_job.rb
@@ -2,9 +2,7 @@ module ProjectCollectionJobs
   class CacheCollectionProjectsJob < ApplicationJob
 
     # rubocop:disable Metrics/LineLength
-    def perform(id)
-      project_collection = ProjectCollection.find(id)
-      return unless project_collection.present?
+    def perform(project_collection)
       ProjectCollections::CacheCollectionProjects.run project_collection: project_collection
     end
     # rubocop:enable Metrics/LineLength

api/app/jobs/project_collection_jobs/queue_cache_collection_projects_job.rb
@@ -4,7 +4,7 @@ module ProjectCollectionJobs
     # rubocop:disable Metrics/LineLength
     def perform
       ProjectCollection.find_each do |project_collection|
-        ProjectCollectionJobs::CacheCollectionProjectsJob.perform_later project_collection.id
+        ProjectCollectionJobs::CacheCollectionProjectsJob.perform_later project_collection
       end
     end
     # rubocop:enable Metrics/LineLength

api/app/services/demonstration/data_loader.rb
@@ -13,14 +13,16 @@ module Demonstration
     end
 
     def load
-      clear_db
-      seed_db
-      create_admin_user
-      # create_fake_users
-      create_pages
-      import_projects
-      create_featured_projects_collection
-      reindex_records
+      ApplicationRecord.transaction do
+        clear_db
+        seed_db
+        create_admin_user
+        # create_fake_users
+        create_pages
+        import_projects
+        create_featured_projects_collection
+        reindex_records
+      end
     end
 
     def cli_user

commit 9f71e31457ba13aea77572006808bdad5af128c0

[B] Fix citations not being generated on children
This was a regression introduced in 96c9e76.  Since the callback for
`update_citable_children` was moved into `after_commit` the comparison
of `citations` to `citations_was` always returned true.  This commit adjusts
that check to see if the previous changes to a record include `:citations`.  If so,
we know that the parent citations changed and can run the children update.

Fixes #2057

api/app/models/concerns/citable.rb
@@ -40,7 +40,7 @@ module Citable
   end
 
   def update_citable_children
-    return unless citations != citations_was
+    return unless previous_changes.key?(:citations)
 
     UpdateCitatableChildren.perform_later(self)
   end

api/spec/models/concerns/citable_spec.rb
@@ -69,6 +69,77 @@ RSpec.describe Citable do
     it_behaves_like "a citable class"
   end
 
+
+  with_model :CitableClass do
+    table do |t|
+      t.string :title
+      t.string :subtitle
+      t.jsonb :citations, default: {}
+    end
+
+    model do
+      include Citable
+
+      has_many :children, class_name: 'CitableChild'
+
+      with_citation do |citable_class|
+        {
+          title: citable_class.title,
+          author: "Rowan Ono",
+          issued: "10/03/2013"
+        }
+      end
+
+      with_citable_children :children
+    end
+  end
+
+  with_model :CitableChild do
+    table do |t|
+      t.jsonb :citations, default: {}
+      t.belongs_to :citable_class, index: { name: "index_name" }
+    end
+
+    model do
+      include Citable
+
+      belongs_to :citable_class
+
+      with_citation do |child|
+        child.citable_class.citation_parts
+      end
+    end
+  end
+
+  describe "with_citable_children" do
+    let!(:parent) { CitableClass.create(title: "Initial") }
+    let!(:child) { CitableChild.create(citable_class_id: parent.id) }
+
+    context "when citations change" do
+      it "updates the child citations" do
+        expect do
+          perform_enqueued_jobs do
+            parent.title = "Updated"
+            parent.save
+          end
+
+          child.reload
+        end.to change(child, :citations)
+      end
+    end
+
+    context "when citations do not change" do
+      it "does not update the child citations" do
+        expect do
+          perform_enqueued_jobs do
+            parent.subtitle = "Updated"
+            parent.save
+          end
+        end.to_not change(child, :citations)
+      end
+    end
+  end
+
 end
 
 