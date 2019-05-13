b9aa20faea49491957b1977f82d951029ca9a492
connor <connor@castironcoding.com>
2019-05-13 11:00:41 -0700

    [C] Get categories_spec working
    I would like extra attention on the third test in categories_spec.

--- api/app/services/updaters/v2/categories.rb
+++ api/app/services/updaters/v2/categories.rb
@@ -2,13 +2,16 @@ module Updaters
   module V2
     class Categories < Updaters::AbstractUpdater
 
+      # make sure it has a position
       has_position!
 
       with_options default: nil do
         string :title
         string :role
+        record :project
       end
 
+      # ensure that the project record exists
       validates :project, presence: true
     end
   end
--- api/spec/services/updaters/v2/categories_spec.rb
+++ api/spec/services/updaters/v2/categories_spec.rb
@@ -2,8 +2,10 @@ require "rails_helper"
 
 RSpec.describe Updaters::V2::Categories, updaters_v2: true do
 
+  # create a fake project
   let!(:project) { FactoryBot.create :project }
 
+  # create our fake category
   let(:attributes) { {
     title:     "Some category",
     role:      "text",
@@ -13,28 +15,30 @@ RSpec.describe Updaters::V2::Categories, updaters_v2: true do
 
   it "can create a category" do
     # there should be one more category
-    perform_within_expectation!
-    # perform_within_expectation! do |e|
-    #   e.to change(Category, :count).by(1)
-    # end
+    perform_within_expectation! do |e|
+      e.to change(Category, :count).by(1)
+    end
   end
 
-  # context "when updating an existing model" do
-  #   let!(:model) { FactoryBot.create :category }
-  #
-  #   it "can update the category" do
-  #     perform_within_expectation! do |e|
-  #       e.to keep_the_same(Category, :count)
-  #     end
-  #   end
-  # end
-  #
-  # context "with a relative position" do
-  #   let(:position) { "up" }
-  #   it "can create a category" do
-  #     perform_within_expectation! do |e|
-  #       e.to keep_the_same(Category, :count)
-  #     end
-  #   end
-  # end
+  context "when updating an existing model" do
+    let!(:model) { FactoryBot.create :category }
+
+    it "can update the category" do
+      perform_within_expectation! do |e|
+        e.to keep_the_same(Category, :count)
+      end
+    end
+  end
+
+  context "with a relative position" do
+
+    let!(:model) { FactoryBot.create :category }
+    let(:position) { "up" }
+
+    it "can accept a different position" do
+      perform_within_expectation! do |e|
+        e.to keep_the_same(Category, :count)
+      end
+    end
+  end
 end
