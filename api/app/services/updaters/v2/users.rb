d236a0498ee20c883b37b57c2a88c9aaf9788980
connor <connor@castironcoding.com>
2019-05-13 11:39:57 -0700

    [C] Create users_spec

---
+++ api/app/services/updaters/v2/users.rb
@@ -0,0 +1,16 @@
+module Updaters
+  module V2
+    class Users < Updaters::AbstractUpdater
+
+      attachment_field :avatar
+
+      with_options default: nil do
+        string :email
+        string :first_name
+        string :last_name
+        string :password
+        string :password_confirmation
+      end
+    end
+  end
+end
---
+++ api/spec/services/updaters/v2/users_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe Updaters::V2::Users, updaters_v2: true do
+  let(:avatar) { attachment_map "avatar.png", manifold_logo_data_uri }
+
+  let(:attributes) { {
+    avatar:                 avatar,
+    email:                  "JohnRambo@rambo.com",
+    first_name:             Faker::Name.first_name,
+    last_name:              Faker::Name.last_name,
+    password:               "password",
+    password_confirmation:  "password"
+  } }
+
+  it "can create a user" do
+    perform_within_expectation! do |e|
+      e.to change(User, :count).by(1)
+    end
+  end
+
+  context "when updating an existing model" do
+    let!(:model) { FactoryBot.create :user }
+
+    it "can update the user" do
+      perform_within_expectation! do |e|
+        e.to keep_the_same(User, :count)
+      end
+    end
+  end
+end
