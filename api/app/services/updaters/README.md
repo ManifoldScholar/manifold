d1c6b0383536c61e7b2a1280820b381c48594e58
connor <connor@castironcoding.com>
2019-05-13 09:57:29 -0700

    [C] Get pages_spec working

--- api/app/services/updaters/README.md
+++ api/app/services/updaters/README.md
@@ -3,3 +3,9 @@
 Each updater has test located in `src/manifold/api/spec/services/updaters/v2/`
 
 The `abstract_updater.rb` file is a parent class for the updaters and the updaters test files.
+
+## Running tests
+
+If you want to run tests on only the updaters:
+
+`rspec -fd api/spec/services/updaters/v2`
--- api/spec/services/updaters/v2/pages_spec.rb
+++ api/spec/services/updaters/v2/pages_spec.rb
@@ -3,27 +3,18 @@ require "rails_helper"
 RSpec.describe Updaters::V2::Pages, updaters_v2: true do
   let!(:creator) { FactoryBot.create :user }
 
-  let(:title) { Faker::String.random(4) }
-  let(:nav_title) { Faker::String.random(4) }
-  let(:slug) { Faker::String.random(4) }
-  let(:body) { Faker::String.random(4) }
-  let(:external_link) { Faker::String.random(4) }
-
-  let(:show_in_header) { true }
-  let(:hidden) { true }
-  let(:is_external_link) { true }
-  let(:open_in_new_tab) { true }
-
   let(:attributes) { {
-    title: title,
-    nav_title: nav_title,
-    slug: slug,
-    body: body,
-    external_link: external_link,
-    show_in_header: show_in_header,
-    hidden: hidden,
-    is_external_link: is_external_link,
-    open_in_new_tab: open_in_new_tab
+    title:            Faker::String.random(4),
+    nav_title:        Faker::String.random(4),
+    slug:             Faker::String.random(4),
+    body:             Faker::String.random(4),
+    external_link:    Faker::String.random(4),
+    show_in_header:   true,
+    hidden:           true,
+    is_external_link: true,
+    open_in_new_tab:  true,
+    purpose:          "supplemental_content",
+    creator:          creator
   } }
 
   it "can create a page" do
@@ -33,14 +24,14 @@ RSpec.describe Updaters::V2::Pages, updaters_v2: true do
     end
   end
 
-  # context "when updating an existing model" do
-  #   let!(:model) { FactoryBot.create :page }
-  #
-  #   it "can update the page" do
-  #     perform_within_expectation! do |e|
-  #       e.to keep_the_same(Page, :count)
-  #     end
-  #   end
+  context "when updating an existing model" do
+    let!(:model) { FactoryBot.create :page }
 
-  # end
+    it "can update the page" do
+      perform_within_expectation! do |e|
+        e.to keep_the_same(Page, :count)
+      end
+    end
+
+  end
 end
