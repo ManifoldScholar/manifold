abf75ba3fbf29ed68f7e320ccf94cb54d43525ab
Connor Christensen <connor@castironcoding.com>
2019-05-14 15:54:32 -0700

    [C] Create files

---
+++ api/app/services/updaters/v2/settings.rb
@@ -0,0 +1,70 @@
+# Oh man I need help on this one
+
+# these are the attributes:
+# -------------------------
+# {
+#   general: [
+#     :installation_name,
+#     :default_publisher,
+#     :default_publisher_place,
+#     :head_description,
+#     :social_share_message,
+#     :contact_email,
+#     :copyright,
+#     :press_site,
+#     :terms_url,
+#     :head_title,
+#     :twitter,
+#     :facebook
+#   ]
+# },
+# {
+#   secrets: [
+#     :facebook_app_secret,
+#     :twitter_app_secret,
+#     :twitter_access_token_secret,
+#     :google_private_key,
+#     :smtp_settings_password
+#   ]
+# },
+# {
+#   integrations: [
+#     :facebook_app_id,
+#     :twitter_app_id,
+#     :twitter_access_token,
+#     :google_project_id,
+#     :google_private_key_id,
+#     :google_client_email,
+#     :google_client_id,
+#     :ga_tracking_id,
+#     :ga_profile_id
+#   ]
+# },
+# {
+#   email: [
+#     :from_address,
+#     :from_name,
+#     :reply_to_address,
+#     :reply_to_name,
+#     :closing,
+#     :delivery_method,
+#     :smtp_settings_address,
+#     :smtp_settings_port,
+#     :smtp_settings_user_name,
+#     :sendmail_settings_location,
+#     :sendmail_settings_arguments
+#   ]
+# },
+# {
+#   theme: [
+#     :logo_styles,
+#     :typekit_id,
+#     :header_offset
+#   ]
+# },
+# :remove_press_logo, attachment(:press_logo),
+# :remove_press_logo_footer, attachment(:press_logo_footer),
+# :remove_press_logo_mobile, attachment(:press_logo_mobile),
+# :remove_favicon, attachment(:favicon),
+# google_service: [:data]
+# ]
