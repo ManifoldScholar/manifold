d3b60225b0e21780470f4b64de66a5a198f9f67b
Zach Davis <zach@castironcoding.com>
2019-05-30 11:58:19 -0700

    [C] Schedule collection cache invalidation
    Fixes #2217

--- api/clock.rb
+++ api/clock.rb
@@ -5,16 +5,25 @@ require "./config/environment"
 # This file contains configuration for our background jobs run via Clockwork.
 module Clockwork
   every(1.hour, "queue_fetch_project_tweets") { ::QueueFetchProjectTweets.perform_later }
+
+  every(15.minutes, "refresh_project_collections") do
+    ::ProjectCollectionJobs::QueueCacheCollectionProjectsJob.perform_later
+  end
+
   every(4.hours, "update_statistics") { ::UpdateAnalyticsCache.perform_later }
+
   every(1.day, "expire_shrine_cache", at: "22:00", tz: "America/Los_Angeles") do
     ExpireShrineCacheJob.perform_later
   end
+
   every(1.day, "expire_tus_uploads", at: "23:00", tz: "America/Los_Angeles") do
     ExpireTusUploadsJob.perform_later
   end
+
   every(1.day, "enqueue_user_daily_digests", at: "06:00") do
     Notifications::EnqueueDigestsJob.perform_later "daily"
   end
+
   every(1.week, "enqueue_user_weekly_digests", at: "Sunday 06:00") do
     Notifications::EnqueueDigestsJob.perform_later "weekly"
   end
