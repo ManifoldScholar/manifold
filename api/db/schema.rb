e9d39665189fe6d84d25689d75148edaf9c5fa9d
Zach Davis <zach@castironcoding.com>
2019-06-17 05:59:10 -0700

    [C] Apply minor schema update

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190506192655) do
+ActiveRecord::Schema.define(version: 2019_05_06_192655) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
-  enable_extension "pg_trgm"
-  enable_extension "citext"
 
   create_table "action_callouts", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
     t.string "title"
