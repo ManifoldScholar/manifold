f02caf1a673c0e474ef783b3d8b9405676d1a83e
Dana Johnson <dana@castironcoding.com>
2019-07-26 10:53:47 -0700

    [B] Improve a11y of ColorPicker & KindPicker
    * Groups ColorPicker radios for improved SR & keyboard interaction
    * Refactors KindPicker to use radio controls rather than buttons
    * Updates markup and CSS of both components to use BEM conventions
    
    Resolves #2292

--- client/src/backend/components/project/form/ColorPicker.js
+++ client/src/backend/components/project/form/ColorPicker.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
+import { UID } from "react-uid";
 import IconComposer from "global/components/utility/IconComposer";
 
 export default class ColorPicker extends Component {
@@ -12,87 +13,101 @@ export default class ColorPicker extends Component {
     getModelValue: PropTypes.func
   };
 
+  get idForPrefix() {
+    return "color-picker";
+  }
+
   renderColorPalette() {
     const avatarColors = [
       {
         value: "primary",
         label: "Primary Background Color",
-        className: "primary"
+        className: "color-picker__item--primary"
       },
       {
         value: "secondary",
         label: "Secondary Background Color",
-        className: "secondary"
+        className: "color-picker__item--secondary"
       },
       {
         value: "tertiary",
         label: "Tertiary Background Color",
-        className: "tertiary"
+        className: "color-picker__item--tertiary"
       },
       {
         value: "quaternary",
         label: "Quaternary Background Color",
-        className: "quaternary"
+        className: "color-picker__item--quaternary"
       },
       {
         value: "quinary",
         label: "Quinary Background Color",
-        className: "quinary"
+        className: "color-picker__item--quinary"
       },
       {
         value: "sentary",
         label: "Sentary Background Color",
-        className: "sentary"
+        className: "color-picker__item--sentary"
       }
     ];
 
     return (
-      <div className="wrapper">
-        {avatarColors.map(color => {
-          const checked =
-            this.props.getModelValue("attributes[avatarColor]") === color.value;
-          const colorClass = color.className ? color.className : "";
-          const inputClassNames = classNames(
-            "form-toggle",
-            "radio",
-            colorClass,
-            { checked }
-          );
-          return (
-            <label
-              htmlFor={color.value}
-              className={inputClassNames}
-              key={color.value}
-            >
-              <span className="screen-reader-text">{color.label}</span>
-              <input
-                type="radio"
-                value={color.value}
-                id={color.value}
-                checked={checked}
-                onChange={() => this.props.onChange(color)}
-              />
-              <span className="toggle-indicator" aria-hidden="true">
-                {checked && (
-                  <IconComposer
-                    icon="checkmark16"
-                    size={18.5}
-                    iconClass="toggle-indicator__icon"
+      <UID name={id => `${this.idForPrefix}-${id}`}>
+        {id => (
+          <div
+            className="color-picker__list"
+            role="group"
+            aria-label="Background Color Picker"
+          >
+            {avatarColors.map(color => {
+              const checked =
+                this.props.getModelValue("attributes[avatarColor]") ===
+                color.value;
+              const colorClass = color.className ? color.className : "";
+              const inputClassNames = classNames(
+                "color-picker__item",
+                "radio",
+                colorClass,
+                { checked }
+              );
+              return (
+                <label
+                  htmlFor={`${id}-${color.value}`}
+                  className={inputClassNames}
+                  key={color.value}
+                >
+                  <span className="screen-reader-text">{color.label}</span>
+                  <input
+                    type="radio"
+                    value={color.value}
+                    id={`${id}-${color.value}`}
+                    checked={checked}
+                    onChange={() => this.props.onChange(color)}
+                    className="color-picker__input"
                   />
-                )}
-              </span>
-            </label>
-          );
-        })}
-      </div>
+                  <span className="color-picker__indicator" aria-hidden="true">
+                    {checked && (
+                      <IconComposer
+                        icon="checkmark16"
+                        size={18.5}
+                        iconClass="color-picker__indicator-icon"
+                      />
+                    )}
+                  </span>
+                </label>
+              );
+            })}
+          </div>
+        )}
+      </UID>
     );
   }
 
   render() {
     return (
       <div className="color-picker">
-        <div className="colors">{this.renderColorPalette()}</div>
-        <div className="default-description" aria-hidden="true">
+        <div className="color-picker__inner">{this.renderColorPalette()}</div>
+        <div className="color-picker__description" aria-hidden="true">
           Select A Different Background Color
         </div>
       </div>
