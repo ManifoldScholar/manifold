3a67ec894b1acf29e3e480df5a754fa5d7f303a9
Zach Davis <zach@castironcoding.com>
2019-07-26 07:51:46 -0700

    [C] Add integration story wrapper

--- client/.storybook/wrappers/Integration.js
+++ client/.storybook/wrappers/Integration.js
@@ -2,15 +2,9 @@ import React, { PureComponent } from "react";
 
 export default class StorybookFrontendWrapper extends PureComponent {
   render() {
-    const styles = {
-      padding: 25
-    };
+    const styles = {};
 
-    const innerStyles = {
-      border: "1px solid #424242",
-      borderStyle: "dashed",
-      position: "relative"
-    };
+    const innerStyles = {};
 
     return (
       <div className="frontend" style={styles}>
