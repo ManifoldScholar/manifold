60b3818741fb6435eab275b6a9abeed33433c095
Zach Davis <zach@castironcoding.com>
2019-09-10 08:47:55 -0700

    [B] Remove deprecated count fields

--- api/spec/factories/reading_group.rb
+++ api/spec/factories/reading_group.rb
@@ -1,8 +1,6 @@
 FactoryBot.define do
   factory :reading_group do
     name { "A Reading Group" }
-    memberships_count { 0 }
-    annotations_count { 0 }
     association :creator, factory: :user
   end
 end
