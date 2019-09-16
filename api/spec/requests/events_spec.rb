89411e98618c6da6c1ac309ed54b239743577571
Connor Christensen <connor@castironcoding.com>
2019-09-16 15:45:14 -0700

    [F] Add swagger documentation for Events resources

--- api/spec/requests/events_spec.rb
+++ api/spec/requests/events_spec.rb
@@ -1,4 +1,4 @@
-require "rails_helper"
+require "swagger_helper"
 
 RSpec.describe "Events API", type: :request do
 
@@ -7,29 +7,24 @@ RSpec.describe "Events API", type: :request do
 
   let(:event) { FactoryBot.create(:event) }
 
-  describe "destroys an event" do
+  path '/events/{id}' do
+    delete I18n.t('swagger.delete.description', type: 'event', attribute: 'ID') do
+      security [ apiKey: [] ]
 
-    let(:path) { api_v1_event_path(event) }
+      parameter name: :id, :in => :path, :type => :string
+      let(:id) { event[:id] }
 
-    context "when the user is an admin" do
+      tags 'Events'
 
-      let(:headers) { admin_headers }
-
-      it "has a 204 NO CONTENT status code" do
-        delete path, headers: headers
-        expect(response).to have_http_status(204)
+      response '204', I18n.t('swagger.delete.204', type: 'event', attribute: 'ID') do
+        let(:Authorization) { admin_auth }
+        run_test!
       end
-    end
 
-    context "when the user is a reader" do
-
-      let(:headers) { reader_headers }
-
-      it "has a 403 FORBIDDEN status code" do
-        delete path, headers: headers
-        expect(response).to have_http_status(403)
+      response '403', I18n.t('swagger.access_denied', type: 'event', attribute: 'ID') do
+        let(:Authorization) { reader_auth }
+        run_test!
       end
     end
   end
 end
-
