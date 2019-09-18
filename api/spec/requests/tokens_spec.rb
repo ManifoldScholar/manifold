ccfef647461a96f874eccde28773df784b6fa473
Connor Christensen <connor@castironcoding.com>
2019-09-18 15:51:07 -0700

    [F] Add swagger documentation for Tokens resources

--- api/spec/requests/tokens_spec.rb
+++ api/spec/requests/tokens_spec.rb
@@ -1,12 +1,35 @@
-require "rails_helper"
+require "swagger_helper"
 
 RSpec.describe "Tokens API", type: :request do
 
   include_context("authenticated request")
   include_context("param helpers")
 
-  describe "creates a token" do
+  path '/tokens' do
+    let(:created_user) { FactoryBot.create(:user, role: Role::ROLE_ADMIN)}
+    post 'Logs in a user' do
+      produces 'application/json'
+      parameter name: :email, :in => :query, type: :string
+      parameter name: :password, :in => :query, type: :string
+      tags 'tokens'
 
+      response '200', 'user logged in' do
+        let(:email) { created_user.email }
+        let(:password) { created_user.password }
+        schema '$ref' => '#/definitions/MeResponse'
+        run_test!
+      end
+
+      response '401', 'user credentials are incorrect' do
+        let(:email) { created_user.email }
+        let(:password) { '' }
+        schema '$ref' => '#/definitions/TokenErrors'
+        run_test!
+      end
+    end
+  end
+
+  describe "when getting a token" do
     let(:path) { api_v1_tokens_path }
     let(:params) { { email: reader.email, password: password } }
     let(:invalid_params) { { email: reader.email, password: "boogers" } }
@@ -15,10 +38,6 @@ RSpec.describe "Tokens API", type: :request do
 
     describe "the response" do
       before(:each) { post path, params: params }
-      it "has a 200 status code" do
-        expect(response).to have_http_status(200)
-      end
-
       it "returns the token as part of the response meta" do
         token = api_response["meta"]["authToken"]
         expect(token.blank?).to be false
@@ -29,15 +48,6 @@ RSpec.describe "Tokens API", type: :request do
         id = api_response["data"]["id"]
         expect(type).to eq ("users")
         expect(id).to eq(reader.id)
-      end
-    end
-
-    context "when the username or password are incorrect" do
-      before(:each) { post path, params: invalid_params }
-      describe "the response" do
-        it "has a 401 status code" do
-          expect(response).to have_http_status(401)
-        end
       end
     end
 
