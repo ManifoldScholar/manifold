99b2a386aae5aafe3c47349fbccedfabbeac3b21
Zach Davis <zach@castironcoding.com>
2019-09-23 09:35:19 -0700

    [C] Simplify with-reading-groups HOC

--- client/src/hoc/with-reading-groups/index.js
+++ client/src/hoc/with-reading-groups/index.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { meAPI, requests } from "api";
 import { select, loaded } from "utils/entityUtils";
 import hoistStatics from "hoist-non-react-statics";
-import connectAndFetch from "utils/connectAndFetch";
+import { connect } from "react-redux";
 import { entityStoreActions, uiReadingGroupActions } from "actions";
 
 const { request } = entityStoreActions;
@@ -18,21 +18,6 @@ export default function withReadingGroups(WrappedComponent) {
   )})`;
 
   class WithReadingGroups extends React.PureComponent {
-    static fetchData = (getState, dispatch) => {
-      const promises = [];
-      const state = getState();
-      if (!loaded(requests.feMyReadingGroups, state.entityStore)) {
-        const readingGroupsFetch = meAPI.readingGroups();
-        const readingGroupsAction = request(
-          readingGroupsFetch,
-          requests.feMyReadingGroups
-        );
-        const { promise } = dispatch(readingGroupsAction);
-        promises.push(promise);
-      }
-      return Promise.all(promises);
-    };
-
     static mapStateToProps = state => {
       return {
         readingGroups: select(requests.feMyReadingGroups, state.entityStore),
@@ -54,6 +39,12 @@ export default function withReadingGroups(WrappedComponent) {
       currentReadingGroup: PropTypes.string
     };
 
+    componentDidMount() {
+      if (!this.props.readingGroupsLoaded) {
+        this.fetchReadingGroups();
+      }
+    }
+
     componentDidUpdate(prevProps) {
       // When the groups are loaded, we need to validate that the selected group is still
       // available to the user.
@@ -68,6 +59,15 @@ export default function withReadingGroups(WrappedComponent) {
         if (!currentGroup)
           this.props.dispatch(uiReadingGroupActions.setReadingGroup("public"));
       }
+    }
+
+    fetchReadingGroups() {
+      const readingGroupsFetch = meAPI.readingGroups();
+      const readingGroupsAction = request(
+        readingGroupsFetch,
+        requests.feMyReadingGroups
+      );
+      this.props.dispatch(readingGroupsAction);
     }
 
     get childProps() {
@@ -87,7 +87,9 @@ export default function withReadingGroups(WrappedComponent) {
     }
   }
 
-  const ConnectedWithReadingGroups = connectAndFetch(WithReadingGroups);
+  const ConnectedWithReadingGroups = connect(WithReadingGroups.mapStateToProps)(
+    WithReadingGroups
+  );
 
   return hoistStatics(ConnectedWithReadingGroups, WrappedComponent);
 }
