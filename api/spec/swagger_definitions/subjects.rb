a1a6fff3192530309d7aee2a266dad506dd8d3a7
Connor Christensen <connor@castironcoding.com>
2019-10-16 14:59:25 -0700

    [C] Update subjects swagger_definitions

--- api/spec/swagger_definitions/subjects.rb
+++ api/spec/swagger_definitions/subjects.rb
@@ -2,26 +2,26 @@ require_relative 'base_types'
 
 module Subjects
   class << self
+
+    def get_resource_attributes
+      { name: Type.string }
+    end
+
     def create_request
-      Type.request(
-        Type.object({
-          name: Type.string
-        })
-      )
+      Type.request( get_resource_attributes )
     end
 
     def get_resource
-      Type.object(
-        Type.data_response_hash(
-          Type.object({
-            name: Type.string
-          })
-        )
-      )
+      Type.resource_response({
+        attributes: get_resource_attributes
+      })
     end
 
     def get_collection
-      Type.paginated( get_resource )
+      Type.collection_response({
+        attributes: get_resource_attributes,
+        paginated: true
+      })
     end
   end
 end
