ManifoldScholar/manifold

commit ab61beb2e2d452f53d4944262a5ff976ef75facd

[F] Implement reading groups
Co-authored-by: Dana Johnson <dana@castironcoding.com>
Co-authored-by: Zach Davis <zach@castironcoding.com>
Co-authored-by: Max Ono <max@castironcoding.com>
Co-authored-by: Molly Jean Bennett <molly@castironcoding.com>

client/src/global/components/form/Switch.js
@@ -1,0 +1,183 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+import { UID } from "react-uid";
+import setter from "./setter";
+import Instructions from "./Instructions";
+import IconComposer from "global/components/utility/IconComposer";
+
+class FormSwitch extends Component {
+  static displayName = "Form.Switch";
+
+  static propTypes = {
+    label: PropTypes.string,
+    labelPos: PropTypes.string,
+    className: PropTypes.string,
+    labelClass: PropTypes.string,
+    instructions: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    set: PropTypes.func,
+    value: PropTypes.any,
+    customValues: PropTypes.shape({
+      true: PropTypes.string,
+      false: PropTypes.string
+    }),
+    focusOnMount: PropTypes.bool,
+    wide: PropTypes.bool,
+    theme: PropTypes.oneOf(["default", "checkbox"])
+  };
+
+  static defaultProps = {
+    labelPos: "above",
+    theme: "default"
+  };
+
+  componentDidMount() {
+    if (this.props.focusOnMount && this.checkbox) {
+      this.checkbox.focus();
+    }
+  }
+
+  get labelClasses() {
+    return classnames(
+      "form-input-heading",
+      "toggle",
+      this.props.labelPos,
+      this.props.labelClass
+    );
+  }
+
+  get themeClasses() {
+    return classnames({
+      "form-switch": this.showSwitch,
+      "checkbox checkbox--gray": this.showCheckbox,
+      checked: this.checked
+    });
+  }
+
+  get wrapperClasses() {
+    return classnames(
+      {
+        "form-input": true,
+        wide: this.props.wide
+      },
+      this.props.className
+    );
+  }
+
+  get switchClasses() {
+    return classnames({
+      "boolean-primary": this.showSwitch,
+      checked: this.checked
+    });
+  }
+
+  get instructionClasses() {
+    return classnames({
+      "instructions--inline": this.showCheckbox
+    });
+  }
+
+  get checked() {
+    return this.determineChecked(this.props.value);
+  }
+
+  get idPrefix() {
+    return "switch-input";
+  }
+
+  get idForInstructionsPrefix() {
+    return "switch-input-instructions";
+  }
+
+  get showSwitch() {
+    return this.props.theme === "default";
+  }
+
+  get showCheckbox() {
+    return this.props.theme === "checkbox";
+  }
+
+  truthy(value) {
+    return value === true || value === "true";
+  }
+
+  handleChange = () => {
+    if (this.props.customValues) return this.handleCustomValues();
+    return this.handleBooleans();
+  };
+
+  handleCustomValues() {
+    const trueValue = this.props.customValues.true;
+    const falseValue = this.props.customValues.false;
+    if (this.props.value === trueValue) return this.props.set(falseValue);
+    return this.props.set(trueValue);
+  }
+
+  handleBooleans() {
+    this.props.set(!this.truthy(this.props.value));
+  }
+
+  determineChecked(value) {
+    if (this.props.customValues) return value === this.props.customValues.true;
+    return this.truthy(value);
+  }
+
+  renderSwitchIndicator() {
+    return (
+      <span className="toggle-indicator">
+        <span className={this.switchClasses} aria-hidden="true" />
+      </span>
+    );
+  }
+
+  renderCheckboxIndicator() {
+    return (
+      <span className="checkbox__indicator" aria-hidden="true">
+        <IconComposer
+          icon="checkmark16"
+          size="default"
+          iconClass="checkbox__icon"
+        />
+      </span>
+    );
+  }
+
+  renderLabelText() {
+    return <span className={this.labelClasses}>{this.props.label}</span>;
+  }
+
+  render() {
+    return (
+      <UID>
+        {id => (
+          <div className={this.wrapperClasses}>
+            <label
+              htmlFor={`${this.idPrefix}-${id}`}
+              className={this.themeClasses}
+            >
+              {this.props.labelPos === "above" && this.renderLabelText()}
+              <input
+                ref={c => {
+                  this.checkbox = c;
+                }}
+                type="checkbox"
+                id={`${this.idPrefix}-${id}`}
+                checked={this.checked}
+                onChange={eventIgnored => this.handleChange()}
+              />
+              {this.props.labelPos === "below" && this.renderLabelText()}
+              {this.showCheckbox && this.renderCheckboxIndicator()}
+              {this.showSwitch && this.renderSwitchIndicator()}
+              <Instructions
+                instructions={this.props.instructions}
+                className={this.instructionClasses}
+              />
+            </label>
+          </div>
+        )}
+      </UID>
+    );
+  }
+}
+
+export default setter(FormSwitch);

client/src/backend/components/form/Switch.js
@@ -1,183 +1,0 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import classnames from "classnames";
-import { UID } from "react-uid";
-import setter from "./setter";
-import Instructions from "./Instructions";
-
-class FormSwitch extends Component {
-  static displayName = "Form.Switch";
-
-  static propTypes = {
-    label: PropTypes.string,
-    labelPos: PropTypes.string,
-    className: PropTypes.string,
-    labelClass: PropTypes.string,
-    instructions: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-    set: PropTypes.func,
-    value: PropTypes.any,
-    customValues: PropTypes.shape({
-      true: PropTypes.string,
-      false: PropTypes.string
-    }),
-    focusOnMount: PropTypes.bool,
-    wide: PropTypes.bool
-  };
-
-  static defaultProps = {
-    labelPos: "above",
-    focusOnMount: false
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = { focused: false };
-  }
-
-  componentDidMount() {
-    if (this.props.focusOnMount) {
-      this.focus();
-    }
-
-    window.addEventListener("keydown", event => {
-      this.handleKeyPress(event);
-    });
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleKeyPress);
-  }
-
-  get labelClasses() {
-    return classnames(
-      "form-input-heading",
-      "toggle",
-      this.props.labelPos,
-      this.props.labelClass
-    );
-  }
-
-  get switchClasses() {
-    return classnames({
-      "boolean-primary": true,
-      checked: this.checked
-    });
-  }
-
-  get wrapperClasses() {
-    return classnames(
-      {
-        "form-input": true,
-        wide: this.props.wide
-      },
-      this.props.className
-    );
-  }
-
-  get checked() {
-    return this.determineChecked(this.props.value);
-  }
-
-  get idPrefix() {
-    return "switch-input";
-  }
-
-  get idForInstructionsPrefix() {
-    return "switch-input-instructions";
-  }
-
-  truthy(value) {
-    return value === true || value === "true";
-  }
-
-  handleClick = event => {
-    event.preventDefault();
-    if (this.props.customValues) return this.handleCustomValues();
-    return this.handleBooleans();
-  };
-
-  handleKeyPress = event => {
-    const spaceOrEnter = event.keyCode === 32 || event.keyCode === 13;
-    if (spaceOrEnter && this.state.focused) {
-      event.preventDefault();
-      event.stopPropagation();
-      if (this.props.customValues) return this.handleCustomValues();
-      return this.handleBooleans();
-    }
-  };
-
-  handleCustomValues() {
-    const trueValue = this.props.customValues.true;
-    const falseValue = this.props.customValues.false;
-    if (this.props.value === trueValue) return this.props.set(falseValue);
-    return this.props.set(trueValue);
-  }
-
-  handleBooleans() {
-    this.props.set(!this.truthy(this.props.value));
-  }
-
-  determineChecked(value) {
-    if (this.props.customValues) return value === this.props.customValues.true;
-    return this.truthy(value);
-  }
-
-  focus = () => {
-    this.setState({ focused: true });
-    if (this.button) {
-      this.button.focus();
-    }
-  };
-
-  blur = () => {
-    this.setState({ focused: false });
-  };
-
-  renderLabel(id) {
-    return (
-      <label className={this.labelClasses} htmlFor={id}>
-        {this.props.label}
-      </label>
-    );
-  }
-
-  render() {
-    return (
-      <UID>
-        {id => (
-          <div className={this.wrapperClasses}>
-            {this.props.labelPos === "above" &&
-              this.renderLabel(`${this.idPrefix}-${id}`)}
-            <div className="toggle-indicator">
-              {/* Add .checked to .boolean-primary to change visual state */}
-              <div
-                ref={b => {
-                  this.button = b;
-                }}
-                onFocus={this.focus}
-                onBlur={this.blur}
-                onClick={this.handleClick}
-                className={this.switchClasses}
-                role="button"
-                tabIndex="0"
-                aria-pressed={this.checked}
-                id={`${this.idPrefix}-${id}`}
-                aria-describedby={`${this.idForInstructionsPrefix}-${id}`}
-              >
-                <span className="screen-reader-text">{this.props.label}</span>
-              </div>
-            </div>
-            {this.props.labelPos === "below" &&
-              this.renderLabel(`${this.idPrefix}-${id}`)}
-            <Instructions
-              instructions={this.props.instructions}
-              id={`${this.idForInstructionsPrefix}-${id}`}
-            />
-          </div>
-        )}
-      </UID>
-    );
-  }
-}
-
-export default setter(FormSwitch);

commit 951a995a6f2e81111c1389be79d4a41b3322d445

[B] Fix regression in Form.Switch layout
This fixes an issue introduced in ab61beb that caused the label text in
Form.Switch to render above the input even when `props.labelPos` was set
to `below`.

Resolves #2378

client/src/global/components/form/Switch.js
@@ -165,9 +165,9 @@ class FormSwitch extends Component {
                 checked={this.checked}
                 onChange={eventIgnored => this.handleChange()}
               />
-              {this.props.labelPos === "below" && this.renderLabelText()}
               {this.showCheckbox && this.renderCheckboxIndicator()}
               {this.showSwitch && this.renderSwitchIndicator()}
+              {this.props.labelPos === "below" && this.renderLabelText()}
               <Instructions
                 instructions={this.props.instructions}
                 className={this.instructionClasses}