ba32095d69831f671d30549f7247645c7a33f328
Zach Davis <zach@castironcoding.com>
2019-10-23 08:46:49 -0700

    [F] Document Comments API endpoints

--- api/app/serializers/v1/comment_serializer.rb
+++ api/app/serializers/v1/comment_serializer.rb
@@ -4,21 +4,19 @@ module V1
     include ::V1::Concerns::ManifoldSerializer
 
     abilities
-    typed_attribute :body, NilClass
-    typed_attribute :parent_id, NilClass
-    typed_attribute :created_at, NilClass
-    typed_attribute :flags_count, NilClass
-    typed_attribute :flagged, NilClass
-    typed_attribute :deleted, NilClass
-    typed_attribute :children_count, NilClass
-    typed_attribute :sort_order, NilClass
-    typed_attribute :author_created, NilClass
-    typed_attribute :flagged, NilClass do |object, params|
+    typed_attribute :parent_id, Types::Serializer::ID.optional
+    typed_attribute :created_at, Types::DateTime
+    typed_attribute :flags_count, Types::Integer
+    typed_attribute :deleted, Types::Bool
+    typed_attribute :children_count, Types::Integer
+    typed_attribute :sort_order, Types::Integer
+    typed_attribute :author_created, Types::Bool
+    typed_attribute :flagged, Types::Bool do |object, params|
       next 0 unless authenticated?(params)
 
       object.flagged_by?(params[:current_user])
     end
-    typed_attribute :body, NilClass do |object, params|
+    typed_attribute :body, Types::String do |object, params|
       object.deleted == true ? deleted_body(object, params) : object.body
     end
 
