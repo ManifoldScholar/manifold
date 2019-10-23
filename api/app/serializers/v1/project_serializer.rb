704c70b9a6bc2f5e5b8de93dab569ab3f0902aa1
Zach Davis <zach@castironcoding.com>
2019-10-23 08:47:11 -0700

    [F] Document Projects API endpoints

--- api/app/serializers/v1/project_serializer.rb
+++ api/app/serializers/v1/project_serializer.rb
@@ -5,65 +5,62 @@ module V1
 
     abilities
 
-    typed_attribute :title, String
-    typed_attribute :subtitle, String
-    typed_attribute :subtitle_formatted, String
-    typed_attribute :subtitle_plaintext, String
-    typed_attribute :title_formatted, String
-    typed_attribute :title_plaintext, String
-    typed_attribute :publication_date, DateTime
-    typed_attribute :created_at, DateTime
-    typed_attribute :updated_at, DateTime
-    typed_attribute :slug, String
-    typed_attribute :avatar_color, String
-    typed_attribute :avatar_meta, Hash
-    typed_attribute :draft, String
-    typed_attribute :finished, String
-    typed_attribute :creator_names, String
-    typed_attribute :recently_updated, String, &:recently_updated?
-    typed_attribute :updated, String, &:updated?
-    typed_attribute :avatar_styles, Hash
+    typed_attribute :title, Types::String # required
+    typed_attribute :subtitle, Types::String.optional
+    typed_attribute :subtitle_formatted, Types::String
+    typed_attribute :subtitle_plaintext, Types::String
+    typed_attribute :title_formatted, Types::String
+    typed_attribute :title_plaintext, Types::String
+    typed_attribute :publication_date, Types::DateTime.optional
+    typed_attribute :created_at, Types::DateTime
+    typed_attribute :updated_at, Types::DateTime
+    typed_attribute :slug, Types::String
+    typed_attribute :avatar_color, Types::String.enum("primary", "secondary", "tertiary", "quaternary", "quinary", "sentary")
+    typed_attribute :avatar_meta, Types::Hash
+    typed_attribute :draft, Types::Bool
+    typed_attribute :finished, Types::Bool
+    typed_attribute :creator_names, Types::String
+    typed_attribute :recently_updated, Types::Bool, &:recently_updated?
+    typed_attribute :updated, Types::Bool, &:updated?
+    typed_attribute :avatar_styles, Types::Serializer::Attachment
 
     typed_has_many :creators, serializer: ::V1::MakerSerializer, record_type: :maker
 
     # rubocop:disable Metrics/BlockLength
     when_full do
       metadata(metadata: true, properties: true, formatted: true)
-      typed_attribute :hero_styles, Hash
-      typed_attribute :cover_styles, Hash
-      typed_attribute :hashtag, String
-      typed_attribute :description, String
-      typed_attribute :featured, NilClass
-      typed_attribute :purchase_url, String
-      typed_attribute :purchase_price_money, Float
-      typed_attribute :purchase_price_currency, String
-      typed_attribute :purchase_price, Float
-      typed_attribute :purchase_call_to_action, String
-      typed_attribute :twitter_id, String
-      typed_attribute :instagram_id, String
-      typed_attribute :facebook_id, String
-      typed_attribute :description_formatted, String
-      typed_attribute :resource_kinds, String
-      typed_attribute :dark_mode, String
-      typed_attribute :image_credits, String
-      typed_attribute :image_credits_formatted, String
-      typed_attribute :tag_list, String
-      typed_attribute :resource_collections_count, Integer
-      typed_attribute :resources_count, Integer
-      typed_attribute :citations, Hash
-      typed_attribute :hide_activity, String
-      typed_attribute :standalone_mode, String
-      typed_attribute :download_url, String
-      typed_attribute :download_call_to_action, String
-      typed_attribute :pending_slug, String
-      typed_attribute :standalone_mode, String
-      typed_attribute :standalone_mode_press_bar_text, String
-      typed_attribute :standalone_mode_press_bar_url, String
-      typed_attribute :event_count, Integer, &:filtered_event_count
-      typed_attribute :event_types, Array, &:uniq_event_types
-      typed_attribute :resource_tags, Array, &:sorted_resource_tags
-      typed_attribute :hero_styles, Hash
-      typed_attribute :cover_styles, Hash
+      typed_attribute :hero_styles, Types::Serializer::Attachment
+      typed_attribute :cover_styles, Types::Serializer::Attachment
+      typed_attribute :hashtag, Types::String.optional
+      typed_attribute :description, Types::String.optional
+      typed_attribute :featured, Types::Bool
+      typed_attribute :purchase_url, Types::Serializer::URL.optional
+      typed_attribute :purchase_price_money, Types::String.meta(example: "$0.00")
+      typed_attribute :purchase_price_currency, Types::String.optional.meta(example: "USD")
+      typed_attribute :purchase_price, Types::Float.optional
+      typed_attribute :purchase_call_to_action, Types::String.optional
+      typed_attribute :twitter_id, Types::String.optional
+      typed_attribute :instagram_id, Types::String.optional
+      typed_attribute :facebook_id, Types::String.optional
+      typed_attribute :description_formatted, Types::String
+      typed_attribute :resource_kinds, Types::Array.of(Types::String)
+      typed_attribute :dark_mode, Types::Bool
+      typed_attribute :image_credits, Types::String.optional
+      typed_attribute :image_credits_formatted, Types::String
+      typed_attribute :tag_list, Types::Array.of(Types::String)
+      typed_attribute :resource_collections_count, Types::Integer
+      typed_attribute :resources_count, Types::Integer
+      typed_attribute :citations, Types::Hash.meta(description: "Citations of the book in a veriety of formats (mla, chicago, apa, etc.)")
+      typed_attribute :hide_activity, Types::Bool
+      typed_attribute :standalone_mode, Types::String.enum("disabled", "enabled", "enforced")
+      typed_attribute :download_url, Types::String.optional
+      typed_attribute :download_call_to_action, Types::String.optional
+      typed_attribute :pending_slug, Types::String
+      typed_attribute :standalone_mode_press_bar_text, Types::String.optional
+      typed_attribute :standalone_mode_press_bar_url, Types::String.optional
+      typed_attribute :event_count, Types::Integer, &:filtered_event_count
+      typed_attribute :event_types, Types::Array.of(Types::String), &:uniq_event_types
+      typed_attribute :resource_tags, Types::Array.of(Types::String), &:sorted_resource_tags
 
       typed_has_many :texts,
                      object_method_name: :text_summaries,
