68ebd95fa72c5ffdeb27fbc38fc3116ca50fee54
Zach Davis <zach@castironcoding.com>
2019-10-23 08:56:16 -0700

    [F] Document Users API endpoints

---
+++ api/spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,23 @@
+require "swagger_helper"
+
+RSpec.describe "User", type: :request do
+  path "/users" do
+    include_examples "an API index request", model: User, auth_type: :admin
+    include_examples "an API create request", model: User
+  end
+  path "/users/{id}" do
+    include_examples "an API show request", model: User, auth_type: :admin
+    include_examples "an API update request", model: User, auth_type: :admin
+    include_examples "an API destroy request", model: User, auth_type: :admin
+  end
+
+  path "/users/whoami" do
+    include_examples "an API show request",
+                     model: User,
+                     auth_type: :admin,
+                     parameters: [],
+                     summary: "Returns the User that is currently logged in",
+                     success_description: "Returns the User that is currently logged in",
+                     exclude: %w(404 403)
+  end
+end
