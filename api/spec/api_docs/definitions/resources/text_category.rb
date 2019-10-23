fdf00e50f8376b7b991094eb469f32ae619c45c2
Zach Davis <zach@castironcoding.com>
2019-10-23 08:47:02 -0700

    [F] Document TextCategories API endpoints

---
+++ api/spec/api_docs/definitions/resources/text_category.rb
@@ -0,0 +1,16 @@
+module ApiDocs
+  module Definitions
+    module Resources
+      class TextCategory
+
+        REQUIRED_CREATE_ATTRIBUTES = [:title].freeze
+
+        class << self
+
+          include Resource
+
+        end
+      end
+    end
+  end
+end
