32e68e5550bd16f9b3fe7dfb7622a563fef17c93
Connor Christensen <connor@castironcoding.com>
2019-10-24 16:15:33 -0700

    [B] Remove two params that are not valid text values

--- api/app/controllers/concerns/validation.rb
+++ api/app/controllers/concerns/validation.rb
@@ -251,8 +251,8 @@ module Validation
 
   def text_params
     params.require(:data)
-    attributes = [:title, :language, :position, :description, :publication_date,
-                  metadata(Text), :rights, :section_kind, :subtitle, :published,
+    attributes = [:title, :position, :description, :publication_date,
+                  metadata(Text), :section_kind, :subtitle, :published,
                   :pending_slug]
     relationships = [:category, :contributors, :creators]
     param_config = structure_params(attributes: attributes, relationships: relationships)
