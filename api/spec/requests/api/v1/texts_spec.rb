34f1d61c03947e6704a586d2c7be227f550ec55f
Connor Christensen <connor@castironcoding.com>
2019-11-15 13:58:39 -0800

    [C] Update included on text spec

--- api/spec/requests/api/v1/texts_spec.rb
+++ api/spec/requests/api/v1/texts_spec.rb
@@ -2,13 +2,12 @@ require "swagger_helper"
 
 RSpec.describe "Text", type: :request do
   path "/texts" do
-    # QUESTION: it seems that only projects are included on this route. Where can I verify this?
-    include_examples "an API index request", model: Text, included: [ :project ], partial: true
+    include_examples "an API index request", model: Text, included: %w(project), partial: true
     # NOTE do not build the create request. This is not a valid route
   end
   path "/texts/{id}" do
-    include_examples "an API show request", model: Text, included: [ :project, :category ]
-    include_examples "an API update request", model: Text, auth_type: :admin, included: [ :maker, :project ]
+    include_examples "an API show request", model: Text, included: %w(project category creators contributors stylesheets)
+    include_examples "an API update request", model: Text, auth_type: :admin, included: %w(project creators contributors)
     include_examples "an API destroy request", model: Text, auth_type: :admin
   end
 end
