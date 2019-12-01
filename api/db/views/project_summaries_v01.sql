e3caa5e66860f327a678d929af07a95674075ef4
Zach Davis <zach@castironcoding.com>
2019-12-01 06:33:03 -0800

    [F] Improve performance of lists with many projects
    Similar to the text refactor, this change introduces the ProjectSummary,
    which is a model backed by a DB view that includes the minimal amount of
    data needed to render a project in a list. This model should always be
    serializable by the project serializer when not in "full" serialization
    mode.
    
    Fixes #2443

---
+++ api/db/views/project_summaries_v01.sql
@@ -0,0 +1,28 @@
+SELECT
+    p.id AS id,
+    p.id AS project_id,
+    p.title,
+    p.cached_title_formatted AS title_formatted,
+    p.cached_title_plaintext AS title_plaintext,
+    p.subtitle,
+    p.cached_subtitle_formatted AS subtitle_formatted,
+    p.cached_subtitle_plaintext AS subtitle_plaintext,
+    p.publication_date,
+    p.created_at,
+    p.updated_at,
+    p.slug,
+    p.avatar_color,
+    p.avatar_data,
+    p.draft,
+    p.finished,
+    pm.creator_names AS creator_names
+  FROM projects p
+  LEFT JOIN LATERAL (
+    SELECT
+      (string_agg(m.cached_full_name, ', ') FILTER (WHERE role = 'creator')) AS creator_names,
+      (string_agg(m.cached_full_name, ', ') FILTER (WHERE role = 'collaborator')) AS collaborator_names
+    FROM collaborators c
+    INNER JOIN makers m ON m.id = c.maker_id
+    WHERE c.collaboratable_type = 'Project' AND c.collaboratable_id = p.id
+  ) pm ON true
+;
