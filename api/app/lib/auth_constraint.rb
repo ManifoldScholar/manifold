9eae17461e2c4fb56be5a267ad17416be4454e18
Zach Davis <zach@castironcoding.com>
2019-12-01 06:27:38 -0800

    [F] Expose Sidekiq and Zhong dashboards
    Fixes #2456

---
+++ api/app/lib/auth_constraint.rb
@@ -0,0 +1,23 @@
+class AuthConstraint
+
+  attr_reader :request
+
+  def initialize(request)
+    @request = request
+  end
+
+  def admin?
+    token = request.cookies["authToken"]
+    return false unless token
+
+    decoded = AuthToken.decode(token)
+    id = decoded["user_id"]
+    return false unless id
+
+    user = User.find(id)
+    return false unless user
+
+    user.admin?
+  end
+
+end
