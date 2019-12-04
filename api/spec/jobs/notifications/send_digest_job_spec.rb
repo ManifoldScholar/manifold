9d7c8bd7753382965a37c9b3c7fd0741c5b5f1c3
Alexa Grey <alexa@castironcoding.com>
2019-12-04 22:30:41 +0000

    [C] Fix digest test

--- api/spec/jobs/notifications/send_digest_job_spec.rb
+++ api/spec/jobs/notifications/send_digest_job_spec.rb
@@ -2,30 +2,38 @@ require "rails_helper"
 
 RSpec.describe Notifications::SendDigestJob, type: :job do
   describe "#perform" do
-    let(:user) { FactoryBot.create(:user) }
-    let(:daily) { NotificationFrequency[:daily] }
-    let(:always) { NotificationFrequency[:always] }
-    let(:daily_notifications) { { digest: daily,  followed_projects: always, digest_comments_and_annotations: always } }
-    let(:project) { FactoryBot.create(:project) }
-
-    before(:each) do
-      user.favorite_projects << project
-      user.save
-      user.update(notification_preferences_by_kind: daily_notifications)
-      Timecop.freeze(Date.today - 1.day) do
-        @a_annotation_event = FactoryBot.create(:event,
-                                                event_type: EventType[:text_annotated],
-                                                subject: FactoryBot.create(:annotation),
-                                                project: project)
+    let!(:user) do
+      FactoryBot.create(:user).tap do |user|
+        user.update_attributes! notification_preferences_by_kind: daily_notifications 
       end
     end
 
-    skip "test failing after adding parallelization. Temporarily disabling." do
-      it "sends a digest email to the user" do
-        described_class.new.perform(user.id, "daily")
-        mail = ActionMailer::Base.deliveries.last
-        expect(mail.to).to eq [user.email]
+    let!(:project) do 
+      FactoryBot.create(:project).tap do |project|
+        user.favorite_projects << project
       end
+    end
+
+    let(:daily_notifications) { { digest: :daily,  followed_projects: :always, digest_comments_and_annotations: :always } }
+
+    let!(:annotation_event) do
+      Timecop.freeze Date.current.yesterday do
+        FactoryBot.create(
+          :event,
+          event_type: EventType[:text_annotated],
+          subject: FactoryBot.create(:annotation),
+          project: project
+        )
+      end
+    end
+
+    it "sends a digest email to the user" do
+      expect(Notifications::SendDigest).to receive(:run).with(frequency: "daily", user: user).and_call_original
+      expect(NotificationMailer).to receive(:digest).with(user, "daily", a_kind_of(Hash)).and_call_original
+
+      expect do
+        described_class.perform_now user.id, "daily"
+      end.to execute_safely
     end
   end
 end
