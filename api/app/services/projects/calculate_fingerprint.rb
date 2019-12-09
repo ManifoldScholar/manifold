cb22c1334061435a426df504028b6b1bdd5ba1ce
Alexa Grey <alexa@castironcoding.com>
2019-12-09 14:45:32 -0800

    [C] Add Project and Resource fingerprint calculators

---
+++ api/app/services/projects/calculate_fingerprint.rb
@@ -0,0 +1,43 @@
+module Projects
+  # Calculate a fingerprint for a {Project}.
+  class CalculateFingerprint < ActiveInteraction::Base
+    include Concerns::FingerprintInteraction
+
+    record :project
+
+    # Presentational and other columns which should not factor into
+    # fingerprint calculation
+    # @api private
+    EXCLUDED_COLUMNS = %w[
+      avatar_color
+      dark_mode
+      download_call_to_action
+      hide_activity
+      sort_title
+
+      fingerprint
+      export_configuration
+    ].freeze
+
+    # @api private
+    COLUMNS = Project.column_names
+      .grep_v(/_(?:id|at|deprecated|count|data|in_cents|currency|call_to_action)\z/)
+      .grep_v(/standalone/)
+      .grep_v(/\Acached/)
+      .without(*EXCLUDED_COLUMNS)
+      .map(&:to_sym)
+      .freeze
+
+    # @api private
+    ATTACHMENTS = Project.column_names.grep(/_data\z/).map do |column|
+      column.sub(/_data\z/, "_original_path").to_sym
+    end.freeze
+
+    def add_details!
+      update_digest_with! project, *COLUMNS, *ATTACHMENTS
+
+      calculate_fingerprints_for! project.texts.published(true)
+      calculate_fingerprints_for! project.resources
+    end
+  end
+end
