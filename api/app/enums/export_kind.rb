9b23b9e67de5ee2ac9a59a06b2315ff10763379f
Alexa Grey <alexa@castironcoding.com>
2019-12-09 15:01:55 -0800

    [C] Make ExportKind validatable

--- api/app/enums/export_kind.rb
+++ api/app/enums/export_kind.rb
@@ -1,14 +1,44 @@
 class ExportKind < ClassyEnum::Base
+  include ActiveModel::Validations
+
   def epub?
     false
+  end
+
+  def known?
+    !unknown?
+  end
+
+  private
+
+  def matches_or_belongs_to?(owner, klass)
+    return true if owner.kind_of?(klass)
+
+    association_name = klass.model_name.i18n_key
+
+    owner.respond_to?(association_name) && owner.public_send(association_name).kind_of?(klass)
+  end
+
+  # @return [void]
+  def owner_must_be!(klass)
+    return unless owner.present?
+
+    errors.add :owner, "must be a type of #{klass}" unless matches_or_belongs_to?(owner, klass)
+  end
+
+  # @return [void]
+  def owner_must_be_text_export!
+    owner_must_be! TextExport
+  end
+end
+
+class ExportKind::EpubV3 < ExportKind
+  validate :owner_must_be_text_export!
+
+  def epub?
+    true
   end
 end
 
 class ExportKind::Unknown < ExportKind
-end
-
-class ExportKind::EpubV3 < ExportKind
-  def epub?
-    true
-  end
 end
