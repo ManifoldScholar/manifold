da94670cc2b04c947faeab3ef80900f23e1cd4ef
Connor Christensen <connor@castironcoding.com>
2019-12-09 17:09:24 -0800

    fixup! [F] Add support for API Documentation specs

--- api/app/services/types/serializer.rb
+++ api/app/services/types/serializer.rb
@@ -1,32 +1,5 @@
 module Types
   module Serializer
-    Abilities = Types::Hash.schema(
-      create: Types::Bool,
-      read: Types::Bool,
-      update: Types::Bool,
-      delete: Types::Bool
-    )
-
-    # each of these are URL's
-    Attachtment = Types::Hash.schema(
-      small: Types::String.optional,
-      small_square: Types::String.optional,
-      small_landscape: Types::String.optional,
-      small_portrait: Types::String.optional,
-      medium: Types::String.optional,
-      medium_square: Types::String.optional,
-      medium_landscape: Types::String.optional,
-      medium_portrait: Types::String.optional,
-      large_landscape: Types::String.optional,
-      original: Types::String.optional
-    )
-
-    Upload = Types::Hash.schema(
-      filename: Types::Bool,
-      data: Types::String,
-      content_type: Types::String
-    )
-
     ID = Types::String.meta(example: "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX")
     URL = Types::String.meta(example: "http://some-website.com")
     Meta = Types::Hash.schema(partial: Types::Bool)
@@ -34,5 +7,31 @@ module Types
     Pointer = Types::Hash.schema(id: ID, type: Types::String)
     Collection = Types::Hash.schema(data: Types::Array.of(Pointer))
     Resource = Types::Hash.schema(data: Pointer)
+
+    Abilities = Types::Hash.schema(
+      create: Types::Bool,
+      read: Types::Bool,
+      update: Types::Bool,
+      delete: Types::Bool
+    )
+
+    Attachtment = Types::Hash.schema(
+      small: URL.optional,
+      small_square: URL.optional,
+      small_landscape: URL.optional,
+      small_portrait: URL.optional,
+      medium: URL.optional,
+      medium_square: URL.optional,
+      medium_landscape: URL.optional,
+      medium_portrait: URL.optional,
+      large_landscape: URL.optional,
+      original: URL.optional
+    )
+
+    Upload = Types::Hash.schema(
+      filename: Types::Bool,
+      data: Types::String,
+      content_type: Types::String
+    )
   end
 end
