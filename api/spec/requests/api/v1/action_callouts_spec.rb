88ee05473d1a02c5889fc11375fa4d63fe6219b4
Connor Christensen <connor@castironcoding.com>
2019-12-11 23:46:54 -0800

    [F] Document ActionCallout API endpoints

---
+++ api/spec/requests/api/v1/action_callouts_spec.rb
@@ -0,0 +1,34 @@
+require "swagger_helper"
+
+RSpec.describe "Action Callouts", type: :request do
+  conditional_requirements = <<~HEREDOC.freeze
+    Requirements:
+    * Requires text relationship if type is read or toc
+    * Requires url if type is link
+  HEREDOC
+
+  path "/action_callouts/{id}" do
+    include_examples "an API show request", model: ActionCallout
+    include_examples "an API update request", model: ActionCallout, auth_type: :admin, description: conditional_requirements
+    include_examples "an API destroy request", model: ActionCallout, auth_type: :admin
+  end
+
+  context "for a project" do
+    let(:parent) { FactoryBot.create(:project) }
+    let(:project_id) { parent.id }
+
+    path "/projects/{project_id}/relationships/action_callouts" do
+      include_examples "an API index request",
+                        model: ActionCallout,
+                        tags: "Projects",
+                        url_parameters: [:project_id]
+
+      include_examples "an API create request",
+                        model: ActionCallout,
+                        auth_type: :admin,
+                        tags: "Projects",
+                        url_parameters: [:project_id],
+                        description: conditional_requirements
+    end
+  end
+end
