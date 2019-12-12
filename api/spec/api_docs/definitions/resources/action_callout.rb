fafb61bf0968362f83966c06a22ef5a77e837f65
Connor Christensen <connor@castironcoding.com>
2019-12-11 23:46:54 -0800

    [F] Document ActionCallout API endpoints

---
+++ api/spec/api_docs/definitions/resources/action_callout.rb
@@ -0,0 +1,31 @@
+module ApiDocs
+  module Definitions
+    module Resources
+      class ActionCallout
+
+        REQUIRED_CREATE_ATTRIBUTES = [
+          :title,
+          :kind,
+          :location
+        ]
+
+        REQUEST_ATTRIBUTES = {
+          # TODO: Make sure attachment works
+          # tested uploading an image and it did not work
+          attachment: Types::Serializer::Upload,
+          remove_attachment: Types::Bool
+        }
+
+        class << self
+
+          include Resource
+
+          def create_attributes
+            request_attributes.except(:remove_attachment)
+          end
+
+        end
+      end
+    end
+  end
+end
