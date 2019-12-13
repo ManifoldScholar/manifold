1f3ae1bf4870cf5dc497f84754824452cd7748a5
Alexa Grey <alexa@castironcoding.com>
2019-12-12 18:05:14 -0800

    [C] Add versions to bagit pipeline container

--- api/app/services/packaging/bag_it_spec/compilation.rb
+++ api/app/services/packaging/bag_it_spec/compilation.rb
@@ -4,6 +4,9 @@ module Packaging
     module Compilation
       # The directory to build archives in.
       TMP_ROOT = Rails.root.join("tmp", "bagit")
+
+      # The compilation pipeline version.
+      VERSION = Gem::Version.new("1.0.0").freeze
     end
   end
 end
