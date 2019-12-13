555d49da3c7b534d5c4c148d01f39bc29ef0b758
Connor Christensen <connor@castironcoding.com>
2019-12-12 17:52:56 -0800

    [F] Gather read_only values from serializer

--- api/spec/api_docs/config.rb
+++ api/spec/api_docs/config.rb
@@ -24,7 +24,7 @@ module ApiDocs
               title: "Manifold",
               version: "v1"
             },
-            host: ENV["DOMAIN"],
+            host: Rails.application.config.manifold.domain,
             basePath: "/api/v1",
             schemes: ["http"],
             paths: {},
