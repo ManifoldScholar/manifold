463c193a3f233a1933e37d377b52cdd690d1579d
Connor Christensen <connor@castironcoding.com>
2019-12-17 14:09:57 -0800

    fixup! [F] Add support for API Documentation specs

--- api/app/services/types/serializer.rb
+++ api/app/services/types/serializer.rb
@@ -3,10 +3,13 @@ module Types
     ID = Types::String.meta(example: "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX")
     URL = Types::String.meta(example: "http://some-website.com")
     Meta = Types::Hash.schema(partial: Types::Bool)
+    Email = Types::String.meta(example: "me@email.com")
 
     Pointer = Types::Hash.schema(id: ID, type: Types::String)
     Collection = Types::Hash.schema(data: Types::Array.of(Pointer))
     Resource = Types::Hash.schema(data: Pointer)
+
+    NOTIFICATION_ENUM = %w[always never daily].freeze
 
     Abilities = Types::Hash.schema(
       create: Types::Bool,
@@ -29,9 +32,9 @@ module Types
     )
 
     Upload = Types::Hash.schema(
-      filename: Types::Bool,
-      data: Types::String,
-      content_type: Types::String
+      filename: Types::String.meta(example: "profile_pic.jpg"),
+      data: Types::String.meta(description: "A base 64 encoded image string"),
+      content_type: Types::String.meta(example: "image/jpeg")
     )
   end
 end
