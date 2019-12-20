dec901ebb20b4a8f1480532db6b39669d4af214a
Connor Christensen <connor@castironcoding.com>
2019-12-19 17:20:39 -0800

    [C] Add metadata to request and response attributes

--- api/spec/api_docs/definitions/resources/project.rb
+++ api/spec/api_docs/definitions/resources/project.rb
@@ -3,8 +3,6 @@ module ApiDocs
     module Resources
       class Project
 
-        # TODO: Accepts metadata(Project)
-
         REQUEST_ATTRIBUTES = {
           avatar: Types::Serializer::Upload,
           hero: Types::Serializer::Upload,
@@ -13,6 +11,29 @@ module ApiDocs
           remove_hero: Types::Bool,
           remove_cover: Types::Bool,
         }.freeze
+
+        METADATA_ATTRIBUTES = {
+          series_title: Types::String,
+          container_title: Types::String,
+          isbn: Types::String,
+          issn: Types::String,
+          doi: Types::String,
+          original_publisher: Types::String,
+          original_publisher_place: Types::String,
+          original_title: Types::String,
+          publisher: Types::String,
+          publisher_place: Types::String,
+          version: Types::String,
+          series_number: Types::String,
+          edition: Types::String,
+          issue: Types::String,
+          volume: Types::String,
+          rights: Types::String,
+          rights_territory: Types::String,
+          restrictions: Types::String,
+          rights_holder: Types::String,
+          resources_doi: Types::String,
+        }
 
         REQUIRED_CREATE_ATTRIBUTES = [:title].freeze
 
