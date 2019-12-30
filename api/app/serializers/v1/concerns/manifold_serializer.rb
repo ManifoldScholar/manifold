a69ad69161cd170773fbddf8d304c3a288df47ad
Zach Davis <zach@castironcoding.com>
2019-12-30 12:34:19 -0800

    [B] Correct partialable check

--- api/app/serializers/v1/concerns/manifold_serializer.rb
+++ api/app/serializers/v1/concerns/manifold_serializer.rb
@@ -92,7 +92,7 @@ module V1
         end
 
         def partialable?
-          @full_register.present?
+          @full_register.present? && @full_register.activated?
         end
 
         def current_register
