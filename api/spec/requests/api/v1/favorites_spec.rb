a1d88f151048804c6e2a35e43ce6803504a66fdb
Connor Christensen <connor@castironcoding.com>
2019-12-31 12:03:18 -0800

    [F] Document Favorite API endpoints

---
+++ api/spec/requests/api/v1/favorites_spec.rb
@@ -0,0 +1,44 @@
+require "swagger_helper"
+
+RSpec.describe "Favorites", type: :request do
+  context "my favorites" do
+    let(:resource) { FactoryBot.create(:favorite, user: admin) }
+
+    path "/me/relationships/favorites" do
+      include_examples "an API index request",
+                       model: Favorite,
+                       authorized_user: :admin
+
+      include_examples "an API create request",
+                       model: Favorite,
+                       authorized_user: :admin,
+                       description: "If the favorite is valid, it will return the current user. "\
+                       "If not, it will return the favorite. If the resource has already been "\
+                       "favorited, the server will respond with a 422 code and a message that "\
+                       "the resource is already taken.",
+                       included_relationships: [:creator] do
+        let(:comment) { FactoryBot.create(:comment) }
+        let(:body) do
+          json_structure(relationships: {
+                           favoritable: {
+                             data: {
+                               id: comment.id,
+                               type: "comment"
+                             }
+                           }
+                         })
+        end
+      end
+    end
+
+    path "/me/relationships/favorites/{id}" do
+      include_examples "an API show request",
+                       model: Favorite,
+                       authorized_user: :admin
+
+      include_examples "an API destroy request",
+                       model: Favorite,
+                       authorized_user: :admin
+    end
+  end
+end
