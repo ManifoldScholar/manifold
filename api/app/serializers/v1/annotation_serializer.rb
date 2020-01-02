b03f477c0c2e38f06526c8fbf2f07edf744af086
Connor Christensen <connor@castironcoding.com>
2020-01-02 00:07:17 -0800

    [F] Document Annotation API endpoints

--- api/app/serializers/v1/annotation_serializer.rb
+++ api/app/serializers/v1/annotation_serializer.rb
@@ -5,51 +5,49 @@ module V1
     current_user_is_creator?
     abilities
 
-    typed_attribute :created_at, NilClass
-    typed_attribute :updated_at, NilClass
-    typed_attribute :start_char, NilClass
-    typed_attribute :start_node, NilClass
-    typed_attribute :end_char, NilClass
-    typed_attribute :end_node, NilClass
-    typed_attribute :id, NilClass
-    typed_attribute :flags_count, NilClass
-    typed_attribute :format, NilClass
-    typed_attribute :subject, NilClass
-    typed_attribute :body, NilClass
-    typed_attribute :private, NilClass
-    typed_attribute :comments_count, NilClass
-    typed_attribute :author_created, NilClass
-    typed_attribute :reading_group_name, NilClass
-    typed_attribute :reading_group_privacy, NilClass
-    typed_attribute :reading_group_id, NilClass
-    typed_attribute :creator_id, NilClass
-    typed_attribute :project_id, NilClass
-    typed_attribute :text_id, NilClass
-    typed_attribute :text_section_id, NilClass
-    typed_attribute :resource_id, NilClass
-    typed_attribute :resource_collection_id, NilClass
-    typed_attribute :text_slug, NilClass
-    typed_attribute :project_title, NilClass
-    typed_attribute :text_title, NilClass
-    typed_attribute :text_title_formatted, NilClass
-    typed_attribute :text_section_title, NilClass
-    typed_attribute :is_anonymous, NilClass
-    typed_attribute :orphaned, NilClass, &:orphaned?
-    typed_attribute :is_anonymous, NilClass do |object, params|
+    typed_attribute :created_at, Types::DateTime.meta(read_only: true)
+    typed_attribute :updated_at, Types::DateTime.meta(read_only: true)
+    typed_attribute :start_char, Types::Integer
+    typed_attribute :start_node, Types::String.meta(example: "start")
+    typed_attribute :end_char, Types::Integer
+    typed_attribute :end_node, Types::String.meta(example: "end")
+    typed_attribute :flags_count, Types::Integer.meta(read_only: true)
+    typed_attribute :format, Types::String.enum("annotation", "highlight", "resource", "resource_collection")
+    typed_attribute :subject, Types::String
+    typed_attribute :body, Types::String.meta(description: "Only required if format is annotation")
+    typed_attribute :private, Types::Bool
+    typed_attribute :comments_count, Types::Integer.meta(read_only: true)
+    typed_attribute :author_created, Types::Bool.meta(read_only: true)
+    typed_attribute :reading_group_name, Types::String.optional.meta(read_only: true)
+    typed_attribute :reading_group_privacy, Types::String.meta(read_only: true).optional
+    typed_attribute :reading_group_id, Types::Serializer::ID.optional
+    typed_attribute :creator_id, Types::Serializer::ID.meta(read_only: true)
+    typed_attribute :project_id, Types::Serializer::ID.meta(read_only: true)
+    typed_attribute :text_id, Types::Serializer::ID.meta(read_only: true)
+    typed_attribute :text_section_id, Types::Serializer::ID.meta(read_only: true)
+    typed_attribute :resource_id, Types::Serializer::ID.optional.meta(read_only: true)
+    typed_attribute :resource_collection_id, Types::Serializer::ID.optional.meta(read_only: true)
+    typed_attribute :text_slug, Types::String.meta(read_only: true)
+    typed_attribute :project_title, Types::String.meta(read_only: true)
+    typed_attribute :text_title, Types::String.meta(read_only: true)
+    typed_attribute :text_title_formatted, Types::String.optional.meta(read_only: true)
+    typed_attribute :text_section_title, Types::String.optional.meta(read_only: true)
+    typed_attribute :orphaned, Types::Bool.meta(read_only: true), &:orphaned?
+    typed_attribute :is_anonymous, Types::Bool.optional.meta(read_only: true) do |object, params|
       anonymous?(object, params)
     end
-    typed_attribute :creator_name, NilClass do |object, params|
+    typed_attribute :creator_name, Types::String.meta(read_only: true) do |object, params|
       next object.creator.full_name if creator_identity_visible?(object, params)
 
       object.anonymous_label
     end
 
-    typed_attribute :creator_avatar_styles, Types::Hash do |object, params|
+    typed_attribute :creator_avatar_styles, Types::Serializer::Attachment.meta(read_only: true) do |object, params|
       creator_identity_visible?(object, params) ? camelize_hash(object.creator_avatar_styles) : {}
     end
 
-    typed_attribute :flagged, NilClass do |object, params|
-      next 0 unless authenticated?(params)
+    typed_attribute :flagged, Types::Bool.meta(read_only: true) do |object, params|
+      next false unless authenticated?(params)
 
       object.flags.where(creator: params[:current_user]).count.positive?
     end
