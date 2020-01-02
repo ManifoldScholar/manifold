ff11d00b6957e1c931a45300e3db5f99bff459ab
Connor Christensen <connor@castironcoding.com>
2020-01-02 00:07:17 -0800

    [F] Document Annotation API endpoints

---
+++ api/spec/api_docs/definitions/resources/annotation.rb
@@ -0,0 +1,28 @@
+module ApiDocs
+  module Definitions
+    module Resources
+      class Annotation
+
+        REQUIRED_CREATE_ATTRIBUTES = [
+          :start_node,
+          :end_node,
+          :start_char,
+          :end_char,
+          :format,
+          :subject,
+          :body
+        ]
+
+        REQUEST_ATTRIBUTES = {
+          section_id: Types::Serializer::ID
+        }
+
+        class << self
+
+          include Resource
+
+        end
+      end
+    end
+  end
+end
