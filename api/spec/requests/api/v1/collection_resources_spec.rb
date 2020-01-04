009cedbabdaea47a5ebc96d8d574e901a5eac652
Connor Christensen <connor@castironcoding.com>
2020-01-03 16:37:37 -0800

    [F] Document CollectionResources API endpoints

---
+++ api/spec/requests/api/v1/collection_resources_spec.rb
@@ -0,0 +1,25 @@
+require "swagger_helper"
+
+RSpec.describe "Collection Resource", type: :request do
+  context "for a resource collection" do
+    let(:resource) { FactoryBot.create(:collection_resource) }
+    let(:resource_collection_id) { resource.resource_collection_id }
+
+    path "/resource_collections/{resource_collection_id}/relationships/collection_resources" do
+      include_examples "an API index request",
+                        model: CollectionResource,
+                        tags: "Resource Collections",
+                        url_parameters: [:resource_collection_id],
+                        included_relationships: [:resource],
+                        paginated: true
+    end
+
+    path "/resource_collections/{resource_collection_id}/relationships/collection_resources/{id}" do
+      include_examples "an API show request",
+                        model: CollectionResource,
+                        tags: "Resource Collections",
+                        url_parameters: [:resource_collection_id],
+                        included_relationships: [:resource]
+    end
+  end
+end
