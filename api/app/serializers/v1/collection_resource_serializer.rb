ada6e2f8ee94164c8e3c22679c01ea064be56417
Connor Christensen <connor@castironcoding.com>
2020-01-03 16:37:37 -0800

    [F] Document CollectionResources API endpoints

--- api/app/serializers/v1/collection_resource_serializer.rb
+++ api/app/serializers/v1/collection_resource_serializer.rb
@@ -3,8 +3,8 @@ module V1
 
     include ::V1::Concerns::ManifoldSerializer
 
-    typed_attribute :position, NilClass
-    typed_attribute :resource_collection_id, NilClass
+    typed_attribute :position, Types::Integer
+    typed_attribute :resource_collection_id, Types::Serializer::ID
     typed_has_one :resource
 
   end
---
+++ api/spec/api_docs/definitions/resources/collection_resource.rb
@@ -0,0 +1,11 @@
+module ApiDocs
+  module Definitions
+    module Resources
+      class CollectionResource
+        class << self
+          include Resource
+        end
+      end
+    end
+  end
+end
