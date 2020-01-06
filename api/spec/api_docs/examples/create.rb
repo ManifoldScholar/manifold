80cbdd6af518609dc490707cf3dfa22aae6f9d87
Connor Christensen <connor@castironcoding.com>
2020-01-06 14:29:38 -0800

    [F] Allow overwrite of rswag success response code

--- api/spec/api_docs/examples/create.rb
+++ api/spec/api_docs/examples/create.rb
@@ -18,7 +18,7 @@ shared_examples_for "an API create request" do |options|
     security [apiKey: []] if api_spec_helper.requires_auth?
     tags api_spec_helper.tags
 
-    response "201", api_spec_helper.success_description, focus: api_spec_helper.focus do
+    response api_spec_helper.success_response_code, api_spec_helper.success_description, focus: api_spec_helper.focus do
       let(:Authorization) { get_user_token(api_spec_helper.authorized_user) } if api_spec_helper.requires_auth?
       schema api_spec_helper.response
       run_test!
