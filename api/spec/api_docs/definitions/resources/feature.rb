2c16505202268d10292f1dd07bd6b2821483b2f5
Connor Christensen <connor@castironcoding.com>
2020-01-06 17:10:13 -0800

    [F] Document Feature API endpoints

---
+++ api/spec/api_docs/definitions/resources/feature.rb
@@ -0,0 +1,23 @@
+module ApiDocs
+  module Definitions
+    module Resources
+      class Feature
+
+        REQUEST_ATTRIBUTES = {
+          background: Types::Serializer::Upload,
+          foreground: Types::Serializer::Upload,
+          remove_background: Types::Bool,
+          remove_foreground: Types::Bool
+        }.freeze
+
+        class << self
+          include Resource
+
+          def create_attributes
+            request_attributes.except(:remove_background, :remove_foreground)
+          end
+        end
+      end
+    end
+  end
+end
