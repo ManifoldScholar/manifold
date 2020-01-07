581e57a2e7fd8c092afca479e68aec7d72d1fe7a
Connor Christensen <connor@castironcoding.com>
2020-01-07 10:04:25 -0800

    [C] Change rswag auth_type param to authorized_users
    authorized_user is more clear about what needs to be passed
    in to add authorization to an rswag test.

--- api/spec/requests/api/v1/comments_spec.rb
+++ api/spec/requests/api/v1/comments_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe "Comments", type: :request do
 
     path "/annotations/{annotation_id}/relationships/comments/{id}" do
       include_examples "an API show request", model: Comment, tags: "Annotation Comments", url_parameters: [:annotation_id]
-      include_examples "an API update request", model: Comment, tags: "Annotation Comments", url_parameters: [:annotation_id], auth_type: :admin
-      include_examples "an API destroy request", model: Comment, tags: "Annotation Comments", url_parameters: [:annotation_id], auth_type: :admin
+      include_examples "an API update request", model: Comment, tags: "Annotation Comments", url_parameters: [:annotation_id], authorized_user: :admin
+      include_examples "an API destroy request", model: Comment, tags: "Annotation Comments", url_parameters: [:annotation_id], authorized_user: :admin
     end
 
     path "/annotations/{annotation_id}/relationships/comments" do
-      it_behaves_like "an API create request", model: Comment, tags: "Annotation Comments", url_parameters: [:annotation_id], auth_type: :admin, included_relationships: [:creator]
+      it_behaves_like "an API create request", model: Comment, tags: "Annotation Comments", url_parameters: [:annotation_id], authorized_user: :admin, included_relationships: [:creator]
       include_examples "an API index request", model: Comment, tags: "Annotation Comments", url_parameters: [:annotation_id], paginated: true, included_relationships: [:creator]
     end
   end
@@ -25,18 +25,18 @@ RSpec.describe "Comments", type: :request do
 
     path "/resources/{resource_id}/relationships/comments/{id}" do
       include_examples "an API show request", model: Comment, tags: "Resource Comments", url_parameters: [:resource_id]
-      include_examples "an API update request", model: Comment, tags: "Resource Comments", url_parameters: [:resource_id], auth_type: :admin
-      include_examples "an API destroy request", model: Comment, tags: "Resource Comments", url_parameters: [:resource_id], auth_type: :admin
+      include_examples "an API update request", model: Comment, tags: "Resource Comments", url_parameters: [:resource_id], authorized_user: :admin
+      include_examples "an API destroy request", model: Comment, tags: "Resource Comments", url_parameters: [:resource_id], authorized_user: :admin
     end
 
     path "/resources/{resource_id}/relationships/comments" do
-      it_behaves_like "an API create request", model: Comment, tags: "Resource Comments", url_parameters: [:resource_id], auth_type: :admin, included_relationships: [:creator]
+      it_behaves_like "an API create request", model: Comment, tags: "Resource Comments", url_parameters: [:resource_id], authorized_user: :admin, included_relationships: [:creator]
     end
   end
 
   path "/comments/{id}" do
     include_examples "an API show request", model: Comment
-    include_examples "an API update request", model: Comment, auth_type: :admin
-    include_examples "an API destroy request", model: Comment, auth_type: :admin
+    include_examples "an API update request", model: Comment, authorized_user: :admin
+    include_examples "an API destroy request", model: Comment, authorized_user: :admin
   end
 end
