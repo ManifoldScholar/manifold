b5f03293afd4fe341b6d1b30a6faf7eec6876ea6
Connor Christensen <connor@castironcoding.com>
2020-01-07 12:36:07 -0800

    [F] Document Contacts API endpoint

---
+++ api/spec/requests/api/v1/contacts_spec.rb
@@ -0,0 +1,50 @@
+require "swagger_helper"
+
+RSpec.describe "Contacts", type: :request do
+  path "/contacts" do
+    post "Sends an email to the provided address" do
+      consumes "application/json"
+      tags "Contacts"
+      parameter name: :body, in: :body, schema: ApiDocumentation::DryTypesParser.convert(
+        Types::Hash.schema(
+          data: Types::Hash.schema(
+            attributes: Types::Hash.schema(
+              email: Types::String,
+              full_name: Types::String,
+              message: Types::String
+            )
+          )
+        )
+      )
+
+      response "204", "Sent the email" do
+        let(:body) do
+          {
+          	data: {
+          		attributes: {
+          			email: "email@email.com",
+          			full_name: "John Rambo",
+          			message: "Hello world"
+          		}
+          	}
+          }
+        end
+        run_test!
+      end
+
+      response "422", "Improper request information" do
+        let(:body) do
+          {
+            data: {
+              attributes: {
+                full_name: "John Rambo",
+                message: "Hello world"
+              }
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
