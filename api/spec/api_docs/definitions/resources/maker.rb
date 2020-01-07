7a858f42d7f6d1fb13a82d05f252bc01568104a6
Connor Christensen <connor@castironcoding.com>
2020-01-07 15:58:21 -0800

    [F] Document Makers API endpoints

---
+++ api/spec/api_docs/definitions/resources/maker.rb
@@ -0,0 +1,23 @@
+module ApiDocs
+  module Definitions
+    module Resources
+      class Maker
+
+        REQUIRED_CREATE_ATTRIBUTES = [].freeze
+
+        REQUEST_ATTRIBUTES = {
+          avatar: Types::Serializer::Upload,
+          remove_avatar: Types::Bool
+        }
+
+        class << self
+          include Resource
+
+          def create_attributes
+            request_attributes.except(:remove_avatar)
+          end
+        end
+      end
+    end
+  end
+end
