38bc6008fbc469aa00ddfbba8930d1462d221311
Connor Christensen <connor@castironcoding.com>
2020-01-07 16:39:21 -0800

    [F] Document Pages API endpoints

--- api/app/serializers/v1/page_serializer.rb
+++ api/app/serializers/v1/page_serializer.rb
@@ -5,23 +5,23 @@ module V1
 
     abilities
 
-    typed_attribute :slug, NilClass
-    typed_attribute :pending_slug, NilClass
-    typed_attribute :title, NilClass
-    typed_attribute :nav_title, NilClass
-    typed_attribute :show_in_footer, NilClass
-    typed_attribute :show_in_header, NilClass
-    typed_attribute :created_at, NilClass
-    typed_attribute :updated_at, NilClass
-    typed_attribute :hidden, NilClass
-    typed_attribute :body_formatted, NilClass
-    typed_attribute :purpose, NilClass
-    typed_attribute :is_external_link, NilClass
-    typed_attribute :external_link, NilClass
-    typed_attribute :open_in_new_tab, NilClass
+    typed_attribute :slug, Types::String.meta(read_only: true)
+    typed_attribute :pending_slug, Types::String
+    typed_attribute :title, Types::String
+    typed_attribute :nav_title, Types::String
+    typed_attribute :show_in_footer, Types::Bool
+    typed_attribute :show_in_header, Types::Bool
+    typed_attribute :created_at, Types::DateTime.meta(read_only: true)
+    typed_attribute :updated_at, Types::DateTime.meta(read_only: true)
+    typed_attribute :hidden, Types::Bool
+    typed_attribute :body_formatted, Types::String.meta(example: "<p>string</p>", read_only: true)
+    typed_attribute :purpose, Types::String.enum("terms_and_conditions", "privacy_policy", "supplemental_content")
+    typed_attribute :is_external_link, Types::Bool
+    typed_attribute :external_link, Types::Serializer::URL.meta(description: "Required if is_external_link is true")
+    typed_attribute :open_in_new_tab, Types::Bool
 
     when_full do
-      typed_attribute :body, NilClass
+      typed_attribute :body, Types::String
     end
 
   end
