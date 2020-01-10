6c0552b4bd13a5312cc8b2e52ae82cfffb250cb6
Connor Christensen <connor@castironcoding.com>
2020-01-10 01:33:33 -0800

    [F] Document Ingestion API endpoints

---
+++ api/spec/requests/api/v1/ingestions_spec.rb
@@ -0,0 +1,24 @@
+require "swagger_helper"
+
+RSpec.describe "Ingestions", type: :request do
+  path "/ingestions/{id}" do
+    include_examples "an API update request", model: Ingestion, authorized_user: :admin
+    include_examples "an API show request",
+                      model: Ingestion,
+                      authorized_user: :admin,
+                      included_relationships: [:creator]
+  end
+
+  context "for a project" do
+    let!(:ingestion) { FactoryBot.create(:ingestion) }
+    let!(:project_id) { ingestion.project_id }
+
+    path "/projects/{project_id}/relationships/ingestions" do
+      include_examples "an API create request",
+                        model: Ingestion,
+                        url_parameters: [:project_id],
+                        authorized_user: :admin,
+                        included_relationships: [:creator]
+    end
+  end
+end
