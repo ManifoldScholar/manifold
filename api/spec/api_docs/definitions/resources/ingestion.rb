4e919dec3db90cd2df5120251c1046ddf381c7b9
Connor Christensen <connor@castironcoding.com>
2020-01-10 01:33:33 -0800

    [F] Document Ingestion API endpoints

---
+++ api/spec/api_docs/definitions/resources/ingestion.rb
@@ -0,0 +1,29 @@
+module ApiDocs
+  module Definitions
+    module Resources
+      class Ingestion
+
+        # TODO: Zach should circle back to this to make sure this makes sense
+        REQUEST_ATTRIBUTES = {
+          source: Types::Hash.schema(
+            id: Types::Serializer::URL,
+            storage: Types::String.enum("cache"),
+            metadata: Types::Hash.schema(
+              filename: Types::String.meta(example: "anton-chekhov_the-duel.epub"),
+              size: Types::Integer.meta(example: "683346"),
+              mimeType: Types::String.meta(example: "application/epub+zip")
+            ).meta(
+              description: 'In order to upload a source, first you must post to the "files" '\
+              'route which is our TUS upload endpoint. From that, you will get a file ID, '\
+              'which is included in the post to ingestions.'
+            )
+          )
+        }.freeze
+
+        class << self
+          include Resource
+        end
+      end
+    end
+  end
+end
