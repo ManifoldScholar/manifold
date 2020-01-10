dff10588410a1634d2657bcd6aae0a8242356ed5
Connor Christensen <connor@castironcoding.com>
2020-01-10 14:16:50 -0800

    [F] Document TwitterQuery API endpoints

--- api/app/serializers/v1/twitter_query_serializer.rb
+++ api/app/serializers/v1/twitter_query_serializer.rb
@@ -3,13 +3,13 @@ module V1
 
     include ::V1::Concerns::ManifoldSerializer
 
-    typed_attribute :created_at, NilClass
-    typed_attribute :updated_at, NilClass
-    typed_attribute :query, NilClass
-    typed_attribute :active, NilClass
-    typed_attribute :events_count, NilClass
-    typed_attribute :result_type, NilClass
-    typed_attribute :display_name, NilClass
+    typed_attribute :created_at, Types::DateTime.meta(read_only: true)
+    typed_attribute :updated_at, Types::DateTime.meta(read_only: true)
+    typed_attribute :query, Types::String
+    typed_attribute :active, Types::Bool
+    typed_attribute :events_count, Types::Integer.meta(read_only: true)
+    typed_attribute :result_type, Types::String.enum("most_recent", "popular")
+    typed_attribute :display_name, Types::String.meta(read_only: true)
 
     typed_belongs_to :project
   end
