0cc36ffd84737fda1e92b3c2aa7a6f23b2edd6ae
Connor Christensen <connor@castironcoding.com>
2020-01-15 10:17:48 -0800

    [B] Fix development eager loading errors
    Only run eager loading in development if no errors are present.

--- api/config/initializers/zzz_eager_load_in_dev.rb
+++ api/config/initializers/zzz_eager_load_in_dev.rb
@@ -1,1 +1,9 @@
-Rails.application.eager_load! if Rails.env.development?
+# rubocop:disable Lint/HandleExceptions
+if Rails.env.development?
+  begin
+    Rails.application.eager_load!
+  rescue ActiveRecord::ActiveRecordError
+    # purposely left blank
+  end
+end
+# rubocop:enable Lint/HandleExceptions
