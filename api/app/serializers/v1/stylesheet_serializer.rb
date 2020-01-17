49108505eed9cbb4912f338d85b722e08bc125e0
Connor Christensen <connor@castironcoding.com>
2020-01-16 17:02:25 -0800

    [F] Document Stylesheet API endpoints

--- api/app/serializers/v1/stylesheet_serializer.rb
+++ api/app/serializers/v1/stylesheet_serializer.rb
@@ -3,15 +3,22 @@ module V1
 
     include ::V1::Concerns::ManifoldSerializer
 
-    typed_attribute :name, NilClass
-    typed_attribute :source_identifier, NilClass
-    typed_attribute :styles, NilClass
-    typed_attribute :ingested, NilClass
-    typed_attribute :position, NilClass
-    typed_attribute :created_at, NilClass
+    typed_attribute :name, Types::String
+    typed_attribute :source_identifier, Types::String.optional.meta(read_only: true)
+    typed_attribute :styles, Types::String.optional.meta(
+      read_only: true,
+      description: "Validated, scoped, and transformed styles that will be "\
+      "applied in the reader."
+    )
+    typed_attribute :ingested, Types::Bool.meta(read_only: true)
+    typed_attribute :position, Types::Integer
+    typed_attribute :created_at, Types::DateTime.meta(read_only: true)
 
     when_full do
-      typed_attribute :raw_styles, NilClass
+      typed_attribute :raw_styles, Types::String.optional.meta(
+        example: ".class-selector { background: black }",
+        description: "CSS styles and selectors for your custom stylesheet"
+      )
 
       typed_belongs_to :text
       typed_has_many :text_sections
