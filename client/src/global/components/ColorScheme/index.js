6b18b08c0a89a4f294fd83cad69fb58f15fba52a
Zach Davis <zach@castironcoding.com>
2020-01-17 12:31:07 -0800

    [B] Correct generated custom color palette

--- client/src/global/components/ColorScheme/index.js
+++ client/src/global/components/ColorScheme/index.js
@@ -1,22 +1,37 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import colorHelper from "color";
+import colorHelper from "tinycolor2";
 import { Helmet } from "react-helmet-async";
 
 export default class ColorScheme extends Component {
+  // https://codesandbox.io/s/clever-dust-1tbcz
   static customProperties = {
     "--accent-primary": color => color,
-    "--accent-primary-dark": color => color.desaturate(0.2097).darken(0.1882),
-    "--accent-primary-medium": color => color.desaturate(0.2094).darken(0.1157),
-    "--accent-primary-light": color => color.saturate(0.36).lighten(0.0804),
-    "--accent-primary-dull": color => color.desaturate(0.1157).darken(0.2094),
-    "--accent-primary-pale": color => color.desaturate(0.1456).lighten(0.2),
-    "--accent-primary-extra-pale": color =>
-      color.desaturate(0.0155).lighten(0.2765),
+    "--accent-primary-dark": color => color.desaturate(20.97).darken(18.82),
+    "--accent-primary-medium": color => color.desaturate(20.94).darken(11.57),
+    "--accent-primary-light": color => color.saturate(0.36).lighten(8.04),
+    "--accent-primary-dull": color =>
+      color
+        .lighten(20)
+        .desaturate(14.56)
+        .spin(-3),
+    "--accent-primary-pale": color =>
+      color
+        .lighten(25.69)
+        .saturate(5)
+        .spin(2),
     "--accent-primary-off-white": color =>
-      color.desaturate(0.2214).lighten(0.3549),
+      color
+        .lighten(35.49)
+        .desaturate(22.14)
+        .spin(5),
+    "--accent-primary-extra-pale": color => color.lighten(27.65).saturate(1.19),
     "--accent-interaction-light": color => color,
-    "--accent-interaction-dark": color => color.saturate(0.1701).darken(0.3529)
+    "--accent-interaction-dark": color =>
+      color
+        .darken(35.29)
+        .saturate(17.01)
+        .spin(4)
   };
 
   componentDidMount() {
@@ -80,19 +95,24 @@ export default class ColorScheme extends Component {
   }
 
   hasCustomColor() {
-    return Boolean(this.accentColor());
+    return Boolean(
+      this.accentColor() && colorHelper(this.accentColor()).isValid()
+    );
   }
 
   setColorScheme() {
     if (!this.hasCustomColor()) return this.restoreDefaultAccent();
     try {
-      const accent = colorHelper(this.accentColor());
       Object.keys(this.constructor.customProperties).forEach(customProperty => {
+        const accent = colorHelper(this.accentColor());
         const transformer = this.constructor.customProperties[customProperty];
-        const value = transformer(accent).hex();
+        const value = transformer(accent).toHexString();
         this.setCustomProperty(customProperty, value);
       });
     } catch (error) {
+      /* eslint-disable no-console */
+      console.log(error, "Manifold color scheme error!");
+      /* eslint-enable no-console */
       this.restoreDefaultAccent();
     }
   }
@@ -104,11 +124,14 @@ export default class ColorScheme extends Component {
       const accent = colorHelper(this.accentColor());
       Object.keys(this.constructor.customProperties).forEach(customProperty => {
         const transformer = this.constructor.customProperties[customProperty];
-        const value = transformer(accent).hex();
+        const value = transformer(accent).toHexString();
         rules.push(`${customProperty}: ${value};`);
       });
       if (rules.length > 0) return `body { ${rules.join(" ")} };`;
     } catch (error) {
+      /* eslint-disable no-console */
+      console.log(error, "Manifold color scheme error!");
+      /* eslint-enable no-console */
       return null;
     }
   }
