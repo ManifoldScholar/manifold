5962484e016262f8fca36784df537a99401d336a
Alexa Grey <alexa@castironcoding.com>
2020-01-17 09:42:05 -0800

    [B] Enforce Lockbox master key length

--- api/config/initializers/25_lockbox.rb
+++ api/config/initializers/25_lockbox.rb
@@ -1,5 +1,8 @@
 secret_key_base = Rails.application.secrets.secret_key_base
 
-secret_key_base = secret_key_base.presence || SecureRandom.hex(32) if Rails.env.test?
+secret_key_base = secret_key_base.presence || Lockbox.generate_key if Rails.env.test?
 
-Lockbox.master_key = secret_key_base
+# Consistently ensure that it's a 64-character hexadecimal key
+enforced_master_key = secret_key_base.gsub(/[^a-zA-Z0-9]+/, "")[/\A([a-zA-Z0-9]{1,64})/, 1].rjust(64, "0")
+
+Lockbox.master_key = enforced_master_key
