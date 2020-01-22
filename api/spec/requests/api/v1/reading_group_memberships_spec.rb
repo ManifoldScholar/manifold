ec8f8c48778af8b7b8ad6182e88d502b17e54d47
Connor Christensen <connor@castironcoding.com>
2020-01-22 10:01:57 -0800

    [F] Document ReadingGroupMembership API endpoints

---
+++ api/spec/requests/api/v1/reading_group_memberships_spec.rb
@@ -0,0 +1,50 @@
+require "swagger_helper"
+
+RSpec.describe "ReadingGroupMembership", type: :request do
+
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:reading_group) { FactoryBot.create(:reading_group) }
+  let(:reading_group_id) { reading_group.id }
+
+  path "/reading_group_memberships" do
+    include_examples "an API create request",
+                      model: ReadingGroupMembership,
+                      paginated: true,
+                      included_relationships: [:user],
+                      authorized_user: :admin do
+                        let(:body) {
+                          {
+                            data: {
+                              relationships: {
+                                user: {
+                                  data: {
+                                    id: user.id
+                                  }
+                                },
+                                readingGroup: {
+                                  data: {
+                                    id: reading_group.id
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      end
+  end
+
+  path "/reading_group_memberships/{id}" do
+    include_examples "an API destroy request",
+                      model: ReadingGroupMembership,
+                      authorized_user: :admin
+  end
+
+  path "/reading_groups/{reading_group_id}/relationships/reading_group_memberships" do
+    include_examples "an API index request",
+                      model: ReadingGroupMembership,
+                      summary: "Returns all ReadingGroupMemberships related to a ReadingGroup",
+                      url_parameters: [:reading_group_id],
+                      tags: "Reading Groups",
+                      authorized_user: :admin
+  end
+end
