f7b587f0c64b26def8c2ce14abe390e7ad7e5c79
Connor Christensen <connor@castironcoding.com>
2020-01-22 14:38:47 -0800

    [F] Document Statistics API endpoints

---
+++ api/spec/api_docs/definitions/resources/statistic.rb
@@ -0,0 +1,14 @@
+module ApiDocs
+  module Definitions
+    module Resources
+      class Statistics
+
+        ID_TYPE = ::Types::String.meta(example: "0")
+
+        class << self
+          include Resource
+        end
+      end
+    end
+  end
+end
