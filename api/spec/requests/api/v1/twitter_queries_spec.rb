ba8c0322b625cdd11364b2c5c9ab514fc995370a
Connor Christensen <connor@castironcoding.com>
2020-01-22 17:08:29 -0800

    [F] Document TwitterQuery fetch route

--- api/spec/requests/api/v1/twitter_queries_spec.rb
+++ api/spec/requests/api/v1/twitter_queries_spec.rb
@@ -26,5 +26,22 @@ RSpec.describe "Twitter Queries", type: :request do
                         summary: "Gets all twitter queries associated with a project",
                         url_parameters: [:project_id]
     end
+
+  end
+
+  context "when belonging to a twitter_query" do
+    let!(:resource) { FactoryBot.create(:twitter_query) }
+    let!(:twitter_query_id) { resource.id }
+    path "/twitter_queries/{twitter_query_id}/relationships/fetch" do
+      include_examples "an API create request",
+                        model: TwitterQuery,
+                        summary: "Returns all tweets that match the query in the TwitterQuery",
+                        description: "In order to run this query successfully, "\
+                        "twitter credentials must be set in the settings for this "\
+                        "manifold instance. Please look at the /settings routes "\
+                        "for more information on how to set twitter credentials.",
+                        authorized_user: :admin,
+                        url_parameters: [:twitter_query_id]
+    end
   end
 end
