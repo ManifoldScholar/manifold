376b980e678afb005e55bbdbdb9c2670ebb98224
Connor Christensen <connor@castironcoding.com>
2020-01-22 16:27:32 -0800

    [B] Fix bugs in TestMailsController create method
    * Use the authenticate_request method to check if no user is provided
    * Return the respond with forbidden if the user can not sent a test mail

--- api/app/controllers/api/v1/test_mails_controller.rb
+++ api/app/controllers/api/v1/test_mails_controller.rb
@@ -2,24 +2,13 @@ module Api
   module V1
     # Test Mail controller
     class TestMailsController < ApplicationController
+      before_action :authenticate_request!
 
       def create
-        respond_with_error unless current_user.can?(:send_test_mail)
+        return respond_with_forbidden("TestMail", "create") unless current_user.can?(:send_test_mail)
+
         TestMailer.test(current_user).deliver_now
       end
-
-      private
-
-      def respond_with_error
-        vars = { resource: "TestMail", action: "create" }
-        options = {
-          status: 403,
-          title: I18n.t("controllers.errors.forbidden.instance.title", vars).titlecase,
-          detail: I18n.t("controllers.errors.forbidden.instance.detail", vars)
-        }
-        render json: { errors: build_api_error(options) }, status: :forbidden
-      end
-
     end
   end
 end
