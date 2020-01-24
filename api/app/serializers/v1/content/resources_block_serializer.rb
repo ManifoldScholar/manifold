f12ebda20915e372f6120cd3813af40b88216d37
Zach Davis <zach@castironcoding.com>
2020-01-24 16:48:52 -0600

    [B] Restore relationship on resources block

--- api/app/serializers/v1/content/resources_block_serializer.rb
+++ api/app/serializers/v1/content/resources_block_serializer.rb
@@ -3,6 +3,9 @@ module V1
     class ResourcesBlockSerializer < ContentBlockSerializer
       # TODO: Address these dynamic attributes
       attributes(*::Content::ResourcesBlock.available_attributes)
+
+      typed_has_many :featured_collections, record_type: "resourceCollection"
+
     end
   end
 end
