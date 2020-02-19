fff53af689a64db16f50666ad608f719f1afbe7b
Zach Davis <zach@castironcoding.com>
2020-02-19 15:28:12 -0800

    [B] Correctly handle errors that begin with caret
    When an error message begins with a caret, the API should not preface the message with the field name.

--- api/app/serializers/v1/helpers/errors.rb
+++ api/app/serializers/v1/helpers/errors.rb
@@ -55,7 +55,7 @@ module V1
         base = @errors.generate_message(attribute, type) if type.is_a? Symbol
         # NOTE: Potential option for conversion to dry types
         msg = base.is_a?(Hash) ? base.values.first : base
-        return msg.slice(0) if msg.start_with?("^")
+        return msg[1..-1] if msg.start_with?("^")
 
         "#{attribute.to_s.humanize} #{msg}"
       end
