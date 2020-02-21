360a076e43ffe6e1465b679737dc1308b8e6d2f9
Zach Davis <zach@castironcoding.com>
2020-02-21 09:58:28 -0800

    [B] Skip some invalid ingestion sources
    When ingesting, we can safely skip ingestion sources that are invalid
    and of the type “publication_resource”. If we dont skip these, all it
    takes is an errant DS_Store file to blow up a manifest ingestion, which
    will try to include every file it finds in the archive, whether or not
    its referenced in the manifest. This is less of a problem with EPUBs,
    which require an explicit reference to every file in the package manifest.
    
    Fixes #2612

--- api/spec/services/ingestions/strategies/manifest_spec.rb
+++ api/spec/services/ingestions/strategies/manifest_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe Ingestions::Strategies::Manifest do
         it "has the correct TOC" do
           expected = [
             { "label" => "Title Set From TOC", "source_path" => "section_1.html", "start_section" => true, "children" => [
-              { "label" => "Section 1.1", "source_path" => "section_1_1.html", "children" => [ { "label" => "Section 1.1a", "source_path" => "section_1_1a.md" } ] },
+              { "label" => "Section 1.1", "source_path" => "section_1_1.html", "children" => [{ "label" => "Section 1.1a", "source_path" => "section_1_1a.md" }] }
             ] },
             { "label" => "Section 1#1", "source_path" => "section_1.html#1" }
           ]
@@ -62,7 +62,7 @@ RSpec.describe Ingestions::Strategies::Manifest do
         end
 
         it "has one text section for every unique ingestion source referenced in TOC" do
-          expected = [{ "source_identifier" => "02e9b3b8b5268c3fe8bd4150fd546a55", "name" => "Title Set From TOC", "kind" => "section", "position"  => 0, "build" => "build/02e9b3b8b5268c3fe8bd4150fd546a55.html" },
+          expected = [{ "source_identifier" => "02e9b3b8b5268c3fe8bd4150fd546a55", "name" => "Title Set From TOC", "kind" => "section", "position" => 0, "build" => "build/02e9b3b8b5268c3fe8bd4150fd546a55.html" },
                       { "source_identifier" => "9fef2b7e781641e6861d1aa79a597a57", "name" => "Section 1.1", "kind" => "section", "position" => 1, "build" => "build/9fef2b7e781641e6861d1aa79a597a57.html" },
                       { "source_identifier" => "e2fe424ecf8e3c87da127ea617e57107", "name" => "Section 1.1a", "kind" => "section", "position" => 2, "build" => "build/e2fe424ecf8e3c87da127ea617e57107.html" }]
           expect(manifest[:relationships][:text_sections]).to eq expected
@@ -102,11 +102,9 @@ RSpec.describe Ingestions::Strategies::Manifest do
         { "label" => "Section1", "source_path" => "2b4f3aa2fe044dbd607d21f2e949eb06.html", "start_section" => true },
         { "label" => "Section2", "source_path" => "e9e7fd64ac05018a6c9289f686581212.html", "children" => [
           { "label" => "Section2-a", "source_path" => "a6b4fe5e132fe438611f444deec4f90a.html" }
-        ]}
+        ] }
       ]
       expect(manifest[:attributes][:toc]).to eq expected
     end
-
   end
-
 end
