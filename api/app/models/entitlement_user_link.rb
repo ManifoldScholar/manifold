82a64a88f05dee7c8612740f5a2554cde55a65da
Alexa Grey <alexa@castironcoding.com>
2020-03-24 08:36:14 -0700

    [C] Remove user from reading group entitlement

--- api/app/models/entitlement_user_link.rb
+++ api/app/models/entitlement_user_link.rb
@@ -6,6 +6,10 @@ class EntitlementUserLink < ApplicationRecord
 
   validates :entitlement_id, uniqueness: { on: :update, scope: :user_id }
 
+  scope :by_target, ->(target) { joins(:entitlement).merge(Entitlement.by_target(target)) }
+  scope :by_entitlement, ->(entitlement) { where(entitlement: entitlement) }
+  scope :by_user, ->(user) { where(user: user) }
+
   after_create :grant_roles!
 
   delegate :granted_role_names, :subject, to: :entitlement
--- api/spec/models/reading_group_membership_spec.rb
+++ api/spec/models/reading_group_membership_spec.rb
@@ -22,15 +22,37 @@ RSpec.describe ReadingGroupMembership, type: :model do
     expect(subject.valid?).to be false
   end
 
-  context "when adding a member to a reading group that has an existing entitlement" do
+  context "in a reading group that has an existing entitlement" do
     let!(:reading_group) { FactoryBot.create :reading_group }
     let!(:entitlement) { FactoryBot.create :entitlement, :global_subscriber, :for_reading_group, target: reading_group }
     let!(:user) { FactoryBot.create :user }
 
-    it "enrolls the user" do
-      expect do
-        FactoryBot.create :reading_group_membership, reading_group: reading_group, user: user
-      end.to change(EntitlementUserLink, :count).by(1).and change { user.reload.has_role?(:subscriber, :any) }.from(false).to(true)
+    context "when adding a member" do
+      it "enrolls the user" do
+        expect do
+          FactoryBot.create :reading_group_membership, reading_group: reading_group, user: user
+        end.to change(EntitlementUserLink, :count).by(1).and change { user.reload.has_role?(:subscriber, :any) }.from(false).to(true)
+      end
+    end
+
+    context "when removing an entitled member" do
+      let!(:membership) { FactoryBot.create :reading_group_membership, reading_group: reading_group, user: user }
+
+      it "destroys the entitlement" do
+        expect do
+          membership.destroy!
+        end.to change(EntitlementUserLink, :count).by(-1)
+      end
+
+      it "keeps the existing role until audit runs again" do
+        expect do
+          membership.destroy!
+        end.to keep_the_same { user.reload.has_role?(:subscriber, :any) }
+
+        expect do
+          Entitlements::AuditJob.perform_now
+        end.to change { user.reload.has_role?(:subscriber, :any) }.from(true).to(false)
+      end
     end
   end
 end
