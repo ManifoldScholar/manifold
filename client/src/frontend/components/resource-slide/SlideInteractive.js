ddee827475a671e9414b091659ff776c88b9c3f0
Connor Christensen <connor@castironcoding.com>
2020-03-25 12:32:28 -0700

    [B] Add poster fallback to interactive slides
    The logic for when an iframe is to be displayed now exists in the
    SlideInteractive component. A Hero simply needs to display the
    iframe, so the SlideInteractive component was replaces with a
    resource player. Some slight SCSS tweaks were made to shift
    some of the iframe sizing from JavaScript to CSS.

--- client/src/frontend/components/resource-slide/SlideInteractive.js
+++ client/src/frontend/components/resource-slide/SlideInteractive.js
@@ -1,21 +1,134 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import ResourcePlayer from "frontend/components/resource-player";
+import throttle from "lodash/throttle";
+import SlideDefault from "frontend/components/resource-slide/SlideDefault";
+import Zoom from "./Zoom";
 
-export default class ResourceListSlideInteractive extends Component {
+export default class ResourceListSlideInteractive extends PureComponent {
+  static displayName = "ResourceList.Slide.Interactive";
+
   static propTypes = {
-    resource: PropTypes.object,
-    enableZoom: PropTypes.bool,
-    noPlaceholder: PropTypes.bool
+    resource: PropTypes.object.isRequired,
+    flexibleHeight: PropTypes.bool,
+    enableZoom: PropTypes.bool
   };
 
   static defaultProps = {
     enableZoom: true,
-    zoomLabel: "View Interactive Resource",
-    noPlaceholder: false
+    flexibleHeight: false
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      containerWidth: 0,
+      containerHeight: 0
+    };
+
+    this.updateStateOnResize = throttle(this.updateContainerState, 100);
+  }
+
+  get resource() {
+    return this.props.resource;
+  }
+
+  get poster() {
+    const { resource } = this.props;
+    if (!resource.attributes.variantPosterStyles) return null;
+    return resource.attributes.variantPosterStyles.mediumLandscape;
+  }
+
+  get backgroundImage() {
+    return this.poster || "/static/images/resource-splash.png";
+  }
+
+  get showInfo() {
+    // do not place the info box over the background if there is a poster
+    return !this.poster;
+  }
+
+  componentDidMount() {
+    this.updateContainerState();
+    window.addEventListener("resize", this.updateStateOnResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateStateOnResize);
+  }
+
+  setContainer = el => {
+    this.containerRef = el;
+  };
+
+  updateContainerState = () => {
+    this.setState({
+      containerWidth: this.containerWidth(),
+      containerHeight: this.containerHeight()
+    });
+  };
+
+  containerWidth() {
+    if (!this.containerRef) return 0;
+    return this.containerRef.offsetWidth;
+  }
+
+  containerHeight() {
+    if (!this.containerRef) return 0;
+    return this.containerRef.offsetHeight;
+  }
+
+  containerIsWideEnough() {
+    return this.minimumWidth() <= this.state.containerWidth;
+  }
+
+  containerIsTallEnough() {
+    if (this.props.flexibleHeight) return true;
+    return this.minimumHeight() <= this.state.containerHeight;
+  }
+
+  minimumHeight() {
+    return this.props.resource.attributes.minimumHeight || 0;
+  }
+
+  minimumWidth() {
+    return this.props.resource.attributes.minimumWidth || 0;
+  }
+
+  get canShowIFrame() {
+    if (this.poster) return false;
+    return this.containerIsWideEnough() && this.containerIsTallEnough();
+  }
+
+  renderSlideContents() {
+    const { resource } = this.props;
+    if (this.canShowIFrame) {
+      return (
+        <ResourcePlayer.Iframe
+          resource={resource}
+          styleProps={{ height: "100%" }}
+        />
+      );
+    }
+
+    return (
+      <SlideDefault
+        resource={resource}
+        info={this.showInfo}
+        backgroundUrl={this.poster}
+      />
+    );
+  }
+
   render() {
-    return <ResourcePlayer.Iframe {...this.props} />;
+    const { resource, enableZoom } = this.props;
+    return (
+      <div ref={this.setContainer} style={{ width: "100%", height: "100%" }}>
+        {enableZoom && (
+          <Zoom label={"View interactive resource"} resource={resource} />
+        )}
+        {this.renderSlideContents()}
+      </div>
+    );
   }
 }
