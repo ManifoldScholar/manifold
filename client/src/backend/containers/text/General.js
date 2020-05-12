7b78abe40d4d43fb62dec5fe997be8448d888a77
Zach Davis <zach@castironcoding.com>
2020-05-12 09:58:47 -0700

    [C] Reorganize text form fields

--- client/src/backend/containers/text/General.js
+++ client/src/backend/containers/text/General.js
@@ -21,47 +21,61 @@ export default class TextGeneralContainer extends PureComponent {
           create={textsAPI.create}
           className="form-secondary"
         >
-          <Form.Header label="General" />
-          <Form.TextInput
-            label="Title"
-            name="attributes[title]"
-            placeholder="Enter Text Title"
-          />
-          <Form.TextInput
-            label="Subtitle"
-            name="attributes[subtitle]"
-            placeholder="Enter Subtitle"
-          />
-          <Form.TextInput
-            wide
-            label="Slug"
-            name="attributes[pendingSlug]"
-            placeholder="Enter Text Slug"
-          />
-          <Form.Switch label="Published?" name="attributes[published]" />
-          <Form.Date
-            label="Publication Date"
-            name="attributes[publicationDate]"
-          />
-          <Form.TextArea
-            label="Description"
-            name="attributes[description]"
-            placeholder="Enter a Brief Description"
-          />
-          <Form.Upload
-            layout="portrait"
-            label="Cover"
-            accepts="images"
-            readFrom="attributes[coverStyles][small]"
-            name="attributes[cover]"
-            remove="attributes[removeCover]"
-          />
-          <Form.TextInput
-            label="Section Label"
-            name="attributes[sectionKind]"
-            placeholder="Enter a label for sections in this text"
-            instructions="For example, “chapter” for books or “article” for journals"
-          />
+          <Form.FieldGroup label="General">
+            <Form.TextInput
+              wide
+              label="Title"
+              name="attributes[title]"
+              placeholder="Enter Text Title"
+            />
+            <Form.TextInput
+              wide
+              label="Subtitle"
+              name="attributes[subtitle]"
+              placeholder="Enter Subtitle"
+            />
+            <Form.DatePicker
+              label="Publication Date"
+              name="attributes[publicationDate]"
+            />
+            <Form.TextInput
+              wide
+              label="Slug"
+              name="attributes[pendingSlug]"
+              placeholder="Enter Text Slug"
+            />
+            <Form.TextArea
+              wide
+              label="Description"
+              name="attributes[description]"
+              placeholder="Enter a Brief Description"
+            />
+            <Form.Upload
+              wide
+              layout="portrait"
+              label="Cover"
+              accepts="images"
+              readFrom="attributes[coverStyles][small]"
+              name="attributes[cover]"
+              remove="attributes[removeCover]"
+            />
+          </Form.FieldGroup>
+          <Form.FieldGroup label="Presentation">
+            <Form.Switch
+              wide
+              className="form-toggle-secondary"
+              instructions={`When on, a "published" badge will appear next to the text in lists of texts.`}
+              label="Published?"
+              name="attributes[published]"
+            />
+            <Form.TextInput
+              wide
+              label="Section Label"
+              name="attributes[sectionKind]"
+              placeholder="Section"
+              instructions="For example, “chapter” for books or “article” for journals"
+            />
+          </Form.FieldGroup>
           <Form.Save text="Save Text" />
         </FormContainer.Form>
       </section>
