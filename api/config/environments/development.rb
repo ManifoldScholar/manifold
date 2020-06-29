331e0884b4641c32f6c471a08e90bf02bc3bc7ef
Zach Davis <zach@castironcoding.com>
2020-06-29 14:55:05 -0700

    [B] Address Rails 6 Host Authorization

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -39,6 +39,10 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  Rails.application.configure do
+    config.hosts << "manifold.lvh"
+    config.hosts << ENV['DOMAIN']
+  end
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
