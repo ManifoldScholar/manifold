d46166f3515ebacc3fdf1d282c7a34bbfc401d2f
Zach Davis <zach@castironcoding.com>
2020-07-09 13:25:18 -0700

    [C] Add uploader specs

--- api/spec/uploaders/attachment_uploader_spec.rb
+++ api/spec/uploaders/attachment_uploader_spec.rb
@@ -1,9 +1,19 @@
 require "rails_helper"
 RSpec.describe AttachmentUploader do
   let(:attachment) { project.reload.avatar }
-  let(:path) { Rails.root.join('spec','data','assets', 'images', 'test_avatar.jpg') }
+  let(:path) { Rails.root.join("spec", "data", "assets", "images", "test_avatar.jpg") }
   let(:project) { FactoryBot.build(:project, avatar: fixture_file_upload(path)) }
-  before { perform_enqueued_jobs { project.save } }
+
+  before do
+    png_path = Rails.root.join("spec", "data", "assets", "images", "1x1.png")
+    url = "https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png"
+    stub_request(:get, url).to_return(
+      status: 200,
+      body: File.read(png_path),
+      headers: { "content-type" => "image/png" }
+    )
+    perform_enqueued_jobs { project.save }
+  end
 
   it "extracts metadata" do
     expect(attachment[:original].mime_type).to eq("image/jpeg")
@@ -19,8 +29,23 @@ RSpec.describe AttachmentUploader do
     expect(attachment.keys - [:original] && Attachments::BASE_STYLES.keys == Attachments::BASE_STYLES.keys).to be true
   end
 
-  it "is valid" do
-    expect(project.valid?).to eq true
+  it "stores dimensions for the original" do
+    expect(attachment[:original].metadata["width"]).to be 642
+    expect(attachment[:original].metadata["height"]).to be 642
+  end
+
+  it "stores dimensions for variants" do
+    expect(attachment[:medium].metadata["width"]).to be 640
+    expect(attachment[:medium].metadata["height"]).to be 640
+  end
+
+  context "when fetching from a remote URL" do
+    let(:project) { FactoryBot.build(:project, avatar_remote_url: "https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png") }
+
+    it "is stored correctly" do
+      expect(attachment[:original].storage_key).to eq("store")
+      expect(attachment[:original].size).to be 95
+    end
   end
 
   it "promotes the asset to the store" do
---
+++ api/spec/uploaders/external_source_uploader_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+RSpec.describe ExternalSourceUploader do
+  let(:attachment) { cached_external_source.reload.asset }
+  let(:path) { Rails.root.join("spec", "data", "assets", "images", "publication_resource.png") }
+  let(:cached_external_source) { FactoryBot.build(:cached_external_source, :png_image, asset: fixture_file_upload(path)) }
+  before do
+    perform_enqueued_jobs { cached_external_source.save }
+  end
+
+  it "extracts metadata" do
+    expect(attachment.mime_type).to eq("image/png")
+    expect(attachment.extension).to eq("png")
+    expect(attachment.size).to be_instance_of(Integer)
+  end
+  it "stores the SHA256 hash" do
+    expect(attachment.metadata["sha256"]).to be_a String
+  end
+
+  it "adds a has_asset? method to the model" do
+    expect(cached_external_source.respond_to?(:has_asset?)).to be true
+  end
+
+  it "adds a has_no_asset? method to the model" do
+    expect(cached_external_source.respond_to?(:has_no_asset?)).to be true
+  end
+
+  it "adds a path method to the model" do
+    expect(cached_external_source.respond_to?(:asset_path)).to be true
+  end
+
+  it "adds a file name method to the model" do
+    expect(cached_external_source.respond_to?(:asset_file_name)).to be true
+  end
+
+  it "promotes the asset to the store" do
+    expect(attachment.storage_key).to eq("store")
+  end
+end
