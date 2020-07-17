6d2553d27c108477fadaa80b06d7cae6c380b672
ConnorChristensen <connor@castironcoding.com>
2020-07-17 16:08:14 -0700

    [C] Clean up project collection storybooks

--- client/src/frontend/components/project-collection/__stories__/ProjectCollection.stories.js
+++ client/src/frontend/components/project-collection/__stories__/ProjectCollection.stories.js
@@ -2,12 +2,13 @@ import React from "react";
 import { storiesOf, fixtures } from "helpers/storybook/exports";
 import { boolean, number, select, text } from "@storybook/addon-knobs";
 import ProjectCollection from "../index";
-
 import iconSVG from "test/assets/icon.png";
 import heroBackground from "test/assets/hero-bg.jpg";
 import squareHero from "test/assets/hero-square.jpg";
 
 const { Summary, Detail } = ProjectCollection;
+
+/* ************ Data Mocks ************ */
 
 const projects = fixtures.collectionFactory("project");
 const collectionProjects = fixtures.collectionFactory("collectionProject");
@@ -15,8 +16,9 @@ const projectCollection = fixtures.factory("projectCollection", {
   relationships: { collectionProjects, subjects: [], projects }
 });
 const pagination = fixtures.pagination();
-
 const defaultDescription = projectCollection.attributes.descriptionFormatted;
+
+/* ************ Storybook Knob Selection Options ************ */
 
 const iconOptions = {
   none: null,
@@ -37,8 +39,19 @@ const bannerOptions = {
   "large size hero": "full_bleed"
 };
 
-// eslint-disable-next-line prettier/prettier
+/* ************ Helper Functions ************ */
+
+/* Our base project collection does not have any images or custom icons attached
+ * to it. This helper function will supply the needed information to the project
+ * collection to display the variety of formats that the project collection headers
+ * can take.
+ */
+
+ // eslint-disable-next-line prettier/prettier
 function createProjectCollectionWithImages(detailType, icon, description, title) {
+  // create an object that will hold on to fields that the storybook knobs control
+  // so we can modify the mock project collection data without mutating the original
+  // object
   const mergedAttributes = {
     iconStyles: null,
     heroStyles: null,
@@ -75,7 +88,6 @@ storiesOf("Frontend/ProjectCollection", module)
   .add("Summary", () => {
     const authenticated = boolean("Authenticated", true);
     const limit = number("Project limit", 5);
-    const invertColor = boolean("Invert Color", false);
     const description = boolean("Show Description", true);
     const title = text("Text", "A Project Collection");
     const detailType = select("Type", bannerOptions, "default");
@@ -92,7 +104,6 @@ storiesOf("Frontend/ProjectCollection", module)
       <Context>
         <Summary
           projectCollection={projectCollectionWithImages}
-          invertColor={invertColor}
           limit={limit}
           authentication={{ authenticated }}
         />
