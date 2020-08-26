3ebeccf8e7318c19aff1f900f5d1cdec0632b17a
Zach Davis <zach@castironcoding.com>
2020-08-26 15:30:34 -0700

    [B] Allow annotation comments when engagement is disabled
    Resolves #2832
    
    If a user can read an annotation, they can comment on it, even if public engagement is disabled on the project.

--- api/spec/authorizers/comment_authorizer_spec.rb
+++ api/spec/authorizers/comment_authorizer_spec.rb
@@ -30,6 +30,20 @@ RSpec.describe "Comment Abilities", :authorizer do
     the_subject_behaves_like "instance abilities", Comment, all: true
   end
 
+  context "when the comment is on an annotation in a closed project with disabled engagement" do
+    let!(:project) { FactoryBot.create(:project, :with_restricted_access, disable_engagement: true ) }
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:reading_group) { FactoryBot.create(:reading_group)}
+    let!(:reading_group_membership) { FactoryBot.create(:reading_group_membership, reading_group: reading_group, user: user)}
+    let!(:entitlement) { FactoryBot.create :entitlement, :project_read_access, :for_reading_group, target: reading_group.reload, subject: project }
+    let(:text) { FactoryBot.create(:text, project: project) }
+    let(:annotation) { FactoryBot.create(:annotation, creator: user, reading_group: reading_group, text: text) }
+    let!(:object) { FactoryBot.build(:comment, creator: user, subject: annotation) }
+    let!(:subject) { user.reload }
+    # abilities = { create: true, read: true, update: true, delete: true }
+    the_subject_behaves_like "instance abilities", Comment, all: true
+  end
+
   context "when the comment is for a project that has disabled engagement" do
     let(:subject) { FactoryBot.create(:user) }
     let(:project) { FactoryBot.create(:project, disable_engagement: true) }
@@ -44,7 +58,7 @@ RSpec.describe "Comment Abilities", :authorizer do
       let(:text) { FactoryBot.create(:text, project: project) }
       let(:text_section) { FactoryBot.create(:text_section, text: text) }
       let(:comment_subject) { FactoryBot.create(:annotation, text_section: text_section) }
-      the_subject_behaves_like "instance abilities", Comment, none: true
+      the_subject_behaves_like "instance abilities", Comment, { create: true, read: true, update: false, delete: false}
     end
   end
 end
