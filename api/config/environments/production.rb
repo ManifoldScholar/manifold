a5ef6a83267fbad2174594acfc35608b57158878
Zach Davis <zach@castironcoding.com>
2020-09-29 09:49:43 -0700

    [F] Upgrade Puma to v5 and DRY up config

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
