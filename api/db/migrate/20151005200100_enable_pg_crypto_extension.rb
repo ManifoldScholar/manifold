91e44514d1f3b4bdc65023570089f9ea707ab676
Zach Davis <zach@castironcoding.com>
2020-10-29 10:28:45 -0700

    [B] Correct migrations

--- api/db/migrate/20151005200100_enable_pg_crypto_extension.rb
+++ api/db/migrate/20151005200100_enable_pg_crypto_extension.rb
@@ -1,11 +1,7 @@
 class EnablePgCryptoExtension < ActiveRecord::Migration[5.2]
 
-  def up
-    enable_extension("pgcrypto") unless extensions.include?("pgcrypto")
-  end
-
-  def down
-    disable_extension("pgcrypto") if extensions.include?("pgcrypto")
+  def change
+    enable_extension("pgcrypto")
   end
 
 end
