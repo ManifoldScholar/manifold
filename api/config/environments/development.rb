de3c2a9aad88ed157016e953d30e06695ae2ea14
Alexa Grey <alexa@castironcoding.com>
2020-12-02 13:57:18 -0800

    [C] Remove redis-rails
    Resolves #2876

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -14,16 +14,16 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp", "caching-dev.txt").exist?
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
+  #if Rails.root.join("tmp", "caching-dev.txt").exist?
+    #config.cache_store = :memory_store
+    #config.public_file_server.headers = {
+      #"Cache-Control" => "public, max-age=#{2.days.to_i}"
+    #}
+  #else
+    #config.action_controller.perform_caching = false
 
-    config.cache_store = :null_store
-  end
+    #config.cache_store = :null_store
+  #end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
