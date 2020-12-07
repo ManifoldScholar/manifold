7ff514ee19f4acd4bc78b9fb6e715bbe7c70761b
Alexa Grey <alexa@castironcoding.com>
2020-12-06 21:45:19 -0800

    [F] Configure Resource Block title and description
    Resolves #2882

--- api/app/serializers/v1/content/resources_block_serializer.rb
+++ api/app/serializers/v1/content/resources_block_serializer.rb
@@ -1,11 +1,12 @@
 module V1
   module Content
     class ResourcesBlockSerializer < ContentBlockSerializer
-      # TODO: Address these dynamic attributes
-      attributes(*::Content::ResourcesBlock.available_attributes)
+      typed_attribute :title, Types::String
+      typed_attribute :description, Types::String
+      typed_attribute :show_all_collections, Types::Bool
+      typed_attribute :show_descriptions, Types::Bool
 
       typed_has_many :featured_collections, record_type: "resourceCollection"
-
     end
   end
 end
