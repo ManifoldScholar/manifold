6d9b696cf0b854887c014d93dc08339cc883d355
Alexa Grey <alexa@castironcoding.com>
2021-02-09 07:45:04 -0800

    [C] Improve performance when not using spring

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -36,5 +36,5 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ENV["DISABLE_SPRING"].present? ? ActiveSupport::FileUpdateChecker : ActiveSupport::EventedFileUpdateChecker
 end
