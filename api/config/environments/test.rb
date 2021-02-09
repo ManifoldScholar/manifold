db6e672c47bbcf453769c0d2229ac0be5211be67
Alexa Grey <alexa@castironcoding.com>
2021-02-09 07:45:04 -0800

    [C] Improve performance when not using spring

--- api/config/environments/test.rb
+++ api/config/environments/test.rb
@@ -6,7 +6,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.cache_classes = false
+  config.cache_classes = ENV["DISABLE_SPRING"].present?
   config.action_view.cache_template_loading = true
 
   # Do not eager load code on boot. This avoids loading your whole application
