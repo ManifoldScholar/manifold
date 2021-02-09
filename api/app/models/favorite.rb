22ab3294cdc9a5d5895e54beef06182c0a136c46
Alexa Grey <alexa@castironcoding.com>
2021-02-09 07:42:35 -0800

    [F] Refactor Favorite to use Collections API

--- api/app/models/favorite.rb
+++ api/app/models/favorite.rb
@@ -1,40 +1,41 @@
-# A favorite represents a user's decision to favorite an object, generally a project.
+# This model exists for legacy reasons, and is simply a view that remaps fields from {UserCollectedCompositeEntry},
+# and also will infer a {Project} based on its `#favoritable` model.
 class Favorite < ApplicationRecord
-
-  # Authority
   include Authority::Abilities
   include SerializedAbilitiesFor
+  include View
 
-  # Associations
+  self.primary_key = :id
+
   belongs_to :user
   belongs_to :favoritable, polymorphic: true
-  validates :favoritable_id, uniqueness: { scope: [:user_id, :favoritable_type] }
-  validates :user, presence: true
+
+  belongs_to :project, optional: true
+
+  has_many :subjects, through: :project
 
   # This makes the favorite authorizer a bit simpler.
   alias_attribute :creator, :user
 
-  # Scopes
-  scope :only_projects, -> { where(favoritable_type: "Project") }
-  scope :only_texts, -> { where(favoritable_type: "Text") }
+  # @return [UserCollectedEntry]
+  def user_collected_entry
+    entry_klass = collectable_definition.entry.klass
 
-  def project
-    return favoritable if favoritable.instance_of?(Project)
-    return favoritable.project if favoritable.respond_to?(:project)
+    collectable_foreign_key = collectable_definition.associations.collectable.foreign_key
 
-    nil
+    entry_klass.where(user: user, collectable_foreign_key => favoritable_id).first!
   end
 
   def to_s
     "favorite #{id}"
   end
 
-  def favorite_subjects
-    user.favorites
-      .where(favoritable_type: "Project")
-      .joins("JOIN projects ON favorites.favoritable_id = projects.id")
-      .joins("JOIN project_subjects ON project_subjects.project_id = projects.id")
-      .pluck(:subject_id, :favoritable_id)
+  # @return [<String>]
+  def favorite_subject_ids
+    subjects.loaded? ? subjects.map(&:id) : subject_ids
   end
 
+  def collectable_definition
+    @collectable_definition ||= Collections::Mapping[User][favoritable_type]
+  end
 end
