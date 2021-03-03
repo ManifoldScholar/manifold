30a20ddb804eb18375e9f0e9fc5244707767f6b4
Dana Johnson <dana@castironcoding.com>
2021-03-03 15:24:49 -0800

    [F] Add Collecting.Toggle to frontend, reader views
    Adds to:
    * project hero
    * resources
    * resource collections
    * search results
    * reader title bar
    * reader TOC

--- client/src/frontend/components/resource/Title.js
+++ client/src/frontend/components/resource/Title.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import FormattedDate from "global/components/FormattedDate";
 import IconComputed from "global/components/icon-computed";
+import Collecting from "frontend/components/collecting";
 
 export default class ResourceTitle extends Component {
   static displayName = "Resource.Title";
@@ -23,12 +24,17 @@ export default class ResourceTitle extends Component {
     return (
       <div className="resource-title">
         {this.props.showIcon ? (
-          <figure className={`resource-icon ${attr.kind}`}>
+          <figure className={`resource-title__icon ${attr.kind}`}>
             <IconComputed.Resource icon={attr.kind} size={60} />
           </figure>
         ) : null}
         <div>
-          <h1 dangerouslySetInnerHTML={{ __html: attr.titleFormatted }} />
+          <div className="resource-title__title-and-toggle">
+            <h1 className="resource-title__title" dangerouslySetInnerHTML={{ __html: attr.titleFormatted }} />
+            <span className="resource-title__collecting-toggle">
+              <Collecting.Toggle collectable={this.props.resource} />
+            </span>
+          </div>
           {this.props.showDate ? (
             <span className="resource-date">
               {"Resource added "}
