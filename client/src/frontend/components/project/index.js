5588c6d5b9149a220f58983ef69557436f2ab1c3
Dana Johnson <dana@castironcoding.com>
2021-03-05 09:20:06 -0800

    [B] Move Project.CoverButton to backend
    And refactor (only used in backend PC view now)

--- client/src/frontend/components/project/index.js
+++ client/src/frontend/components/project/index.js
@@ -1,13 +1,11 @@
 import Detail from "./Detail";
 import Events from "./Events";
 import Hero from "./Hero";
-import Follow from "./Follow";
 import Resources from "./Resources";
 
 export default {
   Detail,
   Events,
   Hero,
-  Follow,
   Resources
 };
