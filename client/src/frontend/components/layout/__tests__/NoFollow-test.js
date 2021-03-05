6abdf52221ab0f5ad4e15d19bd569c66ced318c5
Dana Johnson <dana@castironcoding.com>
2021-03-05 08:18:31 -0800

    [C] Remove client code for following projects

--- client/src/frontend/components/layout/__tests__/NoFollow-test.js
+++
@@ -1,9 +0,0 @@
-import NoFollow from "../NoFollow";
-
-describe("frontend/components/layout/NoFollow", () => {
-  def("root", () => <NoFollow />);
-
-  it("matches the snapshot when rendered", () => {
-    expect(render($withApp($root)).html()).toMatchSnapshot();
-  });
-});
--- client/src/frontend/components/project-list/index.js
+++ client/src/frontend/components/project-list/index.js
@@ -1,11 +1,9 @@
 import Grid from "./Grid";
 import Filters from "./Filters";
-import Following from "./Following";
 import Placeholder from "./Placeholder";
 
 export default {
   Grid,
   Filters,
-  Following,
   Placeholder
 };
