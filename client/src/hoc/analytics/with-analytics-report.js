6dc7fadd1543ff39b44506ae83343f47dffd457c
Tim Frazee <tim@castironcoding.com>
2021-03-05 13:56:22 -0800

    [C] Lint with-analytics-report.js

--- client/src/hoc/analytics/with-analytics-report.js
+++ client/src/hoc/analytics/with-analytics-report.js
@@ -105,9 +105,13 @@ export default function withAnalyticsReport(WrappedComponent) {
       dispatch(analyticsRequest);
     };
 
-    formatDateForFetch = (date) => {
-      return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
-    }
+    formatDateForFetch = date => {
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const day = String(date.getDate()).padStart(2, "0");
+
+      return `${year}-${month}-${day}`;
+    };
 
     analyticsPaginationClickHandler = page => {
       return () => {
