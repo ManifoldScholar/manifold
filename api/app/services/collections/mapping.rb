dc67e54776be15bad5d287a9c20ee4e9a56f73f6
Alexa Grey <alexa@castironcoding.com>
2021-03-09 18:19:16 -0800

    [F] Make text sections collectable

--- api/app/services/collections/mapping.rb
+++ api/app/services/collections/mapping.rb
@@ -2,8 +2,8 @@
 module Collections
   Mapping = MultiKeyable::Map.from(
     [
-      Collections::Definition.new("ReadingGroup", grouping: "ReadingGroupCategory", collects: %w[Project Resource ResourceCollection Text], reorderable: true),
-      Collections::Definition.new("User", entry_prefix: "UserCollected", collects: %w[Project Resource ResourceCollection Text], reorderable: false)
+      Collections::Definition.new("ReadingGroup", grouping: "ReadingGroupCategory", collects: %w[Project Resource ResourceCollection Text TextSection], reorderable: true),
+      Collections::Definition.new("User", entry_prefix: "UserCollected", collects: %w[Project Resource ResourceCollection Text TextSection], reorderable: false)
     ]
   )
 end
