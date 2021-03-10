304ed17319e1f15da720aa28c42616e6a3a788f5
Alexa Grey <alexa@castironcoding.com>
2021-03-09 17:50:31 -0800

    [C] Return unique ID for composed collections

--- api/app/models/user_collection.rb
+++ api/app/models/user_collection.rb
@@ -1,7 +1,7 @@
 class UserCollection < ApplicationRecord
   include ComposedCollection
 
-  self.primary_key = :user_id
+  self.primary_key = :id
 
   belongs_to :user, inverse_of: :user_collection
 end
