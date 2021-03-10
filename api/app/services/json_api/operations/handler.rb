783003bc808cc30d5373e1548a7261c9968abd61
Alexa Grey <alexa@castironcoding.com>
2021-03-10 10:15:46 -0800

    [C] Serialize collections

--- api/app/services/json_api/operations/handler.rb
+++ api/app/services/json_api/operations/handler.rb
@@ -34,7 +34,7 @@ module JSONAPI
             operator = operator_klass.new operator_params
 
             operator.call.fmap do |data|
-              { data: data }
+              yield maybe_serialize data
             end.or do |err|
               unroll = UnrollErrors.new
 
@@ -103,6 +103,60 @@ module JSONAPI
         errors = [error]
 
         Failure[:operation_failure, errors]
+      end
+
+      def maybe_serialize(data)
+        serializer_klass = find_serializer_klass_for data
+
+        return serialize_raw(data) unless serializer_klass.present?
+
+        options = build_serializer_options
+
+        serializer = serializer_klass.new(data, options)
+
+        Success serializer.serializable_hash
+      end
+
+      def find_serializer_klass_for(data)
+        return nil unless data.is_a?(ApplicationRecord)
+
+        possible_serializer_name = "V1::#{data.model_name}Serializer"
+
+        possible_serializer_name.safe_constantize
+      end
+
+      def serialize_raw(data)
+        serialized = yield serialize_raw_json data.as_json
+
+        wrapped = { data: serialized }
+
+        Success wrapped
+      end
+
+      def serialize_raw_json(json)
+        case json
+        when Hash
+          json.deep_transform_keys { |key| key.to_s.camelize(:lower) }
+        when Array
+          json.map { |elt| yield serialize_raw_json elt.as_json }
+        else
+          json
+        end.then do |value|
+          Success value
+        end
+      end
+
+      def build_serializer_options
+        {
+          params: build_serializer_params
+        }
+      end
+
+      def build_serializer_params
+        {
+          authority_user: current_user,
+          current_user: current_user,
+        }
       end
     end
     # rubocop:enable Style/MultilineBlockChain
