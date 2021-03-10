f802fea39b4a54ae3bd503aa62c77e069cd75930
Alexa Grey <alexa@castironcoding.com>
2021-03-09 18:31:07 -0800

    [C] Allow router object to work in CI

--- api/app/services/router_object.rb
+++ api/app/services/router_object.rb
@@ -4,8 +4,8 @@ class RouterObject
   include Rails.application.routes.url_helpers
   include Sliceable
 
-  option :host, Types::String, default: proc { Rails.application.config.manifold.domain }
-  option :protocol, Types::String, default: proc { Rails.application.config.manifold.protocol }
+  option :host, Types::String, default: proc { Rails.application.config.manifold.domain.presence || "example.com" }
+  option :protocol, Types::String, default: proc { Rails.application.config.manifold.protocol.presence || "http" }
 
   def default_url_options
     slice(:host, :protocol)
