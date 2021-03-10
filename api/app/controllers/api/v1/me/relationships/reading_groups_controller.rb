7628bf8b6a9bce0937e7fb08a4c3bb92947ecbd1
Zach Davis <zach@castironcoding.com>
2021-03-10 11:44:06 -0800

    [B] Ensure that serialized RGs include collection

--- api/app/controllers/api/v1/me/relationships/reading_groups_controller.rb
+++ api/app/controllers/api/v1/me/relationships/reading_groups_controller.rb
@@ -5,6 +5,8 @@ module API
         # Reading groups controller
         class ReadingGroupsController < ApplicationController
 
+          SERIALIZED_INCLUDES = %i[collection annotated_texts reading_group_memberships.user].freeze
+
           resourceful! ReadingGroup do
             base = current_user.nil? ? ReadingGroup.none : current_user.reading_groups
             base.with_order.page(page_number).per(page_size)
@@ -12,7 +14,7 @@ module API
 
           def index
             @reading_groups = load_reading_groups
-            render_multiple_resources @reading_groups, include: ["annotated_texts", "reading_group_memberships.user"]
+            render_multiple_resources @reading_groups, include: SERIALIZED_INCLUDES
           end
 
         end
