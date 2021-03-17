0a51efd4d96ed2bc2fda3e80c344cce2896779d9
Leonor Colbert <leonor@castironcoding.com>
2021-03-17 15:07:25 -0700

    [F] Implement translation in Placeholder.js

--- client/src/frontend/components/project-list/Placeholder.js
+++ client/src/frontend/components/project-list/Placeholder.js
@@ -4,6 +4,7 @@ import Authorize from "hoc/authorize";
 import Utility from "global/components/utility";
 import { Link } from "react-router-dom";
 import lh from "helpers/linkHandler";
+import { Translation } from "react-i18next";
 
 export default class ProjectListPlaceholder extends React.PureComponent {
   get actions() {
@@ -24,18 +25,18 @@ export default class ProjectListPlaceholder extends React.PureComponent {
     const helpLink =
       "https://manifoldscholar.github.io/manifold-docusaurus/docs/backend/projects";
     return (
-      <p>
-        {
-          "But it’s easy to create and publish projects with Manifold. If you have backend access, "
-        }
-        <Link to={lh.link("backend")}>head to the backend</Link>
-        {" and select "}
-        <em>Add a New Project</em>
-        {
-          ". For more help, you can learn about creating and publishing Manifold Projects "
-        }
-        <a href={helpLink}>here</a>.
-      </p>
+      <Translation>
+        {t => (
+          <p>
+            {t(`but-its-easy`)}
+            <Link to={lh.link("backend")}>{t(`head-to-the-backend`)}</Link>
+            {t(`and-select`)}
+            <em>{t(`add-a-new-project`)}</em>
+            {t(`for-more-help`)}
+            <a href={helpLink}>{t(`here`)}</a>.
+          </p>
+        )}
+      </Translation>
     );
   }
 
@@ -46,43 +47,47 @@ export default class ProjectListPlaceholder extends React.PureComponent {
     };
 
     return (
-      <section className="bg-neutral05" style={wrapperStyle}>
-        <div className="container">
-          <ContentPlaceholder.Wrapper context="frontend">
-            <ContentPlaceholder.Title>
-              <Authorize entity="projectCollection" ability="create">
-                Uh-oh. This Manifold library is empty.
-              </Authorize>
-              <Authorize
-                entity="project"
-                ability="create"
-                successBehavior="hide"
-              >
-                This Manifold library is empty.
-              </Authorize>
-            </ContentPlaceholder.Title>
-            <ContentPlaceholder.Body>
-              <>
-                <Authorize entity="project" ability="create">
-                  {this.renderAdminMessage()}
-                </Authorize>
-                <Authorize
-                  entity="project"
-                  ability="create"
-                  successBehavior="hide"
-                >
-                  <p>Please check back soon!</p>
-                </Authorize>
-                <Utility.IconComposer
-                  icon="BooksOnShelfColorUnique"
-                  size={205}
-                />
-              </>
-            </ContentPlaceholder.Body>
-            <ContentPlaceholder.Actions actions={this.actions} />
-          </ContentPlaceholder.Wrapper>
-        </div>
-      </section>
+      <Translation>
+        {t => (
+          <section className="bg-neutral05" style={wrapperStyle}>
+            <div className="container">
+              <ContentPlaceholder.Wrapper context="frontend">
+                <ContentPlaceholder.Title>
+                  <Authorize entity="projectCollection" ability="create">
+                    {t(`uh-oh-library-empty`)}
+                  </Authorize>
+                  <Authorize
+                    entity="project"
+                    ability="create"
+                    successBehavior="hide"
+                  >
+                    {t(`library-empty`)}
+                  </Authorize>
+                </ContentPlaceholder.Title>
+                <ContentPlaceholder.Body>
+                  <>
+                    <Authorize entity="project" ability="create">
+                      {this.renderAdminMessage()}
+                    </Authorize>
+                    <Authorize
+                      entity="project"
+                      ability="create"
+                      successBehavior="hide"
+                    >
+                      <p>{t(`check-back-soon`)}</p>
+                    </Authorize>
+                    <Utility.IconComposer
+                      icon="BooksOnShelfColorUnique"
+                      size={205}
+                    />
+                  </>
+                </ContentPlaceholder.Body>
+                <ContentPlaceholder.Actions actions={this.actions} />
+              </ContentPlaceholder.Wrapper>
+            </div>
+          </section>
+        )}
+      </Translation>
     );
   }
 }
