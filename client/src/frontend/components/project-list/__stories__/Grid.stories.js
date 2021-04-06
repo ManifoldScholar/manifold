b0fe99b622affde14f817812f8aa8515c60b1d5e
Dana Johnson <dana@castironcoding.com>
2021-04-06 16:49:20 -0700

    [C] Add ProjectList.Grid to storybook

---
+++ client/src/frontend/components/project-list/__stories__/Grid.stories.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { storiesOf, fixtures } from "helpers/storybook/exports";
+import { number } from "@storybook/addon-knobs";
+import ProjectGrid from "../Grid";
+
+const projects = fixtures.collectionFactory("project", 12);
+const pagination = fixtures.pagination();
+
+function Context(props) {
+  return <div className="browse">{props.children}</div>;
+}
+
+storiesOf("Frontend/Projects", module)
+  .add("Grid", () => {
+    const limit = number("Project limit", 12);
+
+    return (
+      <Context>
+        <ProjectGrid
+          projects={projects}
+          pagination={pagination}
+          paginationClickHandler={() => null}
+          limit={limit}
+        />
+      </Context>
+    );
+  })
