be1e67e2d4c58291b80a99e50fff808eb1e17890
Zach Davis <zach@castironcoding.com>
2021-04-08 11:49:07 -0700

    [B] Reader allows code elements to render
    Resolves #2887

--- client/src/reader/components/section/body-nodes/nodes/index.js
+++ client/src/reader/components/section/body-nodes/nodes/index.js
@@ -1,11 +1,9 @@
-import Code from "./Code";
 import Link from "./Link";
 import Default from "./Default";
 import Text from "./Text";
 import Iframe from "./Iframe";
 
 export default {
-  Code,
   Link,
   Default,
   Text,
