3aeb923cc38717c4341480a7bd054e422e104446
Alexa Grey <alexa@castironcoding.com>
2021-04-21 18:09:15 -0700

    [C] Add proxy `Favorite` factory
    Defers to creating the appropriate user-collected object instead

---
+++ api/spec/factories/favorites.rb
@@ -0,0 +1,25 @@
+FactoryBot.define do
+  factory :favorite do
+    initialize_with do
+      collectable_associations = Collections::Mapping[User][favoritable].associations
+
+      entry_factory = collectable_associations.entry.singular
+
+      favoritable_key = collectable_associations.collectable.singular
+
+      attrs = { user: user, favoritable_key => favoritable, created_at: created_at, updated_at: updated_at }
+
+      entry = create(entry_factory, attrs)
+
+      entry.favorite
+    end
+
+    skip_create
+
+    association(:favoritable) { FactoryBot.create(:project) }
+    association(:user) { FactoryBot.create(:user) }
+
+    created_at { Time.current }
+    updated_at { created_at }
+  end
+end
