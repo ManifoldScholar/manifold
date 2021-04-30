294a47580f0ccb119218fa1df9da94e740e01ae2
Zach Davis <zach@castironcoding.com>
2021-04-30 15:03:22 -0700

    [F] Upgrade Ruby to 2.7

--- api/spec/rails_helper.rb
+++ api/spec/rails_helper.rb
@@ -98,20 +98,27 @@ RSpec.configure do |config|
     /googleapis\.com/
   ]
 
-  config.around(:each, elasticsearch: true) do |example|
-    WebMock.allow_net_connect!
-    Searchkick.callbacks(nil) do
-      example.run
-    end
-    WebMock.disable_net_connect!(allow: allowed_net_connect)
-  end
+  # config.around(:each, elasticsearch: true) do |example|
+  #   WebMock.allow_net_connect!
+  #   Searchkick.callbacks(nil) do
+  #     example.run
+  #   end
+  #   WebMock.disable_net_connect!(allow: allowed_net_connect)
+  # end
 
   # Allow elastic search for tests tagged with elasticsearch
+
   config.around(:example) do |example|
-    unless example.metadata[:elasticsearch] == false
-      WebMock.disable_net_connect!(allow: allowed_net_connect)
-      example.run
+    disable_web_connect = true
+    disable_web_connect = false if example.metadata[:elasticsearch]
+
+    if disable_web_connect
+      WebMock.disable_net_connect!(allow: allowed_net_connect) if disable_web_connect
+    else
+      WebMock.allow_net_connect!
     end
+
+    example.run
   end
 
 end
