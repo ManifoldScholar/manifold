09b28bda77979533395097340390eec5635a6ea1
Alexa Grey <alexa@castironcoding.com>
2021-05-07 16:15:17 -0700

    [C] Add pagination to public_reading_groups

--- api/app/controllers/api/v1/public_reading_groups_controller.rb
+++ api/app/controllers/api/v1/public_reading_groups_controller.rb
@@ -1,14 +1,26 @@
 module API
   module V1
     class PublicReadingGroupsController < ApplicationController
+      SERIALIZED_INCLUDES = %i[kind collection].freeze
+      EAGER_LOADS = [
+        :reading_group_collection, :reading_group_kind,
+        { reading_group_memberships: %i[user] }
+      ].freeze
+
       resourceful! ReadingGroup, authorize_options: { except: [:index, :show] } do
-        ReadingGroup.visible_to_public
+        ReadingGroup.includes(*EAGER_LOADS).visible_to_public.filtered(filter_options)
       end
 
       def index
         @reading_groups = load_reading_groups
 
-        render_multiple_resources @reading_groups
+        render_multiple_resources @reading_groups, include: SERIALIZED_INCLUDES
+      end
+
+      private
+
+      def filter_options
+        with_pagination!({})
       end
     end
   end
