c8588763b5dd09c8d8d4b3f864d6d064c2a73f57
Dana Johnson <dana@castironcoding.com>
2021-05-11 12:25:35 -0700

    [B] Fix misc client bugs

--- client/src/frontend/components/collecting/Dialog/Checkbox.js
+++ client/src/frontend/components/collecting/Dialog/Checkbox.js
@@ -1,17 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { useUID } from "react-uid";
-import {
-  TransitionGroup as ReactTransitionGroup,
-  CSSTransition
-} from "react-transition-group";
 import IconComposer from "global/components/utility/IconComposer";
-
-const transitionProps = {
-  mountOnEnter: true,
-  classNames: "collecting-dialog-checkbox__icon",
-  timeout: { enter: 0, exit: 0 }
-};
 
 function CollectingDialogCheckbox({
   label,
@@ -21,8 +11,20 @@ function CollectingDialogCheckbox({
   showLock
 }) {
   const uid = useUID();
+  const [assumeChecked, setAssumeChecked] = useState(checked);
+
+  useEffect(() => {
+    setAssumeChecked(checked);
+  }, [checked]);
+
   const [hovering, setHovering] = useState(false);
-  const icon = checked || hovering ? "StarFillUnique" : "StarOutlineUnique";
+  const icon =
+    assumeChecked || hovering ? "StarFillUnique" : "StarOutlineUnique";
+
+  function handleChange(event) {
+    setAssumeChecked(event.target.checked); // update checkbox UI immediately, rather than wait for API response
+    onChange(event);
+  }
 
   return (
     <label
@@ -37,20 +39,16 @@ function CollectingDialogCheckbox({
         name="collecting-dialog"
         value={value}
         checked={checked}
-        onChange={onChange}
+        onChange={handleChange}
         className="collecting-dialog-checkbox__input"
       />
       <span className="collecting-dialog-checkbox__label">
         <span className="collecting-dialog-checkbox__icon-block">
-          <ReactTransitionGroup>
-            <CSSTransition key={icon} {...transitionProps}>
-              <IconComposer
-                icon={icon}
-                size="default"
-                iconClass="collecting-dialog-checkbox__icon"
-              />
-            </CSSTransition>
-          </ReactTransitionGroup>
+          <IconComposer
+            icon={icon}
+            size="default"
+            iconClass="collecting-dialog-checkbox__icon"
+          />
         </span>
         <span className="collecting-dialog-checkbox__title">{label}</span>
         {showLock && (
