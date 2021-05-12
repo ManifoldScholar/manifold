8452d55e9eee03fb096cc31a01dfaa8660405617
Alexa Grey <alexa@castironcoding.com>
2021-05-11 21:47:19 -0800

    [B] Correct issues with joining a public RG
    Resolves PROJ-362

--- api/app/operations/reading_groups/join_public.rb
+++ api/app/operations/reading_groups/join_public.rb
@@ -9,19 +9,25 @@ module ReadingGroups
       extend Dry::Initializer
 
       include Dry::Monads[:result, :do]
+      include MonadicJSONAPIErrors
+      include MonadicPersistence
 
       option :reading_group, model: "ReadingGroup"
       option :user, model: "User", optional: true
 
       def call(*)
         yield validate
+
+        rgm = reading_group.reading_group_memberships.build user: user
+
+        monadic_save rgm
       end
 
       private
 
       def validate
         return forbidden_jsonapi_error(code: "not_signed_in", status: :forbidden, title: "Must be authenticated") unless user.present?
-        return forbidden_jsonapi_error(code: "not_public", status: :forbidden, title: "Not Available") unless reading_group.present?
+        return forbidden_jsonapi_error(code: "not_public", status: :forbidden, title: "Not Available") unless reading_group.present? && reading_group.public?
         return jsonapi_error(code: "already_joined", title: "Already a member") if membership_exists?
 
         Success true
