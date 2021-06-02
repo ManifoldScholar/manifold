554b11197098679bbe9b6c88338e4b1d3784dc3b
Zach Davis <zach@castironcoding.com>
2021-06-02 16:24:58 -0700

    [F] Source GCS credentials from env, then DB
    When Manifold is deployed in a k8s environment, we want to be able to ensure that the
    GCS credentials given to it in the environment will always supersede credentials stored
    in the database on the Settings object. This change also allows services like sidekiq
    to get the correct settings before the database is updated from the environment, which
    matters, because these credentails are needed very early in the initialization process,
    when the Shrine storage is setup.

--- api/lib/factory/drive_session.rb
+++ api/lib/factory/drive_session.rb
@@ -4,7 +4,11 @@ module Factory
 
     # rubocop:disable Metrics/AbcSize
     # rubocop:disable Metrics/MethodLength
+    # rubocop:disable Metrics/CyclomaticComplexity
+    # rubocop:disable Metrics/PerceivedComplexity
     def self.config
+      env_config = Settings::ReadFromEnv.run.result
+
       db_config = ActiveRecord::Base.connection_pool.with_connection do |conn|
         if conn.table_exists? "settings"
           Rails.application.executor.wrap do
@@ -24,16 +28,18 @@ module Factory
       db_config = db_config.nil? ? {} : db_config
 
       client_x509_cert_url = nil
+
       if Rails.configuration.manifold.google.client_x509_cert_url && db_config["google_client_email"]
-        client_x509_cert_url = Rails.configuration.manifold.google.client_x509_cert_url + db_config["google_client_email"]
+        client_x509_cert_url = Rails.configuration.manifold.google.client_x509_cert_url +
+                               (env_config.dig(:integrations, :google_client_email) || db_config["google_client_email"])
       end
       {
         "type": "service_account",
-        "project_id": db_config["google_project_id"],
-        "private_key_id": db_config["google_private_key_id"],
-        "private_key": db_config["google_private_key"],
-        "client_email": db_config["google_client_email"],
-        "client_id": db_config["google_client_id"],
+        "project_id": env_config.dig(:integrations, :google_project_id) || db_config["google_project_id"],
+        "private_key_id": env_config.dig(:integrations, :google_private_key_id) || db_config["google_private_key_id"],
+        "private_key": env_config.dig(:secrets, :google_private_key) || db_config["google_private_key"],
+        "client_email": env_config.dig(:integrations, :google_client_email) || db_config["google_client_email"],
+        "client_id": env_config.dig(:integrations, :google_client_id) || db_config["google_client_id"],
         "auth_uri": Rails.configuration.manifold.google.auth_uri,
         "token_uri": Rails.configuration.manifold.google.token_uri,
         "auth_provider_x509_cert_url": Rails.configuration.manifold.google.auth_provider_x509_cert_url,
@@ -42,6 +48,8 @@ module Factory
     end
     # rubocop:enable Metrics/AbcSize
     # rubocop:enable Metrics/MethodLength
+    # rubocop:enable Metrics/CyclomaticComplexity
+    # rubocop:enable Metrics/PerceivedComplexity
 
     def self.create_service_account_session
       fetched_config = config
