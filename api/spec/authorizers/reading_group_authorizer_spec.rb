b337f22b590df00c1068cd6c1f49b7cdcb6dcbca
Alexa Grey <alexa@castironcoding.com>
2021-06-08 11:25:18 -0800

    [C] Add tests to ensure RGM members can only read
    Related to PROJ-384

--- api/spec/authorizers/reading_group_authorizer_spec.rb
+++ api/spec/authorizers/reading_group_authorizer_spec.rb
@@ -24,6 +24,24 @@ RSpec.describe "Reading Group Abilities", :authorizer do
       let(:object) { FactoryBot.create :reading_group, privacy: "public" }
       the_subject_behaves_like "instance abilities", ReadingGroup, read_only: true
     end
+
+    context "when the subject is a moderator" do
+      let(:object) { FactoryBot.create :reading_group }
+      let(:user) { FactoryBot.create :user }
+
+      let!(:membership) { FactoryBot.create :reading_group_membership, :moderator, user: user, reading_group: object }
+
+      the_subject_behaves_like "instance abilities", ReadingGroup, read: true, create: false, update: true, delete: false
+    end
+
+    context "when the subject is a member" do
+      let(:object) { FactoryBot.create :reading_group }
+      let(:user) { FactoryBot.create :user }
+
+      let!(:membership) { FactoryBot.create :reading_group_membership, :member, user: user, reading_group: object }
+
+      the_subject_behaves_like "instance abilities", ReadingGroup, read_only: true
+    end
   end
 
   context "when reading groups are disabled" do
