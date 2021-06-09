293066e04e3e7b64fc1b4ac2984ac06ffb917115
Alexa Grey <alexa@castironcoding.com>
2021-06-09 09:03:34 -0700

    [C] Expose if text is collected
    Resolves PROJ-400

--- api/app/serializers/v1/text_serializer.rb
+++ api/app/serializers/v1/text_serializer.rb
@@ -35,16 +35,20 @@ module V1
         anchor: Types::String,
         type: Types::String.optional,
         id: Types::Serializer::ID,
+        collected: Types::Bool.optional,
         children: Types::Array.of(
           Types::Hash.schema(
             label: Types::String,
             anchor: Types::String,
             type: Types::String.optional,
-            id: Types::Serializer::ID
+            id: Types::Serializer::ID,
+            collected: Types::Bool.optional
           )
         )
       )
-    ).meta(read_only: true), if: INCLUDE_TOC
+    ).meta(read_only: true), if: INCLUDE_TOC do |object, params|
+      object.toc_with_collected_for(params[:current_user])
+    end
 
     typed_attribute :toc_section_id, Types::String.optional.meta(read_only: true), if: INCLUDE_TOC
 
