26055d8e7366792019322ba1d2342d5afde897ac
Dana Johnson <dana@castironcoding.com>
2021-06-15 14:20:54 -0700

    [B] Ensure page content fills viewport

--- client/src/reader/containers/Reader/index.js
+++ client/src/reader/containers/Reader/index.js
@@ -244,46 +244,44 @@ export class ReaderContainer extends Component {
     return (
       <BodyClass className={this.bodyClass}>
         <ReaderContext.Provider value={this.props.text}>
-          <div>
-            <EventTracker
-              event={EVENTS.VIEW_RESOURCE}
-              resource={this.props.text}
-            />
-            <CheckFrontendMode
-              debugLabel="ReaderWrapper"
-              project={this.props.text.relationships.project}
-            />
-            <ScrollAware>
-              {/* Header inside scroll-aware HOC */}
-              <Header
-                // Props required by body component
-                text={this.props.text}
-                section={this.props.section}
-                authentication={this.props.authentication}
-                visibility={this.props.visibility}
-                location={this.props.location}
-                appearance={this.props.appearance}
-                notifications={this.props.notifications}
-                commonActions={this.commonActions}
-                history={this.props.history}
-                match={this.props.match}
-                {...this.readerActions}
-              />
-            </ScrollAware>
-            <Toc
+          <EventTracker
+            event={EVENTS.VIEW_RESOURCE}
+            resource={this.props.text}
+          />
+          <CheckFrontendMode
+            debugLabel="ReaderWrapper"
+            project={this.props.text.relationships.project}
+          />
+          <ScrollAware>
+            {/* Header inside scroll-aware HOC */}
+            <Header
+              // Props required by body component
               text={this.props.text}
               section={this.props.section}
-              tocDrawerVisible={this.props.visibility.uiPanels.tocDrawer}
-              hideTocDrawer={this.hideTocDrawer}
-              showMeta={this.toggleMeta}
+              authentication={this.props.authentication}
+              visibility={this.props.visibility}
+              location={this.props.location}
+              appearance={this.props.appearance}
+              notifications={this.props.notifications}
+              commonActions={this.commonActions}
+              history={this.props.history}
+              match={this.props.match}
+              {...this.readerActions}
             />
-            <main id="skip-to-main">
-              {this.maybeRenderOverlay(this.props)}
-              {this.renderRoutes()}
-            </main>
-            <Footers.ReaderFooter text={this.props.text} />
-            <Layout.PostFooter />
-          </div>
+          </ScrollAware>
+          <Toc
+            text={this.props.text}
+            section={this.props.section}
+            tocDrawerVisible={this.props.visibility.uiPanels.tocDrawer}
+            hideTocDrawer={this.hideTocDrawer}
+            showMeta={this.toggleMeta}
+          />
+          <main id="skip-to-main" className="main-content flex-viewport">
+            {this.maybeRenderOverlay(this.props)}
+            {this.renderRoutes()}
+          </main>
+          <Footers.ReaderFooter text={this.props.text} />
+          <Layout.PostFooter />
         </ReaderContext.Provider>
       </BodyClass>
     );
