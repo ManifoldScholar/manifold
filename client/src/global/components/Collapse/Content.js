fa74a29b8f2a721181a78a2c5267d584eae16bda
Lauren Davidson <lauren@castironcoding.com>
2021-09-10 14:53:50 -0400

    [WIP] Make height animation symmetrical
    [WIP] Continue finessing Collapse animation
    
    [WIP] Move styles to css
    
    [WIP] CSS math
    
    [WIP] Add js function for animation math

--- client/src/global/components/Collapse/Content.js
+++ client/src/global/components/Collapse/Content.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import useResizeObserver from "use-resize-observer";
 import { useCollapseContext } from "hooks";
+import classNames from "classnames";
+
+const getAnimationParams = height => {
+  const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+  const heightToMs = (clamp(height / 700, 0.3, 1) * 1000).toFixed(0);
+  const delay = clamp(heightToMs / 10, 25, 75).toFixed(0);
+  const diff = heightToMs - delay;
+  return { heightToMs, delay, diff };
+};
 
 function Content({ children, className }) {
   const { visible, contentProps } = useCollapseContext();
   const { ref: resizeRef, height } = useResizeObserver();
-  const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
-  const heightFactor = clamp(height / 400, 0.4, 1) * 1000;
+
+  const finalClassName = classNames({
+    collapse__content: true,
+    "collapse__content--visible": visible,
+    "collapse__content--hidden": !visible,
+    [className]: className
+  });
+
+  const contentRef = useRef();
+
+  useLayoutEffect(() => {
+    if (contentRef.current) {
+      const { heightToMs, delay, diff } = getAnimationParams(height);
+      contentRef.current.style.setProperty("--height", `${height}px`);
+      contentRef.current.style.setProperty("--heightToMs", `${heightToMs}ms`);
+      contentRef.current.style.setProperty("--delay", `${delay}ms`);
+      contentRef.current.style.setProperty("--diff", `${diff}ms`);
+    }
+  }, [height]);
 
   return (
-    <div
-      {...contentProps}
-      className={className}
-      style={{
-        height: visible ? height : 0,
-        opacity: visible ? 1 : 0,
-        visibility: visible ? "visible" : "hidden",
-        transition: `height ${heightFactor}ms, opacity ${heightFactor}ms ease-out, ${heightFactor}ms`,
-        overflow: "hidden"
-      }}
-    >
+    <div {...contentProps} ref={contentRef} className={finalClassName}>
       <div ref={resizeRef}>
         {typeof children === "function" ? children(visible) : children}
       </div>
