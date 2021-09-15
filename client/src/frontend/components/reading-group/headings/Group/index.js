9a92a819b8d0095f42e28b7bcb8d4f2b1c488644
Lauren Davidson <lauren@castironcoding.com>
2021-09-15 15:31:29 -0400

    [F] Add new props to Collapse and implement in GroupHeading

--- client/src/frontend/components/reading-group/headings/Group/index.js
+++ client/src/frontend/components/reading-group/headings/Group/index.js
@@ -1,26 +1,13 @@
-import React, { useState, useEffect, useRef } from "react";
+import * as React from "react";
 import PropTypes from "prop-types";
-import { useUID } from "react-uid";
-import { Collapse } from "react-collapse";
+import Collapse from "global/components/Collapse";
 import classNames from "classnames";
 import { Title } from "../parts";
-import DetailsToggle from "./DetailsToggle";
 import ChildNav from "./ChildNav";
 import ManageGroup from "./ManageGroup";
 import GroupSummaryBox from "./GroupSummaryBox";
 
 function GroupHeading({ readingGroup, history, location }) {
-  const uid = useUID();
-  const summaryRef = useRef();
-
-  const [showDetails, setShowDetails] = useState(false);
-
-  useEffect(() => {
-    if (showDetails && summaryRef.current) {
-      summaryRef.current.focus();
-    }
-  }, [showDetails]);
-
   const groupName = readingGroup.attributes.name;
   const canUpdateGroup = readingGroup.attributes.abilities.update;
 
@@ -31,31 +18,34 @@ function GroupHeading({ readingGroup, history, location }) {
         "group-page-heading--can-update": canUpdateGroup
       })}
     >
-      <div className="group-page-heading__container">
-        <div className="group-page-heading__flex-container">
-          <Title title={groupName} />
-          <div className="group-page-heading__button-container">
-            <DetailsToggle
-              onClick={() => setShowDetails(prevState => !prevState)}
-              controls={uid}
-              active={showDetails}
-            />
+      <Collapse>
+        <div className="group-page-heading__container">
+          <div className="group-page-heading__flex-container">
+            <Title title={groupName} />
+            <div className="group-page-heading__button-container">
+              <Collapse.Toggle
+                className={classNames({
+                  "group-page-heading__nav-button": true,
+                  "button-tertiary": true
+                })}
+                activeClassName="button-tertiary--active"
+              >
+                Details
+              </Collapse.Toggle>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="group-page-heading__container group-page-heading__nav-container">
-        <ChildNav readingGroup={readingGroup} />
-      </div>
-      <Collapse isOpened={showDetails}>
-        <div
-          ref={summaryRef}
-          id={uid}
-          tabIndex={-1}
-          aria-label="Group details"
-          className="group-page-heading__container group-page-heading__summary-container"
-        >
-          <GroupSummaryBox readingGroup={readingGroup} />
+        <div className="group-page-heading__container group-page-heading__nav-container">
+          <ChildNav readingGroup={readingGroup} />
         </div>
+        <Collapse.Content focusOnVisible>
+          <div
+            aria-label="Group details"
+            className="group-page-heading__container group-page-heading__summary-container"
+          >
+            <GroupSummaryBox readingGroup={readingGroup} />
+          </div>
+        </Collapse.Content>
       </Collapse>
       {canUpdateGroup && (
         <div className="group-page-heading__container">
--- client/src/global/components/Collapse/Toggle.js
+++ client/src/global/components/Collapse/Toggle.js
@@ -1,19 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useCollapseContext } from "hooks";
+import classNames from "classnames";
 
-function Toggle({ children, className }) {
+function Toggle({ children, className, activeClassName }) {
   const { visible, toggleProps, labelProps } = useCollapseContext();
   const applyLabelPropsToToggle =
     !React.isValidElement(children) || typeof children === "string";
   const mergedToggleProps = {
     ...toggleProps,
-    ...(applyLabelPropsToToggle ? labelProps : {}),
-    className
+    ...(applyLabelPropsToToggle ? labelProps : {})
   };
+  const finalClassName = classNames({
+    [className]: true,
+    [activeClassName]: visible
+  });
 
   return (
-    <button {...mergedToggleProps}>
+    <button className={finalClassName} {...mergedToggleProps}>
       {typeof children === "function"
         ? children(visible, labelProps)
         : children}
