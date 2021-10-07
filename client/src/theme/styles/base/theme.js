7a7a8536d472a3fddfd8cc3e65c5c393b2edc24b
Dana Johnson <dana@castironcoding.com>
2021-10-06 17:04:59 -0700

    [C] Migrate Sass vars/mixins/base styles to CSS and JS vars

---
+++ client/src/theme/styles/base/theme.js
@@ -0,0 +1,93 @@
+// CSS color vars defined by context in Manifold
+export default `
+  body {
+    color: var(--color);
+    background-color: var(--background-color);
+  }
+
+  .browse,
+  .scheme-light,
+  .bg-white {
+    --color: var(--color-neutral-text-dark);
+    --background-color: var(--color-base-neutral-white);
+    --strong-color: var(--color-neutral-text-extra-dark);
+    --medium-color: var(--color-base-neutral80);
+    --weak-color: var(--color-neutral-ui-dull-light);
+    --highlight-color: var(--color-interaction-dark);
+    --focus-color: var(--color-neutral-ui-dark);
+    --hover-color: var(--color-interaction-dark);
+    --error-color: var(--color-notification-error-dark);
+    --warning-color: var(--color-notification-warning-dark);
+    --notice-color: var(--color-notification-notice-dark);
+    --inactive-switch-bg-color: var(--color-base-neutral70);
+    --active-switch-bg-color: #17986d;
+    --switch-toggle-color: var(--color-base-neutral05);
+    --box-bg-color: var(--color-base-neutral05);
+    --box-weak-bg-color: var(--color-base-neutral05);
+    --box-medium-bg-color: var(--color-base-neutral10);
+    --box-strong-bg-color: var(--color-base-neutral20);
+    --box-x-strong-bg-color: var(--color-base-neutral20);
+    --dropzone-bg-color: var(--color-base-neutral30);
+    --select-bg-color: transparent;
+    --select-border-color: currentColor;
+    --button-dull-bg-color: var(--color-base-neutral30);
+    --button-tertiary-bg-color: var(--color-accent-primary-pale);
+    --button-tertiary-bg-hover-color: var(--color-accent-primary-light);
+    --reader-color: var(--strong-color);
+    --input-color: var(--color);
+    --input-bg-color: var(--color-base-neutral-white);
+    --input-border-color: transparent;
+    --input-autofill-color: var(--highlight-color);
+    --input-font-family: var(--font-family-sans);
+
+    color: var(--color);
+  }
+
+  .backend,
+  .drawer--backend,
+  .scheme-dark,
+  .bg-neutral90,
+  .bg-neutral95 {
+    --color: var(--color-neutral-text-light);
+    --background-color: var(--color-base-neutral90);
+    --strong-color: var(--color-neutral-text-extra-light);
+    --medium-color: var(--color-base-neutral20);
+    --weak-color: var(--color-neutral-ui-dull-dark);
+    --highlight-color: var(--color-interaction-light);
+    --focus-color: var(--color-interaction-light);
+    --hover-color: var(--color-interaction-light);
+    --error-color: var(--color-notification-error-light);
+    --warning-color: var(--color-notification-warning-light);
+    --notice-color: var(--color-notification-notice-light);
+    --inactive-switch-bg-color: var(--color-base-neutral70);
+    --active-switch-bg-color: var(--color-interaction-light);
+    --switch-toggle-color: var(--color-base-neutral90);
+    --box-bg-color: var(--color-base-neutral95);
+    --box-weak-bg-color: var(--color-base-neutral85);
+    --box-medium-bg-color: var(--color-base-neutral90);
+    --box-strong-bg-color: var(--color-base-neutral90);
+    --box-x-strong-bg-color: var(--color-base-neutral80);
+    --dropzone-bg-color: var(--color-base-neutral30);
+    --select-bg-color: var(--color-base-neutral90);
+    --select-border-color: var(--color-base-neutral45);
+    --button-dull-bg-color: var(--color-base-neutral10);
+    --button-tertiary-bg-color: var(--color-accent-primary);
+    --button-tertiary-bg-hover-color: var(--color-accent-primary-pale);
+    --reader-color: var(--color-base-neutral-white);
+    --input-color: var(--strong-color);
+    --input-bg-color: transparent;
+    --input-border-color: var(--color-neutral-ui-dull-light);
+    --input-autofill-color: var(--color-accent-primary-pale);
+    --input-font-family: var(--font-family-serif);
+
+    color: var(--color);
+
+    input[type="text"],
+    input[type="email"],
+    input[type="password"],
+    select,
+    textarea {
+      color: var(--input-color);
+    }
+  }
+`;
---
+++ client/src/theme/styles/mixins/appearance.js
@@ -0,0 +1,367 @@
+import get from "lodash/get";
+import { breakpoints } from "../variables/media";
+import { respond } from "./common";
+import { utilityPrimary } from "./typography";
+
+// Media
+// --------------------
+export function hide(size) {
+  const breakpoint = get(breakpoints, size, size);
+  return respond(`display: none;`, breakpoint);
+}
+
+export function show(size, display = "inherit") {
+  const breakpoint = get(breakpoints, size, size);
+  return `
+    display: none;
+    ${respond(`display: ${display};`, breakpoint)}
+  `;
+}
+
+// Interactions
+// --------------------
+export const defaultFocusStyle = `outline: solid 2px;`;
+export const defaultHoverStyle = `color: var(--hover-color);`;
+
+export function setFocusStyle(property = "outline", value = "solid 2px") {
+  return `
+    &:focus:not(:focus-visible) {
+      outline: 0;
+    }
+
+    &:focus-visible {
+      ${property}: ${value};
+    }
+  `;
+}
+
+export function setHoverStyle(
+  property = "color",
+  value = "var(--hover-color)"
+) {
+  return `
+    transition: ${property} var(--transition-duration-default)
+      var(--transition-timing-export function);
+
+    &:hover {
+      ${property}: ${value};
+    }
+  `;
+}
+
+export function outlineOnFocus(color = "var(--focus-color)") {
+  return setFocusStyle("outline", `solid 2px ${color}`);
+}
+
+export function fillOnFocus(color = "var(--hover-color)") {
+  return `
+    ${setFocusStyle("background-color", color)}
+
+    &:focus-visible {
+      outline: 0;
+    }
+  `;
+}
+
+export function fillOnHover(color = "var(--hover-color)") {
+  return setHoverStyle("background-color", color);
+}
+
+// Utility
+// --------------------------------------------------------
+export const screenReaderText = `
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
+`;
+
+// Layout
+// --------------------------------------------------------
+// Apply to a pseudo element to get a tail/triangle pointing up
+export function tailUp(color = "var(--color-base-neutral10)", height = "17px") {
+  return `
+    width: 0;
+    height: 0;
+    border-color: transparent transparent ${color};
+    border-style: solid;
+    border-width: 0 15.5px ${height};
+  `;
+}
+
+// // Buttons
+// // --------------------------------------------------------
+export const buttonUnstyled = `
+  padding: 0;
+  cursor: pointer;
+  background: transparent;
+  border: 0;
+  border-radius: 0;
+  outline: none;
+  appearance: none;
+`;
+
+export const buttonRounded = `
+  cursor: pointer;
+  border-radius: 7px;
+`;
+
+export function buttonAvatar(size = 36, color = "inherit") {
+  return `
+    ${buttonUnstyled}
+    width: ${size}px;
+    height: ${size}px;
+    overflow: hidden;
+    color: ${color};
+    text-align: center;
+    border-radius: 100%;
+
+    &__icon {
+      width: 100%;
+      height: 100%;
+    }
+
+    .avatar-image {
+      width: 100%;
+    }
+  `;
+}
+
+export const marker = `
+  display: flex;
+  align-items: center;
+  padding: 3px 10px 5px;
+  font-family: var(--font-family-sans);
+  font-size: 14px;
+  text-align: center;
+  text-decoration: none;
+  white-space: nowrap;
+  background-color: var(--box-bg-color);
+  border-radius: 12px;
+`;
+
+export const blockLabelRound = `
+  ${utilityPrimary}
+  display: inline-block;
+  padding: 0.333em 8px;
+  font-size: 12px;
+  font-weight: var(--font-weight-semibold);
+  color: var(--color-base-neutral-white);
+  background-color: var(--notice-color);
+  border-radius: 3px;
+}`;
+
+// // Browser UI
+// // --------------------------------------------------------
+// // Styled select
+// // based on: https://github.com/mdo/wtf-forms/blob/master/wtf-forms.css
+export const unstyledSelect = `
+  cursor: pointer;
+  background: transparent;
+  border-radius: 0;
+  outline: 0;
+  appearance: none;
+
+  &:focus-visible:-moz-focusring {
+    color: transparent;
+    text-shadow: 0 0 0 var(--default-medium-color);
+  }
+
+  &::-ms-expand {
+    display: none;
+  }
+`;
+
+export const selectPrimary = `
+  position: relative;
+  display: inline-block;
+  color: var(--default-medium-color);
+
+  select {
+    ${utilityPrimary}
+    @include unstyledSelect;
+    display: inline-block;
+    width: 100%;
+    padding: 9px 13px 11px;
+    margin: 0;
+    font-size: var(--font-size-40);
+    line-height: var(--line-height);
+    border: 2px solid var(--color-neutral40);
+
+    &:focus-visible {
+      border-color: var(--focus-color);
+    }
+  }
+}`;
+
+// // Styled Checkbox (also from wtf forms)
+// // Should be applied to a label with an input type="checkbox" inside
+export const checkboxStyled = `
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+
+  input {
+    position: absolute;
+    z-index: -1;
+    opacity: 0;
+  }
+`;
+
+const inputBase = `
+  font-family: var(--input-font-family);
+  color: var(--input-color);
+  background-color: var(--input-bg-color);
+  border-color: var(--input-border-color);
+
+  &:focus-visible {
+    outline: none;
+    border-color: var(--focus-color);
+  }
+`;
+
+// // Inputs/Textareas
+export const inputLabelPrimary = `
+  display: block;
+  margin-bottom: 12px;
+  text-transform: uppercase;
+  font-size: var(--font-size-30);
+  font-family: var(--input-font-family);
+  color: var(--color-base-neutral50);
+`;
+
+export const inputPrimary = `
+  ${inputBase}
+  padding: 8px 13px 12px;
+  font-size: var(--font-size-70);
+  border: 3px solid;
+`;
+
+// // Dark input with border
+export const inputQuaternary = `
+  ${inputBase}
+  padding: 0.438em 1.125em 0.563em;
+  background: transparent;
+  border: 1px solid;
+  border-radius: 0;
+  appearance: none;
+  outline: none;
+
+  &:focus-visible {
+    outline: none;
+
+    &::placeholder {
+      color: var(--highlight-color);
+    }
+  }
+`;
+
+export const panelRounded = `
+  background-color: var(--box-bg-color);
+  border-radius: var(--box-border-radius);
+`;
+
+export const panelRoundedDark = `
+  --color: var(--color-neutral-ui-light);
+
+  ${panelRounded}
+  color: var(--color);
+`;
+
+export const roundedHeader = `
+  ${panelRoundedDark}
+  display: table;
+  width: 100%;
+  padding: 0.75em 1.5em 0.875em;
+  margin-bottom: 20px;
+  font-size: 16px;
+
+  // if using a rounded label, pad any of its siblings
+  ~ :not(.full-width) {
+    padding-right: min(3.158vw, 24px);
+    padding-left: min(3.158vw, 24px);
+  }
+`;
+
+export const roundedFormHeader = `
+  --label-color: var(--highlight-color);
+
+  ${roundedHeader}
+
+  .browse &,
+  .reader & {
+    --label-color: var(--color);
+  }
+
+  > * {
+    ${utilityPrimary}
+    display: table-cell;
+    width: 100%;
+    font-size: 14px;
+    font-weight: var(--font-weight-semibold);
+    color: var(--label-color);
+    letter-spacing: 0.1em;
+  }
+`;
+
+// // Drag and Drop styles
+export function dropzone(margin = "9px", activeSelector = "--show-dropzone") {
+  return `
+    ${panelRounded}
+    display: block;
+    padding: 0 ${margin};
+    margin-right: -${margin};
+    margin-left: -${margin};
+    background-color: transparent;
+    transition: background-color 0.4s ease;
+
+    &${activeSelector} {
+      background-color: var(--dropzone-bg-color);
+    }
+  `;
+}
+
+export const draggable = `
+  ${panelRoundedDark}
+  cursor: move; // fallback for older browsers
+  cursor: grab;
+  transition: color var(--transition-duration-default)
+      var(--transition-timing-function),
+    background-color var(--transition-duration-default)
+      var(--transition-timing-function),
+    border-color var(--transition-duration-default)
+      var(--transition-timing-function);
+`;
+
+export const dragging = `
+  box-shadow: 0 31px 26px -13px rgba(0 0 0 / 0.33);
+`;
+
+export function reactSlideTransition(
+  from = "right",
+  selector = "&",
+  prefix = "panel"
+) {
+  return `
+    .${prefix}-enter ${selector} {
+      transform: translateX(${from === "right" ? "100%" : "-100%"});
+    }
+
+    .${prefix}-enter-active ${selector} {
+      transition: transform $duration $timing;
+      transform: translateX(0);
+    }
+
+    .${prefix}-exit ${selector} {
+      transform: translateX(0);
+    }
+
+    .${prefix}-exit.${prefix}-exit-active ${selector} {
+      transition: transform $duration $timing;
+      transform: translateX(${from === "right" ? "100%" : "-100%"});
+    }
+  `;
+}
