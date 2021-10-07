91293ae1d577e817ee10d272d6e12fef6df93ebe
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2021-10-07 11:46:34 -0400

    [WIP] Add css vars to sc

--- client/src/frontend/components/EntityThumbnail/EntityThumbnail.styles.js
+++ client/src/frontend/components/EntityThumbnail/EntityThumbnail.styles.js
@@ -1,4 +1,5 @@
 import styled, { css } from "styled-components";
+import { respond } from "theme/styles/mixins/common";
 
 export const EntityListItemWrapper = styled.div`
   display: flex;
@@ -6,10 +7,14 @@ export const EntityListItemWrapper = styled.div`
   color: inherit;
   text-decoration: none;
 
-  /* @include respond($break75) {
-    flex-direction: column;
-    height: 100%;
-    padding: 2.105vw; */
+  ${respond(
+    css`
+      flex-direction: column;
+      height: 100%;
+      padding: 2.105vw;
+    `,
+    75
+  )}
 `;
 
 export const Cover = styled.figure`
@@ -24,30 +29,36 @@ export const Cover = styled.figure`
   ${({ placeholder }) =>
     placeholder &&
     css`
-      --default-hover-color: #52e3ac; /* #{$accentInteractionLight} */
+      /* Why was this set here? */
+      --default-hover-color: var(--hover-color);
 
       /* @include panelRoundedDark; */
       background-color: var(--box-bg-color, #f7f7f7);
       border-radius: var(--box-border-radius);
-      color: #9a9a9a; /*$neutralUILight*/
+      color: var(--color-neutral-uiLight)
 
       display: flex;
-      color: #ffffff; /*$neutralWhite*/
+      color: var(--color-base-neutralWhite);
       text-decoration: none;
       background-repeat: no-repeat;
       background-position: 50% 50%;
       background-size: cover;
-      transition: color 0.2s ease, box-shadow 0.2s ease; /* $duration $timing */
+      transition: color 0.2s ease,
+        box-shadow var(--transition-duration-default)
+          var(--transition-timing-function);
 
-      /* @include respond($break80) {
-        padding-top: 160px;
-      } */
+      ${respond(
+        css`
+          padding-top: 160px;
+        `,
+        80
+      )}
 
       &[href]:hover,
       &[href]:focus-visible {
         /* @include boxShadow(0, 20px, 30px, 2px) */
         box-shadow: 0 20px 30px 2px rgba(#000000, 0.13);
-        color: #52e3ac; /* $defaultHoverColor */
+        color: var(--hover-color);
         outline: 0;
       }
     `}
