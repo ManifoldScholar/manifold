483c90db7d4f86db8d2a383f52fd4fcb989d3e4d
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2021-10-08 18:23:36 -0400

    [F] Update EntityThumbnail component

--- client/src/global/components/atomic/EntityThumbnail/EntityMetadata/EntityMetadata.styles.js
+++ client/src/global/components/atomic/EntityThumbnail/EntityMetadata/EntityMetadata.styles.js
@@ -1,7 +1,11 @@
 import styled, { css } from "styled-components";
-import { respond } from "theme/styles/mixins/common";
+import { blockLabelRound, subtitlePrimary } from "theme/styles/mixins";
 
 export const MetadataWrapper = styled.div`
+  --metadata-padding-top: ${({ stack }) => (stack ? "0.625em" : "0.4em")};
+  --metadata-default-font-size: ${({ stack }) => (stack ? "16px" : "14px")};
+  --metadata-creators-font-size: ${({ stack }) => (stack ? "16px" : "14px")};
+
   display: flex;
   flex-flow: column;
   flex-grow: 1;
@@ -11,16 +15,17 @@ export const MetadataWrapper = styled.div`
   padding-left: 15px;
   line-height: 20px;
 
-  ${respond(
+  ${({ stack }) =>
+    stack &&
     css`
       padding-left: 0;
       padding-right: 0;
-    `,
-    75
-  )}
+    `}
 `;
 
 export const TitleWrapper = styled.h3`
+  --title-display: ${({ stack }) => (stack ? "block" : "inline-block")};
+
   margin: 0;
   font-size: 16px;
   font-weight: var(--font-weight-regular);
@@ -30,158 +35,90 @@ export const TitleWrapper = styled.h3`
     var(--transition-timing-function);
   color: var(--color-base-neutral90);
 
-  ${respond(
+  ${({ stack }) =>
+    stack &&
     css`
       font-size: 17px;
       font-weight: var(--font-weight-semibold);
-    `,
-    75
-  )}
+    `}
 `;
 
 export const TitleText = styled.span`
-  display: inline-block;
-
-  ${respond(
-    css`
-      display: block;
-    `,
-    75
-  )}
-
-  /* @include templateHead */
-  font-family: "sofia-pro", "trueno", sans-serif;
+  display: var(--title-display);
+  font-family: var(--font-family-heading);
 `;
 
 export const Subtitle = styled.h4`
+  ${subtitlePrimary}
+
   margin: 0;
   display: block;
-  padding-top: 0.4em;
+  padding-top: var(--metadata-padding-top);
   font-size: 14px;
-  color: var(--color-base-neutral80);
   transition: color var(--transition-duration-default)
     var(--transition-timing-function);
-
-  /* @include subtitlePrimary */
-  font-style: italic;
-  font-weight: var(--font-weight-regular);
-  letter-spacing: 0.031em;
-
-  /* @include templateCopy */
-  font-family: "freight-text-pro", "aleo", serif;
-
-  ${respond(
-    css`
-      padding-top: 0.625em;
-    `,
-    75
-  )}
 `;
 
 export const Tag = styled.div`
+  ${blockLabelRound}
+
   padding-right: 5px;
   padding-left: 5px;
   margin: 2px 0 5px 9px;
   font-size: 9px;
   vertical-align: middle;
   line-height: 1.188;
+  font-family: var(--font-family-heading);
 
-  /* @include blockLabelRound */
-  display: inline-block;
-  padding: 0.333em 5px;
-  font-weight: var(--font-weight-semibold);
-  color: var(--color-base-neutral-white);
-  background-color: var(--notice-color);
-  border-radius: 3px;
-
-  /* @include utilityPrimary */
-  text-transform: uppercase;
-  letter-spacing: 0.115em;
-
-  /* @include templateHead */
-  font-family: "sofia-pro", "trueno", sans-serif;
-
-  ${respond(
+  ${({ stack }) =>
+    stack &&
     css`
       padding-right: 8px;
       padding-left: 8px;
       margin: 0.625em 0 0;
       font-size: 12px;
-    `,
-    75
-  )}
+    `}
 `;
 
 export const Creators = styled.div`
   hyphens: none;
   transition: color var(--transition-duration-default)
     var(--transition-timing-function);
-  padding-top: 0.4em;
-  color: var(--color-base-neutral80);
-  font-size: 15px;
-
-  /* @include templateCopy */
-  font-family: "freight-text-pro", "aleo", serif;
-
-  ${respond(
-    css`
-      font-size: 16px;
-      padding-top: 0.625em;
-    `,
-    75
-  )}
+  padding-top: var(--metadata-padding-top);
+  font-size: var(--metadata-creators-font-size);
+  font-family: var(--font-family-copy);
 `;
 
 export const Description = styled.div`
-  /* @include templateHead */
-  font-family: "sofia-pro", "trueno", sans-serif;
-
+  font-family: var(--font-family-heading);
   margin-bottom: 20px;
-  font-size: 14px;
-  color: var(--color-base-neutral80);
+  font-size: var(--metadata-default-font-size);
+  padding-top: var(--metadata-padding-top);
 
-  /* &:only-child:not(.pad-bottom) {
+  /*
+  This was in the original scss, but we likely won't use ".pad-bottom" here, so leaving it as comment until needed:
+  &:only-child:not(.pad-bottom) {
     margin-bottom: 0;
   } */
-
-  ${respond(
-    css`
-      font-size: 16px;
-      padding-top: 0.625em;
-    `,
-    75
-  )}
 `;
 
 export const Date = styled.div`
-  font-size: 14px;
+  font-size: var(--metadata-default-font-size);
   font-style: italic;
-  padding-top: 0.4em;
-  color: var(--color-base-neutral80);
-
-  /* @include templateCopy */
-  font-family: "freight-text-pro", "aleo", serif;
-
-  ${respond(
-    css`
-      font-size: 16px;
-      padding-top: 0.625em;
-    `,
-    75
-  )}
+  padding-top: var(--metadata-padding-top);
+  font-family: var(--font-family-copy);
 
   ${({ recentlyUpdated }) =>
     recentlyUpdated &&
     css`
-      ::before {
+      &::before {
         display: inline-block;
         width: 7px;
         height: 7px;
         margin-right: 5px;
         margin-bottom: 2px;
-        color: var(--color-base-neutral-white);
         content: "";
-        background-color: var(--color-base-red45);
+        background-color: var(--error-color);
         border-radius: 50%;
       }
     `}
