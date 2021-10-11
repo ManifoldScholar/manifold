2e94a495bb3cabd2a8fde4e92e97edd95aff5af0
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2021-10-11 13:02:05 -0400

    [WIP] Change ThumbnailGrid from flex to grid

--- client/src/global/components/composed/ThumbnailGrid/ThumbnailGrid.styles.js
+++ client/src/global/components/composed/ThumbnailGrid/ThumbnailGrid.styles.js
@@ -1,81 +1,96 @@
 import styled, { css } from "styled-components";
 import { respond } from "theme/styles/mixins/common";
 
-export const GridWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-top: 30px;
-  position: relative;
-
-  ${respond(
-    css`
-      flex-flow: row wrap;
-      align-items: center;
-      justify-content: space-between;
-      width: calc(100% + 4.21vw);
-      margin-right: -2.105vw;
-      margin-left: -2.105vw;
-    `,
-    75
-  )}
-
-  ${respond(
-    css`
-      width: calc(100% + 52px);
-      margin-right: -26px;
-      margin-left: -26px;
-    `,
-    75
-  )}
-
-  /* Probably use a prop here? */
-  &.list-total--empty {
-    width: calc(100% - 4.21vw);
-    padding-top: 0;
-    text-align: left;
-
-    ${respond(
-      css`
-        margin: auto;
-      `,
-      75
-    )}
-`;
+// export const GridWrapper = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+//   padding-top: 30px;
+//   position: relative;
+//
+//   ${respond(
+//     css`
+//       /* flex-flow: row wrap;
+//       align-items: center;
+//       justify-content: space-between; */
+//       width: calc(100% + 4.21vw);
+//       margin-right: -2.105vw;
+//       margin-left: -2.105vw;
+//     `,
+//     75
+//   )}
+//
+//   ${respond(
+//     css`
+//       width: calc(100% + 52px);
+//       margin-right: -26px;
+//       margin-left: -26px;
+//     `,
+//     120
+//   )}
+//
+//   /* Probably use a prop here? */
+//   &.list-total--empty {
+//     width: calc(100% - 4.21vw);
+//     padding-top: 0;
+//     text-align: left;
+//
+//     ${respond(
+//       css`
+//         margin: auto;
+//       `,
+//       75
+//     )}
+// `;
 
 export const GridList = styled.ul`
+  --list-item-padding: ${({ grid }) => (grid ? 0 : "14px")};
+  --list-item-border: ${({ grid }) => (grid ? "none" : "1px solid")};
+  --list-item-margin: ${({ grid }) => (grid ? "18px" : 0)};
+
+  display: grid;
+  grid-template-columns: ${({ minWidth }) =>
+    `repeat(auto-fill, minmax(${minWidth}, 1fr))`};
+  padding-top: 30px;
+
   /*@include listUnstyled */
   padding-left: 0;
   margin-top: 0;
   margin-bottom: 0;
   list-style-type: none;
 
-  display: flex;
-  flex-direction: column;
-
-  ${respond(
+  ${({ grid }) =>
+    grid &&
     css`
-      flex-flow: row wrap;
-    `,
-    75
-  )}
+      width: calc(100% + 4.21vw);
+      margin-right: -2.105vw;
+      margin-left: -2.105vw;
+
+      ${respond(
+        css`
+          width: calc(100% + 52px);
+          margin-right: -26px;
+          margin-left: -26px;
+        `,
+        120
+      )}
+    `}
+
+  ${({ empty, grid }) =>
+    empty &&
+    css`
+      width: calc(100% - 4.21vw);
+      padding-top: 0;
+      text-align: left;
+
+      ${grid && `margin: auto;`}
+    `}
 `;
 
 export const GridItem = styled.li`
-  border-bottom: 1px solid;
+  border-bottom: var(--list-item-border);
   transition: background-color var(--transition-duration-default) ease-out,
     box-shadow var(--transition-duration-default) ease-out;
   position: relative;
-  padding-left: 14px;
-
-  ${respond(
-    css`
-      padding-left: 0;
-      flex: 1 1 25%;
-      max-width: 25%;
-      padding-left: 0;
-      margin-bottom: 18px;
-      border-bottom: none;
-    `,
-    75
-  )}
+  padding-left: var(--list-item-padding);
+  margin-bottom: var(--list-item-margin);
 `;
--- client/src/global/components/composed/ThumbnailGrid/index.js
+++ client/src/global/components/composed/ThumbnailGrid/index.js
@@ -3,29 +3,36 @@ import EntityThumbnail from "global/components/atomic/EntityThumbnail";
 import * as Styled from "./ThumbnailGrid.styles";
 import PropTypes from "prop-types";
 
-export default function ThumbnailGrid({ entities, onUncollect, userMock }) {
+export default function ThumbnailGrid({
+  entities,
+  onUncollect,
+  userMock,
+  grid = true,
+  minWidth = "200px"
+}) {
   return (
-    <Styled.GridWrapper>
-      <Styled.GridList>
-        {entities.map(entity => {
-          return (
-            <Styled.GridItem key={entity.id}>
-              <EntityThumbnail
-                entity={entity}
-                onUncollect={onUncollect}
-                userMock={userMock}
-              />
-            </Styled.GridItem>
-          );
-        })}
-      </Styled.GridList>
-    </Styled.GridWrapper>
+    <Styled.GridList grid={grid} minWidth={minWidth}>
+      {entities.map(entity => {
+        return (
+          <Styled.GridItem key={entity.id}>
+            <EntityThumbnail
+              entity={entity}
+              onUncollect={onUncollect}
+              userMock={userMock}
+              stack={grid}
+            />
+          </Styled.GridItem>
+        );
+      })}
+    </Styled.GridList>
   );
 }
 
 EntityThumbnail.propTypes = {
   entities: PropTypes.array,
   onUncollect: PropTypes.func,
+  grid: PropTypes.bool,
+  minWidth: PropTypes.string,
   /* For stories */
   userMock: PropTypes.object
 };
