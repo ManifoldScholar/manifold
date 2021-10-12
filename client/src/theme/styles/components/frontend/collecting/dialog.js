2d9ee709925204c3ec207a34d756fc64a4276942
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2021-10-12 18:53:56 -0400

    [WIP] Migrate sass partials

--- client/src/theme/styles/components/frontend/collecting/dialog.js
+++ client/src/theme/styles/components/frontend/collecting/dialog.js
@@ -1,58 +1,60 @@
-.collecting-dialog {
-  $_lateral-padding: min(3.158vw, 24px);
-  $_max-dialog-height: 90vh;
-  $_max-list-height: 220px;
+const lateralPadding = `min(3.158vw, 24px)`;
+const maxDialogHeight = `90vh`;
+const maxListHeight = `220px`;
 
-  max-height: $_max-dialog-height;
-  overflow: auto;
-  font-weight: $regular;
-
-  &__inner {
-    padding-top: min(6.579vw, 50px);
-    padding-right: min(1.974vw, 15px);
-    padding-left: min(1.974vw, 15px);
-  }
-
-  &__header {
-    display: flex;
-    align-items: baseline;
-    padding-right: $_lateral-padding;
-    padding-left: $_lateral-padding;
-  }
-
-  &__header-icon {
-    order: -1;
-    margin-right: 16px;
-    margin-left: -5px;
-  }
-
-  &__title {
-    margin: 0;
-    font-size: 20px;
-    font-weight: $regular;
-    transform: translateY(-75%);
-  }
-
-  &__fieldset {
-    padding: 0;
-    margin: 0;
-    margin-top: 28px;
-    border: none;
-  }
-
-  &__checkbox-group {
-    max-height: $_max-list-height;
-    margin-top: 10px;
+export default `
+  .collecting-dialog {
+    max-height: ${maxDialogHeight};
     overflow: auto;
-  }
+    font-weight: var(--font-weight-regular);
 
-  &__footer {
-    padding-right: $_lateral-padding;
-    padding-left: $_lateral-padding;
-    margin-top: 50px;
-  }
+    &__inner {
+      padding-top: min(6.579vw, 50px);
+      padding-right: min(1.974vw, 15px);
+      padding-left: min(1.974vw, 15px);
+    }
 
-  &__close-button {
-    width: 100%;
+    &__header {
+      display: flex;
+      align-items: baseline;
+      padding-right: ${lateralPadding};
+      padding-left: ${lateralPadding};
+    }
+
+    &__header-icon {
+      order: -1;
+      margin-right: 16px;
+      margin-left: -5px;
+    }
+
+    &__title {
+      margin: 0;
+      font-size: 20px;
+      font-weight: var(--font-weight-regular);
+      transform: translateY(-75%);
+    }
+
+    &__fieldset {
+      padding: 0;
+      margin: 0;
+      margin-top: 28px;
+      border: none;
+    }
+
+    &__checkbox-group {
+      max-height: ${maxListHeight};
+      margin-top: 10px;
+      overflow: auto;
+    }
+
+    &__footer {
+      padding-right: ${lateralPadding};
+      padding-left: ${lateralPadding};
+      margin-top: 50px;
+    }
+
+    &__close-button {
+      width: 100%;
+    }
   }
-}
+`;
