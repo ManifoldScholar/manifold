33ee4be2db8ac5b40a591be6678ae5a5e8c750a5
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2021-10-19 17:18:52 -0400

    fixup! [WIP] Migrate reader components

---
+++ client/src/theme/styles/components/reader/uiPanelMenu.js
@@ -0,0 +1,20 @@
+import { respond, reactSlideTransition } from "theme/styles/mixins";
+
+export const uiPanelMenu = `
+  --hover-color: var(--color-interaction-dark);
+
+  ${reactSlideTransition()}
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100%;
+  background-color: var(--color-base-neutral05);
+
+  ${respond(`width: auto;`, 50)}
+}
+
+.panel-hidden {
+  .screen-reader-text {
+    visibility: hidden;
+  }
+`;
