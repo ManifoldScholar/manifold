7a30e987da52c54bb1b856fbcb99f21705db6918
Dana Johnson <dana@castironcoding.com>
2021-10-27 18:10:34 -0700

    [C] Migrate container styles

---
+++ client/src/theme/styles/components/frontend/loginPage.js
@@ -0,0 +1,54 @@
+import { respond, loginFormPrimary } from "theme/styles/mixins";
+
+export default `
+  .login-page {
+    width: 100%;
+    max-width: calc(300px + var(--container-padding-narrow) * 2);
+    margin: auto;
+
+    ${respond(
+      `max-width: calc(300px + var(--container-padding-responsive) * 2);`,
+      35
+    )}
+
+    ${respond(
+      `max-width: calc(300px + var(--container-padding-full) * 2);`,
+      120
+    )}
+
+    .login-form {
+      ${loginFormPrimary}
+      font-size: 18px;
+      font-family: var(--font-family-serif);
+
+      input[type="text"],
+      input[type="email"],
+      input[type="password"] {
+        border-color: var(--color-base-neutral40);
+      }
+    }
+
+    .login-links {
+      margin-top: 25px;
+    }
+
+    .login-external {
+      margin-top: 45px;
+
+      .button-secondary--dark {
+        display: block;
+        width: 100%;
+      }
+    }
+
+    .login-notice {
+      margin-bottom: 35px;
+      font-size: var(--font-size-60);
+      font-family: var(--font-family-sans);
+    }
+
+    .button-secondary {
+      width: 100%;
+    }
+  }
+`;
