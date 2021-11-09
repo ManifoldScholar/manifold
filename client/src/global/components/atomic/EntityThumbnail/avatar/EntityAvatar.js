b7a65f1755e34755da8529fddcd27ed5ba5fa5b3
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2021-11-09 16:12:56 -0500

    [C] Update filenames

--- client/src/global/components/atomic/EntityThumbnail/avatar/EntityAvatar.js
+++ client/src/global/components/atomic/EntityThumbnail/avatar/EntityAvatar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import * as Styled from "./EntityAvatar.styles";
+import * as Styled from "./styles";
 
 const EntityAvatar = ({ entity }) => {
   const meta = entity.attributes.avatarMeta.original;
