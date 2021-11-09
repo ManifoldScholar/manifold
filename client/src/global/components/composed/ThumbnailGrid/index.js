e320f275313aa62950bb2636da2841d14b6bd9aa
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2021-11-09 16:12:56 -0500

    [C] Update filenames

--- client/src/global/components/composed/ThumbnailGrid/index.js
+++ client/src/global/components/composed/ThumbnailGrid/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import EntityThumbnail from "global/components/atomic/EntityThumbnail";
-import * as Styled from "./ThumbnailGrid.styles";
+import * as Styled from "./styles";
 import PropTypes from "prop-types";
 import useResizeObserver from "use-resize-observer";
 import classnames from "classnames";
