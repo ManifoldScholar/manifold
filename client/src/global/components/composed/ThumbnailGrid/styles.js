0af48839e247f66a416c21796890b0aab8ff9e12
Dana Johnson <dana@castironcoding.com>
2021-11-11 15:05:04 -0800

    [C] Migrate to Emotion

--- client/src/global/components/composed/ThumbnailGrid/styles.js
+++ client/src/global/components/composed/ThumbnailGrid/styles.js
@@ -1,5 +1,26 @@
-import { styled } from "@linaria/react";
+import styled from "@emotion/styled";
 import { respond, listUnstyled } from "theme/styles/mixins";
+
+const gridStyles = `
+  width: calc(100% + 4.21vw);
+  margin-right: -2.105vw;
+  margin-left: -2.105vw;
+
+  ${respond(
+    `
+      width: calc(100% + 52px);
+      margin-right: -26px;
+      margin-left: -26px;
+    `,
+    120
+  )}
+`;
+
+const emptyStyles = `
+  width: calc(100% - 4.21vw);
+  padding-top: 0;
+  text-align: left;
+`;
 
 export const Grid = styled.div`
   --list-item-padding: ${({ $grid }) => ($grid ? 0 : "14px")};
@@ -14,30 +35,11 @@ export const Grid = styled.div`
 
   ${listUnstyled}
 
-  &.grid {
-    width: calc(100% + 4.21vw);
-    margin-right: -2.105vw;
-    margin-left: -2.105vw;
-
-    ${respond(
-      `
-        width: calc(100% + 52px);
-        margin-right: -26px;
-        margin-left: -26px;
-      `,
-      120
-    )}
-  }
-
-  &.empty {
-    width: calc(100% - 4.21vw);
-    padding-top: 0;
-    text-align: left;
-  }
-
-  &.empty.grid {
-    margin: auto;
-  }
+  ${({ $grid, $empty }) => `
+    ${$grid && gridStyles}
+    ${$empty && emptyStyles}
+    ${$grid && $empty && $grid && `margin: auto;`}
+  `}
 
   & > * {
     border-bottom: var(--list-item-border);
