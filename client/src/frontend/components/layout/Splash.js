292fca6c17698a357da91adc77e54a45752ef3d0
Zach Davis <zach@castironcoding.com>
2021-11-15 14:12:15 -0800

    [B] Remove unnecessary nbsp

--- client/src/frontend/components/layout/Splash.js
+++ client/src/frontend/components/layout/Splash.js
@@ -168,7 +168,6 @@ export default class Splash extends Component {
                 alt={this.header(false)}
               />
             ) : null}
-            &nbsp;
           </figure>
           <div className="left">
             <h1
