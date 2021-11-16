fdcaecef73b2e0602f961c11e27010b3d55fd609
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2021-11-16 16:20:23 -0500

    [F] Make EntityAvatar atomic component

--- client/src/global/components/atomic/EntityAvatar/PlaceholderAvatar.js
+++ client/src/global/components/atomic/EntityAvatar/PlaceholderAvatar.js
@@ -1,18 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import UniqueIcons from "global/components/icon/unique";
 import * as Styled from "./styles";
 
 const PlaceholderAvatar = ({ entity }) => {
   if (!entity.attributes.avatarColor) return null;
   return (
-    <Styled.Placeholder>
-      <UniqueIcons.ProjectPlaceholderUnique
-        mode="responsive"
-        color={entity.attributes.avatarColor}
-        ariaLabel={false}
-      />
-    </Styled.Placeholder>
+    <Styled.Placeholder
+      mode="responsive"
+      color={entity.attributes.avatarColor}
+      ariaLabel={false}
+    />
   );
 };
 
