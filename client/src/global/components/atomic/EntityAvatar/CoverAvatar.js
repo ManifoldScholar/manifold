f7b070388bb400f17338c660a82770548d174b52
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2021-11-16 16:20:23 -0500

    [F] Make EntityAvatar atomic component

--- client/src/global/components/atomic/EntityAvatar/CoverAvatar.js
+++ client/src/global/components/atomic/EntityAvatar/CoverAvatar.js
@@ -2,20 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import * as Styled from "./styles";
 
-const EntityAvatar = ({ entity }) => {
+const CoverAvatar = ({ entity }) => {
   const meta = entity.attributes.avatarMeta.original;
   const avatarSrc =
     meta.width >= meta.height
       ? entity.attributes.avatarStyles.smallSquare
       : entity.attributes.avatarStyles.small;
 
-  /* need to fix the alt here */
   return <Styled.Avatar src={avatarSrc} alt="" />;
 };
 
-EntityAvatar.propTypes = {
+CoverAvatar.propTypes = {
   entity: PropTypes.object,
   stack: PropTypes.bool
 };
 
-export default EntityAvatar;
+export default CoverAvatar;
