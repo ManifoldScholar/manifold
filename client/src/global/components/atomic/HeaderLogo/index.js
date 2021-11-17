c6167a1fe599dc6e68f2873368d7536fd3dc1a35
Zach Davis <zach@castironcoding.com>
2021-11-16 17:04:31 -0800

    [C] Suppress Emotion triggered hydration warning
    For some reason that we haven't been able to discovery, Emotion triggers
    a react hydration mismatch for the first styled component that's
    rendered, which, for now, is this HeaderLogo component. The warning
    doesn't seem to be a problem, so we're going to suppress it for now and
    keep an eye on it. Let's see if a future version of Emotion resolves it
    for us and allows us to remove the suppressHydrationWarning attribute.

--- client/src/global/components/atomic/HeaderLogo/index.js
+++ client/src/global/components/atomic/HeaderLogo/index.js
@@ -4,8 +4,18 @@ import { Link } from "react-router-dom";
 
 export default function HeaderLogo({ children, as = "a", href = "/", to }) {
   const linkProps = { href, to };
+  // For some reason that we haven't been able to discovery, Emotion triggers a react
+  // hydration mismatch for the first styled component that's rendered, which, for now,
+  // is this HeaderLogo component. The warning doesn't seem to be a problem, so we're
+  // going to suppress it for now and keep an eye on it. Let's see if a future version
+  // of Emotion resolves it for us and allows us to remove the suppressHydrationWarning
+  // attribute.
   return (
-    <Styled.Link as={as === "Link" ? Link : as} {...linkProps}>
+    <Styled.Link
+      suppressHydrationWarning
+      as={as === "Link" ? Link : as}
+      {...linkProps}
+    >
       {children}
     </Styled.Link>
   );
