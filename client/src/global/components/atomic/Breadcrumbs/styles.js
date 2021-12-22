78eeb1f382ab03b9c7fc5d7368f559e34b50e955
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2021-12-21 16:42:54 -0800

    [F] Add styles and implement Breadcrumbs in backend

--- client/src/global/components/atomic/Breadcrumbs/styles.js
+++ client/src/global/components/atomic/Breadcrumbs/styles.js
@@ -1,19 +1,65 @@
 import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
+import IconComposer from "global/components/utility/IconComposer";
 import {
   respond,
   utilityPrimary,
   containerPrototype,
   defaultTransitionProps
 } from "theme/styles/mixins";
+import { transientOptions } from "helpers/emotionHelpers";
 
 export const Outer = styled.nav`
+  --Breadcrumb-margin-inline: 16px;
+
+  ${utilityPrimary}
   width: 100%;
+  line-height: 1.4;
+
   background-color: var(--box-bg-color);
+  color: var(--weak-color);
+  font-size: 13px;
+  letter-spacing: 0.1em;
+  padding: 13px 0 15px;
+
+  ${respond(
+    `
+    padding: 18px 0 20px;
+    font-size: 14px;
+  `,
+    40
+  )}
 `;
 
-export const OuterSecondary = styled(Outer)`
-  background-color: var(--box-medium-bg-color);
+export const BackendOuter = styled(Outer, transientOptions)`
+  --Breadcrumb-margin-inline: 0.5em;
+
+  background-color: var(--color-base-neutral100);
+  color: var(--box-color);
+  font-size: 16px;
+  letter-spacing: 0;
+  padding: 0.5em 1em 0.6em;
+  border-radius: var(--box-border-radius) var(--box-border-radius) 0 0;
+  text-transform: none;
+
+  ${respond(
+    `
+    padding: 0.5em 1em 0.6em;
+    font-size: 16px;
+  `,
+    40
+  )}
+
+  ${respond(
+    `
+    padding: 0.45em 1em 0.5em;
+    font-size: 20px;
+  `,
+    50
+  )}
+
+  ${({ $hideOnDesktop }) =>
+    $hideOnDesktop && `${respond(`display: none;`, 75)}`}
 `;
 
 export const Inner = styled.div`
@@ -21,35 +67,25 @@ export const Inner = styled.div`
   position: relative;
   display: flex;
   align-items: center;
-  font-size: 13px;
-  background-color: inherit;
-  color: var(--weak-color);
 `;
 
-export const InnerSecondary = styled(Inner)`
-  --Breadcrumb-hover-color: var(--color-neutral-text-extra-dark);
-
-  padding-block-start: 30px;
-  padding-block-end: 28px;
-  background-color: inherit;
-  font-size: 14px;
-  color: var(--color-neutral-text-dark);
+export const BackendInner = styled(Inner)`
+  max-width: 100%;
+  padding-inline-start: 0;
+  padding-inline-end: 0;
 `;
 
 export const Breadcrumb = styled(Link)`
-  ${utilityPrimary}
   display: flex;
-  padding: 13px 0 15px;
-  line-height: 1.4;
+  align-items: center;
   text-decoration: none !important;
-  letter-spacing: 0.1em;
   transition: color ${defaultTransitionProps};
 
   & + & {
     ::before {
       content: " / ";
       display: inline;
-      margin-inline-start: 16px;
+      margin-inline-start: var(--Breadcrumb-margin-inline);
     }
 
     &:last-child {
@@ -64,20 +100,24 @@ export const Breadcrumb = styled(Link)`
   &:hover {
     color: var(--hover-color);
   }
+`;
 
-  ${respond(
-    `
-    padding: 18px 0 20px;
-    font-size: 14px;
-  `,
-    40
-  )}
+export const Icon = styled(IconComposer)`
+  flex-shrink: 0;
+  position: relative;
+
+  ${({ $desktop }) => {
+    if ($desktop) {
+      return `
+        display: none;
+        top: 0.05em;
+        ${respond(`display: inline-block;`, 50)}
+      `;
+    }
+    return `${respond(`display: none;`, 50)}`;
+  }}
 `;
 
 export const Label = styled.span`
-  margin-inline-start: 16px;
-
-  svg + & {
-    padding-top: 2px;
-  }
+  margin-inline-start: var(--Breadcrumb-margin-inline);
 `;
