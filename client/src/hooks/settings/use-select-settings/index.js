e2e0ba8eeeb54740acb289028d89cb4b34c84483
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-01-04 17:23:18 -0800

    [F] Add useSelectSettings hook

---
+++ client/src/hooks/settings/use-select-settings/index.js
@@ -0,0 +1,11 @@
+import { useSelector } from "react-redux";
+import { requests } from "api";
+import { select } from "utils/entityUtils";
+
+export default function useSelectSettings() {
+  const settings = useSelector(state =>
+    select(requests.settings, state.entityStore)
+  );
+
+  return settings;
+}
