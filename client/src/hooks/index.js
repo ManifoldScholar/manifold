32cb7151bc770a423044850f704e17bf434408e8
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-01-05 16:45:42 -0800

    [C] Add usePaginationState hook

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -37,3 +37,4 @@ export { default as usePaginationState } from "./api/usePaginationState";
 export { default as useCurrentUser } from "./useCurrentUser";
 export { default as useAuthentication } from "./useAuthentication";
 export { default as useSelectSettings } from "./settings/use-select-settings";
+export { default as usePaginationState } from "./pagination/use-pagination-state";
