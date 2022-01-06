21d381337021f4e029caee74ca17023de01bb7d8
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-01-05 17:15:05 -0800

    [C] Add useFilterState hook

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -34,3 +34,4 @@ export { default as useCollapseContext } from "./contexts/use-collapse-context";
 export { default as useCurrentUser } from "./user/use-current-user";
 export { default as useSelectSettings } from "./settings/use-select-settings";
 export { default as usePaginationState } from "./pagination/use-pagination-state";
+export { default as useFilterState } from "./filters/use-filter-state";
