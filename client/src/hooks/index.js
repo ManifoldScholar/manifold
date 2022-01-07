104ff06970ea53adde685e5a2db7e47532ef0954
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-01-06 17:03:45 -0800

    [F] Add redux hooks for journals

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -33,4 +33,11 @@ export { default as useCollapseContext } from "./contexts/use-collapse-context";
 
 export { default as useCurrentUser } from "./user/use-current-user";
 export { default as useSelectSettings } from "./settings/use-select-settings";
-export { default as useSelectJournalIssues } from "./journals/use-select-journal-issues";
+export { default as useSelectAllIssues } from "./journals/use-select-all-issues";
+export { default as usePaginationState } from "./pagination/use-pagination-state";
+export { default as useFilterState } from "./filters/use-filter-state";
+export { default as useDispatchAllIssues } from "./journals/use-dispatch-all-issues";
+export { default as useSelectAllJournals } from "./journals/use-select-all-journals";
+export { default as useSelectJournal } from "./journals/use-select-journal";
+export { default as useSelectVolume } from "./journals/use-select-volume";
+export { default as useUrlFromState } from "./url/use-url-from-state";
