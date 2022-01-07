73193d1d6d15cf3909bfdec80be97a07a84f5ca0
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-01-07 12:22:25 -0800

    [C] Add useUrlFromState hook

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -35,3 +35,8 @@ export { default as useCurrentUser } from "./user/use-current-user";
 export { default as useSelectSettings } from "./settings/use-select-settings";
 export { default as usePaginationState } from "./pagination/use-pagination-state";
 export { default as useFilterState } from "./filters/use-filter-state";
+export { default as useSelectJournal } from "./journals/use-select-journal";
+export { default as useSelectVolume } from "./journals/use-select-volume";
+export { default as useSelectAllJournals } from "./journals/use-select-all-journals";
+export { default as useDispatchAllIssues } from "./journals/use-dispatch-all-issues";
+export { default as useUrlFromState } from "./url/use-url-from-state";
---
+++ client/src/hooks/url/use-url-from-state/index.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import queryString from "query-string";
+import { useHistory } from "react-router-dom";
+
+export default function useUrlFromState(location, filterState, page) {
+  const history = useHistory();
+
+  function updateUrlFromState() {
+    const { pathname } = location;
+    const params = { ...filterState, page };
+    const search = queryString.stringify(params);
+    history.push({ pathname, search });
+  }
+
+  useEffect(
+    () => updateUrlFromState(),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [JSON.stringify(filterState), page]
+  );
+}
