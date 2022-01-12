bd2ee6882578c7779afb2e9f7b5e65ab9580d0a6
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-01-12 15:32:53 -0800

    [WIP] Add and implement useDispatchJournal

--- client/src/hooks/journals/use-select-volume/index.js
+++ client/src/hooks/journals/use-select-volume/index.js
@@ -1,25 +1,12 @@
-import { useState } from "react";
+import { useSelector } from "react";
 import { fixtures } from "helpers/storybook/exports";
-import shuffle from "lodash/shuffle";
 
-// Mock data until we have the api
-const issues = fixtures
-  .collectionFactory("issue", 4)
-  .map(issue => issue.data)
-  .map(issue => {
-    const color = shuffle(["primary", "secondary", "tertiary", "quinary"])[0];
-    return {
-      ...issue,
-      attributes: { ...issue.attributes, avatarColor: color }
-    };
-  });
-const sampleData = fixtures.collectionFactory("volume", 1);
-
-export default function useSelectVolume() {
-  const volume = sampleData;
-  const volumeMeta = {
-    pagination: { totalCount: 12, perPage: 12, currentPage: 1 }
-  };
-  // const issues = volume.relationships.issues
-  return { volume, volumeMeta, issues };
+// Redo this hook (or don't use if all volume data is contained in the journal) with api
+export default function useSelectVolume(journal, slug) {
+  const volume = journal
+    ? journal.relationships.volumes.filter(
+        item => item.attributes.slug === slug
+      )[0]
+    : null;
+  return { volume };
 }
