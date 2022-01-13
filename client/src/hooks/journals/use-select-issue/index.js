c30c81853f90ea89c8de813d827c4ad731c50c69
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-01-13 14:16:39 -0800

    [F] Add issue redux hooks

---
+++ client/src/hooks/journals/use-select-issue/index.js
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux";
+import { requests } from "api";
+import { grab, select } from "utils/entityUtils";
+
+export default function useSelectJournal(match) {
+  const issueResponse = useSelector(state =>
+    select("issue", state.entityStore)
+  );
+  const issue = useSelector(state =>
+    grab("issues", match.params.id, state.entityStore)
+  );
+  return { issue, issueResponse };
+}
