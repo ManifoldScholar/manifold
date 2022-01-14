1e6ec75d049d6827ea8f91fb9e6fd60a72e99f49
Dana Johnson <dana@castironcoding.com>
2022-01-14 14:33:10 -0800

    [F] Add initial VolumeDetail UI components

--- client/src/frontend/components/journal/index.js
+++ client/src/frontend/components/journal/index.js
@@ -1,7 +1,9 @@
 import IssueList from "./IssueList";
 import Metadata from "./Metadata";
+import VolumeDetail from "./VolumeDetail";
 
 export default {
   IssueList,
-  Metadata
+  Metadata,
+  VolumeDetail
 };
--- client/src/hooks/journals/use-select-volume/index.js
+++ client/src/hooks/journals/use-select-volume/index.js
@@ -2,11 +2,11 @@ import { useSelector } from "react";
 import { fixtures } from "helpers/storybook/exports";
 
 // Redo this hook (or don't use if all volume data is contained in the journal) with api
-export default function useSelectVolume(journal, slug) {
-  const volume = journal
-    ? journal.relationships.volumes.filter(
-        item => item.attributes.slug === slug
-      )[0]
-    : null;
-  return { volume };
+export default function useSelectVolume(match, journal) {
+  if (!journal) return { volume: null, volumeResponse: null };
+
+  const volume = journal.relationships.volumes.find(
+    v => v.id === match.params.volumeSlug
+  );
+  return { volume, volumeResponse: volume };
 }
