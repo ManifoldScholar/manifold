14fb49d69f91a0ddef499dd8691a4ed31a11139e
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-01-21 15:08:48 -0800

    [B] Remove duplicate sr text in user menu

--- client/src/global/components/UserMenuBody/Link.js
+++ client/src/global/components/UserMenuBody/Link.js
@@ -1,28 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { NavLink, withRouter } from "react-router-dom";
-import { useUID } from "react-uid";
 import IconComposer from "global/components/utility/IconComposer";
 
-function UserMenuBodyLink({ as = "a", to, title, srTitle, icon, onClick }) {
+function UserMenuBodyLink({ as = "a", to, title, icon, onClick }) {
   const Tag = as === "a" ? NavLink : "button";
-  const uid = useUID();
   return (
     <li className="user-menu__item">
-      <Tag
-        to={to}
-        className="user-menu__link"
-        onClick={onClick}
-        aria-describedby={srTitle ? uid : null}
-      >
+      <Tag to={to} className="user-menu__link" onClick={onClick}>
         <IconComposer icon={icon} size={32} className="user-menu__icon" />
         <span className="user-menu__link-text">{title}</span>
       </Tag>
-      {srTitle && (
-        <span id={uid} className="screen-reader-text">
-          {srTitle}
-        </span>
-      )}
     </li>
   );
 }
