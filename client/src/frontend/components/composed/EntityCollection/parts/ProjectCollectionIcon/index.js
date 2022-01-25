85b6353f9bdd7a5706e8c25f33106e2f10fa5577
Dana Johnson <dana@castironcoding.com>
2022-01-25 09:46:13 -0800

    [B] Fix project collection icon render
    Checks if the collection has a defined icon before trying to render it

--- client/src/frontend/components/composed/EntityCollection/parts/ProjectCollectionIcon/index.js
+++ client/src/frontend/components/composed/EntityCollection/parts/ProjectCollectionIcon/index.js
@@ -3,19 +3,18 @@ import PropTypes from "prop-types";
 import IconComputed from "global/components/icon-computed";
 
 function ProjectCollectionIcon({ projectCollection, ...props }) {
-  const { icon, customIconStyles } = projectCollection.attributes;
+  const {
+    icon,
+    customIconStyles: { smallSquare }
+  } = projectCollection.attributes;
   const fill =
     icon === "new-round" ? "var(--color-interaction-light)" : undefined;
-  if (customIconStyles?.smallSquare)
-    return (
-      <img
-        src={customIconStyles.smallSquare}
-        alt=""
-        width={60}
-        height={60}
-        {...props}
-      />
-    );
+
+  if (!icon && !smallSquare) return null;
+
+  if (smallSquare)
+    return <img src={smallSquare} alt="" width={60} height={60} {...props} />;
+
   return (
     <IconComputed.ProjectCollection
       icon={icon}
