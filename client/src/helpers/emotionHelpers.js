6cd6413336db1db4c46942860a1d1877e3f442c1
Dana Johnson <dana@castironcoding.com>
2022-01-25 13:42:48 -0800

    [B] Fix EntityHero callout issues
    * Fixes emotion prop-forwarding to pass url prop to UserLink component
    * Fixes rendered component for "read" and "download" callout types

--- client/src/helpers/emotionHelpers.js
+++ client/src/helpers/emotionHelpers.js
@@ -1,7 +1,13 @@
 import isPropValid from "@emotion/is-prop-valid";
 
+// props that should always be forwarded
+const ALLOWED_PROPS = ["url"];
+
 // styled-components-style transient prop support (`$someProp`)
 export const transientOptions = {
-  shouldForwardProp: propName =>
-    isPropValid(propName) && !propName.startsWith("$")
+  shouldForwardProp: propName => {
+    if (ALLOWED_PROPS.indexOf(propName) >= 0) return true;
+
+    return isPropValid(propName) && !propName.startsWith("$");
+  }
 };
