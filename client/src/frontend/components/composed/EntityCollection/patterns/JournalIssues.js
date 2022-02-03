3dc3c5f11fd6d69e129cfa2e9d887d5845903e50
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-02-03 14:24:57 -0800

    [WIP] Render data in all journals route

--- client/src/frontend/components/composed/EntityCollection/patterns/JournalIssues.js
+++ client/src/frontend/components/composed/EntityCollection/patterns/JournalIssues.js
@@ -1,26 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import memoize from "lodash/memoize";
 import lh from "helpers/linkHandler";
 import EntityThumbnail from "global/components/atomic/EntityThumbnail";
 import ThumbnailGrid from "global/components/composed/ThumbnailGrid";
 import { FooterLink, ProjectCollectionIcon } from "../parts";
 import EntityCollection from "../EntityCollection";
 import { getHeroImage, getHeaderLayout } from "../helpers";
-import { fixtures } from "helpers/storybook/exports";
-
-const mapIssues = memoize(journal => {
-  // TODO: update once API is in place
-  return fixtures.collectionFactory("issue", 4).map(issue => issue.data);
-  // return journal.relationships.collectionProjects.map(
-  //   cp => cp.relationships.project
-  // );
-});
-
-function getIssues(journal, limit) {
-  const issues = mapIssues(journal);
-  return issues.slice(0, limit);
-}
 
 function JournalIssuesEntityCollection({
   journal,
@@ -29,14 +14,15 @@ function JournalIssuesEntityCollection({
   limit,
   ...passThroughProps
 }) {
-  const { title, slug, descriptionFormatted: description } = journal.attributes;
-  const issues = getIssues(journal, limit);
+  if (!journal) return null;
+
+  const { title, slug, descriptionFormatted: description } =
+    journal.attributes ?? {};
+  const issues = journal.relationships?.recentJournalIssues?.slice(0, limit);
   const image = getHeroImage(journal);
   const headerLayout = getHeaderLayout(journal);
 
-  // TODO: update once API is in place
-  // const totalIssueCount = journal.relationships.collectionProjects.length;
-  const totalIssueCount = issues.length;
+  const totalIssueCount = journal.attributes?.journalIssuesCount;
   const footerLinkText =
     totalIssueCount > limit ? "See all issues" : "Visit the journal page";
 
