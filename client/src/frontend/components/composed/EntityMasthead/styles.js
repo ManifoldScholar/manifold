25fec0ed05aafbd1fbcb54d34b9eec5f3fff55eb
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-02-04 11:32:56 -0800

    [F] Defer to journal hero if no issue hero

--- client/src/frontend/components/composed/EntityMasthead/styles.js
+++ client/src/frontend/components/composed/EntityMasthead/styles.js
@@ -14,7 +14,7 @@ export const Wrapper = styled("div", transientOptions)`
 
 export const Image = styled.img`
   width: 100%;
-  height: 100%;
+  height: auto;
   object-fit: cover;
   object-position: top;
 `;
