ce38122292916c4aa2e1bf1e164264cbace653c7
Zach Davis <zach@castironcoding.com>
2022-02-04 15:01:50 -0800

    [F] Add delete buttons to issues, volumes

--- client/src/backend/containers/journal/VolumeEdit.js
+++ client/src/backend/containers/journal/VolumeEdit.js
@@ -3,27 +3,58 @@ import PropTypes from "prop-types";
 import Volume from "backend/components/volume";
 import Navigation from "backend/components/navigation";
 import { journalVolumesAPI } from "api";
-import { useFetch } from "hooks";
+import { useFetch, useApiCallback } from "hooks";
+import withConfirmation from "hoc/withConfirmation";
+import lh from "helpers/linkHandler";
 
 function JournalVolumeEdit({
   refreshVolumes,
   closeUrl,
   match,
   journal,
-  history
+  history,
+  confirm
 }) {
   const { data: journalVolume } = useFetch({
     request: [journalVolumesAPI.show, match.params.vId]
   });
+
+  const destroy = useApiCallback(journalVolumesAPI.destroy, {
+    removes: journalVolume
+  });
+
+  const destroyAndRedirect = useCallback(() => {
+    const redirect = () =>
+      history.push(lh.link("backendJournalVolumes", journal.id));
+    destroy(journalVolume.id).then(
+      () => redirect(),
+      () => redirect()
+    );
+  }, [destroy, history, journal]);
+
+  const onDelete = useCallback(() => {
+    const heading = "Are you sure you want to delete this volume?";
+    const message = "This action cannot be undone.";
+    confirm(heading, message, destroyAndRedirect);
+  }, [destroyAndRedirect, confirm]);
 
   const refreshAndRedirect = useCallback(() => {
     refreshVolumes();
     history.push(closeUrl, { keepNotifications: false });
   }, [history, closeUrl, refreshVolumes]);
 
+  const buttons = [
+    {
+      onClick: onDelete,
+      label: "delete",
+      icon: "delete32",
+      className: "utility-button__icon--notice"
+    }
+  ];
+
   return (
     <div>
-      <Navigation.DrawerHeader title="Edit Volume" />
+      <Navigation.DrawerHeader title="Edit Volume" buttons={buttons} />
       <Volume.Form
         model={journalVolume}
         journalId={journal.id}
@@ -40,4 +71,4 @@ JournalVolumeEdit.propTypes = {
   refreshVolumes: PropTypes.func.isRequired
 };
 
-export default JournalVolumeEdit;
+export default withConfirmation(JournalVolumeEdit);
