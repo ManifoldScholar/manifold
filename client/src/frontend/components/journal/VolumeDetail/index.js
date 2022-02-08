979d91dee1b1962c2bcd34dd5511549dc1905b0c
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-02-08 13:26:05 -0800

    [B] Show placeholder in empty VolumeDetail

--- client/src/frontend/components/journal/VolumeDetail/index.js
+++ client/src/frontend/components/journal/VolumeDetail/index.js
@@ -7,32 +7,35 @@ import ContentBlockList from "frontend/components/content-block-list/List";
 import * as Styled from "./styles";
 
 function VolumeDetail({ journal, volume }) {
-  const journalTitle = journal.attributes.title;
-  const volumeTitle = `Volume ${volume.attributes.number}`;
-  const issues = volume.relationships.journalIssues;
+  if (!journal || !volume) return null;
+
+  const journalTitle = journal.attributes?.title;
+  const volumeTitle = `Volume ${volume.attributes?.number}`;
+  const issues = volume.relationships?.journalIssues ?? [];
 
   return (
     <Styled.Wrapper>
       <EntityGroup title={`${journalTitle}: ${volumeTitle}`}>
-        {issues.map(issue => (
-          <Styled.IssueWrapper key={issue.id}>
-            <ThumbnailGrid minColumns={4} minItemWidth="210px">
-              {({ stack }) => (
-                <EntityThumbnail
-                  entity={issue}
-                  stack={stack}
-                  key={issue.id}
-                  parentView
-                />
-              )}
-            </ThumbnailGrid>
-            <ContentBlockList
-              entity={issue.relationships.project}
-              nested
-              hideHeader
-            />
-          </Styled.IssueWrapper>
-        ))}
+        {!!issues.length &&
+          issues.map(issue => (
+            <Styled.IssueWrapper key={issue.id}>
+              <ThumbnailGrid minColumns={4} minItemWidth="210px">
+                {({ stack }) => (
+                  <EntityThumbnail
+                    entity={issue}
+                    stack={stack}
+                    key={issue.id}
+                    parentView
+                  />
+                )}
+              </ThumbnailGrid>
+              <ContentBlockList
+                entity={issue.relationships?.project}
+                nested
+                hideHeader
+              />
+            </Styled.IssueWrapper>
+          ))}
       </EntityGroup>
     </Styled.Wrapper>
   );
