72e94b17531c4927772aa6da599e99cd09bcdf3f
Dana Johnson <dana@castironcoding.com>
2022-02-10 12:05:46 -0800

    [F] Add pagination to journal volumes route

--- client/src/frontend/containers/JournalVolumesList/index.js
+++ client/src/frontend/containers/JournalVolumesList/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useFetch, usePaginationState } from "hooks";
 import { journalVolumesAPI } from "api";
-import GlobalUtility from "global/components/utility";
 import EntityHeadContent from "frontend/components/atomic/EntityHeadContent";
 import EntityMasthead from "frontend/components/composed/EntityMasthead";
 import EntityCollection from "frontend/components/composed/EntityCollection";
@@ -11,18 +10,18 @@ import lh from "helpers/linkHandler";
 
 export default function JournalVolumesList({ journal }) {
   const { id } = useParams();
-  const [pagination, setPageNumber] = usePaginationState();
+  const [pagination, setPageNumber] = usePaginationState(1, 5);
   const { data: volumes, meta } = useFetch({
     request: [journalVolumesAPI.index, id, pagination]
   });
 
   if (!journal || !volumes || !meta) return null;
 
-  const showPagination = meta.pagination?.totalPages > 1;
+  const { titlePlaintext } = journal.attributes;
 
   return (
     <>
-      <h1 className="screen-reader-text">Volumes</h1>
+      <h1 className="screen-reader-text">{`${titlePlaintext}: Volumes`}</h1>
       <RegisterBreadcrumbs
         breadcrumbs={[
           {
@@ -42,24 +41,15 @@ export default function JournalVolumesList({ journal }) {
       <EntityHeadContent entity={journal} />
       <EntityMasthead entity={journal} />
       <EntityCollection.JournalVolumes
+        title={`${titlePlaintext}: Volumes`}
         journal={journal}
         volumes={volumes}
         meta={meta}
         paginationProps={{
           paginationClickHandler: page => () => setPageNumber(page),
-          paginationTarget: ""
+          paginationTarget: "#"
         }}
       />
-      {showPagination && (
-        <section>
-          <div className="container">
-            <GlobalUtility.Pagination
-              paginationClickHandler={page => () => setPageNumber(page)}
-              pagination={meta.pagination}
-            />
-          </div>
-        </section>
-      )}
     </>
   );
 }
