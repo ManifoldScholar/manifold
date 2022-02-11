0a7b3988f795fb3d85280563b4b89566d318f520
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-02-11 12:13:57 -0800

    [F] Add warning and hide info block for empty journals

--- client/src/frontend/components/journal/Metadata/index.js
+++ client/src/frontend/components/journal/Metadata/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ContentBlock from "frontend/components/content-block/Block";
+import isEmpty from "lodash/isEmpty";
 
 function Metadata({ journal }) {
+  const { metadata } = journal.attributes;
+
+  if (!metadata || isEmpty(metadata)) return null;
+
   return (
     <ContentBlock
       block={{
