890588a8385a98729222247bffbfeb2247e80f19
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-02-11 10:58:45 -0800

    [B] Fix EntityHero theme prop for issues

--- client/src/frontend/components/composed/EntityHero/patterns/IssueHero.js
+++ client/src/frontend/components/composed/EntityHero/patterns/IssueHero.js
@@ -29,7 +29,7 @@ export default function IssueHero({ entity, mock }) {
     <>
       <EntityMasthead entity={entity} />
       <EntityHero
-        theme="journal"
+        theme="issue"
         TitleComponent={({ isStandalone }) => (
           <Title entity={entity} isStandalone={isStandalone} />
         )}
