ManifoldScholar/manifold

commit 3e54e326ec14fcc47f8d1770d6a92869d09b589a

[B] Fix counts in RG summary box

client/src/frontend/components/reading-group/headings/Group/GroupSummaryBox/index.js
@@ -6,11 +6,38 @@ import { getEntityCollection } from "frontend/components/collecting/helpers";
 import Section from "./Section";
 import Item from "./Item";
 
+function getUniqueTypes(mappingValues) {
+  const allTypes = mappingValues.reduce((previous, current) => [
+    ...Object.keys(previous),
+    ...Object.keys(current)
+  ]);
+  return [...new Set(allTypes)];
+}
+
+function aggregateMappingsByType(type, mappingValues) {
+  return mappingValues
+    .reduce((previous, current) => {
+      const prevValues = previous[type] || [];
+      const currentValues = current[type] || [];
+      return prevValues.concat(currentValues);
+    })
+    .filter(Boolean);
+}
+
 function collectedIdsForCollectionByType(readingGroup) {
   const collection = getEntityCollection(readingGroup);
+
   if (!collection?.attributes) return [];
-  const mappings = collection.attributes.categoryMappings;
-  return Object.assign({}, ...Object.values(mappings));
+
+  const mappingValues = Object.values(collection.attributes.categoryMappings);
+  const uniqueTypes = getUniqueTypes(mappingValues);
+
+  return Object.assign(
+    {},
+    ...uniqueTypes.map(type => ({
+      [type]: aggregateMappingsByType(type, mappingValues)
+    }))
+  );
 }
 
 function GroupSummaryBox({ readingGroup }) {
@@ -21,8 +48,7 @@ function GroupSummaryBox({ readingGroup }) {
     annotationsCount,
     membershipsCount,
     currentUserRole,
-    currentUserAnnotationsCount = 12,
-    currentUserHighlightsCount = 9
+    currentUserCounts
   } = readingGroup.attributes;
   const collected = collectedIdsForCollectionByType(readingGroup);
 
@@ -64,10 +90,10 @@ function GroupSummaryBox({ readingGroup }) {
         </Section>
         <Section label="Yours">
           <Item labelText="Annotations" icon="interactAnnotate32">
-            {currentUserAnnotationsCount}
+            {currentUserCounts.annotationsCount}
           </Item>
           <Item labelText="Highlights" icon="interactHighlight32">
-            {currentUserHighlightsCount}
+            {currentUserCounts.highlightsCount}
           </Item>
         </Section>
         <Section label="Content" columns={2}>

commit 3a45f7e7bb9d33dcab1d5f93898b393e1c50a38b

[B] Fix regressions in RG counts
Initially introduced in 3e54e32

client/src/frontend/components/reading-group/headings/Group/GroupSummaryBox/index.js
@@ -6,32 +6,36 @@ import { getEntityCollection } from "frontend/components/collecting/helpers";
 import Section from "./Section";
 import Item from "./Item";
 
-function getUniqueTypes(mappingValues) {
-  const allTypes = mappingValues.reduce((previous, current) => [
-    ...Object.keys(previous),
-    ...Object.keys(current)
-  ]);
+function getUniqueTypes(mappings) {
+  const allTypes = mappings.flatMap(category => Object.keys(category));
   return [...new Set(allTypes)];
 }
 
 function aggregateMappingsByType(type, mappingValues) {
-  return mappingValues
-    .reduce((previous, current) => {
-      const prevValues = previous[type] || [];
-      const currentValues = current[type] || [];
-      return prevValues.concat(currentValues);
-    })
-    .filter(Boolean);
+  // get arrays of collected IDs by type from each category
+  const valuesByType = mappingValues.map(category => category[type] || []);
+  // reduce to a single array
+  return valuesByType.reduce(
+    (previous, current) => previous.concat(current),
+    []
+  );
 }
 
 function collectedIdsForCollectionByType(readingGroup) {
   const collection = getEntityCollection(readingGroup);
 
-  if (!collection?.attributes) return [];
+  if (!collection?.attributes) return {};
 
+  // get an array of objects containing collected IDs by type
   const mappingValues = Object.values(collection.attributes.categoryMappings);
+
+  // if nothing's been collected, just return an empty object
+  if (!mappingValues.length) return {};
+
+  // get an array of unique types for all collected items
   const uniqueTypes = getUniqueTypes(mappingValues);
 
+  // return an object with counts by type
   return Object.assign(
     {},
     ...uniqueTypes.map(type => ({