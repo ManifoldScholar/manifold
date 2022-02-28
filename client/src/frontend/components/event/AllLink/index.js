59e43173be524612c011b522e3487ea9a339d518
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-02-28 15:29:16 -0800

    [F] Localize frontend/components/event

--- client/src/frontend/components/event/AllLink/index.js
+++ client/src/frontend/components/event/AllLink/index.js
@@ -1,31 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import lh from "helpers/linkHandler";
 import * as Styled from "./styles";
 
-export default class EventAllLink extends Component {
-  static displayName = "Event.AllLink";
+export default function EventAllLink({ threshold, entity }) {
+  const { t } = useTranslation();
 
-  static propTypes = {
-    threshold: PropTypes.number.isRequired,
-    entity: PropTypes.object.isRequired
-  };
+  if (entity.attributes.eventCount <= threshold) return null;
 
-  render() {
-    const { entity, threshold } = this.props;
-
-    if (entity.attributes.eventCount <= threshold) return null;
-
-    return (
-      <Styled.Wrapper>
-        <Link
-          to={lh.link("frontendProjectEvents", entity.attributes.slug)}
-          className="button-primary"
-        >
-          <span className="button-primary__text">See all Activity</span>
-        </Link>
-      </Styled.Wrapper>
-    );
-  }
+  return (
+    <Styled.Wrapper>
+      <Link
+        to={lh.link("frontendProjectEvents", entity.attributes.slug)}
+        className="button-primary"
+      >
+        <span className="button-primary__text">
+          {t("navigation.see_all_activity")}
+        </span>
+      </Link>
+    </Styled.Wrapper>
+  );
 }
+
+EventAllLink.displayName = "Event.AllLink";
+
+EventAllLink.propTypes = {
+  threshold: PropTypes.number.isRequired,
+  entity: PropTypes.object.isRequired
+};
