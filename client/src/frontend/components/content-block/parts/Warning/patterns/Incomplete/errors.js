6e039280bc11cc9a83e17c4da90a18129a483765
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-02-28 15:28:50 -0800

    [F] Localize frontend/components/content-block

--- client/src/frontend/components/content-block/parts/Warning/patterns/Incomplete/errors.js
+++ client/src/frontend/components/content-block/parts/Warning/patterns/Incomplete/errors.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Trans } from "react-i18next";
 import has from "lodash/has";
 import lh from "helpers/linkHandler";
 
@@ -20,41 +21,41 @@ export function generateError(key, block) {
   const availableErrors = {
     "Content::TableOfContentsBlock": {
       text: (
-        <>
+        <Trans i18nKey="errors.content_block.no_contents">
           Specify a text to <Link to={blockEditLink}>fix this block</Link>.
-        </>
+        </Trans>
       )
     },
     "Content::RecentActivityBlock": {
       has_activity: (
-        <>
+        <Trans i18nKey="errors.content_block.no_activity">
           No activity has been recorded for this project. This warning will
           disappear once there is project activity.
-        </>
+        </Trans>
       )
     },
     "Content::MarkdownBlock": {
       body: (
-        <>
+        <Trans i18nKey="errors.content_block.no_markdown">
           Add Markdown content to <Link to={blockEditLink}>fix this block</Link>
           .
-        </>
+        </Trans>
       )
     },
     "Content::TextsBlock": {
       texts: (
-        <>
+        <Trans i18nKey="errors.content_block.no_texts">
           <Link to={getProjectEditLink("backendProjectTexts")}>
             Add texts to the project
           </Link>{" "}
           or <Link to={blockEditLink}>adjust the block configuration</Link> to
           fix this block.
-        </>
+        </Trans>
       )
     },
     "Content::ResourcesBlock": {
       resources_or_collections: (
-        <>
+        <Trans i18nKey="errors.content_block.no_resources">
           Add{" "}
           <Link to={getProjectEditLink("backendProjectResources")}>
             resources
@@ -64,17 +65,17 @@ export function generateError(key, block) {
             resource collections
           </Link>{" "}
           to the project to fix this block.
-        </>
+        </Trans>
       )
     },
     "Content::MetadataBlock": {
       has_metadata: (
-        <>
+        <Trans i18nKey="errors.content_block.no_metadata">
           <Link to={getProjectEditLink("backendProjectMetadata")}>
             Add project metadata
           </Link>{" "}
           to fix this block.
-        </>
+        </Trans>
       )
     }
   };
@@ -91,7 +92,9 @@ export function getDefaultError(block) {
   );
   return (
     <span key="default-error">
-      <Link to={blockEditLink}>Fix this content block.</Link>
+      <Trans i18nKey="errors.content_block.default">
+        <Link to={blockEditLink}>Fix this content block.</Link>
+      </Trans>
     </span>
   );
 }
