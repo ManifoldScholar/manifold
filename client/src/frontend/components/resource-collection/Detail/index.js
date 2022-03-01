057bf7e08addfbe151850a05dfe4c3ee4c1054eb
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-03-01 10:40:46 -0800

    [B] Fix filters and localize resource collection detail

--- client/src/frontend/components/resource-collection/Detail/index.js
+++ client/src/frontend/components/resource-collection/Detail/index.js
@@ -1,30 +1,43 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 import ResourceList from "frontend/components/resource-list";
 import { ListFilters } from "global/components/list";
 import Utility from "frontend/components/utility";
 import lh from "helpers/linkHandler";
 import Title from "../Title";
+import { useListFilters } from "hooks";
 import * as Styled from "./styles";
 
-export default class ResourceCollectionDetail extends PureComponent {
-  static displayName = "ResourceCollection.Detail";
+export default function ResourceCollectionDetail(props) {
+  const {
+    resourceCollection,
+    project,
+    resourceCollectionUrl,
+    slideshowResources,
+    slideshowPagination,
+    collectionResources,
+    resourceCollectionPagination,
+    resourceCollectionPaginationHandler,
+    dispatch,
+    filterChange,
+    initialFilterState
+  } = props;
 
-  static propTypes = {
-    resourceCollection: PropTypes.object,
-    project: PropTypes.object,
-    resourceCollectionUrl: PropTypes.string.isRequired,
-    slideshowResources: PropTypes.array,
-    slideshowPagination: PropTypes.object,
-    collectionResources: PropTypes.array,
-    resourceCollectionPagination: PropTypes.object,
-    resourceCollectionPaginationHandler: PropTypes.func,
-    dispatch: PropTypes.func,
-    filterChange: PropTypes.func.isRequired,
-    initialFilterState: PropTypes.object
-  };
+  const { t } = useTranslation();
 
-  renderDescription(description) {
+  const filterProps = useListFilters({
+    onFilterChange: filterChange,
+    initialState: initialFilterState,
+    resetState: initialFilterState,
+    options: {
+      sort: true,
+      kinds: resourceCollection.attributes.resourceKinds,
+      tags: resourceCollection.attributes.resourceTag
+    }
+  });
+
+  const renderDescription = description => {
     if (!description) return null;
     return (
       <div
@@ -32,64 +45,63 @@ export default class ResourceCollectionDetail extends PureComponent {
         dangerouslySetInnerHTML={{ __html: description }}
       />
     );
-  }
+  };
 
-  render() {
-    const project = this.props.project;
-    const resourceCollection = this.props.resourceCollection;
-    if (!project || !resourceCollection) return null;
+  if (!project || !resourceCollection) return null;
 
-    const attr = resourceCollection.attributes;
-    const count = attr.collectionResourcesCount;
-    return (
-      <section className="collection-detail">
-        <div className="collection-detail__container container flush-bottom">
-          <Title resourceCollection={resourceCollection} showCreatedAt />
-          {this.renderDescription(attr.descriptionFormatted)}
-          <div className="collection-detail__utility">
-            <Utility.ShareBar url={this.props.resourceCollectionUrl} />
-          </div>
+  const attr = resourceCollection.attributes;
+  const count = attr.collectionResourcesCount;
+
+  return (
+    <section className="collection-detail">
+      <div className="collection-detail__container container flush-bottom">
+        <Title resourceCollection={resourceCollection} showCreatedAt />
+        {renderDescription(attr.descriptionFormatted)}
+        <div className="collection-detail__utility">
+          <Utility.ShareBar url={resourceCollectionUrl} />
         </div>
-        <h2 className="screen-reader-text">Resource Slideshow</h2>
-        <ResourceList.Slideshow
-          resourceCollection={this.props.resourceCollection}
-          collectionResources={this.props.slideshowResources}
-          count={project.attributes.resourcesCount}
-          pagination={this.props.slideshowPagination}
-          dispatch={this.props.dispatch}
+      </div>
+      <h2 className="screen-reader-text">{t("sections.resource_slideshow")}</h2>
+      <ResourceList.Slideshow
+        resourceCollection={resourceCollection}
+        collectionResources={slideshowResources}
+        count={project.attributes.resourcesCount}
+        pagination={slideshowPagination}
+        dispatch={dispatch}
+      />
+      <div className="container flush-top collection-detail__resources">
+        <h2 className="screen-reader-text">{t("sections.resource_list")}</h2>
+        <Styled.EntityListTotal
+          linkTo={lh.link("frontendProjectResources", project.attributes.slug)}
+          entityName={t("glossary.resource", { count })}
+          count={count}
         />
-        <div className="container flush-top collection-detail__resources">
-          <h2 className="screen-reader-text">Resource List</h2>
-          <Styled.EntityListTotal
-            linkTo={lh.link(
-              "frontendProjectResources",
-              project.attributes.slug
-            )}
-            entityName="Resource"
-            count={count}
-          />
-          <ListFilters
-            onFilterChange={this.props.filterChange}
-            init={this.props.initialFilterState}
-            reset={this.props.initialFilterState}
-            options={{
-              sort: true,
-              kinds: resourceCollection.attributes.resourceKinds,
-              tags: resourceCollection.attributes.resourceTag
-            }}
-          />
-          <ResourceList.Cards
-            resourceCollection={this.props.resourceCollection}
-            project={this.props.project}
-            resources={this.props.collectionResources}
-            pagination={this.props.resourceCollectionPagination}
-            paginationClickHandler={
-              this.props.resourceCollectionPaginationHandler
-            }
-            itemHeadingLevel={3}
-          />
-        </div>
-      </section>
-    );
-  }
+        <ListFilters {...filterProps} />
+        <ResourceList.Cards
+          resourceCollection={resourceCollection}
+          project={project}
+          resources={collectionResources}
+          pagination={resourceCollectionPagination}
+          paginationClickHandler={resourceCollectionPaginationHandler}
+          itemHeadingLevel={3}
+        />
+      </div>
+    </section>
+  );
 }
+
+ResourceCollectionDetail.displayName = "ResourceCollection.Detail";
+
+ResourceCollectionDetail.propTypes = {
+  resourceCollection: PropTypes.object,
+  project: PropTypes.object,
+  resourceCollectionUrl: PropTypes.string.isRequired,
+  slideshowResources: PropTypes.array,
+  slideshowPagination: PropTypes.object,
+  collectionResources: PropTypes.array,
+  resourceCollectionPagination: PropTypes.object,
+  resourceCollectionPaginationHandler: PropTypes.func,
+  dispatch: PropTypes.func,
+  filterChange: PropTypes.func.isRequired,
+  initialFilterState: PropTypes.object
+};
