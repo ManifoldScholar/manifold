db438ffa83366aea33f00127416d14e04af27030
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-03-03 12:11:08 -0800

    [B] Fix unmemoized default prop in useSetLocation

--- client/src/hooks/useSetLocation/index.js
+++ client/src/hooks/useSetLocation/index.js
@@ -2,7 +2,9 @@ import { useEffect, useCallback } from "react";
 import queryString from "query-string";
 import { useHistory, useLocation } from "react-router-dom";
 
-export default function useSetLocation({ filters = {}, page }) {
+// Setting a default of {} or passing in an un-memoized empty object for filters will cause the container to exceed maximum depth. Leave undefined if no filters. --LD
+
+export default function useSetLocation({ filters, page }) {
   const history = useHistory();
   const { pathname } = useLocation();
 
