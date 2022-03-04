af9c0f17647f82bc9212785c7721e64daf4329b5
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-03-04 10:45:11 -0800

    [F] Localize containers/VolumeDetail

--- client/src/frontend/containers/VolumeDetail/index.js
+++ client/src/frontend/containers/VolumeDetail/index.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useMemo } from "react";
 import PropTypes from "prop-types";
 import { useParams } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import CheckFrontendMode from "global/containers/CheckFrontendMode";
 import lh from "helpers/linkHandler";
 import { journalVolumesAPI } from "api";
@@ -9,6 +10,7 @@ import EntityHeadContent from "frontend/components/atomic/EntityHeadContent";
 import EntityMasthead from "frontend/components/composed/EntityMasthead";
 import Journal from "frontend/components/journal";
 import { useFetch } from "hooks";
+import { capitalize } from "utils/string";
 
 function VolumeDetailContainer({ journal }) {
   const { volumeSlug: slug } = useParams();
@@ -16,27 +18,46 @@ function VolumeDetailContainer({ journal }) {
     request: [journalVolumesAPI.show, slug]
   });
 
+  const { t } = useTranslation();
+
+  const breadcrumbs = useMemo(
+    () => [
+      {
+        to: lh.link("frontendJournalsList"),
+        label: t("navigation.breadcrumbs.all_journals")
+      },
+      {
+        to: lh.link("frontendJournalDetail", journal.id),
+        label: journal?.attributes?.titlePlaintext
+      },
+      {
+        to: lh.link("frontendVolumeDetail", journal.id, slug),
+        label: `${t("glossary.volume_one")} ${volume?.attributes?.number}`
+      }
+    ],
+    [
+      t,
+      journal.id,
+      journal?.attributes?.titlePlaintext,
+      volume?.attributes?.number,
+      slug
+    ]
+  );
+
   return journal && volume ? (
     <>
       <CheckFrontendMode debugLabel="VolumeDetail" isProjectSubpage />
-      <RegisterBreadcrumbs
-        breadcrumbs={[
-          {
-            to: lh.link("frontendJournalsList"),
-            label: "All Journals"
-          },
-          {
-            to: lh.link("frontendJournalDetail", journal.id),
-            label: journal.attributes.titlePlaintext
-          },
-          {
-            to: lh.link("frontendVolumeDetail", journal.id, slug),
-            label: `Volume ${volume.attributes.number}`
-          }
-        ]}
+      <RegisterBreadcrumbs breadcrumbs={breadcrumbs} />
+      <EntityHeadContent
+        entity={volume}
+        type={capitalize(t("glossary.volume_one"))}
+        parentEntity={journal}
       />
-      <EntityHeadContent entity={volume} type="Volume" parentEntity={journal} />
-      <h1 className="screen-reader-text">{`${journal.attributes.title}: Volume ${volume.attributes.number}`}</h1>
+      <h1 className="screen-reader-text">
+        {`${journal.attributes.title}: ${capitalize(
+          t("glossary.volume_one")
+        )} ${volume.attributes.number}`}
+      </h1>
       <EntityMasthead entity={journal} />
       <Journal.VolumeDetail journal={journal} volume={volume} />
     </>
