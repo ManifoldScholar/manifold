61b31dfe46fd7aa0fe9a9f43056af0f2d4526cb2
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-03-04 12:53:50 -0800

    [F] Add search result components for journals

---
+++ client/src/global/components/search/results/Types/JournalVolume.js
@@ -0,0 +1,66 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
+import Generic from "./Generic";
+import lh from "helpers/linkHandler";
+import FormattedDate from "global/components/FormattedDate";
+import withSearchResultHelper from "./searchResultHelper";
+import { capitalize } from "utils/string";
+
+function SearchResultsTypeJournalVolume({ result, highlightedAttribute }) {
+  const { t } = useTranslation();
+
+  if (!result) return null;
+
+  const model = result.relationships?.model;
+  const { attributes } = model ?? {};
+
+  const parentJournal = result.relationships?.journal;
+
+  const title = `${capitalize(t("glossary.volume_one"))}${highlightedAttribute(
+    "number"
+  )}`;
+
+  const { searchableType, searchableId } = result.attributes ?? {};
+  const collectable = {
+    type: searchableType,
+    id: searchableId,
+    attributes: { title }
+  };
+  const creators = model?.relationships?.creators ?? [];
+
+  const resultProps = {
+    url: lh.link("frontendProjectDetail", attributes.slug),
+    title,
+    parent: parentJournal?.attributes?.titlePlaintext || null,
+    parentUrl: parentJournal
+      ? lh.link("frontendJournalDetail", parentJournal.id)
+      : null,
+    attribution: creators.map(c => c.attributes.fullName).join(", "),
+    label: t("glossary.volume_one"),
+    collectable
+  };
+
+  return (
+    <Generic
+      {...resultProps}
+      meta={
+        <FormattedDate
+          prefix={capitalize(t("dates.published"))}
+          format="MMMM, yyyy"
+          date={attributes.createdAt}
+        />
+      }
+    />
+  );
+}
+
+SearchResultsTypeJournalVolume.displayName =
+  "Search.Results.Type.JournalVolume";
+
+SearchResultsTypeJournalVolume.propTypes = {
+  result: PropTypes.object,
+  highlightedAttribute: PropTypes.func.isRequired
+};
+
+export default withSearchResultHelper(SearchResultsTypeJournalVolume);
--- client/src/global/components/search/results/Types/index.js
+++ client/src/global/components/search/results/Types/index.js
@@ -3,11 +3,17 @@ import TextSection from "./TextSection";
 import Project from "./Project";
 import Text from "./Text";
 import Resource from "./Resource";
+import Journal from "./Journal";
+import Issue from "./JournalIssue";
+import Volume from "./JournalVolume";
 
 export default {
   Annotation,
   TextSection,
   Project,
   Text,
-  Resource
+  Resource,
+  Journal,
+  Issue,
+  Volume
 };
