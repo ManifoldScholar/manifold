3de8ddbbeef2a0284693c641ccee215979335899
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-03-04 17:11:44 -0800

    [C] Refactor + migrate to styled Generic search result

--- client/src/global/components/search/results/Types/Generic/index.js
+++ client/src/global/components/search/results/Types/Generic/index.js
@@ -1,140 +1,122 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import has from "lodash/has";
-import isPlainObject from "lodash/isPlainObject";
+import { useTranslation } from "react-i18next";
 import Collecting from "frontend/components/collecting";
 import withSearchResultHelper from "../searchResultHelper";
 import Excerpts from "./Excerpts";
+import * as Styled from "./styles";
 
-class SearchResultsTypeGeneric extends PureComponent {
-  static displayName = "Search.Results.Type.Generic";
+function SearchResultsTypeGeneric(props) {
+  const {
+    figure,
+    url,
+    parent,
+    parentUrl,
+    hideParent,
+    title,
+    attribution,
+    description,
+    label,
+    collectable,
+    excerpts,
+    meta
+  } = props;
 
-  get linked() {
-    return this.hasProp("url");
-  }
+  const { t } = useTranslation();
 
-  isHtml(value) {
-    return isPlainObject(value) && has(value, "__html");
-  }
+  const maybeHtml = item => {
+    const isHtml = !!item.__html;
+    return isHtml ? { dangerouslySetInnerHTML: { ...item } } : {};
+  };
 
-  hasProp(prop) {
-    const value = this.props[prop];
-    return Boolean(value);
-  }
+  const maybeReactNode = item => {
+    const isReactNode = React.isValidElement(item) || typeof item === "string";
+    return isReactNode ? item : null;
+  };
 
-  attributesFor(prop) {
-    const value = this.props[prop];
-    const attributes = {};
-    if (this.isHtml(value)) attributes.dangerouslySetInnerHTML = value;
-    return attributes;
-  }
-
-  valueFor(prop) {
-    const value = this.props[prop];
-    if (this.isHtml(value)) return null;
-    return value;
-  }
-
-  wrapWithLink({ url, contents, tabIndex = 0 }) {
-    if (!url) return contents;
-    return (
-      <Link to={url} className="search-result__title-link" tabIndex={tabIndex}>
-        {contents}
-      </Link>
+  const maybeWithLink = ({ to, children, tabIndex = 0 }) => {
+    return url ? (
+      <Styled.Link to={to} tabIndex={tabIndex}>
+        {children}
+      </Styled.Link>
+    ) : (
+      children
     );
-  }
+  };
 
-  render() {
-    return (
-      <li className="search-result">
-        <article className="search-result__content-wrapper">
-          <div className="search-result__figure-column">
-            {this.hasProp("figure") &&
-              this.wrapWithLink({
-                url: this.props.url,
-                contents: (
-                  <figure
-                    className="search-result__figure"
-                    {...this.attributesFor("figure")}
-                  >
-                    {this.valueFor("figure")}
-                  </figure>
-                ),
-                tabIndex: -1
-              })}
-          </div>
-          <div className="search-result__text-column">
-            <div className="search-result__text-column-top">
-              <div className="search-result__text-column-top-left">
-                {this.hasProp("parent") && !this.props.hideParent && (
-                  <div
-                    className="search-result__parent"
-                    {...this.attributesFor("parent")}
-                  >
-                    {this.wrapWithLink({
-                      url: this.props.parentUrl,
-                      contents: this.valueFor("parent")
-                    })}
-                  </div>
-                )}
-                {this.hasProp("title") &&
-                  this.wrapWithLink({
-                    url: this.props.url,
-                    contents: (
-                      <h3
-                        className="search-result__title"
-                        {...this.attributesFor("title")}
-                      >
-                        {this.valueFor("title")}
-                      </h3>
-                    )
+  return (
+    <Styled.Result>
+      <Styled.Inner>
+        <Styled.ImageCol>
+          {figure &&
+            maybeWithLink({
+              to: url,
+              children: (
+                <Styled.Image {...maybeHtml(figure)}>
+                  {maybeReactNode(figure)}
+                </Styled.Image>
+              ),
+              tabIndex: -1
+            })}
+        </Styled.ImageCol>
+        <Styled.TextCol>
+          <Styled.TextTop>
+            <Styled.TextTopLeft>
+              {parent && !hideParent && (
+                <Styled.Parent {...maybeHtml(parent)}>
+                  {maybeWithLink({
+                    to: parentUrl,
+                    children: maybeReactNode(parent)
                   })}
-                {this.hasProp("attribution") && (
-                  <address className="search-result__attribution">
-                    <span {...this.attributesFor("attribution")}>
-                      <em>by</em> {this.valueFor("attribution")}
-                    </span>
-                  </address>
-                )}
-              </div>
-              <div className="search-result__text-column-top-right">
-                <div className="search-result__label">
-                  {this.valueFor("label")}
-                </div>
-                {this.hasProp("collectable") && (
-                  <div className="search-result__collecting-toggle">
-                    <Collecting.Toggle
-                      collectable={this.valueFor("collectable")}
-                    />
-                  </div>
-                )}
-              </div>
-            </div>
-            {this.hasProp("description") && (
-              <p
-                className="search-result__description"
-                {...this.attributesFor("description")}
-              >
-                {this.valueFor("description")}
-              </p>
-            )}
-            {this.hasProp("excerpts") && (
-              <Excerpts excerpts={this.valueFor("excerpts")} />
-            )}
-            {this.hasProp("meta") && (
-              <p
-                className="search-result__meta"
-                {...this.attributesFor("meta")}
-              >
-                {this.valueFor("meta")}
-              </p>
-            )}
-          </div>
-        </article>
-      </li>
-    );
-  }
+                </Styled.Parent>
+              )}
+              {title &&
+                maybeWithLink({
+                  to: url,
+                  children: (
+                    <Styled.Title {...maybeHtml(title)}>
+                      {maybeReactNode(title)}
+                    </Styled.Title>
+                  )
+                })}
+              {attribution && (
+                <Styled.Attribution>
+                  <span {...maybeHtml(attribution)}>
+                    {maybeReactNode(attribution) && (
+                      <>
+                        <em>{t("common.by")}</em> {maybeReactNode(attribution)}
+                      </>
+                    )}
+                  </span>
+                </Styled.Attribution>
+              )}
+            </Styled.TextTopLeft>
+            <Styled.TextTopRight>
+              <Styled.Label>{label}</Styled.Label>
+              {collectable && (
+                <Styled.ToggleWrapper>
+                  <Collecting.Toggle collectable={collectable} />
+                </Styled.ToggleWrapper>
+              )}
+            </Styled.TextTopRight>
+          </Styled.TextTop>
+          {description && (
+            <Styled.Description {...maybeHtml(description)}>
+              {maybeReactNode(description)}
+            </Styled.Description>
+          )}
+          {excerpts && <Excerpts excerpts={excerpts} />}
+          {meta && (
+            <Styled.Meta {...maybeHtml(meta)}>
+              {maybeReactNode(meta)}
+            </Styled.Meta>
+          )}
+        </Styled.TextCol>
+      </Styled.Inner>
+    </Styled.Result>
+  );
 }
+
+SearchResultsTypeGeneric.displayName = "Search.Results.Type.Generic";
 
 export default withSearchResultHelper(SearchResultsTypeGeneric);
