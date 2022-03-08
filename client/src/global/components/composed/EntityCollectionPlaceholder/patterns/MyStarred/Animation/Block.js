fd00a094193d3455dbdff70f1c2989bd2507dd77
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-03-08 13:35:21 -0800

    [F] Start localizing EntityCollectionPlaceholder

--- client/src/global/components/composed/EntityCollectionPlaceholder/patterns/MyStarred/Animation/Block.js
+++ client/src/global/components/composed/EntityCollectionPlaceholder/patterns/MyStarred/Animation/Block.js
@@ -1,33 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 import IconComposer from "global/components/utility/IconComposer";
+import { capitalize } from "utils/string";
 import * as Styled from "./styles";
 
-function getContent(type) {
+function getContent(type, t) {
   switch (type) {
     case "projects":
       return {
-        title: "Projects",
+        title: capitalize(t("glossary.project_other")),
         icon: "projects64"
       };
     case "texts":
       return {
-        title: "Texts",
+        title: capitalize(t("glossary.text_other")),
         icon: "textsStacked64"
       };
     case "textSections":
       return {
-        title: "Text Sections",
+        title: capitalize(t("glossary.text_section_other"), true),
         icon: "toc64"
       };
     case "resourceCollections":
       return {
-        title: "Resource Collections",
+        title: capitalize(t("glossary.resource_collection_other"), true),
         icon: "resourceCollection64"
       };
     case "resources":
       return {
-        title: "Resources",
+        title: capitalize(t("glossary.resource_other")),
         icon: "resources64"
       };
     default:
@@ -39,7 +41,8 @@ function getContent(type) {
 }
 
 function Block({ type }) {
-  const { title, icon } = getContent(type);
+  const { t } = useTranslation();
+  const { title, icon } = getContent(type, t);
 
   return (
     <Styled.Block>
--- client/src/global/components/composed/EntityCollectionPlaceholder/patterns/MyStarred/Animation/index.js
+++ client/src/global/components/composed/EntityCollectionPlaceholder/patterns/MyStarred/Animation/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import Block from "./Block";
 import * as Styled from "./styles";
 
 function CollectingAnimation() {
   const [init, setInit] = useState(false);
+  const { t } = useTranslation();
 
   useEffect(() => {
     setTimeout(() => {
@@ -21,7 +23,7 @@ function CollectingAnimation() {
   return (
     <Styled.Animation
       role="img"
-      aria-label="A grid of tiles with star icons that fade in."
+      aria-label={t("placeholders.my_starred.animation_label")}
       $init={init}
     >
       {types.map(type => (
