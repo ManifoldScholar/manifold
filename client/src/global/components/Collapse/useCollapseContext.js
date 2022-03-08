fe298fb0a85ae3155da1c6434f52d309d20e03a6
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-03-08 11:30:44 -0800

    [F] Localize Collapse + ColorScheme

--- client/src/global/components/Collapse/useCollapseContext.js
+++ client/src/global/components/Collapse/useCollapseContext.js
@@ -1,12 +1,12 @@
 import { useContext } from "react";
 import { CollapseContext } from "helpers/contexts";
+import { useTranslation } from "react-i18next";
 
 export default function useCollapseContext() {
   const context = useContext(CollapseContext);
+  const { t } = useTranslation();
   if (!context) {
-    throw new Error(
-      `Collapse components cannot be rendered outside the Collapse wrapper component.`
-    );
+    throw new Error(t("errors.no_collapse_context"));
   }
   return context;
 }
