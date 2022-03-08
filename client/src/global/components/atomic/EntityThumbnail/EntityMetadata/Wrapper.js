52dfd45c60f09183ebd9e0bb57f1a4a783ec8200
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-03-08 10:40:35 -0800

    [F] Localize global/components/atomic

--- client/src/global/components/atomic/EntityThumbnail/EntityMetadata/Wrapper.js
+++ client/src/global/components/atomic/EntityThumbnail/EntityMetadata/Wrapper.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import MetadataContent from "./Content";
+import { capitalize } from "utils/string";
 
 export default function Wrapper({
   entity,
@@ -8,6 +10,8 @@ export default function Wrapper({
   parentView,
   ...props
 }) {
+  const { t } = useTranslation();
+
   if (!entity) return;
 
   const getMetadataProps = () => {
@@ -22,24 +26,31 @@ export default function Wrapper({
       // Catch issues rendered as part of the Featured Projects collection
       if (entity.attributes.isJournalIssue) {
         title =
-          entity.relationships.journalIssue?.attributes.number ??
-          "Journal Title";
+          entity.relationships.journalIssue?.relationships?.journal?.attributes
+            ?.title;
         additionalData = entity.attributes.journalVolumeNumber
-          ? `Volume ${entity.attributes.journalVolumeNumber}, Issue ${entity.attributes.number}`
-          : `Issue ${entity.attributes.number}`;
+          ? t("journals.volume_issue_number", {
+              volNum: entity.attributes.journalVolumeNumber,
+              issNum: entity.attributes.number
+            })
+          : t("journals.issue_number", { issNum: entity.attributes.number });
         bumpDraftDown = true;
       }
       // Catch thumbnails rendered in context on a Journal or Volume Detail
       else if (parentView) {
-        title = `Issue ${entity.attributes.number}` ?? "Issue";
+        title = t("journals.issue_number", {
+          issNum: entity.attributes.number
+        });
       }
       // Issues rendered on the Homepage or Journals/Issues List
       else {
-        title =
-          entity.relationships.journal?.attributes?.title ?? "Journal Title";
+        title = entity.relationships.journal?.attributes?.title;
         additionalData = entity.attributes.journalVolumeNumber
-          ? `Volume ${entity.attributes.journalVolumeNumber}, Issue ${entity.attributes.number}`
-          : `Issue ${entity.attributes.number}`;
+          ? t("journals.volume_issue_number", {
+              volNum: entity.attributes.journalVolumeNumber,
+              issNum: entity.attributes.number
+            })
+          : t("journals.issue_number", { issNum: entity.attributes.number });
         bumpDraftDown = true;
       }
 
@@ -47,7 +58,9 @@ export default function Wrapper({
         ? entity.relationships.journalIssue?.attributes
         : entity?.attributes;
       const date = data.publicationDate ?? data.updatedAt;
-      const prefix = data.publicationDate ? "Published" : "Updated";
+      const prefix = data.publicationDate
+        ? capitalize(t("dates.published"))
+        : capitalize(t("dates.updated"));
       const draft = data.draft;
       const recentlyUpdated = data.recentlyUpdated;
       return {
@@ -86,7 +99,9 @@ export default function Wrapper({
         ? data.updatedAt
         : data.publicationDate;
     /* eslint-disable no-nested-ternary */
-    const prefix = showUpdated ? "Updated" : "Published";
+    const prefix = showUpdated
+      ? capitalize(t("dates.updated"))
+      : capitalize(t("dates.published"));
     const recentlyUpdated = data.recentlyUpdated;
 
     return {
