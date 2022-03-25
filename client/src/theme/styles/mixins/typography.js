8c47032f20882feb2aeaf4db933405b340f0d8fd
Dana Johnson <dana@castironcoding.com>
2022-03-24 17:30:35 -0700

    [B] Improve annotation underline styles
    Improves size and color of underline SVGs

--- client/src/theme/styles/mixins/typography.js
+++ client/src/theme/styles/mixins/typography.js
@@ -155,13 +155,13 @@ function underlineBgImage(style, color) {
 
   switch (style) {
     case "dots":
-      return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 4.01344 1.79315' fill='${encodedColor}'%3E%3Cpath d='M.75844,1.64657a.75456.75456,0,0,1-.755-.75.746.746,0,0,1,.745-.75h.01a.75.75,0,0,1,0,1.5'/%3E%3C/svg%3E")`;
+      return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 4.01344 1.79315' fill='${encodedColor}'%3E%3Cpath d='M.90256,1.79314A.902.902,0,0,1,0,.89657.89179.89179,0,0,1,.887,0l.0036,0h.012a.89657.89657,0,0,1,0,1.79314'/%3E%3C/svg%3E")`;
     case "dashes":
-      return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 6 1.79315' fill='${encodedColor}'%3E%3Crect y='0.39657' width='3' height='1'/%3E%3C/svg%3E")`;
+      return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 6 1.79315' fill='${encodedColor}'%3E%3Crect y='0.18057' width='3' height='1.432'/%3E%3C/svg%3E")`;
     case "wavy":
-      return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8.28101 1.79315' fill='${encodedColor}'%3E%3Cpath d='M8.083,0A5.49633,5.49633,0,0,0,5.984.42719a4.48742,4.48742,0,0,1-1.888.366,4.48409,4.48409,0,0,1-1.891-.367A5.52606,5.52606,0,0,0,.10425,0H0V.99445a4.50067,4.50067,0,0,1,1.84491.36468,5.45027,5.45027,0,0,0,2.251.434,5.42948,5.42948,0,0,0,2.25-.435,4.48329,4.48329,0,0,1,1.888-.366c.01776,0,.02966.00219.04706.00232V0Z'/%3E%3C/svg%3E")`;
+      return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 7.73456 1.79315' fill='${encodedColor}'%3E%3Cpath d='M7.64024,0A5.33009,5.33009,0,0,0,5.52143.44808,4.12154,4.12154,0,0,1,3.72928.84991,4.12161,4.12161,0,0,1,1.93712.44808,4.08623,4.08623,0,0,0,.09432,0H0V.94324a3.36163,3.36163,0,0,1,1.55983.44808,5.49756,5.49756,0,0,0,2.16945.40183A5.09,5.09,0,0,0,5.8044,1.39132,4.79709,4.79709,0,0,1,7.73456.94324h0V0Z' /%3E%3C/svg%3E%0A")`;
     default:
-      return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8.28101 1.79315' fill='${encodedColor}'%3E%3Cpolygon points='0 1.397 8.28 1.397 8.28 0.397 0 0.397 0 1.397'/%3E%3C/svg%3E")`;
+      return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8.28101 1.79315' fill='${encodedColor}'%3E%3Cpolygon points='0 1.613 8.28 1.613 8.28 0.181 0 0.181 0 1.613' /%3E%3C/svg%3E%0A")`;
   }
 }
 
@@ -169,7 +169,7 @@ export function styledUnderline(style = "solid", color = "neutral75") {
   return `
     background-repeat: repeat-x;
     background-position: 0% 99%;
-    background-size: 8px 2.5px;
+    background-size: ${style === "wavy" ? 10 : 8}px 3px;
     background-image: ${underlineBgImage(style, color)};
   `;
 }
