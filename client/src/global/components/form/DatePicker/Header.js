5bdd65160485ce276379afdf17e64df95fceabf2
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-03-30 10:01:50 -0700

    [F] Localize global/forms/DatePicker

--- client/src/global/components/form/DatePicker/Header.js
+++ client/src/global/components/form/DatePicker/Header.js
@@ -1,63 +1,50 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 import IconComposer from "global/components/utility/IconComposer";
+import format from "date-fns/format";
 
-export default class DatePickerHeader extends Component {
-  static displayName = "Form.DatePicker.Header";
+export default function DatePickerHeader({
+  date,
+  increaseMonth,
+  decreaseMonth,
+  uid
+}) {
+  const { t } = useTranslation();
 
-  static propTypes = {
-    date: PropTypes.instanceOf(Date).isRequired,
-    decreaseMonth: PropTypes.func.isRequired,
-    increaseMonth: PropTypes.func.isRequired,
-    uid: PropTypes.string.isRequired
-  };
-
-  get monthName() {
-    const monthNames = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
-    ];
-    return monthNames[this.props.date.getMonth()];
-  }
-
-  get year() {
-    return this.props.date.getFullYear();
-  }
-
-  render() {
-    return (
-      <div className="react-datepicker__header">
-        <button
-          type="button"
-          className="react-datepicker__navigation react-datepicker__navigation--next"
-          onClick={this.props.increaseMonth}
-        >
-          <IconComposer icon="arrowRight16" size={20} />
-        </button>
-        <button
-          type="button"
-          className="react-datepicker__navigation react-datepicker__navigation--previous"
-          onClick={this.props.decreaseMonth}
-        >
-          <IconComposer icon="arrowLeft16" size={20} />
-        </button>
-        <div
-          id={`range-picker-dialog-label-${this.props.uid}`}
-          className="react-datepicker__current-month"
-        >
-          {this.monthName} {this.year}
-        </div>
+  return (
+    <div className="react-datepicker__header">
+      <button
+        type="button"
+        className="react-datepicker__navigation react-datepicker__navigation--next"
+        onClick={increaseMonth}
+      >
+        <IconComposer icon="arrowRight16" size={20} />
+      </button>
+      <button
+        type="button"
+        className="react-datepicker__navigation react-datepicker__navigation--previous"
+        onClick={decreaseMonth}
+      >
+        <IconComposer icon="arrowLeft16" size={20} />
+      </button>
+      <div
+        id={`range-picker-dialog-label-${uid}`}
+        className="react-datepicker__current-month"
+      >
+        {format(date, "MMMM yyyy", {
+          locale: t("date_fns", { returnObjects: true })
+        })}
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+DatePickerHeader.displayName = "Form.DatePicker.Header";
+
+DatePickerHeader.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+  decreaseMonth: PropTypes.func.isRequired,
+  increaseMonth: PropTypes.func.isRequired,
+  uid: PropTypes.string.isRequired
+};
