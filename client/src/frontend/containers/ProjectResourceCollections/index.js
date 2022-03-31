0c1d23ce70b5ee5f88716412ee51f0931a3cb31d
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-03-31 11:21:40 -0700

    [C] Refactor ProjectResourceCollections container

--- client/src/frontend/containers/ProjectResourceCollections/index.js
+++ client/src/frontend/containers/ProjectResourceCollections/index.js
@@ -1,146 +1,92 @@
-import React, { Component } from "react";
+import React, { useEffect, useMemo } from "react";
 import PropTypes from "prop-types";
-import connectAndFetch from "utils/connectAndFetch";
-import { withTranslation } from "react-i18next";
-import { entityStoreActions } from "actions";
+import { useTranslation } from "react-i18next";
+import { entityStoreActions as store } from "actions";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 import CheckFrontendMode from "global/containers/CheckFrontendMode";
-import { projectsAPI, requests } from "api";
-import { select, meta } from "utils/entityUtils";
+import { projectsAPI } from "api";
 import lh from "helpers/linkHandler";
 import LoadingBlock from "global/components/loading-block";
 import { RegisterBreadcrumbs } from "global/components/atomic/Breadcrumbs";
 import EntityCollectionPlaceholder from "global/components/entity/CollectionPlaceholder";
 import EntityHeadContent from "frontend/components/entity/HeadContent";
 import EntityCollection from "frontend/components/entity/Collection";
+import { useFetch, usePaginationState } from "hooks";
 
-const { request, flush } = entityStoreActions;
-const defaultPage = 1;
-const perPage = 10;
+export default function ProjectResourceCollectionsContainer({
+  project,
+  journalBreadcrumbs
+}) {
+  const [pagination, setPageNumber] = usePaginationState(1, 10);
+  const { id } = useParams();
+  const { data: resourceCollections, meta, uid } = useFetch({
+    request: [projectsAPI.resourceCollections, id, null, pagination]
+  });
+  const dispatch = useDispatch();
+  const { t } = useTranslation();
 
-class ProjectResourceCollectionsContainer extends Component {
-  static fetchData = (getState, dispatch, location, match) => {
-    const pagination = {
-      number: defaultPage,
-      size: perPage
-    };
-    const resourceCollectionRequest = request(
-      projectsAPI.resourceCollections(match.params.id, {}, pagination),
-      requests.feResourceCollections
-    );
-    const { promise: one } = dispatch(resourceCollectionRequest);
-    return Promise.all([one]);
-  };
+  useEffect(() => {
+    return () => dispatch(store.flush(uid));
+  }, [dispatch, uid]);
 
-  static mapStateToProps = state => {
-    return {
-      resourceCollections: select(
-        requests.feResourceCollections,
-        state.entityStore
-      ),
-      resourceCollectionsMeta: meta(
-        requests.feResourceCollections,
-        state.entityStore
-      )
-    };
-  };
+  const { titlePlaintext, slug } = project?.attributes ?? {};
 
-  static propTypes = {
-    project: PropTypes.object,
-    resourceCollections: PropTypes.array,
-    resourceCollectionsMeta: PropTypes.object,
-    dispatch: PropTypes.func.isRequired,
-    journalBreadcrumbs: PropTypes.array,
-    t: PropTypes.func
-  };
-
-  componentWillUnmount() {
-    this.props.dispatch(flush(requests.feResourceCollections));
-  }
-
-  fetchResourceCollections(pageParam = 1) {
-    const action = request(
-      projectsAPI.resourceCollections(
-        this.props.project.id,
-        {},
-        { number: pageParam, size: perPage }
-      ),
-      requests.feResourceCollections
-    );
-    this.props.dispatch(action);
-  }
-
-  pageChangeHandlerCreator = pageParam => {
-    return event => {
-      event.preventDefault();
-      this.handlePageChange(pageParam);
-    };
-  };
-
-  get hasCollections() {
-    return !!this.props.resourceCollections?.length;
-  }
-
-  renderPlaceholder(id) {
-    return <EntityCollectionPlaceholder.ResourceCollections id={id} />;
-  }
-
-  breadcrumbs() {
-    const { journalBreadcrumbs, project, t } = this.props;
+  const breadcrumbs = useMemo(() => {
     const projectCrumb = {
-      to: lh.link("frontendProject", project.attributes.slug),
-      label: project.attributes.titlePlaintext
+      to: lh.link("frontendProject", slug),
+      label: titlePlaintext
     };
     const collectionsCrumb = {
-      to: lh.link(
-        "frontendProjectResourceCollections",
-        project.attributes.slug
-      ),
+      to: lh.link("frontendProjectResourceCollections", slug),
       label: t("glossary.resource_collection_other")
     };
     return journalBreadcrumbs
       ? [...journalBreadcrumbs, collectionsCrumb].filter(Boolean)
       : [projectCrumb, collectionsCrumb].filter(Boolean);
-  }
+  }, [journalBreadcrumbs, slug, titlePlaintext, t]);
 
-  render() {
-    const { project, t } = this.props;
-    if (!project) return <LoadingBlock />;
+  if (!project) return <LoadingBlock />;
 
-    return (
-      <>
-        <CheckFrontendMode
-          debugLabel="ProjectResourceCollections"
-          isProjectSubpage
+  return (
+    <>
+      <CheckFrontendMode
+        debugLabel="ProjectResourceCollections"
+        isProjectSubpage
+      />
+      <EntityHeadContent
+        entity={project}
+        titleOverride={`${t(
+          "glossary.resource_collection_title_case_other"
+        )} | ${project.attributes.titlePlaintext}`}
+      />
+      <h1 className="screen-reader-text">
+        {`${project.attributes.titlePlaintext} ${t(
+          "glossary.resource_collection_other"
+        )}`}
+      </h1>
+      <RegisterBreadcrumbs breadcrumbs={breadcrumbs} />
+      {!resourceCollections?.length ? (
+        <EntityCollectionPlaceholder.ResourceCollections id={project.id} />
+      ) : (
+        <EntityCollection.ProjectResourceCollections
+          resourceCollections={resourceCollections}
+          resourceCollectionsMeta={meta}
+          paginationProps={{
+            paginationClickHandler: page => () => setPageNumber(page),
+            paginationTarget: "#"
+          }}
+          itemHeadingLevel={2}
         />
-        <EntityHeadContent
-          entity={project}
-          titleOverride={`${t(
-            "glossary.resource_collection_title_case_other"
-          )} | ${project.attributes.titlePlaintext}`}
-        />
-        <h1 className="screen-reader-text">
-          {`${project.attributes.titlePlaintext} ${t(
-            "glossary.resource_collection_other"
-          )}`}
-        </h1>
-        <RegisterBreadcrumbs breadcrumbs={this.breadcrumbs()} />
-        {!this.hasCollections ? (
-          this.renderPlaceholder(project.id)
-        ) : (
-          <EntityCollection.ProjectResourceCollections
-            resourceCollections={this.props.resourceCollections}
-            resourceCollectionsMeta={this.props.resourceCollectionsMeta}
-            paginationProps={{
-              paginationClickHandler: this.pageChangeHandlerCreator
-            }}
-            itemHeadingLevel={2}
-          />
-        )}
-      </>
-    );
-  }
+      )}
+    </>
+  );
 }
 
-export default withTranslation()(
-  connectAndFetch(ProjectResourceCollectionsContainer)
-);
+ProjectResourceCollectionsContainer.displayName =
+  "Frontend.Containers.ProjectResourceCollections";
+
+ProjectResourceCollectionsContainer.propTypes = {
+  project: PropTypes.object,
+  journalBreadcrumbs: PropTypes.array
+};
