2d4143a61d1ab214d94a7ff5a15b7616b6242835
Dana Johnson <dana@castironcoding.com>
2022-03-31 15:47:30 -0700

    [C] Mock date_fns key in i18nTest

--- client/src/utils/i18nTest.js
+++ client/src/utils/i18nTest.js
@@ -3,7 +3,7 @@ import { initReactI18next } from "react-i18next";
 
 i18n.use(initReactI18next).init({
   fallbackLng: "en",
-  resources: { en: {} }
+  resources: { en: { translation: { date_fns: null } } }
 });
 
 export default i18n;
