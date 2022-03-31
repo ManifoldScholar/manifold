caff93360a5fb0075d410a155e68d38c86880d78
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-03-30 17:41:03 -0700

    [C] Refactor ProjectResources container

--- client/src/frontend/containers/ProjectResources/index.js
+++ client/src/frontend/containers/ProjectResources/index.js
@@ -1,139 +1,55 @@
-import React, { Component } from "react";
+import React, { useEffect, useMemo } from "react";
 import PropTypes from "prop-types";
-import connectAndFetch from "utils/connectAndFetch";
-import { withTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 import LoadingBlock from "global/components/loading-block";
-import { entityStoreActions, uiFrontendModeActions } from "actions";
-import { select, meta } from "utils/entityUtils";
-import { projectsAPI, requests } from "api";
-import queryString from "query-string";
-import debounce from "lodash/debounce";
-import omitBy from "lodash/omitBy";
-import isNull from "lodash/isNull";
+import { uiFrontendModeActions } from "actions";
+import { projectsAPI } from "api";
 import lh from "helpers/linkHandler";
 import EntityHeadContent from "frontend/components/entity/HeadContent";
 import { RegisterBreadcrumbs } from "global/components/atomic/Breadcrumbs";
-import EntityCollection from "frontend/components/entity/Collection";
+import EntityCollection from "frontend/components/composed/EntityCollection";
+import {
+  useFetch,
+  usePaginationState,
+  useFilterState,
+  useSetLocation,
+  useListFilters
+} from "hooks";
 
-const { request, flush } = entityStoreActions;
-const defaultPage = 1;
-const perPage = 10;
+export default function ProjectResourcesContainer({
+  project,
+  journalBreadcrumbs
+}) {
+  const [pagination, setPageNumber] = usePaginationState(1, 10);
+  const [filters, setFilters] = useFilterState();
+  const { id } = useParams();
+  const { data: resources, meta } = useFetch({
+    request: [projectsAPI.resources, id, filters, pagination]
+  });
+  const dispatch = useDispatch();
+  const { t } = useTranslation();
 
-export class ProjectResourcesContainer extends Component {
-  static fetchData = (getState, dispatch, location, match) => {
-    const params = queryString.parse(location.search);
-    const pagination = {
-      number: params.page ? params.page : defaultPage,
-      size: perPage
-    };
-    const filter = omitBy(params, (v, k) => k === "page");
-    const resourcesRequest = request(
-      projectsAPI.resources(match.params.id, filter, pagination),
-      requests.feResources
-    );
-    const { promise: one } = dispatch(resourcesRequest);
-    return Promise.all([one]);
-  };
+  useEffect(() => {
+    const action = uiFrontendModeActions.isProjectSubpage();
+    dispatch(action);
+  }, [dispatch]);
 
-  static mapStateToProps = state => {
-    const props = {
-      resources: select(requests.feResources, state.entityStore),
-      resourcesMeta: meta(requests.feResources, state.entityStore)
-    };
-    return omitBy(props, isNull);
-  };
+  useSetLocation({ filters, page: pagination.number });
 
-  static propTypes = {
-    project: PropTypes.object,
-    resources: PropTypes.array,
-    resourcesMeta: PropTypes.object,
-    dispatch: PropTypes.func,
-    location: PropTypes.object.isRequired,
-    history: PropTypes.object
-  };
+  const filterProps = useListFilters({
+    onFilterChange: param => setFilters({ newState: param }),
+    initialState: filters,
+    resetState: {},
+    options: {
+      sort: true,
+      kinds: project.attributes.resourceKinds,
+      tags: project.attributes.resourceTags
+    }
+  });
 
-  constructor(props) {
-    super(props);
-    this.state = this.initialState(queryString.parse(props.location.search));
-    this.updateResults = debounce(this.updateResults.bind(this), 250);
-  }
-
-  componentDidMount() {
-    this.props.dispatch(uiFrontendModeActions.isProjectSubpage());
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.location.search === this.props.location.search) return null;
-    this.setState(
-      this.initialState(queryString.parse(this.props.location.search)),
-      this.updateResults
-    );
-  }
-
-  componentWillUnmount() {
-    this.props.dispatch(flush(requests.feResources));
-  }
-
-  initialFilterState(init = {}) {
-    return omitBy(init, (vIgnored, k) => k === "page");
-  }
-
-  initialState(init = {}) {
-    return {
-      filter: { ...this.initialFilterState(init) },
-      pagination: {
-        number: init.page || defaultPage,
-        size: perPage
-      }
-    };
-  }
-
-  doUpdate() {
-    this.updateResults();
-    this.updateUrl();
-  }
-
-  updateResults() {
-    const action = request(
-      projectsAPI.resources(
-        this.props.project.id,
-        this.state.filter,
-        this.state.pagination
-      ),
-      requests.feResources
-    );
-    this.props.dispatch(action);
-  }
-
-  updateUrl() {
-    const pathname = this.props.location.pathname;
-    const filters = this.state.filter;
-    const pageParam = this.state.pagination.number;
-    const params = { ...filters };
-    if (pageParam !== 1) params.page = pageParam;
-
-    const search = queryString.stringify(params);
-    this.props.history.push({ pathname, search });
-  }
-
-  filterChangeHandler = filter => {
-    this.setState({ filter }, this.doUpdate);
-  };
-
-  handlePageChange = pageParam => {
-    const pagination = { ...this.state.pagination, number: pageParam };
-    this.setState({ pagination }, this.doUpdate);
-  };
-
-  pageChangeHandlerCreator = pageParam => {
-    return event => {
-      event.preventDefault();
-      this.handlePageChange(pageParam);
-    };
-  };
-
-  breadcrumbs() {
-    const { journalBreadcrumbs, project, t } = this.props;
+  const breadcrumbs = useMemo(() => {
     const projectCrumb = {
       to: lh.link("frontendProject", project.attributes.slug),
       label: project.attributes.titlePlaintext
@@ -145,43 +61,43 @@ export class ProjectResourcesContainer extends Component {
     return journalBreadcrumbs
       ? [...journalBreadcrumbs, resourcesCrumb].filter(Boolean)
       : [projectCrumb, resourcesCrumb].filter(Boolean);
-  }
+  }, [
+    journalBreadcrumbs,
+    project.attributes.slug,
+    project.attributes.titlePlaintext,
+    t
+  ]);
 
-  render() {
-    const { project, resources, resourcesMeta, t } = this.props;
-    if (!project) return <LoadingBlock />;
+  if (!project) return <LoadingBlock />;
 
-    return (
-      <>
-        <EntityHeadContent
-          entity={project}
-          titleOverride={`${t("glossary.resource_title_case_other")} | ${
-            project.attributes.titlePlaintext
-          }`}
-        />
-        <RegisterBreadcrumbs breadcrumbs={this.breadcrumbs()} />
-        <EntityCollection.ProjectResources
-          project={project}
-          resources={resources}
-          resourcesMeta={resourcesMeta}
-          filterProps={{
-            onFilterChange: this.filterChangeHandler,
-            initialState: this.state.filter,
-            resetState: this.initialFilterState(),
-            options: {
-              sort: true,
-              kinds: project.attributes.resourceKinds,
-              tags: project.attributes.resourceTags
-            }
-          }}
-          paginationProps={{
-            paginationClickHandler: this.pageChangeHandlerCreator
-          }}
-          itemHeadingLevel={3}
-        />
-      </>
-    );
-  }
+  return (
+    <>
+      <EntityHeadContent
+        entity={project}
+        titleOverride={`${t("glossary.resource_title_case_other")} | ${
+          project.attributes.titlePlaintext
+        }`}
+      />
+      <RegisterBreadcrumbs breadcrumbs={breadcrumbs} />
+      <EntityCollection.ProjectResources
+        project={project}
+        resources={resources}
+        resourcesMeta={meta}
+        filterProps={filterProps}
+        paginationProps={{
+          paginationClickHandler: page => () => setPageNumber(page),
+          paginationTarget: "#"
+        }}
+        itemHeadingLevel={3}
+      />
+    </>
+  );
 }
 
-export default withTranslation()(connectAndFetch(ProjectResourcesContainer));
+ProjectResourcesContainer.displayName =
+  "Frontend.Containers.ProjectResourcesContainer";
+
+ProjectResourcesContainer.propTypes = {
+  project: PropTypes.object,
+  journalBreadcrumbs: PropTypes.array
+};
