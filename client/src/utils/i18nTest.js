e7af8315c7d4faad45c57daa2f58f67e86da6366
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-04-06 08:58:40 -0700

    [C] Fix forms/upload test

--- client/src/utils/i18nTest.js
+++ client/src/utils/i18nTest.js
@@ -3,7 +3,14 @@ import { initReactI18next } from "react-i18next";
 
 i18n.use(initReactI18next).init({
   fallbackLng: "en",
-  resources: { en: { translation: { date_fns: null } } }
+  resources: {
+    en: {
+      translation: {
+        date_fns: null,
+        forms: { upload: { image_preview: "<0></0><br/><1></1>" } }
+      }
+    }
+  }
 });
 
 export default i18n;
