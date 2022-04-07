ManifoldScholar/manifold

commit 39ae9d0bb2c53c3a8e10aefe97ec4da553342093

[B] Refactor annotation popup into accessible menu

client/src/reader/containers/annotation/annotatable-components/CaptureClick.js
@@ -99,6 +99,8 @@ export default class AnnotatableCaptureClick extends Component {
   }
 
   render() {
+    /* Element captures events that bubble from nested interactive elements */
+    /* eslint-disable jsx-a11y/no-static-element-interactions */
     return (
       <div
         className="no-focus-outline"

client/src/reader/containers/annotation/annotatable-components/CaptureSelection.js
@@ -15,8 +15,14 @@ export default class AnnotatableCaptureSelection extends Component {
       popupTriggerY: PropTypes.number
     }),
     updateSelection: PropTypes.func.isRequired,
-    children: PropTypes.node.isRequired
+    children: PropTypes.node.isRequired,
+    popupRef: PropTypes.object
   };
+
+  constructor(props) {
+    super(props);
+    this.lastSelection = React.createRef();
+  }
 
   componentDidMount() {
     document.addEventListener("selectionchange", this.handleSelectionChange);
@@ -245,13 +251,33 @@ export default class AnnotatableCaptureSelection extends Component {
       this.updateSelectionState(event, true);
   };
 
+  get focusInPopup() {
+    const selection = document.getSelection();
+    return (
+      this.props.popupRef?.contains(document.activeElement) ||
+      this.props.popupRef?.contains(selection.focusNode)
+    );
+  }
+
+  /**
+   * When the annotation popup appears and focus is set inside it,
+   * Safari and Firefox detect a `selectionchange` event, causing the selection
+   * to be cleared and the popup to be hidden. (Chromium browsers don't do this.)
+   * So we check if the popup contains focus before doing anything with the selection change.
+   */
   handleSelectionChange = event => {
+    if (this.focusInPopup) return;
+
+    this.lastSelection.current = document.getSelection();
     this.updateSelectionState(event);
   };
 
   render() {
+    /* Element captures events that bubble from nested interactive elements */
+    /* eslint-disable jsx-a11y/no-static-element-interactions */
     return (
       <div
+        className="no-focus-outline"
         onTouchEnd={this.handleTouchEnd}
         onMouseUp={this.handleMouseUp}
         onKeyUp={this.handleKeyUp}

client/src/reader/containers/annotation/annotatable-components/Popup/LinkMenu.js
@@ -1,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Menus } from "reader/components/annotation/popup";
+import { useAnnotationMenu } from "reader/components/annotation/popup/hooks";
+
+function LinkMenu({ direction, visible, actions, activeEvent }) {
+  const { menus, activeMenu } = useAnnotationMenu({
+    menuArray: ["main"],
+    defaultMenu: "main",
+    visible
+  });
+
+  return (
+    <Menus.FollowLink
+      menu={menus.main}
+      visible={activeMenu === "main"}
+      direction={direction}
+      activeEvent={activeEvent}
+      actions={actions}
+    />
+  );
+}
+
+LinkMenu.displayName = "Annotation.Popup.LinkMenu";
+
+LinkMenu.propTypes = {
+  activeEvent: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired,
+  direction: PropTypes.oneOf(["up", "down"]),
+  visible: PropTypes.bool
+};
+
+export default LinkMenu;

client/src/reader/containers/annotation/annotatable-components/Popup/LoginMenu.js
@@ -1,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Menus } from "reader/components/annotation/popup";
+import { useAnnotationMenu } from "reader/components/annotation/popup/hooks";
+
+function LoginMenu({ direction, actions, visible }) {
+  const { menus, activeMenu } = useAnnotationMenu({
+    menuArray: ["main"],
+    defaultMenu: "main",
+    visible
+  });
+
+  return (
+    <Menus.Login
+      menu={menus.main}
+      visible={activeMenu === "main"}
+      direction={direction}
+      actions={actions}
+    />
+  );
+}
+
+LoginMenu.displayName = "Annotation.Popup.LoginMenu";
+
+LoginMenu.propTypes = {
+  actions: PropTypes.object.isRequired,
+  direction: PropTypes.oneOf(["up", "down"]),
+  visible: PropTypes.bool
+};
+
+export default LoginMenu;

client/src/reader/containers/annotation/annotatable-components/Popup/PrimaryMenu.js
@@ -1,0 +1,95 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Menus } from "reader/components/annotation/popup";
+import { useAnnotationMenu } from "reader/components/annotation/popup/hooks";
+
+import withReadingGroups from "hoc/withReadingGroups";
+
+function PrimaryMenu({
+  currentAnnotatingReadingGroup,
+  readingGroups,
+  setAnnotatingReadingGroup,
+  text,
+  section,
+  selectionState,
+  actions,
+  activeAnnotation,
+  direction,
+  visible
+}) {
+  const {
+    menus,
+    activeMenu,
+    lastActiveMenu,
+    setActiveMenu,
+    handleKeyDown
+  } = useAnnotationMenu({
+    menuArray: ["main", "share", "readingGroup"],
+    defaultMenu: "main",
+    visible
+  });
+
+  function handleReadingGroupSelect(id) {
+    if (id === currentAnnotatingReadingGroup) return;
+    setAnnotatingReadingGroup(id);
+  }
+
+  const submenuProps = {
+    direction,
+    onBackClick: () => setActiveMenu("main"),
+    onKeyDown: event => handleKeyDown(event)
+  };
+
+  return (
+    <>
+      <Menus.Main
+        menu={menus.main}
+        visible={activeMenu === "main"}
+        direction={direction}
+        activeMenu={activeMenu}
+        lastActiveMenu={lastActiveMenu}
+        openSubmenu={name => setActiveMenu(name)}
+        onKeyDown={event => handleKeyDown(event, "main")}
+        text={text}
+        actions={actions}
+        activeAnnotation={activeAnnotation}
+        readingGroups={readingGroups}
+        currentReadingGroup={currentAnnotatingReadingGroup}
+      />
+      <Menus.Share
+        menu={menus.share}
+        visible={activeMenu === "share"}
+        text={text}
+        section={section}
+        selectionState={selectionState}
+        actions={actions}
+        {...submenuProps}
+      />
+      <Menus.ReadingGroup
+        menu={menus.readingGroup}
+        visible={activeMenu === "readingGroup"}
+        onSelect={handleReadingGroupSelect}
+        readingGroups={readingGroups}
+        currentReadingGroup={currentAnnotatingReadingGroup}
+        {...submenuProps}
+      />
+    </>
+  );
+}
+
+PrimaryMenu.displayName = "Annotation.Popup.PrimaryMenu";
+
+PrimaryMenu.propTypes = {
+  actions: PropTypes.object.isRequired,
+  text: PropTypes.object.isRequired,
+  section: PropTypes.object.isRequired,
+  selectionState: PropTypes.object.isRequired,
+  activeAnnotation: PropTypes.object,
+  currentAnnotatingReadingGroup: PropTypes.string,
+  readingGroups: PropTypes.array,
+  setAnnotatingReadingGroup: PropTypes.func,
+  direction: PropTypes.oneOf(["up", "down"]),
+  visible: PropTypes.bool
+};
+
+export default withReadingGroups(PrimaryMenu);

client/src/reader/containers/annotation/annotatable-components/Popup/index.js
@@ -1,0 +1,76 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import Positioner from "./Positioner";
+import PrimaryMenu from "./PrimaryMenu";
+import LinkMenu from "./LinkMenu";
+import LoginMenu from "./LoginMenu";
+import Authorize from "hoc/Authorize";
+
+export default class AnnotatablePopup extends PureComponent {
+  static displayName = "Annotatable.Popup";
+
+  static propTypes = {
+    annotationState: PropTypes.string,
+    selectionState: PropTypes.object,
+    annotatableRef: PropTypes.object,
+    activeEvent: PropTypes.object,
+    setPopupRef: PropTypes.func
+  };
+
+  get showLinkMenu() {
+    const { activeEvent } = this.props;
+    if (!activeEvent) return false;
+    return activeEvent.annotationIds && activeEvent.link;
+  }
+
+  get visible() {
+    const { selection, selectionComplete } = this.props.selectionState;
+    return !!(selection && selectionComplete);
+  }
+
+  render() {
+    return (
+      <Positioner
+        locked={this.props.annotationState === "locked"}
+        selectionState={this.props.selectionState}
+        annotatableRef={this.props.annotatableRef}
+        setPopupRef={this.props.setPopupRef}
+        className={classNames({
+          "annotation-popup": true,
+          "annotation-popup--visible": this.visible
+        })}
+      >
+        {({ direction }) =>
+          this.visible && (
+            <>
+              <Authorize kind="unauthenticated">
+                <LoginMenu
+                  {...this.props}
+                  direction={direction}
+                  visible={this.visible}
+                />
+              </Authorize>
+              <Authorize kind="any">
+                {this.showLinkMenu && (
+                  <LinkMenu
+                    {...this.props}
+                    direction={direction}
+                    visible={this.visible}
+                  />
+                )}
+                {!this.showLinkMenu && (
+                  <PrimaryMenu
+                    {...this.props}
+                    direction={direction}
+                    visible={this.visible}
+                  />
+                )}
+              </Authorize>
+            </>
+          )
+        }
+      </Positioner>
+    );
+  }
+}

client/src/frontend/components/collecting/Dialog/index.js
@@ -52,6 +52,7 @@ function CollectingDialog({
       describedBy={uidSeed("description")}
       closeCallback={onClose}
       maxWidth={550}
+      onClick={event => event.stopPropagation()}
     >
       <Styled.Inner>
         <Styled.Header>

commit 53e043281809a0d5dfe54b1169eb27fca132d05d

[F] Finalize Safari/FF keyboard annotation support
This commit addresses browser inconsistencies around using the
annotation popup that were introduced by 39ae9d0. That commit refactored
the menu using the WAI-ARIA menu design pattern and implemented keyboard
functionality for the first time. However, due to the way Safari and
Firefox interweave the document's focus and selection states (see
https://github.com/ManifoldScholar/manifold/pull/3086#issuecomment-1030141362),
when focus was moved around the menu it triggered a loss of selection
highlighting in these browsers. Because it did this for both pointer and
keyboard interactions, we felt that this was an unacceptable visual
regression, even if it didn't impact how Manifold internally tracks a
selection for annotating, highlighting, sharing, or attaching a resource
to.

As a workaround, this commit tracks whether the user has opened the menu
with a pointer or a keyboard. For pointer users, it now avoids managing
focus, so that Safari and Firefox maintain the current selection. For
keyboard users, focus is managed by the menu, which is necessary for
interaction. This does cause loss of selection highlighting for keyboard
users of these browsers, but this is ultimately unavoidable and beyond
the responsibility of Manifold (see
https://github.com/ManifoldScholar/manifold/issues/2326#issuecomment-523208990
for more on this). This is admittedly not an ideal solution, but it is
the best we can do under the constraints of browser inconsistencies.
Chromium-based browsers continue to work as desired.

Fortunately this is probably a temporary solution, as the CSS Custom
Highlight API should create an alternative, standards-driven path to
storing selection in the browser.

https://www.w3.org/TR/css-highlight-api-1/
https://css-tricks.com/css-custom-highlight-api-early-loo/

client/src/reader/components/annotation/popup/hooks/useAnnotationMenu/index.js
@@ -15,11 +15,23 @@ export default function useAnnotationMenu({ menuArray, defaultMenu, visible }) {
    */
   /* eslint-disable react-hooks/rules-of-hooks */
   const menus = Object.fromEntries(
-    menuArray.map(menu => [menu, useMenuState(MENU_OPTIONS)])
+    menuArray.map(menu => [
+      menu,
+      {
+        ...useMenuState(MENU_OPTIONS),
+        // Prevent reakit from moving focus based on mouse events,
+        // which causes selection highlighting to disappear in FF and Safari
+        // see https://github.com/ManifoldScholar/manifold/pull/3086#issuecomment-1030141362
+        onMouseEnter: event => event.preventDefault(),
+        onMouseMove: event => event.preventDefault(),
+        onMouseLeave: event => event.preventDefault(),
+        onMouseDown: event => event.preventDefault()
+      }
+    ])
   );
   /* eslint-enable react-hooks/rules-of-hooks */
 
-  const [activeMenu, setActiveMenu] = useState(null);
+  const [activeMenu, setMenu] = useState(null);
 
   // track the previous state so that we can detect movement between two menus (see `handleKeyDown`)
   useEffect(() => {
@@ -29,12 +41,12 @@ export default function useAnnotationMenu({ menuArray, defaultMenu, visible }) {
   useEffect(() => {
     if (visible) {
       lastFocus.current = document.activeElement;
-      setActiveMenu(defaultMenu);
+      setMenu(defaultMenu);
     } else {
       if (lastFocus.current) {
         lastFocus.current.focus();
       }
-      setActiveMenu(null);
+      setMenu(null);
     }
   }, [visible, defaultMenu]);
 
@@ -49,16 +61,26 @@ export default function useAnnotationMenu({ menuArray, defaultMenu, visible }) {
         target: { dataset }
       } = event;
 
-      if (key === "ArrowRight" && sourceMenu === defaultMenu && dataset.name) {
-        setActiveMenu(dataset.name);
+      if (key === "ArrowRight") {
+        if (sourceMenu === defaultMenu && dataset.name) {
+          setMenu(dataset.name);
+        } else {
+          event.preventDefault(); // prevent arrowing out of menu altogether in caret browsing mode
+        }
       }
 
-      if (key === "ArrowLeft" && sourceMenu !== defaultMenu) {
-        setActiveMenu("main");
+      if (key === "ArrowLeft") {
+        if (sourceMenu !== defaultMenu) {
+          setMenu("main");
+        } else {
+          event.preventDefault(); // prevent arrowing out of menu altogether in caret browsing mode
+        }
       }
     },
     [defaultMenu]
   );
+
+  const setActiveMenu = useCallback(name => setMenu(name), []);
 
   return {
     menus,

client/src/reader/components/annotation/popup/menus/FollowLink.js
@@ -4,7 +4,14 @@ import PropTypes from "prop-types";
 import Menu from "../parts/Menu";
 import MenuItem from "../parts/MenuItem";
 
-function FollowLink({ menu, visible, direction, activeEvent, actions }) {
+function FollowLink({
+  menu,
+  visible,
+  direction,
+  activeEvent,
+  actions,
+  isKeyboardSelection
+}) {
   const { link, annotationIds } = activeEvent;
   const { openViewAnnotationsDrawer } = actions;
 
@@ -20,6 +27,7 @@ function FollowLink({ menu, visible, direction, activeEvent, actions }) {
       visible={visible}
       aria-label={t("reader.menus.popup.follow_link")}
       direction={direction}
+      isKeyboardSelection={isKeyboardSelection}
     >
       <MenuItem
         menu={{ ...menu, visible }}

client/src/reader/components/annotation/popup/menus/Login.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import Menu from "../parts/Menu";
 import MenuItem from "../parts/MenuItem";
 
-function Login({ menu, visible, direction, actions }) {
+function Login({ menu, visible, direction, actions, isKeyboardSelection }) {
   const { t } = useTranslation();
 
   return (
@@ -13,6 +13,7 @@ function Login({ menu, visible, direction, actions }) {
       visible={visible}
       aria-label={t("reader.menus.popup.log_in_annotate")}
       direction={direction}
+      isKeyboardSelection={isKeyboardSelection}
     >
       <MenuItem
         menu={menu}
@@ -31,7 +32,8 @@ Login.propTypes = {
   menu: PropTypes.object.isRequired,
   actions: PropTypes.object.isRequired,
   direction: PropTypes.oneOf(["up", "down"]),
-  visible: PropTypes.bool
+  visible: PropTypes.bool,
+  isKeyboardSelection: PropTypes.bool
 };
 
 export default Login;

client/src/reader/components/annotation/popup/menus/Main/Items/CurrentReadingGroup.js
@@ -44,13 +44,13 @@ function CurrentReadingGroup({
         <span className="screen-reader-text">
           {t("reader.actions.select_reading_group")}
         </span>
-        <span className="annotation-popup__button-text">
+        <span className="annotation-popup__button-text" aria-hidden>
           {canAccessReadingGroups
             ? t("reader.menus.popup.current_group")
             : t("reader.menus.popup.current_visibility")}
           :
         </span>
-        <div className="annotation-popup__button-inner-row">
+        <div className="annotation-popup__button-inner-row" aria-hidden>
           <span className="annotation-popup__button-text annotation-popup__button-text--small">
             {getCurrentGroupName()}
           </span>

client/src/reader/components/annotation/popup/menus/Main/Items/Highlight.js
@@ -30,7 +30,11 @@ function Highlight({ menu, actions, activeAnnotation }) {
         })}
         kind="any"
         label={t("reader.menus.popup.highlight")}
-        srLabel={t("reader.menus.popup.highlight_selection")}
+        srLabel={
+          highlighted
+            ? t("reader.menus.popup.unhighlight_selection")
+            : t("reader.menus.popup.highlight_selection")
+        }
         icon="annotate24"
       />
       {highlighted && (

client/src/reader/components/annotation/popup/menus/Main/index.js
@@ -12,6 +12,7 @@ function MainMenu({
   direction,
   onKeyDown,
   openSubmenu,
+  isKeyboardSelection,
   ...restProps
 }) {
   const itemProps = {
@@ -31,6 +32,7 @@ function MainMenu({
       lastActiveMenu={lastActiveMenu}
       direction={direction}
       onKeyDown={onKeyDown}
+      isKeyboardSelection={isKeyboardSelection}
     >
       <MenuItems.Share {...itemProps} onClick={() => openSubmenu("share")} />
       <MenuItems.Notate {...itemProps} />
@@ -55,7 +57,8 @@ MainMenu.propTypes = {
   lastActiveMenu: PropTypes.shape({
     current: PropTypes.string
   }),
-  direction: PropTypes.oneOf(["up", "down"])
+  direction: PropTypes.oneOf(["up", "down"]),
+  isKeyboardSelection: PropTypes.bool
 };
 
 export default MainMenu;

client/src/reader/components/annotation/popup/menus/ReadingGroup.js
@@ -20,7 +20,8 @@ function ReadingGroupMenu({
   readingGroups,
   currentReadingGroup,
   onSelect,
-  currentUser
+  currentUser,
+  isKeyboardSelection
 }) {
   const context = useReaderContext();
   const canEngagePublicly = context.attributes.abilities.engagePublicly;
@@ -39,6 +40,7 @@ function ReadingGroupMenu({
       aria-label={t("glossary.reading_group_one")}
       direction={direction}
       onKeyDown={onKeyDown}
+      isKeyboardSelection={isKeyboardSelection}
     >
       <div className="annotation-popup__header">
         <IconComposer icon="readingGroup24" size="default" />
@@ -115,7 +117,8 @@ ReadingGroupMenu.propTypes = {
   readingGroups: PropTypes.array,
   currentReadingGroup: PropTypes.string.isRequired,
   onSelect: PropTypes.func.isRequired,
-  currentUser: PropTypes.object
+  currentUser: PropTypes.object,
+  isKeyboardSelection: PropTypes.bool
 };
 
 export default withCurrentUser(ReadingGroupMenu);

client/src/reader/components/annotation/popup/menus/Share.js
@@ -15,7 +15,8 @@ function ShareMenu({
   selectionState,
   actions,
   onBackClick,
-  onKeyDown
+  onKeyDown,
+  isKeyboardSelection
 }) {
   const {
     facebookAppId,
@@ -42,6 +43,7 @@ function ShareMenu({
       aria-label={t("actions.share")}
       direction={direction}
       onKeyDown={onKeyDown}
+      isKeyboardSelection={isKeyboardSelection}
     >
       {canCite && (
         <MenuItem
@@ -100,7 +102,8 @@ ShareMenu.propTypes = {
   direction: PropTypes.oneOf(["up", "down"]),
   visible: PropTypes.bool,
   text: PropTypes.object,
-  selectionState: PropTypes.object
+  selectionState: PropTypes.object,
+  isKeyboardSelection: PropTypes.bool
 };
 
 export default ShareMenu;

client/src/reader/components/annotation/popup/parts/Menu/index.js
@@ -9,13 +9,14 @@ function Menu({
   lastActiveMenu,
   onKeyDown,
   direction,
+  isKeyboardSelection = false,
   children,
   ...menuProps
 }) {
   // Focus on first interactive el when menu becomes visible.
   // Or on last interactive el if moving from RG menu to main menu.
   useEffect(() => {
-    if (!menuProps.visible) return;
+    if (!menuProps.visible || !isKeyboardSelection) return;
     const fromRGtoMain =
       activeMenu === "main" && lastActiveMenu?.current === "readingGroup";
     fromRGtoMain ? menu.last() : menu.first();
@@ -36,7 +37,7 @@ function Menu({
     <ReakitMenu
       {...menu}
       {...menuProps}
-      unstable_finalFocusRef={undefined} // we handle this on our own since there's no disclosure component
+      unstable_autoFocusOnHide={false} // we handle this on our own since there's no disclosure component
       className={menuClassName}
       onKeyDown={onKeyDown}
     >

client/src/reader/containers/annotation/annotatable-components/CaptureClick.js
@@ -42,21 +42,21 @@ export default class AnnotatableCaptureClick extends Component {
     }
   };
 
-  handleKeyDown = event => {
+  handleKeyUp = event => {
     if (!event || !event.target || (event.key !== "Enter" && event.key !== " "))
       return;
-    this.handleClick(event);
+    this.handleClick(event, true);
   };
 
-  handleClick = event => {
+  handleClick = (event, isKeyEvent = false) => {
     if (!event || !event.target) return;
     const el = event.target;
     if (this.doesElementContainAnnotationAndHighlight(el))
       this.handleDisambiguationClick(event, el);
     if (this.doesElementContainRemovableHighlight(el))
-      return this.handleRemovableHighlightClick(event, el);
+      return this.handleRemovableHighlightClick(event, el, isKeyEvent);
     if (this.doesElementContainAnnotation(el))
-      return this.handleAnnotationClick(event, el);
+      return this.handleAnnotationClick(event, el, isKeyEvent);
   };
 
   handleDisambiguationClick(eventIgnored, el) {
@@ -68,26 +68,28 @@ export default class AnnotatableCaptureClick extends Component {
     });
   }
 
-  handleRemovableHighlightClick(event, el) {
+  handleRemovableHighlightClick(event, el, isKeyEvent) {
     event.preventDefault();
     event.stopPropagation();
     const id = el.dataset.removableHighlightId;
-    this.props.updateActiveAnnotation(id, event);
+    this.props.updateActiveAnnotation(id, event, { isKeyEvent });
   }
 
-  handleAnnotationClick(event, el) {
+  handleAnnotationClick(event, el, isKeyEvent) {
     event.preventDefault();
     const link = selectionHelpers.closest(el, "a");
     const annotationIds = this.elementAnnotationIds(el);
-    if (link) return this.showLinkMenu(event, el, annotationIds, link);
+    if (link)
+      return this.showLinkMenu(event, el, annotationIds, link, isKeyEvent);
     this.props.actions.openViewAnnotationsDrawer(annotationIds);
   }
 
-  showLinkMenu(event, el, annotationIds, link) {
+  showLinkMenu(event, el, annotationIds, link, isKeyEvent) {
     event.stopPropagation();
     const eventInfo = {
       annotationIds,
-      link
+      link,
+      isKeyEvent
     };
     this.props.updateActiveAnnotation(annotationIds[0], event, eventInfo);
   }
@@ -105,7 +107,7 @@ export default class AnnotatableCaptureClick extends Component {
       <div
         className="no-focus-outline"
         onClick={this.handleClick}
-        onKeyDown={this.handleKeyDown}
+        onKeyUp={this.handleKeyUp}
         onMouseUp={this.handleMouseUp}
         tabIndex={-1}
       >

client/src/reader/containers/annotation/annotatable-components/CaptureSelection.js
@@ -112,7 +112,11 @@ export default class AnnotatableCaptureSelection extends Component {
     return event.type !== "selectionchange";
   }
 
-  updateSelectionState(event = null, selectionComplete = false) {
+  updateSelectionState(
+    event = null,
+    selectionComplete = false,
+    isKeyboardSelection = false
+  ) {
     try {
       const { selectionState } = this.props;
       const selection = this.mapNativeSelection(this.nativeSelection);
@@ -128,7 +132,8 @@ export default class AnnotatableCaptureSelection extends Component {
         ),
         selectionComplete: complete,
         popupTriggerX: selection && x ? x : selectionState.popupTriggerX,
-        popupTriggerY: selection && y ? y : selectionState.popupTriggerY
+        popupTriggerY: selection && y ? y : selectionState.popupTriggerY,
+        isKeyboardSelection
       });
       this.props.updateSelection(newState);
     } catch (error) {
@@ -248,7 +253,7 @@ export default class AnnotatableCaptureSelection extends Component {
   handleKeyUp = event => {
     const { key, shiftKey } = event;
     if (key === "Shift" && shiftKey === false)
-      this.updateSelectionState(event, true);
+      this.updateSelectionState(event, true, true);
   };
 
   get focusInPopup() {

client/src/reader/containers/annotation/annotatable-components/Popup/LinkMenu.js
@@ -3,7 +3,13 @@ import PropTypes from "prop-types";
 import { Menus } from "reader/components/annotation/popup";
 import { useAnnotationMenu } from "reader/components/annotation/popup/hooks";
 
-function LinkMenu({ direction, visible, actions, activeEvent }) {
+function LinkMenu({
+  direction,
+  visible,
+  actions,
+  activeEvent,
+  isKeyboardSelection
+}) {
   const { menus, activeMenu } = useAnnotationMenu({
     menuArray: ["main"],
     defaultMenu: "main",
@@ -17,6 +23,7 @@ function LinkMenu({ direction, visible, actions, activeEvent }) {
       direction={direction}
       activeEvent={activeEvent}
       actions={actions}
+      isKeyboardSelection={isKeyboardSelection}
     />
   );
 }
@@ -27,7 +34,8 @@ LinkMenu.propTypes = {
   activeEvent: PropTypes.object.isRequired,
   actions: PropTypes.object.isRequired,
   direction: PropTypes.oneOf(["up", "down"]),
-  visible: PropTypes.bool
+  visible: PropTypes.bool,
+  isKeyboardSelection: PropTypes.bool
 };
 
 export default LinkMenu;

client/src/reader/containers/annotation/annotatable-components/Popup/LoginMenu.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { Menus } from "reader/components/annotation/popup";
 import { useAnnotationMenu } from "reader/components/annotation/popup/hooks";
 
-function LoginMenu({ direction, actions, visible }) {
+function LoginMenu({ direction, actions, visible, isKeyboardSelection }) {
   const { menus, activeMenu } = useAnnotationMenu({
     menuArray: ["main"],
     defaultMenu: "main",
@@ -16,6 +16,7 @@ function LoginMenu({ direction, actions, visible }) {
       visible={activeMenu === "main"}
       direction={direction}
       actions={actions}
+      isKeyboardSelection={isKeyboardSelection}
     />
   );
 }
@@ -25,7 +26,8 @@ LoginMenu.displayName = "Annotation.Popup.LoginMenu";
 LoginMenu.propTypes = {
   actions: PropTypes.object.isRequired,
   direction: PropTypes.oneOf(["up", "down"]),
-  visible: PropTypes.bool
+  visible: PropTypes.bool,
+  isKeyboardSelection: PropTypes.bool
 };
 
 export default LoginMenu;

client/src/reader/containers/annotation/annotatable-components/Popup/PrimaryMenu.js
@@ -15,7 +15,8 @@ function PrimaryMenu({
   actions,
   activeAnnotation,
   direction,
-  visible
+  visible,
+  isKeyboardSelection
 }) {
   const {
     menus,
@@ -36,6 +37,7 @@ function PrimaryMenu({
 
   const submenuProps = {
     direction,
+    isKeyboardSelection,
     onBackClick: () => setActiveMenu("main"),
     onKeyDown: event => handleKeyDown(event)
   };
@@ -55,6 +57,7 @@ function PrimaryMenu({
         activeAnnotation={activeAnnotation}
         readingGroups={readingGroups}
         currentReadingGroup={currentAnnotatingReadingGroup}
+        isKeyboardSelection={isKeyboardSelection}
       />
       <Menus.Share
         menu={menus.share}
@@ -89,7 +92,8 @@ PrimaryMenu.propTypes = {
   readingGroups: PropTypes.array,
   setAnnotatingReadingGroup: PropTypes.func,
   direction: PropTypes.oneOf(["up", "down"]),
-  visible: PropTypes.bool
+  visible: PropTypes.bool,
+  isKeyboardSelection: PropTypes.bool
 };
 
 export default withReadingGroups(PrimaryMenu);

client/src/reader/containers/annotation/annotatable-components/Popup/index.js
@@ -29,6 +29,13 @@ export default class AnnotatablePopup extends PureComponent {
     return !!(selection && selectionComplete);
   }
 
+  get isKeyboardSelection() {
+    return (
+      this.props.selectionState?.isKeyboardSelection ||
+      this.props.activeEvent?.isKeyEvent
+    );
+  }
+
   render() {
     return (
       <Positioner
@@ -49,6 +56,7 @@ export default class AnnotatablePopup extends PureComponent {
                   {...this.props}
                   direction={direction}
                   visible={this.visible}
+                  isKeyboardSelection={this.isKeyboardSelection}
                 />
               </Authorize>
               <Authorize kind="any">
@@ -57,6 +65,7 @@ export default class AnnotatablePopup extends PureComponent {
                     {...this.props}
                     direction={direction}
                     visible={this.visible}
+                    isKeyboardSelection={this.isKeyboardSelection}
                   />
                 )}
                 {!this.showLinkMenu && (
@@ -64,6 +73,7 @@ export default class AnnotatablePopup extends PureComponent {
                     {...this.props}
                     direction={direction}
                     visible={this.visible}
+                    isKeyboardSelection={this.isKeyboardSelection}
                   />
                 )}
               </Authorize>