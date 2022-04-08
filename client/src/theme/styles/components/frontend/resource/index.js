ba1be97d4431b3050028846735c675baf239ae5c
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-04-08 10:20:39 -0700

    [C] Migrate resource title + tag list

--- client/src/theme/styles/components/frontend/resource/index.js
+++ client/src/theme/styles/components/frontend/resource/index.js
@@ -2,16 +2,12 @@ import detail from "./detail";
 import meta from "./meta";
 import preview from "./preview";
 import slide from "./slide";
-import title from "./title";
-import tagList from "./tagList";
 import videoPlayer from "./videoPlayer";
 import variantList from "./variantList";
 
 export default `
 ${videoPlayer}
 ${variantList}
-${title}
-${tagList}
 ${slide}
 ${meta}
 ${preview}
