81e8686ac8e4b170a631a8a470ac4ca2b8ee5e3a
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-04-08 13:55:35 -0700

    [C] Begin migrating resource-slide

---
+++ client/src/frontend/components/resource-slide/Loading/styles.js
@@ -0,0 +1,40 @@
+import styled from "@emotion/styled";
+import Utility from "global/components/utility";
+import { utilityPrimary, respond } from "theme/styles/mixins";
+
+export const Info = styled.div`
+  padding: 20px 60px;
+  margin: auto;
+  text-align: center;
+`;
+
+export const Icon = styled(Utility.IconComposer)`
+  margin-bottom: 4px;
+
+  ${respond(
+    `
+    width: 21.34vw;
+    height: 21.34vw;`,
+    50
+  )}
+
+  ${respond(
+    `
+    width: 220px;
+    height: 220px;`,
+    120
+  )}
+`;
+
+export const Kind = styled.span`
+  ${utilityPrimary}
+  display: block;
+  padding-bottom: 6px;
+  font-size: 21px;
+  font-weight: var(--font-weight-regular);
+`;
+
+export const Date = styled.span`
+  ${utilityPrimary}
+  font-size: 12px;
+`;
