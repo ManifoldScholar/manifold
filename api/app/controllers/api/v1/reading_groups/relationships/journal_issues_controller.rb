ff5a10e32c900dd5ced19dda656c992aa3643651
Zach Davis <zach@castironcoding.com>
2022-04-11 10:55:07 -0700

    [B] Expose RG collected issues in API

---
+++ api/app/controllers/api/v1/reading_groups/relationships/journal_issues_controller.rb
@@ -0,0 +1,12 @@
+module API
+  module V1
+    module ReadingGroups
+      module Relationships
+        class JournalIssuesController < AbstractController
+          for_collectable! JournalIssue
+          include_scopes! :journal
+        end
+      end
+    end
+  end
+end
