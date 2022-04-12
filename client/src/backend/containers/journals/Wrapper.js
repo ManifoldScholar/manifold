3369c8a562e51b27c34638e6c4f8385a2f830b67
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-04-12 16:06:44 -0700

    [F] Localize journals

--- client/src/backend/containers/journals/Wrapper.js
+++ client/src/backend/containers/journals/Wrapper.js
@@ -1,28 +1,28 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 import { childRoutes } from "helpers/router";
 import Authorize from "hoc/Authorize";
 
-export default class JournalsWrapper extends PureComponent {
-  static propTypes = {
-    route: PropTypes.object
-  };
-
-  render() {
-    return (
-      <Authorize
-        ability="update"
-        entity={["journal"]}
-        failureFatalError={{
-          body: "You are not allowed to manage journals."
-        }}
-      >
-        <div>
-          <main id="skip-to-main" className="backend-detail">
-            {childRoutes(this.props.route)}
-          </main>
-        </div>
-      </Authorize>
-    );
-  }
+export default function JournalsWrapper({ route }) {
+  const { t } = useTranslation();
+  return (
+    <Authorize
+      ability="update"
+      entity={["journal"]}
+      failureFatalError={{
+        body: t("backend_entities.journals.unauthorized")
+      }}
+    >
+      <div>
+        <main id="skip-to-main" className="backend-detail">
+          {childRoutes(route)}
+        </main>
+      </div>
+    </Authorize>
+  );
 }
+
+JournalsWrapper.propTypes = {
+  route: PropTypes.object
+};
