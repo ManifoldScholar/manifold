12fba81a05cb996e609b21079d7b768a3a02fa0b
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-04-13 08:49:26 -0700

    [F] Localize projects

--- client/src/backend/containers/projects/Wrapper.js
+++ client/src/backend/containers/projects/Wrapper.js
@@ -1,39 +1,38 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 import Navigation from "backend/components/navigation";
 import { childRoutes, RedirectToFirstMatch } from "helpers/router";
 import lh from "helpers/linkHandler";
 import navigation from "helpers/router/navigation";
 import Authorize from "hoc/Authorize";
 
-export default class ProjectsWrapper extends PureComponent {
-  static propTypes = {
-    route: PropTypes.object,
-    dispatch: PropTypes.func.isRequired
-  };
+export default function ProjectsWrapper({ route }) {
+  const { t } = useTranslation();
+  const secondaryLinks = navigation.projects();
 
-  render() {
-    const secondaryLinks = navigation.projects();
-
-    return (
-      <Authorize
-        ability="update"
-        entity={["project"]}
-        failureFatalError={{
-          body: "You are not allowed to manage projects."
-        }}
-      >
-        <div>
-          <RedirectToFirstMatch
-            from={lh.link("backendProjects")}
-            candidates={secondaryLinks}
-          />
-          <Navigation.Secondary links={secondaryLinks} />
-          <main id="skip-to-main" className="backend-detail">
-            {childRoutes(this.props.route)}
-          </main>
-        </div>
-      </Authorize>
-    );
-  }
+  return (
+    <Authorize
+      ability="update"
+      entity={["project"]}
+      failureFatalError={{
+        body: t("backend_entities.projects.unauthorized")
+      }}
+    >
+      <div>
+        <RedirectToFirstMatch
+          from={lh.link("backendProjects")}
+          candidates={secondaryLinks}
+        />
+        <Navigation.Secondary links={secondaryLinks} />
+        <main id="skip-to-main" className="backend-detail">
+          {childRoutes(route)}
+        </main>
+      </div>
+    </Authorize>
+  );
 }
+
+ProjectsWrapper.propTypes = {
+  route: PropTypes.object
+};
