b83ae10258f6cc39476bf9888335f7374850e727
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-04-15 10:02:34 -0700

    [F] Refactor + localize backend Metadata.Form

--- client/src/backend/components/metadata/Form.js
+++ client/src/backend/components/metadata/Form.js
@@ -1,35 +1,24 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 import FormContainer from "global/containers/form";
 import Form from "global/components/form";
-import config from "config";
 import humps from "humps";
 import intersection from "lodash/intersection";
 import reduce from "lodash/reduce";
 import concat from "lodash/concat";
 import difference from "lodash/difference";
 
-export default class FormMakers extends PureComponent {
-  static displayName = "Metadata.Form";
+export default function MetadataForm({ model, ...restProps }) {
+  const { t } = useTranslation();
 
-  static propTypes = {
-    model: PropTypes.object.isRequired
-  };
+  const { attributes } = model ?? {};
+  const properties = attributes?.metadataProperties?.sort() ?? [];
 
-  get attributes() {
-    return this.props.model.attributes;
-  }
-
-  get metadataProperties() {
-    if (!this.attributes || !this.attributes.metadataProperties) return [];
-    return this.attributes.metadataProperties.sort();
-  }
-
-  get baseStructure() {
-    const keys = this.metadataProperties;
+  const baseStructure = () => {
     return [
       {
-        label: "Copyright",
+        label: t("metadata.categories.copyright"),
         children: intersection(
           [
             "creator",
@@ -39,18 +28,18 @@ export default class FormMakers extends PureComponent {
             "restrictions",
             "credit"
           ],
-          keys
+          properties
         )
       },
       {
-        label: "Identity",
+        label: t("metadata.categories.identity"),
         children: intersection(
           ["isbn", "issn", "doi", "resourcesDoi", "uniqueIdentifier"],
-          keys
+          properties
         )
       },
       {
-        label: "Publisher",
+        label: t("metadata.categories.publisher"),
         children: intersection(
           [
             "publisher",
@@ -59,11 +48,11 @@ export default class FormMakers extends PureComponent {
             "originalPublisherPlace",
             "status"
           ],
-          keys
+          properties
         )
       },
       {
-        label: "Bibliographic",
+        label: t("metadata.categories.bibliographic"),
         children: intersection(
           [
             "containerTitle",
@@ -76,23 +65,22 @@ export default class FormMakers extends PureComponent {
             "originalTitle",
             "originalPublicationDate"
           ],
-          keys
+          properties
         )
       },
       {
-        label: "Accessibility",
-        children: intersection(["altText"], keys)
+        label: t("metadata.categories.accessibility"),
+        children: intersection(["altText"], properties)
       }
     ];
-  }
+  };
 
-  get structure() {
-    const keys = this.metadataProperties;
-    const filteredStructure = this.baseStructure.filter(
-      group => group.children.length > 0
+  const structure = () => {
+    const filteredStructure = baseStructure().filter(
+      group => group.children?.length > 0
     );
     const leftovers = difference(
-      keys,
+      properties,
       reduce(
         filteredStructure,
         (used, group) => concat(used, group.children),
@@ -101,77 +89,75 @@ export default class FormMakers extends PureComponent {
     );
     if (leftovers.length > 0) {
       filteredStructure.push({
-        label: "Other",
+        label: t("metadata.categories.other"),
         children: leftovers
       });
     }
     return filteredStructure;
-  }
+  };
 
-  get config() {
-    return config.app.locale.metadata;
-  }
+  const getLocalized = (prop, strType) => {
+    const i18nKey = humps.decamelize(prop, { separator: "_" }).toLowerCase();
 
-  configFor(prop) {
-    return this.config[prop];
-  }
+    switch (strType) {
+      case "label":
+        return t(`metadata.${i18nKey}`);
+      case "placeholder":
+        return t(`metadata.${i18nKey}_placeholder`, { defaultValue: "" });
+      case "instructions":
+        return t(`metadata.${i18nKey}_instructions`, { defaultValue: "" });
+      default:
+        return "";
+    }
+  };
 
-  labelize(prop) {
-    return humps.decamelize(prop, { separator: " " });
-  }
+  const componentAndPropsFor = prop => {
+    switch (prop) {
+      case "rights":
+        return { component: Form.TextArea, props: null };
+      case "resourcesDoi":
+        return { component: Form.MaskedTextInput, props: { mask: "doi" } };
+      case "originalPublicationDate":
+        return { component: Form.Date, props: null };
+      default:
+        return { component: Form.TextInput, props: null };
+    }
+  };
 
-  configValueFor(prop, key) {
-    const propConfig = this.configFor(prop);
-    if (!propConfig || !propConfig.placeholder) return null;
-    return propConfig[key];
-  }
-
-  placeholderFor(prop) {
-    return this.configValueFor(prop, "placeholder");
-  }
-
-  inputPropsFor(prop) {
-    return this.configValueFor(prop, "inputProps");
-  }
-
-  componentFor(prop) {
-    const key = this.configValueFor(prop, "type");
-    const component = Form[key];
-    return component || Form.TextInput;
-  }
-
-  instructionsFor(prop) {
-    return this.configValueFor(prop, "instructions");
-  }
-
-  render() {
-    return (
-      <section>
-        <FormContainer.Form {...this.props}>
-          {this.structure.map((group, gi) => {
-            return (
-              <Form.FieldGroup label={group.label} key={group.label}>
-                {group.children.map((prop, i) => {
-                  const InputComponent = this.componentFor(prop);
-                  const focus = gi === 0 && i === 0;
-                  return (
-                    <InputComponent
-                      key={prop}
-                      focusOnMount={focus}
-                      placeholder={this.placeholderFor(prop)}
-                      instructions={this.instructionsFor(prop)}
-                      label={this.labelize(prop)}
-                      name={`attributes[metadata][${prop}]`}
-                      {...this.inputPropsFor(prop)}
-                    />
-                  );
-                })}
-              </Form.FieldGroup>
-            );
-          })}
-          <Form.Save text="Save Metadata" />
-        </FormContainer.Form>
-      </section>
-    );
-  }
+  return (
+    <section>
+      <FormContainer.Form model={model} {...restProps}>
+        {structure().map((group, gi) => {
+          return (
+            <Form.FieldGroup label={group.label} key={group.label}>
+              {group.children.map((prop, i) => {
+                const render = componentAndPropsFor(prop);
+                const InputComponent = render.component;
+                const inputProps = render.props;
+                const focus = gi === 0 && i === 0;
+                return (
+                  <InputComponent
+                    key={prop}
+                    focusOnMount={focus}
+                    placeholder={getLocalized(prop, "placeholder")}
+                    instructions={getLocalized(prop, "instructions")}
+                    label={getLocalized(prop, "label")}
+                    name={`attributes[metadata][${prop}]`}
+                    {...inputProps}
+                  />
+                );
+              })}
+            </Form.FieldGroup>
+          );
+        })}
+        <Form.Save text={t("metadata.save")} />
+      </FormContainer.Form>
+    </section>
+  );
 }
+
+MetadataForm.displayName = "Metadata.Form";
+
+MetadataForm.propTypes = {
+  model: PropTypes.object.isRequired
+};
