6f611ff46a408c9a690b0d87799459711788fd7d
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-04-22 16:08:41 -0700

    [F] Update resource comment editor design + ux

--- client/src/frontend/components/resource/Detail/index.js
+++ client/src/frontend/components/resource/Detail/index.js
@@ -1,101 +1,81 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { withTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import CommentContainer from "global/containers/comment";
 import Utility from "frontend/components/utility";
 import Hero from "../Hero";
-import Link from "../Link";
+import LinkComponent from "../Link";
 import Meta from "../Meta";
 import Title from "../Title";
 import VariantList from "../VariantList";
 import * as Styled from "./styles";
 
-class ResourceDetail extends Component {
-  static displayName = "Resource.Detail";
+export default function ResourceDetail({ resourceUrl, resource }) {
+  const { t } = useTranslation();
 
-  static propTypes = {
-    projectUrl: PropTypes.string,
-    resourceUrl: PropTypes.string.isRequired,
-    resource: PropTypes.object,
-    t: PropTypes.func
-  };
+  if (!resource) return null;
 
-  createDescription(description) {
-    if (!description) return { __html: this.props.t("errors.no_content") };
+  const createDescription = description => {
+    if (!description) return { __html: t("errors.no_content") };
     return {
       __html: description
     };
-  }
+  };
 
-  get canEngagePublicly() {
-    const { resource } = this.props;
-    if (!resource) return false;
-    return resource.attributes.abilities.engagePublicly;
-  }
+  const canEngagePublicly = () => {
+    return resource.attributes?.abilities?.engagePublicly;
+  };
 
-  // The cancel prop is required by CommentEditor.
-  // We don't render the cancel button in this context,
-  // so this is basically a stub. --MO
-  cancelComment(event) {
-    if (!event) return null;
-    event.preventDefault();
-  }
+  const attr = resource.attributes;
 
-  render() {
-    const { resource, resourceUrl } = this.props;
-    if (!resource) return null;
-    const attr = resource.attributes;
-    const t = this.props.t;
+  return (
+    <Styled.Container>
+      <Styled.ColumnWrapper>
+        <Styled.Left>
+          <Title resource={resource} showIcon={false} />
+          <Hero resource={resource} />
+          <Styled.Content>
+            <Styled.Caption
+              dangerouslySetInnerHTML={{ __html: attr.captionFormatted }}
+            />
 
-    /* eslint-disable jsx-a11y/anchor-is-valid                                          */
-    /* jsx-a11y sees the link in this component as missing a href attribute, but it's a */
-    /* false positive, as the child Link component does in fact render an a tag with a  */
-    /* href.                                                                            */
-    return (
-      <Styled.Container>
-        <Styled.ColumnWrapper>
-          <Styled.Left>
-            <Title resource={resource} showIcon={false} />
-            <Hero resource={resource} />
-            <Styled.Content>
-              <Styled.Caption
-                dangerouslySetInnerHTML={{ __html: attr.captionFormatted }}
-              />
-
-              <Styled.DescriptionHeader>
-                {t("pages.subheaders.full_description")}
-              </Styled.DescriptionHeader>
-              <div
-                dangerouslySetInnerHTML={this.createDescription(
-                  attr.descriptionFormatted
-                )}
-              />
-            </Styled.Content>
-          </Styled.Left>
-          <Styled.Right>
-            <Link attributes={attr} />
-            <Utility.ShareBar url={resourceUrl} />
-            <Meta resource={resource} layout={"secondary"} />
-            <VariantList resource={resource} />
-          </Styled.Right>
-        </Styled.ColumnWrapper>
-        <Styled.Comments>
-          {this.canEngagePublicly && (
-            <>
-              <CommentContainer.Thread subject={resource} />
-              <CommentContainer.Editor
-                focus={false}
-                label={t("actions.add_comment")}
-                subject={resource}
-                cancel={event => this.cancelComment(event)}
-              />
-            </>
-          )}
-        </Styled.Comments>
-      </Styled.Container>
-    );
-    /* eslint-enable jsx-a11y/anchor-is-valid */
-  }
+            <Styled.DescriptionHeader>
+              {t("pages.subheaders.full_description")}
+            </Styled.DescriptionHeader>
+            <div
+              dangerouslySetInnerHTML={createDescription(
+                attr.descriptionFormatted
+              )}
+            />
+          </Styled.Content>
+        </Styled.Left>
+        <Styled.Right>
+          <LinkComponent attributes={attr} />
+          <Utility.ShareBar url={resourceUrl} />
+          <Meta resource={resource} layout={"secondary"} />
+          <VariantList resource={resource} />
+        </Styled.Right>
+      </Styled.ColumnWrapper>
+      <Styled.Comments>
+        {canEngagePublicly && (
+          <>
+            <CommentContainer.Thread subject={resource} />
+            <CommentContainer.Editor
+              focus={false}
+              label={t("actions.add_comment_title_case")}
+              subject={resource}
+            />
+          </>
+        )}
+      </Styled.Comments>
+    </Styled.Container>
+  );
 }
 
-export default withTranslation()(ResourceDetail);
+ResourceDetail.displayName = "Resource.Detail";
+
+ResourceDetail.propTypes = {
+  projectUrl: PropTypes.string,
+  resourceUrl: PropTypes.string.isRequired,
+  resource: PropTypes.object
+};
---
+++ client/src/global/containers/comment/Editor/styles.js
@@ -0,0 +1,109 @@
+import styled from "@emotion/styled";
+import { respond, fluidScale } from "theme/styles/mixins";
+import IconComposer from "global/components/utility/IconComposer";
+
+const ACTION_MARGIN_TOP = 18;
+const BUTTON_MARGIN_LEFT = 15;
+
+export const Editor = styled.div`
+  margin-block-end: 5px;
+`;
+
+export const Label = styled.button`
+  display: flex;
+  align-items: center;
+  padding-inline: 24px;
+  padding-block: ${fluidScale("8px", "5px")};
+  border-radius: 20px;
+  border: none;
+  margin-block-start: 50px;
+  font-size: ${fluidScale("17px", "14px")};
+  font-family: var(--font-family-heading);
+  font-weight: var(--font-weight-regular);
+  color: var(--color-neutral-text-extra-dark);
+  background-color: var(--box-medium-bg-color);
+`;
+
+export const Icon = styled(IconComposer)`
+  margin-right: 8px;
+  margin-left: -3px;
+  width: 24px;
+  height: 24px;
+`;
+
+export const TextArea = styled.textarea`
+  margin-block-start: 18px;
+  width: 100%;
+  padding: 1em 1.5em;
+  font-size: 16px;
+  font-family: var(--font-family-sans);
+  color: var(--strong-color);
+  resize: vertical;
+  background-color: var(--box-medium-bg-color);
+  border: 0;
+  border-radius: var(--box-border-radius);
+  outline: 0;
+
+  &::placeholder {
+    color: var(--color);
+  }
+
+  &:focus-visible::placeholder {
+    color: var(--strong-color);
+  }
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: ${10 - ACTION_MARGIN_TOP}px;
+
+  ${respond(`margin-top: ${20 - ACTION_MARGIN_TOP}px;`, 90)}
+
+  .form-input {
+    width: 100%;
+
+    ${respond(`width: auto;`, 95)}
+  }
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  width: 100%;
+  margin-top: ${ACTION_MARGIN_TOP}px;
+  margin-left: -${BUTTON_MARGIN_LEFT}px;
+
+  ${respond(`width: auto;`, 120)}
+
+  &:only-child {
+    justify-content: flex-end;
+    width: 100%;
+    margin-left: 0;
+  }
+
+  .button-primary,
+  .button-secondary {
+    min-width: 100px;
+    padding: 9px 0 11px;
+    margin-left: ${BUTTON_MARGIN_LEFT}px;
+    font-size: 14px;
+  }
+
+  .button-secondary {
+    color: var(--color-neutral-text-extra-dark);
+    border-radius: var(--box-border-radius);
+
+    &:disabled {
+      color: inherit;
+      background-color: var(--color-accent-primary-dull);
+      cursor: default;
+    }
+
+    &:active,
+    &:hover {
+      color: inherit;
+    }
+  }
+`;
