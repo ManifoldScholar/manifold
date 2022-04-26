5532b604dc7eb2a6acaab9c0bcec4c5636b7177e
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-04-26 11:26:59 -0700

    [C] Update snapshots

--- client/src/frontend/containers/ProjectCollectionDetail/__tests__/ProjectCollectionDetail-test.js
+++ client/src/frontend/containers/ProjectCollectionDetail/__tests__/ProjectCollectionDetail-test.js
@@ -1,23 +1,11 @@
-import { ProjectCollectionDetailContainer } from "../";
+import ProjectCollectionDetailContainer from "../";
 
 describe("frontend/containers/ProjectCollectionDetail/ProjectCollectionDetail", () => {
-  def("projectCollection", () => factory("projectCollection"));
-  def("projects", () => collectionFactory("project"));
-  def("settings", () => factory("settings"));
   def("root", () => (
     <ProjectCollectionDetailContainer
-      projectCollection={$projectCollection}
-      projects={$projects}
-      projectsMeta={{ pagination: fixtures.pagination() }}
-      dispatch={$dispatch}
-      settings={$settings}
-      authentication={{}}
-      match={{ params: {} }}
-      location={{}}
-      t={key =>  key}
     />
   ));
   it("matches the snapshot", () => {
-    expect(shallow($root)).toMatchSnapshot();
+    expect(mount($withApp($root))).toMatchSnapshot();
   });
 });
