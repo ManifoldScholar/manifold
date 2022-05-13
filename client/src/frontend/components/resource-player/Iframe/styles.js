2aea4b8d0cb1f653c738cd02b9379b3c0bf85391
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-05-13 14:01:19 -0700

    [B] Fix rendering of interactive resources

--- client/src/frontend/components/resource-player/Iframe/styles.js
+++ client/src/frontend/components/resource-player/Iframe/styles.js
@@ -1,13 +1,14 @@
 import styled from "@emotion/styled";
 
 export const InteractiveWrapper = styled.div`
-  flex: 1;
+  position: absolute;
   height: 100%;
+  width: 100%;
   background: var(--color-base-neutral-white);
 `;
 
 export const Interactive = styled.iframe`
   width: 100%;
-  min-height: 100%;
+  height: auto;
   border: 0;
 `;
