3b789250cdb60472668a44e35f089f3abfa9452d
Dana Johnson <dana@castironcoding.com>
2022-06-02 10:13:05 -0700

    [B] Remove skip-to message

--- client/src/global/components/utility/SkipLink.js
+++ client/src/global/components/utility/SkipLink.js
@@ -1,56 +1,11 @@
-import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
-import { withTranslation } from "react-i18next";
+import React from "react";
+import { useTranslation } from "react-i18next";
 
-class SkipLink extends PureComponent {
-  static propTypes = {
-    skipId: PropTypes.string,
-    skipLinkText: PropTypes.string,
-    skipMessage: PropTypes.string,
-    t: PropTypes.func
-  };
-
-  static defaultProps = {
-    skipId: "skip-to-main"
-  };
-
-  get skipLinkText() {
-    return (
-      this.props.skipLinkText ?? this.props.t("navigation.skip_to_main.link")
-    );
-  }
-
-  get skipMessage() {
-    return (
-      this.props.skipMessage ?? this.props.t("navigation.skip_to_main.message")
-    );
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = { skipped: false };
-  }
-
-  activateSkipMessage = () => {
-    this.setState({ skipped: true });
-  };
-
-  render() {
-    return (
-      <>
-        <a
-          className="skip-to-main"
-          href={`#${this.props.skipId}`}
-          onClick={this.activateSkipMessage}
-        >
-          {this.skipLinkText}
-        </a>
-        <span className="screen-reader-text" role="alert">
-          {this.state.skipped && this.skipMessage}
-        </span>
-      </>
-    );
-  }
+export default function SkipLink() {
+  const { t } = useTranslation();
+  return (
+    <a className="skip-to-main" href="#skip-to-main">
+      {t("navigation.skip_to_main")}
+    </a>
+  );
 }
-
-export default withTranslation()(SkipLink);
