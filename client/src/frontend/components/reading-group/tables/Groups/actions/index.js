e8a938813814d19e4ae740f325cddcc3c840c9d9
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-08-29 17:53:30 -0700

    [B] Add leave action to my groups table

--- client/src/frontend/components/reading-group/tables/Groups/actions/index.js
+++ client/src/frontend/components/reading-group/tables/Groups/actions/index.js
@@ -1,3 +1,4 @@
 export { default as ArchiveGroup } from "./Archive";
 export { default as EditGroup } from "./Edit";
 export { default as JoinGroup } from "./Join";
+export { default as LeaveGroup } from "./Leave";
