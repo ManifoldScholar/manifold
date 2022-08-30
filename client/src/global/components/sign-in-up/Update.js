c91aa977655c71c0a2b223d9572c5cc249f12b85
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-08-30 11:48:29 -0700

    [C] Refactor edit profile form

--- client/src/global/components/sign-in-up/Update.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import UpdateForm from "./UpdateForm";
-
-export default class Update extends Component {
-  render() {
-    return <UpdateForm mode="existing" {...this.props} />;
-  }
-}
--- client/src/global/components/sign-in-up/index.js
+++ client/src/global/components/sign-in-up/index.js
@@ -1,23 +1,19 @@
 import Create from "./Create";
-import CreateUpdate from "./CreateUpdate";
 import Login from "./Login";
 import Logout from "./Logout";
 import Oauth from "./oauth";
 import Overlay from "./Overlay";
 import PasswordForgot from "./PasswordForgot";
-import Update from "./Update";
-import UpdateForm from "./UpdateForm";
+import UpdateForm from "./EditProfileForm";
 import Interface from "./Interface";
 
 export default {
   Create,
-  CreateUpdate,
   Login,
   Logout,
   Oauth,
   Overlay,
   PasswordForgot,
-  Update,
   UpdateForm,
   Interface
 };
