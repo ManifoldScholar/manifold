b36a50e444ea92d97873a5b91ac68630be8d1ea3
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-08-31 13:11:58 -0700

    [F] Add react-hook-form to edit profile

--- client/src/global/components/sign-in-up/EditProfileForm/ProfileFormFields/ProfileFormFields.js
+++ client/src/global/components/sign-in-up/EditProfileForm/ProfileFormFields/ProfileFormFields.js
@@ -1,80 +1,59 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { Input, AvatarDropzone } from "../../form";
+import { useFormContext } from "react-hook-form";
 
 export default function ProfileFormFields(props) {
-  const {
-    errors,
-    handleInputChange,
-    handleFileDrop,
-    getAvatarUrl,
-    handleRemoveAvatar,
-    state
-  } = props;
+  const { errors, ...avatarProps } = props;
 
   const { t } = useTranslation();
+
+  const { register } = useFormContext();
 
   return (
     <div className="row-1-p">
       <div className="row-1-p">
         <Input
-          name="nickname"
-          value={state?.nickname}
           errors={errors}
           ariaLabel="update nickname"
-          onChange={handleInputChange}
           placeholder="forms.signin_overlay.nickname"
+          {...register("nickname")}
         />
       </div>
-      <AvatarDropzone
-        handleFileDrop={handleFileDrop}
-        handleRemoveAvatar={handleRemoveAvatar}
-        avatarUrl={getAvatarUrl()}
-        errors={errors}
-      />
+      <AvatarDropzone errors={errors} {...avatarProps} />
       <div className="row-1-p">
         <p className="overlay-copy">{t("forms.signin_overlay.edit_account")}</p>
         <Input
-          name="firstName"
           label="forms.signin_overlay.first_name"
-          value={state?.firstName}
           errors={errors}
-          onChange={handleInputChange}
           placeholder="forms.signin_overlay.first_name"
+          {...register("firstName")}
         />
         <Input
-          name="lastName"
           label="forms.signin_overlay.last_name"
-          value={state?.lastName}
           errors={errors}
-          onChange={handleInputChange}
           placeholder="forms.signin_overlay.last_name"
+          {...register("lastName")}
         />
         <Input
-          name="email"
           label="forms.signin_overlay.email"
-          value={state?.email}
           errors={errors}
-          onChange={handleInputChange}
           placeholder="forms.signin_overlay.email"
+          {...register("email")}
         />
         <Input
-          name="password"
           type="password"
           label="forms.signin_overlay.new_password"
-          value={state?.password}
           errors={errors}
-          onChange={handleInputChange}
           placeholder="forms.signin_overlay.new_password"
+          {...register("password")}
         />
         <Input
-          name="passwordConfirmation"
           type="password"
           label="forms.signin_overlay.confirm_new_password"
-          value={state?.password}
           errors={errors}
-          onChange={handleInputChange}
           placeholder="forms.signin_overlay.confirm_new_password"
+          {...register("passwordConfirmation")}
         />
       </div>
     </div>
