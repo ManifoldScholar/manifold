acc29cd94b314ad93e00eadb378a4ada483a7157
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-09-01 10:11:06 -0700

    [F] Add BaseHookForm

--- client/src/global/components/sign-in-up/EditProfileForm/EditProfileForm.js
+++ client/src/global/components/sign-in-up/EditProfileForm/EditProfileForm.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useCallback, useState } from "react";
+import React, { useRef, useEffect, useCallback } from "react";
 import PropTypes from "prop-types";
 import { meAPI } from "api";
 import lh from "helpers/linkHandler";
@@ -6,10 +6,10 @@ import ProfileFormFields from "./ProfileFormFields";
 import Greeting from "./Greeting";
 import { Button } from "../form-inputs";
 import { useHistory } from "react-router-dom";
-import { useFromStore, useApiCallback, useNotification } from "hooks";
+import { useFromStore, useNotification } from "hooks";
 import { useTranslation } from "react-i18next";
 import { useUID } from "react-uid";
-import { useForm, FormProvider } from "react-hook-form";
+import BaseHookForm from "../BaseHookForm";
 
 export default function EditProfileForm({ hideSignInUpOverlay, mode }) {
   const authentication = useFromStore("authentication");
@@ -38,23 +38,9 @@ export default function EditProfileForm({ hideSignInUpOverlay, mode }) {
 
   const focusRef = useRef();
 
-  const form = useForm({
-    criteriaMode: "all",
-    defaultValues,
-    mode: "onSubmit"
-  });
-
-  const [formErrors, setFormErrors] = useState([]);
-
   useEffect(() => {
     if (focusRef.current) focusRef.current.focus();
   }, []);
-
-  useEffect(() => {
-    form.reset();
-  }, [authentication.authenticating, userId, form]);
-
-  const updateUser = useApiCallback(meAPI.update);
 
   const notifyUpdate = useNotification(() => ({
     level: 0,
@@ -63,47 +49,42 @@ export default function EditProfileForm({ hideSignInUpOverlay, mode }) {
     expiration: 3000
   }));
 
-  const formatAttributesAndUpdate = useCallback(
-    (data, avatarFileData) => {
-      const baseAttributes = Object.keys(data)
-        .filter(name => name !== "avatar")
-        .filter(name => !(name === "password" && data.password === ""))
-        .reduce((obj, name) => ({ ...obj, [name]: data[name] }), {});
+  const formatAttributes = useCallback((data, avatarFileData) => {
+    const baseAttributes = Object.keys(data)
+      .filter(name => name !== "avatar")
+      .filter(name => !(name === "password" && data.password === ""))
+      .reduce((obj, name) => ({ ...obj, [name]: data[name] }), {});
 
-      const avatar = avatarFileData
-        ? {
-            data: avatarFileData,
-            content_type: data.avatar.type,
-            filename: data.avatar.name
-          }
-        : null;
-      const removeAvatar = data.avatar === null;
+    const avatar = avatarFileData
+      ? {
+          data: avatarFileData,
+          content_type: data.avatar.type,
+          filename: data.avatar.name
+        }
+      : null;
+    const removeAvatar = data.avatar === null;
 
-      const attributes = { ...baseAttributes, avatar, removeAvatar };
-
-      updateUser(attributes)
-        .then(() => {
-          notifyUpdate();
-          if (hideSignInUpOverlay) hideSignInUpOverlay();
-        })
-        .catch(err => setFormErrors(err.body.errors));
-    },
-    [hideSignInUpOverlay, updateUser, notifyUpdate]
-  );
+    return { ...baseAttributes, avatar, removeAvatar };
+  }, []);
 
   const processAvatar = useCallback(
     data => {
       if (data.avatar.path) {
         const reader = new FileReader();
         reader.onload = () => {
-          formatAttributesAndUpdate(data, reader.result);
+          formatAttributes(data, reader.result);
         };
         return reader.readAsDataURL(data.avatar);
       }
-      return formatAttributesAndUpdate(data);
+      return formatAttributes(data);
     },
-    [formatAttributesAndUpdate]
+    [formatAttributes]
   );
+
+  const onSuccess = useCallback(() => {
+    notifyUpdate();
+    if (hideSignInUpOverlay) hideSignInUpOverlay();
+  }, [notifyUpdate, hideSignInUpOverlay]);
 
   const redirect = route => () => {
     if (hideSignInUpOverlay) hideSignInUpOverlay();
@@ -116,26 +97,32 @@ export default function EditProfileForm({ hideSignInUpOverlay, mode }) {
       tabIndex={-1}
       className="sign-in-up-update"
     >
-      <Greeting
-        mode={mode}
-        nickname={form.watch("nickname") ?? nickname ?? firstName}
-      />
-      <FormProvider {...form}>
-        <form onSubmit={form.handleSubmit(processAvatar)} aria-labelledby={uid}>
-          <h2 id={uid} className="screen-reader-text">
-            {t("forms.signin_overlay.update_sr_title")}
-          </h2>
-          <ProfileFormFields errors={formErrors} />
-          <div className="row-1-p">
-            <div className="form-input form-error">
-              <Button
-                type="submit"
-                label="forms.signin_overlay.submit_update_label"
-              />
+      <BaseHookForm
+        defaultValues={defaultValues}
+        formatData={processAvatar}
+        ariaLabelledBy={uid}
+        onSuccess={onSuccess}
+        apiMethod={meAPI.update}
+        reset={authentication.authenticating}
+      >
+        {errors => (
+          <>
+            <Greeting mode={mode} defaultNickname={nickname ?? firstName} />
+            <h2 id={uid} className="screen-reader-text">
+              {t("forms.signin_overlay.update_sr_title")}
+            </h2>
+            <ProfileFormFields errors={errors} />
+            <div className="row-1-p">
+              <div className="form-input form-error">
+                <Button
+                  type="submit"
+                  label="forms.signin_overlay.submit_update_label"
+                />
+              </div>
             </div>
-          </div>
-        </form>
-      </FormProvider>
+          </>
+        )}
+      </BaseHookForm>
       <div style={{ marginTop: "65px" }}>
         <Button
           styleType="outline"
