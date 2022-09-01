6e4a508628af184a6a1fd377b5c861699dbbaad6
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-09-01 09:27:09 -0700

    [C] Update fetch + error handling in edit profile

--- client/src/global/components/sign-in-up/EditProfileForm/ProfileFormFields/ProfileFormFields.js
+++ client/src/global/components/sign-in-up/EditProfileForm/ProfileFormFields/ProfileFormFields.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Input, AvatarDropzone } from "../../form";
+import { Input, AvatarDropzone } from "../../form-inputs";
 import { useFormContext } from "react-hook-form";
 
 export default function ProfileFormFields(props) {
