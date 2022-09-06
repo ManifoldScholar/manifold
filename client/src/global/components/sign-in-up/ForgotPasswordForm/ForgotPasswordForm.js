c630cd86629dc675d9e9695518ffe674e5ab297e
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-09-06 11:07:49 -0700

    [F] Refactor forgot password form

---
+++ client/src/global/components/sign-in-up/ForgotPasswordForm/ForgotPasswordForm.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useRef, useCallback } from "react";
+import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
+import { passwordsAPI } from "api";
+import { useUID } from "react-uid";
+import BaseHookForm from "../BaseHookForm";
+import { Input } from "../form-inputs";
+import { useNotification } from "hooks";
+
+export default function ForgotPasswordForm({
+  handleViewChange,
+  hideSignInUpOverlay
+}) {
+  const { t } = useTranslation();
+
+  const focusRef = useRef();
+
+  useEffect(() => {
+    if (focusRef.current) focusRef.current.focus();
+  }, []);
+
+  const notifySuccess = useNotification(email => ({
+    level: 0,
+    id: "PASSWORD_RESET_SENT",
+    heading: t("forms.signin_overlay.send_reset_success", {
+      email
+    }),
+    expiration: 3000
+  }));
+
+  const formatData = useCallback(data => {
+    return data.email;
+  }, []);
+
+  const onSuccess = useCallback(
+    (res, data) => {
+      notifySuccess(data.email);
+      if (hideSignInUpOverlay) hideSignInUpOverlay();
+    },
+    [hideSignInUpOverlay, notifySuccess]
+  );
+
+  const uid = useUID();
+
+  return (
+    <div ref={el => (focusRef.current = el)} tabIndex={-1}>
+      <BaseHookForm
+        apiMethod={passwordsAPI.create}
+        ariaLabelledBy={uid}
+        onSuccess={onSuccess}
+        formatData={formatData}
+      >
+        {(errors, { register }) => (
+          <>
+            <h2 id={uid} className="form-heading">
+              {t("forms.signin_overlay.reset_password")}
+            </h2>
+            <div className="row-1-p">
+              <Input
+                label="forms.signin_overlay.email"
+                errors={errors}
+                placeholder="forms.signin_overlay.email"
+                {...register("email")}
+              />
+            </div>
+            <div className="row-1-p">
+              <div className="form-input">
+                <input
+                  className="button-secondary button-secondary--with-room"
+                  type="submit"
+                  value={t("forms.signin_overlay.send_password_reset")}
+                />
+              </div>
+            </div>
+          </>
+        )}
+      </BaseHookForm>
+      <p className="login-links">
+        <button
+          onClick={e => handleViewChange("account-login", e)}
+          data-id="show-login"
+        >
+          {t("forms.signin_overlay.remember_password")}
+        </button>
+        <button
+          onClick={e => handleViewChange("account-create", e)}
+          data-id="show-create"
+        >
+          {t("forms.signin_overlay.need_account")}
+        </button>
+      </p>
+    </div>
+  );
+}
