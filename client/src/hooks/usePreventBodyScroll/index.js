54b69a837d33ceaeb9b10ce22b19e2cedac8279d
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-09-06 16:02:51 -0700

    [F] Add usePreventBodyScroll

---
+++ client/src/hooks/usePreventBodyScroll/index.js
@@ -0,0 +1,28 @@
+/* This hook is based on and significantly borrows from the fix Reakit is implementing for their v2 release. See: https://github.com/ariakit/ariakit/pull/1271/files  */
+
+import { useLayoutEffect, useEffect } from "react";
+
+export default function usePreventBodyScroll() {
+  const isBrowser =
+    typeof window !== "undefined" && !!window.document?.createElement;
+  const safeEffect = isBrowser ? useLayoutEffect : useEffect;
+
+  safeEffect(() => {
+    const { body } = document;
+
+    if (!body) return () => {};
+
+    const baseStyle = body.style.cssText;
+
+    const modalVisibleStyle = {
+      overflow: "hidden",
+      scrollbarGutter: "stable",
+      position: "absolute",
+      width: "100%"
+    };
+
+    Object.assign(body.style, modalVisibleStyle);
+
+    return () => (body.style.cssText = baseStyle);
+  }, []);
+}
