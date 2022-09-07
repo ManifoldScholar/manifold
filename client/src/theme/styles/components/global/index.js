c7dd46aad133ac58bb15c06d5b73e34d96e8108d
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-09-07 11:58:24 -0700

    [C] Migrate AvatarDropzone styles

--- client/src/theme/styles/components/global/index.js
+++ client/src/theme/styles/components/global/index.js
@@ -18,7 +18,6 @@ import paginationCount from "./paginationCount";
 import search from "./search";
 import sectionHeading from "./sectionHeading";
 import sectionHeadingSecondary from "./sectionHeadingSecondary";
-import signInUpUpdate from "./signInUpUpdate";
 import shareNavPrimary from "./shareNavPrimary";
 import skipToMain from "./skipToMain";
 import table from "./table";
@@ -46,7 +45,6 @@ export default `
   ${search}
   ${nestedNav}
   ${skipToMain}
-  ${signInUpUpdate}
   ${table}
   ${paginationCount}
   ${annotation}
