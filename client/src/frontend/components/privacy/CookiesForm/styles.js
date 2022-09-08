1a6107ab1e53a5d24dc98129958cdef823acedd3
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-09-08 12:42:46 -0700

    [F] Migrate privacy form styles

---
+++ client/src/frontend/components/privacy/CookiesForm/styles.js
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+import { Button as BaseButton } from "global/components/sign-in-up/form-inputs";
+import { utilityPrimary } from "theme/styles/mixins";
+
+export const Wrapper = styled.div`
+  margin-block-end: 75px;
+`;
+
+export const Header = styled.h2`
+  ${utilityPrimary}
+  padding: 12px 24px;
+  font-size: 14px;
+  color: var(--color-base-neutral75);
+  letter-spacing: 0.1em;
+  background-color: var(--color-base-neutral10);
+  border-radius: 8px;
+`;
+
+export const Group = styled.div`
+  padding-inline: 24px;
+  padding-block-start: 20px;
+
+  & + & {
+    margin-block-start: 60px;
+  }
+`;
+
+export const Button = styled(BaseButton)`
+  margin-block-start: 40px;
+`;
