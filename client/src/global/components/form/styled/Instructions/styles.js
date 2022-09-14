2881752f07f9514d6b3a5f7b565c76be3e153f68
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-09-14 08:42:28 -0700

    [F] Migrate textarea + instructions styles

---
+++ client/src/global/components/form/styled/Instructions/styles.js
@@ -0,0 +1,54 @@
+import styled from "@emotion/styled";
+import { formInstructions } from "theme/styles/mixins";
+
+export const Instructions = styled.span`
+  ${formInstructions}
+  display: block;
+  margin-bottom: 1em;
+
+  a:visited {
+    color: inherit;
+  }
+
+  &.space-bottom {
+    margin-bottom: 2em;
+  }
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  ${({ $secondary }) =>
+    $secondary &&
+    `
+    margin-top: 0.75em;
+
+    &--inline {
+      display: inline;
+    }
+
+    + .form-input-group--primary {
+      margin-top: 32px;
+    }
+
+    + .form-input-group--secondary {
+      margin-top: 24px;
+    }
+
+    a {
+      color: inherit;
+    }
+  `}
+`;
+
+// form-input--with-actions
+// grid-column: 1 / -1;
+// margin-bottom: 1em !important;
+//
+// ${respond(`margin-bottom: 0 !important;`, 60)}
+
+// .form-input .form-input-radios__legend +
+// margin-block-start: 7px;
+
+// .form-toggle .form-toggle-secondary
+// margin-bottom: 0;
---
+++ client/src/global/components/form/styled/TextArea/styles.js
@@ -0,0 +1,73 @@
+import styled from "@emotion/styled";
+import {
+  respond,
+  defaultTransitionProps,
+  formInputPrimary,
+  formLabelPrimary
+} from "theme/styles/mixins";
+
+export const PrimaryLabel = styled.label`
+  ${formLabelPrimary}
+  display: block;
+  margin-block-end: 1em;
+
+  ${({ $hasInstructions }) => $hasInstructions && `margin-block-end: 0.5em;`}
+`;
+
+export const SecondaryLabel = styled(PrimaryLabel)`
+  margin-block-start: 0;
+`;
+
+const TextAreaBase = styled.textarea`
+  width: 100%;
+  resize: vertical;
+  border: 1px solid var(--TextArea-border-color);
+  outline: none;
+  transition: border-color ${defaultTransitionProps};
+
+  &:focus-visible {
+    border-color: var(--TextArea-focus-color);
+  }
+`;
+
+export const TextAreaPrimary = styled(TextAreaBase)`
+  --TextArea-border-color: transparent;
+  --TextArea-focus-color: var(--hover-color);
+
+  ${formInputPrimary}
+  padding: 1.2em 1.5em;
+  background-color: var(--color-base-neutral05);
+  outline: none;
+  transition: border-color ${defaultTransitionProps};
+
+  .bg-neutral05 & {
+    background-color: var(--color-base-neutral-white);
+  }
+
+  ${({ $wide }) => $wide && `padding: 0.7em 1em;`}
+
+  ${({ $large }) => $large && `min-height: 250px;`}
+`;
+
+export const TextAreaSecondary = styled(TextAreaBase)`
+  --TextArea-border-color: var(--textarea-border-color);
+  --TextArea-focus-color: var(--highlight-color);
+
+  padding: 1.2em;
+  font-family: var(--input-font-family);
+  font-size: 16px;
+  background-color: var(--input-bg-color);
+  border-radius: var(--box-border-radius);
+
+  ${respond(
+    `
+    padding: 1.25em 1.389em;
+    font-size: 18px;
+  `,
+    70
+  )}
+
+  &::placeholder {
+    color: var(--color-neutral-ui-light);
+  }
+`;
