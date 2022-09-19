293a7280e7b1399ddf0e2b8ec9290a5b431219c7
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-09-19 10:06:13 -0700

    [F] Update CookiesBanner display for anon users

--- client/src/global/components/CookiesBanner/AnonymousUser.js
+++ client/src/global/components/CookiesBanner/AnonymousUser.js
@@ -1,78 +1,59 @@
-import React, { useCallback } from "react";
-import { useTranslation, Trans } from "react-i18next";
+import React, { useCallback, useState } from "react";
+import { Trans } from "react-i18next";
 import { Link } from "react-router-dom";
-import { AcceptTermsCheckbox } from "global/components/sign-in-up/form-inputs";
+import NarrowBanner from "./NarrowBanner";
+import FormEmbedBanner from "./FormEmbedBanner";
 import { useFromStore } from "hooks";
-import * as Styled from "./styles";
 
 export default function AnonymousUserBanner() {
-  const { t } = useTranslation();
+  const [showForm, setShowForm] = useState(false);
 
   const settings = useFromStore("settings", "select");
   const { manifoldAnalyticsEnabled, googleAnalyticsEnabled } =
     settings?.attributes?.calculated ?? {};
 
+  /* eslint-disable no-nested-ternary */
   const savePrefs = useCallback(
-    declineAll => {
-      const args = {
-        consentManifoldAnalytics: !manifoldAnalyticsEnabled ? null : false,
-        consentGoogleAnalytics: !googleAnalyticsEnabled ? null : false
+    ({ all = null, manifold, google }) => {
+      const prefs = {
+        consentManifoldAnalytics: !manifoldAnalyticsEnabled
+          ? null
+          : typeof manifold === "boolean"
+          ? manifold
+          : all,
+        consentGoogleAnalytics: !googleAnalyticsEnabled
+          ? null
+          : typeof google === "boolean"
+          ? google
+          : all
       };
-      // save cookies prefs somewhere
+      console.info(prefs);
+      // save cookies prefs in redux store or local storage
     },
     [googleAnalyticsEnabled, manifoldAnalyticsEnabled]
   );
 
-  // check where we save the anonymous prefs to see if we need to render the banner
+  // check saved prefs to see if we need to render the banner
 
-  return (
-    <Styled.Banner>
-      <Styled.Inner $wide>
-        <Styled.TextWrapper>
-          <Styled.Heading>{t("messages.cookies_banner.header")}</Styled.Heading>
-          <p>
-            <Trans
-              i18nKey="messages.cookies_banner.body_annonymous_user"
-              components={[<Link to={`/login`} />]}
-            />
-          </p>
-          <Styled.Checkboxes>
-            <Styled.CheckboxWrapper>
-              <AcceptTermsCheckbox
-                label={t("forms.privacy.internal_analytics.label")}
-                labelStyle="heading"
-              />
-              <Styled.Description>
-                {t("forms.privacy.internal_analytics.description")}
-              </Styled.Description>
-            </Styled.CheckboxWrapper>
-            <Styled.CheckboxWrapper>
-              <AcceptTermsCheckbox
-                label={t("forms.privacy.google_analytics.label")}
-                labelStyle="heading"
-              />
-              <Styled.Description>
-                {t("forms.privacy.google_analytics.description")}
-              </Styled.Description>
-            </Styled.CheckboxWrapper>
-          </Styled.Checkboxes>
-        </Styled.TextWrapper>
-        <Styled.ButtonWrapper>
-          <button
-            onClick={savePrefs}
-            className="button-secondary button-secondary--text-white"
-          >
-            {t("messages.cookies_banner.accept_label_truncated")}
-          </button>
-          <Styled.WhiteButton
-            onClick={() => savePrefs(true)}
-            className="button-secondary button-secondary--outlined"
-          >
-            {t("messages.cookies_banner.decline_button_label")}
-          </Styled.WhiteButton>
-        </Styled.ButtonWrapper>
-      </Styled.Inner>
-    </Styled.Banner>
+  const message = (
+    <Trans
+      i18nKey="messages.cookies_banner.body_annonymous_user"
+      components={[<Link to={`/login`} />]}
+    />
+  );
+
+  return showForm ? (
+    <FormEmbedBanner
+      message={message}
+      declineAll={() => savePrefs({ all: false })}
+      save={savePrefs}
+    />
+  ) : (
+    <NarrowBanner
+      message={message}
+      acceptAll={() => savePrefs({ all: true })}
+      onClickSettings={() => setShowForm(true)}
+    />
   );
 }
 
