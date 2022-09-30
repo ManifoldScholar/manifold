fd234565af478e26d3c4885d93b041e70b45c59f
Dana Johnson <dana@castironcoding.com>
2022-09-29 21:10:01 -0700

    [B] Adjust SwitchArray gap

--- client/src/global/components/form/FieldWrapper/styles.js
+++ client/src/global/components/form/FieldWrapper/styles.js
@@ -1,12 +1,11 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
-  --gap: 0.5em; // make available to internal layout elements
   --FieldGroup-child-flex-basis: calc(50% - var(--FieldGroup-column-gap));
 
   display: grid;
   grid-template-columns: 100%;
-  gap: var(--gap);
+  gap: var(--FieldWrapper-gap, 0.5em);
   min-inline-size: 200px;
 
   &.wide {
