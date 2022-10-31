9a113fc6086af8a63089a95cb9b50658c1d581b5
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-10-31 09:36:29 -0400

    [C] Clean up overlay

--- client/src/global/components/sign-in-up/Overlay/index.js
+++ client/src/global/components/sign-in-up/Overlay/index.js
@@ -1,21 +1,50 @@
 import React from "react";
 import PropTypes from "prop-types";
 import FocusTrap from "focus-trap-react";
+import Utility from "global/components/utility";
+import CloseButton from "global/components/Overlay/Close";
+import Interface from "../Interface";
+import { useTranslation } from "react-i18next";
+import { useUID } from "react-uid";
+import { useFromStore } from "hooks";
+import * as Styled from "./styles";
 
 import { usePreventBodyScroll } from "hooks";
-import OverlayContent from "./OverlayContent";
 
 export default function Overlay({ hideOverlay }) {
   usePreventBodyScroll();
+  const { t } = useTranslation();
+  const authentication = useFromStore("authentication");
+  const uid = useUID();
 
   return (
-    <FocusTrap
-      focusTrapOptions={{
-        onDeactivate: hideOverlay
-      }}
-    >
-      <OverlayContent hideOverlay={hideOverlay} />
-    </FocusTrap>
+    <Styled.Dialog role="dialog" aria-labelledby={uid}>
+      <FocusTrap
+        focusTrapOptions={{
+          onDeactivate: hideOverlay
+        }}
+      >
+        <Styled.Header>
+          <Styled.HeaderInner>
+            <Utility.IconComposer size={26} icon="manifoldLogo32" />
+            <Styled.SRTitle id={uid}>
+              {t("forms.signin_overlay.sr_title")}
+            </Styled.SRTitle>
+            <CloseButton onClick={hideOverlay} />
+          </Styled.HeaderInner>
+        </Styled.Header>
+        <Styled.Content>
+          <Styled.LayoutContainer>
+            <Styled.FormContainer>
+              <Interface
+                hideOverlay={hideOverlay}
+                defaultView={authentication?.authenticated ? "update" : "login"}
+              />
+            </Styled.FormContainer>
+          </Styled.LayoutContainer>
+        </Styled.Content>
+      </FocusTrap>
+    </Styled.Dialog>
   );
 }
 
