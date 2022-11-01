c50b34dc965776717faa896d41b1998ddfc9b4bc
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-11-01 12:57:49 -0400

    [F] Render data use content from theme settings

--- client/src/frontend/containers/DataUse/index.js
+++ client/src/frontend/containers/DataUse/index.js
@@ -1,55 +1,18 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
 import { useFromStore } from "hooks";
 import * as Styled from "./styles";
 
 export default function DataUseContainer() {
-  const { t } = useTranslation();
-
   const settings = useFromStore("settings", "select");
-  const usesAnalytics = !settings?.attributes?.general
-    ?.disableInternalAnalytics;
+  const header = settings?.attributes?.theme?.stringDataUseHeader;
+  const body = settings?.attributes?.stringDataUseCopyFormatted;
 
   return (
     <div className="container">
       <Styled.Columns>
         <div>
-          <h1 className="form-heading">
-            {t("pages.frontend.data_use.header")}
-          </h1>
-          <Styled.Text>
-            Nulla quis lorem ut libero malesuada feugiat. Donec rutrum congue
-            leo eget malesuada.
-          </Styled.Text>
-          {usesAnalytics && (
-            <>
-              <Styled.CategoryHeader>
-                {t("pages.frontend.data_use.internal_analytics")}
-              </Styled.CategoryHeader>
-              <Styled.Text>
-                Nulla quis lorem ut libero malesuada feugiat. Donec rutrum
-                congue leo eget malesuada. Nulla porttitor accumsan tincidunt.
-                Vivamus magna justo, lacinia eget consectetur sed, convallis at
-                tellus.
-              </Styled.Text>
-            </>
-          )}
-          <Styled.CategoryHeader>
-            {t("pages.frontend.data_use.annotations")}
-          </Styled.CategoryHeader>
-          <Styled.Text>
-            Nulla quis lorem ut libero malesuada feugiat. Donec rutrum congue
-            leo eget malesuada. Nulla porttitor accumsan tincidunt. Vivamus
-            magna justo, lacinia eget consectetur sed, convallis at tellus.
-          </Styled.Text>
-          <Styled.CategoryHeader>
-            {t("pages.frontend.data_use.reading_groups")}
-          </Styled.CategoryHeader>
-          <Styled.Text>
-            Nulla quis lorem ut libero malesuada feugiat. Donec rutrum congue
-            leo eget malesuada. Nulla porttitor accumsan tincidunt. Vivamus
-            magna justo, lacinia eget consectetur sed, convallis at tellus.
-          </Styled.Text>
+          <Styled.Header>{header}</Styled.Header>
+          <Styled.Body dangerouslySetInnerHTML={{ __html: body }} />
         </div>
         <Styled.ImagePlaceholder />
       </Styled.Columns>
