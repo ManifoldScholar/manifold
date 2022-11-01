3ddcd2ab16aab9490952bf86ba44cfc700acccd1
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-11-01 10:47:40 -0400

    [F] Add initial section order dnd

--- client/src/backend/components/authoring/CreateTextForm/index.js
+++ client/src/backend/components/authoring/CreateTextForm/index.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import classNames from "classnames";
 import FormContainer from "global/containers/form";
 import Form from "global/components/form";
 import { useHistory } from "react-router";
+import SectionsList from "./SectionsList";
 
 export default function CreateTextForm({
   name,
@@ -19,6 +20,19 @@ export default function CreateTextForm({
   const handleCancelClick = e => {
     e.preventDefault();
     cancelUrl ? history.push(cancelUrl) : history.goBack();
+  };
+
+  const [sectionName, setSectionName] = useState();
+  const [sections, setSections] = useState([]);
+  const setSectionOrder = result => {
+    const { draggableId, destination } = result ?? {};
+    const newOrder = sections.filter(s => s !== draggableId);
+    newOrder.splice(destination.index, 0, draggableId);
+    setSections(newOrder);
+  };
+  const handleDeleteSection = section => {
+    const update = sections.filter(s => s !== section);
+    setSections(update);
   };
 
   return (
@@ -45,10 +59,29 @@ export default function CreateTextForm({
         label={t("backend.forms.text_create.sections_section_label")}
         instructions={t("backend.forms.text_create.sections_instructions")}
       >
-        <Form.TextInput
-          placeholder={t("backend.forms.text_create.section_name_placeholder")}
-          buttons={[{ label: t("actions.create") }]}
-        />
+        <div>
+          <Form.TextInput
+            placeholder={t(
+              "backend.forms.text_create.section_name_placeholder"
+            )}
+            onChange={e => setSectionName(e.target.value)}
+            value={sectionName}
+            buttons={[
+              {
+                label: t("actions.create"),
+                onClick: (e, el) => {
+                  setSections([...sections, el.value]);
+                  setSectionName(null);
+                }
+              }
+            ]}
+          />
+          <SectionsList
+            sections={sections}
+            setSectionOrder={setSectionOrder}
+            onDelete={handleDeleteSection}
+          />
+        </div>
         <div className="buttons-icon-horizontal authoring-drawer">
           <button type="submit" className={buttonClasses} disabled={false}>
             <span>{t("backend.forms.text_create.save_button_label")}</span>
