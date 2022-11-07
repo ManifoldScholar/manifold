f999e0b68ac8968477e8f40376162f852efe6647
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-11-07 11:43:20 -0800

    [F] Add link to section edit drawer

--- client/src/backend/components/authoring/SectionsList/SectionListItem.js
+++ client/src/backend/components/authoring/SectionsList/SectionListItem.js
@@ -2,19 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import Utility from "global/components/utility";
 import { useTranslation } from "react-i18next";
+import lh from "helpers/linkHandler";
+import { Link } from "react-router-dom";
 import * as Styled from "./styles";
 
 export default function SectionListItem(props) {
   const {
     entity: section,
+    textId,
     draggableProps,
     dragHandleProps,
     isDragging,
     innerRef,
-    onEdit,
     onDelete
   } = props;
   const { t } = useTranslation();
+
+  const editUrl = lh.link("backendTextSectionsEdit", textId, section.id);
 
   return section ? (
     <Styled.Item ref={innerRef} {...draggableProps}>
@@ -23,7 +27,7 @@ export default function SectionListItem(props) {
           <Styled.Button onClick={onDelete} aria-label={t("actions.delete")}>
             <Utility.IconComposer size={24} icon="delete24" />
           </Styled.Button>
-          <Styled.Button onClick={onEdit} aria-label={t("actions.edit")}>
+          <Styled.Button as={Link} to={editUrl} aria-label={t("actions.edit")}>
             <Utility.IconComposer size={24} icon="annotate24" />
           </Styled.Button>
           <Styled.DragHandle {...dragHandleProps}>
