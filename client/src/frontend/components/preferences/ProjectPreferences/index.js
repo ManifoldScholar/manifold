0e1059866a838ac64b8f027ed0e35268cd1afaa6
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-11-14 15:17:44 -0800

    [C] Migrate subscriptions form to global form

--- client/src/frontend/components/preferences/ProjectPreferences/index.js
+++ client/src/frontend/components/preferences/ProjectPreferences/index.js
@@ -1,18 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
 import useCollapseContext from "global/components/Collapse/useCollapseContext";
-import RadioGroup from "global/components/form/hook-form/RadioGroup";
-import { useFormContext } from "react-hook-form";
-import PropTypes from "prop-types";
+import RadioGroup from "../RadioGroup";
 import * as Styled from "./Styles";
 
-function ProjectPreferences({ showAllProjects }) {
+function ProjectPreferences({
+  showAllProjects,
+  preferences,
+  onChange,
+  onDigestChange
+}) {
   const { toggleProps } = useCollapseContext();
   const { t } = useTranslation();
 
-  const { watch } = useFormContext();
-
-  const digestOpen = watch("digest") !== "never";
+  const digestOpen = preferences?.digest !== "never";
 
   const options = {
     ...(showAllProjects && {
@@ -24,7 +26,7 @@ function ProjectPreferences({ showAllProjects }) {
   return (
     <>
       <RadioGroup
-        setting={{
+        preference={{
           key: "digest",
           label: t("forms.notifications.project_preferences.frequency_label")
         }}
@@ -39,25 +41,35 @@ function ProjectPreferences({ showAllProjects }) {
           type: "radio",
           "aria-expanded": digestOpen
         }}
+        value={preferences.digest}
+        onChange={onChange}
       />
       <Styled.CollapseGroup $expanded={digestOpen}>
-        <RadioGroup
-          setting={{
-            key: "digest-projects",
-            label: t(
-              "forms.notifications.project_preferences.digest_projects_label"
-            )
-          }}
-          options={options}
-        />
-        <RadioGroup
-          setting={{
-            key: "digestCommentsAndAnnotations",
-            label: t(
-              "forms.notifications.project_preferences.digest_comments_label"
-            )
-          }}
-        />
+        <>
+          <RadioGroup
+            preference={{
+              key: "digest-projects",
+              label: t(
+                "forms.notifications.project_preferences.digest_projects_label"
+              )
+            }}
+            options={options}
+            value={Object.keys(preferences).find(
+              prefKey => prefKey in options && preferences[prefKey] === "always"
+            )}
+            onChange={onDigestChange}
+          />
+          <RadioGroup
+            preference={{
+              key: "digestCommentsAndAnnotations",
+              label: t(
+                "forms.notifications.project_preferences.digest_comments_label"
+              )
+            }}
+            value={preferences.digestCommentsAndAnnotations}
+            onChange={onChange}
+          />
+        </>
       </Styled.CollapseGroup>
     </>
   );
--- client/src/frontend/containers/Subscriptions/styles.js
+++ client/src/frontend/containers/Subscriptions/styles.js
@@ -1,34 +1,12 @@
 import styled from "@emotion/styled";
-import {
-  containerPrototype,
-  respond,
-  headingPrimary,
-  formInstructions
-} from "theme/styles/mixins";
-import { Button as BaseButton } from "global/components/sign-in-up/form-inputs";
+import { respond } from "theme/styles/mixins";
+import GlobalForm from "global/containers/form";
 
-export const Container = styled.div`
-  ${containerPrototype}
-  padding-block-start: var(--container-padding-block-start);
-  padding-block-end: var(--container-padding-block-end);
-  display: flex;
-  justify-content: center;
-`;
-
-export const FormWrapper = styled.div`
+export const Form = styled(GlobalForm.Form)`
+  margin: auto;
   ${respond(`width: 600px;`, 75)}
-`;
 
-export const Heading = styled.h1`
-  ${headingPrimary}
-  margin-block-end: 55px;
-`;
-
-export const Instructions = styled.span`
-  ${formInstructions}
-  display: block;
-`;
-
-export const Button = styled(BaseButton)`
-  margin-block-start: 40px;
+  input[type="submit"] {
+    width: 100%;
+  }
 `;
