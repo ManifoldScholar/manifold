b03a959083cdf67b1a655ae529d4c45155a4b7fa
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-11-14 16:29:52 -0800

    [C] Migrate cookies form to global form

--- client/src/frontend/components/privacy/CookiesForm/styles.js
+++ client/src/frontend/components/privacy/CookiesForm/styles.js
@@ -1,32 +1,16 @@
 import styled from "@emotion/styled";
-import { Button as BaseButton } from "global/components/sign-in-up/form-inputs";
-import { utilityPrimary, formInstructions } from "theme/styles/mixins";
+import { formInstructions } from "theme/styles/mixins";
+import Form from "global/components/form";
 
-export const FormSection = styled.div`
-  margin-block-end: 75px;
+export const FieldGroup = styled(Form.FieldGroup)`
+  --FieldGroup-row-gap: 50px;
+  --BaseGroup-flex-flow: column nowrap;
 `;
 
-export const Header = styled.h2`
-  ${utilityPrimary}
-  padding: 12px 24px;
-  font-size: 14px;
-  color: var(--color-base-neutral75);
-  letter-spacing: 0.1em;
-  background-color: var(--color-base-neutral10);
-  border-radius: 8px;
-`;
-
-export const FormFields = styled.div`
-  padding-inline: 24px;
-  padding-block-start: 20px;
-
-  > * + * {
-    margin-block-start: 50px;
-  }
-`;
-
-export const Button = styled(BaseButton)`
-  margin-block-start: 40px;
+export const Save = styled.input`
+  width: 266px;
+  margin-block-end: 20px;
+  margin-block-start: 10px;
 `;
 
 export const NoAnalyticsMessage = styled.span`
