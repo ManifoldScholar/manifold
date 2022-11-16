802592a44df1f04849980bd2ad0d3507e07d2cd4
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-11-16 08:28:26 -0800

    [C] Extract cookies fields into single component

---
+++ client/src/frontend/components/privacy/CookiesForm/CookiesFormFields/styles.js
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+import { formInstructions } from "theme/styles/mixins";
+
+export const NoAnalyticsMessage = styled.span`
+  ${formInstructions}
+  display: block;
+`;
