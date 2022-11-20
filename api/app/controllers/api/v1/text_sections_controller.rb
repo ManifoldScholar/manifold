65a7cd9fafe4a35851ad06c384f81ea7557f1df7
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-11-19 20:15:36 -0800

    [F] Make text sections editable

--- api/app/controllers/api/v1/text_sections_controller.rb
+++ api/app/controllers/api/v1/text_sections_controller.rb
@@ -13,6 +13,17 @@ module API
                                include: includes
       end
 
+      def update
+        @text_section = load_and_authorize_text_section
+        ::Updaters::TextSection.new(text_section_params).update(@text_section)
+        render_single_resource @text_section
+      end
+
+      def destroy
+        @text_section = load_text_section
+        @text_section.destroy
+      end
+
     end
   end
 end
