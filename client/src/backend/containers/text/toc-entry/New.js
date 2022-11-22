ffb455d64929b364bab085456bd673ba74795380
Zach Davis <zach@castironcoding.com>
2022-11-22 14:23:32 -0800

    [F] Wire up add toc entry

--- client/src/backend/containers/text/toc-entry/New.js
+++ client/src/backend/containers/text/toc-entry/New.js
@@ -4,13 +4,13 @@ import { useTranslation } from "react-i18next";
 import Navigation from "backend/components/navigation";
 import AddTOCEntryForm from "backend/components/authoring/AddEditTOCEntryForm";
 
-export default function AddTOCEntryContainer({ entry, textId, sections }) {
+export default function AddTOCEntryContainer(props) {
   const { t } = useTranslation();
 
   return (
     <section>
       <Navigation.DrawerHeader title={t("backend.forms.text_toc.add_header")} />
-      <AddTOCEntryForm text={textId} entry={entry} sections={sections} />
+      <AddTOCEntryForm {...props} />
     </section>
   );
 }
@@ -18,7 +18,8 @@ export default function AddTOCEntryContainer({ entry, textId, sections }) {
 AddTOCEntryContainer.displayName = "Text.TOC.New";
 
 AddTOCEntryContainer.propTypes = {
-  entry: PropTypes.object.isRequired,
   textId: PropTypes.string.isRequired,
-  sections: PropTypes.array
+  sections: PropTypes.array.isRequired,
+  toc: PropTypes.array,
+  setTree: PropTypes.func.isRequired
 };
