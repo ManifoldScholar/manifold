57a29ef5c3e442372b3781d474a0fdaf6ff34336
Alexa Grey <alexa@castironcoding.com>
2022-12-06 12:47:29 -0800

    [C] Update rspec & rspec-rails for Rails 6
    * Ensure eager_loading in dev happens after_initialize

--- api/config/initializers/zzz_eager_load_in_dev.rb
+++ api/config/initializers/zzz_eager_load_in_dev.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 if Rails.env.development?
-  begin
-    Rails.application.eager_load!
-  rescue ActiveRecord::ActiveRecordError
-    # purposely left blank
+  Rails.application.configure do
+    config.after_initialize do
+      Rails.application.eager_load!
+    rescue ActiveRecord::ActiveRecordError
+      # purposely left blank
+    end
   end
 end
