973776af65998033e94f2e32699f855ac1dc9adc
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2022-12-19 11:12:24 -0800

    [F] Wire up add, edit, delete pending entitlement

--- client/src/backend/components/pending-entitlements/AddEditForm.js
+++ client/src/backend/components/pending-entitlements/AddEditForm.js
@@ -16,41 +16,72 @@ export default function AddEditEntitlementForm({ refresh, entitlement }) {
     history.push(lh.link("backendRecordsEntitlements"));
   }, [history, refresh]);
 
+  const formatData = data => {
+    const { expiresOn, entityId, ...rest } = data.attributes;
+    return {
+      attributes: {
+        subjectUrl: `gid://entitlements/Project/${entityId}`,
+        expiration: expiresOn,
+        ...rest
+      }
+    };
+  };
+
+  const model = entitlement
+    ? {
+        id: entitlement.id,
+        attributes: {
+          ...entitlement.attributes,
+          entityId: entitlement.attributes?.subjectUrl.split("/").pop()
+        }
+      }
+    : undefined;
+
   return (
     <FormContainer.Form
-      model={entitlement}
-      name={"backend-entitlement-import-create"}
+      model={model}
+      name={
+        model
+          ? "backend-pending-entitlement-update"
+          : "backend-pending-entitlement-create"
+      }
       className="form-secondary"
       onSuccess={onSuccess}
+      formatData={formatData}
       create={pendingEntitlementsAPI.create}
       update={pendingEntitlementsAPI.update}
     >
       <Form.TextInput
         focusOnMount
-        label={t("backend.entitlement_imports.form.name_label")}
-        instructions={t("backend.entitlement_imports.form.name_instructions")}
+        label={t("backend.pending_entitlements.form.email")}
+        instructions={t("backend.pending_entitlements.form.email_instructions")}
         name="attributes[email]"
       />
       <Form.TextInput
-        label={t("backend.entitlement_imports.form.name_label")}
-        instructions={t("backend.entitlement_imports.form.name_instructions")}
+        label={t("backend.pending_entitlements.form.first_name")}
         name="attributes[firstName]"
       />
       <Form.TextInput
-        label={t("backend.entitlement_imports.form.name_label")}
-        instructions={t("backend.entitlement_imports.form.name_instructions")}
+        label={t("backend.pending_entitlements.form.last_name")}
         name="attributes[lastName]"
       />
       <Form.TextInput
-        label={t("backend.entitlement_imports.form.name_label")}
-        instructions={t("backend.entitlement_imports.form.name_instructions")}
-        name="attributes[name]"
+        label={t("backend.pending_entitlements.form.entity_id")}
+        instructions={t("backend.pending_entitlements.form.id_instructions")}
+        placeholder={t("backend.pending_entitlements.form.id_placeholder")}
+        name="attributes[entityId]"
       />
-      <Form.DatePicker name="attributes[expiration]" />
+      <Form.DatePicker
+        label={t("backend.pending_entitlements.form.expiration")}
+        instructions={t(
+          "backend.pending_entitlements.form.expiration_instructions"
+        )}
+        name="attributes[expiresOn]"
+      />
       <Form.DrawerButtons
         showCancel
         cancelUrl={lh.link("backendRecordsEntitlements")}
-        submitLabel="backend.forms.text_create.save_button_label"
+        submitLabel="backend.pending_entitlements.form.save_label"
       />
     </FormContainer.Form>
   );
