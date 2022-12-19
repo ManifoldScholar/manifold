0582df628dcef21c28a5184a8a4264353db27314
Alexa Grey <alexa@castironcoding.com>
2022-12-19 13:42:04 -0800

    [F] Expose entitlementSubjectUrl on projects and journals

--- api/app/models/concerns/entitleable.rb
+++ api/app/models/concerns/entitleable.rb
@@ -9,6 +9,10 @@ module Entitleable
     has_many :pending_entitlements, as: :subject, dependent: :destroy
   end
 
+  def entitlement_subject_url
+    to_entitlement_gid.to_s
+  end
+
   # @return [GlobalID]
   def to_entitlement_gid(**params)
     params[:app] = :entitlements
