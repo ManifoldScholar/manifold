a256e70d41b7f48ef3456fbc86fb1529f21e94c0
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-01-25 11:42:01 -0800

    [C] Add mathml upgrade class

---
+++ api/app/services/system_upgrades/upgrades/manifold080000.rb
@@ -0,0 +1,23 @@
+module SystemUpgrades
+  module Upgrades
+    class Manifold080000 < SystemUpgrades::AbstractVersion
+
+      def perform!
+        set_has_mathml_on_text_sections!
+      end
+
+      private
+
+      def set_has_mathml_on_text_sections!
+        logger.info("===================================================================")
+        logger.info("Determining which Text Sections contain MathML                     ")
+        logger.info("===================================================================")
+        logger.info("Version 8.0.0 supports MathML in the reader, but we only load the  ")
+        logger.info("required libraries for sections that include math.                 ")
+        logger.info("===================================================================")
+
+        TextSection.find_each { |s| TextSection.has_mathml?(s.body_json) }
+      end
+    end
+  end
+end
