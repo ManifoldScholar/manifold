1284ebb5df7382ae27ebfe6666681fe8ccd4b5b1
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-01-26 16:37:11 -0800

    [C] Hide download account data UI

--- client/src/frontend/components/privacy/AccountData/index.js
+++ client/src/frontend/components/privacy/AccountData/index.js
@@ -14,7 +14,8 @@ export default function AccountData() {
         {t("forms.privacy.account_data")}
       </Styled.SectionHeader>
       <Styled.Section>
-        <Styled.ButtonGroup>
+        {/* Account data download UI to add back when api is ready. */}
+        {/* <Styled.ButtonGroup>
           <Styled.Legend>{t("forms.privacy.download.header")}</Styled.Legend>
           <Styled.Instructions>
             {t("forms.privacy.download.description")}
@@ -22,7 +23,7 @@ export default function AccountData() {
           <Styled.Button className="button-secondary button-secondary--outlined">
             {t("forms.privacy.download.button_label")}
           </Styled.Button>
-        </Styled.ButtonGroup>
+        </Styled.ButtonGroup> */}
         <Styled.ButtonGroup>
           <Styled.Legend>{t("forms.privacy.delete.header")}</Styled.Legend>
           <Styled.Instructions>
