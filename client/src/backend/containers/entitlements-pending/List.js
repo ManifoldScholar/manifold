aaa174857e8bf464ab0df8b3ab439dd4ca74bdab
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-02-02 16:24:50 -0800

    [C] Update i18n keys

--- client/src/backend/containers/entitlements-pending/List.js
+++ client/src/backend/containers/entitlements-pending/List.js
@@ -69,10 +69,12 @@ function PendingEntitlementsList({
   const deleteEntitlement = useApiCallback(pendingEntitlementsAPI.destroy);
 
   const onDelete = id => {
-    const heading = t("backend.pending_entitlements.delete_confirm_heading");
-    const message = t("backend.pending_entitlements.delete_confirm_body");
+    const heading = t("modals.delete_entitlement");
+    const message = t("modals.confirm_body");
     if (confirm)
       confirm(heading, message, () => deleteEntitlement(id).then(refresh()));
+
+    // TODO: catch errors and pass to entities list
   };
 
   return (
@@ -80,7 +82,7 @@ function PendingEntitlementsList({
       {renderChildRoutes()}
       {entitlements && (
         <EntitiesList
-          title={t("backend.pending_entitlements.header")}
+          title={t("entitlements.pending.header")}
           titleStyle="bar"
           entityComponent={PendingEntitlementRow}
           entityComponentProps={{ onEdit, onDelete }}
@@ -89,13 +91,13 @@ function PendingEntitlementsList({
             <Button
               path={lh.link("backendRecordsEntitlementsNew")}
               type="add"
-              text={t("backend.pending_entitlements.add_button_label")}
+              text={t("entitlements.pending.add_button_label")}
               authorizedFor="entitlement"
             />,
             <Button
               path={lh.link("backendRecordsEntitlementsImport")}
               type="import"
-              text={t("backend.pending_entitlements.import.button_label")}
+              text={t("entitlements.pending.import_button_label")}
               authorizedFor="entitlement"
             />
           ]}
