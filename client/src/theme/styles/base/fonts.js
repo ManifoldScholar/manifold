9ac0edfb02109f028e630277f38a0af4747a5308
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-02-13 11:18:00 -0800

    [F] Add math fonts

--- client/src/theme/styles/base/fonts.js
+++ client/src/theme/styles/base/fonts.js
@@ -7,6 +7,17 @@ import TruenoRgIt from "../../assets/fonts/trueno/TruenoRgIt.woff";
 import TruenoBd from "../../assets/fonts/trueno/TruenoBd.woff";
 import TruenoSBd from "../../assets/fonts/trueno/TruenoSBd.woff";
 import TruenoLt from "../../assets/fonts/trueno/TruenoLt.woff";
+import LatinModernMath from "../../assets/fonts/LatinModern/latinmodern-math.woff2";
+import LatinModernRg from "../../assets/fonts/LatinModern/lmroman12-regular.woff2";
+import LatinModernBd from "../../assets/fonts/LatinModern/lmroman12-bold.woff2";
+import LatinModernIt from "../../assets/fonts/LatinModern/lmroman12-italic.woff2";
+import STIXBd from "../../assets/fonts/STIX/STIXTwoText-Bold.woff2";
+import STIXBdIt from "../../assets/fonts/STIX/STIXTwoText-BoldItalic.woff2";
+import STIXIt from "../../assets/fonts/STIX/STIXTwoText-Italic.woff2";
+import STIXRg from "../../assets/fonts/STIX/STIXTwoText-Regular.woff2";
+import STIXMath from "../../assets/fonts/STIX/STIXTwoMath-Regular.woff2";
+import FiraMath from "../../assets/fonts/FiraMath/FiraMath-Regular.woff2";
+
 import { fontWeight } from "../variables/typography";
 
 export default `
@@ -82,5 +93,62 @@ export default `
     font-weight: ${fontWeight.light};
     font-display: optional;
     src: url(${TruenoLt}) format("woff");
+  }
+
+  @font-face {
+      font-family: LMRoman12;
+      src: url(${LatinModernRg}) format("woff2");
+  }
+  @font-face {
+      font-family: LMRoman12;
+      src: url(${LatinModernBd}) format("woff2");
+      font-weight: bold;
+  }
+  @font-face {
+      font-family: LMRoman12;
+      src: url(${LatinModernIt}) format("woff2");
+      font-style: italic;
+  }
+
+  @font-face {
+      font-family: Latin Modern Math;
+      src: local('Latin Modern Math'), local('LatinModernMath-Regular'),
+           url(${LatinModernMath}) format("woff2");
+  }
+
+  @font-face {
+      font-family: STIX Two Text;
+      src: local('STIXTwoText'),
+           url(${STIXRg}) format("woff2");
+  }
+  @font-face {
+      font-family: STIX Two Text;
+      src: local('STIXTwoText-Bold'),
+           url(${STIXBd}) format("woff2");
+      font-weight: bold;
+  }
+  @font-face {
+      font-family: STIX Two Text;
+      src: local('STIXTwoText-Italic'),
+           url(${STIXIt}) format("woff2");
+      font-style: italic;
+  }
+  @font-face {
+      font-family: STIX Two Text;
+      src: local('STIXTwoText-BoldItalic'),
+           url(${STIXBdIt}) format("woff2");
+      font-weight: bold;
+      font-style: italic;
+  }
+  @font-face {
+      font-family: STIX Two Math;
+      src: local('STIXTwoMath-Regular'),
+           url(${STIXMath}) format("woff2");
+  }
+
+  @font-face {
+      font-family: Fira Math;
+      src: local('Fira Math'), local('FiraMath-Regular'),
+           url(${FiraMath}) format("woff2");
   }
 `;
