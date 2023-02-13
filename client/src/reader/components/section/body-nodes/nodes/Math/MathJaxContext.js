ccfb0ce16d3b3170b99e2508175afb05db6d6120
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-02-13 11:18:00 -0800

    [C] Remove MathJax

--- client/src/reader/components/section/body-nodes/nodes/Math/MathJaxContext.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { MathJaxContext as MJContext } from "better-react-mathjax";
-
-export default function MathJaxContext({ children }) {
-  return <MJContext version={3}>{children}</MJContext>;
-}
