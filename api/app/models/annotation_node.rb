2a7cb76f223176106f05d2316d34d1c223397665
Alexa Grey <alexa@castironcoding.com>
2023-02-14 11:53:30 -0800

    [F] Extrapolate text section nodes for annotations
    * Use SQL formatted-schema instead of Ruby
    * Correct invalid default for text_sections.body_json
    * Ensure that a text section's body_json is always an object
      at the database level
    * Calculate a consistent `body_hash based on the stringified version
      of a text section’s `body_json`.
    * Add table and maintenance operation / job to extrapolate nodes
      from a TextSection’s `body_json`
    * Infer text section node connections for any given annotation with
      the AnnotationNode view
    * If nodes get orphaned / moved, try to reconstruct the node from
      the previously extrapolated nodes belonging to another `body_hash`.

---
+++ api/app/models/annotation_node.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# A view that calculates the smallest node possible to satisfy a given {Annotation}.
+#
+# Relies heavily on {TextSectionNode}.
+class AnnotationNode < ApplicationRecord
+  include View
+
+  belongs_to :annotation, inverse_of: :annotation_node
+
+  belongs_to :start_node, class_name: "TextSectionNode"
+  belongs_to :end_node, class_name: "TextSectionNode"
+  belongs_to :ancestor_node, -> { preload(:children) }, class_name: "TextSectionNode"
+
+  attribute :existing_node, :indifferent_hash
+
+  delegate :reconstructed_node, to: :ancestor_node, allow_nil: true
+
+  # @!attribute [r] node
+  # If we don't find a matching / current node on the {TextSection}
+  # (i.e. `ancestor_node.body_hash != text_section.body_hash`),
+  # we will instead {TextSectionNode#reconstructed_node reconstruct the node}.
+  #
+  # For entirely orphaned / legacy annotations, this can still be nil, because
+  # it won't find `ancestor_node`.
+  #
+  # @note Exposed on {Annotation} as `derived_node`.
+  # @return [Hash, nil]
+  def node
+    existing_node.presence || reconstructed_node
+  end
+end
