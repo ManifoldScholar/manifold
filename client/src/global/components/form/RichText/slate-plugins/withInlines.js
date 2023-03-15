f0db315e23c10c642d5a31b4f7de342c39fa999c
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-03-15 15:06:09 -0700

    [B] Remove duplicate insertData

--- client/src/global/components/form/RichText/slate-plugins/withInlines.js
+++ client/src/global/components/form/RichText/slate-plugins/withInlines.js
@@ -1,11 +1,12 @@
 import { wrapLink } from "../controls/LinkButton";
 import { inlineNodes } from "../rteElements";
-import { isValidUrl } from "../slateHelpers";
+import { insertImage } from "../controls/ImageButton";
+import { isValidUrl, isImageUrl } from "../slateHelpers";
 import { mathMLElements } from "reader/containers/annotation/annotatable-components/mathHelpers";
 
 /* eslint-disable no-param-reassign */
 const withInlines = editor => {
-  const { insertData, insertText, isInline } = editor;
+  const { insertText, isInline } = editor;
 
   editor.isInline = element => {
     const isInlineMath =
@@ -18,21 +19,13 @@ const withInlines = editor => {
   };
 
   editor.insertText = text => {
-    if (text && isValidUrl(text)) {
-      wrapLink(editor, text);
-    } else {
-      insertText(text);
+    if (isImageUrl(text)) {
+      return insertImage(editor, text);
     }
-  };
-
-  editor.insertData = data => {
-    const text = data.getData("text/plain");
-
-    if (text && isValidUrl(text)) {
-      wrapLink(editor, text);
-    } else {
-      insertData(data);
+    if (isValidUrl(text)) {
+      return wrapLink(editor, text);
     }
+    insertText(text);
   };
 
   return editor;
