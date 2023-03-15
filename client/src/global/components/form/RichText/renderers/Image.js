c0663080672318b793e95385bd137518dfa5e4e2
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-03-14 18:00:37 -0700

    [F] Add iframe

--- client/src/global/components/form/RichText/renderers/Image.js
+++ client/src/global/components/form/RichText/renderers/Image.js
@@ -4,7 +4,7 @@ import { useSlateStatic, ReactEditor } from "slate-react";
 import { Transforms } from "slate";
 import * as Styled from "./styles";
 
-export default function ImageRenderer({ element, children, attributes }) {
+export default function ImageRenderer({ element, children, attributes, as }) {
   const editor = useSlateStatic();
   const path = ReactEditor.findPath(editor, element);
 
@@ -17,7 +17,11 @@ export default function ImageRenderer({ element, children, attributes }) {
     <span {...attributes}>
       {children}
       <Styled.ImageWrapper contentEditable={false}>
-        <Styled.Image src={element.htmlAttrs.src} alt={element.htmlAttrs.alt} />
+        <Styled.Image
+          as={as}
+          src={element.htmlAttrs.src}
+          alt={!as ? element.htmlAttrs.alt : undefined}
+        />
         <Styled.RemoveButton onClick={onRemove}>
           <Utility.IconComposer icon="delete24" size={20} />
         </Styled.RemoveButton>
