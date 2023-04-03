7d323498952907f3105dc216ea17ab7ef0192119
Dana Johnson <dana@castironcoding.com>
2023-04-03 06:55:07 -0700

    [C] Update browserslist defns

--- client/babel.config.js
+++ client/babel.config.js
@@ -16,6 +16,7 @@ const plugins = [
   "@babel/plugin-proposal-export-namespace-from",
   "@babel/plugin-proposal-function-sent",
   "@babel/plugin-proposal-json-strings",
+  "@babel/plugin-proposal-nullish-coalescing-operator",
   "@babel/plugin-proposal-numeric-separator",
   "@babel/plugin-proposal-throw-expressions",
   "@babel/plugin-syntax-dynamic-import",
