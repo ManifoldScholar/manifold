40dfaba4b3af321a1b737a8e8dc1e49a63a8fc01
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-04-03 10:02:39 -0700

    [F] Adjust deleteBackwards

--- client/src/global/components/form/RichText/plugins/withShortcuts.js
+++ client/src/global/components/form/RichText/plugins/withShortcuts.js
@@ -3,9 +3,13 @@ import {
   Element as SlateElement,
   Point,
   Range,
-  Transforms
+  Transforms,
+  Path,
+  Node
 } from "slate";
 import { toggleBlock } from "../components/controls/BlockButton";
+import { inlineNodes } from "../utils/elements";
+import { getTextContent } from "../transforms/utils";
 
 const SHORTCUTS = {
   "*": "ul",
@@ -49,13 +53,45 @@ const withShortcuts = editor => {
     insertText(text);
   };
 
+  const removeEmptyInlines = (editor, selection) => {
+    const selectionPath = selection.anchor.path;
+    if (selectionPath[selectionPath.length - 1] === 0) return;
+
+    const match = type =>
+      type !== "iframe" && type !== "img" && inlineNodes.includes(type);
+
+    const [inline, inlinePath] =
+      Editor.previous(editor, {
+        match: n => match(n.type)
+      }) ?? [];
+    const inlineStart = Editor.start(editor, inlinePath);
+
+    if (inline) {
+      const textContent = getTextContent(editor, Node.texts(inline));
+
+      if (
+        Point.isBefore(inlineStart, selection.anchor) &&
+        textContent.length <= 1
+      ) {
+        Transforms.removeNodes(editor, {
+          at: inlinePath,
+          match: n => match(n.type)
+        });
+        return removeEmptyInlines(editor, selection);
+      }
+    }
+  };
+
   editor.deleteBackward = (...args) => {
     const { selection } = editor;
 
     if (selection && Range.isCollapsed(selection)) {
-      const match = Editor.above(editor, {
-        match: n => SlateElement.isElement(n) && Editor.isBlock(editor, n)
-      });
+      removeEmptyInlines(editor, selection);
+
+      const match =
+        Editor.above(editor, {
+          match: n => SlateElement.isElement(n) && Editor.isBlock(editor, n)
+        }) ?? [];
 
       if (match) {
         const [block, path] = match;
@@ -67,20 +103,25 @@ const withShortcuts = editor => {
           block.type !== "p" &&
           Point.equals(selection.anchor, start)
         ) {
-          const newProperties = {
-            type: "p"
-          };
-          Transforms.setNodes(editor, newProperties);
-
-          if (block.type === "li") {
+          if (path.length <= 2 && path[path.length - 1] === 0) {
             Transforms.unwrapNodes(editor, {
               match: n =>
                 !Editor.isEditor(n) &&
                 SlateElement.isElement(n) &&
                 (n.type === "ul" || n.type === "ol"),
-              split: true
+              split: false
             });
+            Transforms.setNodes(editor, { type: "p" });
+            console.log(editor.children);
+            return;
           }
+
+          Transforms.removeNodes(editor, { at: path });
+
+          const selectionPath =
+            path[path.length - 1] === 0 ? path : Path.previous(path);
+
+          Transforms.select(editor, Editor.end(editor, selectionPath));
 
           return;
         }
