d4d85bcd1cf581fae1eda1b13d3db685f1965fe1
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-04-06 12:31:37 -0700

    [C] Improve spec structure

--- api/spec/services/ingestions/text_section/ingestor.spec.rb
+++ api/spec/services/ingestions/text_section/ingestor.spec.rb
@@ -1,37 +1,36 @@
 require "rails_helper"
 
 RSpec.describe Ingestions::TextSection::Ingestor do
+  shared_examples_for "a valid text section ingestion" do
+    it "produces a text section" do
+      expect do
+        described_class.run!(ingestion: ingestion, text: text)
+      end.to execute_safely.and change(TextSection, :count).by(1)
+    end
+  end
+
   context "when a Word Doc", slow: true do
     let!(:path) { Rails.root.join("spec", "data", "ingestion", "ms_word", "example.docx") }
     let!(:ingestion) { FactoryBot.create :ingestion, :uningested, :file_source, source_path: path }
     let!(:text) { FactoryBot.create :text }
-    let!(:section) { Ingestions::TextSection::Ingestor.run(ingestion: ingestion, text: text).result }
 
-    it "returns a vaild text section" do
-      expect(section).to be_valid
-    end
+    include_examples "a valid text section ingestion"
   end
 
   context "when HTML" do
     let!(:path) { Rails.root.join("spec", "data", "ingestion", "html", "minimal-single", "index.html") }
     let!(:ingestion) { FactoryBot.create :ingestion, :uningested, :file_source, source_path: path }
     let!(:text) { FactoryBot.create :text }
-    let!(:text_section) { Ingestions::TextSection::Ingestor.run(ingestion: ingestion, text: text).result }
 
-    it "returns a valid text" do
-      expect(text_section).to be_valid
-    end
+    include_examples "a valid text section ingestion"
   end
 
   context "when Markdown" do
     let(:path) { Rails.root.join("spec", "data", "ingestion", "markdown", "minimal-single.zip") }
     let!(:ingestion) { FactoryBot.create :ingestion, :uningested, :file_source, source_path: path }
     let!(:text) { FactoryBot.create :text }
-    let!(:text_section) { Ingestions::TextSection::Ingestor.run(ingestion: ingestion, text: text).result }
 
-    it "returns a valid text" do
-      expect(text_section).to be_valid
-    end
+    include_examples "a valid text section ingestion"
   end
 
   context "when Google Doc", slow: true do
@@ -43,10 +42,7 @@ RSpec.describe Ingestions::TextSection::Ingestor do
     let(:path) { "https://docs.google.com/document/d/1bTY_5mtv0nIGUOLxvltqmwsrruqgVNgNoT2XJv1m5JQ/edit?usp=sharing" }
     let!(:ingestion) { FactoryBot.create :ingestion, :uningested, external_source_url: path }
     let!(:text) { FactoryBot.create :text }
-    let!(:text_section) { Ingestions::TextSection::Ingestor.run(ingestion: ingestion, text: text).result }
 
-    it "returns a valid text" do
-      expect(text_section).to be_valid
-    end
+    include_examples "a valid text section ingestion"
   end
 end
