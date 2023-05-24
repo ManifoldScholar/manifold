498e22db82d84a89d00b93db9fa36caa632bccae
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-05-24 16:05:04 -0700

    [F] Add destroy; update routes

---
+++ api/app/controllers/api/v1/ingestion_sources_controller.rb
@@ -0,0 +1,29 @@
+module API
+  module V1
+    class IngestionSourcesController < ApplicationController
+
+      resourceful! IngestionSource, authorize_options: { except: [:show] }
+
+      def show
+        @ingestion_source = load_ingestion_source
+        authorize_action_for @ingestion_source.text
+
+        render_single_resource @ingestion_source
+      end
+
+      def update
+        @ingestion_source = load_and_authorize_ingestion_source
+        ::Updaters::IngestionSource.new(ingestion_source_params).update(
+          @ingestion_source
+        )
+
+        render_single_resource @ingestion_source
+      end
+
+      def destroy
+        @ingestion_source = load_ingestion_source
+        @ingestion_source&.destroy
+      end
+    end
+  end
+end
--- api/app/controllers/api/v1/texts/relationships/ingestion_sources_controller.rb
+++ api/app/controllers/api/v1/texts/relationships/ingestion_sources_controller.rb
@@ -23,26 +23,10 @@ module API
             )
           end
 
-          def show
-            @ingestion_source = load_ingestion_source
-            authorize_action_for @ingestion_source.text
-
-            render_single_resource @ingestion_source
-          end
-
           def create
             @ingestion_source = ::Updaters::IngestionSource.new(ingestion_source_params).update(
               @text.ingestion_sources.new,
               creator: @current_user
-            )
-
-            render_single_resource @ingestion_source
-          end
-
-          def update
-            @ingestion_source = load_and_authorize_ingestion_source
-            ::Updaters::IngestionSource.new(ingestion_source_params).update(
-              @ingestion_source
             )
 
             render_single_resource @ingestion_source
