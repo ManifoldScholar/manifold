b1eb5162402c94c4547f9a95b05e358a12ac2705
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-06-13 10:38:20 -0700

    [B] Handle invisibles in delete; update annotations

--- client/src/global/components/form/ContentEditor/plugins/index.js
+++ client/src/global/components/form/ContentEditor/plugins/index.js
@@ -5,8 +5,8 @@ import withShortcuts from "./withShortcuts";
 import withHtmlAwareBreaks from "./withHtmlAwareBreaks";
 
 const withPlugins = editor =>
-  withHtmlAwareBreaks(
-    withShortcuts(withInlines(withVoids(withImages(editor))))
+  withInlines(
+    withHtmlAwareBreaks(withShortcuts(withVoids(withImages(editor))))
   );
 
 export default withPlugins;
