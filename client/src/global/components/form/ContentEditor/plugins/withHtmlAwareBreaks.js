0242ba763800c3f26f73a2f80236c57ede71011e
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-06-13 10:38:20 -0700

    [B] Handle invisibles in delete; update annotations

--- client/src/global/components/form/ContentEditor/plugins/withHtmlAwareBreaks.js
+++ client/src/global/components/form/ContentEditor/plugins/withHtmlAwareBreaks.js
@@ -1,9 +1,9 @@
-import { Editor, Range, Transforms, Path, Element, Point } from "slate";
+import { Editor, Range, Transforms, Path, Element, Point, Node } from "slate";
 import { rteElements } from "../utils/elements";
 import { decreaseIndent } from "../transforms/indents";
 import { getListItemNode } from "../transforms/utils";
 
-// The plugins here are for dealing with nodes that are "invisible" in the RTE. For example, with html such as <p><span>my text</span></p>, if a user hit enter at the end of "text" having moved selection there in an inline way (that is, by arrowing left or right or by typing in the line but not by arrowing up and down which selects blocks only), Slate would by default simply split the p block. This would result in a new block with the html <p><span></span></p>. Since this span is invisible to the user in RTE mode, I'm assuming that user really wants html of just <p></p> and inserting that instead. It might make sense in a later iteration to make common html elements like span and div into full RTE elements, so they can be added/removed with a button like list, etc. Doing so would remove much of the complexity here.
+// The plugins here are for dealing with nodes that are "invisible" in the RTE, that is renderedElements in "../utils/elements". For example, with html such as <p><span>my text</span></p>, if a user hit enter at the end of "text" having moved selection there in an inline way (that is, by arrowing left or right or by typing in the line but not by arrowing up and down which selects blocks only), Slate would by default simply split the p block. This would result in a new block with the html <p><span></span></p>. Since this span is invisible to the user in RTE mode, I'm assuming that user really wants html of just <p></p> and inserting that instead. It might make sense in a later iteration to assign all the elements in renderedElements to either void or full RTE elements, so there are not invisisbles and all nodes are either preview only or managed with the toolbars. Doing so would remove much of the complexity here. -LD
 
 /* eslint-disable no-param-reassign */
 const withHtmlAwareBreaks = editor => {
@@ -36,7 +36,7 @@ const withHtmlAwareBreaks = editor => {
       const liIsEmpty = Editor.isEmpty(editor, block);
       if (isCollapsed && liIsEmpty) return decreaseIndent(editor, true);
       // Since the new element we're inserting is an element in an existing list, we'll keep the classname around in case indents are handled with classes
-      next.htmlAttrs = { class: htmlAttrs.class };
+      next.htmlAttrs = { class: htmlAttrs?.class };
     }
 
     try {
@@ -46,10 +46,11 @@ const withHtmlAwareBreaks = editor => {
           { ...next, children: [{ text: "" }] },
           { at: selection, select: true }
         );
-        // If there parent block exists only for Slate normalization, ensure that our new node has a "real" html parent
+        // If the parent block exists only for Slate normalization, ensure that our new node has a "real" html parent
         if (block.slateOnly) {
           Transforms.liftNodes(editor, { mode: "lowest" });
         }
+        // For some reason I don't quite understand, inserting at a nested inline path causes a double node insertion; this cleans up that extra node
         if (!Path.equals(blockPath, nodePath)) {
           Transforms.removeNodes(editor, selection);
         }
@@ -59,28 +60,40 @@ const withHtmlAwareBreaks = editor => {
     }
   };
 
-  // Again, for html such as <p><span>my text</span></p>, ensure that the span is removed when all text content is deleted.
-  const removeEmptyInlines = selection => {
-    const match = type => Editor.isInline(editor, type);
+  // Check for nested empty inlines, again such as <p><span>my text</span></p>, and ensure that they are removed when all text content is deleted
+  const removeEmptyInline = (node, path) => {
+    const { selection } = editor;
 
-    const [inline, inlinePath] =
-      Editor.previous(editor, {
-        match: n => n.type && match(n.type)
-      }) ?? [];
-    const inlineStart = Editor.start(editor, inlinePath);
+    const start = Editor.start(editor, path);
+    const textContent = Node.string(node);
 
-    if (inline) {
-      const textContent = Node.string(inline);
+    // Using <= 1 rather than === 0 here is a choice; the result is that the inner span is removed when all its text content is, which is before the outer block wrapper, usually a paragraph, is deleted. This has the effect of cleaning up styles and other attributes, in case the user types new text in the pargraph node instead of deleting it. But as with all these choices, of course, the user could have wanted to keep those span styles around for the new text.
+    if (Point.isBefore(start, selection.anchor) && textContent.length <= 1) {
+      Transforms.unwrapNodes(editor, {
+        at: path,
+        match: n => Editor.isInline(editor, n)
+      });
 
-      if (
-        Point.isBefore(inlineStart, selection.anchor) &&
-        textContent.length === 1
-      ) {
-        Transforms.removeNodes(editor, {
-          at: inlinePath,
-          match: n => match(n.type)
-        });
-      }
+      const [inline, inlinePath] =
+        Editor.above(editor, {
+          match: n => n.type && Editor.isInline(editor, n)
+        }) ?? [];
+
+      if (inline) return removeEmptyInline(inline, inlinePath);
+    }
+  };
+
+  // If the parent block exists only for Slate normalization, remove it too.
+  const maybeUnwrapSlateOnlyNode = (node, path) => {
+    const { selection } = editor;
+    const start = Editor.start(editor, path);
+
+    if (
+      !Editor.isEditor(node) &&
+      node.slateOnly &&
+      Point.equals(selection.anchor, start)
+    ) {
+      Transforms.unwrapNodes(editor, { at: path, match: n => n.slateOnly });
     }
   };
 
@@ -94,26 +107,45 @@ const withHtmlAwareBreaks = editor => {
         if (liIsEmpty) return decreaseIndent(editor, true);
       }
 
-      removeEmptyInlines(selection);
+      const [inline, inlinePath] =
+        Editor.above(editor, {
+          match: n => n.type && Editor.isInline(editor, n)
+        }) ?? [];
 
-      const match =
+      if (inline) {
+        removeEmptyInline(inline, inlinePath);
+      }
+
+      const [nearestBlock, path] =
         Editor.above(editor, {
           match: n => Element.isElement(n) && Editor.isBlock(editor, n)
         }) ?? [];
 
-      if (match) {
-        const [block, path] = match;
-        const start = Editor.start(editor, path);
+      maybeUnwrapSlateOnlyNode(nearestBlock, path);
+
+      const [blockToTransform, transformPath] = nearestBlock.slateOnly
+        ? Editor.above(editor, {
+            match: n => Element.isElement(n) && Editor.isBlock(editor, n)
+          })
+        : [nearestBlock, path];
+
+      if (blockToTransform) {
+        const start = Editor.start(editor, transformPath);
 
         if (
-          !Editor.isEditor(block) &&
-          Element.isElement(block) &&
-          block.type !== "p" &&
+          !Editor.isEditor(blockToTransform) &&
+          Element.isElement(blockToTransform) &&
+          blockToTransform.type !== "p" &&
           Point.equals(selection.anchor, start)
         ) {
-          Transforms.setNodes(editor, { type: "p" });
+          // It's an RTE default / best practice to replace non-paragraph block nodes with a paragraph rather than removing them from the tree. This prevents other nodes from colliding in unwanted ways, such as when another block ends up in a table.
+          Transforms.setNodes(
+            editor,
+            { type: "p" },
+            { at: transformPath, select: true }
+          );
 
-          if (block.type === "li") {
+          if (blockToTransform.type === "li") {
             Transforms.unwrapNodes(editor, {
               match: n =>
                 !Editor.isEditor(n) &&
