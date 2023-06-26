209270be5a89027738012c68ba322ba1f1a2697c
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-06-26 14:09:06 -0700

    [F] Add default value for display_name
    * Also removes formatted_attribute for display_name

--- api/app/serializers/v1/ingestion_source_serializer.rb
+++ api/app/serializers/v1/ingestion_source_serializer.rb
@@ -6,7 +6,6 @@ module V1
     typed_attribute :source_identifier, Types::String
     typed_attribute :kind, Types::String.enum("publication_resource", "navigation", "section", "cover_image")
     typed_attribute :display_name, Types::String.optional
-    typed_attribute :display_name_formatted, Types::String.optional.meta(read_only: true)
     typed_attribute :attachment_data, Types::Serializer::Attachment.meta(read_only: true)
   end
 end
--- api/db/migrate/20230519033907_add_display_name_to_ingestion_source.rb
+++ api/db/migrate/20230519033907_add_display_name_to_ingestion_source.rb
@@ -1,8 +1,15 @@
 class AddDisplayNameToIngestionSource < ActiveRecord::Migration[6.0]
   def change
-    change_table :ingestion_sources do |t|
-      t.text :display_name, null: true
-      t.jsonb :fa_cache, default: {}, null: false
+    add_column :ingestion_sources, :display_name, :text, null: true
+    
+    reversible do |dir|
+      dir.up do
+        say_with_time 'Sourcing display_name from source_identifier' do
+          execute <<~SQL
+            UPDATE ingestion_sources SET display_name = source_identifier
+          SQL
+        end
+      end
     end
   end
 end
