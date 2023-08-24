2d5a3786564ff3ea16491d012a7176eb9dd0f533
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-08-24 13:58:06 -0700

    [C] Update filters/order/tags

--- client/src/hoc/withFilteredLists/assetFilters.js
+++ client/src/hoc/withFilteredLists/assetFilters.js
@@ -9,24 +9,24 @@ export default function assetFilters({ snapshotState = false } = {}) {
         name: "keyword",
         value: ""
       },
-      {
-        label: "Kind",
-        name: "kind",
-        value: "",
-        options: [
-          { label: "All assets", value: "" },
-          { label: "Cover images", value: "cover_image" },
-          { label: "Table of contents", value: "navigation" },
-          { label: "Text sections", value: "section" },
-          { label: "Other", value: "publication_resource" }
-        ]
-      },
+      // {
+      //   label: "Kind",
+      //   name: "kind",
+      //   value: "",
+      //   options: [
+      //     { label: "All assets", value: "" },
+      //     { label: "Cover images", value: "cover_image" },
+      //     { label: "Table of contents", value: "navigation" },
+      //     { label: "Text sections", value: "section" },
+      //     { label: "Other", value: "publication_resource" }
+      //   ]
+      // },
       {
         label: "Order",
         name: "order",
-        value: "updated_at ASC",
+        value: "updated_at DESC",
         options: [
-          { label: "By recently updated", value: "updated_at ASC" },
+          { label: "By recently updated", value: "updated_at DESC" },
           { label: "By name", value: "name" },
           { label: "Newest first", value: "created_at DESC" },
           { label: "Oldest first", value: "created_at ASC" }
