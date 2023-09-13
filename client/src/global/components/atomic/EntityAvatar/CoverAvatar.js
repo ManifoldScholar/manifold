f7e7bfe34a1c402a11f5e0243208fb2136f17c22
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-09-13 11:19:22 -0700

    [C] Add guards

--- client/src/global/components/atomic/EntityAvatar/CoverAvatar.js
+++ client/src/global/components/atomic/EntityAvatar/CoverAvatar.js
@@ -6,7 +6,7 @@ const CoverAvatar = ({ entity }) => {
   const meta = entity.attributes.avatarMeta?.original;
   const avatarKey = meta.width >= meta.height ? "smallSquare" : "small";
   const src = entity.attributes.avatarStyles[avatarKey];
-  const { width, height } = entity.attributes.avatarMeta[avatarKey];
+  const { width, height } = entity.attributes.avatarMeta[avatarKey] ?? {};
   const alt = entity.attributes.avatarAltText;
 
   return (
