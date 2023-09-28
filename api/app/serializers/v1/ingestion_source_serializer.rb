5a2be220e2db87481c99d65fa8413464d3a9d8cb
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-09-28 13:27:58 -0700

    [E] Format ingestion source attachment data like other attachments

--- api/app/serializers/v1/ingestion_source_serializer.rb
+++ api/app/serializers/v1/ingestion_source_serializer.rb
@@ -6,6 +6,11 @@ module V1
     typed_attribute :source_identifier, Types::String
     typed_attribute :kind, Types::String.enum("publication_resource", "navigation", "section", "cover_image")
     typed_attribute :display_name, Types::String.optional
-    typed_attribute :attachment_data, Types::Serializer::Attachment.meta(read_only: true)
+    typed_attribute :attachment_styles, Types::Serializer::Attachment.meta(read_only: true)
+    typed_attribute :attachment_content_type, Types::String.optional.meta(read_only: true)
+    typed_attribute :attachment_extension, Types::String.optional.meta(read_only: true)
+    typed_attribute :attachment_file_size, Types::String.optional.meta(read_only: true)
+    typed_attribute :attachment_alt_text, Types::String.optional
+
   end
 end
