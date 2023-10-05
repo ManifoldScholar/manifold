23dd725eb845557f94667fcba6154448ecd22e49
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-10-05 14:38:54 -0700

    [F] Add article, cite to RTE elements

--- client/src/global/components/form/ContentEditor/utils/elements.js
+++ client/src/global/components/form/ContentEditor/utils/elements.js
@@ -30,11 +30,12 @@ export const nestableElements = [
   "figure",
   "div",
   "section",
+  "article",
   "figcaption"
 ];
 
 // These elements are rendered in the RTE, but can't be created there. Their text children are editable, and these elements can be deleted in the RTE like any other block.
-export const renderedElements = ["br", "hr", "abbr", "sub", "sup"];
+export const renderedElements = ["br", "hr", "abbr", "sub", "sup", "cite"];
 
 // These elements are applied as marks to text leaves in Slate. They can be added and removed in the RTE. This probably needs some adjustment. For example, currently both del and s become "strikethrough" when serialized to Slate and there's no way to tell what the original tag was.
 export const markElements = ["i", "del", "em", "s", "strong", "u", "code"];
