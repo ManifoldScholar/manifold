59fc7ca2ee37bf3e4cab0128c1a274f58a8fef44
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2023-10-27 11:16:49 -0700

    [E] Show mark tags in html view

--- client/src/global/components/form/ContentEditor/components/renderers/Leaf.js
+++ client/src/global/components/form/ContentEditor/components/renderers/Leaf.js
@@ -1,26 +1,61 @@
 import React from "react";
+import { useHtmlBreadcrumbs } from "../../contexts/htmlBreadcrumbsContext";
+import HtmlLabel from "./HtmlLabel";
+import { getHtmlOutlineStyles } from "./styles";
 
-export default function SlateLeaf({ attributes, children, leaf }) {
+export default function SlateLeaf({ attributes, children, leaf, darkMode }) {
+  const { selectedCrumb } = useHtmlBreadcrumbs();
+  const showHtml = selectedCrumb === "all";
+
   let leafChildren = children;
 
   if (leaf.bold) {
-    leafChildren = <strong>{leafChildren}</strong>;
+    leafChildren = (
+      <strong
+        style={showHtml ? getHtmlOutlineStyles("strong", darkMode) : undefined}
+      >
+        {leafChildren}
+        <HtmlLabel element={{ type: "strong" }} visible={showHtml} />
+      </strong>
+    );
   }
 
   if (leaf.italic) {
-    leafChildren = <em>{leafChildren}</em>;
+    leafChildren = (
+      <em style={showHtml ? getHtmlOutlineStyles("em", darkMode) : undefined}>
+        {leafChildren}
+        <HtmlLabel element={{ type: "em" }} visible={showHtml} />
+      </em>
+    );
   }
 
   if (leaf.underline) {
-    leafChildren = <u>{leafChildren}</u>;
+    leafChildren = (
+      <u style={showHtml ? getHtmlOutlineStyles("u", darkMode) : undefined}>
+        {leafChildren}
+        <HtmlLabel element={{ type: "u" }} visible={showHtml} />
+      </u>
+    );
   }
 
   if (leaf.strikethrough) {
-    leafChildren = <s>{leafChildren}</s>;
+    leafChildren = (
+      <s style={showHtml ? getHtmlOutlineStyles("s", darkMode) : undefined}>
+        {leafChildren}
+        <HtmlLabel element={{ type: "s" }} visible={showHtml} />
+      </s>
+    );
   }
 
   if (leaf.code) {
-    leafChildren = <code>{leafChildren}</code>;
+    leafChildren = (
+      <code
+        style={showHtml ? getHtmlOutlineStyles("code", darkMode) : undefined}
+      >
+        {leafChildren}
+        <HtmlLabel element={{ type: "code" }} visible={showHtml} />
+      </code>
+    );
   }
 
   return <span {...attributes}>{leafChildren}</span>;
