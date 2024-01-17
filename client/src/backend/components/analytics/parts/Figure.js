290261a1a26d00713b707e84aad654d9fc0453c4
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2024-01-17 12:11:01 -0800

    [B] Render analytics blocks with string values

--- client/src/backend/components/analytics/parts/Figure.js
+++ client/src/backend/components/analytics/parts/Figure.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Figure({ stat, caption }) {
-  return !isNaN(stat) ? (
+  return stat ? (
     <figure>
       <div className="analytics-block__stat">{stat}</div>
       <figcaption className="analytics-block__caption">{caption}</figcaption>
