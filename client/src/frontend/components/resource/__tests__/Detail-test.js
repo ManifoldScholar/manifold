26742b27d0511b6341272fee0ff95c24fa705277
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2024-04-10 12:43:01 -0700

    [C] Update snapshots

--- client/src/frontend/components/resource/__tests__/Detail-test.js
+++ client/src/frontend/components/resource/__tests__/Detail-test.js
@@ -2,14 +2,9 @@ import Detail from "../Detail";
 
 describe("frontend/components/resource/Detail", () => {
   def("resource", () => factory("resource"));
-  def("root", () => (
-    <Detail
-      resource={$resource}
-      resourceUrl="resource/url"
-    />
-  ));
+  def("root", () => <Detail resource={$resource} resourceUrl="resource/url" />);
 
   it("matches the snapshot", () => {
-    expect(mount($withApp($root)).html()).toMatchSnapshot();
+    expect(render($withApp($root)).html()).toMatchSnapshot();
   });
 });
