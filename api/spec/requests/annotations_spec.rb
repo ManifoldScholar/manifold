1bf222371d1c12ad63bf45b938f9c78e1080cb8a
Lauren Davidson <32903719+1aurend@users.noreply.github.com>
2024-04-11 11:13:16 -0700

    [F] Add annotations#index endpoint

---
+++ api/spec/requests/annotations_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+RSpec.describe "Annotations API", type: :request do
+  context "when fetching annotations" do
+    let(:filter) do { page: { number: 1, size: 10 } } end
+    let(:params) do { filter: filter } end
+    let(:path) { api_v1_annotations_path }
+
+    let(:annotations) do
+      FactoryBot.create_list :annotation, 3
+    end
+
+    it "renders successfully" do
+      expect do
+        get path, headers: admin_headers, params: params
+      end.to execute_safely
+
+      expect(response).to have_http_status(200)
+    end
+
+  end
+end
