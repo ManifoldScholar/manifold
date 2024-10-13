372dc02f28c5766251a58c96b5389a29d76fd643
1aurend <imlcdavidson@gmail.com>
2024-10-13 14:44:55 -0700

    [F] Setup initial user detail route + containers

--- client/src/backend/containers/users/List.js
+++ client/src/backend/containers/users/List.js
@@ -105,7 +105,7 @@ class UsersListContainerImplementation extends PureComponent {
     return (
       <>
         {childRoutes(this.props.route, {
-          drawer: true,
+          drawer: false,
           drawerProps,
           childProps: { refetch }
         })}
