1a210a2ae661628dd115fddb1e39042f2a84c32b
1aurend <imlcdavidson@gmail.com>
2024-10-22 16:10:56 -0700

    [F] Add user relationships RG memberships endpoint

---
+++ api/app/controllers/api/v1/users/relationships/reading_group_memberships_controller.rb
@@ -0,0 +1,31 @@
+module API
+  module V1
+    module Users
+      module Relationships
+        class ReadingGroupMembershipsController < ApplicationController
+          before_action :set_user, only: [:index]
+
+          resourceful! ReadingGroupMembership do
+            @user.reading_group_memberships.filtered(
+              with_pagination!(reading_group_membership_filter_params)
+            )
+          end
+
+          def index
+            @reading_group_memberships = load_reading_group_memberships
+            render_multiple_resources(
+              @reading_group_memberships,
+              include: [:reading_group]
+            )
+          end
+
+          private
+
+          def set_user
+            @user = User.find(params[:user_id])
+          end
+        end
+      end
+    end
+  end
+end
