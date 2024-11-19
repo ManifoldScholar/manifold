e01f9c415fe70527cd53374b1acb93dcbae01c36
1aurend <imlcdavidson@gmail.com>
2024-11-19 12:48:03 -0800

    [F] Add bulk actions to RG annotations list

---
+++ client/src/backend/components/list/EntitiesList/List/bulkActions/index.js
@@ -0,0 +1,6 @@
+import Checkbox from "./Checkbox";
+import SelectAll from "./SelectAll";
+import useBulkActions from "./hook";
+import { DeleteButton } from "./DeleteButton";
+
+export { Checkbox, SelectAll, DeleteButton, useBulkActions };
