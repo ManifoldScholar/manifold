a2f1fc317b351d4baf7b105a5a972bfbf3b0f877
1aurend <imlcdavidson@gmail.com>
2024-12-12 10:40:05 -0800

    [C] Fix rebase regressions

--- client/src/backend/containers/users/List.js
+++ client/src/backend/containers/users/List.js
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
 import { usersAPI, requests } from "api";
 import lh from "helpers/linkHandler";
-import { childRoutes } from "helpers/router";
 import EntitiesList, {
   Button,
   Search,
@@ -15,8 +14,7 @@ import { useParams } from "react-router-dom";
 
 function UsersListContainer({
   entitiesListSearchProps,
-  entitiesListSearchParams,
-  route
+  entitiesListSearchParams
 }) {
   const { t } = useTranslation();
   const { id } = useParams();
@@ -27,45 +25,33 @@ function UsersListContainer({
     initSearchProps: entitiesListSearchProps("users")
   });
 
-  const { data: users, meta: usersMeta, refresh } = useFetch({
+  const { data: users, meta: usersMeta } = useFetch({
     request: [usersAPI.index, filters, pagination],
     options: { requestKey: requests.beUsers }
   });
 
   if (!users || !usersMeta) return null;
 
-  const drawerProps = {
-    closeUrl: lh.link("backendRecordsUsers"),
-    lockScroll: "always"
-  };
-
   return (
-    <>
-      {childRoutes(route, {
-        drawer: true,
-        drawerProps,
-        childProps: { refetch: refresh }
-      })}
-      <EntitiesList
-        entityComponent={UserRow}
-        entityComponentProps={{ active: id }}
-        title={t("records.users.header")}
-        titleStyle="bar"
-        entities={users}
-        unit={t("glossary.user", { count: usersMeta.pagination.totalCount })}
-        pagination={usersMeta.pagination}
-        showCount
-        search={<Search {...searchProps} />}
-        buttons={[
-          <Button
-            path={lh.link("backendRecordsUsersNew")}
-            text={t("records.users.button_label")}
-            authorizedFor="user"
-            type="add"
-          />
-        ]}
-      />
-    </>
+    <EntitiesList
+      entityComponent={UserRow}
+      entityComponentProps={{ active: id }}
+      title={t("records.users.header")}
+      titleStyle="bar"
+      entities={users}
+      unit={t("glossary.user", { count: usersMeta.pagination.totalCount })}
+      pagination={usersMeta.pagination}
+      showCount
+      search={<Search {...searchProps} />}
+      buttons={[
+        <Button
+          path={lh.link("backendRecordsUserNew")}
+          text={t("records.users.button_label")}
+          authorizedFor="user"
+          type="add"
+        />
+      ]}
+    />
   );
 }
 
