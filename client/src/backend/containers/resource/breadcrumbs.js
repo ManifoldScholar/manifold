da1935fd1ace945aa0516d79750eb53fefb60a10
1aurend <imlcdavidson@gmail.com>
2024-12-17 14:01:23 -0800

    [F] Add admin comments list

--- client/src/backend/containers/resource/breadcrumbs.js
+++ client/src/backend/containers/resource/breadcrumbs.js
@@ -31,16 +31,19 @@ export const getBreadcrumbs = (resource, project, belongsToJournalIssue, t) => {
           label: t("glossary.journal_title_case_other")
         },
         {
-          to: lh.link("backendJournal", project.relationships.journal.id),
-          label: project.relationships.journal.attributes.titlePlaintext
+          to: lh.link("backendJournal", project.relationships.journal?.id),
+          label: project.relationships.journal?.attributes.titlePlaintext
         },
         {
-          to: lh.link("backendJournalIssues", project.relationships.journal.id),
+          to: lh.link(
+            "backendJournalIssues",
+            project.relationships.journal?.id
+          ),
           label: t("glossary.issue_truncated_title_case_other")
         },
         {
           to: lh.link("backendProject", project.id),
-          label: project.relationships.journalIssue.attributes.title
+          label: project.relationships.journalIssue?.attributes.title
         },
         {
           to: lh.link("backendProjectResources", project.id),
