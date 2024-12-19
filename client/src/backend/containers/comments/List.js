8eee74c9e7e005b0f343087717f238680f4712ca
1aurend <imlcdavidson@gmail.com>
2024-12-19 12:52:26 -0800

    [F] Wire up bulk delete comments

--- client/src/backend/containers/comments/List.js
+++ client/src/backend/containers/comments/List.js
@@ -1,12 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
-import { commentsAPI } from "api";
+import { commentsAPI, bulkDeleteAPI } from "api";
 import EntitiesList, {
   Search,
   CommentRow
 } from "backend/components/list/EntitiesList";
-import { useFetch, useApiCallback, useListQueryParams } from "hooks";
+import {
+  useFetch,
+  useApiCallback,
+  useListQueryParams,
+  useNotification
+} from "hooks";
 import withFilteredLists, { commentFilters } from "hoc/withFilteredLists";
 import withConfirmation from "hoc/withConfirmation";
 import PageHeader from "backend/components/layout/PageHeader";
@@ -15,7 +20,8 @@ import { childRoutes } from "helpers/router";
 import {
   useBulkActions,
   useClearBulkSelectionWithFilters,
-  SelectAll
+  SelectAll,
+  BulkActionButtons
 } from "backend/components/list/EntitiesList/List/bulkActions";
 
 function CommentsList({
@@ -41,6 +47,7 @@ function CommentsList({
 
   const {
     bulkActionsActive,
+    toggleBulkActions,
     resetBulkSelection,
     handleSelectAll,
     bulkSelection,
@@ -72,38 +79,36 @@ function CommentsList({
     count: meta?.pagination?.totalCount
   });
 
-  // Bulk Delete logic: add back when API available
+  const bulkDelete = useApiCallback(bulkDeleteAPI.comments);
 
-  // const bulkDelete = useApiCallback(bulkDeleteAPI.comments);
+  const notifyBulkDelete = useNotification(count => ({
+    level: 0,
+    id: "BULK_DELETE_SUCCESS",
+    heading: t("notifications.bulk_delete_success"),
+    body: t("notifications.bulk_delete_success_body", {
+      count,
+      entity: t("glossary.comment", { count })
+    }),
+    expiration: 5000
+  }));
 
-  // const notifyBulkDelete = useNotification(count => ({
-  //   level: 0,
-  //   id: "BULK_DELETE_SUCCESS",
-  //   heading: t("notifications.bulk_delete_success"),
-  //   body: t("notifications.bulk_delete_success_body", {
-  //     count,
-  //     entity: t("glossary.comment", { count })
-  //   }),
-  //   expiration: 5000
-  // }));
-
-  // const onBulkDelete = () => {
-  //   const count = bulkSelection.filters
-  //     ? meta?.pagination?.totalCount
-  //     : bulkSelection.ids.length;
-  //   const heading = t("modals.bulk_delete", { count, unit });
-  //   const message = t("modals.confirm_body");
-  //   if (confirm)
-  //     confirm(heading, message, async () => {
-  //       const params = bulkSelection.filters
-  //         ? { filters: bulkSelection.filters, ids: [] }
-  //         : { filters: {}, ids: bulkSelection.ids };
-  //       const res = await bulkDelete(params);
-  //       notifyBulkDelete(res.bulk_deletions.total);
-  //       refresh();
-  //       resetBulkSelection();
-  //     });
-  // };
+  const onBulkDelete = () => {
+    const count = bulkSelection.filters
+      ? meta?.pagination?.totalCount
+      : bulkSelection.ids.length;
+    const heading = t("modals.bulk_delete", { count, unit });
+    const message = t("modals.confirm_body");
+    if (confirm)
+      confirm(heading, message, async () => {
+        const params = bulkSelection.filters
+          ? { filters: bulkSelection.filters, ids: [] }
+          : { filters: {}, ids: bulkSelection.ids };
+        const res = await bulkDelete(params);
+        notifyBulkDelete(res.bulk_deletions.total);
+        refresh();
+        resetBulkSelection();
+      });
+  };
 
   const renderChildRoutes = () => {
     const closeUrl = lh.link("backendRecordsComments");
@@ -151,6 +156,14 @@ function CommentsList({
             )
           }
           unit={unit}
+          buttons={[
+            <BulkActionButtons
+              active={bulkActionsActive}
+              onBulkDelete={onBulkDelete}
+              toggleBulkActions={toggleBulkActions}
+              actionsDisabled={bulkSelectionEmpty}
+            />
+          ]}
         />
       )}
     </>
