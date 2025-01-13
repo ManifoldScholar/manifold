a69cb4b777e23c41fc318e60425dc3436a035dbd
1aurend <imlcdavidson@gmail.com>
2025-01-13 13:34:45 -0800

    [B] Improve bulk actions button styles

--- client/src/backend/components/list/EntitiesList/List/bulkActions/Buttons/styles.js
+++ client/src/backend/components/list/EntitiesList/List/bulkActions/Buttons/styles.js
@@ -4,15 +4,21 @@ import { Button } from "backend/components/list/EntitiesList";
 export const DeleteButton = styled(Button)`
   color: var(--error-color);
 
-  &:hover {
+  &:hover:not([disabled]) {
     color: var(--color-neutral-text-extra-dark);
     background: var(--error-color);
-    border: var(--error-color);
+    border-color: var(--error-color);
   }
 
   &:disabled {
     color: var(--color-base-neutral75);
     pointer-events: none;
+  }
+
+  &:focus-visible:not([disabled]) {
+    color: var(--color-neutral-text-extra-dark);
+    border-color: var(--error-color);
+    background: var(--color-base-red20);
   }
 `;
 
@@ -23,7 +29,11 @@ export const Toggle = styled(Button)`
     &:hover {
       color: var(--color-neutral-text-extra-dark);
       background: var(--color-neutral-text-light);
-      border: var(--color-neutral-text-light);
+      border-color: var(--color-neutral-text-light);
+    }
+
+    &:focus-visible {
+      color: var(--color-neutral-text-extra-dark);
     }
   }
 `;
