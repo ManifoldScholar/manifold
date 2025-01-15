93f9b94d57164e147527a8e9e5f7554968a965b1
Tim Standen <tim.standen@castironcoding.com>
2025-01-14 16:34:41 -0800

    [F] Add collaborator roles

--- api/app/enums/collaborator_role.rb
+++ api/app/enums/collaborator_role.rb
@@ -9,3 +9,45 @@ end
 
 class CollaboratorRole::Contributor < CollaboratorRole
 end
+
+class CollaboratorRole::Author < CollaboratorRole
+end
+
+class CollaboratorRole::Editor < CollaboratorRole
+end
+class CollaboratorRole::EditedBy < CollaboratorRole
+end
+class CollaboratorRole::Preface < CollaboratorRole
+end
+class CollaboratorRole::Foreward < CollaboratorRole
+end
+class CollaboratorRole::Introduction < CollaboratorRole
+end
+class CollaboratorRole::Afterword < CollaboratorRole
+end
+class CollaboratorRole::Translator < CollaboratorRole
+end
+class CollaboratorRole::Illustrator < CollaboratorRole
+end
+class CollaboratorRole::Photographer < CollaboratorRole
+end
+class CollaboratorRole::Artist < CollaboratorRole
+end
+class CollaboratorRole::Contributor < CollaboratorRole
+end
+class CollaboratorRole::SeriesEditor < CollaboratorRole
+end
+class CollaboratorRole::EssayBy < CollaboratorRole
+end
+class CollaboratorRole::RetoldBy < CollaboratorRole
+end
+class CollaboratorRole::AdaptedBy < CollaboratorRole
+end
+class CollaboratorRole::CollectedBy < CollaboratorRole
+end
+class CollaboratorRole::Agent < CollaboratorRole
+end
+class CollaboratorRole::OtherProprietor < CollaboratorRole
+end
+class CollaboratorRole::Other < CollaboratorRole
+end
