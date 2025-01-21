1dacfe03ee8f808e8267c51e0dabd237e83ef9fe
Dana Johnson <dana@castironcoding.com>
2025-01-21 15:59:59 -0800

    [C] Upgrade webpack to v5

--- client/webpack/plugins/manifest.js
+++ client/webpack/plugins/manifest.js
@@ -2,37 +2,59 @@ import paths from "../helpers/paths";
 import fs from "fs";
 import mkdirp from "mkdirp";
 
+function filterOutHMRAssets(group) {
+  return (
+    group.assets
+      // prevent HMR files from making it into manifest file
+      // https://github.com/webpack/webpack/issues/11670
+      .filter(asset => !/.*hot-update.*$/.test(asset.name))
+      .map(asset => asset.name)
+  );
+}
+
 function ManifestPlugin(options) {
   this.options = options;
 }
 
 ManifestPlugin.prototype.apply = function apply(compiler) {
+  compiler.hooks.afterEmit.tap(
+    "ManifoldWebpackManifestPlugin",
+    (compilation, done) => {
+      const stats = compilation.getStats().toJson({
+        hash: true,
+        version: true,
+        timings: false,
+        assets: true,
+        chunks: false,
+        chunkModules: false,
+        chunkOrigins: false,
+        modules: false,
+        cached: false,
+        reasons: false,
+        children: false,
+        source: false,
+        errors: false,
+        errorDetails: false,
+        warnings: false,
+        publicPath: true
+      });
+      delete stats.assets;
 
-  compiler.hooks.afterEmit.tap("ManifoldWebpackManifestPlugin", (compilation, done) => {
-    const stats = compilation.getStats().toJson({
-      hash: true,
-      version: true,
-      timings: false,
-      assets: true,
-      chunks: false,
-      chunkModules: false,
-      chunkOrigins: false,
-      modules: false,
-      cached: false,
-      reasons: false,
-      children: false,
-      source: false,
-      errors: false,
-      errorDetails: false,
-      warnings: false,
-      publicPath: true
-    });
-    delete stats.assets;
-    const out = { assetsByChunkName: stats.assetsByChunkName };
-    const writePath = `${paths.build}/${this.options.fileName}`;
-    mkdirp.sync(paths.build);
-    fs.writeFileSync(writePath, JSON.stringify(out, null, 2), done);
-  });
+      const filteredGroups = {};
+      for (const groupName in stats.namedChunkGroups) {
+        filteredGroups[groupName] = filterOutHMRAssets(
+          stats.namedChunkGroups[groupName]
+        );
+      }
+      const out = {
+        assetsByChunkName: filteredGroups
+      };
+
+      const writePath = `${paths.build}/${this.options.fileName}`;
+      mkdirp.sync(paths.build);
+      fs.writeFileSync(writePath, JSON.stringify(out, null, 2), done);
+    }
+  );
 };
 
 module.exports = ManifestPlugin;
