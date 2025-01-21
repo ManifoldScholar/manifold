4397bff24942b46d75f7787f3d5a3bc8788fc13e
Dana Johnson <dana@castironcoding.com>
2025-01-21 15:16:30 -0800

    [C] Remove client tests

--- client/src/reader/components/return-menu/__tests__/Button-test.js
+++
@@ -1,9 +0,0 @@
-import Button from "../Button";
-
-describe("reader/components/return-menu/Button", () => {
-  def("toggleMock", () => jest.fn());
-  def("root", () => <Button toggleReaderMenu={$toggleMock} expanded={false} />);
-  it("matches the snapshot", () => {
-    expect(shallow($root)).toMatchSnapshot();
-  });
-});
