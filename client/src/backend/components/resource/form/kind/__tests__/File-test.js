c15517ed921b3786e737574743809a9c4f6dc6e7
Dana Johnson <dana@castironcoding.com>
2025-01-21 15:16:30 -0800

    [C] Remove client tests

--- client/src/backend/components/resource/form/kind/__tests__/File-test.js
+++
@@ -1,9 +0,0 @@
-import File from "../File";
-
-describe("backend/components/resource/form/kind/File", () => {
-  def("root", () => <File />);
-
-  it("matches the snapshot", () => {
-    expect(shallow($root)).toMatchSnapshot();
-  });
-});
