68bf9af233d84a2d944ccb8584f94de124c7b34a
Dana Johnson <dana@castironcoding.com>
2025-01-29 17:26:49 -0800

    [C] Replace react-loadable with @loadable/component
    Needed maintained, webpack 5-compatible library

--- client/src/global/components/form/CodeArea/Ace.js
+++
@@ -1,8 +0,0 @@
-import ace from "react-ace";
-import "brace/mode/css";
-import "brace/mode/javascript";
-import "brace/mode/html";
-import "./darkTheme";
-import "./lightTheme";
-
-export default ace;
