e5bb6e40b994af4ee8dba7e17579a34990d8eb8f
Dana Johnson <dana@castironcoding.com>
2025-01-30 14:24:25 -0800

    [C] Remove react-html5video

--- client/src/frontend/components/resource-player/Video/styles.js
+++ client/src/frontend/components/resource-player/Video/styles.js
@@ -15,7 +15,16 @@ export const VideoWrapper = styled.div`
   }
 `;
 
-export const Video = styled.iframe`
+export const Iframe = styled.iframe`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: var(--color-base-neutral-black);
+`;
+
+export const Video = styled.video`
   position: absolute;
   top: 0;
   left: 0;
