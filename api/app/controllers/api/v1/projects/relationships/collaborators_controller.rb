5c3f1e7bac39a21c876c90015d1518686a7d368a
1aurend <imlcdavidson@gmail.com>
2025-01-31 16:01:43 -0800

    [E] Add manages_flattened_collaborators concern

--- api/app/controllers/api/v1/projects/relationships/collaborators_controller.rb
+++ api/app/controllers/api/v1/projects/relationships/collaborators_controller.rb
@@ -4,6 +4,9 @@ module API
       module Relationships
         # Responds with collaborators in a project
         class CollaboratorsController < AbstractProjectChildController
+          include API::V1::ManagesFlattenedCollaborators
+
+          authority_actions create_from_roles: "create"
 
           resourceful! Collaborator, authorize_options: { except: [:index, :show] } do
             @project.collaborators.filtered(collaborator_filter_params)
@@ -23,7 +26,14 @@ module API
             render_single_resource(@collaborator)
           end
 
+          def create_from_roles
+            @collaborators = collaborators_from_roles(collaborators_from_roles_params, @project.id, Project.name)
+            render_multiple_resources(@collaborators, serializer: ::V1::CollaboratorSerializer)
+          end
+
           def destroy
+            return render_no_maker_error unless maker_param_present?
+
             @collaborators = load_collaborators
             @collaborators.destroy_all
           end
