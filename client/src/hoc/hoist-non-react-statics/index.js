e29b463dd76fcb822b8968e808f9dc3a40cbf196
Dana Johnson <dana@castironcoding.com>
2025-02-01 23:09:16 -0800

    [C] Vendor hoist-non-react-statics
    Project is outdated and unmaintained for last 5 years. We still use it
    in some HOCs, so for now I've put the code (it's tiny) directly in the
    repo and removed the bits supporting old React versions.

---
+++ client/src/hoc/hoist-non-react-statics/index.js
@@ -0,0 +1,109 @@
+/**
+ * Vendored (w/ small modification) from
+ * https://github.com/mridgway/hoist-non-react-statics/blob/main/src/index.js
+ * since project is unmaintained.
+ *
+ * Copyright 2015, Yahoo! Inc.
+ * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
+ */
+import React from "react";
+
+const REACT_STATICS = {
+  childContextTypes: true,
+  contextType: true,
+  contextTypes: true,
+  defaultProps: true,
+  displayName: true,
+  getDefaultProps: true,
+  getDerivedStateFromError: true,
+  getDerivedStateFromProps: true,
+  mixins: true,
+  propTypes: true,
+  type: true
+};
+
+const KNOWN_STATICS = {
+  name: true,
+  length: true,
+  prototype: true,
+  caller: true,
+  callee: true,
+  arguments: true,
+  arity: true
+};
+
+const FORWARD_REF_STATICS = {
+  $$typeof: true,
+  render: true,
+  defaultProps: true,
+  displayName: true,
+  propTypes: true
+};
+
+const MEMO_STATICS = {
+  $$typeof: true,
+  compare: true,
+  defaultProps: true,
+  displayName: true,
+  propTypes: true,
+  type: true
+};
+
+const TYPE_STATICS = {};
+TYPE_STATICS[React.forwardRef] = FORWARD_REF_STATICS;
+TYPE_STATICS[React.memo] = MEMO_STATICS;
+
+function getStatics(component) {
+  return TYPE_STATICS[component["$$typeof"]] || REACT_STATICS;
+}
+
+const defineProperty = Object.defineProperty;
+const getOwnPropertyNames = Object.getOwnPropertyNames;
+const getOwnPropertySymbols = Object.getOwnPropertySymbols;
+const getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
+const getPrototypeOf = Object.getPrototypeOf;
+const objectPrototype = Object.prototype;
+
+export default function hoistNonReactStatics(
+  targetComponent,
+  sourceComponent,
+  excludelist
+) {
+  if (typeof sourceComponent !== "string") {
+    // don't hoist over string (html) components
+
+    if (objectPrototype) {
+      const inheritedComponent = getPrototypeOf(sourceComponent);
+      if (inheritedComponent && inheritedComponent !== objectPrototype) {
+        hoistNonReactStatics(targetComponent, inheritedComponent, excludelist);
+      }
+    }
+
+    let keys = getOwnPropertyNames(sourceComponent);
+
+    if (getOwnPropertySymbols) {
+      keys = keys.concat(getOwnPropertySymbols(sourceComponent));
+    }
+
+    const targetStatics = getStatics(targetComponent);
+    const sourceStatics = getStatics(sourceComponent);
+
+    for (let i = 0; i < keys.length; ++i) {
+      const key = keys[i];
+      if (
+        !KNOWN_STATICS[key] &&
+        !(excludelist && excludelist[key]) &&
+        !(sourceStatics && sourceStatics[key]) &&
+        !(targetStatics && targetStatics[key])
+      ) {
+        const descriptor = getOwnPropertyDescriptor(sourceComponent, key);
+        try {
+          // Avoid failures from read-only properties
+          defineProperty(targetComponent, key, descriptor);
+        } catch (e) {}
+      }
+    }
+  }
+
+  return targetComponent;
+}
--- client/src/hoc/withProjectContext/index.js
+++ client/src/hoc/withProjectContext/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import hoistStatics from "hoist-non-react-statics";
+import hoistStatics from "../hoist-non-react-statics";
 import { FrontendModeContext } from "helpers/contexts";
 import { RegisterBreadcrumbs } from "global/components/atomic/Breadcrumbs";
 import lh from "helpers/linkHandler";
