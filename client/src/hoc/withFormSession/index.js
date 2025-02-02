deb41e35de6742cfca92d07d4aa2f2d44ca7db90
Dana Johnson <dana@castironcoding.com>
2025-02-01 23:09:16 -0800

    [C] Vendor hoist-non-react-statics
    Project is outdated and unmaintained for last 5 years. We still use it
    in some HOCs, so for now I've put the code (it's tiny) directly in the
    repo and removed the bits supporting old React versions.

--- client/src/hoc/withFormSession/index.js
+++ client/src/hoc/withFormSession/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import hoistStatics from "hoist-non-react-statics";
+import hoistStatics from "../hoist-non-react-statics";
 import { connect } from "react-redux";
 import get from "lodash/get";
 import has from "lodash/has";
