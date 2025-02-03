33ea83605bfa12ff8c83067475a336be639c6636
1aurend <imlcdavidson@gmail.com>
2025-02-03 10:51:39 -0800

    [C] Add specs

--- api/spec/requests/api/v1/collaborators_spec.rb
+++ api/spec/requests/api/v1/collaborators_spec.rb
@@ -1,6 +1,13 @@
 require "swagger_helper"
 
 RSpec.describe "Collaborators", type: :request do
+  context "GET /api/v1/collaborators/roles" do
+    it "returns a 200 response" do
+      get roles_api_v1_collaborators_path
+      expect(response).to have_http_status :ok
+    end
+  end
+
   context "when relating to a project" do
     let(:resource) { FactoryBot.create(:collaborator) }
     let(:project_id) { resource.collaboratable_id }
@@ -18,6 +25,27 @@ RSpec.describe "Collaborators", type: :request do
                        parent: "project",
                        model: Collaborator,
                        url_parameters: [:project_id]
+    end
+  end
+
+  context "when relating to a text" do
+    let(:text) { FactoryBot.create(:text) }
+    let(:resource) { FactoryBot.create(:collaborator, collaboratable: text) }
+    let(:text_id) { text.id }
+
+    path "/texts/{text_id}/relationships/collaborators" do
+      include_examples "an API index request",
+                       parent: "text",
+                       model: Collaborator,
+                       url_parameters: [:text_id],
+                       included_relationships: [:maker]
+    end
+
+    path "/texts/{text_id}/relationships/collaborators/{id}" do
+      include_examples "an API show request",
+                       parent: "text",
+                       model: Collaborator,
+                       url_parameters: [:text_id]
     end
   end
 end
