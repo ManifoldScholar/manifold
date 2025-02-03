fe2459ee3d3ced23c2b7f98e286fffd03bc1dd77
1aurend <imlcdavidson@gmail.com>
2025-02-03 10:18:35 -0800

    [C] Rename text collaborator form

--- client/src/backend/containers/text/collaborators/Add.js
+++ client/src/backend/containers/text/collaborators/Add.js
@@ -2,18 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
 import Layout from "backend/components/layout";
-import AddEditCollaboratorForm from "backend/components/collaborator/AddEditForm";
+import AddCollaboratorForm from "backend/components/collaborator/AddForm";
 import lh from "helpers/linkHandler";
 
-// Not using edit for now, but can adapt this container following pattern from assets if we do later
-
-export default function AddEditCollaboratorContainer({ textId, refresh }) {
+export default function AddCollaboratorContainer({ textId, refresh }) {
   const { t } = useTranslation();
 
   return (
     <section>
       <Layout.DrawerHeader title={t("projects.add_contributor_label")} />
-      <AddEditCollaboratorForm
+      <AddCollaboratorForm
         entityId={textId}
         entityType="Text"
         closeUrl={lh.link("backendTextCollaborators", textId)}
@@ -23,9 +21,9 @@ export default function AddEditCollaboratorContainer({ textId, refresh }) {
   );
 }
 
-AddEditCollaboratorContainer.displayName = "Text.Collaborators.AddEdit";
+AddCollaboratorContainer.displayName = "Text.Collaborators.Add";
 
-AddEditCollaboratorContainer.propTypes = {
+AddCollaboratorContainer.propTypes = {
   textId: PropTypes.string.isRequired,
   refresh: PropTypes.func
 };
