49a3609c7598ad52f51d8022a300de8959e18014
Dana Johnson <dana@castironcoding.com>
2025-02-10 14:52:10 -0800

    [C] Remove deprecated defaultProps
    - This warning is for React 19 but I'm dealing with it now in places
    where we're seeing warnings.

--- client/src/global/components/utility/IconComposer.js
+++ client/src/global/components/utility/IconComposer.js
@@ -1,67 +1,41 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import Icons from "global/components/icon";
 import UniqueIcons from "global/components/icon/unique";
 import endsWith from "lodash/endsWith";
-import classNames from "classnames";
-
+import humps from "utils/humps";
 import MissingIcon from "./MissingIcon";
 
-import humps from "utils/humps";
+IconComposer.propTypes = {
+  icon: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  fill: PropTypes.string,
+  stroke: PropTypes.string,
+  svgProps: PropTypes.object
+};
 
-export default class IconComposer extends PureComponent {
-  static displayName = "IconComposer";
+export default function IconComposer({
+  icon = MissingIcon,
+  fill = "currentColor",
+  svgProps = { "aria-hidden": true },
+  className,
+  size,
+  stroke
+}) {
+  const key = humps.pascalize(icon);
+  const source = endsWith(key, "Unique") ? UniqueIcons : Icons;
+  const IconComponent = source[key];
+  const adjustedclassName = classNames(className, `svg-icon--${icon}`);
 
-  static propTypes = {
-    icon: PropTypes.string.isRequired,
-    className: PropTypes.string,
-    size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    fill: PropTypes.string,
-    stroke: PropTypes.string,
-    svgProps: PropTypes.object
+  const props = {
+    svgProps,
+    className: adjustedclassName,
+    size,
+    fill,
+    stroke
   };
 
-  static defaultProps = {
-    fill: "currentColor",
-    svgProps: { "aria-hidden": true }
-  };
-
-  get failure() {
-    return MissingIcon;
-  }
-
-  get iconKey() {
-    if (!this.props.icon) return "";
-    return humps.pascalize(this.props.icon);
-  }
-
-  get iconComponent() {
-    if (!this.props.icon) return this.failure;
-    const key = this.iconKey;
-    const source = this.iconSource(key);
-    const component = source[key];
-    if (!component) return this.failure;
-    return component;
-  }
-
-  iconSource(key) {
-    if (endsWith(key, "Unique")) return UniqueIcons;
-    return Icons;
-  }
-
-  render() {
-    const { className, size, fill, stroke, icon } = this.props;
-    const IconComponent = this.iconComponent;
-    const adjustedclassName = classNames(className, `svg-icon--${icon}`);
-
-    const props = {
-      svgProps: this.props.svgProps,
-      className: adjustedclassName,
-      icon,
-      size,
-      fill,
-      stroke
-    };
-    return React.createElement(IconComponent, props);
-  }
+  return React.createElement(IconComponent ?? MissingIcon, props);
 }
