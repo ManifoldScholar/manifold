01afac612c0b99d272788878ed5dd1f85f5f5695
1aurend <imlcdavidson@gmail.com>
2025-02-11 11:32:19 -0800

    [F] Add featured_resources to resources content block

--- api/app/models/content/resources_block.rb
+++ api/app/models/content/resources_block.rb
@@ -8,10 +8,13 @@ module Content
 
     has_configured_attributes show_all_collections: [:boolean, { default: true }],
                               show_descriptions: [:boolean, { default: false }],
+                              show_resources: [:boolean, { default: true }],
                               title: :string,
                               description: :text
 
     has_many_proxied :featured_collections, source: "ResourceCollection"
+
+    has_many_proxied :featured_resources, source: "Resource"
 
     def resources_or_collections
       project.resource_collections_count&.positive? || project.resources_count&.positive?
