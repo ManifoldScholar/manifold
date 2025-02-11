a13cec6f27d6278c0717e9b55bfac0e27c215a09
1aurend <imlcdavidson@gmail.com>
2025-02-11 11:32:19 -0800

    [F] Add featured_resources to resources content block

--- api/app/serializers/v1/content/resources_block_serializer.rb
+++ api/app/serializers/v1/content/resources_block_serializer.rb
@@ -5,8 +5,10 @@ module V1
       typed_attribute :description, Types::String
       typed_attribute :show_all_collections, Types::Bool
       typed_attribute :show_descriptions, Types::Bool
+      typed_attribute :show_resources, Types::Bool
 
       typed_has_many :featured_collections, record_type: "resourceCollection"
+      typed_has_many :featured_resources, record_type: "resource"
     end
   end
 end
