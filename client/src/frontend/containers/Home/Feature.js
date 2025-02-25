e42dfd63f50ed98fe996645266dc227ddb4a30a4
1aurend <imlcdavidson@gmail.com>
2025-02-25 13:04:48 -0800

    [B][E] Ensure homepage content is SSR; prevent layout flash

--- client/src/frontend/containers/Home/Feature.js
+++ client/src/frontend/containers/Home/Feature.js
@@ -1,20 +1,15 @@
-import React, { useMemo } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Layout from "frontend/components/layout";
-import { featuresAPI } from "api";
-import { useFetch } from "hooks";
+import { useFromStore } from "hooks";
 import { commonActions } from "actions/helpers";
 import { useDispatch } from "react-redux";
 
-export default function HomeFeatureContainer({ authentication }) {
-  const filters = useMemo(() => ({ home: true }), []);
-  const { data: features } = useFetch({
-    request: [featuresAPI.index, filters],
-    withAuthDependency: true
-  });
-
+export default function HomeFeatureContainer({ features }) {
   const dispatch = useDispatch();
   const actions = commonActions(dispatch);
+
+  const authentication = useFromStore("authentication");
 
   return features?.length ? (
     <Layout.Splash
