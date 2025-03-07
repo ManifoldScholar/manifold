3d9c511ff038bdbc515c9795d3af86dd1f0b548b
1aurend <imlcdavidson@gmail.com>
2025-03-07 14:45:31 -0800

    [E] Rework head content logic to implement new fields

---
+++ client/src/frontend/components/entity/HeadContent/helpers.js
@@ -0,0 +1,129 @@
+import { useFromStore } from "hooks";
+import { useTranslation } from "react-i18next";
+
+const appendHeadTitle = (entityTitle, headTitle) => {
+  if (!headTitle) return entityTitle;
+  return `${entityTitle} | ${headTitle}`;
+};
+
+const maybeAppendParentTitle = (entityTitle, parent) => {
+  const { socialTitle, titlePlaintext } = parent.attributes ?? {};
+  const parentTitle = socialTitle ?? titlePlaintext;
+  if (parentTitle) return `${entityTitle} | ${parentTitle}`;
+  return entityTitle;
+};
+
+const TYPE_MAP = {
+  journalVolumes: "glossary.volume_one",
+  journalIssues: "glossary.issue_truncated_one"
+};
+
+const maybeSubstituteNumber = (titlePlaintext, number, type, t) => {
+  const typeStr = TYPE_MAP[type];
+  if (!number || !typeStr) return titlePlaintext;
+  return `${t(typeStr)} ${number}`;
+};
+
+const maybeSocialTitle = (socialTitle, ...args) => {
+  if (socialTitle) return socialTitle;
+  return maybeSubstituteNumber(args);
+};
+
+const maybePrepend = (entityTitle, prepend) => {
+  if (!prepend) return entityTitle;
+  return `${prepend} | ${entityTitle}`;
+};
+
+export const useMetaTitle = (entity, parent, prepend) => {
+  const { t } = useTranslation();
+  const settings = useFromStore("settings", "select");
+  const headTitle = settings?.attributes.general.headTitle;
+
+  const { socialTitle, titlePlaintext, number } = entity.attributes;
+
+  const entityTitle = maybePrepend(
+    maybeSocialTitle(socialTitle, titlePlaintext, number, entity.type, t),
+    prepend
+  );
+
+  return appendHeadTitle(
+    maybeAppendParentTitle(entityTitle, parent),
+    headTitle
+  );
+};
+
+export const useMetaDescription = (entity, parent) => {
+  const { socialDescription, descriptionPlaintext } = entity.attributes;
+  if (socialDescription) return socialDescription;
+  if (descriptionPlaintext) return descriptionPlaintext;
+
+  const {
+    socialDescription: parentSocialDescription,
+    descriptionPlaintext: parentDescriptionPlaintext
+  } = parent?.attributes ?? {};
+  return parentSocialDescription ?? parentDescriptionPlaintext;
+};
+
+const maybeCoverImage = (entity, fallback) => {
+  if (entity.attributes.coverStyles)
+    return entity.attributes.coverStyles.mediumSquare;
+  return fallback;
+};
+
+const maybeAvatarImage = (entity, fallback) => {
+  if (entity.attributes.avatarStyles)
+    return entity.attributes.avatarStyles.mediumSquare;
+  return fallback;
+};
+
+const maybeLogoImage = (entity, fallback) => {
+  if (entity.attributes.logoStyles)
+    return entity.attributes.logoStyles.mediumSquare;
+  return fallback;
+};
+
+const maybeHeroImage = (entity, fallback) => {
+  if (entity.attributes.heroStyles)
+    return entity.attributes.heroStyles.mediumLandscape;
+  return fallback;
+};
+
+const fallbackImageFor = (entity, parent) => {
+  const type = entity?.type;
+  switch (type) {
+    case "projects":
+      return maybeCoverImage(entity, maybeAvatarImage(entity));
+    case "journals":
+      return maybeLogoImage(entity, maybeAvatarImage(entity));
+    case "projectCollections":
+      return maybeHeroImage(entity);
+    case "texts":
+      return maybeCoverImage(entity, fallbackImageFor(parent));
+    case "journalIssues":
+      return maybeCoverImage(
+        entity,
+        maybeAvatarImage(entity, fallbackImageFor(parent))
+      );
+    case "journalVolumes":
+      return fallbackImageFor(parent);
+    default:
+      return null;
+  }
+};
+
+export const useMetaImage = (entity, parent) => {
+  const { socialImageStyles } = entity.attributes;
+
+  if (socialImageStyles?.mediumLandscape)
+    return socialImageStyles.mediumLandscape;
+
+  return fallbackImageFor(entity, parent);
+};
+
+export const useEntityHeadContent = (entity, parent, titlePrepend) => {
+  const title = useMetaTitle(entity, parent, titlePrepend);
+  const description = useMetaDescription(entity, parent);
+  const image = useMetaImage(entity, parent);
+
+  return { title, description, image };
+};
--- client/src/frontend/components/entity/HeadContent/index.js
+++ client/src/frontend/components/entity/HeadContent/index.js
@@ -1,98 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useFromStore } from "hooks";
 import HeadContent from "global/components/HeadContent";
+import { useEntityHeadContent } from "./helpers";
 
-function EntityHeadContent({
-  entity,
-  type,
-  parentEntity,
-  showParentTitle = true,
-  titleOverride
-}) {
-  const {
-    socialTitle,
-    socialDescription,
-    socialImageStyles,
-    titlePlaintext,
-    title,
-    descriptionPlaintext,
-    description,
-    heroStyles,
-    attachmentStyles,
-    variantThumbnailStyles,
-    number
-  } = entity.attributes;
-  const {
-    socialDescription: parentSocialDescription,
-    socialImageStyles: parentSocialImageStyles,
-    titlePlaintext: parentTitle,
-    descriptionPlaintext: parentDescription,
-    heroStyles: parentHeroStyles
-  } = parentEntity?.attributes ?? {};
-  const settings = useFromStore("settings", "select");
-  const headTitle = settings?.attributes.general.headTitle;
-
-  const appendHeadTitle = entityTitle => {
-    if (!headTitle) return entityTitle;
-    return `${entityTitle} | ${headTitle}`;
-  };
-
-  const getTitle = () => {
-    if (titleOverride) return titleOverride;
-    if (parentTitle && showParentTitle) {
-      if (number) return `${type} ${number} | ${parentTitle}`;
-      return `${titlePlaintext || title} | ${parentTitle}`;
-    }
-    return titlePlaintext || title;
-  };
-
-  const getSocialTitle = () => {
-    return socialTitle || appendHeadTitle(getTitle());
-  };
-
-  const getDescription = () => {
-    if (socialDescription) return socialDescription;
-    if (descriptionPlaintext) return descriptionPlaintext;
-    if (description) return description;
-    if (parentSocialDescription) return parentSocialDescription;
-    if (parentDescription) return parentDescription;
-    return null;
-  };
-
-  const getImage = () => {
-    if (socialImageStyles?.mediumLandscape)
-      return socialImageStyles.mediumLandscape;
-    if (heroStyles?.mediumLandscape) return heroStyles.mediumLandscape;
-    if (attachmentStyles?.mediumSquare) return attachmentStyles.mediumSquare;
-    if (variantThumbnailStyles?.mediumSquare)
-      return variantThumbnailStyles.mediumSquare;
-    if (parentSocialImageStyles?.mediumLandscape)
-      return parentSocialImageStyles.mediumLandscape;
-    if (parentHeroStyles?.mediumLandscape)
-      return parentHeroStyles.mediumLandscape;
-    return null;
-  };
-
-  return (
-    <HeadContent
-      title={getTitle()}
-      socialTitle={getSocialTitle()}
-      description={getDescription()}
-      image={getImage()}
-      appendDefaultTitle
-    />
+function EntityHeadContent({ entity, parentEntity, titlePrepend }) {
+  const { title, description, image } = useEntityHeadContent(
+    entity,
+    parentEntity,
+    titlePrepend
   );
+  return <HeadContent title={title} description={description} image={image} />;
 }
 
 EntityHeadContent.displayName = "Frontend.Entity.HeadContent";
 
 EntityHeadContent.propTypes = {
   entity: PropTypes.object.isRequired,
-  type: PropTypes.string,
   parentEntity: PropTypes.object,
-  showParentTitle: PropTypes.bool,
-  titleOverride: PropTypes.string
+  titlePrepend: PropTypes.string
 };
 
 export default EntityHeadContent;
