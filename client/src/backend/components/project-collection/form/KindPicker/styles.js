bab0393988b5a2e5f6b8309b10129639f2020b08
1aurend <imlcdavidson@gmail.com>
2025-03-07 12:35:00 -0800

    [F] Add social fields to project + text properties forms; improve labeling

---
+++ client/src/backend/components/project-collection/form/KindPicker/styles.js
@@ -0,0 +1,5 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  margin-block: 20px;
+`;
--- client/src/backend/containers/text/Properties.js
+++ client/src/backend/containers/text/Properties.js
@@ -88,6 +88,36 @@ export default function TextPropertiesContainer({ text }) {
             instructions={t("texts.properties.section_instructions")}
           />
         </Form.FieldGroup>
+        <Form.FieldGroup
+          label={t("journals.forms.properties.social_header")}
+          instructions={t("texts.properties.social_instructions")}
+        >
+          <Form.TextInput
+            label={t("journals.forms.properties.social_card_label")}
+            name="attributes[socialTitle]"
+            placeholder={t("journals.forms.properties.social_card_placeholder")}
+            instructions={t(
+              "journals.forms.properties.social_card_instructions"
+            )}
+          />
+          <Form.TextArea
+            wide
+            label={t("journals.forms.properties.social_description_label")}
+            name="attributes[socialDescription]"
+            placeholder={t(
+              "journals.forms.properties.social_description_placeholder"
+            )}
+          />
+          <Form.Upload
+            layout="portrait"
+            label={t("journals.forms.properties.social_image_label")}
+            accepts="images"
+            readFrom="attributes[socialImageStyles][small]"
+            name="attributes[socialImage]"
+            remove="attributes[removeSocialImage]"
+            instructions={t("texts.properties.social_image_instructions")}
+          />
+        </Form.FieldGroup>
         <Form.FieldGroup label={t("texts.properties.access_header")}>
           <Form.Switch
             wide
