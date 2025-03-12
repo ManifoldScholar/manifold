27a56c228b6e046a6eabaf030c7e035fdecb2d7f
Tim Standen <tim.standen@castironcoding.com>
2025-03-12 13:47:02 -0700

    [F] Add PG Filtering support for maker, event, and journal issue

--- api/spec/factories/journal_issue.rb
+++ api/spec/factories/journal_issue.rb
@@ -15,4 +15,16 @@ FactoryBot.define do
   factory :draft_journal_issue, parent: :journal_issue do
     association :project, factory: :draft_project
   end
+
+  factory :journal_issue_with_title, parent: :journal_issue do
+    transient do
+      journal_attributes {{title: "new"}}
+      journal_volume_attributes {{number: 1}}
+      project_attributes {{description: "tomorrow"}}
+    end
+    journal { association :journal, **journal_attributes }
+    project { association :project, **project_attributes }
+    association :creator, factory: :user
+    journal_volume { association :journal_volume, **journal_volume_attributes }
+  end
 end
