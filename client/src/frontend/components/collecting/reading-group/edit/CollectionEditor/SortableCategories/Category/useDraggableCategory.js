b6892ee1f364d3c5446d8b13949d37b418b228f3
1aurend <imlcdavidson@gmail.com>
2025-03-13 15:28:00 -0700

    [B] Ensure uncategorized is collectable drop target

--- client/src/frontend/components/collecting/reading-group/edit/CollectionEditor/SortableCategories/Category/useDraggableCategory.js
+++ client/src/frontend/components/collecting/reading-group/edit/CollectionEditor/SortableCategories/Category/useDraggableCategory.js
@@ -8,7 +8,6 @@ import {
 } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
 import { preserveOffsetOnSource } from "@atlaskit/pragmatic-drag-and-drop/element/preserve-offset-on-source";
 import { setCustomNativeDragPreview } from "@atlaskit/pragmatic-drag-and-drop/element/set-custom-native-drag-preview";
-import { combine } from "@atlaskit/pragmatic-drag-and-drop/combine";
 import {
   attachClosestEdge,
   extractClosestEdge
@@ -46,116 +45,120 @@ export default function useDraggableCategory({
   }, []);
 
   useEffect(() => {
-    if (!isStatic) {
-      const categoryEl = categoryRef.current;
-      const wrapperEl = wrapperRef.current;
-      const dragHandleEl = dragHandleRef.current;
-      invariant(categoryEl && wrapperEl && dragHandleEl);
+    const wrapperEl = wrapperRef.current;
+    invariant(wrapperEl);
 
-      return combine(
-        draggable({
-          element: categoryEl,
-          dragHandle: dragHandleEl,
-          getInitialData: ({ element }) => ({
-            type: "categories",
-            category,
-            id,
-            index,
-            rect: element.getBoundingClientRect()
-          }),
-          onDragStart: () => {
-            setDragState({ status: "dragging", type: "categories" });
-          },
-          onDrop: () => {
-            setDragState({ status: "idle" });
-            highlightDroppedEl({ element: categoryEl });
-          },
-          onGenerateDragPreview({ nativeSetDragImage, location }) {
-            setCustomNativeDragPreview({
-              nativeSetDragImage,
-              getOffset: preserveOffsetOnSource({
-                element: categoryEl,
-                input: location.current.input
-              }),
-              render({ container }) {
-                setDragState({
-                  status: "preview",
-                  type: "categories",
-                  container
-                });
-
-                return () =>
-                  setDragState({ status: "dragging", type: "categories" });
-              }
-            });
+    return dropTargetForElements({
+      element: wrapperEl,
+      getIsSticky: () => true,
+      getData: ({ element, input }) => {
+        const data = { id, index, category };
+        return attachClosestEdge(data, {
+          element,
+          input,
+          allowedEdges: ["top", "bottom"]
+        });
+      },
+      canDrop: ({ source }) => {
+        if (isMarkdown && source.data.type !== "categories") return false;
+        if (isStatic && source.data.type === "categories") return false;
+        return !!source.data.type;
+      },
+      onDragEnter({ source, self }) {
+        if (source.data.id === id) {
+          return;
+        }
+        const closestEdge = extractClosestEdge(self.data);
+        if (!closestEdge) {
+          return;
+        }
+        setDragState({
+          status: "is-over",
+          type: source.data.type,
+          closestEdge
+        });
+      },
+      onDrag({ source, self }) {
+        if (source.data.id === id) {
+          return;
+        }
+        const closestEdge = extractClosestEdge(self.data);
+        if (!closestEdge) {
+          return;
+        }
+        const proposed = {
+          status: "is-over",
+          type: source.data.type,
+          closestEdge
+        };
+        setDragState(current => {
+          if (isShallowEqual(proposed, current)) {
+            return current;
           }
-        }),
-        dropTargetForElements({
-          element: wrapperEl,
-          getIsSticky: () => true,
-          getData: ({ element, input }) => {
-            const data = { id, index, category };
-            return attachClosestEdge(data, {
-              element,
-              input,
-              allowedEdges: ["top", "bottom"]
-            });
-          },
-          canDrop: ({ source }) => {
-            if (isMarkdown && source.data.type !== "categories") return false;
-            return !!source.data.type;
-          },
-          onDragEnter({ source, self }) {
-            if (source.data.id === id) {
-              return;
-            }
-            const closestEdge = extractClosestEdge(self.data);
-            if (!closestEdge) {
-              return;
-            }
-            setDragState({
-              status: "is-over",
-              type: source.data.type,
-              closestEdge
-            });
-          },
-          onDrag({ source, self }) {
-            if (source.data.id === id) {
-              return;
-            }
-            const closestEdge = extractClosestEdge(self.data);
-            if (!closestEdge) {
-              return;
-            }
-            const proposed = {
-              status: "is-over",
-              type: source.data.type,
-              closestEdge
-            };
-            setDragState(current => {
-              if (isShallowEqual(proposed, current)) {
-                return current;
-              }
-              return proposed;
-            });
-          },
-          onDragLeave({ source }) {
-            if (source.data.id === id) {
-              setDragState({ status: "has-left", type: "categories" });
-              return;
-            }
-            setDragState({ status: "idle" });
-          },
-          onDrop() {
-            setDragState({ status: "idle" });
-            if (typeof onDropInto === "function") onDropInto();
-          }
-        })
-      );
-    }
+          return proposed;
+        });
+      },
+      onDragLeave({ source }) {
+        if (source.data.id === id) {
+          setDragState({ status: "has-left", type: "categories" });
+          return;
+        }
+        setDragState({ status: "idle" });
+      },
+      onDrop() {
+        setDragState({ status: "idle" });
+        if (typeof onDropInto === "function") onDropInto();
+      }
+    });
   }, [category, id, index, isStatic, isMarkdown, onDropInto]);
 
-  if (isStatic) return { collapsed };
+  useEffect(() => {
+    if (!isStatic) {
+      const categoryEl = categoryRef.current;
+      const dragHandleEl = dragHandleRef.current;
+      invariant(categoryEl && dragHandleEl);
+
+      return draggable({
+        element: categoryEl,
+        dragHandle: dragHandleEl,
+        getInitialData: ({ element }) => ({
+          type: "categories",
+          category,
+          id,
+          index,
+          rect: element.getBoundingClientRect()
+        }),
+        onDragStart: () => {
+          setDragState({ status: "dragging", type: "categories" });
+        },
+        onDrop: () => {
+          setDragState({ status: "idle" });
+          highlightDroppedEl({ element: categoryEl });
+        },
+        onGenerateDragPreview({ nativeSetDragImage, location }) {
+          setCustomNativeDragPreview({
+            nativeSetDragImage,
+            getOffset: preserveOffsetOnSource({
+              element: categoryEl,
+              input: location.current.input
+            }),
+            render({ container }) {
+              setDragState({
+                status: "preview",
+                type: "categories",
+                container
+              });
+
+              return () =>
+                setDragState({ status: "dragging", type: "categories" });
+            }
+          });
+        }
+      });
+    }
+  }, [category, id, index, isStatic]);
+
+  if (isStatic) return { collapsed, dragState, wrapperRef };
 
   return {
     dragState,
