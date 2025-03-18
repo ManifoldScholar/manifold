08384118ee9705a53b0edd752d6d9478eadf9107
Tim Standen <tim.standen@castironcoding.com>
2025-03-18 15:37:43 -0700

    [C] Add pg filter support for maker model

--- api/spec/operations/filtering/apply_spec.rb
+++ api/spec/operations/filtering/apply_spec.rb
@@ -26,11 +26,17 @@ RSpec.shared_examples "common filtering examples" do |use_pg_search|
       expect(operation.call({per_page: 3}, model: Project, scope: Project.all, skip_pagination: true, user: admin_user, use_pg_search: use_pg_search).length).to eq(30)
     end
 
-    it "performs keyword search" do
+    it "performs keyword search on Maker model" do
       stub_request(:get, /elasticsearch/).to_return(status: 200, body: "", headers: {})
-      FactoryBot.create_list(:project, 20)
-      params = {keyword: "body"}
-      expect(operation.call(params, model: Project, scope: Project.all, user: admin_user, use_pg_search: use_pg_search).length).to eq(20)
+      FactoryBot.create_list(:maker, 10)
+      FactoryBot.create_list(:maker, 10, first_name: "Rocky", last_name: "Balboa", name: "Rocky Balboa")
+      aggregate_failures("multiple filters") do
+        expect(operation.call({keyword: "Rocky"}, model: Maker, scope: Maker.all, user: admin_user, use_pg_search: use_pg_search).length).to eq(10)
+        expect(operation.call({keyword: "Balboa"}, model: Maker, scope: Maker.all, user: admin_user, use_pg_search: use_pg_search).length).to eq(10)
+        expect(operation.call({keyword: "Rocky Balboa"}, model: Maker, scope: Maker.all, user: admin_user, use_pg_search: use_pg_search).length).to eq(10)
+        expect(operation.call({keyword: "Rambo"}, model: Maker, scope: Maker.all, user: admin_user, use_pg_search: use_pg_search).length).to eq(10)
+        expect(operation.call({keyword: "Luke"}, model: Maker, scope: Maker.all, user: admin_user, use_pg_search: use_pg_search).length).to eq(0)
+      end
     end
 end
 
