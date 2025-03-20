f72ff2b4711c66c360b62d7c8404e4f339eef4f8
Tim Standen <tim.standen@castironcoding.com>
2025-03-20 15:17:02 -0700

    [F] Run Lint

--- api/app/models/concerns/has_keyword_search.rb
+++ api/app/models/concerns/has_keyword_search.rb
@@ -12,11 +12,13 @@ module HasKeywordSearch
 
   module ClassMethods
     # @return [void]
+    # rubocop:disable Naming/PredicateName
     def has_keyword_search!(**options)
       keyword_search_options options.freeze
 
       pg_search_scope :keyword_search, **options
     end
+    # rubocop:enable Naming/PredicateName
 
     def has_keyword_search?
       keyword_search_options.present?
