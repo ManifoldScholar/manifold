3e4c98baa24ce484bafa2f71d1ac8b01d5d79cf8
Tim Standen <tim.standen@castironcoding.com>
2025-03-21 11:47:12 -0700

    [C] Add subject filtering

--- api/app/models/resource.rb
+++ api/app/models/resource.rb
@@ -117,7 +117,7 @@ class Resource < ApplicationRecord
   after_commit :queue_fetch_thumbnail, on: [:create, :update]
   after_commit :trigger_event_creation, on: [:create]
 
-  has_keyword_search! against:%i[title description]
+  has_keyword_search! against: %i[title description]
   searchkick(word_start: TYPEAHEAD_ATTRIBUTES,
              callbacks: :async,
              batch_size: 500,
