62411d5850b410950f51df61f69d98699f5dd5b9
Dana Johnson <dana@castironcoding.com>
2025-04-03 15:16:40 -0700

    [C] Address resource-related a11y issues
    Resolves MNFLD-995
    Resolves MNFLD-996
    Resolves MNFLD-1000

--- client/src/frontend/components/content-block/types/Resources.js
+++ client/src/frontend/components/content-block/types/Resources.js
@@ -1,16 +1,18 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
+import { withTranslation } from "react-i18next";
 import EntityListTotal from "global/components/entity/ListTotal";
 import ResourceCollectionList from "frontend/components/resource-collection-list";
 import ResourceList from "frontend/components/resource-list";
 import lh from "helpers/linkHandler";
 
-export default class ProjectContentBlockResourcesBlock extends PureComponent {
+class ProjectContentBlockResourcesBlock extends PureComponent {
   static displayName = "Project.Content.Block.Resources";
 
   static propTypes = {
     entity: PropTypes.object.isRequired,
-    block: PropTypes.object.isRequired
+    block: PropTypes.object.isRequired,
+    t: PropTypes.func
   };
 
   static get title() {
@@ -85,7 +87,9 @@ export default class ProjectContentBlockResourcesBlock extends PureComponent {
       <>
         {this.hasVisibleCollections && (
           <>
-            <h3 className="screen-reader-text">Resource Collections</h3>
+            <h3 className="screen-reader-text">
+              {this.props.t("glossary.resource_collection_title_case_other")}
+            </h3>
             <ResourceCollectionList.Grid
               resourceCollections={this.visibleCollections}
             />
@@ -102,7 +106,9 @@ export default class ProjectContentBlockResourcesBlock extends PureComponent {
         )}
         {this.hasVisibleResources && (
           <>
-            <h3 className="screen-reader-text">Single Resources</h3>
+            <h3 className="screen-reader-text">
+              {this.props.t("layout.resources_block_resources_subtitle")}
+            </h3>
             <ResourceList.Thumbnails resources={this.visibleResources} />
 
             <EntityListTotal
@@ -120,3 +126,5 @@ export default class ProjectContentBlockResourcesBlock extends PureComponent {
     );
   }
 }
+
+export default withTranslation()(ProjectContentBlockResourcesBlock);
