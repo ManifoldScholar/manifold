8f00f76847ff9da92a7c34930a5c107b1e351c08
Dana Johnson <dana@castironcoding.com>
2025-04-07 16:39:21 -0700

    [C] Fix couple react deprecation warnings

--- client/src/backend/components/category/List/Texts.js
+++ client/src/backend/components/category/List/Texts.js
@@ -1,67 +1,46 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Droppable } from "react-beautiful-dnd";
 import classNames from "classnames";
 import TextsInner from "./TextsInner";
 
-export default class CategoryListTexts extends PureComponent {
-  static displayName = "Category.List.Texts";
+export default function CategoryListTexts({
+  activeType,
+  category,
+  texts = [],
+  callbacks,
+  onTextKeyboardMove
+}) {
+  const categoryId = category?.id ?? "uncategorized";
 
-  static propTypes = {
-    activeType: PropTypes.string,
-    category: PropTypes.object,
-    texts: PropTypes.array.isRequired,
-    callbacks: PropTypes.object.isRequired,
-    onTextKeyboardMove: PropTypes.func.isRequired
-  };
-
-  static defaultProps = {
-    texts: []
-  };
-
-  get texts() {
-    return this.props.texts;
-  }
-
-  get callbacks() {
-    return this.props.callbacks;
-  }
-
-  get categoryId() {
-    if (this.category) return this.category.id;
-    return "uncategorized";
-  }
-
-  get category() {
-    return this.props.category;
-  }
-
-  get noTexts() {
-    return this.texts.length === 0;
-  }
-
-  render() {
-    return (
-      <Droppable type="text" droppableId={this.categoryId}>
-        {provided => (
-          <div
-            ref={provided.innerRef}
-            className={classNames({
-              "texts-list": true,
-              "texts-list--active": this.props.activeType === "text",
-              "texts-list--empty": this.noTexts
-            })}
-          >
-            <TextsInner
-              callbacks={this.callbacks}
-              texts={this.texts}
-              category={this.category}
-              onTextKeyboardMove={this.props.onTextKeyboardMove}
-            />
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    );
-  }
+  return (
+    <Droppable type="text" droppableId={categoryId}>
+      {provided => (
+        <div
+          ref={provided.innerRef}
+          className={classNames({
+            "texts-list": true,
+            "texts-list--active": activeType === "text",
+            "texts-list--empty": texts.length === 0
+          })}
+        >
+          <TextsInner
+            callbacks={callbacks}
+            texts={texts}
+            category={category}
+            onTextKeyboardMove={onTextKeyboardMove}
+          />
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  );
 }
+
+CategoryListTexts.propTypes = {
+  activeType: PropTypes.string,
+  category: PropTypes.object,
+  texts: PropTypes.array.isRequired,
+  callbacks: PropTypes.object.isRequired,
+  onTextKeyboardMove: PropTypes.func.isRequired
+};
