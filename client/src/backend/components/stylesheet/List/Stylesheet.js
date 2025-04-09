57b533320dc0f491dab6b7c3fa03b127c4433712
1aurend <imlcdavidson@gmail.com>
2025-04-09 09:44:53 -0700

    [C] Run migration codemod

--- client/src/backend/components/stylesheet/List/Stylesheet.js
+++ client/src/backend/components/stylesheet/List/Stylesheet.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
-import { Draggable } from "react-beautiful-dnd";
+import { Draggable } from "@atlaskit/pragmatic-drag-and-drop-react-beautiful-dnd-migration";
 import classNames from "classnames";
 import Utility from "global/components/utility";
 import { Link } from "react-router-dom";
