f1ba78982a5eb4f047cca3454733b2597da59b21
Dana Johnson <dana@castironcoding.com>
2025-04-10 15:40:00 -0700

    [C] Bump slate packages

--- client/src/global/components/form/ContentEditor/components/Wrapper.js
+++ client/src/global/components/form/ContentEditor/components/Wrapper.js
@@ -101,7 +101,7 @@ export default function EditorWrapper({
               setEditingCrumb
             }}
           >
-            <Slate editor={editor} value={localSlate} onChange={onChangeSlate}>
+            <Slate editor={editor} initialValue={localSlate} onChange={onChangeSlate}>
               <Editor
                 localHtml={localHtml}
                 setLocalHtml={setLocalHtml}
