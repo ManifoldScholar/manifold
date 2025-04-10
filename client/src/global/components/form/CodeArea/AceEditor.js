507b886a3e2aad05f944c973d11bfd1de1de0642
Dana Johnson <dana@castironcoding.com>
2025-04-10 12:50:24 -0700

    [C] Bump react-ace 7 -> 14

--- client/src/global/components/form/CodeArea/AceEditor.js
+++ client/src/global/components/form/CodeArea/AceEditor.js
@@ -1,14 +1,22 @@
-import React from "react";
 import Editor from "react-ace";
+import { config } from "ace-builds";
 import Errorable from "../Errorable";
 import BaseLabel from "../BaseLabel";
 import Instructions from "../Instructions";
 
-import "brace/mode/css";
-import "brace/mode/javascript";
-import "brace/mode/html";
+import "ace-builds/src-noconflict/mode-css";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-html";
+
 import "./darkTheme";
 import "./lightTheme";
+
+import cssWorkerUrl from "ace-builds/src-noconflict/worker-css";
+import htmlWorkerUrl from "ace-builds/src-noconflict/worker-html";
+import jsWorkerUrl from "ace-builds/src-noconflict/worker-javascript";
+config.setModuleUrl("ace/mode/css_worker", cssWorkerUrl);
+config.setModuleUrl("ace/mode/html_worker", htmlWorkerUrl);
+config.setModuleUrl("ace/mode/javascript_worker", jsWorkerUrl);
 
 /* eslint-disable react/prop-types */
 export default function AceEditor(props) {
