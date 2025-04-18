b1d298f2c17e132b69f53835d0b27e2f9e4e609b
Tim Standen <tim.standen@castironcoding.com>
2025-04-18 10:54:10 -0700

    [C] Fix additional errors in request specs

--- api/config/environments/test.rb
+++ api/config/environments/test.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   config.cache_store = :memory_store
 
   # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = true
+  config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
