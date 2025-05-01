e081bcfeb05903759b4ada0440ce262289f7ee7a
Tim Standen <tim.standen@castironcoding.com>
2025-05-01 15:39:59 -0700

    [B] Fix issues with search queries, captors, video extractors

--- api/spec/services/demonstration/data_loader_spec.rb
+++ api/spec/services/demonstration/data_loader_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Demonstration::DataLoader do
 
   it "does not overwrite setting if already set" do
     settings.update_attribute("general", ga_tracking_id: "rowan")
-    loader.ensure_settings
     settings.reload
     expect(settings.general["ga_tracking_id"]).to eq("rowan")
   end
