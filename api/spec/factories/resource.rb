9031d371ce188ead1e9aca4630bd3f3b5e916d89
Alexa Grey <alexa@castironcoding.com>
2025-05-02 14:06:39 -0700

    [C] Lint & minor clean-up
    * Disable false positive-producing rubocop rules
    * Use modern Rails enum definition syntax
    * Move constant out of resources factory

--- api/spec/factories/resource.rb
+++ api/spec/factories/resource.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RESOURCE_SAMPLES = Rails.root.join("spec", "data", "resource_import")
+require_relative "../support/helpers/constants"
 
 FactoryBot.define do
   factory :resource do
@@ -11,47 +11,47 @@ FactoryBot.define do
     project
 
     trait :archive do
-      attachment { RESOURCE_SAMPLES.join("sample.zip").open }
+      attachment { ::TestHelpers::RESOURCE_SAMPLES.join("sample.zip").open }
     end
 
     trait :audio do
-      attachment { RESOURCE_SAMPLES.join("sample.mp3").open }
+      attachment { ::TestHelpers::RESOURCE_SAMPLES.join("sample.mp3").open }
     end
 
     trait :csv do
-      attachment { RESOURCE_SAMPLES.join("sample.csv").open }
+      attachment { ::TestHelpers::RESOURCE_SAMPLES.join("sample.csv").open }
     end
 
     trait :image do
-      attachment { RESOURCE_SAMPLES.join("sample.png").open }
+      attachment { ::TestHelpers::RESOURCE_SAMPLES.join("sample.png").open }
     end
 
     trait :pdf do
-      attachment { RESOURCE_SAMPLES.join("sample.pdf").open }
+      attachment { ::TestHelpers::RESOURCE_SAMPLES.join("sample.pdf").open }
     end
 
     trait :presentation do
-      attachment { RESOURCE_SAMPLES.join("sample.pptx").open }
+      attachment { ::TestHelpers::RESOURCE_SAMPLES.join("sample.pptx").open }
     end
 
     trait :spreadsheet do
-      attachment { RESOURCE_SAMPLES.join("sample.xlsx").open }
+      attachment { ::TestHelpers::RESOURCE_SAMPLES.join("sample.xlsx").open }
     end
 
     trait :text_document do
-      attachment { RESOURCE_SAMPLES.join("sample.rtf").open }
+      attachment { ::TestHelpers::RESOURCE_SAMPLES.join("sample.rtf").open }
     end
 
     trait :video do
-      attachment { RESOURCE_SAMPLES.join("sample.mp4").open }
+      attachment { ::TestHelpers::RESOURCE_SAMPLES.join("sample.mp4").open }
     end
 
     trait :with_transcript do
-      transcript { RESOURCE_SAMPLES.join("sample.rtf").open }
+      transcript { ::TestHelpers::RESOURCE_SAMPLES.join("sample.rtf").open }
     end
 
     trait :with_translation do
-      translation { RESOURCE_SAMPLES.join("sample.rtf").open }
+      translation { ::TestHelpers::RESOURCE_SAMPLES.join("sample.rtf").open }
     end
   end
 end
