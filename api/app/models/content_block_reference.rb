9002777964d383d42b9b4b7a67eb3d220667fb00
Alexa Grey <alexa@castironcoding.com>
2025-05-05 11:41:47 -0700

    [B] Handle content STI loading better in dev

--- api/app/models/content_block_reference.rb
+++ api/app/models/content_block_reference.rb
@@ -4,7 +4,8 @@ class ContentBlockReference < ApplicationRecord
   acts_as_list scope: [:content_block_id, :kind]
 
   default_scope { order(position: "ASC") }
-  belongs_to :content_block
+
+  belongs_to :content_block, inverse_of: :content_block_references
   belongs_to :referencable, polymorphic: true
 
   validates :kind, presence: true
