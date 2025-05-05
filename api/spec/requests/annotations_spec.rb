f1c8c0d0b81e40c52a48ae55b7bbcc25f6601a23
1aurend <imlcdavidson@gmail.com>
2025-05-05 11:43:31 -0700

    [B] Ensure anotations in archived RGs can be deleted

--- api/spec/requests/annotations_spec.rb
+++ api/spec/requests/annotations_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe "Annotations API", type: :request do
 
     context "when fetching an annotation detail" do
       let(:annotation) { FactoryBot.create(:annotation) }
-      let(:path) { api_v1_annotations_path(annotation) }
+      let(:path) { api_v1_annotation_path(annotation) }
 
       it "renders successfully" do
         expect do
@@ -28,6 +28,38 @@ RSpec.describe "Annotations API", type: :request do
         end.to execute_safely
 
         expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "destroys an annotation" do
+      let(:annotation) { FactoryBot.create(:annotation, creator: reader) }
+      let(:path) { api_v1_annotation_path(annotation) }
+      let(:other_author_annotation) { FactoryBot.create(:annotation, creator: author) }
+      let(:other_path) { api_v1_annotation_path(other_author_annotation) }
+
+      context "when the user is the author of the annotation" do
+        describe "the response" do
+          it "has a 204 NO CONTENT status code" do
+            delete path, headers: reader_headers
+            expect(response).to have_http_status(204)
+          end
+        end
+      end
+      context "when the user is not the author of the annotation" do
+        describe "the response" do
+          it "has a 403 FORBIDDEN status code" do
+            delete other_path, headers: reader_headers
+            expect(response).to have_http_status(403)
+          end
+        end
+      end
+      context "when the user is an admin" do
+        describe "the response" do
+          it "has a 204 NO CONTENT status code" do
+            delete path, headers: admin_headers
+            expect(response).to have_http_status(204)
+          end
+        end
       end
     end
 
