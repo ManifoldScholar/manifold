16499ed5f43d37a4b1189906ef2e4d88602d7607
April Rieger <april.rieger@yahoo.com>
2025-05-12 20:20:07 -0700

    Updates to add the correct environments

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -8,6 +8,6 @@ Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
   config.environment = ENV['SENTRY_ENVIRONMENT']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger, :sentry_logger]
-  config.enabled_environments = %w[hykuup-knapsack-staging hykuup-knapsack-production]
+  config.enabled_environments = %w[princeton-manifold-friends princeton-manifold-production]
   config.debug = true
 end
