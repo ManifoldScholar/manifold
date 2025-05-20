edd109431114056a473782c02f63d1deaf5d32b2
April Rieger <april.rieger@yahoo.com>
2025-05-19 18:38:01 -0700

    Put the gems in the api dir

--- api/config/initializers/sentry.rb
+++ api/config/initializers/sentry.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require 'sentry-rails'
-require 'sentry-ruby'
-require 'sentry-sidekiq'
+require "sentry-rails"
+require "sentry-ruby"
+require "sentry-sidekiq"
 
 Sentry.init do |config|
-  config.dsn = ENV['SENTRY_DSN']
-  config.environment = ENV['SENTRY_ENVIRONMENT']
+  config.dsn = ENV["SENTRY_DSN"]
+  config.environment = ENV["SENTRY_ENVIRONMENT"]
   config.breadcrumbs_logger = [:active_support_logger, :http_logger, :sentry_logger]
   config.enabled_environments = %w[princeton-manifold-friends princeton-manifold-production]
   config.debug = true
