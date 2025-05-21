2d3782d07b5aff0408d0e029f1079fbe81ede3f2
1aurend <imlcdavidson@gmail.com>
2025-05-21 10:41:48 -0700

    [B] Add resource block formatted description to serializer

--- api/app/serializers/v1/content/resources_block_serializer.rb
+++ api/app/serializers/v1/content/resources_block_serializer.rb
@@ -6,6 +6,8 @@ module V1
       typed_attribute :show_all_collections, Types::Bool
       typed_attribute :show_descriptions, Types::Bool
       typed_attribute :show_resources, Types::Bool
+      typed_attribute :description_formatted, Types::String.optional.meta(read_only: true)
+      typed_attribute :description_plaintext, Types::String.optional.meta(read_only: true)
 
       typed_has_many :featured_collections, record_type: "resourceCollection"
       typed_has_many :featured_resources, record_type: "resource"
