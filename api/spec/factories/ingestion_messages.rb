c90de626046a6f5169f949794db9bd8344112bd4
Tim Standen <tim.standen@castironcoding.com>
2025-05-29 17:04:10 -0700

    [F] Rewrite in response to PR feedback

--- api/spec/factories/ingestion_messages.rb
+++ api/spec/factories/ingestion_messages.rb
@@ -1,5 +1,25 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :ingestion_message do
-    
+    trait :info do
+      payload { ['INFO', 'an info message'] }
+    end
+
+    trait :warn do
+      payload { ['WARNING', 'a warning message'] }
+    end
+
+    trait :error do
+      payload { ['ERROR', 'an error message'] }
+    end
+
+    trait :fatal do
+      payload { ['FATAL', 'a fatal message'] }
+    end
+
+    trait :unknown do
+      payload { ['UNKNOWN', 'an unknown message'] }
+    end
   end
 end
