2dbffa200f77f2773ec6bf1bbcc97003d95d585b
Dana Johnson <dana@castironcoding.com>
2025-06-03 15:45:47 -0700

    [E] Refine RG Add Block layout

--- client/src/frontend/components/collecting/reading-group/edit/CollectionEditor/styles.js
+++ client/src/frontend/components/collecting/reading-group/edit/CollectionEditor/styles.js
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
-import { respond } from "theme/styles/mixins";
+import { respond, utilityPrimary } from "theme/styles/mixins";
 
-export const categoryVerticalPadding = `19px`;
+export const categoryVerticalPadding = `14px`;
 
 export const Editor = styled.div`
   --label-color: var(--strong-color);
@@ -18,12 +18,29 @@ export const Editor = styled.div`
   }
 `;
 
+export const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  & + & {
+    margin-block-start: 40px;
+  }
+`;
+
 export const CategoryInputs = styled.div`
   display: flex;
-  column-gap: 40px;
-  row-gap: 25px;
+  gap: calc(2 * ${categoryVerticalPadding});
   flex-wrap: wrap;
   justify-content: center;
 
   ${respond(`justify-content: flex-start`, 85)}
+`;
+
+export const Header = styled.h3`
+  ${utilityPrimary}
+  margin: 0;
+  font-size: 14px;
+  font-weight: var(--font-weight-semibold);
+  letter-spacing: 0.115em;
 `;
