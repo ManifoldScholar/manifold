f1a8a1ba506706536f29e9442f16014ef74d9861
Tim Standen <tim.standen@castironcoding.com>
2025-06-03 12:58:39 -0700

    [F] Replace sidekiq and zhong with good_job

--- api/config/initializers/sidekiq_and_redis.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Sidekiq.configure_server do |config|
-  config.redis = ManifoldEnv.redis.sidekiq_options
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = ManifoldEnv.redis.sidekiq_options
-end
