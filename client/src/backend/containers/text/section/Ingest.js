7460aca5fd376ad7f4a3777e709d120890522121
1aurend <imlcdavidson@gmail.com>
2025-06-04 11:14:56 -0700

    [B] Ensure ensure ingest edit links redirect properly

--- client/src/backend/containers/text/section/Ingest.js
+++ client/src/backend/containers/text/section/Ingest.js
@@ -4,35 +4,48 @@ import { useTranslation } from "react-i18next";
 import Layout from "backend/components/layout";
 import Ingestion from "backend/components/ingestion";
 import { useParams } from "react-router-dom";
-import { useFetch } from "hooks";
-import { sectionsAPI } from "api";
+import { useFetch, useFromStore } from "hooks";
+import { sectionsAPI, ingestionsAPI } from "api";
 import lh from "helpers/linkHandler";
 
 export default function IngestSectionContainer({ textId }) {
   const { t } = useTranslation();
-  const { sectionId } = useParams();
+  const { sectionId, ingestionId } = useParams();
 
   const { data: section } = useFetch({
     request: [sectionsAPI.show, sectionId, textId],
     condition: !!sectionId
   });
 
-  return (
+  const { data: ingestion } = useFetch({
+    request: [ingestionsAPI.show, ingestionId],
+    condition: !!ingestionId
+  });
+
+  const sectionName = useFromStore(
+    `entityStore.entities.textSections[${ingestion?.attributes.textSectionId}]`
+  )?.attributes.name;
+
+  return section || ingestion ? (
     <section>
       <Layout.DrawerHeader
         title={
+          /* eslint-disable-next-line no-nested-ternary */
           sectionId
             ? section?.attributes?.name
+            : ingestionId
+            ? sectionName
             : t("texts.section.ingest_button_label")
         }
       />
       <Ingestion.Form.SectionWrapper
         textId={textId}
         sectionId={sectionId}
+        ingestion={ingestion}
         cancelUrl={lh.link("backendTextSections", textId)}
       />
     </section>
-  );
+  ) : null;
 }
 
 IngestSectionContainer.displayName = "Text.Sections.Ingest";
