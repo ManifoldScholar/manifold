37c339972e688ec963a71da1a271c9878e2140f7
April Rieger <april.rieger@yahoo.com>
2025-06-06 10:02:05 -0700

    Remove sentry_logger to prevent circular error

--- api/config/initializers/sentry.rb
+++ api/config/initializers/sentry.rb
@@ -6,8 +6,7 @@ require "sentry-sidekiq"
 
 Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
-  config.environment = ENV["SENTRY_ENVIRONMENT"]
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger, :sentry_logger]
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.enabled_environments = %w[princeton-manifold-friends princeton-manifold-production]
   config.debug = true
 end
