ManifoldScholar/manifold

commit 4168d4266314fe35222eda78da59950b26033020

[B] Render HTML meta on fatal error screen

client/src/global/components/FatalError/index.js
@@ -12,6 +12,7 @@ import styles from "theme/styles/globalStyles";
 import ApiTrace from "./ApiTrace";
 import ClientTrace from "./ClientTrace";
 import * as Styled from "./styles";
+import HeadContent from "../HeadContent";
 
 class FatalError extends PureComponent {
   static propTypes = {
@@ -103,6 +104,7 @@ class FatalError extends PureComponent {
 
     return (
       <HelmetProvider>
+        <HeadContent title={`${error.status} Error: ${error.heading}`} />
         <GlobalStyles styles={styles} />
         <Styled.Body className="browse">
           <Styled.Wrapper>

commit d456e8fad77ca0d84f13eb069a9fb2429197a910

[B] Fix SSR exception from HeadContent render in FatalError
Resolves issue introduced by 4168d42

client/src/global/components/FatalError/index.js
@@ -1,7 +1,7 @@
 import { PureComponent } from "react";
 import PropTypes from "prop-types";
 import { Redirect } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { withTranslation } from "react-i18next";
 import { Global as GlobalStyles } from "@emotion/react";
 import has from "lodash/has";
@@ -12,7 +12,6 @@ import styles from "theme/styles/globalStyles";
 import ApiTrace from "./ApiTrace";
 import ClientTrace from "./ClientTrace";
 import * as Styled from "./styles";
-import HeadContent from "../HeadContent";
 
 class FatalError extends PureComponent {
   static propTypes = {
@@ -104,7 +103,7 @@ class FatalError extends PureComponent {
 
     return (
       <HelmetProvider>
-        <HeadContent title={`${error.status} Error: ${error.heading}`} />
+        <Helmet title={`${error.status} Error: ${error.heading}`} />
         <GlobalStyles styles={styles} />
         <Styled.Body className="browse">
           <Styled.Wrapper>