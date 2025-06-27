a91ae1e5c58cc7f296b0b683f8ad91b64df0386f
1aurend <imlcdavidson@gmail.com>
2025-06-27 16:12:11 -0700

    [F] Add resource collection annotations controller + route

--- api/spec/requests/resources/relationships/annotations_spec.rb
+++ api/spec/requests/resources/relationships/annotations_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe "Text Tracks API", type: :request do
+RSpec.describe "Resource Annotations API", type: :request do
   let!(:project) { FactoryBot.create(:project) }
   let!(:resource) { FactoryBot.create(:resource, project: project) }
   let!(:annotation) { FactoryBot.create(:annotation, resource: resource) }
@@ -10,7 +10,7 @@ RSpec.describe "Text Tracks API", type: :request do
   let(:path) { api_v1_resource_relationships_annotations_path(resource_id: resource.id) }
 
   describe "GET #{base_path}" do
-    it "returns a list of text tracks" do
+    it "returns a list of annotations" do
       get path
       expect(response).to have_http_status(:ok)
     end
