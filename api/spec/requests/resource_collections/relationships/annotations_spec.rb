4cbd552cabd55aa674e2e559aba4ba459797a3af
1aurend <imlcdavidson@gmail.com>
2025-06-30 16:10:39 -0700

    [B] Fix typo in resource collection annotations spec

--- api/spec/requests/resource_collections/relationships/annotations_spec.rb
+++ api/spec/requests/resource_collections/relationships/annotations_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe "Resource Collection Annotations API", type: :request do
   let!(:project) { FactoryBot.create(:project) }
   let!(:resource_collection) { FactoryBot.create(:resource_collection, project: project) }
-  let!(:annotation) { FactoryBot.create(:annotation, resource: resource) }
+  let!(:annotation) { FactoryBot.create(:annotation, resource_collection: resource_collection) }
 
   base_path = "/api/v1/resource_collections/:resource_collection_id/relationships/annotations"
 
