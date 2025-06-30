e2cded78ccbe14fbb49c0aaf2610d2fe2dcb9cdc
1aurend <imlcdavidson@gmail.com>
2025-06-30 09:47:05 -0700

    [F] Add annotations list to resource collection detail

--- client/src/frontend/containers/ResourceCollectionDetail/index.js
+++ client/src/frontend/containers/ResourceCollectionDetail/index.js
@@ -1,22 +1,35 @@
-import React from "react";
+import { useEffect } from "react";
 import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { resourceCollectionsAPI, requests } from "api";
+import { entityStoreActions as store } from "actions";
 import { RegisterBreadcrumbs } from "global/components/atomic/Breadcrumbs";
 import CheckFrontendMode from "global/containers/CheckFrontendMode";
 import useEntityHeadContent from "frontend/components/entity/useEntityHeadContent";
 import HeadContent from "global/components/HeadContent";
 import { breadcrumbs } from "./breadcrumbs";
+import Utility from "global/components/utility";
 import EventTracker, { EVENTS } from "global/components/EventTracker";
 import EntityCollection from "frontend/components/entity/Collection";
-import { useFetch, useListQueryParams, useListFilters } from "hooks";
+import AnnotationList from "global/components/Annotation/List/Default";
+import {
+  useFetch,
+  useListQueryParams,
+  useListFilters,
+  useCurrentUser,
+  usePaginationState
+} from "hooks";
+import config from "config";
+import * as Styled from "./styles";
 
 export default function ResourceCollectionDetailContainer({
   project,
   journalBreadcrumbs
 }) {
   const { t } = useTranslation();
+  const dispatch = useDispatch();
 
   const { resourceCollectionId } = useParams();
   const { data: collection } = useFetch({
@@ -42,6 +55,35 @@ export default function ResourceCollectionDetailContainer({
     options: { requestKey: requests.feSlideshow }
   });
 
+  const [annotationsPagination, setAnnotationsPage] = usePaginationState(1, 5);
+  const currentUser = useCurrentUser() ?? { id: null };
+
+  const {
+    data: annotations,
+    meta: annotationsMeta,
+    refresh: refreshAnnotations
+  } = useFetch({
+    request: [
+      resourceCollectionsAPI.annotations,
+      resourceCollectionId,
+      undefined,
+      annotationsPagination
+    ],
+    options: {
+      requestKey: "RESOURCE_COLLECTION_DETAIL_ANNOTATIONS",
+      appends: "RESOURCE_COLLECTION_DETAIL_ANNOTATIONS"
+    },
+    dependencies: [currentUser.id, resourceCollectionId],
+    condition: !config.environment.isServer
+  });
+  /* Fetch these in the client only, so we don't end up with duplicates when navigating between collection resources using back/fwd. */
+
+  useEffect(() => {
+    return () => {
+      dispatch(store.flush("RESOURCE_COLLECTION_DETAIL_ANNOTATIONS"));
+    };
+  }, [dispatch]);
+
   const filterProps = useListFilters({
     onFilterChange: param => setFilters(param),
     initialState: filters,
@@ -54,6 +96,12 @@ export default function ResourceCollectionDetailContainer({
   });
 
   const headContentProps = useEntityHeadContent(collection, project);
+
+  const remainingAnnotations =
+    annotationsMeta?.pagination?.totalCount -
+    5 * annotationsMeta?.pagination?.currentPage;
+  const nextAnnotationsCount =
+    remainingAnnotations > 5 ? 5 : remainingAnnotations;
 
   if (!project || !collection) return null;
 
@@ -73,15 +121,49 @@ export default function ResourceCollectionDetailContainer({
           t
         })}
       />
-      <EntityCollection.ProjectResourceCollectionDetail
-        resourceCollection={collection}
-        resources={resources ?? []}
-        project={project}
-        meta={meta}
-        slideshowResources={slideshowResources}
-        slideshowResourcesMeta={slideshowMeta}
-        filterProps={filterProps}
-      />
+      <Styled.Container>
+        <Styled.Grid>
+          <Styled.Main>
+            <EntityCollection.ProjectResourceCollectionDetail
+              resourceCollection={collection}
+              resources={resources ?? []}
+              project={project}
+              meta={meta}
+              slideshowResources={slideshowResources}
+              slideshowResourcesMeta={slideshowMeta}
+              filterProps={filterProps}
+              nested
+            />
+          </Styled.Main>
+          {!!annotations?.length && (
+            <Styled.Annotations>
+              <Styled.AnnotationsHeader>Annotations</Styled.AnnotationsHeader>
+              <AnnotationList
+                annotations={annotations}
+                refresh={refreshAnnotations}
+                compact
+              />
+              {!!annotationsMeta?.pagination?.nextPage && (
+                <button
+                  className="comment-more"
+                  onClick={() =>
+                    setAnnotationsPage(annotationsMeta?.pagination?.nextPage)
+                  }
+                >
+                  {t("actions.see_next_annotation", {
+                    count: nextAnnotationsCount
+                  })}
+                  <Utility.IconComposer
+                    icon="disclosureDown16"
+                    size={16}
+                    className="comment-more__icon"
+                  />
+                </button>
+              )}
+            </Styled.Annotations>
+          )}
+        </Styled.Grid>
+      </Styled.Container>
     </>
   );
 }
