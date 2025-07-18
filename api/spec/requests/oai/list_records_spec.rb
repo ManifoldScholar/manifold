d440ea0353183897d2b9e620ceb9abeb154ac099
Alexa Grey <alexa@castironcoding.com>
2025-07-18 14:32:12 -0700

    [E] Add skeleton for OAI Sets

--- api/spec/requests/oai/list_records_spec.rb
+++ api/spec/requests/oai/list_records_spec.rb
@@ -3,28 +3,28 @@
 RSpec.describe "OAI PMH List Records", type: :request do
   include_context "OAI testing"
 
+  let_it_be(:old_records) { FactoryBot.create_list(:manifold_oai_record, 5, updated_at: 6.days.ago) }
+  let_it_be(:new_records) { FactoryBot.create_list(:manifold_oai_record, 3, updated_at: 3.days.ago) }
+
   let(:opts) { {} }
-  subject(:response) { client.list_records(opts) }
 
   before do
-    FactoryBot.create_list(:project, 5, :with_metadata)
+    ManifoldOAIRecord.where.not(id: old_records.pluck(:id).concat(new_records.pluck(:id))).delete_all
   end
+
+  subject(:response) { client.list_records(opts) }
 
   it { is_expected.to be_an_instance_of OAI::ListRecordsResponse }
 
   it "finds all records" do
-    expect(response.count).to be 6
+    expect(response.count).to eq 8
   end
 
-  describe "when given filter options" do
+  context "when given filter options" do
     let(:opts) { { from: 5.days.ago, until: 2.days.ago } }
-    before do
-      FactoryBot.create_list(:manifold_oai_record, 5, updated_at: 6.days.ago)
-      FactoryBot.create_list(:manifold_oai_record, 3, updated_at: 3.days.ago)
-    end
 
     it "only finds records between the two dates" do
-      expect(response.count).to be 3
+      expect(response.count).to eq 3
     end
   end
 end
