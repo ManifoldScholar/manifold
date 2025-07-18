d0ccd6631da92637f7a6e710c8b9c376c1d72405
Alexa Grey <alexa@castironcoding.com>
2025-07-18 14:32:12 -0700

    [E] Add skeleton for OAI Sets

--- api/spec/requests/oai/list_identifiers_spec.rb
+++ api/spec/requests/oai/list_identifiers_spec.rb
@@ -2,14 +2,22 @@
 
 RSpec.describe "OAI PMH List Identifiers", type: :request do
   include_context "OAI testing"
-  let!(:records) { FactoryBot.create_list(:manifold_oai_record, 5) }
+
+  let_it_be(:records) { FactoryBot.create_list(:manifold_oai_record, 5) }
+
+  before do
+    ManifoldOAIRecord.where.not(id: records.pluck(:id)).delete_all
+  end
+
   subject(:response) { client.list_identifiers }
 
   it { is_expected.to be_an_instance_of OAI::ListIdentifiersResponse }
 
   it "gets all identifiers" do
-    expect(response.count).to be 6
+    expect(response.count).to eq 5
+
     id_map = response.map(&:identifier)
+
     records.each do |record|
       expect(id_map.include?("oai:manifold:#{record.id}")).to be true
     end
