89ec207192412c4c188aafd56129be6e623f2189
1aurend <imlcdavidson@gmail.com>
2025-07-24 12:59:27 -0700

    [F] Build new add resource annotation form

---
+++ client/src/reader/containers/resource-annotation/AddResourceAnnotationForm/styles.js
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+import EntitiesList from "backend/components/list/EntitiesList";
+
+export const List = styled(EntitiesList)`
+  margin-block-start: 1.5rem;
+`;
