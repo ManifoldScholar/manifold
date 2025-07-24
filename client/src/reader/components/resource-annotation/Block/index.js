9652090710622a76897e4a3bdb4e08bf2e5da6a2
1aurend <imlcdavidson@gmail.com>
2025-07-24 13:12:08 -0700

    [B][E] Check for embed; add resource data to block

--- client/src/reader/components/resource-annotation/Block/index.js
+++ client/src/reader/components/resource-annotation/Block/index.js
@@ -1,10 +1,19 @@
+import { useFromStore } from "hooks";
 import * as Styled from "./styles";
 
 export default function ResourceBlock({ annotation }) {
+  const resource = useFromStore(
+    `entityStore.entities.resources["${annotation.resourceId}"]`
+  );
   return (
     <Styled.Block>
       <p>Resource block for:</p>
-      <p>{annotation.resourceId}</p>
+      <p>{resource?.attributes.title}</p>
+      {annotation.readerDisplayFormat === "embed" ? (
+        <p>Display: Interactive embed</p>
+      ) : (
+        <p>Display: Static callout</p>
+      )}
     </Styled.Block>
   );
 }
