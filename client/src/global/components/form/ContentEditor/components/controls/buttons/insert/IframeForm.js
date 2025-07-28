4f68bb39d476b5f7794abf0b38a6b866cd2e83de
1aurend <imlcdavidson@gmail.com>
2025-07-28 16:34:37 -0700

    [F] Add initial asset browse modal to editor

--- client/src/global/components/form/ContentEditor/components/controls/buttons/insert/IframeForm.js
+++ client/src/global/components/form/ContentEditor/components/controls/buttons/insert/IframeForm.js
@@ -1,24 +1,41 @@
-import React from "react";
+import { useState } from "react";
 import { FormBaseInput as Input } from "../../../../../BaseInput";
 import { useTranslation } from "react-i18next";
+import BrowseButton from "./browse/BrowseButton";
 import * as Styled from "./styles";
 
 export default function InsertIframeForm({ urlRef, titleRef, defaultValues }) {
   const { t } = useTranslation();
 
+  const [formValues, setFormValues] = useState({
+    url: defaultValues?.src || "",
+    title: defaultValues?.title
+  });
+
+  const onSelect = asset => {
+    setFormValues({
+      url: `/api/proxy/ingestion_sources/${asset.id}`,
+      title: asset.attributes.displayName
+    });
+  };
+
   return (
     <Styled.Form>
+      <BrowseButton onSelect={onSelect} format="video" />
       <Input
-        defaultValue={defaultValues?.src}
+        value={formValues.url}
         ref={urlRef}
         name="url"
         label={t("editor.forms.iframe_url_label")}
+        onChange={e => setFormValues({ ...formValues, url: e.target.value })}
       />
       <Input
+        value={formValues.title}
         defaultValue={defaultValues?.title}
         ref={titleRef}
         name="title"
         label={t("editor.forms.title_label")}
+        onChange={e => setFormValues({ ...formValues, title: e.target.value })}
       />
     </Styled.Form>
   );
