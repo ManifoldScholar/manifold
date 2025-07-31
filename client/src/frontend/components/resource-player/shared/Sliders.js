4b2754bf4ddee40200d800cae10664b076c88947
Jen Wardwell <jen@castironcoding.com>
2025-07-31 16:48:36 -0700

    [F] Add speed options, prop types

--- client/src/frontend/components/resource-player/shared/Sliders.js
+++ client/src/frontend/components/resource-player/shared/Sliders.js
@@ -1,4 +1,5 @@
-import { TimeSlider, VolumeSlider, SpeedSlider } from "@vidstack/react";
+import PropTypes from "prop-types";
+import { TimeSlider, VolumeSlider } from "@vidstack/react";
 
 export function Volume({ orientation = "horizontal" }) {
   return (
@@ -16,9 +17,9 @@ export function Volume({ orientation = "horizontal" }) {
   );
 }
 
-// export interface TimeSliderProps {
-//   thumbnails?: string;
-// }
+Volume.propTypes = {
+  orientation: PropTypes.oneOf(["horizontal", "vertical"])
+};
 
 export function Time({ thumbnails }) {
   return (
@@ -59,15 +60,6 @@ export function Time({ thumbnails }) {
   );
 }
 
-export function Speed() {
-  return (
-    <SpeedSlider.Root className="vds-slider">
-      <SpeedSlider.Track className="vds-slider-track" />
-      <SpeedSlider.TrackFill className="vds-slider-track-fill vds-slider-track" />
-      <SpeedSlider.Thumb className="vds-slider-thumb" />
-      <SpeedSlider.Steps class="vds-slider-steps">
-        {step => <div className="vds-slider-step" key={String(step)} />}
-      </SpeedSlider.Steps>
-    </SpeedSlider.Root>
-  );
-}
+Time.propTypes = {
+  thumbnails: PropTypes.string
+};
