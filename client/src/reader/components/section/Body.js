d15370c216cb61205576c5f2bc8612ea09fab6a4
1aurend <imlcdavidson@gmail.com>
2025-07-31 15:42:28 -0700

    [F] Implement new thumb design; finish group logic

--- client/src/reader/components/section/Body.js
+++ client/src/reader/components/section/Body.js
@@ -2,7 +2,6 @@ import { useState, useMemo } from "react";
 import PropTypes from "prop-types";
 import BodyNodes from "./body-nodes";
 import { MarkerContext } from "reader/components/resource-annotation/context";
-import isEqual from "lodash/isEqual";
 
 export default function Body(props) {
   const { section, annotations, pendingAnnotation, location } = props;
@@ -13,26 +12,54 @@ export default function Body(props) {
     location.key
   ];
 
-  const [thumbOffsets, setThumbOffsets] = useState({});
-  const overlaps = useMemo(
-    () =>
-      Object.keys(thumbOffsets).reduce((groups, id) => {
-        if (thumbOffsets[id] < 40) return groups;
-        const filtered = { ...thumbOffsets, [id]: undefined };
-        const current = thumbOffsets[id];
-        const group = Object.keys(filtered)
-          .filter(key => filtered[key] >= 40)
-          .filter(key => Math.abs(current - filtered[key]) <= 100);
-        if (!group.length) return groups;
-        if (
-          Object.keys(groups).some(key =>
-            isEqual(groups[key].toSorted(), [id, ...group].toSorted())
-          )
-        )
-          return groups;
-        return { ...groups, [Object.keys(groups).length]: [id, ...group] };
-      }, {}),
-    [thumbOffsets]
+  const [resourceThumbs, setResourceThumbs] = useState({});
+
+  const sorted = Object.keys(resourceThumbs).sort(
+    (a, b) => resourceThumbs[a].top - resourceThumbs[b].top
+  );
+
+  const getOverlap = (target, positions) => {
+    const { height, top } = target;
+    const range = top + height + 20;
+    return Object.keys(positions).find(
+      id => Math.abs(range - resourceThumbs[id].top) < height + 20
+    );
+  };
+
+  const getAllOverlaps = (target, positions, group = []) => {
+    if (!target) return null;
+
+    const nextItem = getOverlap(target, positions);
+
+    if (!nextItem) return { group, rest: positions };
+
+    const groupHeight = target.height + positions[nextItem].height + 20;
+
+    const nextPositions = Object.keys(positions)
+      .filter(id => id !== nextItem)
+      .reduce((obj, id) => ({ ...obj, [id]: positions[id] }), {});
+
+    return getAllOverlaps({ ...target, height: groupHeight }, nextPositions, [
+      ...group,
+      nextItem
+    ]);
+  };
+
+  const { remainder, ...groups } = sorted.reduce(
+    (groupsObj, id, i) => {
+      const toCheck = Object.keys(groupsObj.remainder)
+        .filter(key => key !== id)
+        .reduce(
+          (obj, key) => ({ ...obj, [key]: groupsObj.remainder[key] }),
+          {}
+        );
+
+      if (!getOverlap(resourceThumbs[id], toCheck)) return groupsObj;
+
+      const { group, rest } = getAllOverlaps(resourceThumbs[id], toCheck, [id]);
+      return { ...groupsObj, [i]: group, remainder: rest };
+    },
+    { remainder: { ...resourceThumbs } }
   );
 
   /* eslint-disable react-hooks/exhaustive-deps */
@@ -48,9 +75,8 @@ export default function Body(props) {
   return (
     <MarkerContext.Provider
       value={{
-        thumbOffsets,
-        setThumbOffsets,
-        overlaps
+        groups,
+        setResourceThumbs
       }}
     >
       {elements}
