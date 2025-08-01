35781e311dfa09436da775e77a97d0c231540e5b
Jen Wardwell <jen@castironcoding.com>
2025-08-01 13:27:21 -0700

    [C] Move composed components, use default layout

--- client/src/theme/styles/components/frontend/resource/index.js
+++ client/src/theme/styles/components/frontend/resource/index.js
@@ -1,5 +1,9 @@
 import vdsTheme from "./vds_theme";
+import vdsDefaultVideo from "./vds_default_video";
+import vdsDefaultAudio from "./vds_default_audio";
 
 export default `
 ${vdsTheme}
+${vdsDefaultVideo}
+${vdsDefaultAudio}
 `;
