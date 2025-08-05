dd6250e37a25a00e5f52979ce6b3f8e77713ceda
1aurend <imlcdavidson@gmail.com>
2025-08-05 09:47:48 -0700

    [C] Refactor AnnotatableDrawer

--- client/src/global/components/drawer/FrontMatter/index.js
+++ client/src/global/components/drawer/FrontMatter/index.js
@@ -33,9 +33,7 @@ export default function FrontMatter(props) {
             <Styled.Title>
               {icon && <Styled.TitleIcon icon={icon} size={24} />}
               {title && (
-                <Styled.TitleText id={headerId}>
-                  {typeof title === "object" ? t(title.key) : title}
-                </Styled.TitleText>
+                <Styled.TitleText id={headerId}>{t(title)}</Styled.TitleText>
               )}
             </Styled.Title>
           ) : (
