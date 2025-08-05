8b6a070dbcf3e0819cd6b4ac7467a1ad64e0bd93
1aurend <imlcdavidson@gmail.com>
2025-08-05 09:47:48 -0700

    [C] Refactor AnnotatableDrawer

--- client/src/reader/containers/annotation/annotatable-components/Drawers/NewAnnotation.js
+++ client/src/reader/containers/annotation/annotatable-components/Drawers/NewAnnotation.js
@@ -1,39 +1,33 @@
-import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import AnnotationEditor from "global/components/Annotation/Editor/index";
 import AnnotationSelectionWrapper from "global/components/Annotation/Annotation/TextContent/index";
 
-export default class NewAnnotation extends PureComponent {
-  static propTypes = {
-    pendingAnnotation: PropTypes.object.isRequired,
-    actions: PropTypes.object.isRequired
-  };
-
-  saveAnnotation = annotation => {
-    const { pendingAnnotation } = this.props;
+export default function NewAnnotation({ pendingAnnotation, actions }) {
+  const saveAnnotation = annotation => {
     const attributes = {
       ...pendingAnnotation,
       ...annotation.attributes
     };
     const toCreate = { ...pendingAnnotation, attributes };
-    return this.props.actions.createAnnotation(toCreate);
+    return actions.createAnnotation(toCreate);
   };
 
-  render() {
-    const { pendingAnnotation } = this.props;
-
-    return (
-      <div className="annotation-selection">
-        <AnnotationSelectionWrapper
-          selection={pendingAnnotation.subject}
-          annotation={{ attributes: pendingAnnotation }}
-        />
-        <AnnotationEditor
-          cancel={this.props.actions.closeDrawer}
-          annotation={{ attributes: {} }}
-          saveAnnotation={this.saveAnnotation}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="annotation-selection">
+      <AnnotationSelectionWrapper
+        selection={pendingAnnotation.subject}
+        annotation={{ attributes: pendingAnnotation }}
+      />
+      <AnnotationEditor
+        cancel={actions.closeDrawer}
+        annotation={{ attributes: {} }}
+        saveAnnotation={saveAnnotation}
+      />
+    </div>
+  );
 }
+
+NewAnnotation.propTypes = {
+  pendingAnnotation: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired
+};
