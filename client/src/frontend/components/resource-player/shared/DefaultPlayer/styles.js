84da2a269e9e3cd13d73be5de138ab311b209639
Dana Johnson <dana@castironcoding.com>
2025-08-06 13:10:35 -0700

    [B] Few rendering and style fixes

--- client/src/frontend/components/resource-player/shared/DefaultPlayer/styles.js
+++ client/src/frontend/components/resource-player/shared/DefaultPlayer/styles.js
@@ -5,15 +5,4 @@ import { MediaPlayer as BaseMediaPlayer } from "@vidstack/react";
 export const MediaPlayer = styled(BaseMediaPlayer)`
   --color: var(--color-base-neutral-white);
   --hover-color: var(--color-interaction-light);
-
-  --video-bg: var(--color-base-neutral95);
-  --video-font-family: var(--font-family-sans);
-
-  --audio-bg: var(--color-base-neutral95);
-  --audio-font-family: var(--font-family-sans);
-
-  --media-chapter-title-color: var(--color);
-  --media-menu-checkbox-handle-bg: var(--color);
-  --media-menu-checkbox-bg-active: var(--highlight-color);
-  --media-slider-track-bg: hsl(0deg 0% 100% / 50%);
 `;
---
+++ client/src/frontend/components/resource-player/shared/DefaultPlayer/vdsStyles/customTheme.js
@@ -0,0 +1,14 @@
+export default `
+:root {
+  --media-font-family: var(--font-family-sans);
+  --media-chapter-title-color: var(--color-base-neutral-white);
+  --media-menu-checkbox-handle-bg: var(--color-base-neutral-white);
+  --media-slider-track-bg: hsl(0deg 0% 100% / 50%);
+
+  --video-bg: var(--color-base-neutral-black);
+  --video-font-family: var(--font-family-sans);
+
+  --audio-bg: var(--color-base-neutral-black);
+  --audio-font-family: var(--font-family-sans);
+}
+`;
