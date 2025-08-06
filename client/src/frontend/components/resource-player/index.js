55e65de7eb0faacd94aa5bf1356dab05374cec55
Dana Johnson <dana@castironcoding.com>
2025-08-06 12:54:16 -0700

    [C] Switch to iframe for external videos
    Vidstack player for external providers hides provider chrome, so we
    can't use captions provided in those places. There's also an error
    thrown when navigating away from the player that does not yet have a
    resolution. See https://github.com/vidstack/player/discussions/1378.

--- client/src/frontend/components/resource-player/index.js
+++ client/src/frontend/components/resource-player/index.js
@@ -1,9 +1,11 @@
 import Audio from "./Audio";
 import Video from "./Video";
 import Iframe from "./Iframe";
+import ExternalVideo from "./ExternalVideo";
 
 export default {
   Audio,
   Video,
-  Iframe
+  Iframe,
+  ExternalVideo
 };
