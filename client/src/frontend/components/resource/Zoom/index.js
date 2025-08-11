aad3c3ed8c77758d88886123587b0ac88e0df917
Dana Johnson <dana@castironcoding.com>
2025-08-11 12:32:17 -0700

    [F] Build out new Resource components; remove old

---
+++ client/src/frontend/components/resource/Zoom/index.js
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
+// import ResourcePreview from "frontend/components/resource-preview";
+import * as Styled from "./styles";
+
+export default function ResourceListSlideZoom({ resource, label }) {
+  const { t } = useTranslation();
+
+  return null;
+
+  return (
+    <ResourcePreview resource={resource} toggleType="slide">
+      <Styled.ZoomIndicator>
+        <span>{label ?? t("actions.zoom")}</span>
+        <Styled.Icon icon="zoomIn16" size={21.333} />
+      </Styled.ZoomIndicator>
+    </ResourcePreview>
+  );
+}
+
+ResourceListSlideZoom.displayName = "ResourceList.Slide.Zoom";
+
+ResourceListSlideZoom.propTypes = {
+  resource: PropTypes.object.isRequired,
+  label: PropTypes.string
+};
