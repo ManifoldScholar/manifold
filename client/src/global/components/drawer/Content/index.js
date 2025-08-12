5014fe828f54922f69e2c390fd9cf185c1eefb1c
1aurend <imlcdavidson@gmail.com>
2025-08-12 15:20:49 -0700

    [B] Ensure outside click doesn't scroll

--- client/src/global/components/drawer/Content/index.js
+++ client/src/global/components/drawer/Content/index.js
@@ -133,7 +133,8 @@ function DrawerContent(props, ref) {
         clickOutsideDeactivates: handleClickOutside,
         escapeDeactivates: handleEscape,
         returnFocusOnDeactivate,
-        fallbackFocus: ref
+        fallbackFocus: ref,
+        preventScroll: context === "reader"
       }}
     >
       <Drawer key="drawer" {...drawerProps}>
