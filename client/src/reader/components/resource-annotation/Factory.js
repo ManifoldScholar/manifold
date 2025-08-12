f5c7c6cb4ebfe5e6329516b94e859d20a4e28453
1aurend <imlcdavidson@gmail.com>
2025-08-12 12:39:12 -0700

    [B] Add static version of resource marker

--- client/src/reader/components/resource-annotation/Factory.js
+++ client/src/reader/components/resource-annotation/Factory.js
@@ -1,8 +1,18 @@
+import { useContext } from "react";
+import { ReaderContext } from "helpers/contexts";
 import partition from "lodash/partition";
 import ResourceBlock from "./Block";
 import ResourceMarker from "./Marker";
+import StaticMarker from "./Marker/Static";
 
 export default function Factory({ annotations }) {
+  const readerContext = useContext(ReaderContext);
+
+  if (!readerContext)
+    return annotations.map(annotation => (
+      <StaticMarker key={annotation.id} annotation={annotation} />
+    ));
+
   const [blocks, markers] = partition(
     annotations,
     a => a.readerDisplayFormat === "block" || a.readerDisplayFormat === "embed"
