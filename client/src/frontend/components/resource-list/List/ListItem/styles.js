450bce95d0b128df9f5f191b6f3b375ed693a2a1
1aurend <imlcdavidson@gmail.com>
2025-08-12 17:02:03 -0700

    [F] Build initial add resource annotation drawer + lists

--- client/src/frontend/components/resource-list/List/ListItem/styles.js
+++ client/src/frontend/components/resource-list/List/ListItem/styles.js
@@ -3,12 +3,34 @@ import IconComposer from "global/components/utility/IconComposer";
 import { utilityPrimary } from "theme/styles/mixins/typography";
 import { fluidScale } from "theme/styles/mixins/common";
 
+export const ImageWrapper = styled.figure`
+  inline-size: 100px;
+  block-size: 63px;
+  flex-shrink: 0;
+  border-radius: 6px;
+  background-color: var(--color-base-neutral10);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid transparent;
+  transition: border-color 200ms ease;
+`;
+
 export const Title = styled.h3`
   font-family: var(--font-family-sans);
   color: var(--color-base-neutral90);
   margin: 0;
   font-weight: 400;
   font-size: ${fluidScale("18px", "16px")};
+  transition: color 200ms ease;
+`;
+
+const titleUnderline = `
+text-decoration-line: underline;
+text-decoration-style: solid;
+text-decoration-skip-ink: auto;
+text-decoration-thickness: 10.5%;
+text-underline-offset: 25%;
 `;
 
 export const Wrapper = styled.li`
@@ -21,15 +43,22 @@ export const Wrapper = styled.li`
 
   &:hover {
     ${Title} {
-      text-decoration-line: underline;
-      text-decoration-style: solid;
-      text-decoration-skip-ink: auto;
-      text-decoration-thickness: 10.5%;
-      text-underline-offset: 25%;
+      ${titleUnderline}
     }
   }
 
-  ${({ $active }) => $active && `${Title} { color: var(--highlight-color); }`}
+  ${({ $active }) =>
+    $active &&
+    `
+    ${Title} {
+      color: var(--highlight-color);
+      ${titleUnderline}
+    }
+
+    ${ImageWrapper} {
+      border-color: var(--highlight-color);
+    }
+    `}
 `;
 
 export const TextColumn = styled.div`
@@ -70,23 +99,12 @@ export const Date = styled.span`
   }
 `;
 
-export const ImageWrapper = styled.figure`
-  inline-size: 100px;
-  block-size: 63px;
-  flex-shrink: 0;
-  border-radius: 6px;
-  background-color: var(--color-base-neutral10);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
 export const Thumb = styled.img`
   height: 100%;
   width: 100%;
   object-fit: cover;
   object-position: center;
-  border-radius: 6px;
+  border-radius: 4px;
 `;
 
 export const Icon = styled(IconComposer)`
