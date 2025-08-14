d74bbc19a447d0113e3b2c71b262b6cc1913f9b6
1aurend <imlcdavidson@gmail.com>
2025-08-13 18:51:52 -0700

    [F] Build + wire up display format modal

---
+++ client/src/reader/containers/Annotatable/Drawer/types/ResourceAnnotation/DisplaySelectModal/DisplayOption/index.js
@@ -0,0 +1,43 @@
+import { useId, useEffect, useState } from "react";
+import InlineIllustration from "./illustrations/Inline";
+import BlockIllustration from "./illustrations/Block";
+import * as Styled from "./styles";
+
+export default function DisplayOption({
+  value,
+  title,
+  description,
+  defaultChecked,
+  onChange
+}) {
+  const id = useId();
+
+  const Illustration =
+    value === "inline" ? InlineIllustration : BlockIllustration;
+
+  const [animating, setAnimating] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setAnimating(true), 500);
+  }, []);
+
+  return (
+    <Styled.Option htmlFor={`${id}-${value}`}>
+      <Styled.Input
+        id={`${id}-${value}`}
+        type="radio"
+        value={value}
+        name="attributes[readerDisplayFormat]"
+        defaultChecked={defaultChecked}
+        onChange={onChange}
+      />
+      <Styled.Title>{title}</Styled.Title>
+      <Styled.Illustration $animating={animating}>
+        <Illustration />
+      </Styled.Illustration>
+      <Styled.Description $animating={animating}>
+        {description}
+      </Styled.Description>
+    </Styled.Option>
+  );
+}
