cc9ba9511ca002ccc3f912bd75e7fc2f81458c84
1aurend <imlcdavidson@gmail.com>
2025-08-21 10:38:10 -0700

    [F] Handle mobile interactions; clean up components

---
+++ client/src/reader/components/resource-annotation/Marker/Mobile.js
@@ -0,0 +1,86 @@
+import { useState, useCallback, useMemo, useEffect } from "react";
+import IconComposer from "global/components/utility/IconComposer";
+import ThumbnailMobile from "./Thumbnail/Mobile";
+import { useFromStore } from "hooks";
+import * as Styled from "./styles";
+
+export default function MobileMarker({
+  id,
+  icon,
+  handleClick,
+  setActiveAnnotation
+}) {
+  const activeAnnotation = useFromStore(
+    `ui.transitory.reader.activeAnnotation`
+  );
+
+  const [markerEl, setMarkerEl] = useState(null);
+  const [visible, setVisible] = useState(false);
+  const [hoverOverride, setHoverOverride] = useState(false);
+
+  const markerRef = useCallback(node => {
+    if (node !== null) {
+      setMarkerEl(node);
+    }
+  }, []);
+
+  /* eslint-disable react-hooks/exhaustive-deps */
+  const callback = useCallback(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setVisible(true);
+          setActiveAnnotation(id);
+        } else {
+          setVisible(false);
+        }
+      });
+    },
+    [id]
+  );
+  /* eslint-enable react-hooks/exhaustive-deps */
+
+  const observer = useMemo(
+    () =>
+      new IntersectionObserver(callback, {
+        root: null,
+        rootMargin: "-80px 0px -100px 0px",
+        threshold: 1.0
+      }),
+    [callback]
+  );
+
+  useEffect(() => {
+    if (markerEl) {
+      observer.observe(markerEl);
+
+      return () => {
+        observer.disconnect();
+      };
+    }
+  }, [markerEl, observer]);
+
+  const active = id === activeAnnotation && visible;
+
+  return (
+    <>
+      <Styled.MarkerMobile
+        ref={markerRef}
+        data-active={active}
+        data-hover-override={hoverOverride}
+        data-annotation-notation={id}
+        onClick={handleClick}
+        onMouseEnter={() => setHoverOverride(true)}
+        onMouseLeave={() => setHoverOverride(false)}
+      >
+        <IconComposer size={20} icon={icon} />
+      </Styled.MarkerMobile>
+      <ThumbnailMobile
+        id={id}
+        active={hoverOverride || active}
+        hoverOverride={hoverOverride}
+        handleClick={handleClick}
+      />
+    </>
+  );
+}
