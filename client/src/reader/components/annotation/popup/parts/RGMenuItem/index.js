463387345ccb0484921dbbec7d8f138df96101f0
1aurend <imlcdavidson@gmail.com>
2025-08-25 13:19:42 -0700

    [C] Componentize annotation popup styles

--- client/src/reader/components/annotation/popup/parts/RGMenuItem/index.js
+++ client/src/reader/components/annotation/popup/parts/RGMenuItem/index.js
@@ -1,11 +1,10 @@
-import React from "react";
 import PropTypes from "prop-types";
 import isEmpty from "lodash/isEmpty";
 import { MenuItemRadio } from "reakit/Menu";
-import IconComposer from "global/components/utility/IconComposer";
+import * as Styled from "./styles";
 
 function RGMenuItem({ menu, label, onClick, selected, value, privateGroup }) {
-  const Tag = !isEmpty(menu) ? MenuItemRadio : "button";
+  const as = !isEmpty(menu) ? MenuItemRadio : "button";
   const itemProps = !isEmpty(menu)
     ? {
         ...menu,
@@ -21,25 +20,13 @@ function RGMenuItem({ menu, label, onClick, selected, value, privateGroup }) {
       };
 
   return (
-    <Tag {...itemProps} className="annotation-popup-menu-item">
-      <div className="annotation-popup-menu-item__inner">
-        {selected && (
-          <IconComposer
-            icon="checkmark16"
-            size={22}
-            className="annotation-popup-menu-item__selected-icon"
-          />
-        )}
-        <span className="annotation-popup-menu-item__text">{label}</span>
-        {privateGroup && (
-          <IconComposer
-            icon="lock16"
-            size={18}
-            className="annotation-popup-menu-item__private-icon"
-          />
-        )}
-      </div>
-    </Tag>
+    <Styled.RGMenuItem as={as} {...itemProps}>
+      <Styled.Inner>
+        {selected && <Styled.SelectedIcon icon="checkmark16" size={22} />}
+        <Styled.Label>{label}</Styled.Label>
+        {privateGroup && <Styled.PrivateIcon icon="lock16" size={18} />}
+      </Styled.Inner>
+    </Styled.RGMenuItem>
   );
 }
 
