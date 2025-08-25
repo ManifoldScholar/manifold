5ffd1851bdc3cff1a44e459c8a9cd1b575bfc6c6
1aurend <imlcdavidson@gmail.com>
2025-08-25 14:38:57 -0700

    [E] Implement new design for popup main menu

--- client/src/reader/components/annotation/popup/menus/Main/index.js
+++ client/src/reader/components/annotation/popup/menus/Main/index.js
@@ -4,6 +4,7 @@ import { useCurrentUser } from "hooks";
 import PropTypes from "prop-types";
 import Menu from "../../parts/Menu";
 import MenuItems from "./Items";
+import * as Styled from "./styles";
 
 function MainMenu({
   menu,
@@ -46,14 +47,16 @@ function MainMenu({
       direction={direction}
       onKeyDown={onKeyDown}
     >
-      {permitAnnotation && (
-        <>
-          <MenuItems.Annotate {...itemProps} />
-          <MenuItems.Highlight {...itemProps} />
-          <MenuItems.Resource {...itemProps} />
-        </>
-      )}
-      <MenuItems.Share {...itemProps} onClick={() => openSubmenu("share")} />
+      <Styled.Actions>
+        {permitAnnotation && (
+          <>
+            <MenuItems.Annotate {...itemProps} />
+            <MenuItems.Highlight {...itemProps} />
+            <MenuItems.Resource {...itemProps} />
+          </>
+        )}
+        <MenuItems.Share {...itemProps} onClick={() => openSubmenu("share")} />
+      </Styled.Actions>
       <MenuItems.CurrentReadingGroup
         {...itemProps}
         onClick={() => openSubmenu("readingGroup")}
