dc83b0a33a4c2cd6a61d2ba1e51d92522e138da6
1aurend <imlcdavidson@gmail.com>
2025-08-26 09:46:59 -0700

    [C] Add unselectable to resource block styles

--- client/src/reader/components/resource-annotation/Block/styles.js
+++ client/src/reader/components/resource-annotation/Block/styles.js
@@ -1,6 +1,9 @@
 import styled from "@emotion/styled";
+import { unselectable } from "../Marker/Sidebar/styles";
 import Box from "global/components/atomic/Box";
 
 export const Block = styled(Box)`
   margin-block: 1rem;
+
+  ${unselectable}
 `;
