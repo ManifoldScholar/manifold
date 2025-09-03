17e5a03aaa01bd72000dac6037a50f4d4d441452
1aurend <imlcdavidson@gmail.com>
2025-09-02 17:27:29 -0700

    [E] Render resource blocks after parent block node

--- client/src/reader/components/section/body-nodes/helpers/NodeTreeIterator.js
+++ client/src/reader/components/section/body-nodes/helpers/NodeTreeIterator.js
@@ -3,7 +3,10 @@ import Nodes from "../nodes";
 import has from "lodash/has";
 import upperFirst from "lodash/upperFirst";
 import { mathNodeHelpers } from "../nodes/Math";
+import selectionHelpers from "reader/containers/Annotatable/helpers/selectionHelpers";
 import { ErrorBoundary } from "react-error-boundary";
+import ResourceAnnotationFactory from "reader/components/resource-annotation";
+import { formatLocalAnnotations } from "../nodes/Text";
 import * as Styled from "./styles";
 
 const MathError = () => <Styled.Error>MathML error</Styled.Error>;
@@ -30,18 +33,23 @@ export default class NodeTreeIterator {
       this.annotations.push(this.makePendingAnnotation(pendingAnnotation));
     this.annotationsMap = {};
     this.annotationStartMap = {};
-    this.annotationEndMap = {};
+    this.annotationEndMap = { blocks: {} };
     this.annotations.forEach(a => {
       this.annotationsMap[a.id] = a;
-      if (this.annotationStartMap.hasOwnProperty(a.attributes.startNode)) {
-        this.annotationStartMap[a.attributes.startNode].push(a.id);
+      if (a.attributes.readerDisplayFormat === "block") {
+        this.annotationEndMap.blocks[a.attributes.endNode] = [
+          ...(this.annotationEndMap.blocks[a.attributes.endNode] ?? []),
+          a.id
+        ];
       } else {
-        this.annotationStartMap[a.attributes.startNode] = [a.id];
-      }
-      if (this.annotationEndMap.hasOwnProperty(a.attributes.endNode)) {
-        this.annotationEndMap[a.attributes.endNode].push(a.id);
-      } else {
-        this.annotationEndMap[a.attributes.endNode] = [a.id];
+        this.annotationStartMap[a.attributes.startNode] = [
+          ...(this.annotationStartMap[a.attributes.startNode] ?? []),
+          a.id
+        ];
+        this.annotationEndMap[a.attributes.endNode] = [
+          ...(this.annotationEndMap[a.attributes.endNode] ?? []),
+          a.id
+        ];
       }
     });
     this.openAnnotations = { isDetail };
@@ -84,7 +92,7 @@ export default class NodeTreeIterator {
     }
   }
 
-  visitElementNode(node, mathUuids, blacklist) {
+  visitElementNode(node, mathUuids, blacklist, resourceBlocks) {
     if (blacklist && blacklist[node.tag]) return <p>{blacklist[node.tag]}</p>;
     let ComponentClass = Nodes.Default;
     const lookup = upperFirst(node.tag);
@@ -101,10 +109,19 @@ export default class NodeTreeIterator {
         </ErrorBoundary>
       );
     }
-    return React.createElement(
-      ComponentClass,
-      node,
-      this.visitChildren(node, blacklist)
+    return (
+      <>
+        <ComponentClass {...node}>
+          {this.visitChildren(node, blacklist)}
+        </ComponentClass>
+        {!!resourceBlocks?.length && (
+          <ResourceAnnotationFactory
+            annotations={formatLocalAnnotations(
+              resourceBlocks.map(block => this.annotationsMap[block])
+            )}
+          />
+        )}
+      </>
     );
   }
 
@@ -160,6 +177,18 @@ export default class NodeTreeIterator {
     }
   }
 
+  maybeAppendResources(node) {
+    if (!Object.keys(this.annotationEndMap.blocks)?.length) return [];
+    const uuids = mathNodeHelpers.getUuids(
+      node.children.filter(c => !selectionHelpers.blockRegex.test(c.tag))
+    );
+    const blocks = new Set(Object.keys(this.annotationEndMap.blocks));
+    return uuids
+      .filter(id => blocks.has(id))
+      .map(id => this.annotationEndMap.blocks[id])
+      .flat();
+  }
+
   visit(node, parent = null, blacklist) {
     let mathUuids;
     if (node.tag === "math") {
@@ -197,9 +226,18 @@ export default class NodeTreeIterator {
     };
     let out;
 
+    const resourceBlocks = selectionHelpers.blockRegex.test(node.tag)
+      ? this.maybeAppendResources(node)
+      : [];
+
     switch (node.nodeType) {
       case "element":
-        out = this.visitElementNode(adjustedNode, mathUuids, blacklist);
+        out = this.visitElementNode(
+          adjustedNode,
+          mathUuids,
+          blacklist,
+          resourceBlocks
+        );
         break;
       case "text":
         out = this.visitTextNode(adjustedNode, parent);
