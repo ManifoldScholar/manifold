d5795437bdcfdc8ab7f26d9a6bbc501ccdcc80be
Zach Davis <zach@castironcoding.com>
2025-09-04 08:34:41 -0700

    [B] Avoid duplicate reindex in v9 upgrade

--- api/app/services/system_upgrades/upgrades/manifold090000.rb
+++ api/app/services/system_upgrades/upgrades/manifold090000.rb
@@ -5,14 +5,8 @@ module SystemUpgrades
     class Manifold090000 < SystemUpgrades::AbstractVersion
       def perform!
         logger.info("[-ANY-]===================================================================")
-        logger.info("[-ANY-] Rebuilding Search Indices for v9                                  ")
+        logger.info("[-ANY-] Upgrading Manifold to v9                                          ")
         logger.info("[-ANY-]===================================================================")
-        logger.info("[-ANY-]V9 replaces ElasticSearch with Postgres seach, so all indexes must ")
-        logger.info("[-ANY-]be rebuilt. This may take a few minutes, so now is a good time to  ")
-        logger.info("[-ANY-]make that cup of tea.                                              ")
-        logger.info("[-ANY-]===================================================================")
-
-        ManifoldApi::Container["search.rebuild_all"].().value!
       end
     end
   end
