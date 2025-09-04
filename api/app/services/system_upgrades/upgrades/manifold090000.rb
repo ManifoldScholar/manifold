dd29c62ae254b2815f10bbe657aa2c8ff2dc4e30
Zach Davis <zach@castironcoding.com>
2025-09-04 10:56:51 -0700

    [B] Remove orphaned text sections in v9 upgrade

--- api/app/services/system_upgrades/upgrades/manifold090000.rb
+++ api/app/services/system_upgrades/upgrades/manifold090000.rb
@@ -4,9 +4,20 @@ module SystemUpgrades
   module Upgrades
     class Manifold090000 < SystemUpgrades::AbstractVersion
       def perform!
-        logger.info("[-ANY-]===================================================================")
-        logger.info("[-ANY-] Upgrading Manifold to v9                                          ")
-        logger.info("[-ANY-]===================================================================")
+        remove_orphaned_text_sections!
+      end
+
+      private
+
+      def remove_orphaned_text_sections!
+        logger.info("===================================================================")
+        logger.info("Remove Orphaned Text Sections                                      ")
+        logger.info("===================================================================")
+        logger.info("In earlier versions of Manifold, it was possible to delete a text  ")
+        logger.info("without deleting associated text sections. We can safely delete any")
+        logger.info("text sections that do not have a corresponding text.               ")
+        logger.info("===================================================================")
+        TextSection.where.not(text_id: nil).where.not(text_id: Text.select(:id)).destroy_all
       end
     end
   end
