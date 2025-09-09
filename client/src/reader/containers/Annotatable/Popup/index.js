2328ba00d4f10b47eb736cc67cb815a5f3d16081
1aurend <imlcdavidson@gmail.com>
2025-09-09 09:08:56 -0700

    [B] Increase fragment generation timeout; don't block setting state

--- client/src/reader/containers/Annotatable/Popup/index.js
+++ client/src/reader/containers/Annotatable/Popup/index.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import PrimaryMenu from "./PrimaryMenu";
 import LinkMenu from "./LinkMenu";
@@ -6,6 +6,7 @@ import LoginMenu from "./LoginMenu";
 import HighlightMenu from "./HighlightMenu";
 import Authorize from "hoc/Authorize";
 import usePositioner from "./usePositioner";
+import { generateFragmentFromRange } from "../helpers/text-fragments-polyfill/fragment-generation-utils";
 import * as Styled from "./styles";
 
 export default function AnnotatablePopup(props) {
@@ -31,6 +32,24 @@ export default function AnnotatablePopup(props) {
     annotatableRef
   });
 
+  const [textFragment, setFragment] = useState("");
+
+  useEffect(() => {
+    if (selectionState.selection?.endRange) {
+      try {
+        const fragment = generateFragmentFromRange(
+          selectionState.selection?.allRanges[
+            selectionState.selection.allRanges.length - 1
+          ]
+        );
+        setFragment(fragment);
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.log("There was a text fragment generation error: %o", err);
+      }
+    }
+  }, [selectionState]);
+
   return (
     <Styled.Popup ref={setPopupRef} style={style}>
       <Authorize kind="unauthenticated">
@@ -45,6 +64,7 @@ export default function AnnotatablePopup(props) {
         ) : (
           <PrimaryMenu
             {...props}
+            selectionState={{ ...props.selectionState, textFragment }}
             locked={props.annotationState === "locked"}
             direction={direction}
             visible
