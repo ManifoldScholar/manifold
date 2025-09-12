66b9d108733715b0422ca5337fd706cb5a0673a6
1aurend <imlcdavidson@gmail.com>
2025-09-12 12:47:29 -0700

    [C] Remove back to button from dialog

--- client/src/reader/components/resource-annotation/Dialog/Header.js
+++ client/src/reader/components/resource-annotation/Dialog/Header.js
@@ -2,15 +2,11 @@ import { useTranslation } from "react-i18next";
 import IconComposer from "global/components/utility/IconComposer";
 import * as Styled from "./styles";
 
-export default function Header({ textTitle, onClose }) {
+export default function Header({ onClose }) {
   const { t } = useTranslation();
 
   return (
     <Styled.Header>
-      <Styled.HeaderButton onClick={onClose}>
-        <IconComposer icon="arrowLeft16" size={14} />
-        <span className="overlay-close__text">Back to {textTitle}</span>
-      </Styled.HeaderButton>
       <Styled.CloseButton onClick={onClose}>
         <Styled.CloseText className="overlay-close__text">
           {t("actions.close")}
