2c8006598429bc2f45e6e3dbaafac40cd434cf93
Dana Johnson <dana@castironcoding.com>
2025-09-23 15:13:51 -0700

    [F] Implement resource annotation dialog UI

--- client/src/reader/components/resource-annotation/Dialog/Header.js
+++ client/src/reader/components/resource-annotation/Dialog/Header.js
@@ -2,11 +2,16 @@ import { useTranslation } from "react-i18next";
 import IconComposer from "global/components/utility/IconComposer";
 import * as Styled from "./styles";
 
-export default function Header({ onClose }) {
+export default function Header({ title, headingId, onClose }) {
   const { t } = useTranslation();
 
   return (
     <Styled.Header>
+      {title && (
+        <h2 id={headingId} className="screen-reader-text">
+          {title}
+        </h2>
+      )}
       <Styled.CloseButton onClick={onClose}>
         <Styled.CloseText className="overlay-close__text">
           {t("actions.close")}
