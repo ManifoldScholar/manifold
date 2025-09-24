e17cbba1c72ad1129ae1cb4b2e719d0f06140635
1aurend <imlcdavidson@gmail.com>
2025-09-24 15:15:22 -0700

    feat: modernize authorization UX
    [B] Add allow partial option to useFromStore
    
    [B] Even more useFromStore migrations
    
    [E] Redirect to login on unauthorized error
    
    [B] Fix formatting on restricted access warning
    
    [E] Handle redirect to login for client-side project redirects
    
    [E] Handle redirect param in Manifold Container
    
    [E] Update login to show unauthorized message
    
    [C] Remove unused prop + notification
    
    [B] Update useFetch auth dep to avoid refetching on logout
    
    [C] Revert logout check; now handled by useFetch
    
    [E] Switch login link to overlay button on resource detail
    
    [E] Add no dismiss option to notifications
    
    [F] Split fatal error presentation from routing; handle logged in users
    
    [B] Correct shouldNotify check in login container
    
    [F] Replace auth fatal error w/ notification
    
    [B] Fix regressions
    
    [E] Refactor Authorize hoc
    
    [B][C] Small fix + clean up
    
    [F] Render error message instead of redirecting if 403
    
    [C] Remove project-specific redirect
    
    [E] Improve user message content
    
    [E] Update user message for `POST` requests
    
    [B][E] Ensure all admin routes check auth
    
    [B] Correct resource comment message for unverified users
    
    [B] Hide reply button for unverified users
    
    [E] Add unverified warning to annotation drawer
    
    [E] Update style of contained FatalError
    
    [E] Redirect to dashboard when entering admin from 403
    
    [B] Fix double login redirect regression

--- client/src/global/components/navigation/helpers.js
+++ client/src/global/components/navigation/helpers.js
@@ -108,7 +108,10 @@ const extractIdentifier = (pathname, basePath) => {
   return identifier === "all" ? null : identifier;
 };
 
-const getAdminPath = pathname => {
+const getAdminPath = (pathname, fatalError) => {
+  if (fatalError && fatalError.type === "AUTHORIZATION")
+    return "/backend/dashboard";
+
   const routeKey = Object.keys(FE_ROUTE_MAP).find(key => {
     return FE_ROUTE_MAP[key]?.regex.test(pathname);
   });
@@ -184,7 +187,12 @@ const getFrontendPath = (pathname, entities) => {
   return `${route.link}/${identifier}`;
 };
 
-export const getDestinationPath = ({ mode, pathname, entities }) => {
-  if (mode === "frontend") return getAdminPath(pathname);
+export const getDestinationPath = ({
+  mode,
+  pathname,
+  entities,
+  fatalError
+}) => {
+  if (mode === "frontend") return getAdminPath(pathname, fatalError);
   return getFrontendPath(pathname, entities);
 };
