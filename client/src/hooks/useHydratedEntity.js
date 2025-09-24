7ec31f7c2dbfc799b202aa88311adfacc1159650
1aurend <imlcdavidson@gmail.com>
2025-09-24 15:16:03 -0700

    [C] Remove useHydratedEntity

--- client/src/hooks/useHydratedEntity.js
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-import { grab } from "utils/entityUtils";
-
-export default function useHydratedEntity(type, id) {
-  return useSelector(state => {
-    return grab(type, id, state.entityStore, true);
-  });
-}
