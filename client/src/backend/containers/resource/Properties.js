fff52014d6798aa16469f74a763b261075c12ccc
Dana Johnson <dana@castironcoding.com>
2025-09-26 17:02:15 -0700

    [C] Reorganize BE resource form

--- client/src/backend/containers/resource/Properties.js
+++ client/src/backend/containers/resource/Properties.js
@@ -64,74 +64,92 @@ export class ResourcePropertiesContainer extends PureComponent {
           formatData={this.formatData}
           className="form-secondary"
         >
-          <Resource.Form.KindPicker name="attributes[kind]" includeButtons />
-          <Form.TextInput
-            label={t("resources.title_label")}
-            name="attributes[title]"
-            placeholder={t("resources.title_placeholder")}
-            instructions={t("resources.properties.title_instructions")}
-          />
-          <Form.Switch
-            label={t("resources.properties.sort_order_label")}
-            name="attributes[sortOrder]"
-            value={sortOrderBool}
-            placeholder={t("resources.properties.sort_order_placeholder")}
-            instructions={t("resources.properties.sort_order_instructions")}
-          />
-          <Form.TextInput
-            label={t("resources.properties.sort_title_label")}
-            name="attributes[pendingSortTitle]"
-            placeholder={t("resources.properties.sort_title_placeholder")}
-            instructions={t("resources.properties.sort_title_instructions")}
-          />
-          <Form.TextInput
-            label={t("resources.properties.fingerprint_label")}
-            name="attributes[fingerprint]"
-            placeholder={t("resources.properties.fingerprint_placeholder")}
-            instructions={t("resources.properties.fingerprint_instructions")}
-          />
-          <Form.TextInput
-            label={t("resources.properties.slug_label")}
-            name="attributes[pendingSlug]"
-            placeholder={t("resources.properties.slug_placeholder")}
-          />
-          <Form.Picker
-            label={t("resources.properties.tags_label")}
-            listStyle="well"
-            listRowComponent="StringRow"
-            name="attributes[tagList]"
-            placeholder={t("resources.properties.tags_placeholder")}
-            options={tagsAPI.index}
-            optionToLabel={tag => tag.attributes.name}
-            optionToValue={tag => tag.attributes.name}
-            allowNew
-          />
-          <Form.Upload
-            layout="square"
-            label={t("resources.properties.thumbnail")}
-            accepts="images"
-            readFrom="attributes[variantThumbnailStyles][small]"
-            name="attributes[variantThumbnail]"
-            remove="attributes[removeVariantThumbnail]"
-            instructions={t("resources.properties.thumbnail_instructions")}
-          />
-          <Form.TextArea
-            label={t("resources.descript_label")}
-            name="attributes[description]"
-            placeholder={t("resources.descript_placeholder")}
-          />
-          <Form.TextArea
-            label={t("resources.properties.caption_label")}
-            name="attributes[caption]"
-            placeholder={t("resources.properties.caption_placeholder")}
-          />
-          {resource.attributes.downloadableKind ? (
-            <Form.Switch
-              label={t("resources.properties.download_label")}
-              name="attributes[allowDownload]"
+          <Form.FieldGroup label={t("projects.forms.properties.header")}>
+            <Resource.Form.KindPicker name="attributes[kind]" includeButtons />
+            <Form.TextInput
+              label={t("resources.title_label")}
+              name="attributes[title]"
+              placeholder={t("resources.title_placeholder")}
+              instructions={t("resources.properties.title_instructions")}
+              wide
             />
-          ) : null}
-          <Resource.Form.KindAttributes />
+            <Form.TextInput
+              label={t("resources.properties.sort_title_label")}
+              name="attributes[pendingSortTitle]"
+              placeholder={t("resources.properties.sort_title_placeholder")}
+              instructions={t("resources.properties.sort_title_instructions")}
+              wide
+            />
+            <Form.TextInput
+              label={t("resources.properties.slug_label")}
+              name="attributes[pendingSlug]"
+              placeholder={t("resources.properties.slug_placeholder")}
+              wide
+            />
+            <Form.TextArea
+              label={t("resources.descript_label")}
+              name="attributes[description]"
+              placeholder={t("resources.descript_placeholder")}
+              wide
+            />
+            <Form.TextArea
+              label={t("resources.properties.caption_label")}
+              name="attributes[caption]"
+              placeholder={t("resources.properties.caption_placeholder")}
+              wide
+            />
+            <Form.TextInput
+              label={t("resources.properties.fingerprint_label")}
+              name="attributes[fingerprint]"
+              placeholder={t("resources.properties.fingerprint_placeholder")}
+              instructions={t("resources.properties.fingerprint_instructions")}
+              wide
+            />
+            <Resource.Form.KindAttributes wide />
+            {resource.attributes.downloadableKind ? (
+              <Form.Switch
+                label={t("resources.properties.download_label")}
+                name="attributes[allowDownload]"
+                wide
+              />
+            ) : null}
+          </Form.FieldGroup>
+          <Form.FieldGroup label={t("resources.properties.thumbnail")}>
+            <Form.Upload
+              layout="square"
+              label={t("resources.properties.thumbnail")}
+              accepts="images"
+              readFrom="attributes[variantThumbnailStyles][small]"
+              name="attributes[variantThumbnail]"
+              remove="attributes[removeVariantThumbnail]"
+            />
+          </Form.FieldGroup>
+          <Form.FieldGroup
+            label={t("projects.forms.properties.taxonomy_header")}
+          >
+            <Form.Picker
+              label={t("resources.properties.tags_label")}
+              listStyle="well"
+              listRowComponent="StringRow"
+              name="attributes[tagList]"
+              placeholder={t("resources.properties.tags_placeholder")}
+              options={tagsAPI.index}
+              optionToLabel={tag => tag.attributes.name}
+              optionToValue={tag => tag.attributes.name}
+              allowNew
+            />
+          </Form.FieldGroup>
+          <Form.FieldGroup
+            label={t("projects.forms.properties.presentation_header")}
+          >
+            <Form.Switch
+              label={t("resources.properties.sort_order_label")}
+              name="attributes[sortOrder]"
+              value={sortOrderBool}
+              placeholder={t("resources.properties.sort_order_placeholder")}
+              instructions={t("resources.properties.sort_order_instructions")}
+            />
+          </Form.FieldGroup>
           <Form.Save text={t("resources.properties.save")} />
         </FormContainer.Form>
       </section>
