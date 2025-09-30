ae81cbcf07cdeb178a55f4bc6fb891415be14ef2
1aurend <imlcdavidson@gmail.com>
2025-09-29 17:01:39 -0700

    [E] Redirect to login on unauthorized error

--- client/src/global/components/FatalError/index.js
+++ client/src/global/components/FatalError/index.js
@@ -1,84 +1,54 @@
-import { PureComponent } from "react";
 import PropTypes from "prop-types";
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { withTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import { Global as GlobalStyles } from "@emotion/react";
 import has from "lodash/has";
 import config from "config";
-import { fatalErrorActions, notificationActions } from "actions";
+import { notificationActions } from "actions";
 import lh from "helpers/linkHandler";
 import styles from "theme/styles/globalStyles";
 import ApiTrace from "./ApiTrace";
 import ClientTrace from "./ClientTrace";
 import * as Styled from "./styles";
 
-class FatalError extends PureComponent {
-  static propTypes = {
-    fatalError: PropTypes.shape({
-      error: PropTypes.object,
-      type: PropTypes.string
-    }).isRequired,
-    headerLineOne: PropTypes.string,
-    headerLineTwo: PropTypes.string,
-    dismiss: PropTypes.func,
-    dispatch: PropTypes.func,
-    i18n: PropTypes.object
+export default function FatalError(props) {
+  const {
+    fatalError: { error },
+    headerLineOne,
+    headerLineTwo,
+    dismiss,
+    redirectPath,
+    dispatch
+  } = props;
+
+  const { t } = useTranslation();
+  const history = useHistory();
+
+  if (!error) return null;
+
+  const defaultHeaders = {
+    headerLineOne: t("errors.fatal.heading_line_one"),
+    headerLineTwo: t("errors.fatal.heading_line_two")
   };
 
-  get defaultHeaders() {
-    const t = this.props.i18n.t;
-    return {
-      headerLineOne: t("errors.fatal.heading_line_one"),
-      headerLineTwo: t("errors.fatal.heading_line_two")
-    };
-  }
+  const isAuthorizationError =
+    error.method === "GET" && (error.status === 403 || error.status === 401);
 
-  get error() {
-    const { error } = this.props.fatalError;
-    return error;
-  }
+  const isProjectAuthorizationError =
+    isAuthorizationError && has(error, "project.id");
 
-  get type() {
-    const { type } = this.props.fatalError;
-    return type;
-  }
+  const shouldRedirectToLogin =
+    isAuthorizationError && !isProjectAuthorizationError;
 
-  get isAuthorizationError() {
-    return this.error && this.type === fatalErrorActions.types.authorization;
-  }
-
-  get isProjectAuthorizationError() {
-    if (!this.isAuthorizationError) return false;
-    if (has(this.error, "method") && this.error.method !== "GET") return false;
-    return has(this.error, "project.id");
-  }
-
-  get apiTrace() {
-    if (!this.error || !this.error.apiTrace) return null;
-    return this.error.apiTrace;
-  }
-
-  get clientTrace() {
-    if (!this.error || !this.error.clientTrace) return null;
-    return this.error.clientTrace;
-  }
-
-  get clientTraceTruncate() {
-    if (!this.error || !this.error.clientTraceTruncate) return null;
-    return this.error.clientTraceTruncate;
-  }
-
-  renderProjectAuthorizationRedirect() {
-    const url = lh.link("frontendProjectDetail", this.error.project.slug);
-    if (this.props.dispatch) {
-      this.props.dispatch(
+  const renderProjectAuthorizationRedirect = () => {
+    const url = lh.link("frontendProjectDetail", error.project.slug);
+    if (dispatch) {
+      dispatch(
         notificationActions.addNotification({
           id: "projectAuthorizationError",
           level: 1,
-          heading: this.props.t(
-            "messages.project_authorization_warning.heading"
-          )
+          heading: t("messages.project_authorization_warning.heading")
         })
       );
     }
@@ -87,84 +57,104 @@ class FatalError extends PureComponent {
       <Redirect
         to={{
           pathname: url,
-          state: { projectAuthorizationError: this.error }
+          state: { projectAuthorizationError: error }
         }}
       />
     );
-  }
+  };
 
-  render() {
-    if (!this.props.fatalError) return null;
-    if (this.isProjectAuthorizationError)
-      return this.renderProjectAuthorizationRedirect();
-    const { error } = this.props.fatalError;
-    const showDetail = config.environment.isDevelopment;
-    const t = this.props.i18n.t;
-
+  const renderLoginRedirect = () => {
     return (
-      <HelmetProvider>
-        <Helmet title={`${error.status} Error: ${error.heading}`} />
-        <GlobalStyles styles={styles} />
-        <Styled.Body className="browse">
-          <Styled.Wrapper>
-            <Styled.Inner>
-              <Styled.Container>
-                <header>
-                  <Styled.Icon icon="stopSign64" size={52} />
-                  <Styled.Message>
-                    {this.props.headerLineOne ??
-                      this.defaultHeaders.headerLineOne}
-                    {this.props.headerLineTwo !== null && (
-                      <>
-                        <br />
-                        {this.props.headerLineTwo ??
-                          this.defaultHeaders.headerLineTwo}
-                      </>
-                    )}
-                  </Styled.Message>
-                </header>
-                <div role="alert" aria-live="assertive" aria-atomic="true">
-                  {error ? (
-                    <Styled.ErrorTitle>
-                      {error.status} Error: {error.heading}
-                    </Styled.ErrorTitle>
-                  ) : null}
-                  {showDetail ? (
-                    <Styled.ErrorBody>
-                      {error.body}
-                      {this.props.dismiss ? (
-                        <span>
-                          <br />
-                          <Styled.Link
-                            role="link"
-                            onClick={this.props.dismiss}
-                            className="dismiss"
-                          >
-                            {t("errors.fatal.dismiss_link")}
-                          </Styled.Link>
-                        </span>
-                      ) : null}
-                    </Styled.ErrorBody>
-                  ) : null}
-                </div>
-              </Styled.Container>
-              {showDetail ? (
-                <div>
-                  {this.apiTrace ? <ApiTrace trace={this.apiTrace} /> : null}
-                  {this.clientTrace ? (
-                    <ClientTrace
-                      trace={this.clientTrace}
-                      truncate={this.clientTraceTruncate}
-                    />
-                  ) : null}
-                </div>
-              ) : null}
-            </Styled.Inner>
-          </Styled.Wrapper>
-        </Styled.Body>
-      </HelmetProvider>
+      <Redirect
+        to={{
+          pathname: "/login",
+          search: redirectPath ? `?redirect_uri=${redirectPath}` : undefined
+        }}
+      />
     );
+  };
+
+  /* Ensure we don't attempt to render router components outside the router context.
+     Redirect to login when an error is caught in SSR and this component is rendered
+     outside of the App Container is handled separately in entry-ssr.js.
+  */
+  if (history) {
+    if (isProjectAuthorizationError)
+      return renderProjectAuthorizationRedirect();
+    if (shouldRedirectToLogin) return renderLoginRedirect();
   }
+
+  return (
+    <HelmetProvider>
+      <Helmet title={`${error.status} Error: ${error.heading}`} />
+      <GlobalStyles styles={styles} />
+      <Styled.Body className="browse">
+        <Styled.Wrapper>
+          <Styled.Inner>
+            <Styled.Container>
+              <header>
+                <Styled.Icon icon="stopSign64" size={52} />
+                <Styled.Message>
+                  {headerLineOne ?? defaultHeaders.headerLineOne}
+                  {headerLineTwo !== null && (
+                    <>
+                      <br />
+                      {headerLineTwo ?? defaultHeaders.headerLineTwo}
+                    </>
+                  )}
+                </Styled.Message>
+              </header>
+              <div role="alert" aria-live="assertive" aria-atomic="true">
+                {error ? (
+                  <Styled.ErrorTitle>
+                    {error.status} Error: {error.heading}
+                  </Styled.ErrorTitle>
+                ) : null}
+                {config.environment.isDevelopment ? (
+                  <Styled.ErrorBody>
+                    {error.body}
+                    {dismiss ? (
+                      <span>
+                        <br />
+                        <Styled.Link
+                          role="link"
+                          onClick={dismiss}
+                          className="dismiss"
+                        >
+                          {t("errors.fatal.dismiss_link")}
+                        </Styled.Link>
+                      </span>
+                    ) : null}
+                  </Styled.ErrorBody>
+                ) : null}
+              </div>
+            </Styled.Container>
+            {config.environment.isDevelopment ? (
+              <div>
+                {error.apiTrace ? <ApiTrace trace={error.apiTrace} /> : null}
+                {error.clientTrace ? (
+                  <ClientTrace
+                    trace={error.clientTrace}
+                    truncate={error.clientTraceTruncate}
+                  />
+                ) : null}
+              </div>
+            ) : null}
+          </Styled.Inner>
+        </Styled.Wrapper>
+      </Styled.Body>
+    </HelmetProvider>
+  );
 }
 
-export default withTranslation()(FatalError);
+FatalError.propTypes = {
+  fatalError: PropTypes.shape({
+    error: PropTypes.object,
+    type: PropTypes.string
+  }).isRequired,
+  headerLineOne: PropTypes.string,
+  headerLineTwo: PropTypes.string,
+  dismiss: PropTypes.func,
+  redirectPath: PropTypes.string,
+  dispatch: PropTypes.func
+};
