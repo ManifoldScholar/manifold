227e17c64601a215ff5adc6ef0d19074721df767
1aurend <imlcdavidson@gmail.com>
2025-09-29 20:47:47 -0700

    [E] Update login to show unauthorized message

--- client/src/frontend/containers/Login/index.js
+++ client/src/frontend/containers/Login/index.js
@@ -1,12 +1,38 @@
-import React from "react";
+import { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 import withProjectContext from "hoc/withProjectContext";
 import SignInUp from "global/components/sign-in-up";
-import { useRouteMatch } from "react-router-dom";
+import { useRouteMatch, useLocation, useHistory } from "react-router-dom";
+import { useNotification } from "hooks";
 import * as Styled from "./styles";
 
 function LoginContainer({ projectBackLink }) {
+  const { t } = useTranslation();
   const isSignUp = useRouteMatch("/signup");
+  const location = useLocation();
+  const history = useHistory();
+
+  const notifiedRef = useRef(false);
+
+  const notifyUnauthorized = useNotification(() => ({
+    id: "authorizationError",
+    level: 2,
+    heading: "Access Denied",
+    body: t("errors.authorization"),
+    scope: "authentication"
+  }));
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const redirectUri = searchParams.get("redirect_uri");
+    const shouldNotify = !!redirectUri || !location.state?.postLoginRedirect;
+
+    if (shouldNotify && !notifiedRef.current) {
+      notifyUnauthorized();
+      notifiedRef.current = true;
+    }
+  }, [location, history, notifyUnauthorized]);
 
   return (
     <Styled.Section className="bg-neutral05">
