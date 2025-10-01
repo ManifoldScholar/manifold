315c8cf10febfab75d5dc1481adffbec33671b8b
1aurend <imlcdavidson@gmail.com>
2025-10-01 11:40:18 -0700

    feat: add color picker; improve theme previews
    [F] Add simple color input
    
    [F] Add header preview component
    
    [E] Add inert to header preview
    
    [E] Fix preview markup; add default colors
    
    [B] Fix swapped defaults
    
    [F] Implement Coloris color picker
    
    [B] Ensure color input can render in drawer
    
    [B] Ensure preview attrs are shown for feature
    
    [E] Update color input instruction copy
    
    [B] Update color picker focus color
    
    [E] Prevent Enter in picker from submitting form
    
    [C] Fix case-sensitivity in imports
    
    [E] Small color picker UI changes

--- client/src/global/components/form/ContentEditor/components/controls/buttons/insert/browse/BrowseList.js
+++ client/src/global/components/form/ContentEditor/components/controls/buttons/insert/browse/BrowseList.js
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 import EntitiesList from "backend/components/list/EntitiesList";
-import EditorAsset from "backend/components/list/EntitiesList/entity/AssetRow/Editor";
+import EditorAsset from "backend/components/list/EntitiesList/Entity/AssetRow/Editor";
 import * as Styled from "./styles";
 
 export default function BrowseList({
