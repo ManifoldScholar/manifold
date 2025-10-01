2ac9f87d20672559ca42c4ad39c02ec3ebcb5fb2
1aurend <imlcdavidson@gmail.com>
2025-10-01 11:40:18 -0700

    feat: add color picker; improve theme previews
    [F] Add simple color input
    
    [F] Add header preview component
    
    [E] Add inert to header preview
    
    [E] Fix preview markup; add default colors
    
    [B] Fix swapped defaults
    
    [F] Implement Coloris color picker
    
    [B] Ensure color input can render in drawer
    
    [B] Ensure preview attrs are shown for feature
    
    [E] Update color input instruction copy
    
    [B] Update color picker focus color
    
    [E] Prevent Enter in picker from submitting form
    
    [C] Fix case-sensitivity in imports
    
    [E] Small color picker UI changes

---
+++ client/src/backend/components/theme/HeaderPreview/index.js
@@ -0,0 +1,103 @@
+import { useFromStore } from "hooks";
+import { useTranslation } from "react-i18next";
+import SetCSSProperty from "global/components/utility/SetCSSProperty";
+import PressLogo from "global/components/PressLogo";
+import HeaderLogo from "global/components/atomic/HeaderLogo";
+import IconComposer from "global/components/utility/IconComposer";
+
+const DEFAULTS = {
+  accentColor: "#52e3ac",
+  foregroundColor: "#696969",
+  backgroundColor: "#ffffff",
+  activeColor: "#363636"
+};
+
+export default function HeaderPreview({
+  accentColor,
+  foregroundColor,
+  backgroundColor,
+  activeColor
+}) {
+  const { t } = useTranslation();
+  const settings = useFromStore({ requestKey: "settings", action: "select" });
+  const offset = settings.attributes.theme.headerOffset;
+  const navStyle = offset
+    ? { position: "relative", top: parseInt(offset, 10) }
+    : {};
+
+  return (
+    <div
+      className="library-header library-header--light"
+      style={{
+        "--color-accent-primary": accentColor || DEFAULTS.accentColor,
+        "--hover-color": accentColor || DEFAULTS.accentColor,
+        "--color-header-background":
+          backgroundColor || DEFAULTS.backgroundColor,
+        "--color-header-foreground":
+          foregroundColor || DEFAULTS.foregroundColor,
+        "--color-header-foreground-active": activeColor || DEFAULTS.activeColor
+      }}
+      inert=""
+    >
+      <SetCSSProperty
+        measurement="height"
+        propertyName="--library-header-height"
+      >
+        <div className="library-header__inner-preview">
+          <HeaderLogo as="span">
+            <>
+              <span className="screen-reader-text">
+                {t("navigation.return_home")}
+              </span>
+              <PressLogo
+                url={settings.attributes.pressLogoStyles.medium}
+                styles={settings.attributes.theme.logoStyles}
+                aria-hidden="true"
+              />
+            </>
+          </HeaderLogo>
+          <nav
+            className="site-nav show-82"
+            aria-label={t("navigation.primary")}
+            style={navStyle}
+          >
+            <ul className="site-nav__list">
+              <li className="site-nav__item">
+                <span className="site-nav__link site-nav__link--active">
+                  {t("titles.home")}
+                </span>
+              </li>
+              <li className="site-nav__item">
+                <span className="site-nav__link">
+                  {t("glossary.project_title_case_other")}
+                </span>
+              </li>
+              <li className="site-nav__item">
+                <span className="site-nav__link">
+                  {t("glossary.journal_title_case_other")}
+                </span>
+              </li>
+              <li className="site-nav__item">
+                <span className="site-nav__link">
+                  {t("glossary.reading_group_title_case_other")}
+                </span>
+              </li>
+            </ul>
+          </nav>
+          <nav className="breadcrumb-list hide-82">
+            <span>
+              <span className="breadcrumb-list__link">{t("titles.home")}</span>
+            </span>
+          </nav>
+          <span className="mobile-nav-toggle hide-82">
+            <IconComposer
+              icon="menu32"
+              size="default"
+              className="mobile-nav-trigger__icon"
+            />
+          </span>
+        </div>
+      </SetCSSProperty>
+    </div>
+  );
+}
