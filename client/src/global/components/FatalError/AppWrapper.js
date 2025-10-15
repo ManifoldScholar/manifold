7a8330daced670fd637448c69b28b5c2e92590da
1aurend <imlcdavidson@gmail.com>
2025-10-15 11:20:46 -0700

    [C] Remove project-specific redirect

--- client/src/global/components/FatalError/AppWrapper.js
+++ client/src/global/components/FatalError/AppWrapper.js
@@ -1,10 +1,7 @@
-import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
-import has from "lodash/has";
-import lh from "helpers/linkHandler";
-import { useCurrentUser, useNotification } from "hooks";
+import { useCurrentUser } from "hooks";
 import FatalErrorRender from ".";
 
 export default function FatalErrorAppWrapper(props) {
@@ -17,56 +14,10 @@ export default function FatalErrorAppWrapper(props) {
   const history = useHistory();
   const currentUser = useCurrentUser();
 
-  const loginNotification = useMemo(
-    () => ({
-      id: "authenticationError",
-      level: 2,
-      heading: t("errors.access_denied.header"),
-      body: t("errors.access_denied.authentication"),
-      scope: "authentication"
-    }),
-    [t]
-  );
-
-  const notifyLogin = useNotification(() => loginNotification);
-
-  const unauthorizedNotification = useMemo(
-    () => ({
-      id: "authorizationError",
-      level: 2,
-      heading: t("errors.access_denied.header"),
-      body: t("errors.access_denied.authorization")
-    }),
-    [t]
-  );
-
-  const notifyUnauthorized = useNotification(() => unauthorizedNotification);
-
   if (!error) return null;
 
   const isAuthorizationError =
     error.method === "GET" && (error.status === 403 || error.status === 401);
-
-  const isProjectAuthError = isAuthorizationError && has(error, "project.id");
-
-  const redirectToProject = () => {
-    const routerState = currentUser?.id
-      ? {}
-      : {
-          showLogin: true,
-          postLoginRedirect: redirectPath
-        };
-
-    history.push({
-      pathname: lh.link("frontendProjectDetail", error.project.slug),
-      state: routerState
-    });
-
-    const notify = currentUser?.id
-      ? () => setTimeout(notifyUnauthorized, 500)
-      : notifyLogin;
-    notify();
-  };
 
   const redirectOrNotify = () => {
     if (!currentUser?.id) {
@@ -86,7 +37,6 @@ export default function FatalErrorAppWrapper(props) {
     );
   };
 
-  if (isProjectAuthError) return redirectToProject();
   if (isAuthorizationError) return redirectOrNotify();
 
   return <FatalErrorRender {...props} />;
