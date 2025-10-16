f9f4078060c3bcca1a474c6309c56ea196d99e15
Jen Wardwell <jen@castironcoding.com>
2025-10-16 15:34:48 -0700

    [B] Update to a disclosure pattern similar to the header nested navigation menus - Updates search, visibility, and appearance menus to a nested disclosure pattern

---
+++ client/src/reader/components/control-menu/DisclosurePanel/DisclosurePanel.js
@@ -0,0 +1,31 @@
+import React, { forwardRef } from "react";
+import PropTypes from "prop-types";
+import * as Styled from "./styles";
+
+const ControlMenuDisclosurePanel = forwardRef((props, ref) => {
+  const { id, visible, direction, children, ...rest } = props;
+  return (
+    <Styled.Panel
+      id={id}
+      ref={ref}
+      {...(!visible ? { inert: "" } : {})}
+      className={`reader-header__panels ${
+        direction ? `reader-header__panels--${direction}` : ""
+      }`}
+      {...rest}
+    >
+      {children}
+    </Styled.Panel>
+  );
+});
+
+ControlMenuDisclosurePanel.displayName = "ControlMenu.DisclosurePanel";
+
+ControlMenuDisclosurePanel.propTypes = {
+  id: PropTypes.string,
+  children: PropTypes.node,
+  visible: PropTypes.bool,
+  direction: PropTypes.oneOf(["left", "right"])
+};
+
+export default ControlMenuDisclosurePanel;
