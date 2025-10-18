49d1887437519c479525af17342adb96944c6af1
Jen Wardwell <jen@castironcoding.com>
2025-10-18 12:10:11 -0700

    [B] Split into new global modal, fix up new dialog modal styles

--- client/src/global/components/dialog/index.js
+++ client/src/global/components/dialog/index.js
@@ -1,9 +1,11 @@
 import Wrapper from "./Wrapper";
+import Modal from "./Modal";
 import Confirm from "./Confirm";
 import ResetPassword from "./ResetPassword";
 
 export default {
   Wrapper,
+  Modal,
   Confirm,
   ResetPassword
 };
