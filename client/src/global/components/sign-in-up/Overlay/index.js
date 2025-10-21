04cf403f43255504884b104b511b58e276e2e5e6
Jen Wardwell <jen@castironcoding.com>
2025-10-21 11:29:08 -0700

    [B] Fix login route contrast issues - Change dialog bg wrapper class, but keep dialog's bg color. The background class applies appropriate theme vars for dark backgrounds
    - Add border to login inputs using theme vars

--- client/src/global/components/sign-in-up/Overlay/index.js
+++ client/src/global/components/sign-in-up/Overlay/index.js
@@ -18,6 +18,7 @@ export default function Overlay({ hideOverlay, active = false }) {
       }}
     >
       <Styled.Dialog
+        className="bg-neutral90"
         role="dialog"
         aria-modal
         aria-labelledby={uid}
