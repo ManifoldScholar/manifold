89f37bc81c2feb588d8a726c880f133f018245d5
1aurend <imlcdavidson@gmail.com>
2025-10-21 10:36:51 -0700

    [F] Expose alt text on resource featured image

--- api/app/serializers/v1/resource_serializer.rb
+++ api/app/serializers/v1/resource_serializer.rb
@@ -34,6 +34,7 @@ module V1
     typed_attribute :attachment_styles, Types::Serializer::Attachment.meta(read_only: true)
     typed_attribute :variant_poster_styles, Types::Serializer::Attachment.meta(read_only: true)
     typed_attribute :variant_thumbnail_styles, Types::Serializer::Attachment.meta(read_only: true)
+    typed_attribute :variant_thumbnail_alt_text, Types::String.optional
 
     typed_has_many :collection_resources
     typed_has_many :text_tracks, serializer: V1::TextTrackSerializer, record_type: "textTrack"
