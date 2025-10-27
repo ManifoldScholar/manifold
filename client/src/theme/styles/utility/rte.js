087716d59f8d2755af29f4b251c684e533dd8a6f
1aurend <imlcdavidson@gmail.com>
2025-10-27 10:13:48 -0700

    [F] Add naive rte utility classes

---
+++ client/src/theme/styles/utility/rte.js
@@ -0,0 +1,16 @@
+export default `
+  .manifold-text-section {
+    .manifold-rte-center {
+      text-align: center;
+    }
+    .manifold-rte-right {
+      text-align: right;
+    }
+    .manifold-rte-justify {
+      text-align: justify;
+    }
+    .manifold-rte-left {
+      text-align: left;
+    }
+  }
+`;
