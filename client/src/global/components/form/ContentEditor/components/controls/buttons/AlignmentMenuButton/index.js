3a5968d31a0603e013e501d8ae8c08dc3a1414cf
1aurend <imlcdavidson@gmail.com>
2025-10-27 08:53:38 -0700

    [E] Handle inline children; DRY alignment helpers

--- client/src/global/components/form/ContentEditor/components/controls/buttons/AlignmentMenuButton/index.js
+++ client/src/global/components/form/ContentEditor/components/controls/buttons/AlignmentMenuButton/index.js
@@ -1,16 +1,12 @@
 import { forwardRef } from "react";
-import { Transforms, Range, Node } from "slate";
-import { useSlate, ReactEditor } from "slate-react";
+import { useSlate } from "slate-react";
 import { useTranslation } from "react-i18next";
 import Utility from "global/components/utility";
-import {
-  Menu as ReakitMenu,
-  MenuItem as ReakitMenuItem,
-  MenuButton as ReakitMenuButton,
-  useMenuState
-} from "reakit/Menu";
+import { MenuButton as ReakitMenuButton, useMenuState } from "reakit/Menu";
+import AlignmentSubmenu from "./Submenu";
 import { getCommonBlock } from "../../../../utils/slate/getters";
 import { alignableElements } from "../../../../utils/elements";
+import { getActiveAlignment } from "./helpers";
 import capitalize from "lodash/capitalize";
 import * as SharedStyled from "../styles";
 import * as Styled from "./styles";
@@ -34,77 +30,10 @@ const AlignmentMenuButton = ({ size, ...rest }, ref) => {
     ? getCommonBlock(editor, condition) ?? []
     : [];
 
-  const activeAlignmentClass = block?.htmlAttrs?.class
-    ? block.htmlAttrs.class.split(" ").find(c => c.includes("manifold-rte"))
-    : null;
+  const activeAlignment = getActiveAlignment(selection, block, path);
 
-  const onClick = style => e => {
-    e.preventDefault();
-
-    if (!block) return;
-
-    const classes = block.htmlAttrs?.class
-      ? block.htmlAttrs.class
-          .split(" ")
-          .filter(c => !c.startsWith("manifold-rte"))
-          .concat(`manifold-rte-${style}`)
-          .join(" ")
-      : `manifold-rte-${style}`;
-
-    if (Range.isCollapsed(selection) && !block.slateOnly) {
-      Transforms.setNodes(
-        editor,
-        {
-          ...block,
-          htmlAttrs: {
-            ...block.htmlAttrs,
-            class: classes
-          }
-        },
-        { at: path, mode: "highest" }
-      );
-    } else {
-      const { anchor, focus } = selection;
-      const anchorIndex = anchor.path[path.length];
-      const focusIndex = focus.path[path.length];
-      const sorted = [anchorIndex, focusIndex].sort();
-
-      for (let i = sorted[0]; i <= sorted[1]; i++) {
-        const childPath = [...path, i];
-        const childBlock = Node.getIf(editor, childPath);
-        if (childBlock && alignableElements.includes(childBlock.type)) {
-          Transforms.setNodes(
-            editor,
-            {
-              ...childBlock,
-              htmlAttrs: {
-                ...block.htmlAttrs,
-                class: classes
-              }
-            },
-            { at: childPath, mode: "highest" }
-          );
-        }
-        if (childBlock?.type === "img") {
-          Transforms.wrapNodes(
-            editor,
-            {
-              type: "figure",
-              children: [childBlock],
-              htmlAttrs: { class: classes }
-            },
-            { at: childPath }
-          );
-        }
-      }
-
-      menu.hide();
-      ReactEditor.focus(editor);
-    }
-  };
-
-  const outerIcon = activeAlignmentClass
-    ? `RTEAlign${capitalize(activeAlignmentClass.split("-")[2])}32`
+  const outerIcon = activeAlignment
+    ? `RTEAlign${capitalize(activeAlignment.split("-")[2])}32`
     : "RTEAlignLeft32";
 
   return (
@@ -114,7 +43,7 @@ const AlignmentMenuButton = ({ size, ...rest }, ref) => {
         ref={ref}
         {...rest}
         aria-label={t("editor.controls.labels.alignment")}
-        data-active={menu.visible || !!activeAlignmentClass}
+        data-active={menu.visible || !!activeAlignment}
         {...menu}
         onClick={e => {
           e.preventDefault();
@@ -123,43 +52,12 @@ const AlignmentMenuButton = ({ size, ...rest }, ref) => {
       >
         <Utility.IconComposer icon={outerIcon} size={24} />
       </ReakitMenuButton>
-      <ReakitMenu as={Styled.Content} {...menu}>
-        <ReakitMenuItem
-          as={Styled.InnerButton}
-          onClick={onClick("left")}
-          data-active={
-            activeAlignmentClass === "manifold-rte-left" ||
-            !activeAlignmentClass
-          }
-          {...menu}
-        >
-          <Utility.IconComposer icon="RTEAlignLeft32" size={24} />
-        </ReakitMenuItem>
-        <ReakitMenuItem
-          as={Styled.InnerButton}
-          onClick={onClick("center")}
-          data-active={activeAlignmentClass === "manifold-rte-center"}
-          {...menu}
-        >
-          <Utility.IconComposer icon="RTEAlignCenter32" size={24} />
-        </ReakitMenuItem>
-        <ReakitMenuItem
-          as={Styled.InnerButton}
-          onClick={onClick("right")}
-          data-active={activeAlignmentClass === "manifold-rte-right"}
-          {...menu}
-        >
-          <Utility.IconComposer icon="RTEAlignRight32" size={24} />
-        </ReakitMenuItem>
-        <ReakitMenuItem
-          as={Styled.InnerButton}
-          onClick={onClick("justify")}
-          data-active={activeAlignmentClass === "manifold-rte-justify"}
-          {...menu}
-        >
-          <Utility.IconComposer icon="RTEAlignJustify32" size={24} />
-        </ReakitMenuItem>
-      </ReakitMenu>
+      <AlignmentSubmenu
+        menu={menu}
+        activeAlignment={activeAlignment}
+        block={block}
+        path={path}
+      />
     </Styled.Wrapper>
   );
 };
