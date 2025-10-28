136359c3adcad58a3c0ab123993329c24fca9ba8
1aurend <imlcdavidson@gmail.com>
2025-10-28 15:32:51 -0700

    [F] Remove redirects to /all

--- client/src/frontend/containers/JournalsWrapper/index.js
+++ client/src/frontend/containers/JournalsWrapper/index.js
@@ -1,19 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { renderRoutes } from "react-router-config";
-import { useRedirectToFirstMatch } from "hooks";
 
 export default function JournalsWrapper({ route }) {
-  useRedirectToFirstMatch({
-    route: "frontendJournals",
-    candidates: [
-      {
-        label: "All Journals",
-        route: "frontendJournalsList"
-      }
-    ]
-  });
-
   return renderRoutes(route.routes);
 }
 
