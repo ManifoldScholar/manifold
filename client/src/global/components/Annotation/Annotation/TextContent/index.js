9b7f2ea8ff90336449c4149a3151570072a54e22
Jen Wardwell <jen@castironcoding.com>
2025-10-29 11:57:17 -0700

    [E] Add 'go to passage' to reader annotation drawer - Fixes issue where focus on drawer close might not be where users expect it - Some users may expect the focus to return to the Notes button, while some may want the focus to return to the annotation - This adds a 'go to passage' button that will close the drawer and focus on the annotation

--- client/src/global/components/Annotation/Annotation/TextContent/index.js
+++ client/src/global/components/Annotation/Annotation/TextContent/index.js
@@ -15,6 +15,7 @@ class AnnotationSelectionWrapper extends PureComponent {
     selection: PropTypes.string.isRequired,
     displayFormat: PropTypes.string,
     visitHandler: PropTypes.func,
+    focusHandler: PropTypes.func,
     onAnnotate: PropTypes.func,
     onLogin: PropTypes.func,
     t: PropTypes.func,
@@ -45,6 +46,12 @@ class AnnotationSelectionWrapper extends PureComponent {
     this.setState({ hovering });
   };
 
+  handleFocusHandler = () => {
+    const { annotation, focusHandler } = this.props;
+
+    if (focusHandler) focusHandler(annotation);
+  };
+
   render() {
     const { annotation, selection, t } = this.props;
     const wrapperClasses = classNames({
@@ -69,28 +76,40 @@ class AnnotationSelectionWrapper extends PureComponent {
             onHover={this.hoverHandler}
           />
         </figure>
-        {this.annotatable && (
-          <>
-            <Authorize kind="any">
+        {(this.annotatable || this.props.focusHandler) && (
+          <div className="annotation-selection__button-wrapper">
+            {this.annotatable && (
+              <>
+                <Authorize kind="any">
+                  <button
+                    className="annotation-selection__button"
+                    onClick={this.props.onAnnotate}
+                    ref={this.props.annotateToggleRef}
+                  >
+                    {t("actions.annotate")}
+                  </button>
+                </Authorize>
+                {this.canLogin && (
+                  <Authorize kind="unauthenticated">
+                    <button
+                      className="annotation-selection__button"
+                      onClick={this.props.onLogin}
+                    >
+                      {t("actions.login_to_annotate")}
+                    </button>
+                  </Authorize>
+                )}
+              </>
+            )}
+            {this.props.focusHandler && (
               <button
                 className="annotation-selection__button"
-                onClick={this.props.onAnnotate}
-                ref={this.props.annotateToggleRef}
+                onClick={this.handleFocusHandler}
               >
-                {t("actions.annotate")}
+                {t("actions.go_to_passage")}
               </button>
-            </Authorize>
-            {this.canLogin && (
-              <Authorize kind="unauthenticated">
-                <button
-                  className="annotation-selection__button"
-                  onClick={this.props.onLogin}
-                >
-                  {t("actions.login_to_annotate")}
-                </button>
-              </Authorize>
             )}
-          </>
+          </div>
         )}
       </div>
     );
