7bde2b496c51509d206a107c219e7d71ffa3176e
Tim Frazee <tim@castironcoding.com>
2025-10-30 18:06:26 -0700

    [B] Fix secret key base lookup
    - Moved from secrets to env

--- api/config/initializers/25_lockbox.rb
+++ api/config/initializers/25_lockbox.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-secret_key_base = Rails.application.secrets.secret_key_base
+secret_key_base = Rails.application.secret_key_base
 
 secret_key_base = secret_key_base.presence || Lockbox.generate_key if Rails.env.test?
 
