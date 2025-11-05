dbc10836a999281f96f97a1f0a074bfe7eb617a7
Dana Johnson <dana@castironcoding.com>
2025-11-04 17:14:18 -0800

    [E] Futher UI adjustment

--- client/src/reader/components/control-menu/AppearanceMenuBody/HighContrastControls.js
+++ client/src/reader/components/control-menu/AppearanceMenuBody/HighContrastControls.js
@@ -1,35 +1,57 @@
-import { useTranslation } from "react-i18next";
-import { Toggle } from "global/components/form/Switch/ToggleOnly";
-import Section from "./Section";
 import { useId } from "react";
+import { useTranslation } from "react-i18next";
+import classNames from "classnames";
+import Section from "./Section";
 
 export default function HighContrastControls({
   highContrast,
   handleHighContrastControl
 }) {
   const { t } = useTranslation();
-  const labelId = useId();
+  const uidSeed = useId();
 
   return (
     <Section
       label={t("reader.menus.appearance.high_contrast")}
-      labelId={labelId}
       className="appearance-menu__radio-group"
     >
-      <button
-        className="appearance-menu__high-contrast-button"
-        type="button"
-        role="switch"
-        aria-labelledby={labelId}
-        aria-checked={highContrast}
-        onClick={handleHighContrastControl}
-      >
-        <span aria-hidden>
-          {t("reader.menus.appearance.high_contrast_off")}
-        </span>
-        <Toggle $checked={highContrast} />
-        <span aria-hidden>{t("reader.menus.appearance.high_contrast_on")}</span>
-      </button>
+      {[false, true].map(option => (
+        <div key={option.toString()} className="appearance-menu__button-group">
+          <button
+            type="button"
+            className={classNames({
+              "appearance-menu__button-base": true,
+              "appearance-menu__high-contrast": true,
+              [`appearance-menu__high-contrast--${
+                option ? "high" : "low"
+              }`]: true,
+              "appearance-menu__high-contrast--active": highContrast === option
+            })}
+            aria-labelledby={`${uidSeed}-${option.toString()}`}
+            aria-pressed={option === highContrast}
+            data-value={option}
+            onClick={handleHighContrastControl}
+          >
+            <div
+              aria-hidden
+              className={`appearance-menu__high-contrast-visual appearance-menu__high-contrast-visual--${
+                option ? "high" : "low"
+              }`}
+            >
+              abc
+            </div>
+          </button>
+          <span
+            id={`${uidSeed}-${option.toString()}`}
+            className="appearance-menu__button-label"
+            aria-hidden
+          >
+            {option
+              ? t("reader.menus.appearance.high_contrast_true")
+              : t("reader.menus.appearance.high_contrast_false")}
+          </span>
+        </div>
+      ))}
     </Section>
   );
 }
