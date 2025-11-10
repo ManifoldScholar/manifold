6e4faa77d765c80e851a8993709bae5e31b5a9de
Dana Johnson <dana@castironcoding.com>
2025-11-10 12:32:58 -0800

    [B] Improve collapsed annotation label

--- client/src/global/components/Annotation/Annotation/TextContent/FromNodes/Wrapper.js
+++ client/src/global/components/Annotation/Annotation/TextContent/FromNodes/Wrapper.js
@@ -1,12 +1,13 @@
-import React from "react";
 import Collapse from "global/components/Collapse";
 import * as Styled from "./styles";
-import { Trans } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import IconComposer from "global/components/utility/IconComposer";
 
 export default function FromNodesWrapper({ children, overlayLight }) {
+  const { t } = useTranslation();
+
   return (
-    <Collapse stubHeight={200}>
+    <Collapse stubHeight={200} label={t("reader.annotated_passage")}>
       <Styled.Content focusOnVisible>
         <blockquote>{children}</blockquote>
         <Styled.Overlay $light={overlayLight} />
