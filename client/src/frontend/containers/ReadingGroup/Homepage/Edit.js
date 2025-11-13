8ebc29f45461260598285f104fe9038e0c3acafa
1aurend <imlcdavidson@gmail.com>
2025-11-13 12:49:45 -0800

    [C] Remove unused imports

--- client/src/frontend/containers/ReadingGroup/Homepage/Edit.js
+++ client/src/frontend/containers/ReadingGroup/Homepage/Edit.js
@@ -1,5 +1,3 @@
-import React from "react";
-import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
 import { useOutletContext } from "react-router-dom";
 import lh from "helpers/linkHandler";
