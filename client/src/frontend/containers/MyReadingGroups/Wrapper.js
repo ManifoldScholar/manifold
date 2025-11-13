16961ba87e360153d2b4bf002ea61563d3cab6d8
1aurend <imlcdavidson@gmail.com>
2025-11-13 12:25:54 -0800

    [C] Move simple auth redirects to loaders

--- client/src/frontend/containers/MyReadingGroups/Wrapper.js
+++ client/src/frontend/containers/MyReadingGroups/Wrapper.js
@@ -1,14 +1,12 @@
 import { useRef, useCallback } from "react";
-import { useNavigate, Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import OutletWithDrawer from "global/components/router/OutletWithDrawer";
 import lh from "helpers/linkHandler";
 import List from "./List";
-import { useCurrentUser } from "hooks";
 
 export default function MyReadingGroupsContainer() {
   const navigate = useNavigate();
   const refreshRef = useRef(null);
-  const currentUser = useCurrentUser();
 
   const handleNewGroupSuccess = useCallback(() => {
     navigate(lh.link("frontendMyReadingGroups"));
@@ -17,14 +15,6 @@ export default function MyReadingGroupsContainer() {
       refreshRef.current.refresh();
     }
   }, [navigate]);
-
-  if (!currentUser)
-    return (
-      <Navigate
-        to={lh.link("frontendLogin")}
-        search={{ redirect_uri: "/my/groups" }}
-      />
-    );
 
   return (
     <>
