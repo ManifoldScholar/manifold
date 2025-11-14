4cd31e5fccd506425eec16e2789e307e13171e7e
Jen Wardwell <jen@castironcoding.com>
2025-11-14 12:21:25 -0800

    -

--- client/src/global/components/atomic/EntityThumbnail/EntityMetadata/patterns/ProjectCollectionMetadata.js
+++ client/src/global/components/atomic/EntityThumbnail/EntityMetadata/patterns/ProjectCollectionMetadata.js
@@ -1,22 +1,23 @@
 import React from "react";
-// import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import MetadataContent from "../Content";
 import { getGenericMetadataProps } from "../helpers";
 
 export default function ProjectCollectionMetadata({ entity, ...props }) {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
 
   if (!entity) return;
 
   const getMetadataProps = () => {
     const genericMetadata = getGenericMetadataProps({ entity });
 
-    // const data = entity?.attributes;
-    // The project count does not reflect if the user is logged in - hidden projects are included in the count
-    // const additionalData = t("counts.project", { count: data.projectsCount });
+    const data = entity?.attributes;
+    const additionalData = t("counts.project", {
+      count: data.availableProjectsCount
+    });
 
     return {
-      // additionalData,
+      additionalData,
       ...genericMetadata
     };
   };
