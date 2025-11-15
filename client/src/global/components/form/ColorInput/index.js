141c6bd2950649e388e78d25b36bb4241fdf380f
Dana Johnson <dana@castironcoding.com>
2025-11-14 16:28:31 -0800

    [WIP]

--- client/src/global/components/form/ColorInput/index.js
+++ client/src/global/components/form/ColorInput/index.js
@@ -1,11 +1,10 @@
 import { useId } from "react";
-import PropTypes from "prop-types";
 import * as Styled from "./styles";
 
 export default function ColorInput({ defaultValue, ...props }) {
   const id = useId();
 
-  const renderValue = value => value ?? defaultValue;
+  const renderValue = value => value || defaultValue;
   return (
     <Styled.ColorInput
       inputType="color"
