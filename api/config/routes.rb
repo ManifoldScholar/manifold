fa87d273b50968754eabcba3dd77aae7f0230a75
Tim Frazee <tim@castironcoding.com>
2025-11-17 12:43:57 -0800

    [E] Add route for SAML callback

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -25,6 +25,9 @@ Rails.application.routes.draw do
 
   get "auth/:provider/callback", to: "oauth#authorize"
 
+  # SAML POST callbacks
+  post "auth/:provider/callback", to: "oauth#authorize"
+
   namespace :api do
     mount Tus::Server => "/files"
 
