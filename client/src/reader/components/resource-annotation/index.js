164746791a5cfbb36be1c3037b707fe5e029aa4d
Tim Frazee <tim@castironcoding.com>
2025-11-19 15:41:47 -0800

    [WIP] fix client docker build

--- client/src/reader/components/resource-annotation/index.js
+++ client/src/reader/components/resource-annotation/index.js
@@ -3,4 +3,4 @@ import Factory from "./Factory";
 export default Factory;
 
 export { default as Marker } from "./Marker";
-export { default as Resource } from "./resource";
+export { default as Resource } from "./Resource";
