1be4bc189b451551bce706b4ace891a5c3ad40b5
1aurend <imlcdavidson@gmail.com>
2025-11-20 10:48:04 -0800

    [F] Scaffold login changes

--- client/src/global/components/sign-in-up/LoginForm/index.js
+++ client/src/global/components/sign-in-up/LoginForm/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import { useCallback, useState } from "react";
 import actions from "actions/currentUser";
 import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
@@ -11,6 +11,7 @@ import {
 import Form from "global/components/form";
 import { tokensAPI } from "api";
 import { useFromStore } from "hooks";
+import OAuthProviderButton from "../oauth/OAuthProviderButton";
 import * as Styled from "./styles";
 import * as SharedStyles from "../styles";
 
@@ -24,6 +25,8 @@ export default function LoginForm({
   const [isLoading, setIsLoading] = useState(false);
   const authentication = useFromStore("authentication");
   const error = authentication?.error?.body;
+  const settings = useFromStore("settings", "select");
+  const { hideLocalLogin = true, identityProviders = ["1", "2"] } = settings;
 
   const formatData = data => {
     dispatch(actions.loginStart());
@@ -77,58 +80,75 @@ export default function LoginForm({
         </Styled.NotificationsWrapper>
       )}
       <div>
-        <SharedStyles.Form
-          name="global-login"
-          create={tokensAPI.loginForm}
-          onSuccess={onSuccess}
-          onError={onError}
-          formatData={formatData}
-        >
-          <>
-            <Form.Header
-              label={t("forms.signin_overlay.log_in")}
-              styleType="primary"
-            />
-            <Form.FieldGroup>
-              <Form.TextInput
-                focusOnMount
-                inputType="email"
-                name="email"
-                id="login-email"
-                placeholder={t("forms.signin_overlay.email")}
-                inputMode="email"
-                label={t("forms.signin_overlay.email")}
-                autoComplete="email"
-                ariaRequired
+        {!hideLocalLogin ? (
+          <SharedStyles.Form
+            name="global-login"
+            create={tokensAPI.loginForm}
+            onSuccess={onSuccess}
+            onError={onError}
+            formatData={formatData}
+          >
+            <>
+              <Form.Header
+                label={t("forms.signin_overlay.log_in")}
+                styleType="primary"
               />
-              <Form.TextInput
-                password
-                name="password"
-                id="login-password"
-                placeholder={t("forms.signin_overlay.password")}
-                label={t("forms.signin_overlay.password")}
-                autoComplete="current-password"
-                ariaRequired
+              <Form.FieldGroup>
+                <Form.TextInput
+                  focusOnMount
+                  inputType="email"
+                  name="email"
+                  id="login-email"
+                  placeholder={t("forms.signin_overlay.email")}
+                  inputMode="email"
+                  label={t("forms.signin_overlay.email")}
+                  autoComplete="email"
+                  ariaRequired
+                />
+                <Form.TextInput
+                  password
+                  name="password"
+                  id="login-password"
+                  placeholder={t("forms.signin_overlay.password")}
+                  label={t("forms.signin_overlay.password")}
+                  autoComplete="current-password"
+                  ariaRequired
+                />
+              </Form.FieldGroup>
+              <Form.InputError
+                errors={error && !isLoading ? [{ detail: error }] : []}
               />
-            </Form.FieldGroup>
-            <Form.InputError
-              errors={error && !isLoading ? [{ detail: error }] : []}
-            />
-            <input
-              type="submit"
-              className="button-secondary"
-              value={t("forms.signin_overlay.log_in")}
-            />
-          </>
-        </SharedStyles.Form>
-        <SharedStyles.LinksWrapper>
-          <SharedStyles.ViewLink onClick={e => handleViewChange("password", e)}>
-            {t("forms.signin_overlay.forgot_password")}
-          </SharedStyles.ViewLink>
-          <SharedStyles.ViewLink onClick={e => handleViewChange("terms", e)}>
-            {t("forms.signin_overlay.need_account")}
-          </SharedStyles.ViewLink>
-        </SharedStyles.LinksWrapper>
+              <input
+                type="submit"
+                className="button-secondary"
+                value={t("forms.signin_overlay.log_in")}
+              />
+            </>
+          </SharedStyles.Form>
+        ) : (
+          <Form.Header
+            label={t("forms.signin_overlay.log_in")}
+            styleType="primary"
+          />
+        )}
+        {!!identityProviders?.length && (
+          <Styled.OauthWrapper>
+            <OAuthProviderButton provider="Provider 1" icon="Privacy24" />
+            <OAuthProviderButton provider="Provider 2" icon="Privacy24" />
+          </Styled.OauthWrapper>
+        )}
+        {!hideLocalLogin && (
+          <SharedStyles.LinksWrapper>
+            <SharedStyles.ViewLink
+              onClick={e => handleViewChange("password", e)}
+            >
+              {t("forms.signin_overlay.forgot_password")}
+            </SharedStyles.ViewLink>
+            <SharedStyles.ViewLink onClick={e => handleViewChange("terms", e)}>
+              {t("forms.signin_overlay.need_account")}
+            </SharedStyles.ViewLink>
+          </SharedStyles.LinksWrapper>
+        )}
       </div>
     </div>
   );
