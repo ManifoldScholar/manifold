2ec2d2b1a56b1c6caf6dc477ff42959206bedfe0
1aurend <imlcdavidson@gmail.com>
2025-11-20 15:35:00 -0800

    [F] Add user_group_serializer + minor fixes for #index

--- api/app/serializers/v1/user_group_serializer.rb
+++ api/app/serializers/v1/user_group_serializer.rb
@@ -2,9 +2,12 @@
 
 module V1
   class UserGroupSerializer < ManifoldSerializer
-    include ::V1::Concerns::ManifoldSerializer
+    include V1::Concerns::ManifoldSerializer
 
+    abilities
+
+    typed_attribute :id, Types::Serializer::ID
+    typed_attribute :created_at, Types::DateTime.meta(read_only: true)
     typed_attribute :name, Types::String.meta(example: "Manifold SAML users")
-
   end
 end
