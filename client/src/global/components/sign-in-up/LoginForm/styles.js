8026f35c8b31bf0a8bc201d5b7b023c84a42454d
1aurend <imlcdavidson@gmail.com>
2025-11-20 10:48:04 -0800

    [F] Scaffold login changes

--- client/src/global/components/sign-in-up/LoginForm/styles.js
+++ client/src/global/components/sign-in-up/LoginForm/styles.js
@@ -3,3 +3,11 @@ import styled from "@emotion/styled";
 export const NotificationsWrapper = styled.div`
   margin-block-end: 25px;
 `;
+
+export const OauthWrapper = styled.div`
+  margin-block-start: 15px;
+
+  > * + * {
+    margin-block-start: 30px;
+  }
+`;
--- client/src/global/components/sign-in-up/oauth/OAuthProviderButton/index.js
+++ client/src/global/components/sign-in-up/oauth/OAuthProviderButton/index.js
@@ -1,8 +1,5 @@
-import React from "react";
 import PropTypes from "prop-types";
 import { oauthActions } from "actions";
-import { providerSetting } from "utils/oauth";
-import { useFromStore } from "hooks";
 import { useDispatch } from "react-redux";
 import { useTranslation } from "react-i18next";
 import IconComposer from "global/components/utility/IconComposer";
@@ -10,13 +7,9 @@ import * as Styled from "./styles";
 
 export default function OAuthProviderButton({ provider, icon }) {
   const { t } = useTranslation();
-  const settings = useFromStore("settings", "select");
   const dispatch = useDispatch();
-  const providerSettings =
-    settings?.attributes?.oauth[providerSetting(provider)] ?? {};
-  const enabled = providerSettings.enabled;
 
-  return enabled ? (
+  return (
     <Styled.Button
       className="button-secondary button-secondary--dark"
       onClick={() => dispatch(oauthActions.prompt(provider))}
@@ -29,7 +22,7 @@ export default function OAuthProviderButton({ provider, icon }) {
         })}
       </span>
     </Styled.Button>
-  ) : null;
+  );
 }
 
 OAuthProviderButton.displayName = "Global.SignInUp.OAuthProviderButton";
