285453f63152fa2630df8361c509683d36dca9cd
1aurend <imlcdavidson@gmail.com>
2025-11-20 16:10:08 -0800

    [F] Setup includes for user_group#show

--- api/app/controllers/api/v1/user_groups_controller.rb
+++ api/app/controllers/api/v1/user_groups_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module API
   module V1
     class UserGroupsController < ApplicationController
@@ -14,23 +16,32 @@ module API
 
       def show
         @user_group = load_and_authorize_user_group
-        render_single_resource @user_group
+        render_single_resource @user_group,
+                               include: includes
       end
 
       def create
         @user_group = ::Updaters::UserGroup.new(user_group_params).update(UserGroup.new)
-        render_single_resource @user_group
+        render_single_resource @user_group,
+                               include: includes
       end
 
       def update
         @user_group = load_and_authorize_user_group
         ::Updaters::UserGroup.new(user_group_params).update(@user_group)
-        render_single_resource @user_group
+        render_single_resource @user_group,
+                               include: includes
       end
 
       def destroy
         @user_group = load_and_authorize_user_group
         @user_group.destroy
+      end
+
+      protected
+
+      def includes
+        [:users, :entitlement_subjects]
       end
     end
   end
---
+++ api/app/models/user_group_entitlement_subject.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class UserGroupEntitleable < ApplicationRecord
+  belongs_to :user_group, inverse_of: :user_group_entitlement_subjects
+  belongs_to :entitleable, polymorphic: true
+end
