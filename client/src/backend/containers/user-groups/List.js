becd23fe374b865511523ab12a24d2c9857c4fbf
1aurend <imlcdavidson@gmail.com>
2025-11-20 16:41:26 -0800

    [F] Wire up user group CRUD

--- client/src/backend/containers/user-groups/List.js
+++ client/src/backend/containers/user-groups/List.js
@@ -1,18 +1,35 @@
-import { useMemo } from "react";
 import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
 import { useLocation } from "react-router-dom";
+import { userGroupsAPI } from "api";
 import lh from "helpers/linkHandler";
 import { childRoutes } from "helpers/router";
 import EntitiesList, {
   Button,
+  Search,
   UserGroupRow
 } from "backend/components/list/EntitiesList";
+import withFilteredLists, { userGroupFilters } from "hoc/withFilteredLists";
+import { useListQueryParams, useFetch } from "hooks";
 
-function UserGroupsListContainer({ route: baseRoute }) {
+function UserGroupsListContainer({
+  route: baseRoute,
+  entitiesListSearchProps,
+  entitiesListSearchParams
+}) {
   const { t } = useTranslation();
   const { pathname } = useLocation();
   const id = pathname.split("/")[4];
+
+  const { pagination, filters, searchProps } = useListQueryParams({
+    initSize: 10,
+    initFilters: entitiesListSearchParams.user_groups,
+    initSearchProps: entitiesListSearchProps("user_groups")
+  });
+
+  const { data: userGroups, meta: userGroupsMeta } = useFetch({
+    request: [userGroupsAPI.index, filters, pagination]
+  });
 
   const drawerProps = {
     closeUrl: lh.link("backendRecordsUserGroups"),
@@ -31,25 +48,22 @@ function UserGroupsListContainer({ route: baseRoute }) {
     };
     return (
       <>
+        {childRoutes(detailRoutes)}
         {childRoutes(drawerRoutes, {
           drawer: true,
           drawerProps
         })}
-        {childRoutes(detailRoutes)}
       </>
     );
   };
 
   const showList = !id || id === "new";
 
-  const userGroup = useMemo(
-    () => ({
-      id: "test",
-      type: "userGroups",
-      attributes: { name: "Test User Group", externalId: "test-group-1" }
-    }),
-    []
-  );
+  if (showList && (!userGroups || !userGroupsMeta)) return null;
+
+  const unit = t("glossary.user_group", {
+    count: userGroupsMeta?.pagination?.totalCount
+  });
 
   return (
     <>
@@ -58,15 +72,18 @@ function UserGroupsListContainer({ route: baseRoute }) {
         <EntitiesList
           title={t("records.user_groups.header")}
           titleStyle="bar"
-          entities={[userGroup]}
+          entities={userGroups}
           entityComponent={UserGroupRow}
+          unit={unit}
+          pagination={userGroupsMeta.pagination}
           showCount
+          search={<Search {...searchProps} />}
           buttons={[
             <Button
               path={lh.link("backendRecordsUserGroupsNew")}
               text={t("records.user_groups.button_label")}
               type="add"
-              // authorizedFor="userGroup"
+              authorizedFor="userGroup"
             />
           ]}
         />
@@ -78,7 +95,11 @@ function UserGroupsListContainer({ route: baseRoute }) {
 UserGroupsListContainer.displayName = "UserGroups.List";
 
 UserGroupsListContainer.propTypes = {
-  route: PropTypes.object
+  route: PropTypes.object,
+  entitiesListSearchProps: PropTypes.func.isRequired,
+  entitiesListSearchParams: PropTypes.object.isRequired
 };
 
-export default UserGroupsListContainer;
+export default withFilteredLists(UserGroupsListContainer, {
+  user_groups: userGroupFilters()
+});
