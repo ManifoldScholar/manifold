c7b219ff5cfd55f5c2be05edf19a11c641c072d5
Tim Frazee <tim@castironcoding.com>
2025-11-21 10:46:25 -0800

    [B] Fix user group entitleable filename and inverse reference

--- api/app/models/user_group_entitleable.rb
+++ api/app/models/user_group_entitleable.rb
@@ -1,4 +1,4 @@
 class UserGroupEntitleable < ApplicationRecord
-  belongs_to :user_group, inverse_of: :user_group_entitlement_subjects
+  belongs_to :user_group, inverse_of: :user_group_entitleables
   belongs_to :entitleable, polymorphic: true
 end
