2d5b2b424f5499440dd0bdb30e72fb40716bf7d2
1aurend <imlcdavidson@gmail.com>
2025-11-24 13:20:02 -0800

    [C] Migrate analytics routes to v6

--- client/src/backend/containers/analytics/Global.js
+++ client/src/backend/containers/analytics/Global.js
@@ -1,100 +1,83 @@
-import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
-import { withTranslation } from "react-i18next";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import {
   AnalyticsFactory,
   Grid,
   RangePicker
 } from "backend/components/analytics";
 import PageHeader from "backend/components/layout/PageHeader";
-import withAnalyticsReport from "hoc/analytics/withAnalyticsReport";
+import useAnalyticsReport from "hooks/useAnalyticsReport";
 
-class AnalyticsGlobalContainer extends PureComponent {
-  static propTypes = {
-    route: PropTypes.object,
-    statistics: PropTypes.object,
-    analytics: PropTypes.object,
-    fetchStats: PropTypes.func.isRequired,
-    fetchAnalytics: PropTypes.func.isRequired,
-    updateAnalyticsRange: PropTypes.func.isRequired,
-    analyticsStartDate: PropTypes.instanceOf(Date),
-    analyticsEndDate: PropTypes.instanceOf(Date),
-    t: PropTypes.func
-  };
+export default function AnalyticsGlobalContainer() {
+  const { t } = useTranslation();
+  const {
+    analytics,
+    statistics,
+    fetchStats,
+    fetchAnalytics,
+    updateAnalyticsRange,
+    analyticsStartDate,
+    analyticsEndDate
+  } = useAnalyticsReport();
 
-  componentDidMount() {
-    this.props.fetchStats();
-    this.props.fetchAnalytics("global");
-  }
+  useEffect(() => {
+    fetchStats();
+    fetchAnalytics("global");
+  }, [fetchStats, fetchAnalytics]);
 
-  render() {
-    const {
-      analytics,
-      statistics,
-      updateAnalyticsRange,
-      analyticsStartDate,
-      analyticsEndDate
-    } = this.props;
-
-    return (
-      <>
-        <PageHeader
-          type="analytics"
-          title={this.props.t("analytics.global_header")}
-        />
-        <Grid columns={4}>
-          {analytics && (
-            <>
-              <RangePicker
-                onNewRangeSelected={updateAnalyticsRange}
-                initialStart={analyticsStartDate}
-                initialEnd={analyticsEndDate}
-                className="analytics-grid__item--100"
-              />
-              <AnalyticsFactory
-                view="Visitors"
-                report="daily_visitors"
-                data={analytics}
-              />
-              <AnalyticsFactory
-                view="ReturnVisits"
-                report="returning_visitors"
-                data={analytics}
-              />
-              <AnalyticsFactory
-                view="AverageVisit"
-                report="average_visit_duration"
-                data={analytics}
-              />
-              <AnalyticsFactory
-                view="Engagement"
-                report="active_visitors"
-                data={analytics}
-              />
-              <AnalyticsFactory
-                view="Collected"
-                report="favorited_projects"
-                data={analytics}
-              />
-              <AnalyticsFactory view="SiteStatistics" data={statistics} />
-              <AnalyticsFactory
-                view="TopProjects"
-                report="top_projects"
-                data={analytics}
-                withAllLink
-              />
-              <AnalyticsFactory
-                view="TopSearches"
-                report="top_search_terms"
-                data={analytics}
-                withAllLink
-              />
-            </>
-          )}
-        </Grid>
-      </>
-    );
-  }
+  return (
+    <>
+      <PageHeader type="analytics" title={t("analytics.global_header")} />
+      <Grid columns={4}>
+        {analytics && (
+          <>
+            <RangePicker
+              onNewRangeSelected={updateAnalyticsRange}
+              initialStart={analyticsStartDate}
+              initialEnd={analyticsEndDate}
+              className="analytics-grid__item--100"
+            />
+            <AnalyticsFactory
+              view="Visitors"
+              report="daily_visitors"
+              data={analytics}
+            />
+            <AnalyticsFactory
+              view="ReturnVisits"
+              report="returning_visitors"
+              data={analytics}
+            />
+            <AnalyticsFactory
+              view="AverageVisit"
+              report="average_visit_duration"
+              data={analytics}
+            />
+            <AnalyticsFactory
+              view="Engagement"
+              report="active_visitors"
+              data={analytics}
+            />
+            <AnalyticsFactory
+              view="Collected"
+              report="favorited_projects"
+              data={analytics}
+            />
+            <AnalyticsFactory view="SiteStatistics" data={statistics} />
+            <AnalyticsFactory
+              view="TopProjects"
+              report="top_projects"
+              data={analytics}
+              withAllLink
+            />
+            <AnalyticsFactory
+              view="TopSearches"
+              report="top_search_terms"
+              data={analytics}
+              withAllLink
+            />
+          </>
+        )}
+      </Grid>
+    </>
+  );
 }
-
-export default withTranslation()(withAnalyticsReport(AnalyticsGlobalContainer));
