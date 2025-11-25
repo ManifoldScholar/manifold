3e3b9a21333d7efbaa57a12fe9a45ac8b2fb5cca
1aurend <imlcdavidson@gmail.com>
2025-11-24 16:45:58 -0800

    [C] Migrate records routes to v6

--- client/src/backend/containers/export-targets/List.js
+++ client/src/backend/containers/export-targets/List.js
@@ -1,99 +1,62 @@
-import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
-import { withTranslation } from "react-i18next";
-import { connect } from "react-redux";
-import { entityStoreActions } from "actions";
-import { select, meta } from "utils/entityUtils";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import { exportTargetsAPI, requests } from "api";
-import get from "lodash/get";
 import lh from "helpers/linkHandler";
-import { childRoutes } from "helpers/router";
+import OutletWithDrawer from "global/components/router/OutletWithDrawer";
 import EntitiesList, {
   Button,
   ExportTargetRow
 } from "backend/components/list/EntitiesList";
+import { useFetch } from "hooks";
 
-const { request } = entityStoreActions;
+export default function ExportTargetsListContainer() {
+  const { t } = useTranslation();
+  const { id } = useParams();
 
-export class ExportTargetsContainerImplementation extends PureComponent {
-  static mapStateToProps = state => {
-    return {
-      exportTargets: select(requests.beExportTargets, state.entityStore),
-      exportTargetMeta: meta(requests.beExportTargets, state.entityStore)
-    };
+  const { data: exportTargets, meta: exportTargetsMeta, refresh } = useFetch({
+    request: [exportTargetsAPI.index],
+    options: { requestKey: requests.beExportTargets }
+  });
+
+  // Refetch when export targets are modified
+  const previousModified = exportTargetsMeta?.modified;
+  useEffect(() => {
+    const currentModified = exportTargetsMeta?.modified;
+    if (currentModified && currentModified !== previousModified) {
+      refresh();
+    }
+  }, [exportTargetsMeta?.modified, previousModified, refresh]);
+
+  if (!exportTargets) return null;
+
+  const active = id || "";
+  const drawerProps = {
+    closeUrl: lh.link("backendSettingsExportTargets"),
+    lockScroll: "always"
   };
 
-  static displayName = "ExportTargets.List";
-
-  static propTypes = {
-    // export target information is required to render the component,
-    // but can be null until the async network request is fulfilled
-    /* eslint-disable react/require-default-props */
-    exportTargets: PropTypes.array,
-    exportTargetsMeta: PropTypes.object,
-    /* eslint-enable react/require-default-props */
-    match: PropTypes.object.isRequired,
-    route: PropTypes.object.isRequired,
-    dispatch: PropTypes.func.isRequired,
-    t: PropTypes.func
-  };
-
-  componentDidMount() {
-    this.fetchExportTargets();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.exportTargetWasModified(prevProps))
-      return this.fetchExportTargets();
-  }
-
-  fetchExportTargets = () => {
-    const action = request(exportTargetsAPI.index(), requests.beExportTargets);
-    this.props.dispatch(action);
-  };
-
-  exportTargetWasModified(prevProps) {
-    const currentModified = get(this.props, "exportTargets.modified");
-    const previousModified = get(prevProps, "exportTargetsMeta.modified");
-    if (!currentModified) return false;
-    return !(currentModified && previousModified);
-  }
-
-  render() {
-    const { exportTargets, match, route, t } = this.props;
-    if (!exportTargets) return null;
-    const active = match.params.id || "";
-    const drawerProps = {
-      closeUrl: lh.link("backendSettingsExportTargets"),
-      lockScroll: "always"
-    };
-
-    return (
-      <>
-        {childRoutes(route, { drawer: true, drawerProps })}
-        <EntitiesList
-          entityComponent={ExportTargetRow}
-          entityComponentProps={{ active }}
-          title={t("settings.export_targets.header")}
-          titleStyle="bar"
-          entities={exportTargets}
-          unit={t("glossary.export_target", { count: exportTargets?.length })}
-          buttons={[
-            <Button
-              path={lh.link("backendSettingsExportTargetsNew")}
-              text={t("settings.export_targets.button_label")}
-              authorizedFor="exportTarget"
-              type="add"
-            />
-          ]}
-        />
-      </>
-    );
-  }
+  return (
+    <>
+      <OutletWithDrawer drawerProps={drawerProps} />
+      <EntitiesList
+        entityComponent={ExportTargetRow}
+        entityComponentProps={{ active }}
+        title={t("settings.export_targets.header")}
+        titleStyle="bar"
+        entities={exportTargets}
+        unit={t("glossary.export_target", { count: exportTargets?.length })}
+        buttons={[
+          <Button
+            path={lh.link("backendSettingsExportTargetsNew")}
+            text={t("settings.export_targets.button_label")}
+            authorizedFor="exportTarget"
+            type="add"
+          />
+        ]}
+      />
+    </>
+  );
 }
 
-export default withTranslation()(
-  connect(ExportTargetsContainerImplementation.mapStateToProps)(
-    ExportTargetsContainerImplementation
-  )
-);
+ExportTargetsListContainer.displayName = "ExportTargets.List";
