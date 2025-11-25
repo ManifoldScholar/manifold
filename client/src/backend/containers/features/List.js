5b7271cf42c378ce57aeb11ad53c0f1d57a60269
1aurend <imlcdavidson@gmail.com>
2025-11-24 16:45:58 -0800

    [C] Migrate records routes to v6

--- client/src/backend/containers/features/List.js
+++ client/src/backend/containers/features/List.js
@@ -1,89 +1,69 @@
-import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
-import { withTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import lh from "helpers/linkHandler";
 import { featuresAPI, requests } from "api";
-import { entityStoreActions } from "actions";
-import connectAndFetch from "utils/connectAndFetch";
-import entityUtils from "utils/entityUtils";
+import { useFetch, useApiCallback } from "hooks";
 import EntitiesList, {
   Button,
   FeatureRow
 } from "backend/components/list/EntitiesList";
 import Authorize from "hoc/Authorize";
 
-const { select } = entityUtils;
-const { request } = entityStoreActions;
+export default function FeaturesList() {
+  const { t } = useTranslation();
 
-class ContentFeaturesList extends PureComponent {
-  static mapStateToProps = state => {
-    return {
-      features: select(requests.beFeatures, state.entityStore)
-    };
+  const { data: features, refresh } = useFetch({
+    request: [featuresAPI.index],
+    options: { requestKey: requests.beFeatures }
+  });
+
+  const updateFeature = useApiCallback(featuresAPI.update, {
+    requestKey: requests.beFeatureUpdate,
+    noTouch: true
+  });
+
+  const togglePublished = async feature => {
+    if (!feature) return;
+    await updateFeature(feature.id, {
+      attributes: {
+        live: !feature.attributes.live
+      }
+    });
+    refresh();
   };
 
-  static displayName = "Features.List";
+  if (!features) return null;
 
-  static propTypes = {
-    features: PropTypes.array,
-    dispatch: PropTypes.func,
-    t: PropTypes.func
-  };
-
-  componentDidMount() {
-    this.fetchFeatures();
-  }
-
-  fetchFeatures() {
-    const call = featuresAPI.index();
-    const featuresRequest = request(call, requests.beFeatures);
-    this.props.dispatch(featuresRequest);
-  }
-
-  togglePublished = feature => {
-    if (!feature) return null;
-    const attributes = {
-      live: !feature.attributes.live
-    };
-    const call = featuresAPI.update(feature.id, { attributes });
-    const featureRequest = request(call, requests.beFeatureUpdate);
-    this.props.dispatch(featureRequest);
-  };
-
-  render() {
-    const { features, t } = this.props;
-    if (!features) return null;
-    return (
-      <Authorize
-        ability="update"
-        entity={["feature"]}
-        failureNotification={{
-          body: t("errors.access_denied.authorization_admin_type", {
-            type: "features"
-          })
+  return (
+    <Authorize
+      ability="update"
+      entity={["feature"]}
+      failureNotification={{
+        body: t("errors.access_denied.authorization_admin_type", {
+          type: "features"
+        })
+      }}
+      failureRedirect
+    >
+      <EntitiesList
+        title={t("records.features.header")}
+        titleStyle="bar"
+        entityComponent={FeatureRow}
+        entityComponentProps={{
+          onTogglePublish: togglePublished
         }}
-        failureRedirect
-      >
-        <EntitiesList
-          title={t("records.features.header")}
-          titleStyle="bar"
-          entityComponent={FeatureRow}
-          entityComponentProps={{
-            onTogglePublish: this.togglePublished
-          }}
-          entities={features}
-          buttons={[
-            <Button
-              path={lh.link("backendRecordsFeatureNew")}
-              type="add"
-              text={t("records.features.button_label")}
-              authorizedFor="feature"
-            />
-          ]}
-        />
-      </Authorize>
-    );
-  }
+        entities={features}
+        buttons={[
+          <Button
+            key="new"
+            path={lh.link("backendRecordsFeatureNew")}
+            type="add"
+            text={t("records.features.button_label")}
+            authorizedFor="feature"
+          />
+        ]}
+      />
+    </Authorize>
+  );
 }
 
-export default withTranslation()(connectAndFetch(ContentFeaturesList));
+FeaturesList.displayName = "Features.List";
