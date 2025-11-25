ba790f4b5eef0f017a68427e872303ebc4bd5c4d
1aurend <imlcdavidson@gmail.com>
2025-11-25 10:38:35 -0800

    [C] Migrate project routes to v6

--- client/src/backend/containers/project/index.js
+++ client/src/backend/containers/project/index.js
@@ -1,18 +1,15 @@
 import Category from "./category";
-import {
-  List as CollaboratorsList,
-  Add as AddCollaborator
-} from "./collaborators";
+import Collaborators from "./collaborators";
 import Layout from "./Layout";
 import Events from "./Events";
-import ProjectExportations from "./ProjectExportations";
+import Exportations from "./ProjectExportations";
 import Properties from "./Properties";
 import Analytics from "./Analytics";
 import Log from "./Log";
 import Metadata from "./Metadata";
 import New from "./New";
-import ProjectPage from "./ProjectPage";
-import Resource from "./resource";
+// import ProjectPage from "./ProjectPage";
+// import Resource from "./resource";
 import ResourceCollections from "./ResourceCollections";
 import Resources from "./Resources";
 import Text from "./text";
@@ -22,18 +19,17 @@ import Access from "./access";
 
 export default {
   Category,
-  CollaboratorsList,
-  AddCollaborator,
+  Collaborators,
   Layout,
   Events,
-  ProjectExportations,
+  Exportations,
   Properties,
   Log,
   Analytics,
   Metadata,
   New,
-  ProjectPage,
-  Resource,
+  // ProjectPage,
+  // Resource,
   ResourceCollections,
   Resources,
   Text,
