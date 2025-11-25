66f27e474a81081f07c122c80c2684faeaad89b5
1aurend <imlcdavidson@gmail.com>
2025-11-25 10:38:35 -0800

    [C] Migrate project routes to v6

--- client/src/backend/containers/content-block/Form.js
+++ client/src/backend/containers/content-block/Form.js
@@ -1,103 +1,85 @@
-import React, { Component } from "react";
+import { useMemo, useCallback } from "react";
 import PropTypes from "prop-types";
-import { withTranslation } from "react-i18next";
-import connectAndFetch from "utils/connectAndFetch";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 import { projectsAPI, contentBlocksAPI, requests } from "api";
+import { useApiCallback } from "hooks";
 import FormContainer from "global/containers/form";
 import Form from "global/components/form";
 import ContentBlock from "backend/components/content-block";
-import { entityStoreActions } from "actions";
 import lh from "helpers/linkHandler";
 
-const { request } = entityStoreActions;
+export default function ProjectContentFormContainer({
+  contentBlock: contentBlockProp,
+  project
+}) {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
 
-export class ProjectContentFormContainer extends Component {
-  static displayName = "Project.Content.Form";
+  const isPendingBlock = contentBlockProp?.id === "pending";
 
-  static propTypes = {
-    contentBlock: PropTypes.object,
-    project: PropTypes.object,
-    history: PropTypes.object.isRequired,
-    dispatch: PropTypes.func.isRequired,
-    match: PropTypes.object,
-    t: PropTypes.func
-  };
-
-  constructor(props) {
-    super(props);
-    this.contentBlock = this.initializeContentBlock;
-  }
-
-  componentDidUpdate() {
-    if (this.isPendingBlock) return null;
-    this.contentBlock = this.props.contentBlock;
-  }
-
-  get isPendingBlock() {
-    return this.props.contentBlock.id === "pending";
-  }
-
-  get initializeContentBlock() {
-    if (this.isPendingBlock) {
-      return { ...this.props.contentBlock, id: null };
+  const contentBlock = useMemo(() => {
+    if (isPendingBlock) {
+      return { ...contentBlockProp, id: null };
     }
-    return this.props.contentBlock;
-  }
+    return contentBlockProp;
+  }, [contentBlockProp, isPendingBlock]);
 
-  get project() {
-    return this.props.project;
-  }
+  const requestName = isPendingBlock
+    ? requests.beContentBlockCreate
+    : requests.beContentBlockUpdate;
 
-  get requestName() {
-    return this.isPendingBlock
-      ? requests.beContentBlockCreate
-      : requests.beContentBlockUpdate;
-  }
+  const fetchContentBlocks = useApiCallback(projectsAPI.contentBlocks, {
+    requestKey: requests.beProjectContentBlocks
+  });
 
-  closeDrawer = () => {
-    this.fetchContentBlocks();
-    return this.props.history.push(
-      lh.link("backendProjectLayout", this.project.id),
-      { noScroll: true }
-    );
-  };
-
-  fetchContentBlocks = () => {
-    const call = projectsAPI.contentBlocks(this.project.id);
-    const contentBlocksRequest = request(call, requests.beProjectContentBlocks);
-    this.props.dispatch(contentBlocksRequest);
-  };
+  const closeDrawer = useCallback(() => {
+    if (project?.id) {
+      fetchContentBlocks(project.id);
+    }
+    navigate(lh.link("backendProjectLayout", project?.id), {
+      state: { noScroll: true }
+    });
+  }, [fetchContentBlocks, navigate, project?.id]);
 
   // We manually assign these values because they come from the pending block, which acts as the
   // source model. Since those values aren't changed, they are not passed to the update request
   // on their own.
-  create = model => {
-    const adjusted = { ...model };
-    adjusted.attributes.position = this.contentBlock.attributes.position;
-    adjusted.attributes.type = this.contentBlock.attributes.type;
+  const create = useCallback(
+    model => {
+      const adjusted = { ...model };
+      adjusted.attributes.position = contentBlock.attributes.position;
+      adjusted.attributes.type = contentBlock.attributes.type;
 
-    return contentBlocksAPI.create(this.project.id, adjusted);
-  };
+      return contentBlocksAPI.create(project.id, adjusted);
+    },
+    [contentBlock, project]
+  );
 
-  render() {
-    return (
-      <FormContainer.Form
-        model={this.contentBlock}
-        name={this.requestName}
-        update={contentBlocksAPI.update}
-        create={this.create}
-        onSuccess={this.closeDrawer}
-        className="form-secondary"
-        notificationScope="drawer"
-      >
-        <ContentBlock.TypeForm
-          contentBlock={this.props.contentBlock}
-          project={this.project}
-        />
-        <Form.Save text={this.props.t("actions.save")} />
-      </FormContainer.Form>
-    );
-  }
+  if (!contentBlock || !project) return null;
+
+  return (
+    <FormContainer.Form
+      model={contentBlock}
+      name={requestName}
+      update={contentBlocksAPI.update}
+      create={create}
+      onSuccess={closeDrawer}
+      className="form-secondary"
+      notificationScope="drawer"
+    >
+      <ContentBlock.TypeForm
+        contentBlock={contentBlockProp}
+        project={project}
+      />
+      <Form.Save text={t("actions.save")} />
+    </FormContainer.Form>
+  );
 }
 
-export default withTranslation()(connectAndFetch(ProjectContentFormContainer));
+ProjectContentFormContainer.displayName = "Project.Content.Form";
+
+ProjectContentFormContainer.propTypes = {
+  contentBlock: PropTypes.object,
+  project: PropTypes.object
+};
