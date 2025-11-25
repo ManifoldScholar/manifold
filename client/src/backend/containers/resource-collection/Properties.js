157ff90647df7a7a95825069304695dd2c99c2ef
1aurend <imlcdavidson@gmail.com>
2025-11-25 12:14:20 -0800

    [C] Migrate resource collection routes to v6

--- client/src/backend/containers/resource-collection/Properties.js
+++ client/src/backend/containers/resource-collection/Properties.js
@@ -1,87 +1,72 @@
-import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
-import { withTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
+import { useOutletContext } from "react-router-dom";
 import Form from "global/components/form";
 import FormContainer from "global/containers/form";
 import { resourceCollectionsAPI } from "api";
-import { connect } from "react-redux";
 
-export class ResourceCollectionPropertiesContainer extends PureComponent {
-  static displayName = "resourceCollection.Properties";
+const formatData = data => {
+  const { thumbnailAltText, thumbnail, ...rest } = data?.attributes ?? {};
 
-  static propTypes = {
-    resourceCollection: PropTypes.object,
-    params: PropTypes.object,
-    t: PropTypes.func
+  const finalThumbnailData =
+    typeof thumbnailAltText === "string"
+      ? { ...thumbnail, altText: thumbnailAltText }
+      : thumbnail;
+
+  return {
+    ...data,
+    attributes: { thumbnail: finalThumbnailData, ...rest }
   };
+};
 
-  render() {
-    const { resourceCollection, t } = this.props;
-    if (!resourceCollection) return null;
+function ResourceCollectionPropertiesContainer() {
+  const { t } = useTranslation();
+  const { resourceCollection } = useOutletContext();
 
-    const formatData = data => {
-      const { thumbnailAltText, thumbnail, ...rest } = data?.attributes ?? {};
+  if (!resourceCollection) return null;
 
-      const finalThumbnailData =
-        typeof thumbnailAltText === "string"
-          ? { ...thumbnail, altText: thumbnailAltText }
-          : thumbnail;
-
-      return {
-        ...data,
-        attributes: { thumbnail: finalThumbnailData, ...rest }
-      };
-    };
-
-    return (
-      <section>
-        <FormContainer.Form
-          model={resourceCollection}
-          name="backend-collection-update"
-          update={resourceCollectionsAPI.update}
-          create={model =>
-            resourceCollectionsAPI.create(this.props.params.projectId, model)
-          }
-          className="form-secondary"
-          formatData={formatData}
-        >
-          <Form.TextInput
-            label={t("resource_collections.forms.title_label")}
-            name="attributes[title]"
-            placeholder={t("resource_collections.forms.title_placeholder")}
-            {...this.props}
-          />
-          <Form.TextArea
-            label={t("resource_collections.forms.descript_label")}
-            name="attributes[description]"
-            placeholder={t("resource_collections.forms.descript_placeholder")}
-            {...this.props}
-          />
-          <Form.TextInput
-            wide
-            label={t("resource_collections.forms.slug_label")}
-            name="attributes[pendingSlug]"
-            placeholder={t("resource_collections.forms.sluslug_placeholder")}
-          />
-          <Form.Upload
-            layout="landscape"
-            accepts="images"
-            label={t("resource_collections.forms.image_label")}
-            readFrom="attributes[thumbnailStyles][small]"
-            name="attributes[thumbnail]"
-            remove="attributes[removeThumbnail]"
-            altTextName="attributes[thumbnailAltText]"
-            altTextLabel={t("hero.cover_image_alt_label")}
-          />
-          <Form.Save text={t("resource_collections.forms.save")} />
-        </FormContainer.Form>
-      </section>
-    );
-  }
+  return (
+    <section>
+      <FormContainer.Form
+        model={resourceCollection}
+        name="backend-collection-update"
+        update={resourceCollectionsAPI.update}
+        create={resourceCollectionsAPI.create}
+        className="form-secondary"
+        formatData={formatData}
+      >
+        <Form.TextInput
+          label={t("resource_collections.forms.title_label")}
+          name="attributes[title]"
+          placeholder={t("resource_collections.forms.title_placeholder")}
+        />
+        <Form.TextArea
+          label={t("resource_collections.forms.descript_label")}
+          name="attributes[description]"
+          placeholder={t("resource_collections.forms.descript_placeholder")}
+        />
+        <Form.TextInput
+          wide
+          label={t("resource_collections.forms.slug_label")}
+          name="attributes[pendingSlug]"
+          placeholder={t("resource_collections.forms.sluslug_placeholder")}
+        />
+        <Form.Upload
+          layout="landscape"
+          accepts="images"
+          label={t("resource_collections.forms.image_label")}
+          readFrom="attributes[thumbnailStyles][small]"
+          name="attributes[thumbnail]"
+          remove="attributes[removeThumbnail]"
+          altTextName="attributes[thumbnailAltText]"
+          altTextLabel={t("hero.cover_image_alt_label")}
+        />
+        <Form.Save text={t("resource_collections.forms.save")} />
+      </FormContainer.Form>
+    </section>
+  );
 }
 
-export default withTranslation()(
-  connect(ResourceCollectionPropertiesContainer.mapStateToProps)(
-    ResourceCollectionPropertiesContainer
-  )
-);
+ResourceCollectionPropertiesContainer.displayName =
+  "ResourceCollection.Properties";
+
+export default ResourceCollectionPropertiesContainer;
