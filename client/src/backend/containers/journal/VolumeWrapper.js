6e055a04a4bd92ec18f077a4227c45a43e0d6ecc
1aurend <imlcdavidson@gmail.com>
2025-11-24 21:53:34 -0800

    [C] Migrate journal routes to v6

--- client/src/backend/containers/journal/VolumeWrapper.js
+++ client/src/backend/containers/journal/VolumeWrapper.js
@@ -1,16 +1,6 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { childRoutes } from "helpers/router";
-import { withRouter } from "react-router-dom";
+import { Outlet, useOutletContext } from "react-router-dom";
 
-function JournalVolumeWrapper({ route, ...props }) {
-  return <div>{childRoutes(route, { childProps: props })}</div>;
+export default function JournalVolumeWrapper() {
+  const context = useOutletContext() || {};
+  return <Outlet context={context} />;
 }
-
-JournalVolumeWrapper.propTypes = {
-  journal: PropTypes.object,
-  history: PropTypes.object,
-  route: PropTypes.object
-};
-
-export default withRouter(JournalVolumeWrapper);
