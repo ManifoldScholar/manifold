9593f1695c6a0d4023f5a9fd92753f0057b5c26d
1aurend <imlcdavidson@gmail.com>
2025-11-25 12:13:59 -0800

    [C] Migrate resource routes to v6

--- client/src/backend/containers/resource/New.js
+++ client/src/backend/containers/resource/New.js
@@ -1,158 +1,125 @@
-import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
-import { withTranslation } from "react-i18next";
-import connectAndFetch from "utils/connectAndFetch";
+import { useTranslation } from "react-i18next";
+import { useParams, useNavigate } from "react-router-dom";
 import FormContainer from "global/containers/form";
 import Layout from "backend/components/layout";
 import Form from "global/components/form";
 import PageHeader from "backend/components/layout/PageHeader";
 import Resource from "backend/components/resource";
 import GlobalForm from "global/components/form";
-import { requests, projectsAPI, resourcesAPI } from "api";
-import { entityStoreActions } from "actions";
-import { select } from "utils/entityUtils";
+import { projectsAPI, resourcesAPI } from "api";
 import lh from "helpers/linkHandler";
 import HeadContent from "global/components/HeadContent";
 import { RegisterBreadcrumbs } from "global/components/atomic/Breadcrumbs";
 import { getBreadcrumbs } from "./breadcrumbs";
+import { useFetch } from "hooks";
 
 import Authorize from "hoc/Authorize";
 
-const { request } = entityStoreActions;
+const DEFAULT_RESOURCE = { attributes: { kind: "image" } };
 
-export class ResourceNewContainer extends PureComponent {
-  static mapStateToProps = state => {
-    return {
-      project: select(requests.beProject, state.entityStore)
-    };
-  };
+const formatData = data => {
+  const { attributes, relationships } = data ?? {};
+  const { attachment: attachmentData, attachmentAltText, ...rest } = attributes;
 
-  static fetchData = (getState, dispatch, location, match) => {
-    const promises = [];
-    const projectCall = projectsAPI.show(match.params.projectId);
-    const { promise: one } = dispatch(request(projectCall, requests.beProject));
-    promises.push(one);
-    return Promise.all(promises);
-  };
-
-  static displayName = "Resource.New";
-
-  static propTypes = {
-    project: PropTypes.object,
-    history: PropTypes.object,
-    match: PropTypes.object,
-    t: PropTypes.func
-  };
-
-  constructor(props) {
-    super(props);
-    this.defaultResource = { attributes: { kind: "image" } };
-  }
-
-  redirectToResource(resource) {
-    const path = lh.link("backendResource", resource.id);
-    this.props.history.push(path);
-  }
-
-  formatData = data => {
-    const { attributes, relationships } = data ?? {};
-    const {
-      attachment: attachmentData,
-      attachmentAltText,
-      ...rest
-    } = attributes;
-
-    return {
-      relationships,
-      attributes: {
-        ...rest,
-        attachment: {
-          ...attachmentData,
-          altText: attributes.kind === "image" ? attachmentAltText : null
-        }
+  return {
+    relationships,
+    attributes: {
+      ...rest,
+      attachment: {
+        ...attachmentData,
+        altText: attributes.kind === "image" ? attachmentAltText : null
       }
-    };
+    }
+  };
+};
+
+function ResourceNewContainer() {
+  const { t } = useTranslation();
+  const { projectId } = useParams();
+  const navigate = useNavigate();
+
+  const { data: project } = useFetch({
+    request: [projectsAPI.show, projectId]
+  });
+
+  const redirectToResource = resource => {
+    const path = lh.link("backendResource", resource.id);
+    navigate(path);
   };
 
-  handleSuccess = resource => {
-    this.redirectToResource(resource);
+  const handleSuccess = resource => {
+    redirectToResource(resource);
   };
 
-  render() {
-    const { project, t } = this.props;
-    if (!project) return null;
+  if (!project) return null;
 
-    const belongsToJournalIssue = project.attributes.isJournalIssue;
+  const belongsToJournalIssue = project.attributes.isJournalIssue;
 
-    const breadcrumbs = getBreadcrumbs(null, project, belongsToJournalIssue, t);
+  const breadcrumbs = getBreadcrumbs(null, project, belongsToJournalIssue, t);
 
-    const parentProps = {
-      parentTitle: project.attributes.titleFormatted,
-      parentSubtitle: project.attributes.subtitle,
-      parentId: project.id
-    };
+  const parentProps = {
+    parentTitle: project.attributes.titleFormatted,
+    parentSubtitle: project.attributes.subtitle,
+    parentId: project.id
+  };
 
-    return (
-      <Authorize
-        entity={project}
-        ability={"createResources"}
-        failureNotification
-        failureRedirect={lh.link("backendProject", project.id)}
-      >
-        <HeadContent
-          title={`${t(`titles.resource_new`)} | ${t("common.admin")}`}
-          appendDefaultTitle
+  return (
+    <Authorize
+      entity={project}
+      ability={"createResources"}
+      failureNotification
+      failureRedirect={lh.link("backendProject", project.id)}
+    >
+      <HeadContent
+        title={`${t(`titles.resource_new`)} | ${t("common.admin")}`}
+        appendDefaultTitle
+      />
+      <div>
+        <RegisterBreadcrumbs breadcrumbs={breadcrumbs ?? []} />
+        <PageHeader
+          type="resource"
+          backUrl={lh.link("backendProjectResources", project.id)}
+          backLabel={project.attributes.titlePlaintext}
+          title={t("resources.new.title")}
+          note={t("resources.new.instructions")}
+          icon="BEResourcesBox64"
+          {...parentProps}
         />
-        <div>
-          <RegisterBreadcrumbs breadcrumbs={breadcrumbs ?? []} />
-          <PageHeader
-            type="resource"
-            backUrl={lh.link("backendProjectResources", project.id)}
-            backLabel={project.attributes.titlePlaintext}
-            title={t("resources.new.title")}
-            note={t("resources.new.instructions")}
-            icon="BEResourcesBox64"
-            {...parentProps}
-          />
-          <Layout.BackendPanel>
-            <FormContainer.Form
-              model={this.defaultResource}
-              name="backend-resource-create"
-              update={resourcesAPI.update}
-              create={model =>
-                resourcesAPI.create(project.id, this.formatData(model))
-              }
-              onSuccess={this.handleSuccess}
-              className="form-secondary"
-            >
-              <Resource.Form.KindPicker
-                name="attributes[kind]"
-                includeButtons
-              />
-              <Form.TextInput
-                label={t("resources.title_label")}
-                name="attributes[title]"
-                placeholder={t("resources.title_placeholder")}
-                wide
-              />
-              <Form.TextArea
-                label={t("resources.descript_label")}
-                name="attributes[description]"
-                placeholder={t("resources.descript_placeholder")}
-                wide
-              />
-              <Resource.Form.KindAttributes />
-              <GlobalForm.Errorable name="attributes[fingerprint]" />
-              <Form.Save
-                text={t("resources.new.save")}
-                cancelRoute={lh.link("backendProjectResources", project.id)}
-              />
-            </FormContainer.Form>
-          </Layout.BackendPanel>
-        </div>
-      </Authorize>
-    );
-  }
+        <Layout.BackendPanel>
+          <FormContainer.Form
+            model={DEFAULT_RESOURCE}
+            name="backend-resource-create"
+            update={resourcesAPI.update}
+            create={model => resourcesAPI.create(project.id, formatData(model))}
+            onSuccess={handleSuccess}
+            className="form-secondary"
+          >
+            <Resource.Form.KindPicker name="attributes[kind]" includeButtons />
+            <Form.TextInput
+              label={t("resources.title_label")}
+              name="attributes[title]"
+              placeholder={t("resources.title_placeholder")}
+              wide
+            />
+            <Form.TextArea
+              label={t("resources.descript_label")}
+              name="attributes[description]"
+              placeholder={t("resources.descript_placeholder")}
+              wide
+            />
+            <Resource.Form.KindAttributes />
+            <GlobalForm.Errorable name="attributes[fingerprint]" />
+            <Form.Save
+              text={t("resources.new.save")}
+              cancelRoute={lh.link("backendProjectResources", project.id)}
+            />
+          </FormContainer.Form>
+        </Layout.BackendPanel>
+      </div>
+    </Authorize>
+  );
 }
 
-export default withTranslation()(connectAndFetch(ResourceNewContainer));
+ResourceNewContainer.displayName = "Resource.New";
+
+export default ResourceNewContainer;
