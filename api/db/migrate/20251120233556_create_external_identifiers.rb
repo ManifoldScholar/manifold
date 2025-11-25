9e7ea2e537edb6c0fd41423cab6fafe5f485fb1f
Tim Frazee <tim@castironcoding.com>
2025-11-25 10:52:37 -0800

    [E] Add unique index to external_identifiers#identifier

--- api/db/migrate/20251120233556_create_external_identifiers.rb
+++ api/db/migrate/20251120233556_create_external_identifiers.rb
@@ -1,9 +1,11 @@
-class AddExternalIdentifiersTable < ActiveRecord::Migration[7.0]
+class CreateExternalIdentifiers < ActiveRecord::Migration[7.0]
   def change
     create_table :external_identifiers, id: :uuid do |t|
       t.string :identifier, null: false
       t.references :identifiable, polymorphic: true
       t.timestamps
+
+      t.index :identifier, unique: true
     end
   end
 end
