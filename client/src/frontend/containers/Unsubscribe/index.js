5354e34e3d6b0671a2229b3d96ce5cb0148d4087
1aurend <imlcdavidson@gmail.com>
2025-11-25 16:02:55 -0800

    [C] Various clean up

--- client/src/frontend/containers/Unsubscribe/index.js
+++ client/src/frontend/containers/Unsubscribe/index.js
@@ -1,28 +1,21 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { requests, notificationPreferencesAPI } from "api";
-import { entityStoreActions } from "actions";
+import { useParams, useNavigate } from "react-router-dom";
+import { notificationPreferencesAPI } from "api";
+import { useApiCallback } from "hooks";
 import lh from "helpers/linkHandler";
 
-const { request } = entityStoreActions;
-
 export default function UnsubscribeContainer() {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
   const { token } = useParams();
 
+  const unsubscribe = useApiCallback(notificationPreferencesAPI.unsubscribe);
+
   useEffect(() => {
     if (token) {
-      const action = notificationPreferencesAPI.unsubscribe(token);
-      const unsubscribeRequest = request(action, requests.feUnsubscribe);
-      dispatch(unsubscribeRequest);
+      unsubscribe(token);
     }
-
-    const url = lh.link("frontend");
-    navigate(url);
-  }, [dispatch, navigate, token]);
+    navigate(lh.link("frontend"));
+  }, [navigate, token, unsubscribe]);
 
   return null;
 }
