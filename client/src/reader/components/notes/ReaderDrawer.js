83e2626421b60916fa8c6151673f4b14699c4e6f
1aurend <imlcdavidson@gmail.com>
2025-11-26 11:48:27 -0800

    [C] Migrate Reader to v6

--- client/src/reader/components/notes/ReaderDrawer.js
+++ client/src/reader/components/notes/ReaderDrawer.js
@@ -1,51 +1,41 @@
-import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 import FilteredList from "./FilteredList";
 import ReaderNotes from "reader/containers/ReaderNotes";
 import GlobalDrawer from "global/containers/drawer";
-import { withTranslation } from "react-i18next";
 
-class ReaderDrawer extends PureComponent {
-  static displayName = "Notes.ReaderDrawer";
+export default function ReaderDrawer({ visible, closeCallback }) {
+  const { t } = useTranslation();
 
-  static propTypes = {
-    match: PropTypes.object,
-    history: PropTypes.object,
-    visible: PropTypes.bool,
-    t: PropTypes.func
+  const drawerProps = {
+    open: visible,
+    context: "reader",
+    size: "wide",
+    position: "overlay",
+    padding: "none",
+    identifier: "notes-drawer",
+    lockScroll: "always",
+    includeDrawerFrontMatter: true,
+    focusTrap: false,
+    title: t("glossary.note_title_case_other"),
+    closeCallback,
+    additionalDrawerProps: { "aria-modal": true }
   };
 
-  renderNotesDrawerContents() {
-    if (!this.props.visible) return null;
-    return (
-      <ReaderNotes match={this.props.match} history={this.props.history}>
-        <FilteredList />
-      </ReaderNotes>
-    );
-  }
-
-  render() {
-    const drawerProps = {
-      open: this.props.visible,
-      context: "reader",
-      size: "wide",
-      position: "overlay",
-      padding: "none",
-      identifier: "notes-drawer",
-      lockScroll: "always",
-      includeDrawerFrontMatter: true,
-      focusTrap: false,
-      title: this.props.t("glossary.note_title_case_other"),
-      closeCallback: this.props.closeCallback,
-      additionalDrawerProps: { "aria-modal": true }
-    };
-
-    return (
-      <GlobalDrawer.Wrapper {...drawerProps}>
-        {this.renderNotesDrawerContents()}
-      </GlobalDrawer.Wrapper>
-    );
-  }
+  return (
+    <GlobalDrawer.Wrapper {...drawerProps}>
+      {visible && (
+        <ReaderNotes>
+          <FilteredList />
+        </ReaderNotes>
+      )}
+    </GlobalDrawer.Wrapper>
+  );
 }
 
-export default withTranslation()(ReaderDrawer);
+ReaderDrawer.displayName = "Notes.ReaderDrawer";
+
+ReaderDrawer.propTypes = {
+  visible: PropTypes.bool,
+  closeCallback: PropTypes.func
+};
