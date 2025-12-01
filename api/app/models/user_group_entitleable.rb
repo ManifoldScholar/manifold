2a8905c61194f2d42bc442b43fb0b84e72facaa5
Tim Frazee <tim@castironcoding.com>
2025-11-30 23:27:01 -0800

    [F] Add user group entitleable controller

--- api/app/models/user_group_entitleable.rb
+++ api/app/models/user_group_entitleable.rb
@@ -1,4 +1,19 @@
 class UserGroupEntitleable < ApplicationRecord
   belongs_to :user_group, inverse_of: :user_group_entitleables
   belongs_to :entitleable, polymorphic: true
+
+  attr_reader :target_url
+
+  before_save :maybe_derive_entitleable
+
+  private
+
+  def maybe_derive_entitleable
+    return unless target_url.present?
+
+    self.entitleable = GlobalID.parse(target_url).find
+  rescue ActiveRecord::RecordNotFound
+    self.entitleable = nil
+    Rils.logger.warn("Entitleable not found for GID #{target_url}")
+  end
 end
