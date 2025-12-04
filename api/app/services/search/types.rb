e1aee055007d5b00609d6f2cfd6871b4a2173d1c
Alexa Grey <alexa@castironcoding.com>
2025-12-04 11:40:01 -0800

    [E] Treat projects that are issues separately when indexing
    Resolves MNFLD-1087

--- api/app/services/search/types.rb
+++ api/app/services/search/types.rb
@@ -1,8 +1,33 @@
 # frozen_string_literal: true
 
 module Search
+  # Types for dealing with searching.
   module Types
     include Dry.Types
+
+    # A list of acceptable facets.
+    # @see Search::Faceter
+    FACETS = %w[
+      Annotation
+      Journal
+      Project
+      Resource
+      Text
+      TextSection
+    ].freeze
+
+    # @see FACETS
+    # @see Search::Faceter
+    Facet = String.enum(*FACETS)
+
+    # @api private
+    SafeFacet = Facet.optional.fallback(nil)
+
+    # @see FACETS
+    # @see Search::Faceter
+    Facets = Coercible::Array.of(Facet).constructor do |value|
+      ::Kernel.Array(value).map { SafeFacet[_1] }.compact.uniq.sort
+    end
 
     KeywordSearchOptions = ::Types::Hash.map(::Types::Symbol, ::Types::Any)
 
