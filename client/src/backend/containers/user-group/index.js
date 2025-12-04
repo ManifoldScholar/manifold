ed5b2d1973dbb26372604442f9622c36bba80ba3
1aurend <imlcdavidson@gmail.com>
2025-12-04 15:26:22 -0800

    [E] Add i18n keys + various clean up

--- client/src/backend/containers/user-group/index.js
+++ client/src/backend/containers/user-group/index.js
@@ -1,6 +1,6 @@
 import Wrapper from "./Wrapper";
 import Properties from "./Properties";
 import Users from "./Users";
-import Entitlements from "./Entitlements";
+import Entitlements from "./entitlements";
 
 export default { Wrapper, Properties, Users, Entitlements };
