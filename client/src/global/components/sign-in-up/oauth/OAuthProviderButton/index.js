3cfed6c2acabbac891d482fe38560a339b2d1c7d
1aurend <imlcdavidson@gmail.com>
2025-12-03 22:44:30 -0800

    [F] Read IDP values from settings

--- client/src/global/components/sign-in-up/oauth/OAuthProviderButton/index.js
+++ client/src/global/components/sign-in-up/oauth/OAuthProviderButton/index.js
@@ -1,24 +1,21 @@
 import PropTypes from "prop-types";
-import { oauthActions } from "actions";
-import { useDispatch } from "react-redux";
 import { useTranslation } from "react-i18next";
 import IconComposer from "global/components/utility/IconComposer";
+import config from "config";
 import * as Styled from "./styles";
 
 export default function OAuthProviderButton({ provider, icon }) {
   const { t } = useTranslation();
-  const dispatch = useDispatch();
 
   return (
     <Styled.Button
       className="button-secondary button-secondary--dark"
-      onClick={() => dispatch(oauthActions.prompt(provider))}
-      type="button"
+      href={`${config.services.api}${provider.url}`}
     >
       <IconComposer icon={icon} size={26} className="button-secondary__icon" />
       <span className="button-secondary__text">
         {t("forms.signin_overlay.log_in_with_provider", {
-          provider
+          provider: provider.name
         })}
       </span>
     </Styled.Button>
@@ -28,6 +25,9 @@ export default function OAuthProviderButton({ provider, icon }) {
 OAuthProviderButton.displayName = "Global.SignInUp.OAuthProviderButton";
 
 OAuthProviderButton.propTypes = {
-  provider: PropTypes.string,
+  provider: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+  }).isRequired,
   icon: PropTypes.string
 };
