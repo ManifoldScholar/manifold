125319b1a496ef19a195fd6420018e0c1da20558
1aurend <imlcdavidson@gmail.com>
2025-12-03 17:47:52 -0800

    [E] Expose external_identifier for user_group/project/journal

--- api/app/serializers/v1/user_group_serializer.rb
+++ api/app/serializers/v1/user_group_serializer.rb
@@ -9,6 +9,7 @@ module V1
     typed_attribute :id, Types::Serializer::ID
     typed_attribute :created_at, Types::DateTime.meta(read_only: true)
     typed_attribute :name, Types::String.meta(example: "Manifold SAML users")
+    typed_attribute :external_identifier, Types::String
 
     typed_has_many :users, serializer: ::V1::UserSerializer, record_type: "user"
     typed_has_many :entitlement_subjects,
