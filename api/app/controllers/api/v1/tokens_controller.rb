bbd14f333871a973341bf1d5ecfde015383250fd
Tim Frazee <tim@castironcoding.com>
2025-12-05 08:50:01 -0800

    [F] Add support for post-oauth redirect and cookie-based token retrieval

--- api/app/controllers/api/v1/tokens_controller.rb
+++ api/app/controllers/api/v1/tokens_controller.rb
@@ -9,14 +9,28 @@ module API
       include Authentication
 
       def create
-        authenticated_user = User.find_by(email: token_params[:email])
-          .try(:authenticate, token_params[:password])
+        authenticated_user = user_by_credentials || user_by_auth_code
         render_authenticated_user(authenticated_user)
       rescue StandardError => e
         render_error_response e
       end
 
       private
+
+      def user_by_credentials
+        return if token_params[:email].blank?
+
+        User.find_by(email: token_params[:email]).try(:authenticate, token_params[:password])
+      end
+
+      def user_by_auth_code
+        return if session[:auth_code].blank?
+
+        user_id = Rails.cache.delete(session.delete(:auth_code))
+        return if user_id.blank?
+
+        User.find_by(id: user_id)
+      end
 
       def token_params
         params.permit(:email, :password)
