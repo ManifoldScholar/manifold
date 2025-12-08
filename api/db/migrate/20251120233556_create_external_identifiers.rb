6767d0eb49bf8a7166c5e99b35a7bf3d332cb563
Tim Frazee <tim@castironcoding.com>
2025-12-08 11:45:05 -0800

    [B] Fix external identifier identifiable relation

--- api/db/migrate/20251120233556_create_external_identifiers.rb
+++ api/db/migrate/20251120233556_create_external_identifiers.rb
@@ -2,7 +2,7 @@ class CreateExternalIdentifiers < ActiveRecord::Migration[7.0]
   def change
     create_table :external_identifiers, id: :uuid do |t|
       t.string :identifier, null: false
-      t.references :identifiable, polymorphic: true
+      t.references :identifiable, polymorphic: true, type: :uuid
       t.timestamps
 
       t.index :identifier, unique: true
