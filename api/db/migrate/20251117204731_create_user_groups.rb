d97d9543fa61963096e5205287e64ae8690a5820
Tim Frazee <tim@castironcoding.com>
2025-12-08 11:45:57 -0800

    [B] Fix user group membership source relation

--- api/db/migrate/20251117204731_create_user_groups.rb
+++ api/db/migrate/20251117204731_create_user_groups.rb
@@ -14,14 +14,16 @@ class CreateUserGroups < ActiveRecord::Migration[7.0]
 
       t.timestamps null: false, default: -> { "CURRENT_TIMESTAMP" }
 
-      t.index [:user_group_id, :entitleable_type, :entitleable_id], unique: true
+      t.index [:user_group_id, :entitleable_type, :entitleable_id],
+              unique: true,
+              name: "index_user_group_entitleables_on_user_group_and_entitleable"
     end
 
     create_table :user_group_memberships, id: :uuid do |t|
       t.references :user, type: :uuid, null: false
       t.references :user_group, type: :uuid, null: false
 
-      t.text :source
+      t.references :source, polymorphic: true, type: :uuid
 
       t.timestamps null: false, default: -> { "CURRENT_TIMESTAMP" }
 
