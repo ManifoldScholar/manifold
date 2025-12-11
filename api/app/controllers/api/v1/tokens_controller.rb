f023861d49813f2fccd6d0564df55088f13064a1
Tim Frazee <tim@castironcoding.com>
2025-12-11 12:22:49 -0800

    [F] Move Oauth auth token from session to cookie
    - Makes cookie readable from FE
    - Todo: restrict cookie domain

--- api/app/controllers/api/v1/tokens_controller.rb
+++ api/app/controllers/api/v1/tokens_controller.rb
@@ -7,6 +7,7 @@ module API
     # Authentication token controller
     class TokensController < ApplicationController
       include Authentication
+      include ManagesOauthCookie
 
       def create
         authenticated_user = user_by_credentials || user_by_auth_code
@@ -23,11 +24,14 @@ module API
         User.find_by(email: token_params[:email]).try(:authenticate, token_params[:password])
       end
 
+      # Look up user from cache based on one-time-use auth_code
       def user_by_auth_code
-        return if session[:auth_code].blank?
+        return if params[:auth_code].blank?
 
-        user_id = Rails.cache.delete(session.delete(:auth_code))
+        user_id = get_user_id_for_auth_code
         return if user_id.blank?
+
+        clean_up_auth_code!
 
         User.find_by(id: user_id)
       end
@@ -35,6 +39,7 @@ module API
       def token_params
         params.permit(:email, :password)
       end
+
     end
   end
 end
