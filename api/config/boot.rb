ab8023fd61322fe598becb7f7caf645653fd49d6
Tim Frazee <tim@castironcoding.com>
2025-12-16 13:23:27 -0800

    [C] Lint

--- api/config/boot.rb
+++ api/config/boot.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
+
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "logger" # Fix concurrent-ruby removing logger dependency which Rails itself does not have
 
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
-
-require "bundler/setup" # Set up gems listed in the Gemfile.
 
 lib_dir = File.expand_path("../lib", __dir__)
 
