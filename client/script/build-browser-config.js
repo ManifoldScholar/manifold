39015d1096c35e14c633235684b20afcc771f9da
Tim Frazee <tim@castironcoding.com>
2026-02-10 11:34:52 -0800

    [C] Lint

--- client/script/build-browser-config.js
+++ client/script/build-browser-config.js
@@ -10,9 +10,11 @@ const output = compileEnv(template);
 const writePath = `${paths.build}/www/`;
 
 /* eslint-disable no-console */
-mkdirp(writePath).then(ignored => {
-  fs.writeFileSync(`${paths.build}/www/browser.config.js`, output);
-}).catch(err => {
-  console.error("Unable to mkdir at " + writePath + ": " + err);
-});
+mkdirp(writePath)
+  .then(ignored => {
+    fs.writeFileSync(`${paths.build}/www/browser.config.js`, output);
+  })
+  .catch(err => {
+    console.error("Unable to mkdir at " + writePath + ": " + err);
+  });
 /* eslint-enable no-console */
