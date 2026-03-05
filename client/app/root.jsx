02874f7d1faf21dafbe8e241a956ad6d4f1233b7
1aurend <imlcdavidson@gmail.com>
2026-03-05 12:27:40 -0800

    [C] Rename static -> public; add link tags to root

--- client/app/root.jsx
+++ client/app/root.jsx
@@ -49,6 +49,10 @@ export default function Root({ loaderData }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         {typekitId && (
           <>
             <link
