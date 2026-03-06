4c0d964d8776e4cff65ad002af192352e6375a83
1aurend <imlcdavidson@gmail.com>
2026-03-05 17:23:53 -0800

    [B] Allow nil on license

--- api/spec/enums/license_spec.rb
+++ api/spec/enums/license_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe License, type: :enum do
+  subject { described_class.new }
+
   describe License::CcBy do
     its(:name) { is_expected.to eq "CC BY" }
   end
