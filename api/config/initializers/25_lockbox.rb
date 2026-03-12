f7c38f1fdecf0284f8ddf66ed2a3d5d326f57cb4
Tim Frazee <tim@castironcoding.com>
2026-03-12 11:13:06 -0700

    [F] Add optional override env var for Lockbox master key

--- api/config/initializers/25_lockbox.rb
+++ api/config/initializers/25_lockbox.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
-secret_key_base = Rails.env.test? ? Lockbox.generate_key : Rails.application.secret_key_base
+lockbox_master_key = if Rails.env.test?
+  Lockbox.generate_key
+elsif ENV["LOCKBOX_MASTER_KEY"].present?
+  ENV["LOCKBOX_MASTER_KEY"]
+else
+  Rails.application.secret_key_base
+end
 
 # Consistently ensure that it's a 64-character hexadecimal key
-enforced_master_key = secret_key_base.gsub(/[^a-zA-Z0-9]+/, "")[/\A([a-zA-Z0-9]{1,64})/, 1].rjust(64, "0")
+enforced_master_key = lockbox_master_key.gsub(/[^a-zA-Z0-9]+/, "")[/\A([a-zA-Z0-9]{1,64})/, 1].rjust(64, "0")
 
 Lockbox.master_key = enforced_master_key
