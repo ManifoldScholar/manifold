2f21e63a5034b0ef3342cdebee13319a064f3b96
Tim Frazee <tim@castironcoding.com>
2026-03-18 14:07:58 -0700

    [C] Lint

--- api/config/initializers/25_lockbox.rb
+++ api/config/initializers/25_lockbox.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 lockbox_master_key = if Rails.env.test?
-  Lockbox.generate_key
-elsif ENV["LOCKBOX_MASTER_KEY"].present?
-  ENV["LOCKBOX_MASTER_KEY"]
-else
-  Rails.application.secret_key_base
-end
+                       Lockbox.generate_key
+                     elsif ENV["LOCKBOX_MASTER_KEY"].present?
+                       ENV["LOCKBOX_MASTER_KEY"]
+                     else
+                       Rails.application.secret_key_base
+                     end
 
 # Consistently ensure that it's a 64-character hexadecimal key
 enforced_master_key = lockbox_master_key.gsub(/[^a-zA-Z0-9]+/, "")[/\A([a-zA-Z0-9]{1,64})/, 1].rjust(64, "0")
