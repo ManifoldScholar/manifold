1cebb739f395344cdc513c87172da232511d203d
1aurend <imlcdavidson@gmail.com>
2026-04-06 10:37:21 -0700

    [B] Don't set oai settings default in migration

--- api/db/migrate/20260127185424_add_oai_to_settings.rb
+++ api/db/migrate/20260127185424_add_oai_to_settings.rb
@@ -1,9 +1,5 @@
 class AddOAIToSettings < ActiveRecord::Migration[7.0]
   def change
-    add_column :settings, :oai, :jsonb, default: {
-      repository_name: "Manifold",
-      admin_email: "admin@manifold.app",
-      directory_enabled: true
-    }
+    add_column :settings, :oai, :jsonb, default: {}
   end
 end
