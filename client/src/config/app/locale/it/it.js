d2b6d88df280bab3db5b94d5ae54d052f62e18a6
1aurend <imlcdavidson@gmail.com>
2026-04-06 15:52:15 -0700

    [B] Fix json imports

--- client/src/config/app/locale/it/it.js
+++ client/src/config/app/locale/it/it.js
@@ -1,10 +1,18 @@
 /* eslint-disable import/extensions */
 import base from "./json/base.json";
+import backend from "./json/backend";
+import reader from "./json/reader";
+import frontend from "./json/frontend";
+import shared from "./json/shared";
 import it from "date-fns/locale/it";
 
 export default {
   translation: {
     ...base,
+    ...backend,
+    ...frontend,
+    ...reader,
+    ...shared,
     date_fns: it
   }
 };
