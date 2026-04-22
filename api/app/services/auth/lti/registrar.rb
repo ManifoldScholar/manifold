cdae18680c8ca14dbde5085fb10e8fe731d049d1
Tim Frazee <tim@castironcoding.com>
2026-04-22 15:11:51 -0700

    [F] Conditionally create LTI Deployment during registration
    If the platform provides a deployment_id

--- api/app/services/auth/lti/registrar.rb
+++ api/app/services/auth/lti/registrar.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
 
 module Auth
   module Lti
+    # Handles registration of a LMS platform via the LTI 1.3 standard
     class Registrar
       include Rails.application.routes.url_helpers
 
@@ -12,19 +14,38 @@ module Auth
         "https://purl.imsglobal.org/spec/lti-ags/scope/score" => "ags_score"
       }.freeze
 
+      NRPS_READONLY_SCOPE = "https://purl.imsglobal.org/spec/lti-nrps/scope/contextmembership.readonly"
+      DL_MESSAGE_TYPE     = "LtiDeepLinkingRequest"
+
+      TOOL_CONFIGURATION_KEY = "https://purl.imsglobal.org/spec/lti-tool-configuration"
+
+      REQUIRED_OPENID_CONFIG_KEYS = %i[
+        registration_endpoint
+        issuer
+        authorization_endpoint
+        token_endpoint
+        jwks_uri
+      ].freeze
+
       attr_reader :errors, :openid_configuration_url, :referrer, :registration_token
 
+      # Accepts params from an LTI 1.3 autoregistration flow
+      # @param params [#[]] params containing keys :registration_token and :openid_configuration
       def initialize(params)
         @registration_token = params[:registration_token]
         @openid_configuration_url = params[:openid_configuration]
         @options = params[:options]
-        @errors = []
+        @errors = Set.new
       end
 
       def register_platform!
+        ensure_valid_configuration!
         return unless valid?
 
-        configure_platform!
+        platform_configuration
+        return unless valid?
+
+        persist_registration!
       end
 
       def success?
@@ -35,31 +56,42 @@ module Auth
         !success?
       end
 
-      def valid?
-        # errors << "invalid host" if openid_uri.host != referrer_uri.host
-        errors << "unable to fetch openid configuration" if openid_configuration.blank?
-        errors << "invalid issuer" if openid_configuration.present? && openid_uri.host != URI.parse(openid_configuration[:issuer]).host
-        errors << "autoregistration for this platform is not allowed" unless issuer_allowed?
+      # @return [void]
+      def ensure_valid_configuration!
+        unless issuer_allowed?
+          errors << "Autoregistration for this platform is not allowed"
+          return false
+        end
 
+        errors << "Unable to fetch openid configuration" unless openid_configuration_valid?
+
+        errors << "Invalid issuer" if openid_configuration.present? && openid_uri.host != URI.parse(openid_configuration[:issuer]).host
+      end
+
+      # @return [Boolean]
+      def valid?
         errors.none?
       end
 
-      def configure_platform!
-        persist_registration!
-      end
+      private
 
-      def lti_tool_configuration
-        platform_configuration[:"https://purl.imsglobal.org/spec/lti-tool-configuration"] || {}
-      end
-
+      # Fetches the OIDC configuration from the LMS-provided URL
+      # @return [Hash]
       def openid_configuration
-        @openid_configuration ||= HTTParty.get(@openid_configuration_url).deep_symbolize_keys
+        @openid_configuration ||= HTTParty.get(@openid_configuration_url).then do |response|
+          handle_response!(response)
+        rescue HTTParty::Foul, Socket::ResolutionError => e
+          @errors << e.message
+        end
       end
 
+      # @return [String, nil]
       def registration_endpoint
         @registration_endpoint ||= openid_configuration[:registration_endpoint]
       end
 
+      # POSTs Manifold's tool configuration to the LMS Platform and returns the memoized response JSON
+      # @return [Hash]
       def platform_configuration
         @platform_configuration ||= HTTParty.post(
           registration_endpoint,
@@ -68,9 +100,14 @@ module Auth
             "Content-Type" => "application/json",
             "Authorization" => "Bearer #{registration_token}"
           }
-        ).deep_symbolize_keys
+        ).then do |response|
+          handle_response!(response)
+        rescue HTTParty::Foul, Socket::ResolutionError => e
+          @errors << e.message
+        end
       end
 
+      # @return [Hash<Symbol, Any>]
       def platform_registration_payload
         {
           application_type: "web",
@@ -81,58 +118,89 @@ module Auth
           client_name: settings.general.installation_name,
           token_endpoint_auth_method: "private_key_jwt",
           jwks_uri:,
+          scope: NRPS_READONLY_SCOPE,
           logo_uri: settings.press_logo&.url,
-          "https://purl.imsglobal.org/spec/lti-tool-configuration" => {
+          TOOL_CONFIGURATION_KEY => {
             domain: client_host,
             target_link_uri: client_uri.to_s,
+            description: settings.general.installation_name,
             claims: %w[iss sub name given_name family_name email],
             messages: [
               {
                 type: "LtiResourceLinkRequest",
                 target_link_uri: client_uri.to_s,
-                label: "Link to #{settings.general.installation_name}"
+                label: "Link to #{settings.general.installation_name}",
+                placements: ["link_selection", "course_navigation"],
+                icon_uri: settings.press_logo&.url
+              },
+              {
+                type: DL_MESSAGE_TYPE,
+                target_link_uri: client_uri.to_s,
+                label: "Select multiple resources from #{settings.general.installation_name}",
+                placements: [
+                  "link_selection",
+                  "editor_button",
+                  "assignment_selection",
+                  "course_assignments_menu",
+                  "module_index_menu_modal"
+                ],
+                icon_uri: settings.press_logo&.url
               }
             ]
           }
         }.compact
       end
 
+      def persist_registration!
+        lti_registration
+        return unless valid?
+
+        lti_deployment if deployment_id.present?
+      end
+
+      # Finds or creates an LTI registration for the issuer and client
+      # @return [LtiRegistration, nil]
       def lti_registration
-        @lti_registration ||= LtiRegistration.find_or_initialize_by(
+        @lti_registration ||= LtiRegistration.find_or_create_by!(
           issuer: openid_configuration[:issuer],
           client_id: platform_configuration[:client_id]
-        )
-      end
-
-      def lti_deployment
-        @lti_deployment ||= lti_registration.lti_deployments.find_or_create_by!(
-          deployment_id: platform_configuration[:deployment_id]
-        )
-      end
-
-      def persist_registration!
-        lti_registration.update!(
-          name: lti_registration.name || URI.parse(openid_configuration[:issuer]).host,
-          authorization_endpoint: openid_configuration[:authorization_endpoint],
-          token_endpoint: openid_configuration[:token_endpoint],
-          jwks_uri: openid_configuration[:jwks_uri],
-          token_endpoint_auth_method: platform_configuration[:token_endpoint_auth_method],
-          grant_types: platform_configuration[:grant_types] || [],
-          scopes: normalize_scopes(platform_configuration[:scope]),
-          registration_access_token: platform_configuration[:registration_access_token]
-        )
-
-        lti_deployment
+        ) do |registration|
+          registration.name = URI.parse(openid_configuration[:issuer]).host
+          registration.authorization_endpoint = openid_configuration[:authorization_endpoint]
+          registration.token_endpoint = openid_configuration[:token_endpoint]
+          registration.jwks_uri = openid_configuration[:jwks_uri]
+          registration.token_endpoint_auth_method = platform_configuration[:token_endpoint_auth_method]
+          registration.grant_types = platform_configuration[:grant_types] || []
+          registration.scopes = normalize_scopes(platform_configuration[:scope])
+          registration.registration_access_token = platform_configuration[:registration_access_token]
+        end
       rescue ActiveRecord::RecordInvalid => e
         @errors.concat(e.record.errors.full_messages)
       end
 
+      # Finds or creates an LTI deployment for the registration
+      # Fails silently if the registration payload does not include a deployment_id
+      # @return [LtiDeployment, nil]
+      def lti_deployment
+        @lti_deployment ||= LtiDeployment.create!(
+          lti_registration:,
+          deployment_id:
+        )
+      end
+
+      # @return [String, nil]
+      def deployment_id
+        @deployment_id ||= platform_configuration[:deployment_id]
+      end
+
+      # @return [Array<String>]
       def normalize_scopes(scope_string)
         return [] if scope_string.blank?
 
         scope_string.to_s.split.filter_map { |uri| SCOPE_MAP[uri] }
       end
 
+      # Is the issuer allowed to register according to settings?
       def issuer_allowed?
         return false if openid_configuration.blank?
 
@@ -141,51 +209,84 @@ module Auth
         allowlist.blank? || allowlist.include?(openid_configuration[:issuer])
       end
 
+      # Does the OIDC config exist and contain all necessary keys?
+      def openid_configuration_valid?
+        openid_configuration.present? &&
+          REQUIRED_OPENID_CONFIG_KEYS.all? { openid_configuration[_1].present? }
+      end
+
+      # @return [URI::HTTP]
       def api_uri
         URI.parse(Rails.application.config.manifold.api_url)
       end
 
+      # @return [URI::HTTP]
       def client_uri
         URI.parse(Rails.application.config.manifold.url)
       end
 
       # TODO: Maybe set default_url_options
+      # @return [String]
       def initiate_login_uri
         auth_redirect_url(:lti, host: api_host)
       end
 
+      # @return [Array<String>]
       def redirect_uris
         [auth_callback_url(:lti, host: api_host)]
       end
 
+      # @return [String]
       def jwks_uri
         auth_jwks_url(host: api_host)
       end
 
+      # @return [URI::HTTP]
       def openid_uri
         @openid_uri ||= URI.parse(openid_configuration_url)
       end
 
+      # @return [URI::HTTP]
       def referrer_uri
         @referrer_uri ||= URI.parse(referrer)
       end
 
+      # The API host with port appended only if localhost
+      # @return [String]
       def api_host
         return api_uri.host unless api_uri.host == "localhost"
 
         "#{api_uri.host}:#{api_uri.port}"
       end
 
+      # The client host with port appended only if localhost
+      # @return [String]
       def client_host
-          return client_uri.host unless client_uri.host == "localhost"
+        return client_uri.host unless client_uri.host == "localhost"
 
         "#{client_uri.host}:#{client_uri.port}"
       end
 
+      # @return [Settings]
       def settings
         @settings ||= Settings.current
       end
 
+      # Handles JSON responses from the LMS platform and catches error states
+      # Raises an exception if the response is invalid
+      # Exception handling must occur in the calling method
+      # @raise [HTTParty::Foul]
+      # @param response [HTTParty::Response] the response object
+      # @param error_message [String] the message to print to the user in case of error - status code will be appended
+      # @return [Hash<Symbol, Any>]
+      def handle_response!(response, error_message: "A registration error occurred")
+        if response.success? && response.content_type.include?("json")
+          response.deep_symbolize_keys
+        else
+          Rails.logger.error("LTI Registration error: #{r.inspect}")
+          raise HTTParty::Foul, "#{error_message} (HTTP status #{response.code})"
+        end
+      end
     end
   end
 end
--- api/spec/services/auth/lti/registrar_spec.rb
+++ api/spec/services/auth/lti/registrar_spec.rb
@@ -24,9 +24,7 @@ RSpec.describe Auth::Lti::Registrar do
       grant_types: %w[implicit client_credentials],
       scope: "https://purl.imsglobal.org/spec/lti-nrps/scope/contextmembership.readonly https://purl.imsglobal.org/spec/lti-ags/scope/lineitem",
       registration_access_token: "access_token_456",
-      "https://purl.imsglobal.org/spec/lti-tool-configuration": {
-        deployment_id: "deploy_1"
-      }
+      "https://purl.imsglobal.org/spec/lti-tool-configuration": {}
     }
   end
 
@@ -47,17 +45,13 @@ RSpec.describe Auth::Lti::Registrar do
       .to_return(status: 200, body: platform_registration_response.to_json, headers: { "Content-Type" => "application/json" })
   end
 
-  describe "#configure_platform!" do
+  describe "#register_platform!" do
     it "creates an LtiRegistration" do
-      expect { registrar.configure_platform! }.to change(LtiRegistration, :count).by(1)
-    end
-
-    it "creates an LtiDeployment" do
-      expect { registrar.configure_platform! }.to change(LtiDeployment, :count).by(1)
+      expect { registrar.register_platform! }.to change(LtiRegistration, :count).by(1)
     end
 
     it "persists the correct registration attributes" do
-      registrar.configure_platform!
+      registrar.register_platform!
 
       expect(LtiRegistration.last).to have_attributes(
         issuer: issuer,
@@ -72,20 +66,11 @@ RSpec.describe Auth::Lti::Registrar do
       )
     end
 
-    it "persists the correct deployment attributes" do
-      registrar.configure_platform!
-
-      expect(LtiDeployment.last).to have_attributes(
-        deployment_id: "deploy_1",
-        lti_registration_id: LtiRegistration.last.id
-      )
-    end
-
-    context "when the registration is invalid" do
+    context "when the OIDC issuer host does not match the openid_configuration URL host" do
       let(:openid_config_response) do
         {
-          issuer: issuer,
-          authorization_endpoint: nil,
+          issuer: "https://other-domain.example.com",
+          authorization_endpoint: "#{issuer}/api/lti/authorize_redirect",
           token_endpoint: "#{issuer}/login/oauth2/token",
           jwks_uri: "#{issuer}/api/lti/security/jwks",
           registration_endpoint: "#{issuer}/api/lti/registrations"
@@ -93,8 +78,13 @@ RSpec.describe Auth::Lti::Registrar do
       end
 
       it "captures errors and reports failure" do
-        registrar.configure_platform!
+        registrar.register_platform!
         expect(registrar).to be_failure
+        expect(registrar.errors).to include("Invalid issuer")
+      end
+
+      it "does not create a registration" do
+        expect { registrar.register_platform! }.not_to change(LtiRegistration, :count)
       end
     end
 
@@ -109,36 +99,64 @@ RSpec.describe Auth::Lti::Registrar do
       end
 
       it "does not create a new registration" do
-        expect { registrar.configure_platform! }.not_to change(LtiRegistration, :count)
+        expect { registrar.register_platform! }.not_to change(LtiRegistration, :count)
       end
 
-      it "updates the mutable registration fields" do
-        registrar.configure_platform!
+      it "preserves the existing registration fields" do
+        registrar.register_platform!
         existing_registration.reload
 
-        expect(existing_registration.authorization_endpoint).to eq "#{issuer}/api/lti/authorize_redirect"
+        expect(existing_registration.authorization_endpoint).to eq "#{issuer}/old/authorize"
       end
 
       it "preserves the existing name" do
-        registrar.configure_platform!
+        registrar.register_platform!
         existing_registration.reload
 
         expect(existing_registration.name).to eq "Custom Name"
       end
+    end
+  end
 
-      it "creates a new deployment" do
-        expect { registrar.configure_platform! }.to change(LtiDeployment, :count).by(1)
-      end
+  describe "#platform_registration_payload" do
+    it "advertises the NRPS readonly scope at the top level" do
+      registrar.register_platform!
 
-      context "when the deployment already exists" do
-        let!(:existing_deployment) do
-          FactoryBot.create(:lti_deployment, lti_registration: existing_registration, deployment_id: "deploy_1")
-        end
+      expect(WebMock).to have_requested(:post, "#{issuer}/api/lti/registrations").with { |req|
+        body = JSON.parse(req.body)
+        body["scope"] == Auth::Lti::Registrar::NRPS_READONLY_SCOPE
+      }
+    end
 
-        it "does not create a duplicate deployment" do
-          expect { registrar.configure_platform! }.not_to change(LtiDeployment, :count)
-        end
-      end
+    it "advertises LtiDeepLinkingRequest in the tool-configuration messages array" do
+      registrar.register_platform!
+
+      expect(WebMock).to have_requested(:post, "#{issuer}/api/lti/registrations").with { |req|
+        body = JSON.parse(req.body)
+        messages = body.dig("https://purl.imsglobal.org/spec/lti-tool-configuration", "messages")
+        messages.is_a?(Array) && messages.any? { |m| m["type"] == "LtiDeepLinkingRequest" }
+      }
+    end
+
+    it "preserves the existing LtiResourceLinkRequest message entry" do
+      registrar.register_platform!
+
+      expect(WebMock).to have_requested(:post, "#{issuer}/api/lti/registrations").with { |req|
+        body = JSON.parse(req.body)
+        messages = body.dig("https://purl.imsglobal.org/spec/lti-tool-configuration", "messages")
+        messages.any? { |m| m["type"] == "LtiResourceLinkRequest" }
+      }
+    end
+
+    it "uses the tool client_uri as target_link_uri for the DL message" do
+      registrar.register_platform!
+
+      expect(WebMock).to have_requested(:post, "#{issuer}/api/lti/registrations").with { |req|
+        body = JSON.parse(req.body)
+        dl_msg = body.dig("https://purl.imsglobal.org/spec/lti-tool-configuration", "messages")
+                   &.find { |m| m["type"] == "LtiDeepLinkingRequest" }
+        dl_msg && dl_msg["target_link_uri"].present?
+      }
     end
   end
 
@@ -149,6 +167,7 @@ RSpec.describe Auth::Lti::Registrar do
       end
 
       it "allows an issuer in the allowlist" do
+        registrar.ensure_valid_configuration!
         expect(registrar).to be_valid
       end
     end
@@ -159,8 +178,9 @@ RSpec.describe Auth::Lti::Registrar do
       end
 
       it "rejects the issuer" do
+        registrar.ensure_valid_configuration!
         expect(registrar).not_to be_valid
-        expect(registrar.errors).to include("autoregistration for this platform is not allowed")
+        expect(registrar.errors).to include("Autoregistration for this platform is not allowed")
       end
     end
 
@@ -170,6 +190,7 @@ RSpec.describe Auth::Lti::Registrar do
       end
 
       it "allows any issuer" do
+        registrar.ensure_valid_configuration!
         expect(registrar).to be_valid
       end
     end
@@ -177,7 +198,7 @@ RSpec.describe Auth::Lti::Registrar do
 
   describe "scope normalization" do
     it "maps known LTI scope URIs to enum values" do
-      registrar.configure_platform!
+      registrar.register_platform!
       expect(LtiRegistration.last.scopes).to eq %w[nrps_membership_readonly ags_lineitem]
     end
 
@@ -188,14 +209,12 @@ RSpec.describe Auth::Lti::Registrar do
           token_endpoint_auth_method: "private_key_jwt",
           grant_types: %w[implicit client_credentials],
           scope: "https://purl.imsglobal.org/spec/lti-nrps/scope/contextmembership.readonly https://unknown.example.com/scope",
-          "https://purl.imsglobal.org/spec/lti-tool-configuration": {
-            deployment_id: "deploy_1"
-          }
+          "https://purl.imsglobal.org/spec/lti-tool-configuration": {}
         }
       end
 
       it "discards unrecognized scopes" do
-        registrar.configure_platform!
+        registrar.register_platform!
         expect(LtiRegistration.last.scopes).to eq %w[nrps_membership_readonly]
       end
     end
