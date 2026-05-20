99a139e81e8561009f0545838a62a17aae5ac863
Tim Frazee <tim@castironcoding.com>
2026-05-20 12:51:26 -0700

    [F] Hide email field on user profile if changes disallowed
    Instead shows "Your registered email address is", but it cannot be edited.

--- client/src/global/components/sign-in-up/EditProfileForm/ProfileFormFields/styles.js
+++ client/src/global/components/sign-in-up/EditProfileForm/ProfileFormFields/styles.js
@@ -3,3 +3,13 @@ import styled from "@emotion/styled";
 export const Text = styled.p`
   margin-block-end: 20px;
 `;
+
+export const EmailLabel = styled(Text)`
+  margin-block-end: 5px;
+`;
+
+export const TextBlock = styled.p`
+  font-size: clamp(17px, calc(17px + 3 * (100vw - 620px) / 404), 20px);
+  margin-block-end: 25px;
+  color: var(--highlight-color);
+`
