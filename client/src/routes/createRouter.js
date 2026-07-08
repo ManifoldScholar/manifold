3a2ab78ec2c9635d8398d03a860e5f3000914428
1aurend <imlcdavidson@gmail.com>
2026-07-07 22:10:20 -0700

    feat: port LTI routes from deep-linking

--- client/src/routes/createRouter.js
+++ client/src/routes/createRouter.js
@@ -1,12 +1,18 @@
 import frontendRoutes from "frontend/routes";
 import backendRoutes from "backend/routes";
 import readerRoutes from "reader/routes";
+import ltiRoutes from "lti/routes";
 import Manifold from "global/containers/Manifold";
 import linkHandler from "helpers/linkHandler";
 import RouteError from "global/components/FatalError/RouteError";
 
 export default function createRouter() {
-  const allRoutes = [...readerRoutes, ...frontendRoutes, ...backendRoutes];
+  const allRoutes = [
+    ...readerRoutes,
+    ...frontendRoutes,
+    ...backendRoutes,
+    ...ltiRoutes
+  ];
 
   const routes = [
     {
