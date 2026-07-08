0666e4052de2c719b6963648cdc54bfb91ad5646
1aurend <imlcdavidson@gmail.com>
2026-07-07 22:10:20 -0700

    feat: port LTI routes from deep-linking

---
+++ client/src/lti/containers/Landing/index.js
@@ -0,0 +1,34 @@
+import { useTranslation, Trans } from "react-i18next";
+import SearchQuery from "global/components/search/query";
+import { PRIMARY_FACETS } from "lti/containers/Search/filters";
+import IconComposer from "global/components/utility/IconComposer";
+import * as Styled from "./styles";
+
+export default function LtiLanding() {
+  const { t } = useTranslation();
+
+  return (
+    <Styled.Landing>
+      <IconComposer icon="DeepLinkingLogoUnique" size={100} />
+      <Styled.Title>{t("lti.landing.title")}</Styled.Title>
+      <Styled.Subtitle>{t("lti.landing.subtitle")}</Styled.Subtitle>
+      <Styled.Search>
+        <SearchQuery.Provider>
+          <SearchQuery.Form
+            action="/lti/search"
+            placeholder={t("lti.landing.placeholder")}
+          >
+            {PRIMARY_FACETS.map(facet => (
+              <input key={facet} type="hidden" name="facets" value={facet} />
+            ))}
+          </SearchQuery.Form>
+        </SearchQuery.Provider>
+      </Styled.Search>
+      <Styled.Message title={t("lti.landing.message_heading")}>
+        <Trans i18nKey="lti.landing.message_body" components={[<p />]} />
+      </Styled.Message>
+    </Styled.Landing>
+  );
+}
+
+LtiLanding.displayName = "Lti.LandingContainer";
